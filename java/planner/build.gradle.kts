92a13e133d70b916776d2ecfa69c5bd2607bffdd
Bowen <36908971+BowenXiao1999@users.noreply.github.com>
2021-07-22 09:11:57 +0800

    feat: add antlr gradle dependency

--- java/planner/build.gradle.kts
+++ java/planner/build.gradle.kts
@@ -1,7 +1,19 @@
+plugins {
+    antlr
+}
+
 dependencies {
     // Get recommended versions from platform project
     api(platform(project(":bom")))
 
     // Declare dependencies, no version required
     api("org.apache.calcite:calcite-core")
+
+    // TODO: Manage all dependency versions in one place.
+    antlr("org.antlr:antlr4:4.9.2")
+}
+
+tasks.generateGrammarSource {
+    arguments = arguments + listOf("-visitor")
+    outputDirectory = File("src/main/java/com/risingwave/parser/antlr/v4")
 }
