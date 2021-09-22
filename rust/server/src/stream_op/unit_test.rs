7aff893dba39d952032782a077eea8ba3e5be960
lmatz <lmatz823@gmail.com>
2021-09-22 11:28:41 +0800

    feat: Add Hash Dispatcher Unit Test
    * feat: Add Hash Dispatcher Unit Test
    
    * revision
    
    * remove rand

--- rust/server/src/stream_op/unit_test.rs
+++ rust/server/src/stream_op/unit_test.rs
@@ -1,8 +1,13 @@
 use super::data_source::*;
-use super::{LocalOutput, ProjectionOperator};
-use crate::array2::{Array, ArrayImpl};
+use super::{LocalOutput, Output, ProjectionOperator};
+use crate::array2::column::Column;
+use crate::array2::{Array, ArrayBuilder, ArrayImpl, I32ArrayBuilder};
 use crate::expr::{ArithmeticExpression, InputRefExpression};
-use crate::types::{ArithmeticOperatorKind, Int64Type};
+use crate::stream_op::Op;
+use crate::stream_op::{DataDispatcher, HashDataDispatcher, StreamChunk};
+use crate::types::{ArithmeticOperatorKind, Int32Type, Int64Type};
+use crate::util::hash_util::CRC32FastBuilder;
+use std::hash::{BuildHasher, Hasher};
 use std::sync::Arc;
 use std::time;
 use tokio::sync::Mutex;
@@ -57,4 +62,96 @@ async fn test_projection() {
         }
     }
     println!("{} items collected.", expected - start);
+}
+
+#[tokio::test]
+async fn test_hash_dispatcher() {
+    let num_outputs = 5;
+    let cardinality = 10;
+    let dimension = 4;
+    let key_indices = &[0, 2];
+    let output_data_vecs = (0..num_outputs)
+        .map(|_| Arc::new(Mutex::new(Vec::new())))
+        .collect::<Vec<_>>();
+    let outputs = output_data_vecs
+        .iter()
+        .map(|data| Box::new(MockOutput::new(data.clone())) as Box<dyn Output>)
+        .collect::<Vec<_>>();
+    let mut hash_dispatcher = HashDataDispatcher::new(outputs, key_indices.to_vec());
+
+    let mut ops = Vec::new();
+    for idx in 0..cardinality {
+        if idx % 2 == 0 {
+            ops.push(Op::Insert);
+        } else {
+            ops.push(Op::Delete);
+        }
+    }
+
+    let mut start = 19260817..;
+    let mut builders = (0..dimension)
+        .map(|_| I32ArrayBuilder::new(cardinality).unwrap())
+        .collect::<Vec<_>>();
+    let mut output_cols = vec![vec![vec![]; dimension]; num_outputs];
+    let mut output_ops = vec![vec![]; num_outputs];
+    for row_idx in 0..cardinality {
+        let hash_builder = CRC32FastBuilder {};
+        let mut hasher = hash_builder.build_hasher();
+        let one_row: Vec<i32> = (0..dimension)
+            .map(|_| start.next().unwrap())
+            .collect::<Vec<_>>();
+        for key_idx in key_indices.iter() {
+            let val = one_row[*key_idx];
+            let bytes = val.to_le_bytes();
+            hasher.update(&bytes);
+        }
+        let output_idx = hasher.finish() as usize % num_outputs;
+        for (builder, val) in builders.iter_mut().zip(one_row.iter()) {
+            builder.append(Some(*val)).unwrap();
+        }
+        output_cols[output_idx]
+            .iter_mut()
+            .zip(one_row.iter())
+            .for_each(|(each_column, val)| each_column.push(*val));
+        output_ops[output_idx].push(ops[row_idx]);
+    }
+
+    let columns = builders
+        .into_iter()
+        .map(|builder| {
+            let array = builder.finish().unwrap();
+            Column::new(Arc::new(array.into()), Arc::new(Int32Type::new(false)))
+        })
+        .collect::<Vec<_>>();
+
+    let chunk = StreamChunk {
+        ops,
+        columns,
+        visibility: None,
+        cardinality,
+    };
+    hash_dispatcher.dispatch_data(chunk).await.unwrap();
+
+    for (output_idx, output) in output_data_vecs.into_iter().enumerate() {
+        let guard = output.lock().await;
+        assert_eq!(guard.len(), 1);
+        let real_chunk = guard.get(0).unwrap();
+        real_chunk
+            .columns
+            .iter()
+            .zip(output_cols[output_idx].iter())
+            .for_each(|(real_col, expect_col)| {
+                let real_vals = real_chunk
+                    .visibility
+                    .as_ref()
+                    .unwrap()
+                    .iter()
+                    .enumerate()
+                    .filter(|(_, vis)| *vis)
+                    .map(|(row_idx, _)| real_col.array_ref().as_int32().value_at(row_idx).unwrap())
+                    .collect::<Vec<_>>();
+                assert_eq!(real_vals.len(), expect_col.len());
+                assert_eq!(real_vals, *expect_col);
+            });
+    }
 }
