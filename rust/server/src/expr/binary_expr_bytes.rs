8aa668c73071415201a51d6fc9e7d25ad7981700
August <yezizp2012@gmail.com>
2021-09-27 15:05:22 +0800

    refactor: add macro gen_expr_bytes

--- rust/server/src/expr/binary_expr_bytes.rs
+++ rust/server/src/expr/binary_expr_bytes.rs
@@ -1,122 +1,18 @@
-use crate::array2::{
-    Array, ArrayBuilder, ArrayImpl, ArrayRef, BytesGuard, BytesWriter, DataChunk, I32Array,
-    UTF8Array, UTF8ArrayBuilder,
-};
-use crate::error::Result;
-use crate::expr::{BoxedExpression, Expression};
-use crate::types::{DataType, DataTypeRef};
+use crate::array2::{I32Array, UTF8Array};
+use crate::expr::expr_tmpl::BinaryBytesExpression;
+use crate::expr::BoxedExpression;
+use crate::types::DataTypeRef;
 use crate::vector_op::substr::*;
 use std::marker::PhantomData;
-use std::sync::Arc;
-
-struct BinaryBytesExpression<
-    IA1: Array,
-    IA2: Array,
-    F: for<'a> Fn(IA1::RefItem<'a>, IA2::RefItem<'a>, BytesWriter) -> Result<BytesGuard>,
-> {
-    left_expr: BoxedExpression,
-    right_expr: BoxedExpression,
-    return_type: DataTypeRef,
-    func: F,
-    data1: PhantomData<(IA1, IA2)>,
-}
-
-impl<
-        IA1: Array,
-        IA2: Array,
-        F: for<'a> Fn(IA1::RefItem<'a>, IA2::RefItem<'a>, BytesWriter) -> Result<BytesGuard>
-            + Sized
-            + Sync
-            + Send,
-    > Expression for BinaryBytesExpression<IA1, IA2, F>
-where
-    for<'a> &'a IA1: std::convert::From<&'a ArrayImpl>,
-    for<'a> &'a IA2: std::convert::From<&'a ArrayImpl>,
-{
-    fn return_type(&self) -> &dyn DataType {
-        &*self.return_type
-    }
-
-    fn return_type_ref(&self) -> DataTypeRef {
-        self.return_type.clone()
-    }
-
-    fn eval(&mut self, data_chunk: &DataChunk) -> Result<ArrayRef> {
-        let left_ret = self.left_expr.eval(data_chunk)?;
-        let right_ret = self.right_expr.eval(data_chunk)?;
-        let left_arr: &IA1 = left_ret.as_ref().into();
-        let right_arr: &IA2 = right_ret.as_ref().into();
-
-        let bitmap = data_chunk.get_visibility_ref();
-        let mut output_array = UTF8ArrayBuilder::new(data_chunk.capacity())?;
-        // TODO: Consider simplify the branch below.
-        Ok(Arc::new(match bitmap {
-            Some(bitmap) => {
-                for ((l, r), visible) in left_arr.iter().zip(right_arr.iter()).zip(bitmap.iter()) {
-                    if !visible {
-                        continue;
-                    }
-                    if let (Some(l), Some(r)) = (l, r) {
-                        let writer = output_array.writer();
-                        let guard = (self.func)(l, r, writer)?;
-                        output_array = guard.into_inner();
-                    } else {
-                        output_array.append(None)?;
-                    }
-                }
-                output_array.finish()?.into()
-            }
-            None => {
-                for (l, r) in left_arr.iter().zip(right_arr.iter()) {
-                    if let (Some(l), Some(r)) = (l, r) {
-                        let writer = output_array.writer();
-                        let guard = (self.func)(l, r, writer)?;
-                        output_array = guard.into_inner();
-                    } else {
-                        output_array.append(None)?;
-                    }
-                }
-                output_array.finish()?.into()
-            }
-        }))
-    }
-}
-
-impl<
-        IA1: Array,
-        IA2: Array,
-        F: for<'a> Fn(IA1::RefItem<'a>, IA2::RefItem<'a>, BytesWriter) -> Result<BytesGuard>
-            + Sized
-            + Sync
-            + Send,
-    > BinaryBytesExpression<IA1, IA2, F>
-{
-    // Compile failed due to some GAT lifetime issues so make this field private.
-    // Check issues #742.
-    fn new(
-        left_expr: BoxedExpression,
-        right_expr: BoxedExpression,
-        return_type: DataTypeRef,
-        func: F,
-    ) -> Self {
-        Self {
-            left_expr,
-            right_expr,
-            return_type,
-            func,
-            data1: PhantomData,
-        }
-    }
-}
 
 pub fn new_substr_start(
-    left_expr: BoxedExpression,
-    right_expr: BoxedExpression,
+    expr_ia1: BoxedExpression,
+    expr_ia2: BoxedExpression,
     return_type: DataTypeRef,
 ) -> BoxedExpression {
     Box::new(BinaryBytesExpression::<UTF8Array, I32Array, _> {
-        left_expr,
-        right_expr,
+        expr_ia1,
+        expr_ia2,
         return_type,
         func: substr_start,
         data1: PhantomData,
@@ -124,13 +20,13 @@ pub fn new_substr_start(
 }
 
 pub fn new_substr_end(
-    left_expr: BoxedExpression,
-    right_expr: BoxedExpression,
+    expr_ia1: BoxedExpression,
+    expr_ia2: BoxedExpression,
     return_type: DataTypeRef,
 ) -> BoxedExpression {
     Box::new(BinaryBytesExpression::<UTF8Array, I32Array, _> {
-        left_expr,
-        right_expr,
+        expr_ia1,
+        expr_ia2,
         return_type,
         func: substr_end,
         data1: PhantomData,
--- rust/server/src/expr/expr_tmpl.rs
+++ rust/server/src/expr/expr_tmpl.rs
@@ -1,4 +1,6 @@
-use crate::array2::{Array, ArrayBuilder, ArrayImpl, ArrayRef, DataChunk};
+use crate::array2::{
+    Array, ArrayBuilder, ArrayImpl, ArrayRef, BytesGuard, BytesWriter, DataChunk, UTF8ArrayBuilder,
+};
 use crate::error::Result;
 use crate::expr::{BoxedExpression, Expression};
 use crate::types::{DataType, DataTypeRef, Scalar};
@@ -101,6 +103,99 @@ macro_rules! gen_expr_normal {
   }
 }
 
+macro_rules! gen_expr_bytes {
+  ($ty_name:ident, $($arg:ident),*) => {
+    paste! {
+      pub struct $ty_name<
+        $($arg: Array, )*
+        F: for<'a> Fn($($arg::RefItem<'a>, )* BytesWriter) -> Result<BytesGuard>,
+      > {
+        // pub the fields in super mod, so that we can construct it directly.
+        // FIXME: make private while new function available.
+        $(pub(super) [<expr_ $arg:lower>]: BoxedExpression,)*
+        pub(super) return_type: DataTypeRef,
+        pub(super) func: F,
+        pub(super) data1: std::marker::PhantomData<($($arg, )*)>,
+      }
+
+      impl<$($arg: Array, )*
+        F: for<'a> Fn($($arg::RefItem<'a>, )* BytesWriter) -> Result<BytesGuard> + Sized + Sync + Send,
+      > Expression for $ty_name<$($arg, )* F>
+      where
+        $(for<'a> &'a $arg: std::convert::From<&'a ArrayImpl>,)*
+      {
+        fn return_type(&self) -> &dyn DataType {
+          &*self.return_type
+        }
+
+        fn return_type_ref(&self) -> DataTypeRef {
+          self.return_type.clone()
+        }
+
+        fn eval(&mut self, data_chunk: &DataChunk) -> Result<ArrayRef> {
+          $(
+            let [<ret_ $arg:lower>] = self.[<expr_ $arg:lower>].eval(data_chunk)?;
+            let [<arr_ $arg:lower>]: &$arg = [<ret_ $arg:lower>].as_ref().into();
+          )*
+
+          let bitmap = data_chunk.get_visibility_ref();
+          let mut output_array = UTF8ArrayBuilder::new(data_chunk.capacity())?;
+          Ok(Arc::new(match bitmap {
+            Some(bitmap) => {
+              for (($([<v_ $arg:lower>], )*), visible) in multizip(($([<arr_ $arg:lower>].iter(), )*)).zip(bitmap.iter()) {
+                if !visible {
+                  continue;
+                }
+                if let ($(Some([<v_ $arg:lower>]), )*) = ($([<v_ $arg:lower>], )*) {
+                  let writer = output_array.writer();
+                  let guard = (self.func)($([<v_ $arg:lower>], )* writer)?;
+                  output_array = guard.into_inner();
+                } else {
+                  output_array.append(None)?;
+                }
+              }
+              output_array.finish()?.into()
+            }
+            None => {
+              for ($([<v_ $arg:lower>], )*) in multizip(($([<arr_ $arg:lower>].iter(), )*)) {
+                if let ($(Some([<v_ $arg:lower>]), )*) = ($([<v_ $arg:lower>], )*) {
+                  let writer = output_array.writer();
+                  let guard = (self.func)($([<v_ $arg:lower>], )* writer)?;
+                  output_array = guard.into_inner();
+                } else {
+                  output_array.append(None)?;
+                }
+              }
+              output_array.finish()?.into()
+            }
+          }))
+        }
+      }
+
+      impl<$($arg: Array, )*
+        F: for<'a> Fn($($arg::RefItem<'a>, )* BytesWriter) -> Result<BytesGuard> + Sized + Sync + Send,
+      > $ty_name<$($arg, )* F> {
+        fn new(
+          $([<expr_ $arg:lower>]: BoxedExpression, )*
+          return_type: DataTypeRef,
+          func: F,
+        ) -> Self {
+          Self {
+            $([<expr_ $arg:lower>], )*
+            return_type,
+            func,
+            data1: std::marker::PhantomData,
+          }
+        }
+      }
+    }
+  }
+}
+
 gen_expr_normal!(UnaryExpression, IA1);
 gen_expr_normal!(BinaryExpression, IA1, IA2);
 gen_expr_normal!(TenaryExpression, IA1, IA2, IA3);
+
+gen_expr_bytes!(UnaryBytesExpression, IA1);
+gen_expr_bytes!(BinaryBytesExpression, IA1, IA2);
+gen_expr_bytes!(TenaryBytesExpression, IA1, IA2, IA3);
