8142747a105a9ccbb4d61180ddacd261ac6578f3
Yanghao <wyhyhyhyh@163.com>
2021-10-01 03:42:31 +0800

    feat: add example proto cases for streaming plan
    * local update
    
    * add more test cases
    
    * update examples according to new agg proto
    
    * Revert "update examples according to new agg proto"
    
    This reverts commit d287ce0ef88bdfdc72c7327750f0ed82099eac6e.
    
    * update test cases according to new aggr proto

--- java/planner/src/test/java/com/risingwave/planner/MaterializedViewPlanTest.java
+++ java/planner/src/test/java/com/risingwave/planner/MaterializedViewPlanTest.java
@@ -1,13 +1,21 @@
 package com.risingwave.planner;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
+import com.risingwave.execution.handler.CreateMaterializedViewHandler;
 import com.risingwave.planner.rel.physical.streaming.StreamingPlan;
 import com.risingwave.planner.rel.serialization.ExplainWriter;
+import com.risingwave.planner.util.PlanTestCaseLoader;
+import com.risingwave.planner.util.PlannerTestCase;
+import com.risingwave.planner.util.ToPlannerTestCase;
+import com.risingwave.proto.streaming.plan.StreamNode;
+import com.risingwave.rpc.Messages;
 import org.apache.calcite.sql.SqlNode;
+import org.apache.calcite.sql.ddl.SqlCreateMaterializedView;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ArgumentsSource;
 
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class MaterializedViewPlanTest extends StreamPlanTestBase {
@@ -18,19 +26,29 @@ public class MaterializedViewPlanTest extends StreamPlanTestBase {
 
   @Test
   void testStreamPlanCase1() {
-    // TODO: implement serializer and migrate this test case to TestBase implementation.
-    String sql = "create materialized view T1 as select sum(v2) as V from t where v1>1";
+    // A sample code for stream testing. Keep this for debugging.
+    String sql = "create materialized view T_test as select sum(v1)+1 as V from t where v1>v2";
     System.out.println("query sql: \n" + sql);
     SqlNode ast = parseSql(sql);
     StreamingPlan plan = streamPlanner.plan(ast, executionContext);
     String explainPlan = ExplainWriter.explainPlan(plan.getStreamingPlan());
 
-    String expectedPlan =
-        "RwStreamMaterializedView\n"
-            + "  RwStreamAgg(group=[{}], v=[SUM($0)])\n"
-            + "    RwStreamProject(v2=[$1])\n"
-            + "      RwStreamFilter(condition=[>($0, 1)])\n"
-            + "        RwStreamTableSource(table=[[test_schema, t]])";
-    assertEquals(explainPlan, expectedPlan, "Plan not match!");
+    CreateMaterializedViewHandler handler = new CreateMaterializedViewHandler();
+    SqlCreateMaterializedView createMaterializedView = (SqlCreateMaterializedView) ast;
+    String tableName = createMaterializedView.name.getSimple();
+    handler.convertPlanToCatalog(tableName, plan, executionContext);
+
+    StreamNode serializedProto = serializer.serialize(plan);
+    String serializedJsonPlan = Messages.jsonFormat(serializedProto);
+    System.out.println(serializedJsonPlan);
+  }
+
+  @ParameterizedTest(name = "{index} => {0}")
+  @DisplayName("Streaming plan tests")
+  @ArgumentsSource(PlanTestCaseLoader.class)
+  public void testStreamingPlan(@ToPlannerTestCase PlannerTestCase testCase) {
+    // Comment the line below if you want to make quick proto changes and this test block the
+    // progress.
+    runTestCase(testCase);
   }
 }
--- java/planner/src/test/java/com/risingwave/planner/StreamPlanTestBase.java
+++ java/planner/src/test/java/com/risingwave/planner/StreamPlanTestBase.java
@@ -1,10 +1,19 @@
 package com.risingwave.planner;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import com.risingwave.execution.handler.CreateMaterializedViewHandler;
 import com.risingwave.planner.planner.streaming.StreamPlanner;
+import com.risingwave.planner.rel.physical.streaming.StreamingPlan;
+import com.risingwave.planner.rel.serialization.ExplainWriter;
 import com.risingwave.planner.rel.serialization.StreamPlanSerializer;
+import com.risingwave.planner.util.PlannerTestCase;
 import com.risingwave.planner.util.PlannerTestDdlLoader;
+import com.risingwave.proto.streaming.plan.StreamNode;
+import com.risingwave.rpc.Messages;
 import java.util.List;
 import org.apache.calcite.sql.SqlNode;
+import org.apache.calcite.sql.ddl.SqlCreateMaterializedView;
 
 public abstract class StreamPlanTestBase extends SqlTestBase {
   protected StreamPlanner streamPlanner;
@@ -28,6 +37,29 @@ public abstract class StreamPlanTestBase extends SqlTestBase {
           .getSqlHandlerFactory()
           .create(ddlSql, executionContext)
           .handle(ddlSql, executionContext);
+    }
+  }
+
+  protected void runTestCase(PlannerTestCase testCase) {
+    String sql = testCase.getSql();
+
+    SqlNode ast = parseSql(sql);
+    StreamingPlan plan = streamPlanner.plan(ast, executionContext);
+
+    String explainedPlan = ExplainWriter.explainPlan(plan.getStreamingPlan());
+    assertEquals(testCase.getPlan(), explainedPlan, "Plans do not match!");
+
+    // We must pass the stream plan through MV handler to assign a TableId.
+    CreateMaterializedViewHandler handler = new CreateMaterializedViewHandler();
+    SqlCreateMaterializedView createMaterializedView = (SqlCreateMaterializedView) ast;
+    String tableName = createMaterializedView.name.getSimple();
+    handler.convertPlanToCatalog(tableName, plan, executionContext);
+
+    // Do not error if no json test.
+    if (testCase.getJson() != null) {
+      StreamNode serializedProto = serializer.serialize(plan);
+      String serializedJsonPlan = Messages.jsonFormat(serializedProto);
+      assertEquals(testCase.getJson(), serializedJsonPlan, "Proto json not match!");
     }
   }
 }
