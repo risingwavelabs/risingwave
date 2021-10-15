2ade721658ddaac05e6942810f0ffb3433f40d3b
Bowen <36908971+BowenXiao1999@users.noreply.github.com>
2021-10-15 15:03:04 +0800

    feat: add PG compare in CI

--- scripts/sqllogictest_java.py
+++ scripts/sqllogictest_java.py
@@ -1,12 +1,21 @@
 import os
 import sys
 import subprocess
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument("-p", "--port")
+parser.add_argument("-db", "--pgdb", default="postgres")
+parser.add_argument("-u", "--user", default="postgres")
+# Note we do not need pass word yet.
+args = parser.parse_args()
 return_code = 0
 for file in os.listdir("./e2e_test_java/"):
     if file.endswith(".slt"):
         proc = subprocess.run([os.path.abspath("./go/bin/sqllogictest"),
-                                                       "-port", "4567", "-file", "e2e_test_java/" + file, "-pgdb", "dev"])
+                                                       "-port", args.port, "-file", "e2e_test_java/" + file, "-pgdb", args.pgdb])
         if proc.returncode != 0:
             return_code = 1
-
+            break
+        
 sys.exit(return_code)
