9b80f7943bdf515ac180ac6813137dff7b5605d3
Renjie Liu <liurenjie2008@gmail.com>
2021-10-28 11:28:25 +0800

    fix: Olap nested loop join input may have incorrect distribution.
    * fix: Batch nested loop join input may have incorrect distribution.

--- java/planner/src/main/java/com/risingwave/planner/rules/physical/batch/join/BatchNestedLoopJoinRule.java
+++ java/planner/src/main/java/com/risingwave/planner/rules/physical/batch/join/BatchNestedLoopJoinRule.java
@@ -1,5 +1,7 @@
 package com.risingwave.planner.rules.physical.batch.join;
 
+import static com.risingwave.execution.context.ExecutionContext.contextOf;
+import static com.risingwave.planner.planner.PlannerUtils.isSingleMode;
 import static com.risingwave.planner.rel.physical.batch.RisingWaveBatchPhyRel.BATCH_PHYSICAL;
 import static com.risingwave.planner.rules.physical.batch.join.BatchJoinRules.isEquiJoin;
 
@@ -10,6 +12,7 @@ import org.apache.calcite.plan.RelOptRule;
 import org.apache.calcite.plan.RelOptRuleCall;
 import org.apache.calcite.plan.RelRule;
 
+/** Implementation rule for generating nested loop join plan. */
 public class BatchNestedLoopJoinRule extends RelRule<BatchNestedLoopJoinRule.Config> {
   protected BatchNestedLoopJoinRule(Config config) {
     super(config);
@@ -26,12 +29,30 @@ public class BatchNestedLoopJoinRule extends RelRule<BatchNestedLoopJoinRule.Con
   @Override
   public void onMatch(RelOptRuleCall call) {
     RwLogicalJoin join = call.rel(0);
+    var joinType = join.getJoinType();
     var left = join.getLeft();
-    var leftTraits = left.getTraitSet().plus(BATCH_PHYSICAL).plus(RwDistributions.ANY).simplify();
+    var leftTraits = left.getTraitSet().plus(BATCH_PHYSICAL);
+    if (!isSingleMode(contextOf(join.getCluster()))) {
+      // Full or Right Outer
+      if (joinType.generatesNullsOnLeft()) {
+        leftTraits = leftTraits.plus(RwDistributions.SINGLETON);
+      }
+    }
+    leftTraits = leftTraits.simplify();
     var newLeft = convert(left, leftTraits);
 
     var right = join.getRight();
-    var rightTraits = right.getTraitSet().plus(BATCH_PHYSICAL).plus(RwDistributions.ANY).simplify();
+    var rightTraits = right.getTraitSet().plus(BATCH_PHYSICAL);
+    if (!isSingleMode(contextOf(join.getCluster()))) {
+      // Full or Right Outer
+      if (joinType.generatesNullsOnLeft()) {
+        rightTraits = rightTraits.plus(RwDistributions.SINGLETON);
+      } else {
+        rightTraits = rightTraits.plus(RwDistributions.BROADCAST_DISTRIBUTED);
+      }
+    }
+    rightTraits = rightTraits.simplify();
+
     var newRight = convert(right, rightTraits);
 
     var newJoinTraits = newLeft.getTraitSet();
@@ -48,6 +69,7 @@ public class BatchNestedLoopJoinRule extends RelRule<BatchNestedLoopJoinRule.Con
     call.transformTo(newJoin);
   }
 
+  /** Configuration for {@link BatchNestedLoopJoinRule}. */
   public interface Config extends RelRule.Config {
     Config DEFAULT =
         Config.EMPTY
