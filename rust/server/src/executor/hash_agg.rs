a5d633418b0f6cd4971932cb90d726a6723dd986
stonepage <40830455+st1page@users.noreply.github.com>
2021-11-10 17:48:39 +0800

    feat: derive HashKey type for hash agg
    * derive HashKey type for hash agg
    
    * hash agg unit test use proto to test deserialize

--- rust/server/src/executor/hash_agg.rs
+++ rust/server/src/executor/hash_agg.rs
@@ -6,32 +6,47 @@ use prost::Message;
 use risingwave_common::array::column::Column;
 use risingwave_common::array::{DataChunk, RwError};
 use risingwave_common::catalog::{Field, Schema};
-use risingwave_common::collection::hash_map::{HashKey, PrecomputedBuildHasher, SerializedKey};
+use risingwave_common::collection::hash_map::hash_key_dispatch;
+use risingwave_common::collection::hash_map::PrecomputedBuildHasher;
+use risingwave_common::collection::hash_map::{calc_hash_key_kind, HashKey, HashKeyDispatcher};
+use risingwave_common::collection::hash_map::{
+    HashKeyKind, Key128, Key16, Key256, Key32, Key64, KeySerialized,
+};
 use risingwave_common::error::{ErrorCode, Result};
 use risingwave_common::types::{DataType, DataTypeRef};
 use risingwave_common::vector_op::agg::{AggStateFactory, BoxedAggState};
 use risingwave_pb::plan::plan_node::PlanNodeType;
 use risingwave_pb::plan::HashAggNode;
 use std::collections::HashMap;
+use std::marker::PhantomData;
 use std::sync::Arc;
 use std::{mem, vec};
 
 type AggHashMap<K> = HashMap<K, Vec<BoxedAggState>, PrecomputedBuildHasher>;
-pub(super) struct HashAggExecutorBuilder;
-impl BoxedExecutorBuilder for HashAggExecutorBuilder {
-    fn new_boxed_executor(source: &ExecutorBuilder) -> Result<BoxedExecutor> {
-        ensure!(source.plan_node().get_node_type() == PlanNodeType::HashAgg);
-        ensure!(source.plan_node().get_children().len() == 1);
-        let proto_child = source
-            .plan_node()
-            .get_children()
-            .get(0)
-            .ok_or_else(|| ErrorCode::InternalError(String::from("")))?;
-        let child = source.clone_for_plan(proto_child).build()?;
 
-        let hash_agg_node = HashAggNode::decode(&(source.plan_node()).get_body().value[..])
-            .map_err(|e| RwError::from(ErrorCode::ProstError(e)))?;
+struct HashAggExecutorBuilderDispatcher<K> {
+    _marker: PhantomData<K>,
+}
 
+/// A dispatcher to help create specialized hash agg executor.
+impl<K: HashKey> HashKeyDispatcher<K> for HashAggExecutorBuilderDispatcher<K> {
+    type Input = HashAggExecutorBuilder;
+    type Output = BoxedExecutor;
+
+    fn dispatch(input: HashAggExecutorBuilder) -> Self::Output {
+        Box::new(HashAggExecutor::<K>::new(input))
+    }
+}
+pub(super) struct HashAggExecutorBuilder {
+    agg_factories: Vec<AggStateFactory>,
+    group_key_columns: Vec<usize>,
+    child: BoxedExecutor,
+    group_key_types: Vec<DataTypeRef>,
+    schema: Schema,
+}
+
+impl HashAggExecutorBuilder {
+    fn deserialize(hash_agg_node: &HashAggNode, child: BoxedExecutor) -> Result<BoxedExecutor> {
         let group_key_columns = hash_agg_node
             .get_group_keys()
             .iter()
@@ -58,16 +73,38 @@ impl BoxedExecutorBuilder for HashAggExecutorBuilder {
             .map(|t| Field { data_type: t })
             .collect::<Vec<Field>>();
 
-        // todo, calc the K type
-        Ok(Box::new(HashAggExecutor::<SerializedKey> {
+        let hash_key_kind = calc_hash_key_kind(&group_key_types);
+
+        let builder = HashAggExecutorBuilder {
             agg_factories,
             group_key_columns,
             child,
-            groups: AggHashMap::<SerializedKey>::default(),
             group_key_types,
-            result: None,
             schema: Schema { fields },
-        }) as BoxedExecutor)
+        };
+
+        Ok(hash_key_dispatch!(
+            hash_key_kind,
+            HashAggExecutorBuilderDispatcher,
+            builder
+        ))
+    }
+}
+
+impl BoxedExecutorBuilder for HashAggExecutorBuilder {
+    fn new_boxed_executor(source: &ExecutorBuilder) -> Result<BoxedExecutor> {
+        ensure!(source.plan_node().get_node_type() == PlanNodeType::HashAgg);
+        ensure!(source.plan_node().get_children().len() == 1);
+        let proto_child = source
+            .plan_node()
+            .get_children()
+            .get(0)
+            .ok_or_else(|| ErrorCode::InternalError(String::from("")))?;
+        let child = source.clone_for_plan(proto_child).build()?;
+
+        let hash_agg_node = HashAggNode::decode(&(source.plan_node()).get_body().value[..])
+            .map_err(|e| RwError::from(ErrorCode::ProstError(e)))?;
+        Self::deserialize(&hash_agg_node, child)
     }
 }
 /// `HashAggExecutor` implements the hash aggregate algorithm.
@@ -85,6 +122,20 @@ pub(super) struct HashAggExecutor<K> {
     /// the data types of key columns
     group_key_types: Vec<DataTypeRef>,
     schema: Schema,
+}
+
+impl<K> HashAggExecutor<K> {
+    fn new(builder: HashAggExecutorBuilder) -> Self {
+        HashAggExecutor {
+            agg_factories: builder.agg_factories,
+            group_key_columns: builder.group_key_columns,
+            child: builder.child,
+            groups: AggHashMap::<K>::default(),
+            group_key_types: builder.group_key_types,
+            result: None,
+            schema: builder.schema,
+        }
+    }
 }
 
 #[async_trait::async_trait]
@@ -139,15 +190,13 @@ impl<K: HashKey + Send + Sync> Executor for HashAggExecutor<K> {
                 .try_for_each(|(aggregator, builder)| aggregator.output(builder))?;
         }
 
-        let columns = mem::take(&mut self.group_key_types)
-            .into_iter()
-            .chain(
-                self.agg_factories
-                    .iter()
-                    .map(|agg_factory| agg_factory.get_return_type()),
-            )
+        let columns = self
+            .schema
+            .fields
+            .iter()
+            .cloned()
             .zip(group_builders.into_iter().chain(agg_builders))
-            .map(|(t, b)| Ok(Column::new(Arc::new(b.finish()?), t)))
+            .map(|(t, b)| Ok(Column::new(Arc::new(b.finish()?), t.data_type)))
             .collect::<Result<Vec<_>>>()?;
 
         let ret = DataChunk::builder().columns(columns).build();
@@ -218,7 +267,7 @@ mod tests {
             },
         );
 
-        let prost = AggCall {
+        let agg_call = AggCall {
             r#type: Type::Sum as i32,
             args: vec![Arg {
                 input: Some(InputRefExpr { column_idx: 2 }),
@@ -233,7 +282,13 @@ mod tests {
             }),
         };
 
-        let agg_factory = AggStateFactory::new(&prost).unwrap();
+        let agg_prost = HashAggNode {
+            group_keys: vec![0, 1],
+            agg_calls: vec![agg_call],
+        };
+
+        let actual_exec =
+            HashAggExecutorBuilder::deserialize(&agg_prost, Box::new(src_exec)).unwrap();
 
         let schema = Schema {
             fields: vec![
@@ -248,15 +303,7 @@ mod tests {
                 },
             ],
         };
-        let actual_exec = HashAggExecutor {
-            agg_factories: vec![agg_factory],
-            group_key_columns: vec![0, 1],
-            child: Box::new(src_exec),
-            groups: AggHashMap::<SerializedKey>::default(),
-            group_key_types: vec![t32.clone(), t32.clone()],
-            result: None,
-            schema: schema.clone(),
-        };
+
         // TODO: currently the order is fixed
         let group1_col = Arc::new(array_nonnull! { I32Array, [0,1,0,1] }.into());
         let group2_col = Arc::new(array_nonnull! { I32Array, [0,1,1,0] }.into());
@@ -272,6 +319,6 @@ mod tests {
                 .build(),
             schema,
         );
-        diff_executor_output(Box::new(actual_exec), Box::new(expect_exec)).await;
+        diff_executor_output(actual_exec, Box::new(expect_exec)).await;
     }
 }
