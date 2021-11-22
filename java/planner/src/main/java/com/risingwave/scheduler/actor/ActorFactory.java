8a284deaba84119d39c5d1926886845e0feea6cb
Tao Wu <wutao.as.neverchanje@gmail.com>
2021-11-22 17:39:24 +0800

    ci: include streaming e2e tests in Java coverage

--- java/planner/src/main/java/com/risingwave/scheduler/actor/ActorFactory.java
+++
@@ -1,8 +0,0 @@
-package com.risingwave.scheduler.actor;
-
-import akka.actor.typed.ActorRef;
-import akka.actor.typed.Behavior;
-
-public interface ActorFactory {
-  <T> ActorRef<T> createActor(Behavior<T> behavior, String name);
-}
