98179535ef663b46b0cac6e0d519f6435a0e08ee
Tao Wu <wutao.as.neverchanje@gmail.com>
2021-11-23 12:58:46 +0800

    test: selecting all data types

--- scripts/sqllogictest.py
+++ scripts/sqllogictest.py
@@ -2,6 +2,7 @@ import os
 import sys
 import subprocess
 import argparse
+import glob
 
 parser = argparse.ArgumentParser()
 parser.add_argument("-p", "--port", default="4567")
@@ -11,12 +12,13 @@ parser.add_argument("-f", "--files", default="./e2e_test/distributed/")
 # Note we do not need pass word yet.
 args = parser.parse_args()
 return_code = 0
-for file in os.listdir(args.files):
-    if file.endswith(".slt"):
-        proc = subprocess.run([os.path.abspath("./go/bin/sqllogictest"),
-                                                       "-port", args.port, "-file", args.files + file, "-pgdb", args.pgdb])
-        if proc.returncode != 0:
-            return_code = 1
-            break
-        
+
+files = glob.glob(os.path.join(args.files, "**/*.slt"), recursive=True)
+for file in files:
+    proc = subprocess.run([os.path.abspath("./go/bin/sqllogictest"),
+                           "-port", args.port, "-file", file, "-pgdb", args.pgdb])
+    if proc.returncode != 0:
+        return_code = 1
+        break
+
 sys.exit(return_code)
