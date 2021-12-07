20f554b53a5847f0f88da2f3d9c84e96cf8f1fc3
Liang <44948473+soundOfDestiny@users.noreply.github.com>
2021-12-07 19:19:08 +0800

    test(expr): add tests to expr_binary_bytes
    close

--- rust/common/src/expr/expr_binary_bytes.rs
+++ rust/common/src/expr/expr_binary_bytes.rs
@@ -33,3 +33,73 @@ pub fn new_substr_for(
         _phantom: PhantomData,
     })
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::array::DataChunk;
+    use crate::expr::LiteralExpression;
+    use crate::types::{Datum, Int32Type, ScalarImpl, StringType};
+
+    fn create_str_i32_binary_expr(
+        f: fn(BoxedExpression, BoxedExpression, DataTypeRef) -> BoxedExpression,
+        str_arg: Datum,
+        i32_arg: Datum,
+    ) -> BoxedExpression {
+        f(
+            Box::new(LiteralExpression::new(
+                StringType::create(false, 100, crate::types::DataTypeKind::Char),
+                str_arg,
+            )),
+            Box::new(LiteralExpression::new(Int32Type::create(false), i32_arg)),
+            StringType::create(false, 100, crate::types::DataTypeKind::Char),
+        )
+    }
+
+    #[test]
+    fn test_substr() {
+        let text = "quick brown";
+        let start_pos = 3;
+        let for_pos = 4;
+        let mut res;
+
+        let mut substr_start_normal = create_str_i32_binary_expr(
+            new_substr_start,
+            Some(ScalarImpl::from(String::from(text))),
+            Some(ScalarImpl::Int32(start_pos)),
+        );
+        res = substr_start_normal.eval(&DataChunk::new_dummy(1)).unwrap();
+        assert_eq!(
+            res.to_datum(),
+            Some(ScalarImpl::from(String::from(
+                &text[start_pos as usize - 1..]
+            )))
+        );
+
+        let mut substr_start_i32_none = create_str_i32_binary_expr(
+            new_substr_start,
+            Some(ScalarImpl::from(String::from(text))),
+            None,
+        );
+        res = substr_start_i32_none
+            .eval(&DataChunk::new_dummy(1))
+            .unwrap();
+        assert_eq!(res.to_datum(), None);
+
+        let mut substr_for_normal = create_str_i32_binary_expr(
+            new_substr_for,
+            Some(ScalarImpl::from(String::from(text))),
+            Some(ScalarImpl::Int32(for_pos)),
+        );
+        res = substr_for_normal.eval(&DataChunk::new_dummy(1)).unwrap();
+        assert_eq!(
+            res.to_datum(),
+            Some(ScalarImpl::from(String::from(&text[..for_pos as usize])))
+        );
+
+        let mut substr_for_str_none =
+            create_str_i32_binary_expr(new_substr_for, None, Some(ScalarImpl::Int32(for_pos)));
+        res = substr_for_str_none.eval(&DataChunk::new_dummy(1)).unwrap();
+        assert_eq!(res.to_datum(), None);
+    }
+}
