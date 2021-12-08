64993cac0ad114bfdc22ab9707dc481231c776b6
Liang <44948473+soundOfDestiny@users.noreply.github.com>
2021-12-08 11:36:10 +0800

    fix(vector_op): fix substr_for_len return err when len<0

--- rust/common/src/vector_op/substr.rs
+++ rust/common/src/vector_op/substr.rs
@@ -1,5 +1,5 @@
 use crate::array::{BytesGuard, BytesWriter};
-use crate::error::Result;
+use crate::error::{ErrorCode, Result};
 use std::cmp::{max, min};
 
 #[inline(always)]
@@ -21,6 +21,13 @@ pub fn substr_start_for(
     count: i32,
     writer: BytesWriter,
 ) -> Result<BytesGuard> {
+    if count < 0 {
+        return Err(ErrorCode::InvalidInputSyntax(
+            String::from("non-negative substring length"),
+            count.to_string(),
+        )
+        .into());
+    }
     let begin = max(start - 1, 0) as usize;
     let end = min(max(start - 1 + count, 0) as usize, s.len());
     writer.write_ref(&s[begin..end])
@@ -38,7 +45,9 @@ mod tests {
         let cases = [
             (s.to_owned(), Some(4), None, "cgccdd"),
             (s.to_owned(), None, Some(3), "cxs"),
+            (s.to_owned(), Some(4), Some(-2), "[unused result]"),
             (s.to_owned(), Some(4), Some(2), "cg"),
+            (s.to_owned(), Some(-1), Some(-5), "[unused result]"),
             (s.to_owned(), Some(-1), Some(5), "cxs"),
         ];
 
@@ -46,7 +55,15 @@ mod tests {
             let builder = Utf8ArrayBuilder::new(1)?;
             let writer = builder.writer();
             let guard = match (off, len) {
-                (Some(off), Some(len)) => substr_start_for(&s, off, len, writer)?,
+                (Some(off), Some(len)) => {
+                    let result = substr_start_for(&s, off, len, writer);
+                    if len < 0 {
+                        assert!(result.is_err());
+                        continue;
+                    } else {
+                        result?
+                    }
+                }
                 (Some(off), None) => substr_start(&s, off, writer)?,
                 (None, Some(len)) => substr_for(&s, len, writer)?,
                 _ => unreachable!(),
