76503aa0980baa601b454c45a95979faa7720fff
Renjie Liu <liurenjie2008@gmail.com>
2021-12-28 14:17:55 +0800

    feat: introducing tpch q3
    * feat: introducing tpch q3

---
+++ scripts/gen_batch_tpch_slt.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+cd "$SCRIPT_DIR"/../e2e_test/batch/tpch || exit
+all_files=("create_tables" "insert_customer" "insert_lineitem" "insert_nation" "insert_orders" "insert_part"
+"insert_partsupp" "insert_supplier" "insert_region" "q1" "q6" "drop_tables")
+
+for file in "${all_files[@]}"; do
+  cat "${file}".slt.part >> tpch.slt
+  echo "" >> tpch.slt
+done
