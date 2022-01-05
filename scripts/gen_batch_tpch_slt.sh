50e75c325b50cc45ea26142ed180c9275171dbb7
Renjie Liu <liurenjie2008@gmail.com>
2022-01-05 16:16:44 +0800

    feat: Introducing batch tpch q7.
    * Introducing batch tpch q7

--- scripts/gen_batch_tpch_slt.sh
+++ scripts/gen_batch_tpch_slt.sh
@@ -2,12 +2,23 @@
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 cd "$SCRIPT_DIR"/../e2e_test/batch/tpch || exit
-
-all_files=("create_tables" "insert_customer" "insert_lineitem" "insert_nation" "insert_orders" "insert_part"
-"insert_partsupp" "insert_supplier" "insert_region" "q1" "q3" "q5" "q6" "drop_tables")
-
-#all_files=("create_tables" "insert_customer" "insert_lineitem" "insert_nation" "insert_orders" "insert_part"
-#"insert_partsupp" "insert_supplier" "insert_region" "q1" "q3" "q5" "q6")
+all_files=(
+"create_tables"
+"insert_customer"
+"insert_lineitem"
+"insert_nation"
+"insert_orders"
+"insert_part"
+"insert_partsupp"
+"insert_supplier"
+"insert_region"
+"q1"
+"q3"
+"q5"
+"q6"
+"q7"
+"drop_tables"
+)
 
 rm -f tpch.slt
 
