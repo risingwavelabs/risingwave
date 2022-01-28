19b967936c3b0bc1e0499df63132a9e4d947bc62
lmatz <lmatz823@gmail.com>
2022-01-28 10:04:25 +0800

    fix(planner): wrong distribution trait derivation in project

--- java/planner/src/main/java/com/risingwave/planner/rel/physical/RwBatchProject.java
+++ java/planner/src/main/java/com/risingwave/planner/rel/physical/RwBatchProject.java
@@ -3,11 +3,14 @@ package com.risingwave.planner.rel.physical;
 import static com.risingwave.planner.rel.logical.RisingWaveLogicalRel.LOGICAL;
 
 import com.google.common.collect.ImmutableList;
+import com.risingwave.planner.rel.common.dist.RwDistributionTrait;
 import com.risingwave.planner.rel.common.dist.RwDistributionTraitDef;
+import com.risingwave.planner.rel.common.dist.RwDistributions;
 import com.risingwave.planner.rel.logical.RwLogicalProject;
 import com.risingwave.planner.rel.serialization.RexToProtoSerializer;
 import com.risingwave.proto.plan.PlanNode;
 import com.risingwave.proto.plan.ProjectNode;
+import java.util.ArrayList;
 import java.util.List;
 import org.apache.calcite.plan.RelOptCluster;
 import org.apache.calcite.plan.RelOptRule;
@@ -18,7 +21,9 @@ import org.apache.calcite.rel.convert.ConverterRule;
 import org.apache.calcite.rel.core.Project;
 import org.apache.calcite.rel.hint.RelHint;
 import org.apache.calcite.rel.type.RelDataType;
+import org.apache.calcite.rex.RexInputRef;
 import org.apache.calcite.rex.RexNode;
+import org.apache.calcite.util.ImmutableIntList;
 import org.apache.calcite.util.Pair;
 import org.checkerframework.checker.nullness.qual.Nullable;
 
@@ -81,8 +86,48 @@ public class RwBatchProject extends Project implements RisingWaveBatchPhyRel, Ph
     var newTraits = traitSet;
     var dist = childTraits.getTrait(RwDistributionTraitDef.getInstance());
     if (dist != null) {
-      newTraits = newTraits.plus(dist);
+      // We remark that the project may:
+      // 1. move a column into a different position. We also use the new position as the key.
+      // 2. remove a column. We lose a key. This has implications depending on the type of
+      // distribution:
+      //    a. hash(a,b,c) => hash(a,b) invalid.
+      //    b. range(a,b,c) => range(a,b) still valid.
+      //    c. range(a,b,c) => range(b,c) invalid.
+      // 3. use the column in some function. This is treated as (2).
+      var keys = dist.getKeys();
+      List<Integer> newKeys = new ArrayList<>();
+      for (var key : keys) {
+        var projects = this.getProjects();
+        for (int i = 0; i < projects.size(); i++) {
+          var project = projects.get(i);
+          if (project instanceof RexInputRef) {
+            var index = ((RexInputRef) project).getIndex();
+            if (key == index) {
+              newKeys.add(i);
+              break;
+            }
+          }
+        }
+      }
+      if (newKeys.size() != 0) {
+        var newDistTrait =
+            new RwDistributionTrait(dist.getType(), ImmutableIntList.copyOf(newKeys));
+        // Therefore, we need to check whether the old distribution can satisfy the new one.
+        // If yes, we plus it.
+        if (dist.satisfies(newDistTrait)) {
+          newTraits = newTraits.plus(newDistTrait);
+        } else {
+          newTraits = newTraits.plus(RwDistributions.RANDOM_DISTRIBUTED);
+        }
+      } else {
+        if (dist == RwDistributions.SINGLETON) {
+          newTraits = newTraits.plus(RwDistributions.SINGLETON);
+        } else {
+          newTraits = newTraits.plus(RwDistributions.RANDOM_DISTRIBUTED);
+        }
+      }
     }
+    // Notice the default should be random_distributed instead of any. Any is a placeholder.
     return Pair.of(newTraits, ImmutableList.of(childTraits));
   }
 
