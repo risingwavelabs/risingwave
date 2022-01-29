63b4a958932c9c5287afdb69ccb0a98cb1aad158
Ting Sun <suntcrick@gmail.com>
2022-01-29 12:00:07 +0800

    feat(bench): support various workload types

--- rust/bench/ss_bench/benchmarks/get_random.rs
+++ rust/bench/ss_bench/benchmarks/get_random.rs
@@ -10,61 +10,61 @@ use risingwave_storage::StateStore;
 
 use crate::utils::latency_stat::LatencyStat;
 use crate::utils::workload::{get_epoch, Workload};
-use crate::Opts;
+use crate::{Opts, WorkloadType};
 
 pub(crate) async fn run(store: &impl StateStore, opts: &Opts) {
-    let batch = Workload::new_sorted_workload(opts, Some(0)).batch;
-    store
-        .ingest_batch(batch.clone(), get_epoch())
-        .await
-        .unwrap();
+  let batch = Workload::new(opts, WorkloadType::GetRandom, None).batch;
+  store
+    .ingest_batch(batch.clone(), get_epoch())
+    .await
+    .unwrap();
 
-    // generate queried point get key
-    let mut rng = StdRng::seed_from_u64(233);
-    let range = Uniform::from(0..opts.kvs_per_batch as usize);
-    let key_num = (opts.iterations - opts.iterations % opts.concurrency_num) as usize;
-    let mut get_keys = (0..key_num)
-        .into_iter()
-        .map(|_| batch[range.sample(&mut rng)].0.clone())
-        .collect_vec();
+  // generate queried point get key
+  let mut rng = StdRng::seed_from_u64(233);
+  let range = Uniform::from(0..opts.kvs_per_batch as usize);
+  let key_num = (opts.iterations - opts.iterations % opts.concurrency_num) as usize;
+  let mut get_keys = (0..key_num)
+    .into_iter()
+    .map(|_| batch[range.sample(&mut rng)].0.clone())
+    .collect_vec();
 
-    // partitioned these keys for each concurrency
-    let mut grouped_keys = vec![vec![]; opts.concurrency_num as usize];
-    for (i, key) in get_keys.drain(..).enumerate() {
-        grouped_keys[i % opts.concurrency_num as usize].push(key);
-    }
+  // partitioned these keys for each concurrency
+  let mut grouped_keys = vec![vec![]; opts.concurrency_num as usize];
+  for (i, key) in get_keys.drain(..).enumerate() {
+    grouped_keys[i % opts.concurrency_num as usize].push(key);
+  }
 
-    // actual point get process
-    let get = |keys: Vec<Bytes>| async {
-        let mut latencies = vec![];
-        for key in keys {
-            let start = Instant::now();
-            store.get(&key).await.unwrap();
-            let time_nano = start.elapsed().as_nanos();
-            latencies.push(time_nano);
-        }
-        latencies
-    };
-    let total_start = Instant::now();
-    let futures = grouped_keys.drain(..).map(|keys| get(keys)).collect_vec();
-    let latencies_list: Vec<Vec<u128>> = future::join_all(futures).await;
-    let total_time_nano = total_start.elapsed().as_nanos();
-
-    // calculate metrics
+  // actual point get process
+  let get = |keys: Vec<Bytes>| async {
     let mut latencies = vec![];
-    for list in latencies_list {
-        for latency in list {
-            latencies.push(latency);
-        }
+    for key in keys {
+      let start = Instant::now();
+      store.get(&key).await.unwrap();
+      let time_nano = start.elapsed().as_nanos();
+      latencies.push(time_nano);
     }
-    let stat = LatencyStat::new(latencies);
-    let qps = key_num as u128 * 1_000_000_000 / total_time_nano as u128;
+    latencies
+  };
+  let total_start = Instant::now();
+  let futures = grouped_keys.drain(..).map(|keys| get(keys)).collect_vec();
+  let latencies_list: Vec<Vec<u128>> = future::join_all(futures).await;
+  let total_time_nano = total_start.elapsed().as_nanos();
 
-    println!(
-        "
+  // calculate metrics
+  let mut latencies = vec![];
+  for list in latencies_list {
+    for latency in list {
+      latencies.push(latency);
+    }
+  }
+  let stat = LatencyStat::new(latencies);
+  let qps = key_num as u128 * 1_000_000_000 / total_time_nano as u128;
+
+  println!(
+    "
     Point get
       {}
       QPS: {}",
-        stat, qps
-    );
+    stat, qps
+  );
 }
--- rust/bench/ss_bench/utils/workload.rs
+++ rust/bench/ss_bench/utils/workload.rs
@@ -1,73 +1,143 @@
 use std::sync::atomic::AtomicU64;
 
 use bytes::{Bytes, BytesMut};
-use rand::distributions::Alphanumeric;
+use itertools::Itertools;
+use rand::distributions::Uniform;
 use rand::rngs::StdRng;
 use rand::{Rng, SeedableRng};
+use risingwave_storage::hummock::key::next_key;
 
-use crate::Opts;
+use crate::WorkloadType::*;
+use crate::{Opts, WorkloadType};
 
-pub(crate) struct Workload {
-    pub batch: Vec<(Bytes, Option<Bytes>)>,
-    pub prefixes: Vec<Bytes>,
+type Keys = Vec<Bytes>;
+type Prefixes = Vec<Bytes>;
+
+pub struct Workload {
+  pub batch: Vec<(Bytes, Option<Bytes>)>,
+  pub prefixes: Vec<Bytes>,
 }
 
 impl Workload {
-    pub(crate) fn new_sorted_workload(opts: &Opts, seed: Option<u64>) -> Workload {
-        let base_seed = seed.unwrap_or(233);
+  pub(crate) fn new(opts: &Opts, workload_type: WorkloadType, seed: Option<u64>) -> Workload {
+    let base_seed = seed.unwrap_or(233);
 
-        // get ceil result
-        let prefix_num = (opts.kvs_per_batch + opts.key_prefix_frequency - 1) as u64
-            / opts.key_prefix_frequency as u64;
+    let (prefixes, keys) = match workload_type {
+      WriteBatch | GetRandom | PrefixScanRandom | DeleteRandom => {
+        Self::new_random_keys(opts, base_seed)
+      }
+      GetSequential | DeleteSequential => Self::new_sequential_keys(opts),
+    };
 
-        let prefixes: Vec<Bytes> = (0..prefix_num)
-            .into_iter()
-            .map(|i| {
-                // set random seed to make bench reproducable
-                let prefix: String = StdRng::seed_from_u64(base_seed + i)
-                    .sample_iter(&Alphanumeric)
-                    .take(opts.key_prefix_size as usize)
-                    .map(char::from)
-                    .collect();
+    let values = match workload_type {
+      DeleteRandom | DeleteSequential => vec![None; keys.len()],
+      _ => Self::new_values(opts, base_seed),
+    };
 
-                Bytes::from(prefix)
-            })
-            .collect();
+    let mut batch = keys.into_iter().zip(values.into_iter()).collect_vec();
 
-        let mut batch: Vec<(Bytes, Option<Bytes>)> = (0..opts.kvs_per_batch as u64)
-            .into_iter()
-            .map(|i| {
-                // set random seed to make bench reproducable
-                let pure_key: String = StdRng::seed_from_u64(base_seed + i + 1)
-                    .sample_iter(&Alphanumeric)
-                    .take(opts.key_size as usize)
-                    .map(char::from)
-                    .collect();
-                let mut key =
-                    BytesMut::with_capacity((opts.key_prefix_size + opts.key_size) as usize);
-                // make sure prefixes are evenly distributed
-                key.extend_from_slice(&prefixes[i as usize % prefixes.len()]);
-                key.extend_from_slice(pure_key.as_ref());
+    batch.sort_by(|(k1, _), (k2, _)| k1.cmp(k2));
+    // As duplication rate is low, ignore filling data after deduplicating.
+    batch.dedup_by(|(k1, _), (k2, _)| k1 == k2);
 
-                let value: String = StdRng::seed_from_u64(base_seed + i + 2)
-                    .sample_iter(&Alphanumeric)
-                    .take(opts.value_size as usize)
-                    .map(char::from)
-                    .collect();
+    Workload { batch, prefixes }
+  }
 
-                (key.freeze(), Some(Bytes::from(value)))
-            })
-            .collect();
-        batch.sort_by(|(k1, _), (k2, _)| k1.cmp(k2));
+  fn new_values(opts: &Opts, base_seed: u64) -> Vec<Option<Bytes>> {
+    let str_dist = Uniform::new_inclusive(0, 255);
+    let value_num = opts.kvs_per_batch as u64;
+    (0..value_num)
+      .into_iter()
+      .map(|i| {
+        // set random seed to make bench reproducable
+        let value = StdRng::seed_from_u64(base_seed + i)
+          .sample_iter(&str_dist)
+          .take(opts.value_size as usize)
+          .map(u8::from)
+          .collect_vec();
 
-        // As duplication rate is low, ignore filling data after deduplicating.
-        batch.dedup_by(|(k1, _), (k2, _)| k1 == k2);
+        Some(Bytes::from(value))
+      })
+      .collect()
+  }
 
-        Workload { batch, prefixes }
+  fn new_random_keys(opts: &Opts, base_seed: u64) -> (Prefixes, Keys) {
+    // --- get prefixes ---
+    // get ceil result
+    let prefix_num = (opts.kvs_per_batch + opts.key_prefix_frequency - 1) as u64
+      / opts.key_prefix_frequency as u64;
+    let str_dist = Uniform::new_inclusive(0, 255);
+
+    let prefixes = (0..prefix_num)
+      .into_iter()
+      .map(|i| {
+        // set random seed to make bench reproducable
+        let prefix = StdRng::seed_from_u64(base_seed + i)
+          .sample_iter(&str_dist)
+          .take(opts.key_prefix_size as usize)
+          .map(u8::from)
+          .collect_vec();
+
+        Bytes::from(prefix)
+      })
+      .collect_vec();
+
+    // --- get keys ---
+    let keys = (0..opts.kvs_per_batch as u64)
+      .into_iter()
+      .map(|i| {
+        // set random seed to make bench reproducable
+        let user_key = StdRng::seed_from_u64(base_seed + i + 1)
+          .sample_iter(&str_dist)
+          .take(opts.key_size as usize)
+          .map(u8::from)
+          .collect_vec();
+        let mut key = BytesMut::with_capacity((opts.key_prefix_size + opts.key_size) as usize);
+        // make sure prefixes are evenly distributed
+        key.extend_from_slice(&prefixes[i as usize % prefixes.len()]);
+        key.extend_from_slice(user_key.as_ref());
+
+        key.freeze()
+      })
+      .collect();
+
+    (prefixes, keys)
+  }
+
+  fn new_sequential_keys(opts: &Opts) -> (Prefixes, Keys) {
+    // --- get prefixes ---
+    // get ceil result
+    let prefix_num = (opts.kvs_per_batch + opts.key_prefix_frequency - 1) as u64
+      / opts.key_prefix_frequency as u64;
+    let mut prefixes = Vec::with_capacity(prefix_num as usize);
+    let prefix = vec![b'\0'; opts.key_prefix_size as usize];
+    for _ in 0..prefix_num as u64 {
+      let prefix = next_key(&prefix);
+      // ensure next prefix exist
+      assert_ne!(prefix.len(), 0);
+      prefixes.push(Bytes::from(prefix));
     }
+
+    // --- get keys ---
+    let mut keys = Vec::with_capacity(opts.kvs_per_batch as usize);
+    let user_key = vec![b'\0'; opts.key_size as usize];
+    for i in 0..opts.kvs_per_batch as u64 {
+      let user_key = next_key(&user_key);
+      // ensure next key exist
+      assert_ne!(user_key.len(), 0);
+      let mut key = BytesMut::with_capacity((opts.key_prefix_size + opts.key_size) as usize);
+      // make sure prefixes are evenly distributed
+      key.extend_from_slice(&prefixes[i as usize % prefixes.len()]);
+      key.extend_from_slice(user_key.as_ref());
+
+      keys.push(key.freeze());
+    }
+
+    (prefixes, keys)
+  }
 }
 
 pub(crate) fn get_epoch() -> u64 {
-    static EPOCH: AtomicU64 = AtomicU64::new(0);
-    EPOCH.fetch_add(1, std::sync::atomic::Ordering::SeqCst)
+  static EPOCH: AtomicU64 = AtomicU64::new(0);
+  EPOCH.fetch_add(1, std::sync::atomic::Ordering::SeqCst)
 }
