68f33b74939cd1dd29acce249e48cd9128cb0fce
stonepage <40830455+st1page@users.noreply.github.com>
2022-02-07 17:08:14 +0800

    feat(optimizer): use fixedbitset instead of bit-set (#114)
    * feat(optimizer): use fixedbitset instead of bit-set
    
    * cargo.lock

--- rust/frontend/src/optimizer/plan_node/col_pruning.rs
+++ rust/frontend/src/optimizer/plan_node/col_pruning.rs
@@ -1,4 +1,4 @@
-use bit_set::BitSet;
+use fixedbitset::FixedBitSet;
 use paste::paste;
 
 use super::*;
@@ -9,10 +9,10 @@ use crate::{for_batch_plan_nodes, for_stream_plan_nodes};
 /// The trait for column pruning, only logical plan node will use it, though all plan node impl it.
 pub trait ColPrunable: WithSchema + IntoPlanRef {
     /// transform the plan node to only output the required columns ordered by index number.
-    fn prune_col(&self, required_cols: BitSet) -> PlanRef {
+    fn prune_col(&self, required_cols: FixedBitSet) -> PlanRef {
         let schema = self.schema();
         let exprs: Vec<ExprImpl> = required_cols
-            .iter()
+            .ones()
             .map(|i| InputRef::new(i, schema.fields()[i].data_type()).bound_expr())
             .collect();
         let alias = vec![None; required_cols.len()];
@@ -25,7 +25,7 @@ macro_rules! ban_prune_col {
   ([], $( { $convention:ident, $name:ident }),*) => {
     paste!{
       $(impl ColPrunable for [<$convention $name>] {
-        fn prune_col(&self, _required_cols: BitSet) -> PlanRef {
+        fn prune_col(&self, _required_cols: FixedBitSet) -> PlanRef {
           panic!("column pruning is only allowed on logical plan")
         }
      })*
