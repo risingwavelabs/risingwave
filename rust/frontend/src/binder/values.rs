acd64527958926c4429285e4e40f83e409edb3a6
Kaige Li <3454378359@qq.com>
2022-02-08 17:17:34 +0800

    feat(binder): derive column types for values.

--- rust/frontend/src/binder/values.rs
+++ rust/frontend/src/binder/values.rs
@@ -1,9 +1,12 @@
+use risingwave_common::array::RwError;
 use risingwave_common::catalog::{Field, Schema};
-use risingwave_common::error::Result;
+use risingwave_common::error::{ErrorCode, Result};
+use risingwave_common::types::DataTypeKind;
+use risingwave_pb::expr::expr_node::Type;
 use risingwave_sqlparser::ast::Values;
 
 use crate::binder::Binder;
-use crate::expr::{Expr as _, ExprImpl};
+use crate::expr::{infer_type, Expr as _, ExprImpl, FunctionCall};
 
 #[derive(Debug)]
 pub struct BoundValues {
@@ -19,11 +22,47 @@ impl Binder {
             .map(|vec| vec.into_iter().map(|expr| self.bind_expr(expr)).collect())
             .collect::<Result<Vec<Vec<_>>>>()?;
         // calc row type and insert casts here
-        let types = bound[0].iter().map(|expr| expr.return_type());
-        let schema = Schema::new(types.map(Field::unnamed).collect());
-        Ok(BoundValues {
-            rows: bound,
-            schema,
-        })
+        let mut types = bound[0]
+            .iter()
+            .map(|expr| expr.return_type())
+            .collect::<Vec<DataTypeKind>>();
+        for vec in &bound {
+            for (i, expr) in vec.iter().enumerate() {
+                if let Some(tmp) = Self::check_compat(types[i], expr.return_type()) {
+                    types[i] = tmp;
+                } else {
+                    return Err(RwError::from(ErrorCode::NotImplementedError(
+                        "bind_values".to_string(),
+                    )));
+                }
+            }
+        }
+        let rows = bound
+            .into_iter()
+            .map(|vec| {
+                vec.into_iter()
+                    .enumerate()
+                    .map(|(i, expr)| {
+                        if types[i] != expr.return_type() {
+                            ExprImpl::FunctionCall(Box::new(FunctionCall::new_with_return_type(
+                                Type::Cast,
+                                vec![expr],
+                                types[i],
+                            )))
+                        } else {
+                            expr
+                        }
+                    })
+                    .collect::<Vec<ExprImpl>>()
+            })
+            .collect::<Vec<Vec<ExprImpl>>>();
+        let schema = Schema::new(types.into_iter().map(Field::unnamed).collect());
+        Ok(BoundValues { rows, schema })
+    }
+
+    fn check_compat(left: DataTypeKind, right: DataTypeKind) -> Option<DataTypeKind> {
+        let func_type = Type::Add;
+        let inputs_type = vec![left, right];
+        infer_type(func_type, inputs_type)
     }
 }
