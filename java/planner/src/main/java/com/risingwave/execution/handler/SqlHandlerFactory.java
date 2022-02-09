098b7659ce53073950e2387aa88591c497c21def
Bugen Zhao <i@bugenzhao.com>
2022-02-09 18:57:42 +0800

    fix java doc

--- java/planner/src/main/java/com/risingwave/execution/handler/SqlHandlerFactory.java
+++ java/planner/src/main/java/com/risingwave/execution/handler/SqlHandlerFactory.java
@@ -3,6 +3,7 @@ package com.risingwave.execution.handler;
 import com.risingwave.execution.context.ExecutionContext;
 import org.apache.calcite.sql.SqlNode;
 
+/** Interface for SQL handler factory. */
 public interface SqlHandlerFactory {
   SqlHandler create(SqlNode ast, ExecutionContext context);
 
