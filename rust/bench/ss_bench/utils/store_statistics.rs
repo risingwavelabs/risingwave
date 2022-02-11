b887b94c7599487a93b958f80cfc7b08c181991b
Ting Sun <suntcrick@gmail.com>
2022-02-11 17:26:02 +0800

    add ut

--- rust/bench/ss_bench/utils/store_statistics.rs
+++ rust/bench/ss_bench/utils/store_statistics.rs
@@ -8,44 +8,46 @@ fn proc_counter(counter: &GenericCounter<AtomicU64>) {
     println!("{desc} COUNT : {counter}");
 }
 
-fn proc_histogram(histogram: &Histogram) {
-    let desc = &histogram.desc()[0].fq_name;
-
+fn get_percentile(histogram: &Histogram, p: f64) -> f64 {
     let metric = histogram.metric();
     let histogram = metric.get_histogram();
     let sample_count = histogram.get_sample_count();
-    let sample_sum = histogram.get_sample_sum();
     let buckets = histogram.get_bucket();
 
-    let get_quantile = |p: f64| -> f64 {
-        // empty bucket may appear
-        if sample_count == 0 {
-            return 0.0;
-        }
-        let threshold = (sample_count as f64 * p / 100.0) as u64;
-        let mut last_upper_bound = 0.0;
-        let mut last_count = 0;
-        for bucket in buckets {
-            let upper_bound = bucket.get_upper_bound();
-            let count = bucket.get_cumulative_count();
-            if count >= threshold {
-                // assume scale linearly within this bucket,
-                // return a value bwtween last_upper_bound and upper_bound
-                let right_left_diff = upper_bound - last_upper_bound;
-                return last_upper_bound
-                    + right_left_diff * (threshold - last_count) as f64
-                        / (count - last_count) as f64;
-            }
-            last_upper_bound = upper_bound;
-            last_count = count;
-        }
-        0.0
-    };
+    // empty bucket may appear
+    if sample_count == 0 {
+        return 0.0;
+    }
+    let threshold = (sample_count as f64 * (p / 100.0_f64)).ceil() as u64;
+    let mut last_upper_bound = 0.0;
+    let mut last_count = 0;
+    for bucket in buckets {
+        let upper_bound = bucket.get_upper_bound();
+        let count = bucket.get_cumulative_count();
+        if count >= threshold {
+            // assume scale linearly within this bucket,
+            // return a value bwtween last_upper_bound and upper_bound
 
-    let p50 = get_quantile(50.0);
-    let p95 = get_quantile(95.0);
-    let p99 = get_quantile(99.0);
-    let p100 = get_quantile(100.0);
+            let right_left_diff = upper_bound - last_upper_bound;
+            return last_upper_bound
+                + right_left_diff * (threshold - last_count) as f64 / (count - last_count) as f64;
+        }
+        last_upper_bound = upper_bound;
+        last_count = count;
+    }
+    0.0
+}
+
+fn proc_histogram(histogram: &Histogram) {
+    let desc = &histogram.desc()[0].fq_name;
+
+    let p50 = get_percentile(histogram, 50.0);
+    let p95 = get_percentile(histogram, 95.0);
+    let p99 = get_percentile(histogram, 99.0);
+    let p100 = get_percentile(histogram, 100.0);
+
+    let sample_count = histogram.get_sample_count();
+    let sample_sum = histogram.get_sample_sum();
 
     println!("{desc} P50 : {p50} P95 : {p95} P99 : {p99} P100 : {p100} COUNT : {sample_count} SUM : {sample_sum}");
 }
@@ -78,4 +80,48 @@ pub(crate) fn print_statistics() {
     proc_histogram(&stat.iter_next_latency);
 
     println!();
+}
+
+#[cfg(test)]
+mod tests {
+    use prometheus::{histogram_opts, register_histogram_with_registry};
+
+    use super::*;
+
+    struct Stats {
+        pub hist: Histogram,
+    }
+
+    impl Stats {
+        fn new(upper_bound: u64) -> Self {
+            let registry = prometheus::default_registry();
+            let buckets = (1 ..upper_bound + 1)
+                .map(|x| x as f64)
+                .collect::<Vec<f64>>();
+            let opts = histogram_opts!("test_histogram", "test histogram", buckets);
+            let hist = register_histogram_with_registry!(opts, &registry).unwrap();
+
+            Stats { hist }
+        }
+    }
+
+    #[test]
+    fn test_proc_histogram() {
+        let upper_bound = 1000;
+        let stat = Stats::new(upper_bound);
+        let histogram = &stat.hist;
+        for value in 1..upper_bound {
+            histogram.observe(value as f64);
+        }
+
+        println!("{:?}", get_percentile(histogram, 50.0));
+        println!("{:?}", get_percentile(histogram, 90.0));
+        println!("{:?}", get_percentile(histogram, 99.0));
+        println!("{:?}", get_percentile(histogram, 99.9));
+
+        assert_eq!(get_percentile(histogram, 50.0).ceil() as u64, 501);
+        assert_eq!(get_percentile(histogram, 90.0).ceil() as u64, 901);
+        assert_eq!(get_percentile(histogram, 99.0).ceil() as u64, 991);
+        assert_eq!(get_percentile(histogram, 99.9).ceil() as u64, 999);
+    }
 }
