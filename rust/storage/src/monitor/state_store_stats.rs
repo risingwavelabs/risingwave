bb58781aa1c4937adb40e6db5268ddb401d6e78f
Congyi Wang <15605187270@163.com>
2022-02-14 17:57:26 +0800

    add metrics for hummock grpc

--- rust/storage/src/monitor/state_store_stats.rs
+++ rust/storage/src/monitor/state_store_stats.rs
@@ -22,6 +22,14 @@ pub const ADD_L0_LATENCT_SCALE: f64 = 0.00001;
 pub const ITER_NEXT_LATENCY_SCALE: f64 = 0.0001;
 pub const ITER_SEEK_LATENCY_SCALE: f64 = 0.0001;
 
+pub const PIN_VERSION_LATENCY_SCALE: f64 = 0.1;
+pub const UNPIN_VERSION_LATENCY_SCALE: f64 = 0.1;
+pub const PIN_SNAPSHOT_LATENCY_SCALE: f64 = 0.1;
+pub const UNPIN_SNAPSHOT_LATENCY_SCALE: f64 = 0.1;
+pub const ADD_TABLE_LATENCT_SCALE: f64 = 0.1;
+pub const GET_NEW_TABLE_ID_LATENCY_SCALE: f64 = 0.1;
+pub const GET_COMPATION_TASK_LATENCY_SCALE: f64 = 0.1;
+pub const REPORT_COMPATION_TASK_LATENCY_SCALE: f64 = 0.1;
 /// `StateStoreStats` stores the performance and IO metrics of `XXXStorage` such as
 /// In practice, keep in mind that this represents the whole Hummock utilizations of
 /// a `RisingWave` instance. More granular utilizations of per `materialization view`
@@ -48,6 +56,24 @@ pub struct StateStoreStats {
     pub iter_next_counts: GenericCounter<AtomicU64>,
     pub iter_seek_latency: Histogram,
     pub iter_next_latency: Histogram,
+
+    pub pin_version_counts: GenericCounter<AtomicU64>,
+    pub unpin_version_counts: GenericCounter<AtomicU64>,
+    pub pin_snapshot_counts: GenericCounter<AtomicU64>,
+    pub unpin_snapshot_counts: GenericCounter<AtomicU64>,
+    pub add_tables_counts: GenericCounter<AtomicU64>,
+    pub get_new_table_id_counts: GenericCounter<AtomicU64>,
+    pub get_compaction_task_counts: GenericCounter<AtomicU64>,
+    pub report_compaction_task_counts: GenericCounter<AtomicU64>,
+
+    pub pin_version_latency: Histogram,
+    pub unpin_version_latency: Histogram,
+    pub pin_snapshot_latency: Histogram,
+    pub unpin_snapshot_latency: Histogram,
+    pub add_tables_latency: Histogram,
+    pub get_new_table_id_latency: Histogram,
+    pub get_compaction_task_latency: Histogram,
+    pub report_compaction_task_latency: Histogram,
 }
 
 lazy_static::lazy_static! {
@@ -203,6 +229,151 @@ impl StateStoreStats {
         );
         let iter_next_latency = register_histogram_with_registry!(opts, registry).unwrap();
 
+        // ----- gRPC -----
+        // gRPC count
+        let pin_version_counts =
+            register_int_counter_with_registry!("state_store_pin_version_counts", "233", registry)
+                .unwrap();
+        let unpin_version_counts = register_int_counter_with_registry!(
+            "state_store_unpin_version_counts",
+            "233",
+            registry
+        )
+        .unwrap();
+        let pin_snapshot_counts = register_int_counter_with_registry!(
+            "state_store_pin_snapshot_counts",
+            "Total number of iter.next requests that have been issued to state store",
+            registry
+        )
+        .unwrap();
+        let unpin_snapshot_counts = register_int_counter_with_registry!(
+            "state_store_unpin_snapshot_counts",
+            "Total number of iter.next requests that have been issued to state store",
+            registry
+        )
+        .unwrap();
+        let add_tables_counts = register_int_counter_with_registry!(
+            "state_store_add_tables_counts",
+            "Total number of iter.next requests that have been issued to state store",
+            registry
+        )
+        .unwrap();
+        let get_new_table_id_counts = register_int_counter_with_registry!(
+            "state_store_get_new_table_id_counts",
+            "Total number of iter.next requests that have been issued to state store",
+            registry
+        )
+        .unwrap();
+        let get_compaction_task_counts = register_int_counter_with_registry!(
+            "state_store_get_compaction_task_counts",
+            "Total number of iter.next requests that have been issued to state store",
+            registry
+        )
+        .unwrap();
+        let report_compaction_task_counts = register_int_counter_with_registry!(
+            "state_store_report_compaction_task_counts",
+            "Total number of iter.next requests that have been issued to state store",
+            registry
+        )
+        .unwrap();
+
+        // gRPC latency
+        // --
+        let buckets = DEFAULT_BUCKETS
+            .map(|x| x * PIN_VERSION_LATENCY_SCALE)
+            .to_vec();
+        let pin_version_latency_opts = histogram_opts!(
+            "state_store_pin_version_latency",
+            "Total latency of get that have been issued to state store",
+            buckets
+        );
+        let pin_version_latency =
+            register_histogram_with_registry!(pin_version_latency_opts, registry).unwrap();
+
+        // --
+        let buckets = DEFAULT_BUCKETS
+            .map(|x| x * UNPIN_VERSION_LATENCY_SCALE)
+            .to_vec();
+        let unpin_version_latency_opts = histogram_opts!(
+            "state_store_unpin_version_latency",
+            "Total latency of get that have been issued to state store",
+            buckets
+        );
+        let unpin_version_latency =
+            register_histogram_with_registry!(unpin_version_latency_opts, registry).unwrap();
+
+        // --
+        let buckets = DEFAULT_BUCKETS
+            .map(|x| x * PIN_SNAPSHOT_LATENCY_SCALE)
+            .to_vec();
+        let pin_snapshot_latency_opts = histogram_opts!(
+            "state_store_pin_snapshot_latency",
+            "Total latency of get that have been issued to state store",
+            buckets
+        );
+        let pin_snapshot_latency =
+            register_histogram_with_registry!(pin_snapshot_latency_opts, registry).unwrap();
+
+        // --
+        let buckets = DEFAULT_BUCKETS
+            .map(|x| x * UNPIN_SNAPSHOT_LATENCY_SCALE)
+            .to_vec();
+        let unpin_snapshot_latency_opts = histogram_opts!(
+            "state_store_unpin_snapshot_latency",
+            "Total latency of get that have been issued to state store",
+            buckets
+        );
+        let unpin_snapshot_latency =
+            register_histogram_with_registry!(unpin_snapshot_latency_opts, registry).unwrap();
+
+        // --
+        let buckets = DEFAULT_BUCKETS
+            .map(|x| x * ADD_TABLE_LATENCT_SCALE)
+            .to_vec();
+        let add_tables_latency_opts = histogram_opts!(
+            "state_store_add_tables_latency",
+            "Total latency of get that have been issued to state store",
+            buckets
+        );
+        let add_tables_latency =
+            register_histogram_with_registry!(add_tables_latency_opts, registry).unwrap();
+
+        // --
+        let buckets = DEFAULT_BUCKETS
+            .map(|x| x * GET_NEW_TABLE_ID_LATENCY_SCALE)
+            .to_vec();
+        let get_new_table_id_latency_opts = histogram_opts!(
+            "state_store_get_new_table_id_latency",
+            "Total latency of get that have been issued to state store",
+            buckets
+        );
+        let get_new_table_id_latency =
+            register_histogram_with_registry!(get_new_table_id_latency_opts, registry).unwrap();
+
+        // --
+        let buckets = DEFAULT_BUCKETS
+            .map(|x| x * GET_COMPATION_TASK_LATENCY_SCALE)
+            .to_vec();
+        let get_compaction_task_latency_opts = histogram_opts!(
+            "state_store_get_compaction_task_latency",
+            "Total latency of get that have been issued to state store",
+            buckets
+        );
+        let get_compaction_task_latency =
+            register_histogram_with_registry!(get_compaction_task_latency_opts, registry).unwrap();
+
+        // --
+        let buckets = DEFAULT_BUCKETS
+            .map(|x| x * REPORT_COMPATION_TASK_LATENCY_SCALE)
+            .to_vec();
+        let report_compaction_task_latency_opts = histogram_opts!(
+            "state_store_report_compaction_task_latency",
+            "Total latency of get that have been issued to state store",
+            buckets
+        );
+        let report_compaction_task_latency =
+            register_histogram_with_registry!(report_compaction_task_latency_opts, registry)
+                .unwrap();
         Self {
             get_latency,
             get_key_size,
@@ -224,26 +395,24 @@ impl StateStoreStats {
             iter_next_counts,
             iter_seek_latency,
             iter_next_latency,
+
+            pin_version_counts,
+            unpin_version_counts,
+            pin_snapshot_counts,
+            unpin_snapshot_counts,
+            add_tables_counts,
+            get_new_table_id_counts,
+            get_compaction_task_counts,
+            report_compaction_task_counts,
+
+            pin_version_latency,
+            unpin_version_latency,
+            pin_snapshot_latency,
+            unpin_snapshot_latency,
+            add_tables_latency,
+            get_new_table_id_latency,
+            get_compaction_task_latency,
+            report_compaction_task_latency,
         }
     }
 }
-
-// TODO(xiangyhu): use macro to process the stats
-// macro_rules! process_stats {
-//   (struct $name:ident { $($fname:ident : $ftype:ty),* }) => {
-//       struct $name {
-//           $($fname : $ftype),*
-//       }
-
-//       impl $name {
-//           fn field_names() -> &'static [&'static str] {
-//               static NAMES: &'static [&'static str] = &[$(stringify!($fname)),*];
-//               NAMES
-//           }
-
-//           pub fn new() -> Self {
-//             $fname: $ftype::new(0);
-//           }
-//       }
-//   }
-// }
