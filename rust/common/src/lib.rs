f436bd77c3d2af5ceaf5858b13fd56f3a1093ba4
Alex Chi <iskyzh@gmail.com>
2022-02-14 15:49:42 +0800

    refactor: enable clippy await_holding_lock (#288)
    Signed-off-by: Alex Chi <iskyzh@gmail.com>

--- rust/common/src/lib.rs
+++ rust/common/src/lib.rs
@@ -6,6 +6,7 @@
 #![warn(clippy::inconsistent_struct_constructor)]
 #![warn(clippy::map_flatten)]
 #![warn(clippy::no_effect_underscore_binding)]
+#![warn(clippy::await_holding_lock)]
 #![deny(unused_must_use)]
 #![feature(trait_alias)]
 #![feature(generic_associated_types)]
