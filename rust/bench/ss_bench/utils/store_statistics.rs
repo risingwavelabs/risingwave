a29940003d53689fb59c2528f4071b1af2311297
Ting Sun <suntcrick@gmail.com>
2022-02-14 16:18:04 +0800

    add ut

--- rust/bench/ss_bench/utils/store_statistics.rs
+++ rust/bench/ss_bench/utils/store_statistics.rs
@@ -85,41 +85,94 @@ mod tests {
 
     use super::*;
 
-    fn new_histogram(upper_bound: u64) -> Histogram {
-        let registry = Registry::new();
-        let buckets = (1..=upper_bound).map(|x| x as f64).collect::<Vec<f64>>();
-        let opts = histogram_opts!("test_histogram", "test_histogram", buckets);
+    #[test]
+    fn test_proc_histogram_basic() {
+        fn new_simple_histogram(upper_bound: u64) -> Histogram {
+            let registry = Registry::new();
+            let buckets = (1..=upper_bound).map(|x| x as f64).collect::<Vec<f64>>();
+            let opts = histogram_opts!("test_histogram", "test_histogram", buckets);
 
-        let histogram = register_histogram_with_registry!(opts, registry).unwrap();
+            let histogram = register_histogram_with_registry!(opts, registry).unwrap();
 
-        for value in 1..=upper_bound {
-            histogram.observe(value as f64);
+            for value in 1..=upper_bound {
+                histogram.observe(value as f64);
+            }
+
+            histogram
         }
 
-        histogram
-    }
-
-    #[test]
-    fn test_proc_histogram1000() {
-        let histogram = new_histogram(999);
+        let histogram = new_simple_histogram(999);
         assert_eq!(get_percentile(&histogram, 50.0) as u64, 500);
         assert_eq!(get_percentile(&histogram, 90.0) as u64, 900);
         assert_eq!(get_percentile(&histogram, 99.0) as u64, 990);
         assert_eq!(get_percentile(&histogram, 99.9) as u64, 999);
         assert_eq!(get_percentile(&histogram, 100.0) as u64, 999);
 
-        let histogram = new_histogram(1000);
+        let histogram = new_simple_histogram(1000);
         assert_eq!(get_percentile(&histogram, 50.0) as u64, 500);
         assert_eq!(get_percentile(&histogram, 90.0) as u64, 900);
         assert_eq!(get_percentile(&histogram, 99.0) as u64, 990);
         assert_eq!(get_percentile(&histogram, 99.9) as u64, 1000);
         assert_eq!(get_percentile(&histogram, 100.0) as u64, 1000);
 
-        let histogram = new_histogram(9999);
+        let histogram = new_simple_histogram(9999);
         assert_eq!(get_percentile(&histogram, 50.0) as u64, 5000);
         assert_eq!(get_percentile(&histogram, 90.0) as u64, 9000);
         assert_eq!(get_percentile(&histogram, 99.0) as u64, 9900);
         assert_eq!(get_percentile(&histogram, 99.9) as u64, 9990);
         assert_eq!(get_percentile(&histogram, 100.0) as u64, 9999);
+    }
+
+    #[test]
+    fn test_proc_histogram_uneven_distributed() {
+        let registry = Registry::new();
+        let buckets = vec![
+            0.005, 0.01, 0.025, 0.05, 0.1, 0.25, 0.5, 1.0, 2.5, 5.0, 10.0,
+        ];
+        let opts = histogram_opts!("test_histogram", "test_histogram", buckets);
+        let histogram = register_histogram_with_registry!(opts, registry).unwrap();
+
+        let mut i = 0.005;
+        while i < 10.0 {
+            histogram.observe(i);
+            i += 0.005;
+        }
+
+        assert_eq!(get_percentile(&histogram, 50.0), 5.0);
+        assert_eq!(get_percentile(&histogram, 90.0), 9.004004004004004);
+        assert_eq!(get_percentile(&histogram, 99.0), 9.904904904904905);
+        assert_eq!(get_percentile(&histogram, 99.9), 9.994994994994995);
+        assert_eq!(get_percentile(&histogram, 100.0), 10.0);
+    }
+
+    #[test]
+    fn test_proc_histogram_realistic() {
+        let registry = Registry::new();
+        let buckets = vec![
+            0.005, 0.01, 0.025, 0.05, 0.1, 0.25, 0.5, 1.0, 2.5, 5.0, 10.0,
+        ];
+        let opts = histogram_opts!("test_histogram", "test_histogram", buckets);
+        let histogram = register_histogram_with_registry!(opts, registry).unwrap();
+
+        histogram.observe(0.001);
+        histogram.observe(0.003);
+
+        histogram.observe(0.0113);
+        histogram.observe(0.0143);
+        histogram.observe(0.0249);
+
+        histogram.observe(0.041);
+        histogram.observe(0.042);
+
+        histogram.observe(0.098);
+        histogram.observe(0.99);
+
+        histogram.observe(6.11);
+
+        assert_eq!(get_percentile(&histogram, 50.0), 0.025);
+        assert_eq!(get_percentile(&histogram, 90.0), 1.00);
+        assert_eq!(get_percentile(&histogram, 99.0), 10.00);
+        assert_eq!(get_percentile(&histogram, 99.9), 10.00);
+        assert_eq!(get_percentile(&histogram, 100.0), 10.00);
     }
 }
