afe52b3147e05a39ce3304a5a7885a269f546c8b
xiangjinwu <17769960+xiangjinwu@users.noreply.github.com>
2022-02-15 10:44:55 +0800

    feat(binder): bind & plan insert (#302)

---
+++ rust/frontend/src/binder/insert.rs
@@ -0,0 +1,24 @@
+use risingwave_common::error::Result;
+use risingwave_sqlparser::ast::{Ident, ObjectName, Query};
+
+use crate::binder::{BaseTableRef, Binder, BoundQuery};
+
+#[derive(Debug)]
+pub struct BoundInsert {
+    pub table: BaseTableRef,
+    pub source: BoundQuery,
+}
+
+impl Binder {
+    pub(super) fn bind_insert(
+        &mut self,
+        table_name: ObjectName,
+        _columns: Vec<Ident>,
+        source: Query,
+    ) -> Result<BoundInsert> {
+        Ok(BoundInsert {
+            table: self.bind_table(table_name)?,
+            source: self.bind_query(source)?,
+        })
+    }
+}
