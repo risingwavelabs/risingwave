d565272fb2f7309797e5184f3c68a0db121cee7d
wyhyhyhyh <wyhyhyhyh@163.com>
2022-02-18 18:52:24 +0800

    fix BatchQUeryPlanTest cases

--- java/planner/src/main/java/com/risingwave/scheduler/streaming/MockStreamManager.java
+++ java/planner/src/main/java/com/risingwave/scheduler/streaming/MockStreamManager.java
@@ -1,18 +1,20 @@
 package com.risingwave.scheduler.streaming;
 
 import com.google.inject.Inject;
-import com.risingwave.common.exception.PgErrorCode;
-import com.risingwave.common.exception.PgException;
 import com.risingwave.proto.plan.TableRefId;
 import com.risingwave.proto.streaming.plan.StreamNode;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /** This mock implementation only work for test env init. */
 public class MockStreamManager implements StreamManager {
+  private static final Logger log = LoggerFactory.getLogger(MockStreamManager.class);
+
   @Inject
   public MockStreamManager() {}
 
   @Override
   public void createMaterializedView(StreamNode streamNode, TableRefId tableRefId) {
-    throw new PgException(PgErrorCode.INTERNAL_ERROR, "only used for test env config init.");
+    log.debug("only used for test env config init.");
   }
 }
