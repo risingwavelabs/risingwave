9c0bb79ec1ea11cca2b717dbedf90a5c25337a02
Bugen Zhao <i@bugenzhao.com>
2022-02-20 17:52:41 +0800

    rename batch query executor

--- rust/stream/src/executor/mview/mod.rs
+++ rust/stream/src/executor/mview/mod.rs
@@ -1,13 +1,11 @@
 mod materialize;
-mod snapshot;
 mod state;
 
 #[cfg(test)]
 mod table_state_tests;
 #[cfg(test)]
-mod test_utils;
+pub mod test_utils;
 
 pub use materialize::*;
 pub use risingwave_storage::table::mview::*;
-pub use snapshot::*;
 pub use state::*;
