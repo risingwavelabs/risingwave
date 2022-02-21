c52d556d048bff69ba03be9d057d7d5cff99987a
Ting Sun <suntcrick@gmail.com>
2022-02-21 13:21:22 +0800

    add bytes/sec for getrandom

--- rust/bench/ss_bench/operations/get_random.rs
+++ rust/bench/ss_bench/operations/get_random.rs
@@ -41,37 +41,46 @@ impl Operations {
             .drain(..)
             .map(|(keys, store)| async move {
                 let mut latencies: Vec<u128> = vec![];
+                let mut sizes: Vec<usize> = vec![];
                 for key in keys {
                     let start = Instant::now();
-                    store.get(&key, u64::MAX).await.unwrap();
+                    let len = match store.get(&key, u64::MAX).await.unwrap() {
+                        Some(v) => v.len(),
+                        None => 0,
+                    };
                     let time_nano = start.elapsed().as_nanos();
                     latencies.push(time_nano);
+                    sizes.push(len);
                 }
-                latencies
+                (latencies, sizes)
             })
             .collect_vec();
 
         let total_start = Instant::now();
 
         let handles = futures.into_iter().map(tokio::spawn).collect_vec();
-        let latencies_list = futures::future::join_all(handles).await;
+        let results = futures::future::join_all(handles).await;
 
         let total_time_nano = total_start.elapsed().as_nanos();
 
         // calculate metrics
-        let latencies: Vec<u128> = latencies_list
-            .into_iter()
-            .flat_map(|res| res.unwrap())
-            .collect_vec();
-        let stat = LatencyStat::new(latencies);
+        let mut total_latencies: Vec<u128> = Vec::new();
+        let mut total_sizes: usize = 0;
+        let _ = results.into_iter().map(|res| {
+            let (latencies, sizes) = res.unwrap();
+            total_latencies.extend(latencies);
+            total_sizes += sizes.iter().sum::<usize>();
+        });
+        let stat = LatencyStat::new(total_latencies);
         let qps = opts.reads as u128 * 1_000_000_000 / total_time_nano as u128;
+        let bytes_pre_sec = total_sizes as u128 * 1_000_000_000 / total_time_nano as u128;
 
         println!(
             "
     getrandom
       {}
-      QPS: {}",
-            stat, qps
+      QPS: {} {} ",
+            stat, qps, bytes_pre_sec
         );
     }
 }
