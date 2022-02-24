ed924909338521b552e663209d564c0f5727d96c
st1page <1245835950@qq.com>
2022-02-24 15:52:24 +0800

    refactor column desc in rust

--- rust/stream/src/executor/mview/materialize.rs
+++ rust/stream/src/executor/mview/materialize.rs
@@ -151,7 +151,6 @@ mod tests {
     use risingwave_common::util::sort_util::OrderType;
     use risingwave_pb::data::data_type::TypeName;
     use risingwave_pb::data::DataType;
-    use risingwave_pb::plan::column_desc::ColumnEncodingType;
     use risingwave_pb::plan::ColumnDesc;
     use risingwave_storage::memory::MemoryStateStore;
     use risingwave_storage::table::{SimpleTableManager, TableManager};
@@ -175,7 +174,6 @@ mod tests {
                     type_name: TypeName::Int32 as i32,
                     ..Default::default()
                 }),
-                encoding: ColumnEncodingType::Raw as i32,
                 name: "v1".to_string(),
                 column_id: 0,
                 ..Default::default()
@@ -185,7 +183,6 @@ mod tests {
                     type_name: TypeName::Int32 as i32,
                     ..Default::default()
                 }),
-                encoding: ColumnEncodingType::Raw as i32,
                 name: "v2".to_string(),
                 column_id: 1,
                 ..Default::default()
