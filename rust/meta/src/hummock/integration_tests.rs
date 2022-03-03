7f3006a626e1117ccf89f9341d2117e6c1e0c667
Xiangyu Hu <xiangyu@singularity-data.com>
2022-03-03 15:22:55 +0800

    feat: refine the storage configurations on compute node.

--- rust/meta/src/hummock/integration_tests.rs
+++ rust/meta/src/hummock/integration_tests.rs
@@ -44,7 +44,7 @@ async fn get_hummock_storage() -> HummockStorage {
         sstable_size: 64,
         block_size: 1 << 10,
         bloom_false_positive: 0.1,
-        remote_dir: remote_dir.clone(),
+        data_directory: remote_dir.clone(),
         checksum_algo: ChecksumAlg::XxHash64,
     };
     let hummock_meta_client = Arc::new(get_hummock_meta_client().await);
