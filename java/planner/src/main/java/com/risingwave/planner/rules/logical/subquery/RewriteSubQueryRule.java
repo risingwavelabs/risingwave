cf93b106f3b3eef2d8dabdb45dbbb58b04dc0893
Renjie Liu <liurenjie2008@gmail.com>
2022-03-03 17:16:10 +0800

    feat: Introducing RewriteSubQueryRule

--- java/planner/src/main/java/com/risingwave/planner/rules/logical/subquery/RewriteSubQueryRule.java
+++ java/planner/src/main/java/com/risingwave/planner/rules/logical/subquery/RewriteSubQueryRule.java
@@ -37,35 +37,28 @@ import org.apache.calcite.sql.fun.SqlCountAggFunction;
 import org.apache.calcite.sql.type.SqlTypeFamily;
 
 /**
- * Planner rule that rewrites scalar query in filter like:
- * `select * from T1 where (select count(*) from T2) > 0`
- * to
- * `select * from T1 where exists (select * from T2)`,
- * which could be converted to SEMI join by [[FlinkSubQueryRemoveRule]].
- * <p>
- * Without this rule, the original query will be rewritten to a filter on a join on an aggregate
- * by [[org.apache.calcite.rel.rules.SubQueryRemoveRule]]. the full logical plan is
- * {{{
- * LogicalProject(a=[$0], b=[$1], c=[$2])
- * +- LogicalJoin(condition=[$3], joinType=[semi])
- * :- LogicalTableScan(table=[[x, source: [TestTableSource(a, b, c)]]])
- * +- LogicalProject($f0=[IS NOT NULL($0)])
- * +- LogicalAggregate(group=[{}], m=[MIN($0)])
- * +- LogicalProject(i=[true])
- * +- LogicalTableScan(table=[[y, source: [TestTableSource(d, e, f)]]])
- * }}}
+ * Planner rule that rewrites scalar query in filter like: `select * from T1 where (select count(*)
+ * from T2) > 0` to `select * from T1 where exists (select * from T2)`, which could be converted to
+ * SEMI join by [[FlinkSubQueryRemoveRule]].
  *
- * <p> This file is adapted from flink.
+ * <p>Without this rule, the original query will be rewritten to a filter on a join on an aggregate
+ * by [[org.apache.calcite.rel.rules.SubQueryRemoveRule]]. the full logical plan is {{{
+ * LogicalProject(a=[$0], b=[$1], c=[$2]) +- LogicalJoin(condition=[$3], joinType=[semi]) :-
+ * LogicalTableScan(table=[[x, source: [TestTableSource(a, b, c)]]]) +- LogicalProject($f0=[IS NOT
+ * NULL($0)]) +- LogicalAggregate(group=[{}], m=[MIN($0)]) +- LogicalProject(i=[true]) +-
+ * LogicalTableScan(table=[[y, source: [TestTableSource(d, e, f)]]]) }}}
+ *
+ * <p>This file is adapted from flink.
  */
 class RewriteSubQueryRule extends RelOptRule {
   public static final RewriteSubQueryRule INSTANCE = new RewriteSubQueryRule();
 
   public RewriteSubQueryRule() {
-    super(operandJ(Filter.class, null, RexUtil.SubQueryFinder.FILTER_PREDICATE, any()),
+    super(
+        operandJ(Filter.class, null, RexUtil.SubQueryFinder.FILTER_PREDICATE, any()),
         RelFactories.LOGICAL_BUILDER,
         "FlinkRewriteSubQueryRule:Filter");
   }
-
 
   @Override
   public void onMatch(RelOptRuleCall call) {
@@ -82,19 +75,20 @@ class RewriteSubQueryRule extends RelOptRule {
 
   // scalar query like: `(select count(*) from T) > 0` can be converted to `exists(select * from T)`
   RexNode rewriteScalarQuery(RexNode condition) {
-    return condition.accept(new RexShuttle() {
-      @Override
-      public RexNode visitCall(RexCall call) {
-        var subQuery = getSupportedScalarQuery(call);
-        if (subQuery.isPresent()) {
-          var sq = subQuery.get();
-          var aggInput = sq.rel.getInput(0);
-          return RexSubQuery.exists(aggInput);
-        } else {
-          return super.visitCall(call);
-        }
-      }
-    });
+    return condition.accept(
+        new RexShuttle() {
+          @Override
+          public RexNode visitCall(RexCall call) {
+            var subQuery = getSupportedScalarQuery(call);
+            if (subQuery.isPresent()) {
+              var sq = subQuery.get();
+              var aggInput = sq.rel.getInput(0);
+              return RexSubQuery.exists(aggInput);
+            } else {
+              return super.visitCall(call);
+            }
+          }
+        });
   }
 
   private boolean isScalarQuery(RexNode n) {
@@ -121,10 +115,10 @@ class RewriteSubQueryRule extends RelOptRule {
       var agg = (Aggregate) n;
       if (agg.getGroupCount() == 0 && agg.getAggCallList().size() == 1) {
         var aggCall = agg.getAggCallList().get(0);
-        return !aggCall.isDistinct() &&
-            aggCall.filterArg < 0 &&
-            aggCall.getArgList().isEmpty() &&
-            (aggCall.getAggregation() instanceof SqlCountAggFunction);
+        return !aggCall.isDistinct()
+            && aggCall.filterArg < 0
+            && aggCall.getArgList().isEmpty()
+            && (aggCall.getAggregation() instanceof SqlCountAggFunction);
       }
     }
     return false;
@@ -132,60 +126,61 @@ class RewriteSubQueryRule extends RelOptRule {
 
   private Optional<RexSubQuery> getSupportedScalarQuery(RexCall call) {
     switch (call.getKind()) {
-      // (select count(*) from T) > X (X is between 0 (inclusive) and 1 (exclusive))
-      case GREATER_THAN: {
-        if (isScalarQuery(call.operands.get(0))) {
-          var subQuery = (RexSubQuery) call.operands.get(0);
-          if (isCountStarAggWithoutGroupBy(subQuery.rel) &&
-              isBetween0And1(call.operands.get(call.operands.size() - 1), true, false)) {
-            return Optional.of(subQuery);
-          } else {
-            return Optional.empty();
+      case GREATER_THAN:
+        {
+          if (isScalarQuery(call.operands.get(0))) {
+            var subQuery = (RexSubQuery) call.operands.get(0);
+            if (isCountStarAggWithoutGroupBy(subQuery.rel)
+                && isBetween0And1(call.operands.get(call.operands.size() - 1), true, false)) {
+              return Optional.of(subQuery);
+            } else {
+              return Optional.empty();
+            }
           }
+          break;
         }
-        break;
-      }
 
-      // (select count(*) from T) >= X (X is between 0 (exclusive) and 1 (inclusive))
-      case GREATER_THAN_OR_EQUAL: {
-        if (isScalarQuery(call.operands.get(0))) {
-          var subQuery = (RexSubQuery) call.operands.get(0);
+      case GREATER_THAN_OR_EQUAL:
+        {
+          if (isScalarQuery(call.operands.get(0))) {
+            var subQuery = (RexSubQuery) call.operands.get(0);
 
-          if (isCountStarAggWithoutGroupBy(subQuery.rel) &&
-              isBetween0And1(call.operands.get(call.getOperands().size() - 1), false, true)) {
-            return Optional.of(subQuery);
-          } else {
-            return Optional.empty();
+            if (isCountStarAggWithoutGroupBy(subQuery.rel)
+                && isBetween0And1(call.operands.get(call.getOperands().size() - 1), false, true)) {
+              return Optional.of(subQuery);
+            } else {
+              return Optional.empty();
+            }
           }
+          break;
         }
-        break;
-      }
-      // X < (select count(*) from T) (X is between 0 (inclusive) and 1 (exclusive))
-      case LESS_THAN: {
-        if (isScalarQuery(call.operands.get(call.getOperands().size() - 1))) {
-          var subQuery = (RexSubQuery) call.operands.get(call.getOperands().size() - 1);
-          if (isCountStarAggWithoutGroupBy(subQuery.rel) &&
-              isBetween0And1(call.operands.get(0), true, false)) {
-            return Optional.of(subQuery);
-          } else {
-            return Optional.empty();
+      case LESS_THAN:
+        {
+          if (isScalarQuery(call.operands.get(call.getOperands().size() - 1))) {
+            var subQuery = (RexSubQuery) call.operands.get(call.getOperands().size() - 1);
+            if (isCountStarAggWithoutGroupBy(subQuery.rel)
+                && isBetween0And1(call.operands.get(0), true, false)) {
+              return Optional.of(subQuery);
+            } else {
+              return Optional.empty();
+            }
           }
+          break;
         }
-        break;
-      }
-      // X <= (select count(*) from T) (X is between 0 (exclusive) and 1 (inclusive))
-      case LESS_THAN_OR_EQUAL: {
-        if (isScalarQuery(call.operands.get(call.operands.size() - 1))) {
-          var subQuery = (RexSubQuery) call.operands.get(call.operands.size() - 1);
-          if (isCountStarAggWithoutGroupBy(subQuery.rel) &&
-              isBetween0And1(call.operands.get(0), false, true)) {
-            return Optional.of(subQuery);
-          } else {
-            return Optional.empty();
+        // X <= (select count(*) from T) (X is between 0 (exclusive) and 1 (inclusive))
+      case LESS_THAN_OR_EQUAL:
+        {
+          if (isScalarQuery(call.operands.get(call.operands.size() - 1))) {
+            var subQuery = (RexSubQuery) call.operands.get(call.operands.size() - 1);
+            if (isCountStarAggWithoutGroupBy(subQuery.rel)
+                && isBetween0And1(call.operands.get(0), false, true)) {
+              return Optional.of(subQuery);
+            } else {
+              return Optional.empty();
+            }
           }
+          break;
         }
-        break;
-      }
       default:
         break;
     }
