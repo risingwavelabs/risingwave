0af50a7fffa44de23843ace3a222cf999d43f42d
st1page <1245835950@qq.com>
2022-03-03 16:09:50 +0800

    clippy

--- rust/batch/src/executor/create_table.rs
+++ rust/batch/src/executor/create_table.rs
@@ -84,7 +84,8 @@ impl Executor for CreateTableExecutor {
             Info::TableSource(_) => {
                 // Create table_v2.
                 info!("Create table id:{}", &self.table_id.table_id());
-                self.table_manager
+                let _table = self
+                    .table_manager
                     .create_table_v2(&self.table_id, table_columns.clone());
                 self.source_manager
                     .create_table_source_v2(&self.table_id, table_columns)?;
