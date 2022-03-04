b5ef2a802c7e704dc0017237578113010151e027
Kaige Li <3454378359@qq.com>
2022-03-04 18:08:17 +0800

    bind selection.

--- rust/frontend/src/binder/select.rs
+++ rust/frontend/src/binder/select.rs
@@ -1,10 +1,11 @@
 use std::fmt::Debug;
-use risingwave_common::error::{ErrorCode,Result};
+
+use risingwave_common::error::{ErrorCode, Result};
 use risingwave_common::types::DataType;
-use crate::expr::{Expr, ExprImpl};
 use risingwave_sqlparser::ast::Select;
 
 use crate::binder::{Binder, TableRef};
+use crate::expr::{Expr, ExprImpl};
 
 #[derive(Debug)]
 pub struct BoundSelect {
@@ -27,13 +28,21 @@ impl Binder {
         //     None=>None,
         // }
         // let return_type = select.selection.map(|expr| expr.return_type())
-        let return_typ = select
-        .selection
-        .map(|expr| self.bind_expr(expr))
-        .unwrap();
-        .Result;
-        if return_typ != DataType::Boolean {
-            return ErrorCode::InvalidInputSyntax("argument of WHERE (must be type boolean)".to_string(),return_type).transpose();
+        // let return_type = select.selection.map(|expr| self.bind_expr(expr)).unwrap();
+        // if return_type != DataType::Boolean {
+        //     return ErrorCode::InvalidInputSyntax(
+        //         "argument of WHERE (must be type boolean)".to_string(),
+        //         return_type,
+        //     )
+        //     .transpose();
+        // }
+        if let Some(selection) = &selection {
+            if !matches!(selection.return_type(), DataType::Boolean) {
+                return Err(ErrorCode::InternalError(
+                    "The return type must be boolean!".to_string(),
+                )
+                .into());
+            }
         }
         let projection = self.bind_projection(select.projection)?;
         Ok(BoundSelect {
