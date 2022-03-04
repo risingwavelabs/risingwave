326e723632681291679daa0bcf34043abef0c860
xxchan <xxchan22f@gmail.com>
2022-03-04 18:25:52 +0800

    Add docs for `prune_col`. Update filter: insert project only when needed

--- rust/frontend/src/optimizer/plan_node/col_pruning.rs
+++ rust/frontend/src/optimizer/plan_node/col_pruning.rs
@@ -7,7 +7,17 @@ use crate::{for_batch_plan_nodes, for_stream_plan_nodes};
 
 /// The trait for column pruning, only logical plan node will use it, though all plan node impl it.
 pub trait ColPrunable {
-    /// transform the plan node to only output the required columns ordered by index number.
+    /// Transform the plan node to only output the required columns ordered by index number.
+    ///
+    /// `required_cols` must be a subset of the range `0..self.schema().len()`.
+    ///
+    /// After calling `prune_col` on the children, their output schema may change, so
+    /// the caller may need to transform its [`InputRef`] using
+    /// [`ColIndexMapping`](crate::utils::ColIndexMapping).
+    ///
+    /// When implementing this method for a node, it may require its children to produce additional
+    /// columns besides `required_cols`. In this case, it may need to insert a
+    /// [`LogicalProject`](super::LogicalProject) above to satisfy the given `required_cols`.
     fn prune_col(&self, required_cols: &FixedBitSet) -> PlanRef;
 }
 
