0f3e9290c8999d02b4e0b305ff29bed06190bc8d
Eric Fu <fuyufjh@163.com>
2022-03-09 16:38:53 +0800

    revert: actor cancellation (#795)
    * Revert "feat: report failed actors in barrier manager (#759)"
    
    This reverts commit f584ba8d8199e95dc2a133ec24303e5c81d434a0.
    
    * Revert "fix(#691): Source actor cancellation cause CN, MN to panic. (#736)"
    
    This reverts commit 376c484edec45bd385eaeac763af48bbeb101e54.

--- rust/stream/src/task/stream_manager.rs
+++ rust/stream/src/task/stream_manager.rs
@@ -20,7 +20,6 @@ use risingwave_storage::{dispatch_state_store, Keyspace, StateStore, StateStoreI
 use tokio::sync::oneshot;
 use tokio::task::JoinHandle;
 
-use super::FailedActors;
 use crate::executor::*;
 use crate::task::{
     ConsumableChannelPair, SharedContext, StreamEnvironment, UpDownActorIds,
@@ -118,7 +117,7 @@ impl StreamManager {
         barrier: &Barrier,
         actor_ids_to_send: impl IntoIterator<Item = u32>,
         actor_ids_to_collect: impl IntoIterator<Item = u32>,
-    ) -> Result<oneshot::Receiver<FailedActors>> {
+    ) -> Result<oneshot::Receiver<()>> {
         let core = self.core.lock().unwrap();
         let mut barrier_manager = core.context.lock_barrier_manager();
         let rx = barrier_manager
@@ -133,11 +132,11 @@ impl StreamManager {
         barrier: &Barrier,
         actor_ids_to_send: impl IntoIterator<Item = u32>,
         actor_ids_to_collect: impl IntoIterator<Item = u32>,
-    ) -> Result<FailedActors> {
+    ) -> Result<()> {
         let rx = self.send_barrier(barrier, actor_ids_to_send, actor_ids_to_collect)?;
 
-        // Wait for all actors to finish this barrier.
-        let failed_actors = rx.await.unwrap();
+        // Wait for all actors finishing this barrier.
+        rx.await.unwrap();
 
         // Sync states from shared buffer to S3 before telling meta service we've done.
         dispatch_state_store!(self.state_store(), store, {
@@ -152,7 +151,7 @@ impl StreamManager {
             }
         });
 
-        Ok(failed_actors)
+        Ok(())
     }
 
     /// Broadcast a barrier to all senders. Returns immediately, and caller won't be notified when
