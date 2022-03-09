7048721290a4bfd8a5455146a3c01d235b9fe471
lmatz <lmatz823@gmail.com>
2022-03-09 13:36:52 +0800

    refactor: clean MViewTable a bit (#774)

--- rust/stream/src/executor/mview/test_utils.rs
+++ rust/stream/src/executor/mview/test_utils.rs
@@ -1,23 +1,29 @@
 use risingwave_common::array::Row;
+use risingwave_common::catalog::ColumnDesc;
+use risingwave_common::types::DataType;
 use risingwave_common::util::sort_util::OrderType;
 use risingwave_storage::memory::MemoryStateStore;
 use risingwave_storage::Keyspace;
 
-use super::super::test_utils::schemas;
 use super::{MViewTable, ManagedMViewState};
 
 pub async fn gen_basic_table(row_count: usize) -> MViewTable<MemoryStateStore> {
     let state_store = MemoryStateStore::new();
-    let schema = schemas::iii();
-    let pk_columns = vec![0, 1];
+    // let pk_columns = vec![0, 1]; leave a message to indicate pk columns
     let orderings = vec![OrderType::Ascending, OrderType::Descending];
     let keyspace = Keyspace::executor_root(state_store, 0x42);
+    let column_ids = vec![0.into(), 1.into(), 2.into()];
     let mut state = ManagedMViewState::new(
         keyspace.clone(),
-        vec![0.into(), 1.into(), 2.into()],
+        column_ids.clone(),
         vec![OrderType::Ascending, OrderType::Descending],
     );
-    let table = MViewTable::new_for_test(keyspace.clone(), schema, pk_columns.clone(), orderings);
+    let column_descs = vec![
+        ColumnDesc::unnamed(column_ids[0], DataType::Int32),
+        ColumnDesc::unnamed(column_ids[1], DataType::Int32),
+        ColumnDesc::unnamed(column_ids[2], DataType::Int32),
+    ];
+    let table = MViewTable::new_for_test(keyspace.clone(), column_descs, orderings);
     let epoch: u64 = 0;
 
     for idx in 0..row_count {
