9ca92c6634199db687ab5c633afb39f79ffbed5f
st1page <1245835950@qq.com>
2022-03-10 11:24:25 +0800

    comments

--- rust/common/src/catalog/physical_table.rs
+++ rust/common/src/catalog/physical_table.rs
@@ -1,7 +1,7 @@
 use super::{OrderedColumnDesc, TableId};
 
-/// the catalog of physical table in our system which can be query by batch, and the enum including
-/// the table descriptor all information for compute node to access data of the table.
+/// the catalog of physical table in our system which can be access by batch query, and the enum
+/// including the table descriptor all information for compute node to access data of the table.
 enum PhysicalTable {
     CellBased(CellBasedTableDesc),
 }
@@ -14,7 +14,6 @@ pub struct CellBasedTableDesc {
     /// the primary key columns' descriptor
     pub pk: Vec<OrderedColumnDesc>,
     // TODO: the other columns' descriptor, or all the columns? it is actually not used in the
-    // compute node... for the `RowSeqScan` plan node include its requiring `columnDesc`. so we
-    // don't need add it to the desc
+    // compute node... for the plan node include its requiring `columnDesc`.
     // pub columns: Vec<ColumnDesc>,
 }
