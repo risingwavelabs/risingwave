9d2f281f2c1c46d26d5a9ff91ba6c224ed8823da
st1page <1245835950@qq.com>
2022-03-10 13:20:55 +0800

    remove physical table over design

--- rust/common/src/catalog/physical_table.rs
+++ rust/common/src/catalog/physical_table.rs
@@ -1,9 +1,5 @@
 use super::{OrderedColumnDesc, TableId};
 
-enum PhysicalTable {
-    CellBased(CellBasedTableDesc),
-}
-
 /// the table descriptor of table with cell based encoding in state store and include all
 /// information for compute node to access data of the table.
 #[derive(Debug, Clone)]
@@ -12,7 +8,4 @@ pub struct CellBasedTableDesc {
     pub table_id: TableId,
     /// the primary key columns' descriptor
     pub pk: Vec<OrderedColumnDesc>,
-    // TODO: the other columns' descriptor, or all the columns? it is actually not used in the
-    // compute node... for the plan node include its requiring `columnDesc`.
-    // pub columns: Vec<ColumnDesc>,
 }
