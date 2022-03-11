f175bbd4068cc4c0e026bfe633b79d4bdc074d52
cykbls01 <chenyikun@ChendeMacBook-Pro.local>
2022-03-10 23:15:49 -0500

    fix: ci error

--- rust/frontend/src/handler/create_source.rs
+++ rust/frontend/src/handler/create_source.rs
@@ -164,7 +164,7 @@ mod tests {
         let frontend = LocalFrontend::new().await;
         frontend.run_sql(sql).await.unwrap();
 
-        let catalog_manager = frontend.session().env().catalog_mgr();
+        let catalog_manager = frontend.session().ctx.env().catalog_mgr();
         let table = catalog_manager.get_table("dev", "dev", "t").unwrap();
 
         let mut rows = vec![];
