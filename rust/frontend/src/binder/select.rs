1d076a5e510cc26832bddf5ba2a4039d40e2e04c
Xiangjin <xiangjin@singularity-data.com>
2022-03-14 23:12:11 +0800

    bind aliases

--- rust/frontend/src/binder/select.rs
+++ rust/frontend/src/binder/select.rs
@@ -2,11 +2,11 @@ use std::fmt::Debug;
 
 use risingwave_common::error::{ErrorCode, Result};
 use risingwave_common::types::DataType;
-use risingwave_sqlparser::ast::{Select, SelectItem};
+use risingwave_sqlparser::ast::{Expr, Select, SelectItem};
 
 use super::bind_context::Clause;
 use crate::binder::{Binder, TableRef};
-use crate::expr::{Expr, ExprImpl, InputRef};
+use crate::expr::{Expr as _, ExprImpl, InputRef};
 
 #[derive(Debug)]
 pub struct BoundSelect {
@@ -38,8 +38,7 @@ impl Binder {
                 .into());
             }
         }
-        let select_items = self.bind_project(select.projection)?;
-        let aliases = vec![None; select_items.len()];
+        let (select_items, aliases) = self.bind_project(select.projection)?;
         Ok(BoundSelect {
             distinct: select.distinct,
             select_items,
@@ -49,31 +48,54 @@ impl Binder {
         })
     }
 
-    pub fn bind_project(&mut self, select_items: Vec<SelectItem>) -> Result<Vec<ExprImpl>> {
+    pub fn bind_project(
+        &mut self,
+        select_items: Vec<SelectItem>,
+    ) -> Result<(Vec<ExprImpl>, Vec<Option<String>>)> {
         let mut select_list = vec![];
+        let mut aliases = vec![];
         for item in select_items {
             match item {
                 SelectItem::UnnamedExpr(expr) => {
+                    let alias = match &expr {
+                        Expr::Identifier(ident) => Some(ident.value.clone()),
+                        Expr::CompoundIdentifier(idents) => {
+                            idents.last().map(|ident| ident.value.clone())
+                        }
+                        _ => None,
+                    };
                     let expr = self.bind_expr(expr)?;
                     select_list.push(expr);
+                    aliases.push(alias);
                 }
-                SelectItem::ExprWithAlias { .. } => todo!(),
+                SelectItem::ExprWithAlias { expr, alias } => {
+                    let expr = self.bind_expr(expr)?;
+                    select_list.push(expr);
+                    aliases.push(Some(alias.value));
+                }
                 SelectItem::QualifiedWildcard(_) => todo!(),
                 SelectItem::Wildcard => {
-                    select_list.extend(self.bind_all_columns()?.into_iter());
+                    let (exprs, names) = self.bind_all_columns()?;
+                    select_list.extend(exprs);
+                    aliases.extend(names);
                 }
             }
         }
-        Ok(select_list)
+        Ok((select_list, aliases))
     }
 
-    pub fn bind_all_columns(&mut self) -> Result<Vec<ExprImpl>> {
+    pub fn bind_all_columns(&mut self) -> Result<(Vec<ExprImpl>, Vec<Option<String>>)> {
         let bound_columns = self
             .context
             .columns
             .iter()
-            .map(|column| InputRef::new(column.index, column.data_type.clone()).into())
-            .collect();
+            .map(|column| {
+                (
+                    InputRef::new(column.index, column.data_type.clone()).into(),
+                    Some(column.column_name.clone()),
+                )
+            })
+            .unzip();
         Ok(bound_columns)
     }
 }
