e701642aeeba08ab5f770825d45ec41e2e3220f1
cykbls01 <chenyikun@ChendeMacBook-Pro.local>
2022-03-14 14:05:00 -0400

    feat: query handler

---
+++ rust/frontend/src/handler/query.rs
@@ -0,0 +1,79 @@
+use std::cell::RefCell;
+use std::rc::Rc;
+use log::info;
+
+use pgwire::pg_response::{PgResponse, StatementType};
+use risingwave_common::error::ErrorCode::InternalError;
+use risingwave_common::error::{Result, RwError};
+use risingwave_pb::plan::{QueryId, StageId, TaskId, TaskSinkId};
+use risingwave_rpc_client::{ComputeClient, ExchangeSource, GrpcExchangeSource};
+use risingwave_sqlparser::ast::{Query, Statement};
+
+use crate::binder::Binder;
+use crate::handler::util::to_pg_rows;
+use crate::planner::Planner;
+use crate::scheduler::schedule::WorkerNodeManager;
+use crate::session::QueryContext;
+
+pub async fn handle_query(context: QueryContext, query: Box<Query>) -> Result<PgResponse> {
+    let session = context.session_ctx.clone();
+    let catalog_mgr = session.env().catalog_mgr();
+    let catalog = catalog_mgr
+        .get_database_snapshot(session.database())
+        .unwrap();
+    let mut binder = Binder::new(catalog);
+    let bound = binder.bind(Statement::Query(query))?;
+    let nodes = Planner::new(Rc::new(RefCell::new(context)))
+        .plan(bound)?
+        .gen_batch_query_plan()
+        .to_batch_prost()
+        .children
+        .clone();
+
+    // Choose the first node by WorkerNodeManager.
+    let manager = WorkerNodeManager::new(session.env().meta_client().clone()).await?;
+    let address = manager
+        .list_worker_nodes()
+        .get(0)
+        .ok_or_else(|| RwError::from(InternalError("first work node not found".to_string())))?
+        .host
+        .as_ref()
+        .ok_or_else(|| RwError::from(InternalError("host address not found".to_string())))?
+        .to_socket_addr()?;
+    let compute_client: ComputeClient = ComputeClient::new(&address).await?;
+
+    // Build task id and task sink id
+    let task_id = TaskId {
+        stage_id: Some(StageId {
+            query_id: Some(QueryId {
+                trace_id: uuid::Uuid::new_v4().to_string(),
+            }),
+            stage_id: 0,
+        }),
+        task_id: 0,
+    };
+    let task_sink_id = TaskSinkId {
+        task_id: Some(task_id.clone()),
+        sink_id: 0,
+    };
+
+    let mut rows = vec![];
+    for node in nodes {
+        compute_client
+            .create_task(task_id.clone(), node.clone())
+            .await?;
+        let mut source =
+            GrpcExchangeSource::create_with_client(compute_client.clone(), task_sink_id.clone())
+                .await?;
+        while let Some(chunk) = source.take_data().await? {
+            rows.append(&mut to_pg_rows(chunk));
+        }
+    }
+
+    Ok(PgResponse::new(
+        StatementType::SELECT,
+        rows.len() as i32,
+        rows,
+        vec![],
+    ))
+}
