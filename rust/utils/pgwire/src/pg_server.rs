094c099af71f3589a0f92726dd30c4bc943ec4d5
Bowen <36908971+BowenXiao1999@users.noreply.github.com>
2022-03-15 19:10:33 +0800

    refactor(session): run statement use Arc and remove SessionContext (#913)

--- rust/utils/pgwire/src/pg_server.rs
+++ rust/utils/pgwire/src/pg_server.rs
@@ -26,7 +26,11 @@ use crate::pg_response::PgResponse;
 /// The interface for a database system behind pgwire protocol.
 /// We can mock it for testing purpose.
 pub trait SessionManager: Send + Sync {
+<<<<<<< HEAD
     fn connect(&self, database: &str) -> Result<Arc<dyn Session>, Box<dyn Error + Send + Sync>>;
+=======
+    fn connect(&self) -> Arc<dyn Session>;
+>>>>>>> aa996054 (refactor(session): run statement use Arc and remove SessionContext (#913))
 }
 
 /// A psql connection. Each connection binds with a database. Switching database will need to
@@ -98,11 +102,16 @@ mod tests {
     struct TestSessionManager {}
 
     impl SessionManager for TestSessionManager {
+<<<<<<< HEAD
         fn connect(
             &self,
             _database: &str,
         ) -> Result<Arc<dyn super::Session>, Box<dyn Error + Send + Sync>> {
             Ok(Arc::new(TestSession {}))
+=======
+        fn connect(&self) -> Arc<dyn super::Session> {
+            Arc::new(TestSession {})
+>>>>>>> aa996054 (refactor(session): run statement use Arc and remove SessionContext (#913))
         }
     }
 
