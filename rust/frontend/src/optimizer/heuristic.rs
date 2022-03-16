8038a5b00af9fd3dc8adbeb32fec1248b8c5c023
Eric Fu <eric@singularity-data.com>
2022-03-16 16:46:16 +0800

    refactor: remove plan_pass and modify heuristic_optimizer

--- rust/frontend/src/optimizer/heuristic.rs
+++ rust/frontend/src/optimizer/heuristic.rs
@@ -1,7 +1,5 @@
 use itertools::Itertools;
 
-use super::PlanPass;
-use crate::optimizer::property::{Distribution, Order};
 use crate::optimizer::rule::BoxedRule;
 use crate::optimizer::PlanRef;
 
@@ -34,7 +32,7 @@ impl HeuristicOptimizer {
         }
     }
 
-    fn optimize_self_node(&mut self, mut plan: PlanRef) -> PlanRef {
+    fn optimize_node(&self, mut plan: PlanRef) -> PlanRef {
         for rule in &self.rules {
             if let Some(applied) = rule.apply(plan.clone()) {
                 plan = applied;
@@ -43,39 +41,25 @@ impl HeuristicOptimizer {
         plan
     }
 
-    fn optimize_inputs(&mut self, plan: PlanRef) -> PlanRef {
-        let order_required = plan.inputs_order_required();
-        let dists_required = plan.inputs_distribution_required();
-
+    fn optimize_inputs(&self, plan: PlanRef) -> PlanRef {
         let inputs = plan
             .inputs()
             .into_iter()
-            .zip_eq(order_required.into_iter())
-            .zip_eq(dists_required.into_iter())
-            .map(|((sub_tree, order), dist)| self.pass_with_require(sub_tree, order, dist))
+            .map(|sub_tree| self.optimize(sub_tree))
             .collect_vec();
         plan.clone_with_inputs(&inputs)
     }
-}
 
-impl PlanPass for HeuristicOptimizer {
-    fn pass_with_require(
-        &mut self,
-        mut plan: PlanRef,
-        required_order: &Order,
-        required_dist: &Distribution,
-    ) -> PlanRef {
-        plan = match self.apply_order {
+    pub fn optimize(&self, mut plan: PlanRef) -> PlanRef {
+        match self.apply_order {
             ApplyOrder::TopDown => {
-                plan = self.optimize_self_node(plan);
+                plan = self.optimize_node(plan);
                 self.optimize_inputs(plan)
             }
             ApplyOrder::BottomUp => {
                 plan = self.optimize_inputs(plan);
-                self.optimize_self_node(plan)
+                self.optimize_node(plan)
             }
-        };
-        plan = required_order.enforce_if_not_satisfies(plan);
-        required_dist.enforce_if_not_satisfies(plan, required_order)
+        }
     }
 }
