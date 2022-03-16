d620a5a127edc2b0f3b408adf0fb5a6364ebb10c
Alex Chi <iskyzh@gmail.com>
2022-03-16 11:24:49 +0800

    chore(frontend): better expr display string (#941)
    Signed-off-by: Alex Chi <iskyzh@gmail.com>

--- rust/frontend/src/expr/function_call.rs
+++ rust/frontend/src/expr/function_call.rs
@@ -3,12 +3,72 @@ use risingwave_common::types::DataType;
 use super::{infer_type, Expr, ExprImpl};
 use crate::expr::ExprType;
 
-#[derive(Clone, Debug, PartialEq)]
+#[derive(Clone, PartialEq)]
 pub struct FunctionCall {
     func_type: ExprType,
     return_type: DataType,
     inputs: Vec<ExprImpl>,
 }
+
+fn debug_binary_op(
+    f: &mut std::fmt::Formatter<'_>,
+    op: &str,
+    inputs: &[ExprImpl],
+) -> std::fmt::Result {
+    use std::fmt::Debug;
+
+    assert_eq!(inputs.len(), 2);
+
+    write!(f, "(")?;
+    inputs[0].fmt(f)?;
+    write!(f, " {} ", op)?;
+    inputs[1].fmt(f)?;
+    write!(f, ")")?;
+
+    Ok(())
+}
+
+impl std::fmt::Debug for FunctionCall {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        if f.alternate() {
+            f.debug_struct("FunctionCall")
+                .field("func_type", &self.func_type)
+                .field("return_type", &self.return_type)
+                .field("inputs", &self.inputs)
+                .finish()
+        } else {
+            match &self.func_type {
+                ExprType::Cast => {
+                    assert_eq!(self.inputs.len(), 1);
+                    self.inputs[0].fmt(f)?;
+                    return write!(f, "::{:?}", self.return_type);
+                }
+                ExprType::Add => debug_binary_op(f, "+", &self.inputs),
+                ExprType::Subtract => debug_binary_op(f, "-", &self.inputs),
+                ExprType::Multiply => debug_binary_op(f, "*", &self.inputs),
+                ExprType::Divide => debug_binary_op(f, "/", &self.inputs),
+                ExprType::Modulus => debug_binary_op(f, "%", &self.inputs),
+                ExprType::Equal => debug_binary_op(f, "=", &self.inputs),
+                ExprType::NotEqual => debug_binary_op(f, "<>", &self.inputs),
+                ExprType::LessThan => debug_binary_op(f, "<", &self.inputs),
+                ExprType::LessThanOrEqual => debug_binary_op(f, "<=", &self.inputs),
+                ExprType::GreaterThan => debug_binary_op(f, ">", &self.inputs),
+                ExprType::GreaterThanOrEqual => debug_binary_op(f, ">=", &self.inputs),
+                ExprType::And => debug_binary_op(f, "AND", &self.inputs),
+                ExprType::Or => debug_binary_op(f, "OR", &self.inputs),
+                _ => {
+                    let func_name = format!("{:?}", self.func_type);
+                    let mut builder = f.debug_tuple(&func_name);
+                    self.inputs.iter().for_each(|child| {
+                        builder.field(child);
+                    });
+                    builder.finish()
+                }
+            }
+        }
+    }
+}
+
 impl FunctionCall {
     pub fn new(func_type: ExprType, inputs: Vec<ExprImpl>) -> Option<Self> {
         let return_type = infer_type(
