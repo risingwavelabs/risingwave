12d4ca6a74f943cf1b6ffc14bb0fed6636974ca8
Eric Fu <eric@singularity-data.com>
2022-03-18 15:41:51 +0800

    fix stream_exchange.rs

--- rust/frontend/src/optimizer/plan_node/stream_exchange.rs
+++ rust/frontend/src/optimizer/plan_node/stream_exchange.rs
@@ -6,7 +6,6 @@ use risingwave_pb::stream_plan::stream_node::Node;
 use risingwave_pb::stream_plan::{Dispatcher, ExchangeNode};
 
 use super::{PlanRef, PlanTreeNodeUnary, StreamBase, ToStreamProst};
-use crate::optimizer::property::order::WithOrder;
 use crate::optimizer::property::{Distribution, WithDistribution, WithSchema};
 
 /// `StreamExchange` imposes a particular distribution on its input
@@ -37,10 +36,7 @@ impl StreamExchange {
 
 impl fmt::Display for StreamExchange {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        f.debug_struct("StreamExchange")
-            .field("order", self.order())
-            .field("dist", self.distribution())
-            .finish()
+        write!(f, "StreamExchange {{ dist: {:?} }}", self.base.dist)
     }
 }
 
