b2f7ad42d34d4176ec0bb98e260de1a846fc446b
xxchan <37948597+xxchan@users.noreply.github.com>
2022-03-22 09:30:08 +0100

    chore: use option for target_upper in mapping (#1152)

--- rust/frontend/src/optimizer/plan_node/logical_project.rs
+++ rust/frontend/src/optimizer/plan_node/logical_project.rs
@@ -89,12 +89,13 @@ impl LogicalProject {
             input,
             mapping
         );
-        if mapping.is_empty() {
-            // The parent actually doesn't need the output of the input.
+        let mut input_refs = if let Some(target_upper) = mapping.target_upper() {
+            vec![None; target_upper + 1]
+        } else {
+            // The mapping is empty, so the parent actually doesn't need the output of the input.
             // This can happen when the parent node only selects constant expressions.
             return input;
-        }
-        let mut input_refs = vec![None; mapping.target_upper() + 1];
+        };
         for (src, tar) in mapping.mapping_pairs() {
             assert_eq!(input_refs[tar], None);
             input_refs[tar] = Some(src);
