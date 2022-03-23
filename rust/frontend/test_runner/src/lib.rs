503a5c368bbbf9dfe33bfb68ac21f3e8ca9ad36b
Alex Chi <iskyzh@gmail.com>
2022-03-23 19:41:51 +0800

    use yaml for proto test
    Signed-off-by: Alex Chi <iskyzh@gmail.com>

--- rust/frontend/test_runner/src/lib.rs
+++ rust/frontend/test_runner/src/lib.rs
@@ -236,7 +236,7 @@ impl TestCase {
 
             // Only generate batch_plan_proto if it is specified in test case
             if self.batch_plan_proto.is_some() {
-                ret.batch_plan_proto = Some(serde_json::to_string_pretty(
+                ret.batch_plan_proto = Some(serde_yaml::to_string(
                     &batch_plan.to_batch_prost_identity(false),
                 )?);
             }
@@ -259,7 +259,7 @@ impl TestCase {
 
             // Only generate stream_plan_proto if it is specified in test case
             if self.stream_plan_proto.is_some() {
-                ret.stream_plan_proto = Some(serde_json::to_string_pretty(
+                ret.stream_plan_proto = Some(serde_yaml::to_string(
                     &stream_plan.to_stream_prost_identity(false),
                 )?);
             }
