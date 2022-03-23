81c2776081f8e7bc7cd7d216ad9694998bea518a
Alex Chi <iskyzh@gmail.com>
2022-03-23 10:09:40 +0800

    feat: enable deadlock detection for parking_lot (#1171)
    Signed-off-by: Alex Chi <iskyzh@gmail.com>

--- rust/stream/src/task/stream_manager.rs
+++ rust/stream/src/task/stream_manager.rs
@@ -11,10 +11,10 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-
 use std::collections::{HashMap, HashSet};
 use std::convert::TryFrom;
 use std::fmt::Debug;
+use std::net::SocketAddr;
 use std::sync::Arc;
 
 use futures::channel::mpsc::{channel, Receiver};
