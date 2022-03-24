37ad6634004249a6a16e9e5dece3dc0a6d408064
Bugen Zhao <i@bugenzhao.com>
2022-03-24 13:49:13 +0800

    fix affected rows
    Signed-off-by: Bugen Zhao <i@bugenzhao.com>

--- rust/frontend/src/handler/util.rs
+++ rust/frontend/src/handler/util.rs
@@ -38,14 +38,8 @@ pub fn to_pg_rows(chunk: DataChunk) -> Vec<Row> {
 /// Get the [`PgFieldDescriptor`] for return values of the given statement.
 pub fn get_pg_field_descs(bound: &BoundStatement) -> Result<Vec<PgFieldDescriptor>> {
     let pg_descs = match bound {
-        BoundStatement::Insert(_) | BoundStatement::Delete(_) => {
-            // affected rows
-            // TODO: do not record this in return values
-            vec![PgFieldDescriptor::new(
-                "affected_rows".to_owned(),
-                TypeOid::BigInt,
-            )]
-        }
+        // Only return the count of affected rows for these commands.
+        BoundStatement::Insert(_) | BoundStatement::Delete(_) => vec![],
 
         BoundStatement::Query(query) => match &query.body {
             BoundSetExpr::Select(select) => select
