508e0751e283ae963d62fee77fc9be17b660a711
TennyZhuang <zty0826@gmail.com>
2022-03-24 18:39:43 +0800

    refactor(stream): introduce MockSourceV2 && add filter UT back (#1245)
    Signed-off-by: TennyZhuang <zty0826@gmail.com>

--- rust/stream/src/lib.rs
+++ rust/stream/src/lib.rs
@@ -30,6 +30,8 @@
 #![feature(hash_drain_filter)]
 #![feature(drain_filter)]
 #![feature(generators)]
+#![feature(proc_macro_hygiene)]
+#![feature(stmt_expr_attributes)]
 
 #[macro_use]
 extern crate log;
