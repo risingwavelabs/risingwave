7a959cbfb3367b1abd74a6c9d9f3695681488f2a
Bugen Zhao <i@bugenzhao.com>
2022-03-29 13:37:10 +0800

    process inject responses on meta
    Signed-off-by: Bugen Zhao <i@bugenzhao.com>

--- rust/meta/src/barrier/mod.rs
+++ rust/meta/src/barrier/mod.rs
@@ -24,7 +24,7 @@ use risingwave_common::error::{ErrorCode, Result, RwError, ToRwResult};
 use risingwave_pb::common::worker_node::State::Running;
 use risingwave_pb::common::WorkerType;
 use risingwave_pb::data::Barrier;
-use risingwave_pb::stream_service::InjectBarrierRequest;
+use risingwave_pb::stream_service::{InjectBarrierRequest, InjectBarrierResponse};
 use smallvec::SmallVec;
 use tokio::sync::{oneshot, watch, RwLock};
 use uuid::Uuid;
@@ -246,7 +246,8 @@ where
             let mut notifiers = notifiers;
             notifiers.iter_mut().for_each(Notifier::notify_to_send);
             match self.run_inner(&command_ctx).await {
-                Ok(_) => {
+                Ok(_responses) => {
+                    // TODO: process the finished DDLs
                     prev_epoch = new_epoch;
                     notifiers.iter_mut().for_each(Notifier::notify_collected);
                 }
@@ -262,14 +263,17 @@ where
     }
 
     /// Running a scheduled command.
-    async fn run_inner<'a>(&self, command_context: &CommandContext<'a, S>) -> Result<()> {
+    async fn run_inner<'a>(
+        &self,
+        command_context: &CommandContext<'a, S>,
+    ) -> Result<Vec<InjectBarrierResponse>> {
         let timer = self.metrics.barrier_latency.start_timer();
 
         // Wait for all barriers collected
-        let collect_result = self.inject_barrier(command_context).await;
+        let result = self.inject_barrier(command_context).await;
         // Commit this epoch to Hummock
         if command_context.prev_epoch != INVALID_EPOCH {
-            match collect_result {
+            match result {
                 Ok(_) => {
                     self.hummock_manager
                         .commit_epoch(command_context.prev_epoch)
@@ -282,14 +286,19 @@ where
                 }
             };
         }
-        collect_result?;
+        let responses = result?;
 
         timer.observe_duration();
-        command_context.post_collect().await // do some post stuffs
+        command_context.post_collect().await?; // do some post stuffs
+
+        Ok(responses)
     }
 
     /// Inject barrier to all computer nodes.
-    async fn inject_barrier<'a>(&self, command_context: &CommandContext<'a, S>) -> Result<()> {
+    async fn inject_barrier<'a>(
+        &self,
+        command_context: &CommandContext<'a, S>,
+    ) -> Result<Vec<InjectBarrierResponse>> {
         let mutation = command_context.to_mutation().await?;
         let info = command_context.info;
 
@@ -329,16 +338,17 @@ where
                     );
 
                     // This RPC returns only if this worker node has collected this barrier.
-                    client.inject_barrier(request).await.to_rw_result()?;
-
-                    Ok::<_, RwError>(())
+                    client
+                        .inject_barrier(request)
+                        .await
+                        .map(tonic::Response::<_>::into_inner)
+                        .to_rw_result()
                 }
                 .into()
             }
         });
 
-        try_join_all(collect_futures).await?;
-        Ok(())
+        try_join_all(collect_futures).await
     }
 
     /// Resolve actor information from cluster and fragment manager.
