8b5fce4f9e0041c458474d33036e3ed113a2510d
Bugen Zhao <i@bugenzhao.com>
2022-03-31 15:13:34 +0800

    port the traditional chain
    Signed-off-by: Bugen Zhao <i@bugenzhao.com>

--- rust/stream/src/executor_v2/rearrange_chain.rs
+++ rust/stream/src/executor_v2/rearrange_chain.rs
@@ -14,10 +14,12 @@
 
 use futures::StreamExt;
 use futures_async_stream::try_stream;
+use risingwave_common::array::StreamChunk;
 use risingwave_common::catalog::Schema;
 
 use super::error::TracedStreamExecutorError;
 use super::{BoxedExecutor, Executor, ExecutorInfo, Message};
+use crate::task::FinishCreateMviewNotifier;
 
 pub struct RearrangeChainExecutor {
     snapshot: BoxedExecutor,
@@ -26,13 +28,77 @@ pub struct RearrangeChainExecutor {
 
     upstream_indices: Vec<usize>,
 
+    notifier: FinishCreateMviewNotifier,
+
     info: ExecutorInfo,
+}
+
+fn mapping(upstream_indices: &[usize], msg: Message) -> Message {
+    match msg {
+        Message::Chunk(chunk) => {
+            let columns = upstream_indices
+                .iter()
+                .map(|i| chunk.columns()[*i].clone())
+                .collect();
+            Message::Chunk(StreamChunk::new(
+                chunk.ops().to_vec(),
+                columns,
+                chunk.visibility().clone(),
+            ))
+        }
+        _ => msg,
+    }
 }
 
 impl RearrangeChainExecutor {
     #[try_stream(ok = Message, error = TracedStreamExecutorError)]
     async fn execute_inner(self) {
-        todo!()
+        let snapshot = self.snapshot.execute();
+        let mut upstream = self.upstream.execute();
+
+        // 1. Consume the upstream to get the first barrier.
+        //
+        let first_msg = upstream.next().await.unwrap()?;
+        let barrier = first_msg
+            .as_barrier()
+            .expect("the first message received by chain must be a barrier");
+        let epoch = barrier.epoch;
+
+        let to_consume_snapshot = if barrier.is_add_output_mutation() {
+            // If the barrier is a conf change of creating this mview, init snapshot from its epoch
+            // and begin to consume the snapshot.
+            // TODO: init the snapshot with epoch.prev
+            true
+        } else {
+            // If the barrier is not a conf change, it means we've recovered and the snapshot is
+            // already consumed.
+            false
+        };
+        // The first barrier message should be propagated.
+        yield first_msg;
+
+        // 2. Consume the snapshot if needed. Note that the snapshot is alreay projected, so there's
+        // no mapping required.
+        //
+        if to_consume_snapshot {
+            #[for_await]
+            for msg in snapshot {
+                let msg = msg?;
+                yield msg;
+            }
+        }
+
+        // 3. Report that we've finished the creation (for a workaround).
+        //
+        self.notifier.notify(epoch.curr);
+
+        // 4. Continuously consume the upstream.
+        //
+        #[for_await]
+        for msg in upstream {
+            let msg = msg?;
+            yield mapping(&self.upstream_indices, msg);
+        }
     }
 }
 
