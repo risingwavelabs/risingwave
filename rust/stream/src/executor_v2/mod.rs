480de46b2aab90ded3005c7f221e5daf2b63ae97
Bugen Zhao <i@bugenzhao.com>
2022-04-01 20:54:18 +0800

    fix(streaming): use chain v1 for workaround
    Signed-off-by: Bugen Zhao <i@bugenzhao.com>

--- rust/stream/src/executor_v2/mod.rs
+++ rust/stream/src/executor_v2/mod.rs
@@ -22,17 +22,17 @@ pub use super::executor::{
     Barrier, Executor as ExecutorV1, Message, Mutation, PkIndices, PkIndicesRef,
 };
 
-#[allow(dead_code)]
 mod chain;
 mod filter;
+#[allow(dead_code)]
 mod rearranged_chain;
 mod simple;
 #[cfg(test)]
 mod test_utils;
 mod v1_compat;
 
+pub use chain::ChainExecutor;
 pub use filter::FilterExecutor;
-pub use rearranged_chain::RearrangedChainExecutor as ChainExecutor;
 pub(crate) use simple::{SimpleExecutor, SimpleExecutorWrapper};
 pub use v1_compat::StreamExecutorV1;
 
