25c49727fc86314c3dc56b6f72c16ffc5d618c67
mgt <lwzbill@foxmail.com>
2022-04-02 16:38:43 +0800

    add ut for fuse

--- rust/batch/src/executor/fuse.rs
+++ rust/batch/src/executor/fuse.rs
@@ -72,3 +72,62 @@ impl<T: Executor> Executor for FusedExecutor<T> {
         self.inner.identity()
     }
 }
+
+#[cfg(test)]
+mod tests {
+
+    use std::sync::Arc;
+
+    use assert_matches::assert_matches;
+    use risingwave_common::array::column::Column;
+    use risingwave_common::array::{Array, DataChunk, PrimitiveArray};
+    use risingwave_common::catalog::{Field, Schema};
+    use risingwave_common::types::DataType;
+
+    use super::*;
+    use crate::executor::test_utils::MockExecutor;
+
+    #[tokio::test]
+    async fn test_fused_executor() {
+        let col1 = create_column(&[Some(2), Some(2)]).unwrap();
+        let col2 = create_column(&[Some(1), Some(2)]).unwrap();
+        let data_chunk = DataChunk::builder().columns([col1, col2].to_vec()).build();
+        let schema = Schema {
+            fields: vec![
+                Field::unnamed(DataType::Int32),
+                Field::unnamed(DataType::Int32),
+            ],
+        };
+        let mut mock_executor = MockExecutor::with_chunk(data_chunk, schema).fuse();
+        let fields = &mock_executor.schema().fields;
+        assert_eq!(fields[0].data_type, DataType::Int32);
+        assert_eq!(fields[1].data_type, DataType::Int32);
+        mock_executor.open().await.unwrap();
+        let res = mock_executor.next().await.unwrap();
+        assert_matches!(res, Some(_));
+        if let Some(res) = res {
+            let col1 = res.column_at(0);
+            let array = col1.array();
+            let col1 = array.as_int32();
+            assert_eq!(col1.len(), 2);
+            assert_eq!(col1.value_at(0), Some(2));
+            assert_eq!(col1.value_at(1), Some(2));
+            let col2 = res.column_at(1);
+            let array = col2.array();
+            let col2 = array.as_int32();
+            assert_eq!(col2.len(), 2);
+            assert_eq!(col2.value_at(0), Some(1));
+            assert_eq!(col2.value_at(1), Some(2));
+        }
+        let res = mock_executor.next().await.unwrap();
+        assert_matches!(res, None);
+        let res = mock_executor.next().await;
+        assert_matches!(res, Err(_));
+        mock_executor.close().await.unwrap();
+    }
+
+    fn create_column(vec: &[Option<i32>]) -> Result<Column> {
+        let array = PrimitiveArray::from_slice(vec).map(|x| Arc::new(x.into()))?;
+        Ok(Column::new(array))
+    }
+}
