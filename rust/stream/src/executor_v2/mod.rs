c05cc98d4f1eebc2b8d30f1211f55eb5418d7ddb
Bowen Zhou <bowenzhou@singularity-data.com>
2022-04-02 11:59:45 +0800

    fmt
    Signed-off-by: Bowen Zhou <bowenzhou@singularity-data.com>

--- rust/stream/src/executor_v2/mod.rs
+++ rust/stream/src/executor_v2/mod.rs
@@ -24,8 +24,8 @@ pub use super::executor::{
 
 mod chain;
 mod filter;
-pub(crate) mod mview;
 mod merge;
+pub(crate) mod mview;
 #[allow(dead_code)]
 mod rearranged_chain;
 mod simple;
@@ -35,8 +35,8 @@ mod v1_compat;
 
 pub use chain::ChainExecutor;
 pub use filter::FilterExecutor;
-pub use mview::*;
 pub use merge::MergeExecutor;
+pub use mview::*;
 pub(crate) use simple::{SimpleExecutor, SimpleExecutorWrapper};
 pub use v1_compat::StreamExecutorV1;
 
