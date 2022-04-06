6d8963872a35936017bdf651aaef0df6c4bb1fed
cykbls01 <chenyikun@ChendeMacBook-Pro.local>
2022-04-06 15:58:45 -0400

    fix

--- src/frontend/src/planner/set_expr.rs
+++ src/frontend/src/planner/set_expr.rs
@@ -16,7 +16,7 @@ use risingwave_common::error::ErrorCode::InternalError;
 use risingwave_common::error::{Result, RwError};
 use risingwave_common::types::DataType;
 
-use crate::binder::{BoundSetExpr, Relation};
+use crate::binder::{BoundSelect, BoundSetExpr, Relation};
 use crate::expr::{Expr, ExprImpl};
 use crate::optimizer::plan_node::PlanRef;
 use crate::planner::Planner;
@@ -25,33 +25,53 @@ impl Planner {
     pub(super) fn plan_set_expr(&mut self, set_expr: BoundSetExpr) -> Result<PlanRef> {
         match set_expr {
             BoundSetExpr::Select(s) => {
-                let table = {
-                    if let Relation::BaseTable(table) = s.from.as_ref().unwrap() {
-                        &table.table_desc.columns
-                    } else {
-                        return Err(RwError::from(InternalError("not have index".to_string())));
-                    }
-                };
-                for i in 0..s.select_items.len() {
-                    let item = &s.select_items[i];
-                    let name = s.aliases[i].as_ref().unwrap();
-                    if let DataType::Struct { .. } = item.return_type() {
-                        if let ExprImpl::InputRef(expr) = item {
-                            self.name_to_column_desc.insert(
-                                name.clone(),
-                                table
-                                    .get(expr.index())
-                                    .ok_or_else(|| {
-                                        RwError::from(InternalError("not have index".to_string()))
-                                    })?
-                                    .clone(),
-                            );
-                        }
-                    }
-                }
+                self.deal(&s)?;
+                // let table = {
+                //     if let Relation::BaseTable(table) = s.from.as_ref().unwrap() {
+                //         &table.table_desc.columns
+                //     } else {
+                //         return Err(RwError::from(InternalError("not have index".to_string())));
+                //     }
+                // };
+
                 self.plan_select(*s)
             }
             BoundSetExpr::Values(v) => self.plan_values(*v),
         }
+    }
+
+    pub fn deal(&mut self, select: &BoundSelect) -> Result<()> {
+        // TODO: Support other relation type.
+        let table = {
+            if let Relation::BaseTable(table) = select.from.as_ref().unwrap() {
+                &table.table_desc.columns
+            } else {
+                return Ok(());
+            }
+        };
+
+        // For every select items, if it is struct type, store its alias name or column name
+        // and column desc in the map. This map will be used in StreamMaterialize::create.
+        for i in 0..select.select_items.len() {
+            let item = &select.select_items[i];
+            if let DataType::Struct { .. } = item.return_type() {
+                if let ExprImpl::InputRef(expr) = item {
+                    let column = table
+                        .get(expr.index())
+                        .ok_or_else(|| RwError::from(InternalError("not found index".to_string())))?
+                        .clone();
+                    let name = {
+                        match select.aliases.get(i).ok_or_else(|| {
+                            RwError::from(InternalError("index out of range".to_string()))
+                        })? {
+                            Some(name) => name.clone(),
+                            None => column.name.clone(),
+                        }
+                    };
+                    self.name_to_column_desc.insert(name, column);
+                }
+            }
+        }
+        Ok(())
     }
 }
