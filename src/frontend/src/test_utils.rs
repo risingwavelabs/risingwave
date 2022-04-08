3730e5493b3e8ce26fe6555d53877e3069df49a7
Zhanxiang (Patrick) Huang <hzxa21@hotmail.com>
2022-04-08 13:37:46 +0800

    refactor: simplify FrontendMetaClient (#1686)

--- src/frontend/src/test_utils.rs
+++ src/frontend/src/test_utils.rs
@@ -227,7 +227,7 @@ pub struct MockFrontendMetaClient {}
 
 #[async_trait::async_trait]
 impl FrontendMetaClient for MockFrontendMetaClient {
-    async fn pin_snapshot(&self) -> Result<u64> {
+    async fn pin_snapshot(&self, _last_pinned: u64) -> Result<u64> {
         Ok(0)
     }
 
