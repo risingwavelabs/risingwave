2401b86758faf26c7757c734f5e492b746b9a2eb
TennyZhuang <zty0826@gmail.com>
2022-04-12 13:56:58 +0800

    fix
    Signed-off-by: TennyZhuang <zty0826@gmail.com>

--- src/stream/src/executor_v2/batch_query.rs
+++ src/stream/src/executor_v2/batch_query.rs
@@ -11,7 +11,7 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-//
+
 use futures::StreamExt;
 use futures_async_stream::try_stream;
 use risingwave_common::array::{Op, StreamChunk};
