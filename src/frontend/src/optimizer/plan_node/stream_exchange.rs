8952d5cea59d2cc0668444a1957e53baf481f1d8
Alex Chi <iskyzh@gmail.com>
2022-04-13 18:06:04 +0800

    feat(meta): split stateful operators into different fragments (#1817)
    Signed-off-by: Alex Chi <iskyzh@gmail.com>

--- src/frontend/src/optimizer/plan_node/stream_exchange.rs
+++ src/frontend/src/optimizer/plan_node/stream_exchange.rs
@@ -64,7 +64,6 @@ impl_plan_tree_node_for_unary! {StreamExchange}
 impl ToStreamProst for StreamExchange {
     fn to_stream_prost_body(&self) -> Node {
         Node::ExchangeNode(ExchangeNode {
-            fields: self.schema().to_prost(),
             strategy: Some(DispatchStrategy {
                 r#type: match &self.base.dist {
                     Distribution::HashShard(_) => DispatcherType::Hash,
