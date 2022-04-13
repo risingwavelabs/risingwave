2d064b99167d987fc62af4d6adcc79221afa7421
TennyZhuang <zty0826@gmail.com>
2022-04-13 15:40:29 +0800

    feat(stream): Introduce RowsV2Iter
    Signed-off-by: TennyZhuang <zty0826@gmail.com>

--- src/common/src/array/stream_chunk_iter.rs
+++ src/common/src/array/stream_chunk_iter.rs
@@ -12,6 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+use super::column::Column;
 use crate::array::{Op, StreamChunk};
 use crate::types::DatumRef;
 
@@ -65,5 +66,135 @@ impl<'a> RowRef<'a> {
 
     pub fn size(&self) -> usize {
         self.values.len()
+    }
+}
+
+impl StreamChunk {
+    pub fn rows_v2(&self) -> impl Iterator<Item = RowRefV2<'_>> {
+        StreamChunkRefIterV2 {
+            chunk: self,
+            idx: 0,
+        }
+    }
+}
+
+struct StreamChunkRefIterV2<'a> {
+    chunk: &'a StreamChunk,
+    idx: usize,
+}
+
+impl<'a> Iterator for StreamChunkRefIterV2<'a> {
+    type Item = RowRefV2<'a>;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        match self.chunk.visibility() {
+            Some(bitmap) => {
+                loop {
+                    let idx = self.idx;
+                    if idx >= self.chunk.capacity() {
+                        return None;
+                    }
+                    // SAFETY: idx is checked.
+                    let vis = unsafe { bitmap.is_set_unchecked(idx) };
+                    self.idx += 1;
+                    if vis {
+                        return Some(RowRefV2 {
+                            chunk: self.chunk,
+                            idx,
+                        });
+                    }
+                }
+            }
+            None => {
+                if self.idx >= self.chunk.capacity() {
+                    return None;
+                }
+                self.idx += 1;
+                Some(RowRefV2 {
+                    chunk: self.chunk,
+                    idx: self.idx,
+                })
+            }
+        }
+    }
+}
+
+pub struct RowRefV2<'a> {
+    chunk: &'a StreamChunk,
+    idx: usize,
+}
+
+impl RowRefV2<'_> {
+    pub fn value_at(&self, pos: usize) -> DatumRef<'_> {
+        // TODO: It's safe to use value_at_unchecked here.
+        self.chunk.columns()[pos].array_ref().value_at(self.idx)
+    }
+
+    pub fn op(&self) -> Op {
+        // SAFETY: idx is checked while creating `RowRefV2`.
+        unsafe { *self.chunk.ops().get_unchecked(self.idx) }
+    }
+
+    pub fn size(&self) -> usize {
+        self.chunk.columns().len()
+    }
+
+    pub fn iter(&self) -> impl Iterator<Item = DatumRef<'_>> {
+        RowRefV2Iter {
+            columns: self.chunk.columns().iter(),
+            row_idx: self.idx,
+        }
+    }
+}
+
+struct RowRefV2Iter<'a> {
+    columns: std::slice::Iter<'a, Column>,
+    row_idx: usize,
+}
+
+impl<'a> Iterator for RowRefV2Iter<'a> {
+    type Item = DatumRef<'a>;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        // TODO: It's safe to use value_at_unchecked here.
+        self.columns
+            .next()
+            .map(|col| col.array_ref().value_at(self.row_idx))
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::RowRefV2;
+    use crate::array::Row;
+    use crate::test_utils::test_stream_chunk::{TestStreamChunk, WhatEverStreamChunk};
+    use crate::types::ToOwnedDatum;
+
+    #[test]
+    fn test_chunk_rows() {
+        let chunk = WhatEverStreamChunk::stream_chunk();
+        let mut rows = chunk.rows_v2();
+        let row_to_owned = |row: RowRefV2| {
+            Row(row
+                .iter()
+                .map(ToOwnedDatum::to_owned_datum)
+                .collect::<Vec<_>>())
+        };
+        assert_eq!(
+            Some(WhatEverStreamChunk::expected_row_at(0)),
+            rows.next().map(row_to_owned)
+        );
+        assert_eq!(
+            Some(WhatEverStreamChunk::expected_row_at(1)),
+            rows.next().map(row_to_owned)
+        );
+        assert_eq!(
+            Some(WhatEverStreamChunk::expected_row_at(2)),
+            rows.next().map(row_to_owned)
+        );
+        assert_eq!(
+            Some(WhatEverStreamChunk::expected_row_at(3)),
+            rows.next().map(row_to_owned)
+        );
     }
 }
---
+++ src/common/src/test_utils/test_stream_chunk.rs
@@ -0,0 +1,119 @@
+// Copyright 2022 Singularity Data
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+use crate::array::column::Column;
+use crate::array::{F32Array, I32Array, I64Array, Op, Row, StreamChunk};
+use crate::catalog::{Field, Schema};
+use crate::types::{DataType, Datum};
+
+pub trait TestStreamChunk {
+    fn stream_chunk() -> StreamChunk;
+
+    fn cardinality() -> usize;
+
+    fn schema() -> Schema;
+
+    fn pk_indices() -> Vec<usize> {
+        unimplemented!()
+    }
+
+    fn data_types() -> Vec<DataType> {
+        Self::schema().data_types()
+    }
+
+    fn op_at(idx: usize) -> Op;
+
+    fn expected_row_at(idx: usize) -> Row;
+
+    fn expected_value_at(row_idx: usize, col_idx: usize) -> Datum {
+        Self::expected_row_at(row_idx)[col_idx].clone()
+    }
+}
+
+pub struct WhatEverStreamChunk;
+
+impl TestStreamChunk for WhatEverStreamChunk {
+    fn stream_chunk() -> StreamChunk {
+        let ops = vec![
+            Op::Insert,
+            Op::Delete,
+            Op::Insert,
+            Op::UpdateDelete,
+            Op::UpdateInsert,
+        ];
+        let visibility = Some(vec![true, true, false, true, true].try_into().unwrap());
+        let col1 = Column::from(crate::array!(
+            I32Array,
+            [Some(1), Some(2), None, Some(3), Some(4)],
+        ));
+        let col2 = Column::from(crate::array!(
+            F32Array,
+            [Some(4.0), None, Some(3.5), Some(2.2), Some(1.8)],
+        ));
+        let col3 = Column::from(crate::array!(
+            I64Array,
+            [Some(5), Some(6), Some(7), Some(8), Some(9)],
+        ));
+        let cols = vec![col1, col2, col3];
+        StreamChunk::new(ops, cols, visibility)
+    }
+
+    fn cardinality() -> usize {
+        4
+    }
+
+    fn pk_indices() -> Vec<usize> {
+        vec![0]
+    }
+
+    fn schema() -> Schema {
+        let field1 = Field::with_name(DataType::Int32, "pk");
+        let field2 = Field::with_name(DataType::Float32, "v2");
+        let field3 = Field::with_name(DataType::Int64, "v3");
+        let fields = vec![field1, field2, field3];
+        Schema::new(fields)
+    }
+
+    fn op_at(idx: usize) -> Op {
+        match idx {
+            0 => Op::Insert,
+            1 => Op::Delete,
+            2 => Op::UpdateInsert,
+            3 => Op::UpdateDelete,
+            _ => unreachable!(),
+        }
+    }
+
+    fn expected_row_at(idx: usize) -> Row {
+        match idx {
+            0 => Row(vec![
+                Some(1i32.into()),
+                Some(4.0f32.into()),
+                Some(5i64.into()),
+            ]),
+            1 => Row(vec![Some(2i32.into()), None, Some(6i64.into())]),
+            2 => Row(vec![
+                Some(3i32.into()),
+                Some(2.2f32.into()),
+                Some(8i64.into()),
+            ]),
+            3 => Row(vec![
+                Some(4i32.into()),
+                Some(1.8f32.into()),
+                Some(9i64.into()),
+            ]),
+            _ => unreachable!(),
+        }
+    }
+}
