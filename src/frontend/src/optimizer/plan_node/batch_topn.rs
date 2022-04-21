582181d545ef2bdbd492d819054741ea0e32a73c
Eric Fu <eric@singularity-data.com>
2022-04-21 19:31:52 +0800

    add batch plan test

--- src/frontend/src/optimizer/plan_node/batch_topn.rs
+++ src/frontend/src/optimizer/plan_node/batch_topn.rs
@@ -18,6 +18,7 @@ use risingwave_pb::plan::plan_node::NodeBody;
 use risingwave_pb::plan::TopNNode;
 
 use super::{LogicalTopN, PlanBase, PlanRef, PlanTreeNodeUnary, ToBatchProst, ToDistributedBatch};
+use crate::optimizer::property::{Distribution, Order};
 
 /// `BatchTopN` implements [`super::LogicalTopN`] to find the top N elements with a heap
 #[derive(Debug, Clone)]
@@ -65,7 +66,9 @@ impl_plan_tree_node_for_unary! {BatchTopN}
 
 impl ToDistributedBatch for BatchTopN {
     fn to_distributed(&self) -> PlanRef {
-        let new_input = self.input().to_distributed();
+        let new_input = self
+            .input()
+            .to_distributed_with_required(Order::any(), &Distribution::Single);
         self.clone_with_input(new_input).into()
     }
 }
