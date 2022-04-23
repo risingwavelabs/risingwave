7be7871f1f3b2b33ee7004553e7a12f7382ace9f
zwang28 <84491488@qq.com>
2022-04-23 21:49:26 +0800

    fix: correct compaction metrics

--- src/meta/src/hummock/hummock_manager_tests.rs
+++ src/meta/src/hummock/hummock_manager_tests.rs
@@ -930,7 +930,7 @@ async fn test_print_compact_task() -> Result<()> {
         0
     );
 
-    let s = compact_task_to_string(compact_task);
+    let s = compact_task_to_string(&compact_task);
     assert!(s.contains("Compaction task id: 1, target level: 1"));
 
     Ok(())
