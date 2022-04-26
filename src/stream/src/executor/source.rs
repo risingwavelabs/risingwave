1278d90a4a3ea098fe72254b329dc0d129504727
tabVersion <tabvision@bupt.icu>
2022-04-26 12:27:10 +0800

    allow stream_source_splits is empty
    Signed-off-by: tabVersion <tabvision@bupt.icu>

--- src/stream/src/executor/source.rs
+++ src/stream/src/executor/source.rs
@@ -332,6 +332,12 @@ impl<S: StateStore> ExecutorV1 for SourceExecutor<S> {
 
                 // todo: use epoch from msg to restore state from state store
                 assert!(matches!(msg, Message::Barrier(_)));
+
+                // the executor is assigned no split, should check if the barrier contains assign_split mutation later
+                if self.stream_source_splits.is_empty() {
+                    return Ok(msg);
+                }
+
                 let epoch_prev = match &msg {
                     Message::Barrier(barrier) => barrier.epoch.prev,
                     _ => {
