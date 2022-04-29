aa947bcd4ee614b09007502d64da0cbd0f3d5453
d2lark <100247023+D2Lark@users.noreply.github.com>
2022-04-29 19:41:43 +0800

    impl stream scan executor2 (#2232)
    Signed-off-by: d2lark <lichengamoy@gmail.com>
    
    Co-authored-by: August <pin@singularity-data.com>
    Co-authored-by: TennyZhuang <zty0826@gmail.com>

--- src/batch/src/executor/mod.rs
+++ src/batch/src/executor/mod.rs
@@ -30,13 +30,13 @@ pub use crate::executor::create_table::CreateTableExecutor;
 use crate::executor::generate_series::GenerateSeriesI32Executor;
 use crate::executor::join::nested_loop_join::NestedLoopJoinExecutor;
 use crate::executor::join::sort_merge_join::SortMergeJoinExecutor;
-use crate::executor::stream_scan::StreamScanExecutor;
 use crate::executor::trace::TraceExecutor;
 use crate::executor2::executor_wrapper::ExecutorWrapper;
 use crate::executor2::{
     BoxedExecutor2, BoxedExecutor2Builder, DeleteExecutor2, ExchangeExecutor2, FilterExecutor2,
     HashAggExecutor2Builder, HashJoinExecutor2Builder, InsertExecutor2, LimitExecutor2,
-    ProjectExecutor2, SortAggExecutor2, TopNExecutor2, TraceExecutor2, ValuesExecutor2,
+    ProjectExecutor2, SortAggExecutor2, StreamScanExecutor2, TopNExecutor2, TraceExecutor2,
+    ValuesExecutor2,
 };
 use crate::task::{BatchEnvironment, TaskId};
 
@@ -52,7 +52,6 @@ mod merge_sort_exchange;
 pub mod monitor;
 mod order_by;
 mod row_seq_scan;
-mod stream_scan;
 #[cfg(test)]
 pub mod test_utils;
 mod trace;
@@ -195,7 +194,7 @@ impl<'a> ExecutorBuilder<'a> {
             NodeBody::SortAgg => SortAggExecutor2,
             NodeBody::OrderBy => OrderByExecutor,
             NodeBody::CreateSource => CreateSourceExecutor,
-            NodeBody::SourceScan => StreamScanExecutor,
+            NodeBody::SourceScan => StreamScanExecutor2,
             NodeBody::TopN => TopNExecutor2,
             NodeBody::Limit => LimitExecutor2,
             NodeBody::Values => ValuesExecutor2,
@@ -225,7 +224,7 @@ impl<'a> ExecutorBuilder<'a> {
             NodeBody::SortAgg => SortAggExecutor2,
             NodeBody::OrderBy => OrderByExecutor,
             NodeBody::CreateSource => CreateSourceExecutor,
-            NodeBody::SourceScan => StreamScanExecutor,
+            NodeBody::SourceScan => StreamScanExecutor2,
             NodeBody::TopN => TopNExecutor2,
             NodeBody::Limit => LimitExecutor2,
             NodeBody::Values => ValuesExecutor2,
