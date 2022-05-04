d788b2e70f715a907cb34dfc5875730b7b3d8e08
d2lark <100247023+D2Lark@users.noreply.github.com>
2022-05-04 13:02:52 +0800

    impl merge sort exchange executor2 (#2230)
    Signed-off-by: d2lark <lichengamoy@gmail.com>
    
    Co-authored-by: TennyZhuang <zty0826@gmail.com>

--- src/batch/src/executor/mod.rs
+++ src/batch/src/executor/mod.rs
@@ -14,7 +14,6 @@
 
 use drop_stream::*;
 use drop_table::*;
-use merge_sort_exchange::*;
 use order_by::OrderByExecutor;
 use risingwave_common::array::DataChunk;
 use risingwave_common::catalog::Schema;
@@ -32,8 +31,9 @@ use crate::executor2::executor_wrapper::ExecutorWrapper;
 use crate::executor2::{
     BoxedExecutor2, BoxedExecutor2Builder, DeleteExecutor2, ExchangeExecutor2, FilterExecutor2,
     GenerateSeriesI32Executor2, HashAggExecutor2Builder, HashJoinExecutor2Builder, InsertExecutor2,
-    LimitExecutor2, ProjectExecutor2, RowSeqScanExecutor2Builder, SortAggExecutor2,
-    SortMergeJoinExecutor2, StreamScanExecutor2, TopNExecutor2, TraceExecutor2, ValuesExecutor2,
+    LimitExecutor2, MergeSortExchangeExecutor2, ProjectExecutor2, RowSeqScanExecutor2Builder,
+    SortAggExecutor2, SortMergeJoinExecutor2, StreamScanExecutor2, TopNExecutor2, TraceExecutor2,
+    ValuesExecutor2,
 };
 use crate::task::{BatchEnvironment, TaskId};
 
@@ -44,7 +44,6 @@ mod drop_table;
 pub mod executor2_wrapper;
 mod fuse;
 mod join;
-mod merge_sort_exchange;
 mod order_by;
 #[cfg(test)]
 pub mod test_utils;
@@ -197,7 +196,7 @@ impl<'a> ExecutorBuilder<'a> {
             NodeBody::SortMergeJoin => SortMergeJoinExecutor2,
             NodeBody::DropSource => DropStreamExecutor,
             NodeBody::HashAgg => HashAggExecutor2Builder,
-            NodeBody::MergeSortExchange => MergeSortExchangeExecutor,
+            NodeBody::MergeSortExchange => MergeSortExchangeExecutor2,
             NodeBody::GenerateInt32Series => GenerateSeriesI32Executor2,
             NodeBody::HopWindow => NotImplementedBuilder,
         }?;
@@ -227,7 +226,7 @@ impl<'a> ExecutorBuilder<'a> {
             NodeBody::SortMergeJoin => SortMergeJoinExecutor2,
             NodeBody::DropSource => DropStreamExecutor,
             NodeBody::HashAgg => HashAggExecutor2Builder,
-            NodeBody::MergeSortExchange => MergeSortExchangeExecutor,
+            NodeBody::MergeSortExchange => MergeSortExchangeExecutor2,
             NodeBody::GenerateInt32Series => GenerateSeriesI32Executor2,
             NodeBody::HopWindow => NotImplementedBuilder,
         }?;
