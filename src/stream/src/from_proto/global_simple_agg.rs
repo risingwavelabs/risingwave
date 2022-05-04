ClearloveWei/risingwave_e

rename module `executor` to `from_proto`
Signed-off-by: Runji Wang <wangrunji0408@163.com>

src/stream/src/from_proto/global_simple_agg.rs
@@ -12,33 +12,43 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-use risingwave_expr::expr::build_from_prost;
+//! Streaming Aggregators
 
 use super::*;
-use crate::executor_v2::ProjectExecutor;
+use crate::executor_v2::aggregation::AggCall;
+use crate::executor_v2::SimpleAggExecutor;
+use crate::task::build_agg_call_from_prost;
 
-pub struct ProjectExecutorBuilder;
+pub struct SimpleAggExecutorBuilder;
 
-impl ExecutorBuilder for ProjectExecutorBuilder {
+impl ExecutorBuilder for SimpleAggExecutorBuilder {
     fn new_boxed_executor(
         mut params: ExecutorParams,
-        node: &stream_plan::StreamNode,
-        _store: impl StateStore,
+        node: &StreamNode,
+        store: impl StateStore,
         _stream: &mut LocalStreamManagerCore,
     ) -> Result<BoxedExecutor> {
-        let node = try_match_expand!(node.get_node().unwrap(), Node::ProjectNode)?;
-        let project_exprs = node
-            .get_select_list()
+        let node = try_match_expand!(node.get_node().unwrap(), Node::GlobalSimpleAggNode)?;
+        let agg_calls: Vec<AggCall> = node
+            .get_agg_calls()
             .iter()
-            .map(build_from_prost)
-            .collect::<Result<Vec<_>>>()?;
+            .map(build_agg_call_from_prost)
+            .try_collect()?;
+        let keyspace = Keyspace::executor_root(store, params.executor_id);
+        let key_indices = node
+            .get_distribution_keys()
+            .iter()
+            .map(|key| *key as usize)
+            .collect::<Vec<_>>();
 
-        Ok(ProjectExecutor::new(
+        Ok(SimpleAggExecutor::new(
             params.input.remove(0),
+            agg_calls,
+            keyspace,
             params.pk_indices,
-            project_exprs,
             params.executor_id,
-        )
+            key_indices,
+        )?
         .boxed())
     }
 }

src/stream/src/from_proto/lookup_union.rs
@@ -13,17 +13,21 @@
 // limitations under the License.
 
 use super::*;
+use crate::executor_v2::LookupUnionExecutor;
 
-pub struct MergeExecutorBuilder;
+pub struct LookupUnionExecutorBuilder;
 
-impl ExecutorBuilder for MergeExecutorBuilder {
+impl ExecutorBuilder for LookupUnionExecutorBuilder {
     fn new_boxed_executor(
         params: ExecutorParams,
-        node: &stream_plan::StreamNode,
+        node: &StreamNode,
         _store: impl StateStore,
-        stream: &mut LocalStreamManagerCore,
+        _stream: &mut LocalStreamManagerCore,
     ) -> Result<BoxedExecutor> {
-        let node = try_match_expand!(node.get_node().unwrap(), Node::MergeNode)?;
-        stream.create_merge_node(params, node)
+        let lookup_union = try_match_expand!(node.get_node().unwrap(), Node::LookupUnionNode)?;
+        Ok(
+            LookupUnionExecutor::new(params.pk_indices, params.input, lookup_union.order.clone())
+                .boxed(),
+        )
     }
 }

src/stream/src/from_proto/project.rs
@@ -12,22 +12,33 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+use risingwave_expr::expr::build_from_prost;
+
 use super::*;
-use crate::executor_v2::LookupUnionExecutor;
+use crate::executor_v2::ProjectExecutor;
 
-pub struct LookupUnionExecutorBuilder;
+pub struct ProjectExecutorBuilder;
 
-impl ExecutorBuilder for LookupUnionExecutorBuilder {
+impl ExecutorBuilder for ProjectExecutorBuilder {
     fn new_boxed_executor(
-        params: ExecutorParams,
-        node: &stream_plan::StreamNode,
+        mut params: ExecutorParams,
+        node: &StreamNode,
         _store: impl StateStore,
         _stream: &mut LocalStreamManagerCore,
-    ) -> risingwave_common::error::Result<BoxedExecutor> {
-        let lookup_union = try_match_expand!(node.get_node().unwrap(), Node::LookupUnionNode)?;
-        Ok(
-            LookupUnionExecutor::new(params.pk_indices, params.input, lookup_union.order.clone())
-                .boxed(),
+    ) -> Result<BoxedExecutor> {
+        let node = try_match_expand!(node.get_node().unwrap(), Node::ProjectNode)?;
+        let project_exprs = node
+            .get_select_list()
+            .iter()
+            .map(build_from_prost)
+            .collect::<Result<Vec<_>>>()?;
+
+        Ok(ProjectExecutor::new(
+            params.input.remove(0),
+            params.pk_indices,
+            project_exprs,
+            params.executor_id,
         )
+        .boxed())
     }
 }

risingwavelabs/risingwave

src/stream/src/from_proto/global_simple_agg.rs
@@ -12,39 +12,50 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-use itertools::Itertools;
-use risingwave_common::error::Result;
-use risingwave_common::try_match_expand;
-use risingwave_pb::stream_plan;
-use risingwave_pb::stream_plan::stream_node::NodeBody;
-use risingwave_storage::StateStore;
+//! Streaming Aggregators
 
-use crate::executor::ExecutorBuilder;
+use risingwave_common::catalog::TableId;
+
+use super::*;
 use crate::executor_v2::aggregation::AggCall;
-use crate::executor_v2::{BoxedExecutor, Executor, LocalSimpleAggExecutor};
-use crate::task::{build_agg_call_from_prost, ExecutorParams, LocalStreamManagerCore};
+use crate::executor_v2::SimpleAggExecutor;
+use crate::task::build_agg_call_from_prost;
 
-pub struct LocalSimpleAggExecutorBuilder;
+pub struct SimpleAggExecutorBuilder;
 
-impl ExecutorBuilder for LocalSimpleAggExecutorBuilder {
+impl ExecutorBuilder for SimpleAggExecutorBuilder {
     fn new_boxed_executor(
         mut params: ExecutorParams,
-        node: &stream_plan::StreamNode,
-        _store: impl StateStore,
+        node: &StreamNode,
+        store: impl StateStore,
         _stream: &mut LocalStreamManagerCore,
     ) -> Result<BoxedExecutor> {
-        let node = try_match_expand!(node.get_node_body().unwrap(), NodeBody::LocalSimpleAgg)?;
+        let node = try_match_expand!(node.get_node_body().unwrap(), NodeBody::GlobalSimpleAgg)?;
         let agg_calls: Vec<AggCall> = node
             .get_agg_calls()
             .iter()
             .map(build_agg_call_from_prost)
             .try_collect()?;
+        // Build vector of keyspace via table ids.
+        // One keyspace for one agg call.
+        let keyspace = node
+            .get_table_ids()
+            .iter()
+            .map(|table_id| Keyspace::table_root(store.clone(), &TableId::new(*table_id)))
+            .collect();
+        let key_indices = node
+            .get_distribution_keys()
+            .iter()
+            .map(|key| *key as usize)
+            .collect::<Vec<_>>();
 
-        Ok(LocalSimpleAggExecutor::new(
+        Ok(SimpleAggExecutor::new(
             params.input.remove(0),
             agg_calls,
+            keyspace,
             params.pk_indices,
             params.executor_id,
+            key_indices,
         )?
         .boxed())
     }

src/stream/src/from_proto/lookup_union.rs
@@ -13,18 +13,21 @@
 // limitations under the License.
 
 use super::*;
-use crate::executor_v2::UnionExecutor;
+use crate::executor_v2::LookupUnionExecutor;
 
-pub struct UnionExecutorBuilder;
+pub struct LookupUnionExecutorBuilder;
 
-impl ExecutorBuilder for UnionExecutorBuilder {
+impl ExecutorBuilder for LookupUnionExecutorBuilder {
     fn new_boxed_executor(
         params: ExecutorParams,
-        node: &stream_plan::StreamNode,
+        node: &StreamNode,
         _store: impl StateStore,
         _stream: &mut LocalStreamManagerCore,
-    ) -> risingwave_common::error::Result<BoxedExecutor> {
-        try_match_expand!(node.get_node_body().unwrap(), NodeBody::Union)?;
-        Ok(UnionExecutor::new(params.pk_indices, params.input).boxed())
+    ) -> Result<BoxedExecutor> {
+        let lookup_union = try_match_expand!(node.get_node_body().unwrap(), NodeBody::LookupUnion)?;
+        Ok(
+            LookupUnionExecutor::new(params.pk_indices, params.input, lookup_union.order.clone())
+                .boxed(),
+        )
     }
 }

src/stream/src/from_proto/project.rs
@@ -12,22 +12,33 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+use risingwave_expr::expr::build_from_prost;
+
 use super::*;
-use crate::executor_v2::LookupUnionExecutor;
+use crate::executor_v2::ProjectExecutor;
 
-pub struct LookupUnionExecutorBuilder;
+pub struct ProjectExecutorBuilder;
 
-impl ExecutorBuilder for LookupUnionExecutorBuilder {
+impl ExecutorBuilder for ProjectExecutorBuilder {
     fn new_boxed_executor(
-        params: ExecutorParams,
-        node: &stream_plan::StreamNode,
+        mut params: ExecutorParams,
+        node: &StreamNode,
         _store: impl StateStore,
         _stream: &mut LocalStreamManagerCore,
-    ) -> risingwave_common::error::Result<BoxedExecutor> {
-        let lookup_union = try_match_expand!(node.get_node_body().unwrap(), NodeBody::LookupUnion)?;
-        Ok(
-            LookupUnionExecutor::new(params.pk_indices, params.input, lookup_union.order.clone())
-                .boxed(),
+    ) -> Result<BoxedExecutor> {
+        let node = try_match_expand!(node.get_node_body().unwrap(), NodeBody::Project)?;
+        let project_exprs = node
+            .get_select_list()
+            .iter()
+            .map(build_from_prost)
+            .collect::<Result<Vec<_>>>()?;
+
+        Ok(ProjectExecutor::new(
+            params.input.remove(0),
+            params.pk_indices,
+            project_exprs,
+            params.executor_id,
         )
+        .boxed())
     }
 }