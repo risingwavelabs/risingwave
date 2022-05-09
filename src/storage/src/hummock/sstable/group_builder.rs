4f545c83b5bc50304c4fbfc7559d61bff5eb9894
William Wen <william123.wen@gmail.com>
2022-05-09 16:44:55 +0800

    feat(storage): use the highest bit of sstable id to mark the sstable id generated from remote or local

--- src/storage/src/hummock/sstable/group_builder.rs
+++ src/storage/src/hummock/sstable/group_builder.rs
@@ -19,6 +19,7 @@ use bytes::Bytes;
 use itertools::Itertools;
 use risingwave_hummock_sdk::compaction_group::{CompactionGroupId, Prefix};
 use risingwave_hummock_sdk::key::{get_table_id, FullKey};
+use risingwave_hummock_sdk::HummockSSTableId;
 use risingwave_pb::hummock::VNodeBitmap;
 
 use crate::hummock::multi_builder::CapacitySplitTableBuilder;
@@ -101,12 +102,12 @@ pub struct GroupedSstableBuilder<B> {
 
 impl<B, F> GroupedSstableBuilder<B>
 where
-    B: Copy + Fn() -> F,
-    F: Future<Output = HummockResult<(u64, SSTableBuilder)>>,
+    B: Clone + Fn() -> F,
+    F: Future<Output = HummockResult<(HummockSSTableId, SSTableBuilder)>>,
 {
     pub fn new(get_id_and_builder: B, grouping: KeyValueGroupingImpl) -> Self {
         Self {
-            get_id_and_builder,
+            get_id_and_builder: get_id_and_builder.clone(),
             grouping,
             builders: HashMap::from([(
                 DEFAULT_KEY_VALUE_GROUP_ID,
@@ -137,7 +138,7 @@ where
         let entry = self
             .builders
             .entry(group_id)
-            .or_insert_with(|| CapacitySplitTableBuilder::new(self.get_id_and_builder));
+            .or_insert_with(|| CapacitySplitTableBuilder::new(self.get_id_and_builder.clone()));
         entry.add_full_key(full_key, value, allow_split).await
     }
 
