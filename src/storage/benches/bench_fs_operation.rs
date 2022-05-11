d392deb813a6ebc68b29790744a3c4f8988c3ab7
William Wen <william123.wen@gmail.com>
2022-05-11 18:05:56 +0800

    update bench

--- src/storage/benches/bench_fs_operation.rs
+++ src/storage/benches/bench_fs_operation.rs
@@ -12,23 +12,59 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+use std::fmt::{Display, Formatter};
 use std::fs::OpenOptions;
 use std::io::{Read, Seek, SeekFrom, Write};
 use std::path::PathBuf;
+use std::time::SystemTime;
 
 use criterion::{criterion_group, criterion_main, BenchmarkId, Criterion};
 use tokio::io::AsyncReadExt;
 
-const BENCH_SIZE: usize = 100;
+#[derive(Default)]
+struct BenchStats {
+    total_time_read_millis: f64,
+    total_read_count: usize,
+    total_time_write_millis: f64,
+    total_write_count: usize,
+}
+
+impl Display for BenchStats {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        write!(
+            f,
+            "Write {} times, avg {}ms. Read {} times, avg {}ms",
+            self.total_write_count,
+            self.total_time_write_millis / self.total_write_count as f64,
+            self.total_read_count,
+            self.total_time_read_millis / self.total_read_count as f64
+        )?;
+        Ok(())
+    }
+}
+
+impl BenchStats {
+    fn add_read_time(&mut self, time: f64) {
+        self.total_time_read_millis += time;
+        self.total_read_count += 1;
+    }
+
+    fn add_write_time(&mut self, time: f64) {
+        self.total_time_write_millis += time;
+        self.total_write_count += 1;
+    }
+}
+
+const BENCH_SIZE: usize = 10;
 fn gen_test_payload() -> Vec<u8> {
     let mut ret = Vec::new();
-    for i in 0..10000 {
-        ret.extend(format!("{:05}", i).as_bytes());
+    for i in 0..1_000_000 {
+        ret.extend(format!("{:08}", i).as_bytes());
     }
     ret
 }
 
-async fn run_tokio_fs(payload: &[u8]) {
+async fn run_tokio_fs(payload: &[u8], stats: &mut BenchStats) {
     use tempfile::tempdir;
     use tokio::fs::OpenOptions;
     use tokio::io::{AsyncSeekExt, AsyncWriteExt};
@@ -45,15 +81,29 @@ async fn run_tokio_fs(payload: &[u8]) {
             .open(&path)
             .await
             .unwrap();
+        let start_time = SystemTime::now();
         file.write_all(payload).await.unwrap();
+        stats.add_write_time(
+            SystemTime::now()
+                .duration_since(start_time)
+                .unwrap()
+                .as_millis() as f64,
+        );
         file.seek(SeekFrom::Start(0)).await.unwrap();
         let mut buffer = Vec::with_capacity(payload.len());
+        let start_time = SystemTime::now();
         file.read_to_end(&mut buffer).await.unwrap();
+        stats.add_read_time(
+            SystemTime::now()
+                .duration_since(start_time)
+                .unwrap()
+                .as_millis() as f64,
+        );
         assert_eq!(buffer, payload);
     }
 }
 
-fn run_std_fs(payload: &[u8]) {
+fn run_std_fs(payload: &[u8], stats: &mut BenchStats) {
     use tempfile::tempdir;
     let temp_dir = tempdir().unwrap();
 
@@ -66,10 +116,24 @@ fn run_std_fs(payload: &[u8]) {
             .create(true)
             .open(&path)
             .unwrap();
+        let start_time = SystemTime::now();
         file.write_all(payload).unwrap();
+        stats.add_write_time(
+            SystemTime::now()
+                .duration_since(start_time)
+                .unwrap()
+                .as_millis() as f64,
+        );
         file.seek(SeekFrom::Start(0)).unwrap();
         let mut buffer = Vec::with_capacity(payload.len());
+        let start_time = SystemTime::now();
         file.read_to_end(&mut buffer).unwrap();
+        stats.add_read_time(
+            SystemTime::now()
+                .duration_since(start_time)
+                .unwrap()
+                .as_millis() as f64,
+        );
         assert_eq!(buffer, payload);
     }
 }
@@ -77,16 +141,24 @@ fn run_std_fs(payload: &[u8]) {
 fn criterion_benchmark(c: &mut Criterion) {
     let payload = gen_test_payload();
     let runtime = tokio::runtime::Runtime::new().unwrap();
+    let mut tokio_stats = BenchStats::default();
     c.bench_with_input(
         BenchmarkId::new("fs_operation", "tokio"),
         &payload,
-        |b, payload| b.iter(|| runtime.block_on(run_tokio_fs(&payload))),
+        |b, payload| {
+            b.iter(|| runtime.block_on(run_tokio_fs(payload, &mut tokio_stats)));
+        },
     );
+    println!("tokio stat: {}", tokio_stats);
+    let mut std_stats = BenchStats::default();
     c.bench_with_input(
         BenchmarkId::new("fs_operation", "std"),
         &payload,
-        |b, payload| b.iter(|| run_std_fs(&payload)),
+        |b, payload| {
+            b.iter(|| run_std_fs(payload, &mut std_stats));
+        },
     );
+    println!("std stat: {}", std_stats);
 }
 
 criterion_group!(benches, criterion_benchmark);
