cf35a8f6ba88807d1081b25aad8201d62d3d3d1c
xxchan <xxchan22f@gmail.com>
2022-05-12 23:14:59 +0200

    fix

--- src/expr/src/vector_op/agg/general_agg.rs
+++ src/expr/src/vector_op/agg/general_agg.rs
@@ -40,7 +40,12 @@ where
     F: for<'a> RTFn<'a, T, R>,
     R: Array,
 {
-    pub fn new(return_type: DataType, input_col_idx: usize, f: F, init_result: Option<R::OwnedItem>) -> Self {
+    pub fn new(
+        return_type: DataType,
+        input_col_idx: usize,
+        f: F,
+        init_result: Option<R::OwnedItem>,
+    ) -> Self {
         Self {
             return_type,
             input_col_idx,
