7127a611cda1f1079b30158b27b962ebc00bcf06
Yikun Chen <36026213+cykbls01@users.noreply.github.com>
2022-05-12 22:06:37 -0400

    fix: frontend crashed on unsupported psql commands (#2480)
    * fix
    
    * fix
    
    * fix

--- src/utils/pgwire/src/error.rs
+++ src/utils/pgwire/src/error.rs
@@ -19,6 +19,8 @@ use thiserror::Error;
 pub enum PsqlError {
     #[error("Encode error {0}.")]
     CancelError(String),
+    #[error("Read sql error: {0}.")]
+    ReadError(String),
 }
 
 impl PsqlError {
