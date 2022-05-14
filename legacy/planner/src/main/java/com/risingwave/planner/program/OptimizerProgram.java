2ef98f693fb42eeb1bf0df3facfa95549f4b35f2
Eric Fu <eric@singularity-data.com>
2022-05-14 17:01:49 +0800

    remove legacy folder

--- legacy/planner/src/main/java/com/risingwave/planner/program/OptimizerProgram.java
+++
@@ -1,9 +0,0 @@
-package com.risingwave.planner.program;
-
-import com.risingwave.execution.context.ExecutionContext;
-import org.apache.calcite.rel.RelNode;
-
-/** An optimizer program is used to group a collection of op */
-public interface OptimizerProgram {
-  RelNode optimize(RelNode root, ExecutionContext context);
-}
