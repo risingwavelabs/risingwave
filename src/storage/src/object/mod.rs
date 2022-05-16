10cce2ea7ec277a74069eb4c83b03dcbd75d0ef6
William Wen <44139337+wenym1@users.noreply.github.com>
2022-05-16 16:12:13 +0800

    feat(storage): suppport disk object store (#2389)
    * feat(storage): suppport disk object store
    * add benchmark for tokio and std file system
    * refine disk object error
    * add opened file cache for read
    * use &[...] in object store readv

--- src/storage/src/object/mod.rs
+++ src/storage/src/object/mod.rs
@@ -22,8 +22,11 @@ pub use mem::*;
 pub mod s3;
 pub use s3::*;
 
+mod disk;
 pub mod error;
 pub use error::*;
+
+use crate::object::disk::LocalDiskObjectStore;
 
 #[derive(Debug, Copy, Clone)]
 pub struct BlockLocation {
@@ -58,7 +61,7 @@ pub trait ObjectStore: Send + Sync {
     /// <https://d1.awsstatic.com/whitepapers/AmazonS3BestPractices.pdf?stod_obj2>
     async fn read(&self, path: &str, block_loc: Option<BlockLocation>) -> ObjectResult<Bytes>;
 
-    async fn readv(&self, path: &str, block_locs: Vec<BlockLocation>) -> ObjectResult<Vec<Bytes>>;
+    async fn readv(&self, path: &str, block_locs: &[BlockLocation]) -> ObjectResult<Vec<Bytes>>;
 
     /// Obtains the object metadata.
     async fn metadata(&self, path: &str) -> ObjectResult<ObjectMetadata>;
@@ -72,6 +75,7 @@ pub type ObjectStoreRef = Arc<ObjectStoreImpl>;
 pub enum ObjectStoreImpl {
     Mem(InMemObjectStore),
     S3(S3ObjectStore),
+    Disk(LocalDiskObjectStore),
 }
 
 /// Manually dispatch trait methods.
@@ -80,6 +84,7 @@ impl ObjectStoreImpl {
         match self {
             ObjectStoreImpl::Mem(mem) => mem.upload(path, obj).await,
             ObjectStoreImpl::S3(s3) => s3.upload(path, obj).await,
+            ObjectStoreImpl::Disk(disk) => disk.upload(path, obj).await,
         }
     }
 
@@ -87,17 +92,19 @@ impl ObjectStoreImpl {
         match self {
             ObjectStoreImpl::Mem(mem) => mem.read(path, block_loc).await,
             ObjectStoreImpl::S3(s3) => s3.read(path, block_loc).await,
+            ObjectStoreImpl::Disk(disk) => disk.read(path, block_loc).await,
         }
     }
 
     pub async fn readv(
         &self,
         path: &str,
-        block_locs: Vec<BlockLocation>,
+        block_locs: &[BlockLocation],
     ) -> ObjectResult<Vec<Bytes>> {
         match self {
             ObjectStoreImpl::Mem(mem) => mem.readv(path, block_locs).await,
             ObjectStoreImpl::S3(s3) => s3.readv(path, block_locs).await,
+            ObjectStoreImpl::Disk(disk) => disk.readv(path, block_locs).await,
         }
     }
 
@@ -105,6 +112,7 @@ impl ObjectStoreImpl {
         match self {
             ObjectStoreImpl::Mem(mem) => mem.metadata(path).await,
             ObjectStoreImpl::S3(s3) => s3.metadata(path).await,
+            ObjectStoreImpl::Disk(disk) => disk.metadata(path).await,
         }
     }
 
@@ -112,19 +120,33 @@ impl ObjectStoreImpl {
         match self {
             ObjectStoreImpl::Mem(mem) => mem.delete(path).await,
             ObjectStoreImpl::S3(s3) => s3.delete(path).await,
+            ObjectStoreImpl::Disk(disk) => disk.delete(path).await,
         }
     }
 }
 
-pub async fn parse_object_store(hummock: &str) -> ObjectStoreImpl {
-    match hummock {
-        s3 if s3.starts_with("hummock+s3://") => ObjectStoreImpl::S3(
-            S3ObjectStore::new(s3.strip_prefix("hummock+s3://").unwrap().to_string()).await,
-        ),
-        minio if minio.starts_with("hummock+minio://") => ObjectStoreImpl::S3(
-            S3ObjectStore::with_minio(minio.strip_prefix("hummock+").unwrap()).await,
-        ),
-        memory if memory.starts_with("hummock+memory") => {
+pub async fn parse_object_store(url: &str, is_remote: bool) -> ObjectStoreImpl {
+    match url {
+        s3 if s3.starts_with("s3://") => {
+            assert!(
+                is_remote,
+                "S3 object store is only supported for remote stores"
+            );
+            ObjectStoreImpl::S3(
+                S3ObjectStore::new(s3.strip_prefix("s3://").unwrap().to_string()).await,
+            )
+        }
+        minio if minio.starts_with("minio://") => {
+            assert!(
+                is_remote,
+                "S3 object store is only supported for remote stores"
+            );
+            ObjectStoreImpl::S3(S3ObjectStore::with_minio(minio).await)
+        }
+        disk if disk.starts_with("disk://") => ObjectStoreImpl::Disk(LocalDiskObjectStore::new(
+            disk.strip_prefix("disk://").unwrap(),
+        )),
+        memory if memory.starts_with("memory") => {
             tracing::warn!("You're using Hummock in-memory object store. This should never be used in benchmarks and production environment.");
             ObjectStoreImpl::Mem(InMemObjectStore::new())
         }
