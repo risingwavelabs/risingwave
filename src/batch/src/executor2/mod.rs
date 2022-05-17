6f76c26cc4be97b6ab28e9676fad5877acaa7a47
d2lark <100247023+D2Lark@users.noreply.github.com>
2022-05-17 16:31:06 +0800

    refactor(batch): use wrapper executor to impl time and i32 generate series (#2524)

--- src/batch/src/executor2/mod.rs
+++ src/batch/src/executor2/mod.rs
@@ -14,6 +14,7 @@
 
 mod delete;
 mod filter;
+mod generate_i32_series;
 mod generate_series;
 mod generate_time_series;
 mod generic_exchange;
@@ -35,6 +36,7 @@ mod values;
 pub use delete::*;
 pub use filter::*;
 use futures::stream::BoxStream;
+pub use generate_i32_series::*;
 pub use generate_series::*;
 pub use generate_time_series::*;
 pub use generic_exchange::*;
