b25707b1661ee3dfb4252b861b65ebafde49db77
jonch <9093549+jon-chuang@users.noreply.github.com>
2022-05-19 09:27:06 +0800

    move connected components labeller to utils

--- src/frontend/src/optimizer/plan_node/logical_multi_join.rs
+++ src/frontend/src/optimizer/plan_node/logical_multi_join.rs
@@ -12,7 +12,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-use std::collections::{HashMap, HashSet};
 use std::fmt;
 
 use risingwave_common::error::{ErrorCode, Result, RwError};
@@ -23,7 +22,7 @@ use super::{
     ToBatch, ToStream,
 };
 use crate::optimizer::plan_node::PlanTreeNode;
-use crate::utils::{ColIndexMapping, Condition};
+use crate::utils::{ColIndexMapping, Condition, ConnectedComponentLabeller};
 
 /// `LogicalMultiJoin` combines two or more relations according to some condition.
 ///
@@ -113,111 +112,6 @@ impl PlanTreeNode for LogicalMultiJoin {
              a temporary lifetime. It only facilitates join reordering during logical planning."
         )
     }
-}
-
-#[derive(Debug)]
-struct ConnectedComponentLabeller {
-    vertex_to_label: HashMap<usize, usize>,
-    labels_to_vertices: HashMap<usize, HashSet<usize>>,
-    labels_to_edges: HashMap<usize, HashSet<(usize, usize)>>,
-}
-
-impl ConnectedComponentLabeller {
-    fn new(vertices: usize) -> Self {
-        let mut vertex_to_label = HashMap::with_capacity(vertices);
-        let mut labels_to_vertices = HashMap::with_capacity(vertices);
-        let labels_to_edges = HashMap::new();
-        for i in 0..vertices {
-            vertex_to_label.insert(i, i);
-            labels_to_vertices.insert(i, vec![i].into_iter().collect());
-        }
-        Self {
-            vertex_to_label,
-            labels_to_vertices,
-            labels_to_edges,
-        }
-    }
-
-    fn add_edge(&mut self, v1: usize, v2: usize) {
-        let v1_label = *self.vertex_to_label.get(&v1).unwrap();
-        let v2_label = *self.vertex_to_label.get(&v2).unwrap();
-
-        let (new_label, old_label) = if v1_label < v2_label {
-            (v1_label, v2_label)
-        } else {
-            // v1_label > v2_label
-            (v2_label, v1_label)
-        };
-
-        {
-            let edges = self
-                .labels_to_edges
-                .entry(new_label)
-                .or_insert_with(HashSet::new);
-
-            let new_edge = if v1 < v2 { (v1, v2) } else { (v2, v1) };
-            edges.insert(new_edge);
-        }
-
-        if v1_label == v2_label {
-            return;
-        }
-
-        // Reassign to the smaller label
-        let old_vertices = self.labels_to_vertices.remove(&old_label).unwrap();
-        self.labels_to_vertices
-            .get_mut(&new_label)
-            .unwrap()
-            .extend(old_vertices.iter());
-        for v in old_vertices {
-            self.vertex_to_label.insert(v, new_label);
-        }
-        if let Some(old_edges) = self.labels_to_edges.remove(&old_label) {
-            let edges = self
-                .labels_to_edges
-                .entry(new_label)
-                .or_insert_with(HashSet::new);
-            edges.extend(old_edges);
-        }
-    }
-
-    fn into_edge_sets(self) -> Vec<HashSet<(usize, usize)>> {
-        self.labels_to_edges.into_values().collect()
-    }
-}
-
-#[test]
-fn test_connected_component_labeller() {
-    // Graph:
-    // 0-1-2  3-4-5  6
-    // => 0-1-2-3-4-5  6
-    // => 0-1-2-3-4-5-6
-    let mut labeller = ConnectedComponentLabeller::new(7);
-    labeller.add_edge(0, 1);
-    labeller.add_edge(1, 2);
-
-    labeller.add_edge(3, 4);
-    labeller.add_edge(4, 5);
-
-    assert_eq!(labeller.labels_to_vertices.len(), 3);
-
-    labeller.add_edge(2, 3);
-
-    assert_eq!(labeller.labels_to_vertices.len(), 2);
-
-    labeller.add_edge(5, 6);
-
-    assert_eq!(labeller.labels_to_vertices.len(), 1);
-    assert_eq!(
-        *labeller.labels_to_vertices.iter().next().unwrap().1,
-        (0..7).collect::<HashSet<_>>()
-    );
-    assert_eq!(
-        *labeller.labels_to_edges.iter().next().unwrap().1,
-        vec![(0, 1), (1, 2), (2, 3), (3, 4), (4, 5), (5, 6)]
-            .into_iter()
-            .collect::<HashSet<_>>()
-    );
 }
 
 impl LogicalMultiJoin {
