a95c7ccbd0e07a8af07b6a89451c0ecbc8a57d70
congyi <15605187270@163.com>
2022-05-20 20:16:36 +0800

    remove redundant cloneclone

--- src/storage/src/table/state_table.rs
+++ src/storage/src/table/state_table.rs
@@ -145,13 +145,14 @@ impl<'a, S: StateStore> StateTableRowIter<'a, S> {
         order_types_vec: &[OrderType],
         epoch: u64,
     ) -> StorageResult<StateTableRowIter<'a, S>> {
-        let cell_based_streaming_iter =
+        let mut cell_based_streaming_iter =
             CellBasedTableStreamingIter::new(keyspace, table_descs, epoch).await?;
         let pk_serializer = OrderedRowSerializer::new(order_types_vec.to_vec());
+        let cell_based_item = cell_based_streaming_iter.next().await.map_err(err)?;
         let state_table_iter = Self {
             mem_table_iter,
             cell_based_streaming_iter,
-            cell_based_item: None,
+            cell_based_item,
             pk_serializer,
         };
 
@@ -160,17 +161,16 @@ impl<'a, S: StateStore> StateTableRowIter<'a, S> {
 
     pub async fn next(&mut self) -> StorageResult<Option<Row>> {
         let mut mem_table_iter_next_flag = false;
+        let mut cell_based_iter_next_flag = false;
         let mut res = None;
-        if self.cell_based_item.is_none() {
-            self.cell_based_item = self.cell_based_streaming_iter.next().await.map_err(err)?;
-        }
-        match (self.cell_based_item.as_ref(), self.mem_table_iter.peek()) {
+        let cell_based_item = std::mem::take(&mut self.cell_based_item);
+        match (cell_based_item, self.mem_table_iter.peek()) {
             (None, None) => {
                 res = None;
             }
             (Some((_, row)), None) => {
-                res = Some(row.clone());
-                self.cell_based_item = self.cell_based_streaming_iter.next().await.map_err(err)?
+                res = Some(row);
+                cell_based_iter_next_flag = true;
             }
             (None, Some((_, row_op))) => {
                 mem_table_iter_next_flag = true;
@@ -186,16 +186,15 @@ impl<'a, S: StateStore> StateTableRowIter<'a, S> {
                 match cell_based_pk.cmp(&mem_table_pk_bytes) {
                     Ordering::Less => {
                         // cell_based_table_item will be return
-                        res = Some(cell_based_row.clone());
-                        self.cell_based_item =
-                            self.cell_based_streaming_iter.next().await.map_err(err)?
+                        res = Some(cell_based_row);
+                        cell_based_iter_next_flag = true;
                     }
                     Ordering::Equal => {
                         // mem_table_item will be return, while both cell_based_streaming_iter and
                         // mem_table_iter need to execute next() once.
+
                         mem_table_iter_next_flag = true;
-                        self.cell_based_item =
-                            self.cell_based_streaming_iter.next().await.map_err(err)?;
+                        cell_based_iter_next_flag = true;
                         match mem_table_row_op {
                             RowOp::Insert(row) => {
                                 res = Some(row.clone());
@@ -207,20 +206,25 @@ impl<'a, S: StateStore> StateTableRowIter<'a, S> {
                     Ordering::Greater => {
                         // mem_table_item will be return
                         mem_table_iter_next_flag = true;
+
                         match mem_table_row_op {
                             RowOp::Insert(row) => res = Some(row.clone()),
                             RowOp::Delete(_) => {}
                             RowOp::Update((old_row, new_row)) => {
-                                debug_assert!(old_row == cell_based_row);
+                                debug_assert!(old_row == &cell_based_row);
                                 res = Some(new_row.clone());
                             }
                         }
+                        self.cell_based_item = Some((cell_based_pk, cell_based_row));
                     }
                 }
             }
         }
         if mem_table_iter_next_flag {
             self.mem_table_iter.next();
+        }
+        if cell_based_iter_next_flag {
+            self.cell_based_item = self.cell_based_streaming_iter.next().await.map_err(err)?
         }
         Ok(res)
     }
