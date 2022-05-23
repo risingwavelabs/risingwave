cdb966f2a67a71da3341d33857fac35f22726086
Renjie Liu <liurenjie2008@gmail.com>
2022-05-23 13:54:24 +0800

    feat(batch): Add correct single distribution in `to_local`. (#2734)
    * Add correct single distribution in `to_local`.

--- src/frontend/src/optimizer/plan_node/batch_insert.rs
+++ src/frontend/src/optimizer/plan_node/batch_insert.rs
@@ -84,7 +84,6 @@ impl ToBatchProst for BatchInsert {
 
 impl ToLocalBatch for BatchInsert {
     fn to_local(&self) -> Result<PlanRef> {
-        let new_input = self.input().to_local()?;
-        Ok(self.clone_with_input(new_input).into())
+        unreachable!()
     }
 }
