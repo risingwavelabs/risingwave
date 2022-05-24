01577f68f63ef9e4c1715909f114363476e4fde0
Alex Chi <iskyzh@gmail.com>
2022-05-24 19:57:44 +0800

    update
    Signed-off-by: Alex Chi <iskyzh@gmail.com>

--- docker/aws/aws-build.sh
+++ docker/aws/aws-build.sh
@@ -7,10 +7,8 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 cd "$DIR/../.."
 cargo build -p risingwave_cmd_all --release
-cp target/release/risingwave "$DIR/risingwave"
+objcopy --compress-debug-sections=zlib-gnu target/release/risingwave "$DIR/risingwave"
 
 cd "$DIR"
-objcopy --compress-debug-sections=zlib-gnu risingwave
-
 docker build -t ${RW_REGISTRY}:latest .
 docker push ${RW_REGISTRY}:latest
