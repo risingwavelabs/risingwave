e653d73d7104dd53032f0200d423231f30f58a7d
Alex Chi <iskyzh@gmail.com>
2022-05-24 22:39:14 +0800

    fix shellcheck
    Signed-off-by: Alex Chi <iskyzh@gmail.com>

--- docker/aws/aws-build.sh
+++ docker/aws/aws-build.sh
@@ -10,5 +10,5 @@ cargo build -p risingwave_cmd_all --release
 objcopy --compress-debug-sections=zlib-gnu target/release/risingwave "$DIR/risingwave"
 
 cd "$DIR"
-docker build -t ${RW_REGISTRY}:latest .
-docker push ${RW_REGISTRY}:latest
+docker build -t "${RW_REGISTRY}:latest" .
+docker push "${RW_REGISTRY}:latest"
