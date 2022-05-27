efced767bb04de2964b4e248168bc0f6b1f4a667
Steven Chua <stevengkc714@protonmail.com>
2022-05-27 23:42:17 +0800

    feat(expr): Add eval_row to Expression trait (#2829)
    * Add eval_row_ref to Expression trait
    
    * Add tests
    
    * Add remaining tests
    
    * Fix formatting
    
    * Make suggested changes

--- src/expr/src/expr/expr_in.rs
+++ src/expr/src/expr/expr_in.rs
@@ -17,8 +17,8 @@ use std::fmt::Debug;
 use std::sync::Arc;
 
 use itertools::Itertools;
-use risingwave_common::array::{ArrayBuilder, ArrayRef, BoolArrayBuilder, DataChunk};
-use risingwave_common::types::{DataType, Datum, ToOwnedDatum};
+use risingwave_common::array::{ArrayBuilder, ArrayRef, BoolArrayBuilder, DataChunk, Row};
+use risingwave_common::types::{DataType, Datum, Scalar, ToOwnedDatum};
 
 use crate::expr::{BoxedExpression, Expression};
 
@@ -79,19 +79,25 @@ impl Expression for InExpression {
         };
         Ok(Arc::new(output_array.finish()?.into()))
     }
+
+    fn eval_row(&self, input: &Row) -> risingwave_common::error::Result<Datum> {
+        let data = self.left.eval_row(input)?;
+        let ret = self.exists(&data);
+        Ok(Some(ret.to_scalar_value()))
+    }
 }
 
 #[cfg(test)]
 mod tests {
-    use risingwave_common::array::DataChunk;
+    use risingwave_common::array::{DataChunk, Row};
     use risingwave_common::test_prelude::DataChunkTestExt;
-    use risingwave_common::types::{DataType, ScalarImpl};
+    use risingwave_common::types::{DataType, Scalar, ScalarImpl};
 
     use crate::expr::expr_in::InExpression;
     use crate::expr::{Expression, InputRefExpression};
 
     #[test]
-    fn test_search_expr() {
+    fn test_eval_search_expr() {
         let input_ref = Box::new(InputRefExpression::new(DataType::Varchar, 0));
         let data = vec![
             Some(ScalarImpl::Utf8("abc".to_string())),
@@ -110,5 +116,24 @@ mod tests {
         assert_eq!(res.datum_at(1), Some(ScalarImpl::Bool(false)));
         assert_eq!(res.datum_at(2), Some(ScalarImpl::Bool(true)));
         assert_eq!(res.datum_at(3), Some(ScalarImpl::Bool(true)));
+    }
+
+    #[test]
+    fn test_eval_row_search_expr() {
+        let input_ref = Box::new(InputRefExpression::new(DataType::Varchar, 0));
+        let data = vec![
+            Some(ScalarImpl::Utf8("abc".to_string())),
+            Some(ScalarImpl::Utf8("def".to_string())),
+        ];
+        let search_expr = InExpression::new(input_ref, data.into_iter(), DataType::Boolean);
+
+        let row_inputs = vec!["abc", "a", "def"];
+        let expected = vec![true, false, true];
+
+        for (i, row_input) in row_inputs.iter().enumerate() {
+            let row = Row::new(vec![Some(row_input.to_string().to_scalar_value())]);
+            let result = search_expr.eval_row(&row).unwrap().unwrap().into_bool();
+            assert_eq!(result, expected[i]);
+        }
     }
 }
