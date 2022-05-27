4889f8884de4e02dd5d230488f48aeb5d8f88359
Wallace <bupt2013211450@gmail.com>
2022-05-27 21:26:56 +0800

    feat(common): replace select_all (#2865)
    * implement a fair select
    
    Signed-off-by: Little-Wallace <bupt2013211450@gmail.com>

--- src/stream/src/executor/union.rs
+++ src/stream/src/executor/union.rs
@@ -14,10 +14,10 @@
 
 use std::sync::Arc;
 
-use futures::stream::select_all;
 use futures::StreamExt;
 use futures_async_stream::try_stream;
 use risingwave_common::catalog::Schema;
+use risingwave_common::util::select_all;
 
 use super::*;
 use crate::executor::{BoxedMessageStream, ExecutorInfo};
