3db27fd735650ca02382ea1f6d3acd9a77ec7eb2
marvenlee2486 <leezongyu6878@gmail.com>
2022-05-30 01:57:55 +0800

    Edit minor changes

--- src/expr/src/expr/mod.rs
+++ src/expr/src/expr/mod.rs
@@ -83,7 +83,8 @@ pub fn build_from_prost(prost: &ExprNode) -> Result<BoxedExpression> {
         | IsNotNull | Neg | Ascii | Abs | Ceil | Floor | Round | BitwiseNot => build_unary_expr_prost(prost),
         Equal | NotEqual | LessThan | LessThanOrEqual | GreaterThan | GreaterThanOrEqual | Add
         | Subtract | Multiply | Divide | Modulus | Extract | RoundDigit | TumbleStart
-        | Position | PgBitwiseShiftLeft | PgBitwiseShiftRight | BitwiseAnd | BitwiseOr | BitwiseXor => build_binary_expr_prost(prost),
+        | Position | PgBitwiseShiftLeft | PgBitwiseShiftRight | BitwiseAnd | BitwiseOr
+        | BitwiseXor => build_binary_expr_prost(prost),
         And | Or | IsDistinctFrom => build_nullable_binary_expr_prost(prost),
         ToChar => build_to_char_expr(prost),
         Coalesce => CoalesceExpression::try_from(prost).map(Expression::boxed),
