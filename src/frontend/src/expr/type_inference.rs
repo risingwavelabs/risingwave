b773ad77328394c1e81d13dbb56046d2a59c3506
marvenlee2486 <leezongyu6878@gmail.com>
2022-06-02 02:15:14 +0800

    Resolve error

--- src/frontend/src/expr/type_inference.rs
+++ src/frontend/src/expr/type_inference.rs
@@ -608,15 +608,15 @@ mod tests {
             test_simple_infer_type(expr, vec![t1, t2], tr);
         }
 
-        for (expr, (t1, t2), tr) in iproduct!(
+        for (expr, (t1, t2)) in iproduct!(
             vec![ExprType::BitwiseShiftLeft, ExprType::BitwiseShiftRight,],
             vec![
-                (Int16, Int16, Int16),
-                (Int32, Int16, Int32),
-                (Int64, Int16, Int64),
-                (Int16, Int32, Int16),
-                (Int64, Int32, Int64),
-                (Int32, Int32, Int32),
+                (Int16, Int16),
+                (Int32, Int16),
+                (Int64, Int16),
+                (Int16, Int32),
+                (Int64, Int32),
+                (Int32, Int32),
             ]
         ) {
             test_simple_infer_type(expr, vec![t1, t2], tr);
