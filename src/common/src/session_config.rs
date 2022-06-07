ac5f2cc1939cfab4d6c34f3016e44d31f10cb75a
LIU Zhi <lmatz823@gmail.com>
2022-06-07 23:36:03 +0800

    add license header

--- src/common/src/session_config.rs
+++ src/common/src/session_config.rs
@@ -1,3 +1,17 @@
+// Copyright 2022 Singularity Data
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 /// If `RW_IMPLICIT_FLUSH` is on, then every INSERT/UPDATE/DELETE statement will block
 /// until the entire dataflow is refreshed. In other words, every related table & MV will
 /// be able to see the write.
