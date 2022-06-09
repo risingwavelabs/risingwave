7d7c3d0d68cc4ee59e74ac9961c84b977b6673d9
Alex Chi <iskyzh@gmail.com>
2022-06-09 11:53:43 +0800

    fix build script
    Signed-off-by: Alex Chi <iskyzh@gmail.com>

--- ci/build-ci-image.sh
+++ ci/build-ci-image.sh
@@ -8,4 +8,8 @@ cd "$DIR"/..
 
 cat ../rust-toolchain
 export DEFAULT_RUST_TOOLCHAIN=$(cat ../rust-toolchain)
+export BUILD_ENV_VERSION=v20220609
+export BUILD_TAG="public.ecr.aws/x5u3w5h6/rw-build-env:${BUILD_ENV_VERSION}"
 
+docker build -t ${BUILD_TAG} .
+docker push ${BUILD_TAG}
