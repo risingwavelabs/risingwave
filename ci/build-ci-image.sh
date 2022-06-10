02108c46e720cde349bf6127590f1a1983f4091d
Alex Chi <iskyzh@gmail.com>
2022-06-10 12:43:19 +0800

    update login
    Signed-off-by: Alex Chi <iskyzh@gmail.com>

--- ci/build-ci-image.sh
+++ ci/build-ci-image.sh
@@ -13,4 +13,5 @@ export BUILD_ENV_VERSION=v20220610
 export BUILD_TAG="public.ecr.aws/x5u3w5h6/rw-build-env:${BUILD_ENV_VERSION}"
 
 docker build -t ${BUILD_TAG} --build-arg "RUST_TOOLCHAIN=${RUST_TOOLCHAIN}" .
+aws ecr-public get-login-password --region us-east-1 | docker login --username AWS --password-stdin public.ecr.aws/x5u3w5h6
 docker push ${BUILD_TAG}
