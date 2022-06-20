2aa7e8ef39dc707a5945e45e93d98e4720b55e31
Xinpeng Wei <windowsxp@sjtu.edu.cn>
2022-06-20 22:11:51 +0800

    feat(frontend): add InternalStateTable Catalog (#3139)
    * use TableMessage for internal table
    
    * fix risedev check
    
    * update planner test
    
    * fix unit test
    
    * fix misc check
    
    * fix ci
    
    * fix issues in PR comments
    
    * fix clippy
    
    * fix ci
    
    * update planner test

--- src/frontend/src/optimizer/plan_node/stream_hash_agg.rs
+++ src/frontend/src/optimizer/plan_node/stream_hash_agg.rs
@@ -15,6 +15,7 @@
 use std::fmt;
 
 use itertools::Itertools;
+use risingwave_common::catalog::{DatabaseId, SchemaId};
 use risingwave_pb::stream_plan::stream_node::NodeBody as ProstStreamNode;
 
 use super::logical_agg::PlanAggCall;
@@ -91,7 +92,7 @@ impl_plan_tree_node_for_unary! { StreamHashAgg }
 impl ToStreamProst for StreamHashAgg {
     fn to_stream_prost_body(&self) -> ProstStreamNode {
         use risingwave_pb::stream_plan::*;
-
+        let (internal_tables, column_mapping) = self.logical.infer_internal_table_catalog();
         ProstStreamNode::HashAgg(HashAggNode {
             distribution_keys: self
                 .distribution_keys()
@@ -103,7 +104,19 @@ impl ToStreamProst for StreamHashAgg {
                 .iter()
                 .map(PlanAggCall::to_protobuf)
                 .collect_vec(),
-            table_ids: vec![],
+            internal_tables: internal_tables
+                .into_iter()
+                .map(|table_catalog| {
+                    table_catalog.to_prost(
+                        SchemaId::placeholder() as u32,
+                        DatabaseId::placeholder() as u32,
+                    )
+                })
+                .collect_vec(),
+            column_mapping: column_mapping
+                .into_iter()
+                .map(|(k, v)| (k as u32, v))
+                .collect(),
             is_append_only: self.input().append_only(),
         })
     }
