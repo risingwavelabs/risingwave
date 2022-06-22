7444c9c20c95a463da3d2d4c6ceec26f1d127809
marvenlee2486 <leezongyu6878@gmail.com>
2022-06-22 14:49:03 +0800

    Edit update.sh

--- grafana/update.sh
+++ grafana/update.sh
@@ -6,9 +6,12 @@ set -euo pipefail
 
 echo "$(tput setaf 4)Upload dashboard to localhost:3001$(tput sgr0)"
 
-payload="{\"dashboard\": $(jq . risingwave-dashboard.json), \"overwrite\": true}"
+payload="{\"dashboard\": $(jq . risingwave-dashboard.json), \"overwrite\": true}" 
+echo $payload > payload.txt
 
 curl -X POST \
   -H 'Content-Type: application/json' \
-  -d "${payload}" \
+  -d @payload.txt \
   "http://admin:admin@localhost:3001/api/dashboards/db"
+
+rm payload.txt
