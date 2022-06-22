8cfe6068457397cee6911cb2c14c3fde59c9b9b2
marvenlee2486 <leezongyu6878@gmail.com>
2022-06-22 14:54:49 +0800

    Edit format error

--- grafana/update.sh
+++ grafana/update.sh
@@ -7,7 +7,7 @@ set -euo pipefail
 echo "$(tput setaf 4)Upload dashboard to localhost:3001$(tput sgr0)"
 
 payload="{\"dashboard\": $(jq . risingwave-dashboard.json), \"overwrite\": true}" 
-echo $payload > payload.txt
+echo "$payload" > payload.txt
 
 curl -X POST \
   -H 'Content-Type: application/json' \
