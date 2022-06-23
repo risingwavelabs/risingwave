550ece006c85f775cd0a5cf2d22827c11ee60688
Alex Chi <iskyzh@gmail.com>
2022-06-23 13:28:03 +0800

    fix(ci): avoid tag mutability
    Signed-off-by: Alex Chi <iskyzh@gmail.com>

--- ci/build-ci-image.sh
+++ ci/build-ci-image.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-set -e
+set -euo pipefail
 
 export DOCKER_BUILDKIT=1
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
@@ -12,11 +12,22 @@ export RUST_TOOLCHAIN=$(cat ../rust-toolchain)
 export BUILD_ENV_VERSION=v20220621
 export BUILD_TAG="public.ecr.aws/x5u3w5h6/rw-build-env:${BUILD_ENV_VERSION}"
 
-echo "--- Docker build"
-docker build -t ${BUILD_TAG} --build-arg "RUST_TOOLCHAIN=${RUST_TOOLCHAIN}" .
+echo "--- Arch"
+arch
 
 echo "--- Docker login"
 aws ecr-public get-login-password --region us-east-1 | docker login --username AWS --password-stdin public.ecr.aws/x5u3w5h6
+
+echo "--- Check image existence"
+set +e
+if docker manifest inspect ${BUILD_TAG}; then
+    echo "${BUILD_TAG} already exists - please change build env version"
+    exit 1
+fi
+set -e
+
+echo "--- Docker build"
+docker build -t ${BUILD_TAG} --build-arg "RUST_TOOLCHAIN=${RUST_TOOLCHAIN}" .
 
 echo "--- Docker push"
 docker push ${BUILD_TAG}
