40368257619650e0d28e8a34cdd131999c6ec28a
Alex Chi <iskyzh@gmail.com>
2022-06-23 19:18:55 +0800

    chore(ci): better nextest config
    Signed-off-by: Alex Chi <iskyzh@gmail.com>

--- ci/scripts/unit-test.sh
+++ ci/scripts/unit-test.sh
@@ -7,7 +7,7 @@ source ci/scripts/common.env.sh
 
 echo "--- Run unit tests with coverage"
 # use tee to disable progress bar
-cargo llvm-cov nextest --lcov --output-path lcov.info --features failpoints -- --no-fail-fast 2> >(tee)
+cargo llvm-cov nextest --lcov --output-path lcov.info --features failpoints -- -P ci 2> >(tee)
 
 echo "--- Codecov upload coverage reports"
 curl -Os https://uploader.codecov.io/latest/linux/codecov && chmod +x codecov
