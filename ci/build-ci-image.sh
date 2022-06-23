80d98954c399414e84e63079a39734f292b3dfa5
Alex Chi <iskyzh@gmail.com>
2022-06-23 13:49:13 +0800

    fix
    Signed-off-by: Alex Chi <iskyzh@gmail.com>

--- ci/build-ci-image.sh
+++ ci/build-ci-image.sh
@@ -9,7 +9,7 @@ cd "$DIR"
 cat ../rust-toolchain
 # shellcheck disable=SC2155
 export RUST_TOOLCHAIN=$(cat ../rust-toolchain)
-export BUILD_ENV_VERSION=v20220622
+export BUILD_ENV_VERSION=v20220623
 export BUILD_TAG="public.ecr.aws/x5u3w5h6/rw-build-env:${BUILD_ENV_VERSION}"
 
 echo "--- Arch"
@@ -29,7 +29,7 @@ aws ecr-public get-login-password --region us-east-1 | docker login --username A
 echo "--- Check image existence"
 set +e
 # remove all local images to ensure we fetch remote images
-docker rm ${BUILD_TAG}
+docker image rm ${BUILD_TAG}
 # check manifest
 if docker manifest inspect "${BUILD_TAG}"; then
     echo "+++ Image already exists"
