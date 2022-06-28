95934981b01ac4e0b8f340950849757a0149e343
jonch <9093549+jon-chuang@users.noreply.github.com>
2022-06-28 12:15:52 +0800

    license

--- src/frontend/src/optimizer/plan_node/stream_dynamic_filter.rs
+++ src/frontend/src/optimizer/plan_node/stream_dynamic_filter.rs
@@ -1,3 +1,16 @@
+// Copyright 2022 Singularity Data
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 use std::fmt;
 
 use risingwave_pb::stream_plan::stream_node::NodeBody;
@@ -20,7 +33,7 @@ impl StreamDynamicFilter {
     pub fn new(predicate: Condition, left: PlanRef, right: PlanRef) -> Self {
         // TODO: derive from input
         let base = PlanBase::new_stream(
-            left.ctx(),
+            left.ctx().clone(),
             left.schema().clone(),
             left.pk_indices().to_vec(),
             left.distribution().clone(),
