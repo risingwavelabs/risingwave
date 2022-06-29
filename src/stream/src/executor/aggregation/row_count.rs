8e7b4cd3c97741b7b5dde05783027debb4d29a09
xxchan <xxchan22f@gmail.com>
2022-06-29 18:17:10 +0200

    refactor: improve the interface of bitmap

--- src/stream/src/executor/aggregation/row_count.rs
+++ src/stream/src/executor/aggregation/row_count.rs
@@ -140,7 +140,7 @@ mod tests {
         state
             .apply_batch(
                 &[Op::Delete, Op::Insert],
-                Some(&(vec![false, true]).try_into().unwrap()),
+                Some(&(vec![false, true]).into_iter().collect()),
                 &[],
             )
             .unwrap();
@@ -152,7 +152,7 @@ mod tests {
         state
             .apply_batch(
                 &[Op::Delete, Op::Insert],
-                Some(&(vec![true, false]).try_into().unwrap()),
+                Some(&(vec![true, false]).into_iter().collect()),
                 &[],
             )
             .unwrap();
