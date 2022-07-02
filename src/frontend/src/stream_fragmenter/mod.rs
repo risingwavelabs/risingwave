9316e9b3eb7588fa975e301556845239a76ed933
congyi <15605187270@163.com>
2022-07-02 15:28:20 +0800

    fix

--- src/frontend/src/stream_fragmenter/mod.rs
+++ src/frontend/src/stream_fragmenter/mod.rs
@@ -351,6 +351,7 @@ impl StreamFragmenter {
 
             NodeBody::AppendOnlyTopN(append_only_top_n_node) => {
                 append_only_top_n_node.table_id_l = state.gen_table_id();
+                append_only_top_n_node.table_id_m = state.gen_table_id();
                 append_only_top_n_node.table_id_h = state.gen_table_id();
             }
 
@@ -537,8 +538,8 @@ mod tests {
             StreamFragmenter::assign_local_table_id_to_stream_node(&mut state, &mut stream_node);
 
             if let NodeBody::TopN(top_n_node) = stream_node.node_body.as_ref().unwrap() {
-                expect_table_id += 1;
-                assert_eq!(expect_table_id, top_n_node.table_id_l);
+                expect_table_id += 3;
+                assert_eq!(expect_table_id, top_n_node.table_id_h);
             }
         }
 
@@ -556,7 +557,7 @@ mod tests {
                 stream_node.node_body.as_ref().unwrap()
             {
                 expect_table_id += 3;
-                assert_eq!(expect_table_id, append_only_top_n_node.table_id_l);
+                assert_eq!(expect_table_id, append_only_top_n_node.table_id_h);
             }
         }
     }
