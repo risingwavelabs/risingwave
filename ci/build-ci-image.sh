0145df076f0e0e1580aaee4c0164c920e8ff871a
Alex Chi <iskyzh@gmail.com>
2022-07-02 12:55:24 +0800

    fix docker-compose
    Signed-off-by: Alex Chi <iskyzh@gmail.com>

--- ci/build-ci-image.sh
+++ ci/build-ci-image.sh
@@ -9,7 +9,13 @@ cd "$DIR"
 cat ../rust-toolchain
 # shellcheck disable=SC2155
 export RUST_TOOLCHAIN=$(cat ../rust-toolchain)
+
+######################################################
+# !!! CHANGE THIS WHEN YOU WANT TO BUMP CI IMAGE !!! #
+#          AND ALSO docker-compose.yml               #
+######################################################
 export BUILD_ENV_VERSION=v20220702
+
 export BUILD_TAG="public.ecr.aws/x5u3w5h6/rw-build-env:${BUILD_ENV_VERSION}"
 
 echo "+++ Arch"
