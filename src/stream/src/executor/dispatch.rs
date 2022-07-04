cd3e3555dee4a497275f18b51875c0872737ed34
Ting Sun <suntcrick@gmail.com>
2022-07-04 23:01:07 +0800

    change to measure accurate blocking time

--- src/stream/src/executor/dispatch.rs
+++ src/stream/src/executor/dispatch.rs
@@ -22,7 +22,7 @@ use futures::Stream;
 use futures_async_stream::try_stream;
 use itertools::Itertools;
 use madsim::collections::{HashMap, HashSet};
-use madsim::task::JoinHandle;
+
 use madsim::time::Instant;
 use risingwave_common::array::{Op, StreamChunk};
 use risingwave_common::buffer::BitmapBuilder;
