0c2445d5236c29a4494ad105c6ba7f0215b6a67d
Arne (X1) <aleitert@kent.edu>
2022-07-04 17:20:10 +0800

    Added test for new_overlay_exp.

--- src/expr/src/expr/expr_ternary_bytes.rs
+++ src/expr/src/expr/expr_ternary_bytes.rs
@@ -226,4 +226,35 @@ mod tests {
             test_evals_dummy(expr, Some(ScalarImpl::from(String::from(expected))), false);
         }
     }
+
+    #[test]
+    fn test_overlay() {
+        let cases = vec![
+            ("aaa__aaa", "XY", 4, "aaaXYaaa"),
+            ("aaa", "XY", 3, "aaXY"),
+            ("aaa", "XY", 4, "aaaXY"),
+            ("aaa", "XY", -123, "XYa"),
+            ("aaa", "XY", 123, "aaaXY"),
+        ];
+
+        for (s, new_sub_str, start, expected) in cases {
+            let expr = new_overlay_exp(
+                Box::new(LiteralExpression::new(
+                    DataType::Varchar,
+                    Some(ScalarImpl::from(String::from(s))),
+                )),
+                Box::new(LiteralExpression::new(
+                    DataType::Varchar,
+                    Some(ScalarImpl::from(String::from(new_sub_str))),
+                )),
+                Box::new(LiteralExpression::new(
+                    DataType::Int32,
+                    Some(ScalarImpl::from(start)),
+                )),
+                DataType::Varchar,
+            );
+
+            test_evals_dummy(expr, Some(ScalarImpl::from(String::from(expected))), false);
+        }
+    }
 }
