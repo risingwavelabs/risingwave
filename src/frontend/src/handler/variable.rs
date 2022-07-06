ffe21730dce3356edadc2c3888da172deb5bb87d
Dylan <chenzl25@mail2.sysu.edu.cn>
2022-07-06 21:59:54 +0800

    feat(frontend): support show all (#3665) (#3694)
    * feat(frontend): support show all (#3665)
    
    * fmt
    
    * Update src/frontend/src/handler/variable.rs
    
    Co-authored-by: August <pin@singularity-data.com>
    
    * fix test
    
    * remove test
    
    Co-authored-by: August <pin@singularity-data.com>

--- src/frontend/src/handler/variable.rs
+++ src/frontend/src/handler/variable.rs
@@ -42,11 +42,7 @@ pub(super) fn handle_show(context: OptimizerContext, variable: Vec<Ident>) -> Re
     }
     let name = &variable[0].value;
     if name.eq_ignore_ascii_case("ALL") {
-        return Err(ErrorCode::NotImplemented(
-            "SHOW ALL is not supported".to_string(),
-            3665.into(),
-        )
-        .into());
+        return handle_show_all(&context);
     }
     let row = Row::new(vec![Some(config_reader.get(name)?)]);
 
@@ -58,6 +54,35 @@ pub(super) fn handle_show(context: OptimizerContext, variable: Vec<Ident>) -> Re
             name.to_ascii_lowercase(),
             TypeOid::Varchar,
         )],
+        true,
+    ))
+}
+
+pub(super) fn handle_show_all(context: &OptimizerContext) -> Result<PgResponse> {
+    let config_reader = context.session_ctx.config();
+
+    let all_variables = config_reader.get_all();
+
+    let rows = all_variables
+        .iter()
+        .map(|info| {
+            Row::new(vec![
+                Some(info.name.to_string()),
+                Some(info.setting.to_string()),
+                Some(info.description.to_string()),
+            ])
+        })
+        .collect();
+
+    Ok(PgResponse::new(
+        StatementType::SHOW_COMMAND,
+        all_variables.len() as i32,
+        rows,
+        vec![
+            PgFieldDescriptor::new("name".to_string(), TypeOid::Varchar),
+            PgFieldDescriptor::new("setting".to_string(), TypeOid::Varchar),
+            PgFieldDescriptor::new("description".to_string(), TypeOid::Varchar),
+        ],
         true,
     ))
 }
