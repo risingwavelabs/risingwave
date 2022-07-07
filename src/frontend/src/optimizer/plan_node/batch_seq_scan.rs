841cc362933f002882209475fb53c60609fde18d
xxchan <xxchan22f@gmail.com>
2022-07-07 21:14:13 +0200

    fix batch scan distribution

--- src/frontend/src/optimizer/plan_node/batch_seq_scan.rs
+++ src/frontend/src/optimizer/plan_node/batch_seq_scan.rs
@@ -70,7 +70,10 @@ impl BatchSeqScan {
             if self.logical.is_sys_table() {
                 Distribution::Single
             } else {
-                Distribution::SomeShard
+                match self.logical.distribution_key() {
+                    Some(dist_key) => Distribution::HashShard(dist_key),
+                    None => Distribution::SomeShard,
+                }
             },
             self.scan_range.clone(),
         )
