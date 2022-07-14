60b8ec6f9a6751fbfbce58fc48d70b7e9832986c
Alex Chi <iskyzh@gmail.com>
2022-07-14 18:20:15 +0800

    chore(ci): start cluster only once on e2e test
    Signed-off-by: Alex Chi <iskyzh@gmail.com>

--- ci/scripts/e2e-test.sh
+++ ci/scripts/e2e-test.sh
@@ -39,22 +39,16 @@ echo "--- Prepare RiseDev playground"
 cargo make pre-start-playground
 cargo make link-all-in-one-binaries
 
-echo "--- e2e, ci-3cn-1fe, streaming"
+ehoc "--- Start cluster"
 cargo make ci-start ci-3cn-1fe
+
+echo "--- e2e, ci-3cn-1fe, streaming"
 timeout 5m sqllogictest -p 4566 -d dev './e2e_test/streaming/**/*.slt' --junit "streaming-${profile}"
 
-echo "--- Kill cluster"
-cargo make ci-kill
-
 echo "--- e2e, ci-3cn-1fe, delta join"
-cargo make ci-start ci-3cn-1fe
 timeout 3m sqllogictest -p 4566 -d dev './e2e_test/streaming_delta_join/**/*.slt' --junit "streaming-delta-join-${profile}"
 
-echo "--- Kill cluster"
-cargo make ci-kill
-
 echo "--- e2e, ci-3cn-1fe, batch distributed"
-cargo make ci-start ci-3cn-1fe
 timeout 2m sqllogictest -p 4566 -d dev './e2e_test/ddl/**/*.slt' --junit "batch-ddl-${profile}"
 timeout 2m sqllogictest -p 4566 -d dev './e2e_test/batch/**/*.slt' --junit "batch-${profile}"
 timeout 2m sqllogictest -p 4566 -d dev './e2e_test/database/prepare.slt'
