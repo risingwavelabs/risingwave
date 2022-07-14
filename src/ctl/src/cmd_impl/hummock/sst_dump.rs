2d48d2c378d058965fa0808086fae2734e3635ff
Arne (X1) <aleitert@kent.edu>
2022-07-14 20:28:49 +0800

    Added interpretation KV-paris based on the table schema.

--- src/ctl/src/cmd_impl/hummock/sst_dump.rs
+++ src/ctl/src/cmd_impl/hummock/sst_dump.rs
@@ -15,6 +15,8 @@
 use std::collections::HashMap;
 
 use bytes::Buf;
+use risingwave_common::util::value_encoding::deserialize_cell;
+use risingwave_frontend::catalog::TableCatalog;
 use risingwave_hummock_sdk::compaction_group::hummock_version_ext::HummockVersionExt;
 use risingwave_hummock_sdk::key::{get_epoch, get_table_id, user_key};
 use risingwave_object_store::object::{BlockLocation, ObjectStore};
@@ -23,7 +25,7 @@ use risingwave_storage::hummock::value::HummockValue;
 use risingwave_storage::hummock::{Block, BlockHolder, BlockIterator, CompressionAlgorithm};
 use risingwave_storage::monitor::StoreLocalStatistic;
 
-use crate::common::HummockServiceOpts;
+use crate::common::{HummockServiceOpts, MetaServiceOpts};
 
 pub async fn sst_dump() -> anyhow::Result<()> {
     // Retrieves the SSTable store so we can access the SSTableMeta
@@ -39,6 +41,25 @@ pub async fn sst_dump() -> anyhow::Result<()> {
     let sstable_id_infos = meta_client.list_sstable_id_infos(version.id).await?;
     sstable_id_infos.iter().for_each(|id_info| {
         id_info_map.insert(id_info.id, id_info);
+    });
+
+    // Determine all database tables ...
+    let meta_opts = MetaServiceOpts::from_env()?;
+    let meta = meta_opts.create_meta_client().await?;
+    let mvs = meta.risectl_list_state_tables().await?;
+
+    // ... and add a list of their types into a hash table.
+    let mut column_table = HashMap::new();
+    mvs.iter().for_each(|tbl| {
+        let mut col_list = vec![];
+        TableCatalog::from(tbl).columns.iter().for_each(|clm| {
+            col_list.push((
+                clm.data_type().clone(),
+                String::from(clm.name()),
+                clm.is_hidden().clone(),
+            ));
+        });
+        column_table.insert(tbl.id, (tbl.name.clone(), col_list));
     });
 
     for level in version.get_combined_levels() {
@@ -111,28 +132,53 @@ pub async fn sst_dump() -> anyhow::Result<()> {
                 let mut block_iter = BlockIterator::new(holder);
                 block_iter.seek_to_first();
 
+                let mut column_idx: usize = 0;
                 while block_iter.is_valid() {
                     let full_key = block_iter.key();
                     let user_key = user_key(full_key);
 
                     let full_val = block_iter.value();
                     let humm_val = HummockValue::from_slice(block_iter.value())?;
-                    let (val_type, val_meta, user_val) = match humm_val {
-                        HummockValue::Put(meta, uval) => ("Put", meta.vnode, uval),
-                        HummockValue::Delete(meta) => ("Delete", meta.vnode, &[] as &[u8]),
+                    let (isPut, val_meta, user_val) = match humm_val {
+                        HummockValue::Put(meta, uval) => (true, meta.vnode, uval),
+                        HummockValue::Delete(meta) => (false, meta.vnode, &[] as &[u8]),
                     };
 
                     let epoch = get_epoch(full_key);
-                    let table_id = get_table_id(full_key).unwrap();
 
                     println!("\t\t  full key: {:02x?}", full_key);
                     println!("\t\tfull value: {:02x?}", full_val);
                     println!("\t\t  user key: {:02x?}", user_key);
                     println!("\t\tuser value: {:02x?}", user_val);
-                    println!("\t\tvalue-meta: {}", val_meta);
                     println!("\t\t     epoch: {}", epoch);
-                    println!("\t\t  table ID: {}", table_id);
-                    println!("\t\t      type: {}", val_type);
+                    println!("\t\t      type: {}", if isPut { "Put" } else { "Delete" });
+                    println!("\t\tvalue-meta: {}", val_meta);
+
+                    if let Some(table_id) = get_table_id(full_key) {
+                        let (table_name, columns) = &column_table.get(&table_id).unwrap();
+                        println!("\t\t     table: {} - {}", table_id, table_name);
+
+                        // Check if new row.
+                        if isPut {
+                            if user_val.len() == 0
+                                && (&user_key[user_key.len() - 4..]).get_i32() == 0x7fffffff
+                            {
+                                // New row. Reset column index.
+                                column_idx = 0;
+                            } else {
+                                let (data_type, name, is_hidden) = &columns[column_idx];
+                                let datum = deserialize_cell(user_val, data_type).unwrap().unwrap();
+                                println!(
+                                    "\t\t    column: {} {}",
+                                    name,
+                                    if is_hidden.clone() { "(hidden)" } else { "" }
+                                );
+                                println!("\t\t     datum: {:?}", datum);
+                                column_idx += 1;
+                            }
+                        }
+                    }
+
                     println!();
 
                     block_iter.next();
