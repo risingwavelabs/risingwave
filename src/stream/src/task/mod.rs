8eb7c49f0bbe60053cdefcb3d13c884ad38a44e3
Runji Wang <wangrunji0408@163.com>
2022-07-18 17:33:00 +0800

    fix(test): make deterministic test compile (#3951)
    * add cfg `tokio_unstable` to simulation build
    
    Signed-off-by: Runji Wang <wangrunji0408@163.com>
    
    * revert `madsim::collections` to `std::collections`
    
    * bump madsim version
    
    * enable simulation test for more crates
    
    Co-authored-by: mergify[bot] <37929162+mergify[bot]@users.noreply.github.com>

--- src/stream/src/task/mod.rs
+++ src/stream/src/task/mod.rs
@@ -12,9 +12,9 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+use std::collections::HashMap;
 use std::sync::Arc;
 
-use madsim::collections::HashMap;
 use parking_lot::{Mutex, MutexGuard, RwLock};
 use risingwave_common::error::{ErrorCode, Result, RwError};
 use risingwave_common::util::addr::HostAddr;
