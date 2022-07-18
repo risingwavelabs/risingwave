3137c4342e65d0bfabe66419b5dfc1c16d9163e0
Richard Chien <stdrc@outlook.com>
2022-07-18 12:37:15 +0800

    remove unnecessary `EncodedColumn` struct

--- src/common/src/util/encoding_for_comparison.rs
+++ src/common/src/util/encoding_for_comparison.rs
@@ -19,8 +19,6 @@ use crate::error::Result;
 use crate::types::{serialize_datum_ref_into, DataType, ScalarRefImpl};
 use crate::util::sort_util::{OrderPair, OrderType};
 
-struct EncodedColumn(pub Vec<Vec<u8>>);
-
 /// This function is used to check whether we can perform encoding on this type.
 /// TODO: based on `memcomparable`, we may support more data type in the future.
 pub fn is_type_encodable(t: DataType) -> bool {
@@ -43,14 +41,12 @@ fn encode_value(value: Option<ScalarRefImpl>, order: &OrderType) -> Result<Vec<u
     Ok(serializer.into_inner())
 }
 
-fn encode_array(array: &ArrayImpl, order: &OrderType) -> Result<EncodedColumn> {
+fn encode_array(array: &ArrayImpl, order: &OrderType) -> Result<Vec<Vec<u8>>> {
     let mut data = Vec::with_capacity(array.len());
-
     for datum in array.iter() {
         data.push(encode_value(datum, order)?);
     }
-
-    Ok(EncodedColumn(data))
+    Ok(data)
 }
 
 /// This function is used to accelerate the comparison of tuples. It takes datachunk and
@@ -66,7 +62,7 @@ pub fn encode_chunk(chunk: &DataChunk, order_pairs: &[OrderPair]) -> Vec<Vec<u8>
 
     let mut encoded_chunk = vec![vec![]; chunk.capacity()];
     for encoded_column in encoded_columns {
-        for (encoded_row, data) in encoded_chunk.iter_mut().zip_eq(encoded_column.0) {
+        for (encoded_row, data) in encoded_chunk.iter_mut().zip_eq(encoded_column) {
             encoded_row.extend(data);
         }
     }
