4dde970d459a6b96c7faaff80e0cbfb99231936f
Noel Kwan <noelkwan1998@gmail.com>
2022-07-18 23:39:09 +0800

    refactor time_window to separate file

---
+++ src/tests/sqlsmith/src/time_window.rs
@@ -0,0 +1,88 @@
+use itertools::Itertools;
+use rand::prelude::SliceRandom;
+use rand::Rng;
+use risingwave_frontend::expr::DataTypeName;
+use risingwave_sqlparser::ast::{
+    FunctionArg, FunctionArgExpr, Ident, ObjectName, TableAlias, TableFactor, TableWithJoins,
+};
+
+use crate::{Column, Expr, SqlGenerator, Table};
+
+impl<'a, R: Rng> SqlGenerator<'a, R> {
+    /// Generates time window functions.
+    pub(crate) fn gen_time_window_func(&mut self) -> TableWithJoins {
+        self.gen_tumble()
+    }
+
+    /// Generates `TUMBLE`.
+    /// TUMBLE(data: TABLE, timecol: COLUMN, size: INTERVAL, offset?: INTERVAL)
+    fn gen_tumble(&mut self) -> TableWithJoins {
+        let tables = find_tables_with_timestamp_cols(self.tables.clone());
+        let (source_table_name, time_cols, schema) = tables
+            .choose(&mut self.rng)
+            .expect("seeded tables all do not have timestamp");
+        let table_name = format!("tumble_{}", &self.bound_relations.len());
+        let alias = TableAlias {
+            name: Ident::new(table_name.clone()),
+            columns: vec![],
+        };
+
+        let time_col = time_cols.choose(&mut self.rng).unwrap();
+
+        let name = Expr::Identifier(source_table_name.as_str().into());
+        // TODO: Currently only literal interval supported.
+        // Tracked in: <TODO>
+        let size = self.gen_simple_scalar(DataTypeName::Interval);
+        let time_col = Expr::Identifier(time_col.name.as_str().into());
+        let args = [name, time_col, size]
+            .into_iter()
+            .map(create_function_arg_from_expr)
+            .collect_vec();
+
+        let factor = TableFactor::Table {
+            name: ObjectName(vec!["tumble".into()]),
+            alias: Some(alias),
+            args,
+        };
+        let relation = TableWithJoins {
+            relation: factor,
+            joins: vec![],
+        };
+
+        let table = Table {
+            name: table_name,
+            columns: schema.clone(),
+        };
+        self.add_relation_to_context(table);
+
+        relation
+    }
+}
+
+/// Create `FunctionArg` from an `Expr`.
+fn create_function_arg_from_expr(expr: Expr) -> FunctionArg {
+    FunctionArg::Unnamed(FunctionArgExpr::Expr(expr))
+}
+
+fn is_timestamp_col(c: &Column) -> bool {
+    c.data_type == DataTypeName::Timestamp || c.data_type == DataTypeName::Timestampz
+}
+
+fn get_table_name_and_cols_with_timestamp(table: Table) -> (String, Vec<Column>, Vec<Column>) {
+    let name = table.name.clone();
+    let cols_with_timestamp = table
+        .get_qualified_columns()
+        .iter()
+        .cloned()
+        .filter(is_timestamp_col)
+        .collect_vec();
+    (name, cols_with_timestamp, table.columns)
+}
+
+fn find_tables_with_timestamp_cols(tables: Vec<Table>) -> Vec<(String, Vec<Column>, Vec<Column>)> {
+    tables
+        .into_iter()
+        .map(get_table_name_and_cols_with_timestamp)
+        .filter(|(_name, timestamp_cols, _schema)| !timestamp_cols.is_empty())
+        .collect()
+}
