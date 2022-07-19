d9f354f9513ec55d51ed811208e7bdfcfe03d1ed
Noel Kwan <47273164+kwannoel@users.noreply.github.com>
2022-07-19 22:31:32 +0800

    feat(sqlsmith): generate `with` (#4011)
    * add gen_with skeleton
    
    * refactor
    
    * gen alias
    
    * gen with query
    
    * complete `gen_with` implementation
    
    * fmt
    
    * reduce query complexity
    
    * add local query
    
    * extract with tables
    
    * pass with table
    
    * use with table
    
    * fmt
    
    * exclude with from joins
    
    * better description
    
    * revert complexity change + clarify docs
    
    * Update src/tests/sqlsmith/src/utils.rs
    
    Co-authored-by: Tao Wu <wutao@singularity-data.com>
    
    * Update src/tests/sqlsmith/src/utils.rs
    
    Co-authored-by: Tao Wu <wutao@singularity-data.com>
    
    * address review comments
    
    Co-authored-by: Tao Wu <wutao@singularity-data.com>
    Co-authored-by: mergify[bot] <37929162+mergify[bot]@users.noreply.github.com>

--- src/tests/sqlsmith/src/time_window.rs
+++ src/tests/sqlsmith/src/time_window.rs
@@ -17,9 +17,10 @@ use rand::prelude::SliceRandom;
 use rand::Rng;
 use risingwave_frontend::expr::DataTypeName;
 use risingwave_sqlparser::ast::{
-    DataType, FunctionArg, FunctionArgExpr, ObjectName, TableAlias, TableFactor, TableWithJoins,
+    DataType, FunctionArg, ObjectName, TableAlias, TableFactor, TableWithJoins,
 };
 
+use crate::utils::{create_args, create_table_alias};
 use crate::{Column, Expr, SqlGenerator, Table};
 
 impl<'a, R: Rng> SqlGenerator<'a, R> {
@@ -39,7 +40,7 @@ impl<'a, R: Rng> SqlGenerator<'a, R> {
             .choose(&mut self.rng)
             .expect("seeded tables all do not have timestamp");
         let table_name = self.create_table_name_with_prefix("tumble");
-        let alias = create_alias(&table_name);
+        let alias = create_table_alias(&table_name);
 
         let name = Expr::Identifier(source_table_name.as_str().into());
         // TODO: Currently only literal size expr supported.
@@ -64,7 +65,7 @@ impl<'a, R: Rng> SqlGenerator<'a, R> {
             .choose(&mut self.rng)
             .expect("seeded tables all do not have timestamp");
         let table_name = self.create_table_name_with_prefix("hop");
-        let alias = create_alias(&table_name);
+        let alias = create_table_alias(&table_name);
 
         let time_col = time_cols.choose(&mut self.rng).unwrap();
 
@@ -87,24 +88,6 @@ impl<'a, R: Rng> SqlGenerator<'a, R> {
 
         relation
     }
-
-    fn create_table_name_with_prefix(&self, prefix: &str) -> String {
-        format!("{}_{}", prefix, &self.bound_relations.len())
-    }
-}
-
-fn create_args(arg_exprs: Vec<Expr>) -> Vec<FunctionArg> {
-    arg_exprs
-        .into_iter()
-        .map(create_function_arg_from_expr)
-        .collect()
-}
-
-fn create_alias(table_name: &str) -> TableAlias {
-    TableAlias {
-        name: table_name.into(),
-        columns: vec![],
-    }
 }
 
 /// Create a table view function.
@@ -118,11 +101,6 @@ fn create_tvf(name: &str, alias: TableAlias, args: Vec<FunctionArg>) -> TableWit
         relation: factor,
         joins: vec![],
     }
-}
-
-/// Create `FunctionArg` from an `Expr`.
-fn create_function_arg_from_expr(expr: Expr) -> FunctionArg {
-    FunctionArg::Unnamed(FunctionArgExpr::Expr(expr))
 }
 
 fn is_timestamp_col(c: &Column) -> bool {
