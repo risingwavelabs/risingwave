7e715774e5ad7216b0ef0ab477851d4f38778c82
Runji Wang <wangrunji0408@163.com>
2022-07-22 18:45:14 +0800

    `CREATE TABLE` works on madsim

--- src/tests/simulation/tests/basic.rs
+++ src/tests/simulation/tests/basic.rs
@@ -1,5 +1,7 @@
 // #![cfg(madsim)]
 
+use std::time::Duration;
+
 use clap::Parser;
 
 #[madsim::test]
@@ -33,6 +35,8 @@ async fn basic() {
                 "frontend-node",
                 "--host",
                 "0.0.0.0:4566",
+                "--client-address",
+                "192.168.2.1:4566",
                 "--meta-addr",
                 "192.168.1.1:5690",
             ]);
@@ -50,12 +54,47 @@ async fn basic() {
                 "compute-node",
                 "--host",
                 "0.0.0.0:5688",
+                "--client-address",
+                "192.168.3.1:5688",
                 "--meta-address",
                 "192.168.1.1:5690",
             ]);
             risingwave_compute::start(opts).await
         })
         .build();
+    // wait for the service to be ready
+    tokio::time::sleep(std::time::Duration::from_secs(1)).await;
 
-    tokio::signal::ctrl_c().await.unwrap();
+    // client
+    handle
+        .create_node()
+        .name("client")
+        .ip("192.168.100.1".parse().unwrap())
+        .build()
+        .spawn(async {
+            let (client, connection) = tokio_postgres::Config::new()
+                .host("192.168.2.1")
+                .port(4566)
+                .dbname("dev")
+                .user("root")
+                .connect_timeout(Duration::from_secs(5))
+                .connect(tokio_postgres::NoTls)
+                .await
+                .expect("Failed to connect to database");
+            tokio::spawn(async move {
+                connection.await.expect("Postgres connection error");
+            });
+            let sql = "CREATE TABLE supplier (
+                s_suppkey INTEGER,
+                s_name VARCHAR(25),
+                s_address VARCHAR(40),
+                s_nationkey INTEGER,
+                s_phone VARCHAR(15),
+                s_acctbal NUMERIC,
+                s_comment VARCHAR(101)
+            );";
+            client.execute(sql, &[]).await.unwrap();
+        })
+        .await
+        .unwrap();
 }
