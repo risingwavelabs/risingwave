f9beff0db3691fbe4d6a98c62d0537f7a6557355
Noel Kwan <noelkwan1998@gmail.com>
2022-07-25 14:53:22 +0800

    run sqlsmith frontend tests in separate script

---
+++ ci/scripts/sqlsmith-fe.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+# Exits as soon as any line fails.
+set -euo pipefail
+
+source ci/scripts/common.env.sh
+
+echo "+++ Run sqlsmith frontend tests"
+# use tee to disable progress bar
+NEXTEST_PROFILE=ci cargo nextest run --package risingwave_sqlsmith 2> >(tee)
--- ci/scripts/unit-test.sh
+++ ci/scripts/unit-test.sh
@@ -6,8 +6,13 @@ set -euo pipefail
 source ci/scripts/common.env.sh
 
 echo "+++ Run unit tests with coverage"
+# disable fuzzing tests (risingwave_sqlsmith)
 # use tee to disable progress bar
-NEXTEST_PROFILE=ci cargo llvm-cov nextest --lcov --output-path lcov.info --features failpoints 2> >(tee)
+NEXTEST_PROFILE=ci cargo llvm-cov nextest \
+    --lcov --output-path lcov.info \
+    --features failpoints \
+    --workspace --exclude risingwave_sqlsmith
+    2> >(tee)
 
 echo "--- Codecov upload coverage reports"
 curl -Os https://uploader.codecov.io/latest/linux/codecov && chmod +x codecov
