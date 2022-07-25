e16c4740a77397dee863b12e3a625f8fb4febd7d
Noel Kwan <noelkwan1998@gmail.com>
2022-07-25 21:45:38 +0800

    separate code cov from test

--- ci/scripts/unit-test.sh
+++ ci/scripts/unit-test.sh
@@ -8,11 +8,8 @@ source ci/scripts/common.env.sh
 echo "+++ Run unit tests with coverage"
 # disable fuzzing tests (risingwave_sqlsmith)
 # use tee to disable progress bar
-NEXTEST_PROFILE=ci cargo llvm-cov nextest \
-    --exclude-from-test risingwave_sqlsmith \
-    --lcov --output-path lcov.info \
-    --features failpoints \
-    2> >(tee)
+NEXTEST_PROFILE=ci cargo nextest run -E "not package(sqlsmith)" 2> >(tee)
+cargo llvm-cov --lcov --output-path lcov.info --features failpoints 2> >(tee)
 
 echo "--- Codecov upload coverage reports"
 curl -Os https://uploader.codecov.io/latest/linux/codecov && chmod +x codecov
