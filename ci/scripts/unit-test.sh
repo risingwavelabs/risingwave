4a7a7d89493e471a286fcc0b325189535a22105d
Noel Kwan <noelkwan1998@gmail.com>
2022-07-25 22:44:40 +0800

    use alternate workaround + sync up fe tests for sqlsmith

--- ci/scripts/unit-test.sh
+++ ci/scripts/unit-test.sh
@@ -8,8 +8,15 @@ source ci/scripts/common.env.sh
 echo "+++ Run unit tests with coverage"
 # disable fuzzing tests (risingwave_sqlsmith)
 # use tee to disable progress bar
-NEXTEST_PROFILE=ci cargo nextest run -E "not package(risingwave_sqlsmith)" 2> >(tee)
-cargo llvm-cov --lcov --output-path lcov.info --features failpoints 2> >(tee)
+# NOTE: We pass in arguments to nextest BEFORE
+# nextest subcommand.
+# This is required due to: <https://github.com/taiki-e/cargo-llvm-cov/issues/151>
+NEXTEST_PROFILE=ci cargo llvm-cov \
+    --workspace --exclude risingwave_sqlsmith \
+    nextest \
+    --lcov --output-path lcov.info \
+    --features failpoints \
+    2> >(tee)
 
 echo "--- Codecov upload coverage reports"
 curl -Os https://uploader.codecov.io/latest/linux/codecov && chmod +x codecov
