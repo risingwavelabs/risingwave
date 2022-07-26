2f42399e59592345bc5097cd229659c676fd7f7c
nanderstabel <nanderstabel@gmail.com>
2022-07-26 09:55:55 +0200

    e2e-sink init

---
+++ ci/scripts/e2e-sink-test.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Exits as soon as any line fails.
+set -euo pipefail
+
+source ci/scripts/common.env.sh
+
+echo "--- Download artifacts"
+mkdir -p target/debug
+buildkite-agent artifact download risingwave-dev target/debug/
+buildkite-agent artifact download risedev-playground-dev target/debug/
+mv target/debug/risingwave-dev target/debug/risingwave
+mv target/debug/risedev-playground-dev target/debug/risedev-playground
+
+echo "--- Adjust permission"
+chmod +x ./target/debug/risingwave
+chmod +x ./target/debug/risedev-playground
+
+echo "--- Generate RiseDev CI config"
+cp ci/risedev-components.ci.env risedev-components.user.env
+
+echo "--- Prepare RiseDev playground"
+cargo make pre-start-playground
+cargo make link-all-in-one-binaries
