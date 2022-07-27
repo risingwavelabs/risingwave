2f8626639261c81e39efdb0cbebee9b459d26f4b
Alex Chi <iskyzh@gmail.com>
2022-07-27 17:45:49 +0800

    fix(ci): fix unit test failure (#4223)
    * fix(ci): fix unit test failure
    
    Signed-off-by: Alex Chi <iskyzh@gmail.com>
    
    * fix
    
    Signed-off-by: Alex Chi <iskyzh@gmail.com>

--- ci/scripts/pr.env.sh
+++ ci/scripts/pr.env.sh
@@ -1,10 +1,13 @@
 #!/bin/sh
 set -euo pipefail
 
+set +e
 # Set features, depending on our workflow
 # If sqlsmith files are modified, we run unit tests with sqlsmith enabled
 # by overriding RUSTFLAGS to enable sqlsmith feature.
 CHANGED=$(git diff --name-only origin/main | grep "ci/scripts/pr.env.sh\|src/tests/sqlsmith")
+set -e
+
 if [[ -n "$CHANGED" ]]; then
     echo "Changes to Sqlsmith source files detected.";
     export RUN_SQLSMITH=1;
