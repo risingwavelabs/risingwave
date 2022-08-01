2c34b331d394f0a61bf07ada2cf52229d90dfc94
Dylan <chenzl25@mail2.sysu.edu.cn>
2022-08-01 10:58:54 +0800

    fix(frontend): fix display pk of stream table scan (#4279)
    * fix(frontend): fix display pk of stream table scan
    
    * fmt
    
    Co-authored-by: mergify[bot] <37929162+mergify[bot]@users.noreply.github.com>

--- src/frontend/src/optimizer/plan_node/batch_lookup_join.rs
+++ src/frontend/src/optimizer/plan_node/batch_lookup_join.rs
@@ -110,7 +110,7 @@ impl fmt::Display for BatchLookupJoin {
                     &format_args!(
                         "{:?}",
                         &IndicesDisplay {
-                            vec: self.logical.output_indices(),
+                            indices: self.logical.output_indices(),
                             input_schema: &concat_schema,
                         }
                     ),
