090a7c0b1942291ce037a479fb0aa2952cf98131
Bugen Zhao <i@bugenzhao.com>
2022-08-04 22:49:02 +0800

    fix licenses
    Signed-off-by: Bugen Zhao <i@bugenzhao.com>

--- src/utils/stack_trace/src/lib.rs
+++ src/utils/stack_trace/src/lib.rs
@@ -1,6 +1,24 @@
-#![allow(clippy::declare_interior_mutable_const)]
+// Copyright 2022 Singularity Data
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 #![feature(generators)]
 #![feature(map_try_insert)]
+#![feature(lint_reasons)]
+// FIXME: This is a false-positive clippy test, remove this while bumping toolchain.
+// https://github.com/tokio-rs/tokio/issues/4836
+// https://github.com/rust-lang/rust-clippy/issues/8493
+#![expect(clippy::declare_interior_mutable_const)]
 
 use std::borrow::Cow;
 use std::cell::{RefCell, RefMut};
