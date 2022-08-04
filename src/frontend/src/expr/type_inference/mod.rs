cc4224dd18218a9025fdac688f805b369d3ad025
Noel Kwan <47273164+kwannoel@users.noreply.github.com>
2022-08-04 11:44:17 +0800

    feat(sqlsmith): generate explicit type castings (#4419)
    * gen cast map
    
    * gen cast
    
    * dump cast table
    
    * gen explicit cast expr
    
    * avoid varchar casts
    
    * fix review comments

--- src/frontend/src/expr/type_inference/mod.rs
+++ src/frontend/src/expr/type_inference/mod.rs
@@ -21,7 +21,8 @@ mod cast;
 mod func;
 pub use agg::{agg_func_sigs, AggFuncSig};
 pub use cast::{
-    align_types, cast_map_array, cast_ok, cast_ok_base, least_restrictive, CastContext,
+    align_types, cast_map_array, cast_ok, cast_ok_base, cast_sigs, least_restrictive, CastContext,
+    CastSig,
 };
 pub use func::{func_sigs, infer_type, FuncSign};
 /// `DataTypeName` is designed for type derivation here. In other scenarios,
