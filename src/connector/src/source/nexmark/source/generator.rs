dd4e2c638c5d878a1ab5a615f2145cf05fb63a09
Bugen Zhao <i@bugenzhao.com>
2022-08-04 13:25:08 +0800

    yield if ns = 0
    Signed-off-by: Bugen Zhao <i@bugenzhao.com>

--- src/connector/src/source/nexmark/source/generator.rs
+++ src/connector/src/source/nexmark/source/generator.rs
@@ -104,11 +104,15 @@ impl NexmarkEventGenerator {
             )));
         }
 
-        if !self.use_real_time && self.min_event_gap_in_ns > 0 {
-            tokio::time::sleep(std::time::Duration::from_nanos(
-                (self.events_so_far - old_events_so_far) as u64 * self.min_event_gap_in_ns,
-            ))
-            .await;
+        if !self.use_real_time {
+            if self.min_event_gap_in_ns > 0 {
+                tokio::time::sleep(std::time::Duration::from_nanos(
+                    (self.events_so_far - old_events_so_far) as u64 * self.min_event_gap_in_ns,
+                ))
+                .await;
+            } else {
+                tokio::task::yield_now().await;
+            }
         }
 
         Ok(res)
