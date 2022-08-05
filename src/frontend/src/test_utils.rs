59def412f8dfae6b3d9a2499a35d5c07cced381f
sinemora <31039007+cnissnzg@users.noreply.github.com>
2022-08-05 12:50:34 +0800

    feat(frontend): add CREATEUSER and NOCREATEUSER option for user create/alter (#4447)
    * initial
    
    * fix judge
    
    * fix
    
    * fix
    
    * fix pg_user
    
    Co-authored-by: mergify[bot] <37929162+mergify[bot]@users.noreply.github.com>

--- src/frontend/src/test_utils.rs
+++ src/frontend/src/test_utils.rs
@@ -413,6 +413,8 @@ impl UserInfoWriter for MockUserInfoWriter {
                 user_info.can_login = update_user.can_login;
             } else if field == UpdateField::CreateDb as i32 {
                 user_info.can_create_db = update_user.can_create_db;
+            } else if field == UpdateField::CreateUser as i32 {
+                user_info.can_create_user = update_user.can_create_user;
             } else if field == UpdateField::AuthInfo as i32 {
                 user_info.auth_info = update_user.auth_info.clone();
             } else if field == UpdateField::Rename as i32 {
@@ -501,6 +503,7 @@ impl MockUserInfoWriter {
             name: DEFAULT_SUPER_USER.to_string(),
             is_supper: true,
             can_create_db: true,
+            can_create_user: true,
             can_login: true,
             ..Default::default()
         });
