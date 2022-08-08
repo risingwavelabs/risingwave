ff18d5bfd29eeadf35e0918814dd96e17772344b
Zejiong Dong <810918843@qq.com>
2022-08-08 17:00:01 +0800

    modify e2e-test

--- ci/scripts/e2e-test.sh
+++ ci/scripts/e2e-test.sh
@@ -52,8 +52,14 @@ cargo make ci-start ci-3cn-1fe
 timeout 2m sqllogictest -p 4566 -d dev './e2e_test/ddl/**/*.slt' --junit "batch-ddl-${profile}"
 timeout 3m sqllogictest -p 4566 -d dev './e2e_test/batch/**/*.slt' --junit "batch-${profile}"
 timeout 2m sqllogictest -p 4566 -d dev './e2e_test/database/prepare.slt'
-timeout 2m sqllogictest -p 4566 -d dev -e postgres-extended './e2e_test/extended_query/**/*.slt'
 timeout 2m sqllogictest -p 4566 -d test './e2e_test/database/test.slt'
+
+echo "--- Kill cluster"
+cargo make ci-kill
+
+echo "--- e2e, ci-3cn-1fe, extended query"
+cargo make ci-start ci-3cn-1fe
+timeout 2m sqllogictest -p 4566 -d dev -e postgres-extended './e2e_test/extended_query/**/*.slt'
 
 echo "--- Kill cluster"
 cargo make ci-kill
