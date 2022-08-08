2145298887544f387299b8e0a2e5ae77fa2ccceb
Runji Wang <wangrunji0408@163.com>
2022-08-08 10:52:20 +0800

    ci: add timeout for determinsitic test
    Signed-off-by: Runji Wang <wangrunji0408@163.com>

--- ci/scripts/deterministic-e2e-test.sh
+++ ci/scripts/deterministic-e2e-test.sh
@@ -9,16 +9,16 @@ echo "--- Generate RiseDev CI config"
 cp ci/risedev-components.ci.env risedev-components.user.env
 
 echo "--- Build deterministic simulation e2e test runner"
-cargo make sslt --release -- --help
+timeout 10m cargo make sslt --release -- --help
 
 echo "--- deterministic simulation e2e, ci-3cn-1fe, ddl"
-cargo make sslt --release -- './e2e_test/ddl/**/*.slt'
+timeout 10s cargo make sslt --release -- './e2e_test/ddl/**/*.slt'
 
 echo "--- deterministic simulation e2e, ci-3cn-1fe, streaming"
-cargo make sslt --release -- './e2e_test/streaming/**/*.slt'
+timeout 1m cargo make sslt --release -- './e2e_test/streaming/**/*.slt'
 
 echo "--- deterministic simulation e2e, ci-3cn-1fe, streaming_delta_join"
-cargo make sslt --release -- './e2e_test/streaming_delta_join/**/*.slt'
+timeout 10s cargo make sslt --release -- './e2e_test/streaming_delta_join/**/*.slt'
 
 echo "--- deterministic simulation e2e, ci-3cn-1fe, batch"
-cargo make sslt --release -- './e2e_test/batch/**/*.slt'
+timeout 10s cargo make sslt --release -- './e2e_test/batch/**/*.slt'
