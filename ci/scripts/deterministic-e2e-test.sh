170e4da9ee64a3a39a2cdd34504bf421aeaa2822
Alex Chi <iskyzh@gmail.com>
2022-08-08 00:31:08 -0400

    refactor(meta): remove force rewrite of join -> delta (#4480)
    * refactor(meta): remove force rewrite of join -> delta
    
    Signed-off-by: Alex Chi <iskyzh@gmail.com>
    
    * fix
    
    Signed-off-by: Alex Chi <iskyzh@gmail.com>
    
    * fix e2e test
    
    Signed-off-by: Alex Chi <iskyzh@gmail.com>
    
    * fix planner test
    
    Signed-off-by: Alex Chi <iskyzh@gmail.com>
    
    Co-authored-by: mergify[bot] <37929162+mergify[bot]@users.noreply.github.com>

--- ci/scripts/deterministic-e2e-test.sh
+++ ci/scripts/deterministic-e2e-test.sh
@@ -17,8 +17,5 @@ timeout 10s cargo make sslt --release -- './e2e_test/ddl/**/*.slt'
 echo "--- deterministic simulation e2e, ci-3cn-1fe, streaming"
 timeout 1m cargo make sslt --release -- './e2e_test/streaming/**/*.slt'
 
-echo "--- deterministic simulation e2e, ci-3cn-1fe, streaming_delta_join"
-timeout 10s cargo make sslt --release -- './e2e_test/streaming_delta_join/**/*.slt'
-
 echo "--- deterministic simulation e2e, ci-3cn-1fe, batch"
 timeout 10s cargo make sslt --release -- './e2e_test/batch/**/*.slt'
