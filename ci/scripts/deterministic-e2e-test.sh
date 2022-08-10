e5c5c1e24631f9064046711f41777220e9b99a1c
Runji Wang <wangrunji0408@163.com>
2022-08-10 13:56:57 +0800

    ci: switch back deterministic-e2e-test to release mode
    Signed-off-by: Runji Wang <wangrunji0408@163.com>

--- ci/scripts/deterministic-e2e-test.sh
+++ ci/scripts/deterministic-e2e-test.sh
@@ -5,33 +5,17 @@ set -euo pipefail
 
 source ci/scripts/common.env.sh
 
-while getopts 'p:' opt; do
-    case ${opt} in
-        p )
-            profile=$OPTARG
-            ;;
-        \? )
-            echo "Invalid Option: -$OPTARG" 1>&2
-            exit 1
-            ;;
-        : )
-            echo "Invalid option: $OPTARG requires an argument" 1>&2
-            ;;
-    esac
-done
-shift $((OPTIND -1))
-
 echo "--- Generate RiseDev CI config"
 cp ci/risedev-components.ci.env risedev-components.user.env
 
 echo "--- Build deterministic simulation e2e test runner"
-timeout 5m cargo make sslt --profile "$profile" -- --help
+timeout 10m cargo make sslt --release -- --help
 
 echo "--- deterministic simulation e2e, ci-3cn-1fe, ddl"
-timeout 10s cargo make sslt --profile "$profile" -- './e2e_test/ddl/**/*.slt'
+timeout 10s cargo make sslt --release -- './e2e_test/ddl/**/*.slt'
 
 echo "--- deterministic simulation e2e, ci-3cn-1fe, streaming"
-timeout 5m cargo make sslt --profile "$profile" -- './e2e_test/streaming/**/*.slt'
+timeout 1m cargo make sslt --release -- './e2e_test/streaming/**/*.slt'
 
 echo "--- deterministic simulation e2e, ci-3cn-1fe, batch"
-timeout 3m cargo make sslt --profile "$profile" -- './e2e_test/batch/**/*.slt'
+timeout 30s cargo make sslt --release -- './e2e_test/batch/**/*.slt'
