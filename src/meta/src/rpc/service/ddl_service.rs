583f9c837b329d5b2736ee8cb1090e234b2759bc
st1page <1245835950@qq.com>
2022-08-12 14:39:08 +0800

    fix(index): create index name to lowercase

--- src/meta/src/rpc/service/ddl_service.rs
+++ src/meta/src/rpc/service/ddl_service.rs
@@ -321,7 +321,7 @@ where
         &self,
         request: Request<CreateIndexRequest>,
     ) -> Result<Response<CreateIndexResponse>, Status> {
-        self.ddl_lock.read().await;
+        let _ddl_lock = self.ddl_lock.read().await;
         self.env.idle_manager().record_activity();
 
         let req = request.into_inner();
@@ -347,7 +347,7 @@ where
         &self,
         request: Request<DropIndexRequest>,
     ) -> Result<Response<DropIndexResponse>, Status> {
-        self.ddl_lock.read().await;
+        let _ddl_lock = self.ddl_lock.read().await;
         use risingwave_common::catalog::TableId;
 
         self.env.idle_manager().record_activity();
