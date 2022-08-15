d4448a3672e4617135a5d789a4fa3a8c0879b950
nanderstabel <nanderstabel@gmail.com>
2022-08-15 15:28:42 +0200

    test

--- ci/scripts/e2e-sink-test.sh
+++ ci/scripts/e2e-sink-test.sh
@@ -41,8 +41,10 @@ cargo make link-all-in-one-binaries
 
 echo "debug"
 cat /etc/hosts
-apt install ufw -y
-ufw status verbose
+whoami
+# apt install ufw -y
+# ufw status verbose
+
 
 echo "--- e2e test w/ Rust frontend - sink with mysql"
 cargo make clean-data
