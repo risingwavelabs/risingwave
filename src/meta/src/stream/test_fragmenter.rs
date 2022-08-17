8b5adbf6098bbeb27bab5f0aadb386bf0bc066b7
August <pin@singularity-data.com>
2022-08-17 20:25:01 +0800

    fix

--- src/meta/src/stream/test_fragmenter.rs
+++ src/meta/src/stream/test_fragmenter.rs
@@ -144,7 +144,7 @@ fn make_stream_fragments() -> Vec<StreamFragment> {
         fragment_type: FragmentType::Source as i32,
         is_singleton: false,
         table_ids_cnt: 0,
-        dependent_table_id: 0
+        dependent_table_id: 0,
     });
 
     // exchange node
@@ -217,7 +217,7 @@ fn make_stream_fragments() -> Vec<StreamFragment> {
         fragment_type: FragmentType::Others as i32,
         is_singleton: false,
         table_ids_cnt: 0,
-        dependent_table_id: 0
+        dependent_table_id: 0,
     });
 
     // exchange node
@@ -305,7 +305,7 @@ fn make_stream_fragments() -> Vec<StreamFragment> {
         fragment_type: FragmentType::Sink as i32,
         is_singleton: true,
         table_ids_cnt: 0,
-        dependent_table_id: 0
+        dependent_table_id: 0,
     });
 
     fragments
