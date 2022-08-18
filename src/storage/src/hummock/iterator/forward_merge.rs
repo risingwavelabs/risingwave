2e342211f8655b6c049f26c381ac56737f2ac79f
MrCroxx <mrcroxx@outlook.com>
2022-08-18 09:13:38 +0000

    fix(metrics): fix iterator metrics data report duplication and lost

--- src/storage/src/hummock/iterator/forward_merge.rs
+++ src/storage/src/hummock/iterator/forward_merge.rs
@@ -30,7 +30,6 @@ mod test {
     };
     use crate::hummock::test_utils::{create_small_table_cache, gen_test_sstable};
     use crate::hummock::value::HummockValue;
-    use crate::monitor::StateStoreMetrics;
 
     #[tokio::test]
     async fn test_merge_basic() {
@@ -40,7 +39,6 @@ mod test {
             OrderedMergeIteratorInner<SstableIterator>,
         > = HummockIteratorUnion::First(UnorderedMergeIteratorInner::new(
             gen_merge_iterator_interleave_test_sstable_iters(TEST_KEYS_COUNT, 3).await,
-            Arc::new(StateStoreMetrics::unused()),
         ));
         let mut ordered_iter: HummockIteratorUnion<
             Forward,
@@ -48,7 +46,6 @@ mod test {
             OrderedMergeIteratorInner<SstableIterator>,
         > = HummockIteratorUnion::Second(OrderedMergeIteratorInner::new(
             gen_merge_iterator_interleave_test_sstable_iters(TEST_KEYS_COUNT, 3).await,
-            Arc::new(StateStoreMetrics::unused()),
         ));
 
         // Test both ordered and unordered iterators
@@ -83,7 +80,6 @@ mod test {
             OrderedMergeIteratorInner<SstableIterator>,
         > = HummockIteratorUnion::First(UnorderedMergeIteratorInner::new(
             gen_merge_iterator_interleave_test_sstable_iters(TEST_KEYS_COUNT, 3).await,
-            Arc::new(StateStoreMetrics::unused()),
         ));
         let mut ordered_iter: HummockIteratorUnion<
             Forward,
@@ -91,7 +87,6 @@ mod test {
             OrderedMergeIteratorInner<SstableIterator>,
         > = HummockIteratorUnion::Second(OrderedMergeIteratorInner::new(
             gen_merge_iterator_interleave_test_sstable_iters(TEST_KEYS_COUNT, 3).await,
-            Arc::new(StateStoreMetrics::unused()),
         ));
 
         // Test both ordered and unordered iterators
@@ -170,40 +165,34 @@ mod test {
             Forward,
             UnorderedMergeIteratorInner<SstableIterator>,
             OrderedMergeIteratorInner<SstableIterator>,
-        > = HummockIteratorUnion::First(UnorderedMergeIteratorInner::new(
-            vec![
-                SstableIterator::create(
-                    cache.insert(table0.id, table0.id, 1, table0),
-                    sstable_store.clone(),
-                    read_options.clone(),
-                ),
-                SstableIterator::create(
-                    cache.insert(table1.id, table1.id, 1, table1),
-                    sstable_store.clone(),
-                    read_options.clone(),
-                ),
-            ],
-            Arc::new(StateStoreMetrics::unused()),
-        ));
+        > = HummockIteratorUnion::First(UnorderedMergeIteratorInner::new(vec![
+            SstableIterator::create(
+                cache.insert(table0.id, table0.id, 1, table0),
+                sstable_store.clone(),
+                read_options.clone(),
+            ),
+            SstableIterator::create(
+                cache.insert(table1.id, table1.id, 1, table1),
+                sstable_store.clone(),
+                read_options.clone(),
+            ),
+        ]));
         let mut ordered_iter: HummockIteratorUnion<
             Forward,
             UnorderedMergeIteratorInner<SstableIterator>,
             OrderedMergeIteratorInner<SstableIterator>,
-        > = HummockIteratorUnion::Second(OrderedMergeIteratorInner::new(
-            vec![
-                SstableIterator::create(
-                    cache.lookup(0, &0).unwrap(),
-                    sstable_store.clone(),
-                    read_options.clone(),
-                ),
-                SstableIterator::create(
-                    cache.lookup(1, &1).unwrap(),
-                    sstable_store.clone(),
-                    read_options.clone(),
-                ),
-            ],
-            Arc::new(StateStoreMetrics::unused()),
-        ));
+        > = HummockIteratorUnion::Second(OrderedMergeIteratorInner::new(vec![
+            SstableIterator::create(
+                cache.lookup(0, &0).unwrap(),
+                sstable_store.clone(),
+                read_options.clone(),
+            ),
+            SstableIterator::create(
+                cache.lookup(1, &1).unwrap(),
+                sstable_store.clone(),
+                read_options.clone(),
+            ),
+        ]));
 
         // Test both ordered and unordered iterators
         let test_iters = vec![&mut unordered_iter, &mut ordered_iter];
@@ -278,41 +267,38 @@ mod test {
         );
         let cache = create_small_table_cache();
 
-        let mut iter = OrderedMergeIteratorInner::new(
-            vec![
-                SstableIterator::create(
-                    cache.insert(
-                        non_overlapped_sstable.id,
-                        non_overlapped_sstable.id,
-                        1,
-                        non_overlapped_sstable,
-                    ),
-                    sstable_store.clone(),
-                    read_options.clone(),
+        let mut iter = OrderedMergeIteratorInner::new(vec![
+            SstableIterator::create(
+                cache.insert(
+                    non_overlapped_sstable.id,
+                    non_overlapped_sstable.id,
+                    1,
+                    non_overlapped_sstable,
                 ),
-                SstableIterator::create(
-                    cache.insert(
-                        overlapped_new_sstable.id,
-                        overlapped_new_sstable.id,
-                        1,
-                        overlapped_new_sstable,
-                    ),
-                    sstable_store.clone(),
-                    read_options.clone(),
+                sstable_store.clone(),
+                read_options.clone(),
+            ),
+            SstableIterator::create(
+                cache.insert(
+                    overlapped_new_sstable.id,
+                    overlapped_new_sstable.id,
+                    1,
+                    overlapped_new_sstable,
                 ),
-                SstableIterator::create(
-                    cache.insert(
-                        overlapped_old_sstable.id,
-                        overlapped_old_sstable.id,
-                        1,
-                        overlapped_old_sstable,
-                    ),
-                    sstable_store.clone(),
-                    read_options.clone(),
+                sstable_store.clone(),
+                read_options.clone(),
+            ),
+            SstableIterator::create(
+                cache.insert(
+                    overlapped_old_sstable.id,
+                    overlapped_old_sstable.id,
+                    1,
+                    overlapped_old_sstable,
                 ),
-            ],
-            Arc::new(StateStoreMetrics::unused()),
-        );
+                sstable_store.clone(),
+                read_options.clone(),
+            ),
+        ]);
 
         iter.rewind().await.unwrap();
 
