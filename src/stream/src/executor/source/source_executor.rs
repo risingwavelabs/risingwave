d4ed3ab2c4010c6b2b052d9c31a0f2a81f0bbe78
August <pin@singularity-data.com>
2022-08-18 22:38:48 +0800

    adjust ctx

--- src/stream/src/executor/source/source_executor.rs
+++ src/stream/src/executor/source/source_executor.rs
@@ -313,7 +313,7 @@ impl<S: StateStore> SourceExecutor<S> {
                                 // Update row id generator if vnode mapping is changed.
                                 // Note that: since update barrier will only occurs between pause
                                 // and resume barrier, duplicated row id won't be generated.
-                                if let Some(vnode_bitmaps) = vnode_bitmaps.get(&self.actor_id) {
+                                if let Some(vnode_bitmaps) = vnode_bitmaps.get(&self.ctx.id) {
                                     let vnode_id =
                                         vnode_bitmaps.next_set_bit(0).unwrap_or(0) as u32;
                                     if self.row_id_generator.vnode_id != vnode_id {
