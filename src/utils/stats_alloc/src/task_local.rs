3f3d872feeb150667422e9ca294eead533d346b6
TennyZhuang <zty0826@gmail.com>
2022-08-23 15:55:52 +0800

    fix realloc
    Signed-off-by: TennyZhuang <zty0826@gmail.com>

--- src/utils/stats_alloc/src/task_local.rs
+++ src/utils/stats_alloc/src/task_local.rs
@@ -43,14 +43,12 @@ unsafe impl GlobalAlloc for TaskLocalAlloc {
             .try_with(|bytes| {
                 bytes.0.as_ref().fetch_add(layout.size(), Ordering::Relaxed);
                 let ptr = GLOBAL_ALLOC.alloc(new_layout);
-                dbg!(bytes.0.as_ptr() as usize, ptr);
                 *(ptr as *mut usize) = bytes.0.as_ptr() as usize;
                 let ptr = ptr.add(usize::BITS as usize);
                 ptr
             })
             .unwrap_or_else(|_| {
                 let ptr = GLOBAL_ALLOC.alloc(new_layout);
-                dbg!((1, ptr));
                 *(ptr as *mut usize) = 0;
                 let ptr = ptr.add(usize::BITS as usize);
                 ptr
@@ -61,7 +59,6 @@ unsafe impl GlobalAlloc for TaskLocalAlloc {
         let new_layout =
             Layout::from_size_align_unchecked(layout.size() + usize::BITS as usize, layout.align());
         let ptr = ptr.sub(usize::BITS as usize);
-        dbg!((2, ptr));
         let bytes = (*(ptr as *const usize)) as *const AtomicUsize;
         if let Some(bytes) = bytes.as_ref() {
             bytes.fetch_sub(layout.size(), Ordering::Relaxed);
@@ -77,7 +74,6 @@ unsafe impl GlobalAlloc for TaskLocalAlloc {
                 bytes.0.as_ref().fetch_add(layout.size(), Ordering::Relaxed);
                 let ptr = GLOBAL_ALLOC.alloc_zeroed(new_layout);
                 *(ptr as *mut usize) = bytes.0.as_ptr() as usize;
-                dbg!(bytes.0.as_ptr() as usize, ptr);
                 let ptr = ptr.add(usize::BITS as usize);
                 ptr
             })
@@ -98,7 +94,14 @@ unsafe impl GlobalAlloc for TaskLocalAlloc {
             bytes.fetch_sub(layout.size(), Ordering::Relaxed);
         }
         let new_size = new_size + usize::BITS as usize;
-        GLOBAL_ALLOC.realloc(ptr, new_layout, new_size)
+        let ptr = GLOBAL_ALLOC.realloc(ptr, new_layout, new_size);
+        if ptr.is_null() {
+            ptr
+        } else {
+            *(ptr as *mut usize) = bytes as usize;
+            let ptr = ptr.add(usize::BITS as usize);
+            ptr
+        }
     }
 }
 
