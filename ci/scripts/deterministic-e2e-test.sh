24f74bc5be4296995fc67fb7ce6394d594308877
Runji Wang <wangrunji0408@163.com>
2022-08-24 16:15:27 +0800

    ci: disable log for recovery test
    Signed-off-by: Runji Wang <wangrunji0408@163.com>

--- ci/scripts/deterministic-e2e-test.sh
+++ ci/scripts/deterministic-e2e-test.sh
@@ -27,8 +27,8 @@ echo "--- deterministic simulation e2e, ci-3cn-2fe, parallel, batch"
 timeout 30s cargo make sslt --profile ci-release -- -j 16 './e2e_test/batch/**/*.slt'
 
 # bugs here!
-# echo "--- deterministic simulation e2e, ci-3cn-1fe, recovery, streaming"
-# timeout 2m cargo make sslt --profile ci-release -- --kill-node './e2e_test/streaming/**/*.slt'
+echo "--- deterministic simulation e2e, ci-3cn-1fe, recovery, streaming"
+RUST_LOG=off timeout 2m cargo make sslt --profile ci-release -- --kill-node './e2e_test/streaming/**/*.slt' || true
 
 echo "--- deterministic simulation e2e, ci-3cn-1fe, recovery, batch"
-timeout 2m cargo make sslt --profile ci-release -- --kill-node './e2e_test/batch/**/*.slt'
+RUST_LOG=off timeout 2m cargo make sslt --profile ci-release -- --kill-node './e2e_test/batch/**/*.slt'
