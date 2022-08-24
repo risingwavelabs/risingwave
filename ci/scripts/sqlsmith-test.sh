75f547470b93add1444911aa8f094b1775af474b
Noel Kwan <noelkwan1998@gmail.com>
2022-08-24 14:03:08 +0800

    setup cluster for sqlsmith

--- ci/scripts/sqlsmith-test.sh
+++ ci/scripts/sqlsmith-test.sh
@@ -17,13 +17,33 @@ while getopts 'p:' opt; do
 done
 shift $((OPTIND -1))
 
-echo "+++ Setup env"
+echo "--- Setup env vars"
 source ci/scripts/common.env.sh
 
-echo "+++ Run sqlsmith frontend tests"
+echo "--- Run Sqlsmith frontend tests"
 NEXTEST_PROFILE=ci cargo nextest run run_sqlsmith_on_frontend --features "failpoints enable_sqlsmith_unit_test" 2> >(tee);
 
-echo "+++ e2e, ci-3cn-1fe, fuzzing"
+echo "--- Setup Sqlsmith E2E test env"
+
+echo "--- Download artifacts"
+mkdir -p target/debug
+buildkite-agent artifact download risingwave-"$profile" target/debug/
+buildkite-agent artifact download risedev-dev-"$profile" target/debug/
+mv target/debug/risingwave-"$profile" target/debug/risingwave
+mv target/debug/risedev-dev-"$profile" target/debug/risedev-dev
+
+echo "--- Adjust permission"
+chmod +x ./target/debug/risingwave
+chmod +x ./target/debug/risedev-dev
+
+echo "--- Generate RiseDev CI config"
+cp ci/risedev-components.ci.env risedev-components.user.env
+
+echo "--- Prepare RiseDev dev cluster"
+cargo make pre-start-dev
+cargo make link-all-in-one-binaries
+
+echo "--- e2e, ci-3cn-1fe, Sqlsmith"
 buildkite-agent artifact download sqlsmith-"$profile" target/debug/
 mv target/debug/sqlsmith-"$profile" target/debug/sqlsmith
 chmod +x ./target/debug/sqlsmith
@@ -31,7 +51,7 @@ chmod +x ./target/debug/sqlsmith
 cargo make ci-start ci-3cn-1fe
 timeout 20m ./target/debug/sqlsmith test --testdata ./src/tests/sqlsmith/tests/testdata
 
-echo "+++ Kill cluster"
+echo "--- Kill cluster"
 # Using `kill` instead of `ci-kill` avoids storing excess logs.
 # If there's errors, the failing query will be printed to stderr.
 # We can use that to reproduce logs on local machine.
