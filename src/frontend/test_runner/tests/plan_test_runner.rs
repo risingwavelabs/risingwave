ed703efb38d81efd5afc9a918e03c246e695c7d5
Noel Kwan <noelkwan1998@gmail.com>
2022-08-24 17:05:36 +0800

    propagate errors

--- src/frontend/test_runner/tests/plan_test_runner.rs
+++ src/frontend/test_runner/tests/plan_test_runner.rs
@@ -55,7 +55,7 @@ fn main() {
                     .join(file_name);
 
                 let file_content = std::fs::read_to_string(path).unwrap();
-                build_runtime().block_on(run_test_file(&test_case_name, &file_content));
+                build_runtime().block_on(run_test_file(&test_case_name, &file_content))?;
                 Ok(())
             }));
         }
