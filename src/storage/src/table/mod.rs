6ee20f9adc4dd84156f68871afcd02f747e4b0c8
congyi <15605187270@163.com>
2022-08-24 16:47:01 +0800

    remove state table

--- src/storage/src/table/mod.rs
+++ src/storage/src/table/mod.rs
@@ -12,8 +12,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-pub mod mem_table;
-pub mod state_table;
 pub mod storage_table;
 
 #[cfg(test)]
