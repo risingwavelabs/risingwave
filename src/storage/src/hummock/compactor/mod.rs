2c62ae9528ad63e222f68a136d91032b123f1992
zwang28 <70626450+zwang28@users.noreply.github.com>
2022-08-25 16:51:39 +0800

    feat(test): support sync point for integration test (#4855)
    * feat(test): support sync point in test and add demo tests.
    
    * add comments
    
    * add comments
    
    * move sync to dedicated folder
    
    * add license header
    
    * rename sync* to sync_point*
    
    Co-authored-by: mergify[bot] <37929162+mergify[bot]@users.noreply.github.com>

--- src/storage/src/hummock/compactor/mod.rs
+++ src/storage/src/hummock/compactor/mod.rs
@@ -37,6 +37,7 @@ use futures::{stream, FutureExt, StreamExt};
 pub use iterator::ConcatSstableIterator;
 use itertools::Itertools;
 use risingwave_common::config::constant::hummock::CompactionFilterFlag;
+use risingwave_common::util::sync_point::on_sync_point;
 use risingwave_hummock_sdk::compact::compact_task_to_string;
 use risingwave_hummock_sdk::filter_key_extractor::FilterKeyExtractorImpl;
 use risingwave_hummock_sdk::key::{get_epoch, FullKey};
@@ -255,6 +256,7 @@ impl Compactor {
         // Sort by split/key range index.
         output_ssts.sort_by_key(|(split_index, _)| *split_index);
 
+        on_sync_point("BEFORE_COMPACT_REPORT").await.unwrap();
         // After a compaction is done, mutate the compaction task.
         Self::compact_done(
             &mut compact_task,
@@ -263,6 +265,7 @@ impl Compactor {
             compact_success,
         )
         .await;
+        on_sync_point("AFTER_COMPACT_REPORT").await.unwrap();
         let cost_time = timer.stop_and_record() * 1000.0;
         tracing::info!(
             "Finished compaction task in {:?}ms: \n{}",
