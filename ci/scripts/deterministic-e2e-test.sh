ec56f35ddcfbfab354a63fcd97360e2b49ce4be3
Runji Wang <wangrunji0408@163.com>
2022-08-25 18:05:59 +0800

    ci: run 10 times for each deterministic test
    Signed-off-by: Runji Wang <wangrunji0408@163.com>

--- ci/scripts/deterministic-e2e-test.sh
+++ ci/scripts/deterministic-e2e-test.sh
@@ -11,24 +11,26 @@ cp ci/risedev-components.ci.env risedev-components.user.env
 echo "--- Build deterministic simulation e2e test runner"
 timeout 10m cargo make sslt --profile ci-release -- --help
 
+export RUNNER=./target/sim/ci-release/risingwave_simulation
+
 echo "--- deterministic simulation e2e, ci-3cn-1fe, ddl"
-timeout 10s cargo make sslt --profile ci-release -- './e2e_test/ddl/**/*.slt'
+seq 10 | timeout 10s parallel MADSIM_TEST_SEED={} $RUNNER './e2e_test/ddl/**/*.slt'
 
 echo "--- deterministic simulation e2e, ci-3cn-1fe, streaming"
-timeout 1m cargo make sslt --profile ci-release -- './e2e_test/streaming/**/*.slt'
+seq 10 | timeout 1m parallel MADSIM_TEST_SEED={} $RUNNER './e2e_test/streaming/**/*.slt'
 
 echo "--- deterministic simulation e2e, ci-3cn-1fe, batch"
-timeout 30s cargo make sslt --profile ci-release -- './e2e_test/batch/**/*.slt'
+seq 10 | timeout 30s parallel MADSIM_TEST_SEED={} $RUNNER './e2e_test/batch/**/*.slt'
 
 echo "--- deterministic simulation e2e, ci-3cn-2fe, parallel, streaming"
-timeout 90s cargo make sslt --profile ci-release -- -j 16 './e2e_test/streaming/**/*.slt'
+seq 10 | timeout 90s parallel MADSIM_TEST_SEED={} $RUNNER -j 16 './e2e_test/streaming/**/*.slt'
 
 echo "--- deterministic simulation e2e, ci-3cn-2fe, parallel, batch"
-timeout 30s cargo make sslt --profile ci-release -- -j 16 './e2e_test/batch/**/*.slt'
+seq 10 | timeout 30s parallel MADSIM_TEST_SEED={} $RUNNER -j 16 './e2e_test/batch/**/*.slt'
 
 # bugs here!
 echo "--- deterministic simulation e2e, ci-3cn-1fe, recovery, streaming"
-RUST_LOG=off timeout 2m cargo make sslt --profile ci-release -- --kill-node './e2e_test/streaming/**/*.slt' || true
+seq 10 | timeout 2m parallel MADSIM_TEST_SEED={} RUST_LOG=off $RUNNER --kill-node './e2e_test/streaming/**/*.slt' || true
 
 echo "--- deterministic simulation e2e, ci-3cn-1fe, recovery, batch"
-RUST_LOG=off timeout 2m cargo make sslt --profile ci-release -- --kill-node './e2e_test/batch/**/*.slt'
+seq 10 | timeout 2m parallel MADSIM_TEST_SEED={} RUST_LOG=off $RUNNER --kill-node './e2e_test/batch/**/*.slt'
