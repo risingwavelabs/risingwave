9940f9a27684eccea96700feea6a7b90355eb3b0
Dylan Chen <zilin@singularity-data.com>
2022-08-25 20:43:37 +0800

    fmt

--- src/frontend/src/utils/condition.rs
+++ src/frontend/src/utils/condition.rs
@@ -12,7 +12,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-use std::cmp::min;
 use std::collections::{HashMap, HashSet};
 use std::fmt::{self, Debug, Display};
 use std::ops::Bound;
