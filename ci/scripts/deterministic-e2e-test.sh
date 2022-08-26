13dbd576c0fd1a1836e79666e78b9a7f1651d530
Runji Wang <wangrunji0408@163.com>
2022-08-26 22:43:17 +0800

    ci: use global timeout
    Signed-off-by: Runji Wang <wangrunji0408@163.com>

--- ci/scripts/deterministic-e2e-test.sh
+++ ci/scripts/deterministic-e2e-test.sh
@@ -11,29 +11,29 @@ echo "--- Generate RiseDev CI config"
 cp ci/risedev-components.ci.env risedev-components.user.env
 
 echo "--- Build deterministic simulation e2e test runner"
-timeout 10m cargo make sslt --profile ci-release -- --help
+cargo make sslt --profile ci-release -- --help
 
 export RUNNER=./target/sim/ci-release/risingwave_simulation
 export RUST_LOG=off
 
 echo "--- deterministic simulation e2e, ci-3cn-1fe, ddl"
-seq 10 | timeout 10s parallel MADSIM_TEST_SEED={} $RUNNER './e2e_test/ddl/\*\*/\*.slt'
+seq 1 | parallel MADSIM_TEST_SEED={} $RUNNER './e2e_test/ddl/\*\*/\*.slt'
 
 echo "--- deterministic simulation e2e, ci-3cn-1fe, streaming"
-seq 1 | timeout 1m parallel MADSIM_TEST_SEED={} $RUNNER './e2e_test/streaming/\*\*/\*.slt'
+seq 1 | parallel MADSIM_TEST_SEED={} $RUNNER './e2e_test/streaming/\*\*/\*.slt'
 
 echo "--- deterministic simulation e2e, ci-3cn-1fe, batch"
-seq 1 | timeout 30s parallel MADSIM_TEST_SEED={} $RUNNER './e2e_test/batch/\*\*/\*.slt'
+seq 1 | parallel MADSIM_TEST_SEED={} $RUNNER './e2e_test/batch/\*\*/\*.slt'
 
 echo "--- deterministic simulation e2e, ci-3cn-2fe, parallel, streaming"
-seq 1 | timeout 2m parallel MADSIM_TEST_SEED={} $RUNNER -j 16 './e2e_test/streaming/\*\*/\*.slt'
+seq 5 | parallel MADSIM_TEST_SEED={} $RUNNER -j 16 './e2e_test/streaming/\*\*/\*.slt'
 
 echo "--- deterministic simulation e2e, ci-3cn-2fe, parallel, batch"
-seq 1 | timeout 30s parallel MADSIM_TEST_SEED={} $RUNNER -j 16 './e2e_test/batch/\*\*/\*.slt'
+seq 5 | parallel MADSIM_TEST_SEED={} $RUNNER -j 16 './e2e_test/batch/\*\*/\*.slt'
 
 # bugs here!
 echo "--- deterministic simulation e2e, ci-3cn-1fe, recovery, streaming"
-seq 1 | timeout 2m parallel MADSIM_TEST_SEED={} $RUNNER --kill-node './e2e_test/streaming/\*\*/\*.slt' || true
+seq 1 | parallel MADSIM_TEST_SEED={} $RUNNER --kill-node './e2e_test/streaming/\*\*/\*.slt' || true
 
 echo "--- deterministic simulation e2e, ci-3cn-1fe, recovery, batch"
-seq 1 | timeout 2m parallel MADSIM_TEST_SEED={} $RUNNER --kill-node './e2e_test/batch/\*\*/\*.slt'
+seq 1 | parallel MADSIM_TEST_SEED={} $RUNNER --kill-node './e2e_test/batch/\*\*/\*.slt'
