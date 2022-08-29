3968ee509ac5a8af226ad0f0ea9b0c93f683604d
Zehua Zou <41586196+HuaHuaY@users.noreply.github.com>
2022-08-29 19:37:19 +0800

    fix e2e

--- src/frontend/src/scheduler/distributed/stage.rs
+++ src/frontend/src/scheduler/distributed/stage.rs
@@ -649,8 +649,15 @@ impl StageRunner {
                             .as_ref()
                             .expect("no side table desc");
                         node.probe_side_vnode_mapping = self
-                            .worker_node_manager
-                            .get_fragment_mapping(&side_table_desc.table_id)
+                            .catalog_reader
+                            .read_guard()
+                            .get_table_by_id(&side_table_desc.table_id.into())
+                            .map(|table| {
+                                self.worker_node_manager
+                                    .get_fragment_mapping(&table.fragment_id)
+                            })
+                            .ok()
+                            .flatten()
                             .unwrap_or_default();
                         node.worker_nodes = self.worker_node_manager.list_worker_nodes();
                     }
