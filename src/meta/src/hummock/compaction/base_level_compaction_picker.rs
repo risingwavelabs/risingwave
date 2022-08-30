9231805bc9dd6a4ae4809d93ec36bb095b9d4974
Little-Wallace <bupt2013211450@gmail.com>
2022-08-30 15:41:46 +0800

    fix test
    Signed-off-by: Little-Wallace <bupt2013211450@gmail.com>

--- src/meta/src/hummock/compaction/base_level_compaction_picker.rs
+++ src/meta/src/hummock/compaction/base_level_compaction_picker.rs
@@ -285,8 +285,8 @@ pub mod tests {
         push_table_level0(&mut levels, generate_table(9, 1, 100, 400, 3));
         let ret = picker.pick_compaction(&levels, &levels_handler).unwrap();
         assert_eq!(ret.input_levels[0].table_infos.len(), 1);
-        assert_eq!(ret.input_levels[0].table_infos[0].id, 8);
-        assert_eq!(ret.input_levels[1].table_infos[0].id, 9);
+        assert_eq!(ret.input_levels[0].table_infos[0].id, 9);
+        assert_eq!(ret.input_levels[1].table_infos[0].id, 8);
         ret.add_pending_task(2, &mut levels_handler);
 
         levels_handler[0].remove_task(1);
@@ -307,10 +307,10 @@ pub mod tests {
         push_table_level0(&mut levels, generate_table(13, 1, 100, 400, 4));
         let ret = picker.pick_compaction(&levels, &levels_handler).unwrap();
         assert_eq!(ret.input_levels.len(), 4);
-        assert_eq!(ret.input_levels[0].table_infos[0].id, 5);
-        assert_eq!(ret.input_levels[1].table_infos[0].id, 6);
-        assert_eq!(ret.input_levels[2].table_infos.len(), 3);
-        assert_eq!(ret.input_levels[3].table_infos.len(), 1);
+        assert_eq!(ret.input_levels[0].table_infos[0].id, 13);
+        assert_eq!(ret.input_levels[1].table_infos.len(), 3);
+        assert_eq!(ret.input_levels[2].table_infos[0].id, 6);
+        assert_eq!(ret.input_levels[3].table_infos[0].id, 5);
     }
 
     #[test]
@@ -424,11 +424,11 @@ pub mod tests {
                 level_idx: 1,
                 level_type: LevelType::Nonoverlapping as i32,
                 table_infos: vec![generate_table(2, 1, 150, 300, 2)],
-                total_file_size: 0,
+                total_file_size: 150,
                 sub_level_id: 0,
             }],
             l0: Some(generate_l0_with_overlap(vec![generate_table(
-                1, 1, 200, 250, 2,
+                1, 1, 160, 280, 2,
             )])),
         };
 
@@ -440,11 +440,12 @@ pub mod tests {
 
         levels.l0.as_mut().unwrap().sub_levels[0].table_infos = vec![
             generate_table(3, 1, 100, 140, 3),
-            generate_table(1, 1, 200, 250, 2),
-            generate_table(4, 1, 400, 500, 3),
+            generate_table(1, 1, 160, 280, 2),
+            generate_table(5, 1, 290, 500, 3),
         ];
 
         let ret = picker.pick_compaction(&levels, &levels_handler).unwrap();
+        ret.add_pending_task(1, &mut levels_handler);
 
         // Will be trivial move. The second file can not be picked up because the range of files
         // [3,4] would be overlap with file [0]
@@ -458,6 +459,8 @@ pub mod tests {
                 .collect_vec(),
             vec![3]
         );
+        let ret = picker.pick_compaction(&levels, &levels_handler);
+        assert!(ret.is_none());
     }
 
     #[test]
@@ -501,7 +504,7 @@ pub mod tests {
                     generate_table(2, 1, 200, 500, 2),
                     generate_table(3, 1, 510, 600, 2),
                 ],
-                total_file_size: 0,
+                total_file_size: 590,
                 sub_level_id: 0,
             }],
             l0: Some(generate_l0_with_overlap(vec![])),
@@ -509,17 +512,17 @@ pub mod tests {
         push_tables_level0(
             &mut levels,
             vec![
-                generate_table(4, 1, 130, 180, 2),
+                generate_table(4, 1, 100, 180, 2),
                 generate_table(5, 1, 190, 250, 2),
-                generate_table(6, 1, 260, 300, 2),
+                generate_table(6, 1, 260, 400, 2),
             ],
         );
         push_tables_level0(
             &mut levels,
             vec![
-                generate_table(7, 1, 130, 180, 2),
+                generate_table(7, 1, 100, 180, 2),
                 generate_table(8, 1, 190, 250, 2),
-                generate_table(9, 1, 260, 300, 2),
+                generate_table(9, 1, 260, 400, 2),
             ],
         );
         let levels_handler = vec![LevelHandler::new(0), LevelHandler::new(1)];
