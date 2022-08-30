beb75089d7601cd3c9c1d5789a5c4572797506cd
Shmiwy <wyf000219@126.com>
2022-08-30 10:40:29 +0800

    featur(stream): generate 2-phase top n rule.
    Signed-off-by: Shmiwy <wyf000219@126.com>

--- src/frontend/src/optimizer/plan_node/stream_group_topn.rs
+++ src/frontend/src/optimizer/plan_node/stream_group_topn.rs
@@ -140,7 +140,7 @@ impl fmt::Display for StreamGroupTopN {
             &format!(
                 "{}",
                 OrderDisplay {
-                    order: self.order(),
+                    order: &self.order,
                     input_schema
                 }
             ),
