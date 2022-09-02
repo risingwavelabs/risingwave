0d298e86383dafbef10f9419fb51be1b624552f2
MrCroxx <mrcroxx@outlook.com>
2022-09-02 16:28:21 +0800

    fix validator iter local stats

--- src/storage/src/hummock/validator.rs
+++ src/storage/src/hummock/validator.rs
@@ -102,5 +102,6 @@ pub async fn validate_ssts(task: ValidationTask, sstable_store: SstableStoreRef)
             sst_id,
             task.epoch
         );
+        iter.collect_local_statistic(&mut unused);
     }
 }
