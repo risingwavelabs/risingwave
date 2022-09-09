abf7b5fef7d83ae777415064128cd9af15e6402c
Richard Chien <stdrc@outlook.com>
2022-09-09 18:34:31 +0800

    decrease the timeout
    Signed-off-by: Richard Chien <stdrc@outlook.com>

--- ci/scripts/run-e2e-test.sh
+++ ci/scripts/run-e2e-test.sh
@@ -64,7 +64,7 @@ cargo make ci-kill
 
 echo "--- e2e, ci-3cn-1fe, generated"
 cargo make ci-start ci-3cn-1fe
-timeout 2m sqllogictest -p 4566 -d dev -e postgres-extended './e2e_test/generated/**/*.slt'
+timeout 30s sqllogictest -p 4566 -d dev -e postgres-extended './e2e_test/generated/**/*.slt'
 
 echo "--- Kill cluster"
 cargo make ci-kill
