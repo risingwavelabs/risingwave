1635c83c7c3667d9fa9dc2fc427fbb2ce2389659
nanderstabel <nanderstabel@gmail.com>
2022-09-09 10:57:26 +0200

    add

--- simple-docker/scripts/simple-db.sql
+++ simple-docker/scripts/simple-db.sql
@@ -1,11 +1,23 @@
 
+-- CREATE TABLE IF NOT EXISTS t
+-- (
+--     id bigint NOT NULL,
+--     status character varying(25) COLLATE pg_catalog."default",
+--     CONSTRAINT t_pkey PRIMARY KEY (id)
+-- );
+
+
+-- INSERT INTO t values (1, 'COMPLETED');
+-- INSERT INTO t values (2, 'COMPLETED');
+-- INSERT INTO t values (3, 'PROCESSING');
+
 CREATE TABLE IF NOT EXISTS t
 (
     id bigint NOT NULL,
-    status character varying(25) COLLATE pg_catalog."default",
     CONSTRAINT t_pkey PRIMARY KEY (id)
 );
 
-INSERT INTO t values (1, 'PROCESSING');
--- INSERT INTO t values (2, 'PROCESSING');
--- UPDATE t SET status='COMPLETED' WHERE id=1;
+
+INSERT INTO t values (1);
+INSERT INTO t values (2);
+INSERT INTO t values (3);
