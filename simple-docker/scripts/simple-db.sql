4f497c8e5ac7a5a9f2f3a4f0ebf7ead07da70996
nanderstabel <nanderstabel@gmail.com>
2022-09-09 20:23:23 +0200

    remove tpcc and simple-docker

--- simple-docker/scripts/simple-db.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE IF NOT EXISTS t
-(
-    id bigint NOT NULL,
-    status character varying(25) COLLATE pg_catalog."default",
-    CONSTRAINT t_pkey PRIMARY KEY (id)
-);
-
-INSERT INTO t values (1, 'PROCESSING');
