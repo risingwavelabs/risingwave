926bbc3aab92b2a82b71d72d7b4c53e617036abc
Richard Chien <stdrc@outlook.com>
2022-09-13 21:38:42 +0800

    fix ci e2e tests
    Signed-off-by: Richard Chien <stdrc@outlook.com>

--- ci/scripts/run-e2e-test.sh
+++ ci/scripts/run-e2e-test.sh
@@ -55,16 +55,16 @@ sqllogictest -p 4566 -d test './e2e_test/database/test.slt'
 echo "--- Kill cluster"
 cargo make ci-kill
 
-echo "--- e2e, ci-3cn-1fe, extended query"
+echo "--- e2e, ci-3cn-1fe, generated"
 cargo make ci-start ci-3cn-1fe
-sqllogictest -p 4566 -d dev -e postgres-extended './e2e_test/extended_query/**/*.slt'
+sqllogictest -p 4566 -d dev './e2e_test/generated/**/*.slt' --junit "generated-${profile}"
 
 echo "--- Kill cluster"
 cargo make ci-kill
 
-echo "--- e2e, ci-3cn-1fe, generated"
+echo "--- e2e, ci-3cn-1fe, extended query"
 cargo make ci-start ci-3cn-1fe
-sqllogictest -p 4566 -d dev -e postgres-extended './e2e_test/generated/**/*.slt'
+sqllogictest -p 4566 -d dev -e postgres-extended './e2e_test/extended_query/**/*.slt'
 
 echo "--- Kill cluster"
 cargo make ci-kill
