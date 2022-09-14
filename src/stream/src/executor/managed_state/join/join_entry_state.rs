33923ad361714feb6644a927aa8772023730ebd4
congyi <15605187270@163.com>
2022-09-14 14:08:29 +0800

    fix ut

--- src/stream/src/executor/managed_state/join/join_entry_state.rs
+++ src/stream/src/executor/managed_state/join/join_entry_state.rs
@@ -134,7 +134,7 @@ mod tests {
 
         for row_ref in data_chunk.rows() {
             let row: Row = row_ref.into();
-            let value_indices = (0..row.0.len()).collect_vec();
+            let value_indices = (0..row.0.len() - 1).collect_vec();
             let pk = pk_indices.iter().map(|idx| row[*idx].clone()).collect_vec();
             // Pk is only a `i64` here, so encoding method does not matter.
             let pk = Row(pk).serialize(&value_indices);
