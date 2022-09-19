3aa48aaeb10df4dd3817f7bf0a974481d5926cf6
jon-chuang <jon-chuang@users.noreply.github.com>
2022-09-19 16:41:56 +0800

    fix build scripts

--- ci/scripts/common.env.sh
+++ ci/scripts/common.env.sh
@@ -1,5 +1,5 @@
 export CARGO_TERM_COLOR=always
-export RUSTFLAGS="-D warnings -C --target-cpu=native --cfg tokio_unstable"
+export RUSTFLAGS="-D warnings -Ctarget-cpu=native --cfg tokio_unstable"
 export PROTOC_NO_VENDOR=true
 export CARGO_HOME=/risingwave/.cargo
 export RISINGWAVE_CI=true
