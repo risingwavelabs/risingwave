73d0f8b0fa30603dfb70e40298234a74554d113f
st1page <1245835950@qq.com>
2022-09-20 18:06:19 +0800

    add explain table in stream fragment test

--- src/frontend/src/utils/stream_graph_formatter.rs
+++ src/frontend/src/utils/stream_graph_formatter.rs
@@ -12,12 +12,15 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-use std::collections::HashMap;
+use std::collections::{BTreeMap, HashMap};
 
 use itertools::Itertools;
 use risingwave_common::error::{ErrorCode, Result};
+use risingwave_pb::catalog::Table;
 use risingwave_pb::stream_plan::stream_fragment_graph::{StreamFragment, StreamFragmentEdge};
 use risingwave_pb::stream_plan::{stream_node, DispatcherType, StreamFragmentGraph, StreamNode};
+
+use crate::TableCatalog;
 
 pub fn explain_stream_graph(graph: &StreamFragmentGraph, is_verbose: bool) -> Result<String> {
     let mut output = String::new();
@@ -33,14 +36,22 @@ struct StreamGraphFormatter {
     /// exchange's operator_id -> edge
     edges: HashMap<u64, StreamFragmentEdge>,
     verbose: bool,
+    tables: BTreeMap<u32, Table>,
 }
 
 impl StreamGraphFormatter {
     fn new(verbose: bool) -> Self {
         StreamGraphFormatter {
             edges: HashMap::default(),
+            tables: BTreeMap::default(),
             verbose,
         }
+    }
+
+    /// collect the table catalog and return the table id
+    fn add_table(&mut self, tb: &Table) -> u32 {
+        self.tables.insert(tb.id, tb.clone());
+        tb.id
     }
 
     fn explain_graph(
@@ -56,7 +67,36 @@ impl StreamGraphFormatter {
         for (_, fragment) in graph.fragments.iter().sorted_by_key(|(id, _)| **id) {
             self.explain_fragment(fragment, f)?;
         }
+        let tbs = self.tables.clone();
+        for tb in tbs.values() {
+            self.explain_table(tb, f)?;
+        }
         Ok(())
+    }
+
+    fn explain_table(&mut self, tb: &Table, f: &mut impl std::fmt::Write) -> std::fmt::Result {
+        writeln!(f, "Table {}", tb.id)?;
+        let tb = TableCatalog::from(tb.clone());
+        writeln!(
+            f,
+            "  columns: [{}]",
+            tb.columns
+                .iter()
+                .map(|c| {
+                    if self.verbose {
+                        format!("{}:{}", c.name(), c.data_type())
+                    } else {
+                        c.name().to_string()
+                    }
+                })
+                .join(", ")
+        )?;
+        writeln!(f, "  primary key: {:?}", tb.order_key)?;
+        writeln!(f, "  values indices: {:?}", tb.value_indices)?;
+        if let Some(vnode_col_idx) = tb.vnode_col_idx {
+            writeln!(f, "  vnode column idx: {}", vnode_col_idx)?;
+        }
+        writeln!(f)
     }
 
     fn explain_fragment(
@@ -95,6 +135,69 @@ impl StreamGraphFormatter {
             _ => node.identity.clone(),
         };
         writeln!(f, "{}{}", " ".repeat(level * 2), one_line_explain)?;
+        let explain_table_oneline = match node.get_node_body().unwrap() {
+            stream_node::NodeBody::Materialize(node) => Some(format!(
+                "materialized table: {}",
+                self.add_table(node.get_table().unwrap())
+            )),
+            stream_node::NodeBody::GlobalSimpleAgg(node) => Some(format!(
+                "state tables: [{}]",
+                node.internal_tables
+                    .iter()
+                    .map(|tb| { self.add_table(tb) })
+                    .join(", ")
+            )),
+            stream_node::NodeBody::HashAgg(node) => Some(format!(
+                "state tables: [{}]",
+                node.internal_tables
+                    .iter()
+                    .map(|tb| { self.add_table(tb) })
+                    .join(", ")
+            )),
+            stream_node::NodeBody::AppendOnlyTopN(node) => Some(format!(
+                "state table: {}",
+                self.add_table(node.get_table().unwrap())
+            )),
+            stream_node::NodeBody::HashJoin(node) => Some(format!(
+                "left table: {}, right table {},{}{}",
+                self.add_table(node.get_left_table().unwrap()),
+                self.add_table(node.get_right_table().unwrap()),
+                match &node.left_degree_table {
+                    Some(tb) => format!(" left degree table: {},", self.add_table(tb)),
+                    None => "".to_string(),
+                },
+                match &node.right_degree_table {
+                    Some(tb) => format!(" right degree table: {},", self.add_table(tb)),
+                    None => "".to_string(),
+                },
+            )),
+            stream_node::NodeBody::TopN(node) => Some(format!(
+                "state table: {}",
+                self.add_table(node.get_table().unwrap())
+            )),
+            stream_node::NodeBody::Lookup(node) => Some(format!(
+                "arrange table: {}",
+                self.add_table(node.get_arrangement_table().unwrap())
+            )),
+            stream_node::NodeBody::Arrange(node) => Some(format!(
+                "arrange table: {}",
+                self.add_table(node.get_table().unwrap())
+            )),
+            stream_node::NodeBody::DynamicFilter(node) => Some(format!(
+                "left table: {}, right table {}",
+                self.add_table(node.get_left_table().unwrap()),
+                self.add_table(node.get_right_table().unwrap()),
+            )),
+            stream_node::NodeBody::GroupTopN(node) => Some(format!(
+                "state table: {}",
+                self.add_table(node.get_table().unwrap())
+            )),
+            _ => None,
+        };
+        if let Some(explain_table_oneline) = explain_table_oneline {
+            writeln!(f, "{}{}", " ".repeat(level * 2 + 1), explain_table_oneline)?;
+        }
+
         if self.verbose {
             writeln!(
                 f,
