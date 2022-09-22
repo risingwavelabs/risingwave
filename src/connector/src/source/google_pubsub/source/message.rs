a51f753a76ceacb405b98a00229745d0e0831ac6
ditsuke <ditsuke@protonmail.com>
2022-09-22 11:09:43 +0530

    fix: pubsub SourceMessage transformer
    - pubsub message type: google_cloud_pubsub returns its own type
    - ack_id is not yet exposed: linked tracking upstream issue

--- src/connector/src/source/google_pubsub/source/message.rs
+++ src/connector/src/source/google_pubsub/source/message.rs
@@ -1,13 +1,23 @@
 use bytes::Bytes;
-use google_cloud_googleapis::pubsub::v1::ReceivedMessage;
+use google_cloud_pubsub::subscriber::ReceivedMessage;
 
 use crate::source::SourceMessage;
 
 impl From<ReceivedMessage> for SourceMessage {
     fn from(message: ReceivedMessage) -> Self {
         Self {
-            payload: message.message.map(|m| Bytes::from(m.data)),
-            offset: message.ack_id,
+            payload: {
+                let load = message.message.data;
+                match load.len() {
+                    0 => None,
+                    _ => Some(Bytes::from(load)),
+                }
+            },
+            // ! won't work yet -- ack_id is private
+            // reference: https://github.com/yoshidan/google-cloud-rust/issues/49
+            // ! offset: message.ack_id,
+            offset: 0.to_string(),
+
             // ? what are the ramifications
             split_id: 0.to_string().into(),
         }
