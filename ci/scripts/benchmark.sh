ed480acb19f4b228c34a764e31346e9ebd54154a
Jianwei Huang <1223644280@qq.com>
2022-09-22 16:09:23 +0800

    feat(ci): add python commands to update matrices

--- ci/scripts/benchmark.sh
+++ ci/scripts/benchmark.sh
@@ -3,21 +3,21 @@
 # Exits as soon as any line fails.
 set -euo pipefail
 
-while getopts 's:' opt; do
-    case ${opt} in
-        s )
-            SKU=$OPTARG
-            ;;
-        \? )
-            echo "Invalid Option: -$OPTARG" 1>&2
-            exit 1
-            ;;
-        : )
-            echo "Invalid option: $OPTARG requires an argument" 1>&2
-            ;;
-    esac
-done
-shift $((OPTIND -1))
+#while getopts 's:' opt; do
+#    case ${opt} in
+#        s )
+#            SKU=$OPTARG
+#            ;;
+#        \? )
+#            echo "Invalid Option: -$OPTARG" 1>&2
+#            exit 1
+#            ;;
+#        : )
+#            echo "Invalid option: $OPTARG requires an argument" 1>&2
+#            ;;
+#    esac
+#done
+#shift $((OPTIND -1))
 
 function polling() {
     set +e
@@ -51,16 +51,26 @@ else
   IMAGE_TAG="${RISINGWAVE_IMAGE_TAG}"
 fi
 
+if [ -z "${BENCH_SKU+x}" ] || [ "${BENCH_SKU}" == "MultiNodeBench" ]; then
+  SKU="multinode"
+  BENCH_SKU="MultiNodeBench"
+elif [ "${BENCH_SKU}" == "SingleNodeBench" ]; then
+  SKU="singlenode"
+else
+  exit 1
+fi
+
 date=$(date '+%Y%m%d-%H%M%S')
 TENANT_NAME="${SKU}-${date}"
 
 echo "--- Echo Info"
+echo "BENCH-SKU: ${BENCH_SKU}"
 echo "Tenant-Name: ${TENANT_NAME}"
 echo "Host-Ip: ${HOST_IP}"
 echo "IMAGE-TAG: ${IMAGE_TAG}"
 
 echo "--- Download Necessary Tools"
-apt-get -y install golang-go librdkafka-dev
+apt-get -y install golang-go librdkafka-dev python3-pip
 curl -L -o ./rwc https://rwc-cli-internal-release.s3.ap-southeast-1.amazonaws.com/bench-tmp/rwc && chmod 755 ./rwc && mv rwc /usr/local/bin
 
 echo "--- RWC Config and Login"
@@ -69,7 +79,7 @@ rwc config ls
 rwc login -account benchmark -password "$BENCH_TOKEN"
 
 echo "--- RWC Create a Risingwave Instance"
-rwc tenant create -name ${TENANT_NAME} -sku ${SKU} -imagetag ${IMAGE_TAG}
+rwc tenant create -name ${TENANT_NAME} -sku ${BENCH_SKU} -imagetag ${IMAGE_TAG}
 
 sleep 2
 
@@ -91,3 +101,15 @@ echo "--- Run Tpch-Bench"
 cd tpch-bench/
 ./scripts/build.sh
 ./scripts/launch_risedev_bench.sh
+
+echo "--- sleep 10"
+sleep 10
+
+echo "--- Clone Risingwave-test Repo"
+git clone https://"$GITHUB_TOKEN"@github.com/risingwavelabs/risingwave-test.git
+
+echo "--- Send Matrices to S3 for Slack Bot"
+cd risingwave-test/rwbot
+pip3 install -r requirement.txt
+cd commands/utilities/
+python3 promql.py ${SKU}
