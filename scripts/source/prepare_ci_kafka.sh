9f9a8ececd778adf642d1bc9991c9b17802ef3b8
Runji Wang <wangrunji0408@163.com>
2022-09-22 12:39:35 +0800

    use empty file to create kafka sink in test
    Signed-off-by: Runji Wang <wangrunji0408@163.com>

--- scripts/source/prepare_ci_kafka.sh
+++ scripts/source/prepare_ci_kafka.sh
@@ -7,8 +7,6 @@ SCRIPT_PATH="$(cd "$(dirname "$0")" >/dev/null 2>&1 && pwd)"
 cd "$SCRIPT_PATH/.." || exit 1
 
 KAFKA_BIN="$SCRIPT_PATH/../../.risingwave/bin/kafka/bin"
-SINK_TARGET_TOPIC="sink_target"
-SINK_DEBEZIUM_TOPIC="sink_debezium"
 
 echo "Create topics"
 for filename in "$SCRIPT_PATH"/test_data/*; do
@@ -24,12 +22,6 @@ for filename in "$SCRIPT_PATH"/test_data/*; do
     echo "Recreate topic $topic with partition $partition"
     "$KAFKA_BIN"/kafka-topics.sh --bootstrap-server 127.0.0.1:29092 --topic "$topic" --create --partitions "$partition"
 done
-
-# prepare for sink test
-"$KAFKA_BIN"/kafka-topics.sh --bootstrap-server 127.0.0.1:29092 --topic "$SINK_TARGET_TOPIC" --delete || true
-"$KAFKA_BIN"/kafka-topics.sh --bootstrap-server 127.0.0.1:29092 --topic "$SINK_TARGET_TOPIC" --create --partitions 1
-"$KAFKA_BIN"/kafka-topics.sh --bootstrap-server 127.0.0.1:29092 --topic "$SINK_DEBEZIUM_TOPIC" --delete || true
-"$KAFKA_BIN"/kafka-topics.sh --bootstrap-server 127.0.0.1:29092 --topic "$SINK_DEBEZIUM_TOPIC" --create --partitions 1
 
 echo "Fulfill kafka topics"
 for filename in "$SCRIPT_PATH"/test_data/*; do
