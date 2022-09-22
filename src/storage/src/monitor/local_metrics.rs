807659fa3dafb663fad0642fa700c187d75d061f
Li0k <yuli@singularity-data.com>
2022-09-22 14:50:36 +0800

    feat(storage): iter skip metrics

--- src/storage/src/monitor/local_metrics.rs
+++ src/storage/src/monitor/local_metrics.rs
@@ -27,7 +27,8 @@ pub struct StoreLocalStatistic {
     pub cache_meta_block_total: u64,
 
     // include multiple versions of one key.
-    pub skip_key_count: u64,
+    pub skip_multi_version_key_count: u64,
+    pub skip_delete_key_count: u64,
     pub processed_key_count: u64,
     pub bloom_filter_true_negative_count: u64,
     pub remote_io_time: Arc<AtomicU64>,
@@ -47,7 +48,8 @@ impl StoreLocalStatistic {
         self.cache_data_block_miss += other.cache_data_block_miss;
         self.cache_data_block_total += other.cache_data_block_total;
 
-        self.skip_key_count += other.skip_key_count;
+        self.skip_multi_version_key_count += other.skip_multi_version_key_count;
+        self.skip_delete_key_count += other.skip_delete_key_count;
         self.processed_key_count += other.processed_key_count;
         self.bloom_filter_true_negative_count += other.bloom_filter_true_negative_count;
         self.remote_io_time.fetch_add(
@@ -113,11 +115,19 @@ impl StoreLocalStatistic {
                 .with_label_values(&["processed"])
                 .inc_by(self.processed_key_count);
         }
-        if self.skip_key_count > 0 {
+
+        if self.skip_multi_version_key_count > 0 {
             metrics
                 .iter_scan_key_counts
-                .with_label_values(&["skip"])
-                .inc_by(self.skip_key_count);
+                .with_label_values(&["skip_multi_version"])
+                .inc_by(self.skip_multi_version_key_count);
+        }
+
+        if self.skip_delete_key_count > 0 {
+            metrics
+                .iter_scan_key_counts
+                .with_label_values(&["skip_delete"])
+                .inc_by(self.skip_delete_key_count);
         }
 
         #[cfg(all(debug_assertions, not(any(test, feature = "test"))))]
@@ -137,7 +147,8 @@ impl StoreLocalStatistic {
             || self.cache_data_block_total != 0
             || self.cache_meta_block_miss != 0
             || self.cache_meta_block_total != 0
-            || self.skip_key_count != 0
+            || self.skip_multi_version_key_count != 0
+            || self.skip_delete_key_count != 0
             || self.processed_key_count != 0
             || self.bloom_filter_true_negative_count != 0
             || self.remote_io_time.load(Ordering::Relaxed) != 0
