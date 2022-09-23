88d4ea39a9a248ee6c555e644d81aedd0d3d15cf
Jianwei Huang <1223644280@qq.com>
2022-09-23 16:45:50 +0800

    fix

--- ci/scripts/flink-bench.sh
+++ ci/scripts/flink-bench.sh
@@ -11,12 +11,10 @@ echo "--- start the flink bench instance to run the benchmark"
 aws ec2 start-instances --instance-ids i-029fdf626052dcdaf
 
 echo "--- do ssh to the flink benchmark machine"
-.ssh ssh -o "StrictHostKeyChecking no" -i test.pem ubuntu@52.220.89.140
+ssh -o "StrictHostKeyChecking no" -i test.pem ubuntu@52.220.89.140
 printf "logged in to the flink benchmark setup\n"
 
-echo "==========================================="
-echo "starts both zookeeper and kafka"
-echo "==========================================="
+echo "--- starts both zookeeper and kafka"
 ./start_kafka.sh
 
 if jps | grep 'QuorumPeerMain'; then
@@ -33,19 +31,15 @@ else
   exit 1
 fi
 
-echo "==========================================="
-echo "start Node Exporter, Pushgateway, Prometheus and Grafana"
-echo "==========================================="
+echo "--- start Node Exporter, Pushgateway, Prometheus and Grafana"
 if ./show_four_monitoring_components.sh | grep 'node_exporter' | grep 'grafana-server' | grep 'pushgateway' | grep 'prometheus'; then
   printf "all services have started successfully\n"
-else:
+else
   printf "above services did not start\n"
   exit 1
 fi
 
-echo "==========================================="
-echo "starts both the Flink cluster and Nexmark monitoring service"
-echo "==========================================="
+echo "--- starts both the Flink cluster and Nexmark monitoring service"
 if jps | grep -e "CpuMetricSender" -e "TaskManagerRunner" > /dev/null; then
   printf "flink cluster and nexmark started."
   printf "TaskManagerRunner represents the Flink and CpuMetricSender represents Nexmark monitoring service\n"
@@ -54,31 +48,21 @@ else
   exit 1
 fi
 
-echo "==========================================="
-echo "runs a Flink job using Datagen"
-echo "==========================================="
+echo "--- runs a Flink job using Datagen"
 ./run_insert_kafka.sh
 printf "flink job has finished\n"
 
-echo "==========================================="
-echo "check the number of records in the Kafka topic"
-echo "==========================================="
+echo "--- check the number of records in the Kafka topic"
 ./show_kafka_topic_records.sh "topic"
 
-echo "==========================================="
-echo " run the benchmark"
-echo "==========================================="
+echo "--- run the benchmark"
 ./run_kafka_source.sh
 printf "completed the benchmark for all the queries\n"
 
-echo "==========================================="
-echo "run Datagen source provided by Flink to generate in-memory data directly"
-echo "==========================================="
+echo "---- run Datagen source provided by Flink to generate in-memory data directly"
 ./run_datagen_source.sh
 
-echo "==========================================="
-echo "stop the flink bench instance"
-echo "==========================================="
+echo "--- stop the flink bench instance"
 aws ec2 stop-instances --instance-ids i-029fdf626052dcdaf
 printf "stopped the flink bench instance\n"
 
