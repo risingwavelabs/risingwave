03a2278524e6300ba99513f9b1b5ffa22d91cd24
Sumit Mittal <mittal.sumit1984@gmail.com>
2022-09-23 16:29:16 +0530

    minor updates

--- ci/scripts/main-flink-bench.sh
+++ ci/scripts/main-flink-bench.sh
@@ -11,9 +11,8 @@ echo "--- start the flink bench instance to run the benchmark"
 aws ec2 start-instances --instance-ids i-029fdf626052dcdaf
 
 echo "--- do ssh and run the flink benchmark steps"
-pwd
 ls
-scp -o "StrictHostKeyChecking no" -i test.pem flink-bench.sh ubuntu@52.220.89.140:/home/ubuntu
+scp -o "StrictHostKeyChecking no" -i test.pem scripts/flink-bench.sh ubuntu@52.220.89.140:/home/ubuntu
 ssh -o "StrictHostKeyChecking no" -i test.pem ubuntu@52.220.89.140 ./flink-bench.sh
 
 echo "--- stop the flink bench instance"
