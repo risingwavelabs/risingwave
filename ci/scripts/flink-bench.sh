7261cd5fd03775ef902423e4b0a1539c7265d56d
Sumit Mittal <mittal.sumit1984@gmail.com>
2022-09-24 17:40:47 +0530

    minor fix

--- ci/scripts/flink-bench.sh
+++ ci/scripts/flink-bench.sh
@@ -77,5 +77,3 @@ echo "---- run datagen source provided by flink to generate in-memory data direc
 
 echo "---- restart the flink for the graceful start of next benchmark"
 ./restart-flink.sh
-
-exit 0
--- ci/scripts/main-flink-bench.sh
+++ ci/scripts/main-flink-bench.sh
@@ -16,7 +16,7 @@ scp -o "StrictHostKeyChecking no" -i test.pem ci/scripts/flink-bench.sh ubuntu@5
 ssh -o "StrictHostKeyChecking no" -i test.pem ubuntu@52.220.89.140 "chmod 756 flink-bench.sh"
 
 echo "--- queries to be run: $1"
-ssh -o "StrictHostKeyChecking no" -i test.pem ubuntu@52.220.89.140 ./flink-bench.sh $1
+ssh -o "StrictHostKeyChecking no" -i test.pem ubuntu@52.220.89.140 './flink-bench.sh $1 && exit'
 
 echo "--- stop the flink bench instance"
 aws ec2 stop-instances --instance-ids i-029fdf626052dcdaf
