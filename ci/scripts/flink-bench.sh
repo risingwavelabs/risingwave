7f0cb0fa991a0c551ca0202b2ff17778149f4da9
Sumit Mittal <mittal.sumit1984@gmail.com>
2022-09-24 14:54:31 +0530

    minor changes

--- ci/scripts/flink-bench.sh
+++ ci/scripts/flink-bench.sh
@@ -66,7 +66,7 @@ echo "--- check the number of records in the Kafka topic"
 ./show_kafka_topic_records.sh "topic"
 
 echo "--- run the benchmark"
-./run_kafka_source.sh
+./run_kafka_source.sh ${1:-q0,q1,q2,q3,q4,q5,q6,q7,q8,q9,q10,q11,q12,q13,q14,q15,q16,q17,q18,q19,q20,q21,q22}
 printf "completed the benchmark for all the queries\n"
 
 echo "---- run Datagen source provided by Flink to generate in-memory data directly"
