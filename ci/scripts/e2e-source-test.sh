e4203baa6ee1d611da4cc7d096aa2db3c3ae1f1d
nanderstabel <nanderstabel@gmail.com>
2022-09-24 18:10:10 +0200

    move

--- ci/scripts/e2e-source-test.sh
+++ ci/scripts/e2e-source-test.sh
@@ -49,6 +49,6 @@ cargo make ci-start ci-kafka
 sqllogictest -p 4566 -d dev  './e2e_test/source/**/*.slt'
 
 echo "--- Run CH-benCHmark queries"
-./risedev slt -p 4566 -d dev ./e2e_test/streaming/ch-benchmark/create_sources.slt.part
-./risedev slt -p 4566 -d dev ./e2e_test/streaming/ch-benchmark/q13.slt.part
-./risedev slt -p 4566 -d dev ./e2e_test/streaming/ch-benchmark/drop_sources.slt.part
+./risedev slt -p 4566 -d dev ./e2e_test/ch-benchmark/create_sources.slt.part
+./risedev slt -p 4566 -d dev ./e2e_test/ch-benchmark/q13.slt.part
+./risedev slt -p 4566 -d dev ./e2e_test/ch-benchmark/drop_sources.slt.part
