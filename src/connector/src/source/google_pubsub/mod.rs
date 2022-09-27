ed19053862478388611e53f3376d7b6417e041f5
ditsuke <ditsuke@protonmail.com>
2022-09-27 23:15:44 +0530

    chore: Remove redundant serde aliases

--- src/connector/src/source/google_pubsub/mod.rs
+++ src/connector/src/source/google_pubsub/mod.rs
@@ -10,23 +10,23 @@ pub const GOOGLE_PUBSUB_CONNECTOR: &str = "google_pubsub";
 
 #[derive(Clone, Debug, Deserialize)]
 pub struct PubsubProperties {
-    #[serde(rename = "topic", alias = "pubsub.topic")]
+    #[serde(rename = "pubsub.topic")]
     pub topic: String,
 
-    #[serde(rename = "split_count", alias = "pubsub.split_count")]
+    #[serde(rename = "pubsub.split_count")]
     pub split_count: u32,
 
     // existing subscription -- or should that even be an option?
     // ? question: can we generate this
-    #[serde(rename = "subscription", alias = "pubsub.subscription")]
+    #[serde(rename = "pubsub.subscription")]
     pub subscription: String,
 
     // optionally filter by attribute
-    #[serde(rename = "attributes", alias = "pubsub.attributes")]
+    #[serde(rename = "pubsub.attributes")]
     pub attributes: Option<HashMap<String, String>>,
 
     // use against the pubsub emulator
-    #[serde(rename = "emulator_host", alias = "pubsub.emulator_host")]
+    #[serde(rename = "pubsub.emulator_host")]
     pub emulator_host: Option<String>,
     // @todo: more relevant properties: region, access key, secret, session token? (maybe)
     // roles: not sure
