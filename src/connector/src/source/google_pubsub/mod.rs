dcb1eedad7d34d10d870ce1580d1c29c5f6482c8
ditsuke <ditsuke@protonmail.com>
2022-09-28 14:52:58 +0530

    chore: Cleanup pubsub properties
    - Removes topic as configuration option as discussed.
      Our consumption primitive is a subscription, which
      we'll require to be made for us before we consume.
    - Adds the `credentials` field.

--- src/connector/src/source/google_pubsub/mod.rs
+++ src/connector/src/source/google_pubsub/mod.rs
@@ -10,24 +10,22 @@ pub const GOOGLE_PUBSUB_CONNECTOR: &str = "google_pubsub";
 
 #[derive(Clone, Debug, Deserialize)]
 pub struct PubsubProperties {
-    #[serde(rename = "pubsub.topic")]
-    pub topic: String,
-
     #[serde(rename = "pubsub.split_count")]
     pub split_count: u32,
 
-    // existing subscription -- or should that even be an option?
-    // ? question: can we generate this
     #[serde(rename = "pubsub.subscription")]
     pub subscription: String,
 
     // optionally filter by attribute
+    // ? Is there an equivalent for other sources
+    // ? Should we support filtering
     #[serde(rename = "pubsub.attributes")]
     pub attributes: Option<HashMap<String, String>>,
 
     // use against the pubsub emulator
     #[serde(rename = "pubsub.emulator_host")]
     pub emulator_host: Option<String>,
-    // @todo: more relevant properties: region, access key, secret, session token? (maybe)
-    // roles: not sure
+
+    #[serde(rename = "pubsub.emulator_host")]
+    pub credentials: Option<String>,
 }
