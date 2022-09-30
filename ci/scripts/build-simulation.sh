99cd61a6ccab96b8d78e8fc86c4189d8a3fc4c54
Bugen Zhao <i@bugenzhao.com>
2022-09-30 12:45:41 +0800

    fix workflow
    Signed-off-by: Bugen Zhao <i@bugenzhao.com>

--- ci/scripts/build-simulation.sh
+++ ci/scripts/build-simulation.sh
@@ -11,6 +11,10 @@ cp ci/risedev-components.ci.env risedev-components.user.env
 echo "--- Build deterministic simulation e2e test runner"
 cargo make sslt --profile ci-release -- --help
 
+echo "--- Build deterministic simulation e2e test runner"
+cargo make sbuild-scale-test --profile ci-release
+
 echo "--- Upload artifacts"
 cp target/sim/ci-release/risingwave_simulation ./risingwave_simulation
 buildkite-agent artifact upload risingwave_simulation
+buildkite-agent artifact upload scale-test.tar.zst
--- ci/scripts/deterministic-scale-test.sh
+++ ci/scripts/deterministic-scale-test.sh
@@ -5,8 +5,8 @@ set -euo pipefail
 
 source ci/scripts/common.env.sh
 
-echo "--- Generate RiseDev CI config"
-cp ci/risedev-components.ci.env risedev-components.user.env
+echo "--- Download artifacts"
+buildkite-agent artifact download scale-test.tar.zst .
 
 echo "--- Run scaling tests in deterministic simulation mode"
-MADSIM_TEST_NUM=5 cargo make stest-scale --no-fail-fast
+MADSIM_TEST_NUM=5 cargo nextest run --archive-file scale-test.tar.zst --no-fail-fast
