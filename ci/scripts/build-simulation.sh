8e89c8c01dff77c7d42f051672d9c4f898267e5b
Bugen Zhao <i@bugenzhao.com>
2022-09-30 14:56:20 +0800

    align cfg with sslt
    Signed-off-by: Bugen Zhao <i@bugenzhao.com>

--- ci/scripts/build-simulation.sh
+++ ci/scripts/build-simulation.sh
@@ -9,12 +9,12 @@ echo "--- Generate RiseDev CI config"
 cp ci/risedev-components.ci.env risedev-components.user.env
 
 echo "--- Build deterministic simulation e2e test runner"
-cargo make sslt --profile ci-sim -- --help
+cargo make sslt --profile ci-release -- --help
 
 echo "--- Build deterministic simulation e2e test runner"
-cargo make sbuild-scale-test --cargo-profile ci-sim
+cargo make sbuild-scale-test --cargo-profile ci-release
 
 echo "--- Upload artifacts"
-cp target/sim/ci-sim/risingwave_simulation ./risingwave_simulation
+cp target/sim/ci-release/risingwave_simulation ./risingwave_simulation
 buildkite-agent artifact upload risingwave_simulation
 buildkite-agent artifact upload scale-test.tar.zst
