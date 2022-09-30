b7861bce1dd3a7cd30cf2125cd0cccaf0b405b57
Bugen Zhao <i@bugenzhao.com>
2022-09-30 13:25:22 +0800

    try use ci-sim
    Signed-off-by: Bugen Zhao <i@bugenzhao.com>

--- ci/scripts/build-simulation.sh
+++ ci/scripts/build-simulation.sh
@@ -9,12 +9,12 @@ echo "--- Generate RiseDev CI config"
 cp ci/risedev-components.ci.env risedev-components.user.env
 
 echo "--- Build deterministic simulation e2e test runner"
-cargo make sslt --profile ci-release -- --help
+cargo make sslt --profile ci-sim -- --help
 
 echo "--- Build deterministic simulation e2e test runner"
-cargo make sbuild-scale-test --cargo-profile ci-release
+cargo make sbuild-scale-test --cargo-profile ci-sim
 
 echo "--- Upload artifacts"
-cp target/sim/ci-release/risingwave_simulation ./risingwave_simulation
+cp target/sim/ci-sim/risingwave_simulation ./risingwave_simulation
 buildkite-agent artifact upload risingwave_simulation
 buildkite-agent artifact upload scale-test.tar.zst
