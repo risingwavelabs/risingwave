3efe88e729bd4b755f5c98b14d8dcab5468d7577
Eric Fu <eric@singularity-data.com>
2022-10-06 17:23:32 +0800

    refactor: remove `batch_deserialize` in stata table (#5696)
    * refactor batch deserialize
    
    * fix clippy
    
    * add back assertion
    
    * fmt

--- src/storage/src/row_serde/row_serde_util.rs
+++ src/storage/src/row_serde/row_serde_util.rs
@@ -12,18 +12,12 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-use std::sync::Arc;
-
-use bytes::Buf;
 use memcomparable::from_slice;
 use risingwave_common::array::Row;
 use risingwave_common::catalog::ColumnId;
 use risingwave_common::error::Result;
-use risingwave_common::types::{DataType, VirtualNode, VIRTUAL_NODE_SIZE};
+use risingwave_common::types::{VirtualNode, VIRTUAL_NODE_SIZE};
 use risingwave_common::util::ordered::{OrderedRowDeserializer, OrderedRowSerializer};
-use risingwave_common::util::value_encoding::deserialize_datum;
-
-use super::ColumnDescMapping;
 
 pub fn serialize_pk(pk: &Row, serializer: &OrderedRowSerializer) -> Vec<u8> {
     let mut result = vec![];
@@ -59,30 +53,4 @@ pub fn parse_raw_key_to_vnode_and_key(raw_key: &[u8]) -> (VirtualNode, &[u8]) {
     let (vnode_bytes, key_bytes) = raw_key.split_at(VIRTUAL_NODE_SIZE);
     let vnode = VirtualNode::from_be_bytes(vnode_bytes.try_into().unwrap());
     (vnode, key_bytes)
-}
-
-/// used for batch table deserialize
-// TODO(eric): remove usages of this. Use `RowDeserializer` instead
-pub fn batch_deserialize(
-    column_mapping: Arc<ColumnDescMapping>,
-    value: impl AsRef<[u8]>,
-) -> Result<Row> {
-    let mut origin_row = streaming_deserialize(&column_mapping.all_data_types, value.as_ref())?;
-
-    let mut output_row = Vec::with_capacity(column_mapping.output_index.len());
-    for col_idx in &column_mapping.output_index {
-        output_row.push(origin_row.0[*col_idx].take());
-    }
-    Ok(Row(output_row))
-}
-
-// TODO(eric): remove me along with batch_deserialize
-fn streaming_deserialize(data_types: &[DataType], mut row: impl Buf) -> Result<Row> {
-    // value encoding
-    let mut values = Vec::with_capacity(data_types.len());
-    for ty in data_types {
-        values.push(deserialize_datum(&mut row, ty)?);
-    }
-
-    Ok(Row(values))
 }
