24eb6d33679fa5bd15bd69b778f711d41876c291
xx01cyx <caoyuanxin0531@outlook.com>
2022-10-07 03:19:23 +0000

    remove large_enum_variant clippy warning

--- src/source/src/lib.rs
+++ src/source/src/lib.rs
@@ -60,7 +60,6 @@ pub enum SourceImpl {
     Connector(ConnectorSource),
 }
 
-#[expect(clippy::large_enum_variant)]
 pub enum SourceStreamReaderImpl {
     Table(TableStreamReader),
     Connector(ConnectorSourceReader),
