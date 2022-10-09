31c925148f5d6199982a3c3cdc71170315b96c89
congyi <15605187270@163.com>
2022-10-09 12:06:53 +0800

    do-apply-planner-test

--- src/frontend/src/optimizer/plan_node/logical_agg.rs
+++ src/frontend/src/optimizer/plan_node/logical_agg.rs
@@ -367,8 +367,9 @@ impl LogicalAgg {
         if let Some(tb_vnode_idx) = vnode_col_idx.and_then(|idx| mapping.try_map(idx)) {
             internal_table_catalog_builder.set_vnode_col_idx(tb_vnode_idx);
         }
-        internal_table_catalog_builder
-            .set_value_indices((self.group_key().len()..internal_table_catalog_builder.columns().len()).collect_vec());
+        internal_table_catalog_builder.set_value_indices(
+            (self.group_key().len()..internal_table_catalog_builder.columns().len()).collect_vec(),
+        );
         internal_table_catalog_builder.build(tb_dist)
     }
 
