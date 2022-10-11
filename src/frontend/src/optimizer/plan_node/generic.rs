70ff9da4f868eebdc52918396c7a60651a26f535
Tesla I. Zhang‮ <ice1000kotlin@foxmail.com>
2022-10-11 15:32:01 -0400

    refactor(plan_node): Last part on rewriting logical nodes (#5766)
    * Improve some methods
    
    * Rua!!
    
    * Make other pieces of code happy

--- src/frontend/src/optimizer/plan_node/generic.rs
+++ src/frontend/src/optimizer/plan_node/generic.rs
@@ -16,18 +16,89 @@ use std::fmt;
 use std::rc::Rc;
 
 use itertools::Itertools;
-use risingwave_common::catalog::{FieldDisplay, Schema, TableDesc};
-use risingwave_common::types::DataType;
+use risingwave_common::catalog::{Field, FieldDisplay, Schema, TableDesc};
+use risingwave_common::types::{DataType, IntervalUnit};
 use risingwave_expr::expr::AggKind;
 use risingwave_pb::expr::agg_call::OrderByField as ProstAggOrderByField;
 use risingwave_pb::expr::AggCall as ProstAggCall;
 use risingwave_pb::plan_common::JoinType;
 
+use super::utils::IndicesDisplay;
 use crate::catalog::source_catalog::SourceCatalog;
 use crate::catalog::IndexCatalog;
-use crate::expr::{Expr, ExprImpl, InputRef, InputRefDisplay};
+use crate::expr::{Expr, ExprDisplay, ExprImpl, InputRef, InputRefDisplay};
 use crate::optimizer::property::{Direction, Order};
 use crate::utils::{Condition, ConditionDisplay};
+
+/// [`HopWindow`] implements Hop Table Function.
+#[derive(Debug, Clone)]
+pub struct HopWindow<PlanRef> {
+    pub input: PlanRef,
+    pub(super) time_col: InputRef,
+    pub(super) window_slide: IntervalUnit,
+    pub(super) window_size: IntervalUnit,
+    pub(super) output_indices: Vec<usize>,
+}
+
+impl<PlanRef> HopWindow<PlanRef> {
+    pub fn into_parts(self) -> (PlanRef, InputRef, IntervalUnit, IntervalUnit, Vec<usize>) {
+        (
+            self.input,
+            self.time_col,
+            self.window_slide,
+            self.window_size,
+            self.output_indices,
+        )
+    }
+
+    pub fn fmt_with_name(
+        &self,
+        f: &mut fmt::Formatter<'_>,
+        name: &str,
+        schema: impl Fn(&PlanRef) -> &Schema,
+    ) -> fmt::Result {
+        write!(
+            f,
+            "{} {{ time_col: {}, slide: {}, size: {}, output: {} }}",
+            name,
+            format_args!(
+                "{}",
+                InputRefDisplay {
+                    input_ref: &self.time_col,
+                    input_schema: schema(&self.input)
+                }
+            ),
+            self.window_slide,
+            self.window_size,
+            if self
+                .output_indices
+                .iter()
+                .copied()
+                // Behavior is the same as `LogicalHopWindow::internal_column_num`
+                .eq(0..(schema(&self.input).len() + 2))
+            {
+                "all".to_string()
+            } else {
+                let original_schema: Schema = schema(&self.input)
+                    .clone()
+                    .into_fields()
+                    .into_iter()
+                    .chain([
+                        Field::with_name(DataType::Timestamp, "window_start"),
+                        Field::with_name(DataType::Timestamp, "window_end"),
+                    ])
+                    .collect();
+                format!(
+                    "{:?}",
+                    &IndicesDisplay {
+                        indices: &self.output_indices,
+                        input_schema: &original_schema,
+                    }
+                )
+            },
+        )
+    }
+}
 
 /// [`Agg`] groups input data by their group key and computes aggregation functions.
 ///
@@ -393,6 +464,23 @@ pub struct Expand<PlanRef> {
     pub input: PlanRef,
 }
 
+impl<PlanRef> Expand<PlanRef> {
+    pub fn column_subsets_display(
+        &self,
+        schema: impl Fn(&PlanRef) -> &Schema,
+    ) -> Vec<Vec<FieldDisplay<'_>>> {
+        self.column_subsets
+            .iter()
+            .map(|subset| {
+                subset
+                    .iter()
+                    .map(|&i| FieldDisplay(schema(&self.input).fields.get(i).unwrap()))
+                    .collect_vec()
+            })
+            .collect_vec()
+    }
+}
+
 /// [`Filter`] iterates over its input and returns elements for which `predicate` evaluates to
 /// true, filtering out the others.
 ///
@@ -448,5 +536,26 @@ impl<PlanRef> Project<PlanRef> {
 
     pub fn decompose(self) -> (Vec<ExprImpl>, PlanRef) {
         (self.exprs, self.input)
+    }
+
+    pub(super) fn fmt_with_name(
+        &self,
+        f: &mut fmt::Formatter<'_>,
+        name: &str,
+        schema: impl Fn(&PlanRef) -> &Schema,
+    ) -> fmt::Result {
+        let mut builder = f.debug_struct(name);
+        builder.field(
+            "exprs",
+            &self
+                .exprs
+                .iter()
+                .map(|expr| ExprDisplay {
+                    expr,
+                    input_schema: schema(&self.input),
+                })
+                .collect_vec(),
+        );
+        builder.finish()
     }
 }
