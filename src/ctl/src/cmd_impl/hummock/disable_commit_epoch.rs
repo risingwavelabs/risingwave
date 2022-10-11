4e5619c962d5649afbf2479be1eacd17c19b46a6
StrikeW <wangsiyuanse@gmail.com>
2022-10-11 17:05:45 +0800

    feat(storage): compaction deterministic test tool (#5441)
    * feat(risectl) risectl hummock: add list-version-deltas
    
    * minor
    
    * feat(storage): compaction deterministic test tool
    
    * send notification after replay and compaction
    
    * fix some bugs
    
    * refine
    
    * runnable version!
    
    * add a DisableCommitEpoch to risectl
    
    * remove compaction finish channel
    
    * remove compaction status channel
    
    * remove side compaction channel
    
    * clean code
    
    * fix comments
    
    * minor
    
    * fix coverage
    
    * fix
    
    * check snapshot read
    
    * poll at client side
    
    * refine
    
    * minor
    
    * remove version delta notification
    
    * minor
    
    Co-authored-by: Gun9niR <gun9nir.guo@gmail.com>
    Co-authored-by: mergify[bot] <37929162+mergify[bot]@users.noreply.github.com>

--- src/ctl/src/cmd_impl/hummock/disable_commit_epoch.rs
+++ src/ctl/src/cmd_impl/hummock/disable_commit_epoch.rs
@@ -12,18 +12,16 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-mod list_version;
-pub use list_version::*;
-mod list_kv;
-pub use list_kv::*;
-mod sst_dump;
-pub use sst_dump::*;
-mod disable_commit_epoch;
-mod list_version_deltas;
-mod trigger_full_gc;
-mod trigger_manual_compaction;
+use crate::common::MetaServiceOpts;
 
-pub use disable_commit_epoch::*;
-pub use list_version_deltas::*;
-pub use trigger_full_gc::*;
-pub use trigger_manual_compaction::*;
+pub async fn disable_commit_epoch() -> anyhow::Result<()> {
+    let meta_opts = MetaServiceOpts::from_env()?;
+    let meta_client = meta_opts.create_meta_client().await?;
+    let version = meta_client.disable_commit_epoch().await?;
+    println!(
+        "Disabled.\
+        Current version: id {}, max_committed_epoch {}",
+        version.id, version.max_committed_epoch
+    );
+    Ok(())
+}
