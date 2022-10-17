79c9bf8b083032fed3bf24092379059cacefe293
Yuanxin Cao <60498509+xx01cyx@users.noreply.github.com>
2022-10-17 12:33:05 +0800

    feat(batch): clear query executions for distributed query after execution (#5827)
    * clear query executions for distributed query after the execution
    
    * rename end_session to end_query
    
    * make end_query return Result
    
    * add comments
    
    * clear session when connection is terminated
    
    * remove FIXME and TODO
    
    * clean query execution in DistributedQueryStream::drop instead of pgwire
    
    * misc
    
    * introduce QueryExecutionInfo
    
    * correct variable name and error message
    
    * clean query execution on error
    
    * refine comments
    
    Co-authored-by: mergify[bot] <37929162+mergify[bot]@users.noreply.github.com>

--- src/utils/pgwire/src/pg_protocol.rs
+++ src/utils/pgwire/src/pg_protocol.rs
@@ -59,6 +59,19 @@ where
     named_portals: HashMap<String, PgPortal<VS>>,
 }
 
+impl<S, SM, VS> Drop for PgProtocol<S, SM, VS>
+where
+    SM: SessionManager<VS>,
+    VS: Stream<Item = RowSetResult> + Unpin + Send,
+{
+    fn drop(&mut self) {
+        if let Some(session) = &self.session {
+            // Clear the session in session manager.
+            self.session_mgr.end_session(session);
+        }
+    }
+}
+
 /// States flow happened from top to down.
 enum PgProtocolState {
     Startup,
