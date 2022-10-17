3c8379331d642787fc943de5a21dbe1bd202f014
Gun9niR <gun9nir.guo@gmail.com>
2022-10-17 07:28:26 +0000

    misc

--- src/frontend/src/observer/observer_manager.rs
+++ src/frontend/src/observer/observer_manager.rs
@@ -43,7 +43,6 @@ impl ObserverState for FrontendObserverNode {
     type SubscribeType = SubscribeFrontend;
 
     fn handle_notification(&mut self, resp: SubscribeResponse) {
-        tracing::info!("recv notification");
         let Some(info) = resp.info.as_ref() else {
             return;
         };
