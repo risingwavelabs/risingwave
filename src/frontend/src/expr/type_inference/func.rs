76fabd86078079e8975e614975679c1ef21ff6c9
Xiangjin <xiangjin@singularity-data.com>
2022-10-18 17:22:19 +0800

    extract(epoch from timestamptz) -> seconds: decimal

--- src/frontend/src/expr/type_inference/func.rs
+++ src/frontend/src/expr/type_inference/func.rs
@@ -796,7 +796,7 @@ fn build_type_derive_map() -> FuncSigMap {
         map.insert(E::Divide, vec![T::Interval, t], T::Interval);
     }
 
-    for t in [T::Timestamp, T::Time, T::Date] {
+    for t in [T::Timestampz, T::Timestamp, T::Time, T::Date] {
         map.insert(E::Extract, vec![T::Varchar, t], T::Decimal);
     }
     for t in [T::Timestamp, T::Date] {
