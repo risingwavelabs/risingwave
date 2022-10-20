de6ce3123d5bc1d8d5d794f550b025375e145992
nanderstabel <nanderstabel@gmail.com>
2022-10-20 02:39:21 +0200

    delete

--- src/storage/src/hummock/mod.rs
+++ src/storage/src/hummock/mod.rs
@@ -19,7 +19,6 @@ use std::sync::Arc;
 use bytes::Bytes;
 use risingwave_common::config::StorageConfig;
 use risingwave_hummock_sdk::{HummockEpoch, *};
-// #[cfg(any(test, feature = "test"))]
 use risingwave_pb::hummock::GroupHummockVersion;
 use risingwave_pb::hummock::{pin_version_response, SstableInfo};
 use risingwave_rpc_client::HummockMetaClient;
@@ -71,9 +70,7 @@ use risingwave_hummock_sdk::compaction_group::StaticCompactionGroupId;
 use risingwave_hummock_sdk::filter_key_extractor::{
     FilterKeyExtractorManager, FilterKeyExtractorManagerRef,
 };
-// #[cfg(any(test, feature = "test"))]
 use risingwave_pb::hummock::pin_version_response::Payload;
-// #[cfg(any(test, feature = "test"))]
 use tokio::task::yield_now;
 pub use validator::*;
 use value::*;
