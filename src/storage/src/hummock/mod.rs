43598152208e98b73ce077ecdad8db7f203f1a16
nanderstabel <nanderstabel@gmail.com>
2022-10-20 02:16:04 +0200

    quick fix

--- src/storage/src/hummock/mod.rs
+++ src/storage/src/hummock/mod.rs
@@ -19,7 +19,7 @@ use std::sync::Arc;
 use bytes::Bytes;
 use risingwave_common::config::StorageConfig;
 use risingwave_hummock_sdk::{HummockEpoch, *};
-#[cfg(any(test, feature = "test"))]
+// #[cfg(any(test, feature = "test"))]
 use risingwave_pb::hummock::GroupHummockVersion;
 use risingwave_pb::hummock::{pin_version_response, SstableInfo};
 use risingwave_rpc_client::HummockMetaClient;
@@ -71,9 +71,9 @@ use risingwave_hummock_sdk::compaction_group::StaticCompactionGroupId;
 use risingwave_hummock_sdk::filter_key_extractor::{
     FilterKeyExtractorManager, FilterKeyExtractorManagerRef,
 };
-#[cfg(any(test, feature = "test"))]
+// #[cfg(any(test, feature = "test"))]
 use risingwave_pb::hummock::pin_version_response::Payload;
-#[cfg(any(test, feature = "test"))]
+// #[cfg(any(test, feature = "test"))]
 use tokio::task::yield_now;
 pub use validator::*;
 use value::*;
@@ -294,7 +294,7 @@ impl HummockStorage {
             .clone()
     }
 
-    #[cfg(any(test, feature = "test"))]
+    // #[cfg(any(test, feature = "test"))]
     pub async fn update_version_and_wait(&self, version: GroupHummockVersion) {
         let version_id = version.hummock_version.as_ref().unwrap().id;
         self.local_version_manager
