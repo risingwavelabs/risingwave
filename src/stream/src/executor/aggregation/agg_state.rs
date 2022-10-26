8855cdc6cb08b493709bbe842ab9dd38d4a05a0b
Dylan <chenzl25@mail2.sysu.edu.cn>
2022-10-26 14:23:04 +0800

    feat(streaming): support sum0 to fix stream 2 phase count output with empty input rows. (#5993)
    * fix stream agg initail value output when no input rows
    
    * fix batch sum0
    
    * fix array_agg initial value and refactor I64Sum0
    
    * fix unit test
    
    * Apply suggestions from code review
    
    Co-authored-by: Richard Chien <stdrc@outlook.com>
    
    * add doc
    
    * add doc
    
    Co-authored-by: Richard Chien <stdrc@outlook.com>
    Co-authored-by: mergify[bot] <37929162+mergify[bot]@users.noreply.github.com>

--- src/stream/src/executor/aggregation/agg_state.rs
+++ src/stream/src/executor/aggregation/agg_state.rs
@@ -133,4 +133,16 @@ impl<S: StateStore> AggState<S> {
             }
         }
     }
+
+    /// reset the value state to initial state.
+    pub fn reset(&mut self) {
+        match self {
+            Self::Value(state) => {
+                state.reset();
+            }
+            Self::MaterializedInput(_state) => {
+                // pass
+            }
+        }
+    }
 }
