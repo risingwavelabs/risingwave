40e2a444e06f521502592017ebd010498797e507
Noel Kwan <noelkwan1998@gmail.com>
2022-10-26 14:24:00 +0800

    increase sqlsmith e2e runtime

--- ci/scripts/run-e2e-test.sh
+++ ci/scripts/run-e2e-test.sh
@@ -77,11 +77,11 @@ if [[ "$RUN_SQLSMITH" -eq "1" ]]; then
     chmod +x ./target/debug/sqlsmith
 
     cargo make ci-start ci-3cn-1fe
-    # This avoids storing excess logs.
+    timeout 20m ./target/debug/sqlsmith test --count 500 --testdata ./src/tests/sqlsmith/tests/testdata
+
+    # Using `kill` instead of `ci-kill` avoids storing excess logs.
     # If there's errors, the failing query will be printed to stderr.
     # Use that to reproduce logs on local machine.
-    timeout 20m ./target/debug/sqlsmith test --testdata ./src/tests/sqlsmith/tests/testdata
-
     echo "--- Kill cluster"
     cargo make kill
 fi
