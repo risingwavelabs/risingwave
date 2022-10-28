2a31691f14160de41f0d3c6ee2af8d5979ba1e8b
jon-chuang <jon-chuang@users.noreply.github.com>
2022-10-28 13:40:00 +0800

    minor

--- ci/scripts/deterministic-e2e-test.sh
+++ ci/scripts/deterministic-e2e-test.sh
@@ -10,20 +10,19 @@ buildkite-agent artifact download risingwave_simulation .
 chmod +x ./risingwave_simulation
 
 export RUNNER=./risingwave_simulation
-# export RUST_LOG=off TODO: undo this
-export RUST_LOG=info
+export RUST_LOG=off
 
-# echo "--- deterministic simulation e2e, ci-3cn-1fe, ddl"
-# seq 16 | parallel MADSIM_TEST_SEED={} $RUNNER './e2e_test/ddl/\*\*/\*.slt'
+echo "--- deterministic simulation e2e, ci-3cn-1fe, ddl"
+seq 16 | parallel MADSIM_TEST_SEED={} $RUNNER './e2e_test/ddl/\*\*/\*.slt'
 
-# echo "--- deterministic simulation e2e, ci-3cn-1fe, streaming"
-# seq 16 | parallel MADSIM_TEST_SEED={} $RUNNER './e2e_test/streaming/\*\*/\*.slt'
+echo "--- deterministic simulation e2e, ci-3cn-1fe, streaming"
+seq 16 | parallel MADSIM_TEST_SEED={} $RUNNER './e2e_test/streaming/\*\*/\*.slt'
 
-# echo "--- deterministic simulation e2e, ci-3cn-1fe, batch"
-# seq 16 | parallel MADSIM_TEST_SEED={} $RUNNER './e2e_test/batch/\*\*/\*.slt'
+echo "--- deterministic simulation e2e, ci-3cn-1fe, batch"
+seq 16 | parallel MADSIM_TEST_SEED={} $RUNNER './e2e_test/batch/\*\*/\*.slt'
 
-# echo "--- deterministic simulation e2e, ci-3cn-1fe, kafka source"
-# seq 16 | parallel MADSIM_TEST_SEED={} $RUNNER './e2e_test/source/kafka.slt'
+echo "--- deterministic simulation e2e, ci-3cn-1fe, kafka source"
+seq 16 | parallel MADSIM_TEST_SEED={} $RUNNER './e2e_test/source/kafka.slt'
 
 echo "--- deterministic simulation e2e, ci-3cn-2fe, parallel, streaming"
 seq 16 | parallel MADSIM_TEST_SEED={} $RUNNER -j 16 './e2e_test/streaming/\*\*/\*.slt'
