cfe7e18bbadb24df38916c11e51049a864f65791
Runji Wang <wangrunji0408@163.com>
2022-10-28 15:38:30 +0800

    remove logs after success
    Signed-off-by: Runji Wang <wangrunji0408@163.com>

--- ci/scripts/deterministic-e2e-test.sh
+++ ci/scripts/deterministic-e2e-test.sh
@@ -12,37 +12,25 @@ chmod +x ./risingwave_simulation
 export RUST_LOG=info
 export LOGDIR=.risingwave/log
 
-echo "--- deterministic simulation e2e, ci-3cn-1fe, ddl"
 mkdir -p $LOGDIR
-seq 16 | parallel MADSIM_TEST_SEED={} './risingwave_simulation ./e2e_test/ddl/\*\*/\*.slt > $LOGDIR/{}.log'
-rm -rf $LOGDIR
+
+echo "--- deterministic simulation e2e, ci-3cn-1fe, ddl"
+seq 16 | parallel MADSIM_TEST_SEED={} './risingwave_simulation ./e2e_test/ddl/\*\*/\*.slt > $LOGDIR/{}.log && rm $LOGDIR/{}.log'
 
 echo "--- deterministic simulation e2e, ci-3cn-1fe, streaming"
-mkdir -p $LOGDIR
-seq 16 | parallel MADSIM_TEST_SEED={} './risingwave_simulation ./e2e_test/streaming/\*\*/\*.slt > $LOGDIR/{}.log'
-rm -rf $LOGDIR
+seq 16 | parallel MADSIM_TEST_SEED={} './risingwave_simulation ./e2e_test/streaming/\*\*/\*.slt > $LOGDIR/{}.log && rm $LOGDIR/{}.log'
 
 echo "--- deterministic simulation e2e, ci-3cn-1fe, batch"
-mkdir -p $LOGDIR
-seq 16 | parallel MADSIM_TEST_SEED={} './risingwave_simulation ./e2e_test/batch/\*\*/\*.slt > $LOGDIR/{}.log'
-rm -rf $LOGDIR
+seq 16 | parallel MADSIM_TEST_SEED={} './risingwave_simulation ./e2e_test/batch/\*\*/\*.slt > $LOGDIR/{}.log && rm $LOGDIR/{}.log'
 
 echo "--- deterministic simulation e2e, ci-3cn-1fe, kafka source"
-mkdir -p $LOGDIR
-seq 16 | parallel MADSIM_TEST_SEED={} './risingwave_simulation ./e2e_test/source/kafka.slt > $LOGDIR/{}.log'
-rm -rf $LOGDIR
+seq 16 | parallel MADSIM_TEST_SEED={} './risingwave_simulation ./e2e_test/source/kafka.slt > $LOGDIR/{}.log && rm $LOGDIR/{}.log'
 
 echo "--- deterministic simulation e2e, ci-3cn-2fe, parallel, streaming"
-mkdir -p $LOGDIR
-seq 16 | parallel MADSIM_TEST_SEED={} './risingwave_simulation -j 16 ./e2e_test/streaming/\*\*/\*.slt > $LOGDIR/{}.log'
-rm -rf $LOGDIR
+seq 16 | parallel MADSIM_TEST_SEED={} './risingwave_simulation -j 16 ./e2e_test/streaming/\*\*/\*.slt > $LOGDIR/{}.log && rm $LOGDIR/{}.log'
 
 echo "--- deterministic simulation e2e, ci-3cn-2fe, parallel, batch"
-mkdir -p $LOGDIR
-seq 16 | parallel MADSIM_TEST_SEED={} './risingwave_simulation -j 16 ./e2e_test/batch/\*\*/\*.slt > $LOGDIR/{}.log'
-rm -rf $LOGDIR
+seq 16 | parallel MADSIM_TEST_SEED={} './risingwave_simulation -j 16 ./e2e_test/batch/\*\*/\*.slt > $LOGDIR/{}.log && rm $LOGDIR/{}.log'
 
 echo "--- deterministic simulation e2e, ci-3cn-1fe, fuzzing"
-mkdir -p $LOGDIR
-seq 16 | parallel MADSIM_TEST_SEED={} './risingwave_simulation --sqlsmith 100 ./src/tests/sqlsmith/tests/testdata > $LOGDIR/{}.log'
-rm -rf $LOGDIR
+seq 16 | parallel MADSIM_TEST_SEED={} './risingwave_simulation --sqlsmith 100 ./src/tests/sqlsmith/tests/testdata > $LOGDIR/{}.log && rm $LOGDIR/{}.log'
--- ci/scripts/deterministic-recovery-test.sh
+++ ci/scripts/deterministic-recovery-test.sh
@@ -9,20 +9,16 @@ chmod +x ./risingwave_simulation
 export RUST_LOG=info
 export LOGDIR=.risingwave/log
 
+mkdir -p $LOGDIR
+
 # bugs here! Tracking issue https://github.com/risingwavelabs/risingwave/issues/4527
 echo "--- deterministic simulation e2e, ci-3cn-1fe, recovery, streaming"
-mkdir -p $LOGDIR
-seq 1 | parallel MADSIM_TEST_SEED={} './risingwave_simulation --kill-meta --kill-frontend --kill-compute --kill-compactor ./e2e_test/streaming/\*\*/\*.slt > $LOGDIR/{}.log'
-rm -rf $LOGDIR
+seq 1 | parallel MADSIM_TEST_SEED={} './risingwave_simulation --kill-meta --kill-frontend --kill-compute --kill-compactor ./e2e_test/streaming/\*\*/\*.slt > $LOGDIR/{}.log && rm $LOGDIR/{}.log'
 
 # bugs here! Tracking issue https://github.com/risingwavelabs/risingwave/issues/4527
 echo "--- deterministic simulation e2e, ci-3cn-1fe, recovery, batch"
-mkdir -p $LOGDIR
-seq 1 | parallel MADSIM_TEST_SEED={} './risingwave_simulation --kill-meta --kill-frontend --kill-compute --kill-compactor ./e2e_test/batch/\*\*/\*.slt > $LOGDIR/{}.log'
-rm -rf $LOGDIR
+seq 1 | parallel MADSIM_TEST_SEED={} './risingwave_simulation --kill-meta --kill-frontend --kill-compute --kill-compactor ./e2e_test/batch/\*\*/\*.slt > $LOGDIR/{}.log && rm $LOGDIR/{}.log'
 
 # bugs here! Tracking issue https://github.com/risingwavelabs/risingwave/issues/5103
 echo "--- deterministic simulation e2e, ci-3cn-1fe, recovery, streaming"
-mkdir -p $LOGDIR
-seq 1 | parallel MADSIM_TEST_SEED={} './risingwave_simulation --etcd-timeout-rate=0.01 ./e2e_test/streaming/\*\*/\*.slt > $LOGDIR/{}.log'
-rm -rf $LOGDIR
+seq 1 | parallel MADSIM_TEST_SEED={} './risingwave_simulation --etcd-timeout-rate=0.01 ./e2e_test/streaming/\*\*/\*.slt > $LOGDIR/{}.log && rm $LOGDIR/{}.log'
