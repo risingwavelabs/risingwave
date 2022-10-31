65d61f51607457e86c62bf20bd112d2409b3cf52
Bugen Zhao <i@bugenzhao.com>
2022-10-31 03:41:21 +0000

    switch the order
    Signed-off-by: Bugen Zhao <i@bugenzhao.com>

--- ci/scripts/pre-unit-test.sh
+++ ci/scripts/pre-unit-test.sh
@@ -5,11 +5,11 @@ set -euo pipefail
 
 source ci/scripts/common.env.sh
 
+echo "--- Run clippy check w/ all targets and features"
+cargo clippy --all-targets --all-features --locked -- -D warnings
+
 echo "--- Run clippy check"
 cargo clippy --locked -- -D warnings
-
-echo "--- Run clippy check w/ all targets and features"
-cargo clippy --all-targets --features failpoints,sync_point --locked -- -D warnings
 
 echo "--- Build documentation"
 cargo doc --document-private-items --no-deps
