693a2d1671340fc6d5288b11f34317400ad37c6b
Bugen Zhao <i@bugenzhao.com>
2022-11-15 16:49:56 +0800

    add module doc
    Signed-off-by: Bugen Zhao <i@bugenzhao.com>

--- src/common/src/row/vec_datum.rs
+++ src/common/src/row/vec_datum.rs
@@ -12,6 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+//! An owned row type with a `Vec<Datum>`.
+
 use std::hash::{BuildHasher, Hasher};
 use std::{cmp, ops};
 
