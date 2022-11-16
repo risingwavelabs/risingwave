fcba62ab0894a6174ecd140c8958bcc641c0635c
Renjie Liu <liurenjie2008@gmail.com>
2022-11-16 14:41:54 +0800

    Fix counter

--- src/batch/benches/mem_counter.rs
+++ src/batch/benches/mem_counter.rs
@@ -7,18 +7,20 @@ use hytra::TrAdder;
 use risingwave_pb::expr::expr_node::Type::Or;
 use tokio::runtime::Runtime;
 
+const THREAD_NUM: usize = 20;
+
 fn bench_atomic_counter(c: &mut Criterion) {
     let counter = Arc::new(AtomicUsize::new(0));
     let stop = Arc::new(AtomicBool::new(false));
-    for _ in 0..20 {
+    for _ in 0..THREAD_NUM {
         let c = counter.clone();
         let s = stop.clone();
-        thread::spawn(move || {
+        thread::spawn(move || loop {
             for _ in 0..1000 {
                 black_box(c.fetch_add(100, Ordering::SeqCst));
-                if s.load(Ordering::SeqCst) {
-                    break;
-                }
+            }
+            if s.load(Ordering::SeqCst) {
+                break;
             }
         });
     }
@@ -33,15 +35,15 @@ fn bench_atomic_counter(c: &mut Criterion) {
 fn bench_add_hytra(c: &mut Criterion) {
     let counter = Arc::new(TrAdder::new());
     let stop = Arc::new(AtomicBool::new(false));
-    for _ in 0..20 {
+    for _ in 0..THREAD_NUM {
         let c = counter.clone();
         let s = stop.clone();
-        thread::spawn(move || {
+        thread::spawn(move || loop {
             for _ in 0..1000 {
                 c.inc(100);
-                if s.load(Ordering::SeqCst) {
-                    break;
-                }
+            }
+            if s.load(Ordering::SeqCst) {
+                break;
             }
         });
     }
@@ -51,5 +53,42 @@ fn bench_add_hytra(c: &mut Criterion) {
     stop.store(true, Ordering::SeqCst);
 }
 
-criterion_group!(benches, bench_atomic_counter, bench_add_hytra);
+fn bench_get_hytra(c: &mut Criterion) {
+    let counter = Arc::new(TrAdder::new());
+    let stop = Arc::new(AtomicBool::new(false));
+    for _ in 0..THREAD_NUM {
+        let c = counter.clone();
+        let s = stop.clone();
+        thread::spawn(move || loop {
+            for _ in 0..1000 {
+                c.inc(100);
+            }
+            if s.load(Ordering::SeqCst) {
+                break;
+            }
+        });
+    }
+
+    c.bench_function("Add tradder", |b| b.iter(|| black_box(counter.get())));
+
+    stop.store(true, Ordering::SeqCst);
+}
+
+fn bench_add_usize(c: &mut Criterion) {
+    c.bench_function("Add num", |b| {
+        b.iter(|| {
+            let mut a = 100usize;
+            a += 1;
+            black_box(a)
+        })
+    });
+}
+
+criterion_group!(
+    benches,
+    bench_atomic_counter,
+    bench_add_hytra,
+    bench_add_usize,
+    bench_get_hytra
+);
 criterion_main!(benches);
