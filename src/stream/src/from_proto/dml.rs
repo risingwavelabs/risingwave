f7e2c6ad526c9d9d3dc79c3677949d5523aa56be
xx01cyx <caoyuanxin0531@outlook.com>
2022-11-22 07:30:37 +0000

    resolve conflict from main

--- src/stream/src/from_proto/dml.rs
+++ src/stream/src/from_proto/dml.rs
@@ -14,9 +14,7 @@
 
 use itertools::Itertools;
 use risingwave_common::catalog::{Schema, TableId};
-use risingwave_common::try_match_expand;
-use risingwave_pb::stream_plan::stream_node::NodeBody;
-use risingwave_pb::stream_plan::StreamNode;
+use risingwave_pb::stream_plan::DmlNode;
 use risingwave_storage::StateStore;
 
 use super::ExecutorBuilder;
@@ -24,17 +22,19 @@ use crate::error::StreamResult;
 use crate::executor::dml::DmlExecutor;
 use crate::executor::BoxedExecutor;
 use crate::task::{ExecutorParams, LocalStreamManagerCore};
+
 pub struct DmlExecutorBuilder;
 
 #[async_trait::async_trait]
 impl ExecutorBuilder for DmlExecutorBuilder {
+    type Node = DmlNode;
+
     async fn new_boxed_executor(
         params: ExecutorParams,
-        node: &StreamNode,
+        node: &Self::Node,
         _store: impl StateStore,
         _stream_manager: &mut LocalStreamManagerCore,
     ) -> StreamResult<BoxedExecutor> {
-        let node = try_match_expand!(node.get_node_body().unwrap(), NodeBody::Dml)?;
         let [upstream]: [_; 1] = params.input.try_into().unwrap();
         let table_id = TableId::new(node.table_id);
         let column_descs = node.column_descs.iter().map(Into::into).collect_vec();
