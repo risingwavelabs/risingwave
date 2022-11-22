45fe9a95ab5f82d4014dd8cc2ff6d22959eb8c33
StrikeW <wangsiyuanse@gmail.com>
2022-11-22 10:46:58 +0800

    fix

--- ci/scripts/run-e2e-test.sh
+++ ci/scripts/run-e2e-test.sh
@@ -27,9 +27,6 @@ buildkite-agent artifact download "e2e_test/generated/*" ./
 mv target/debug/risingwave-"$profile" target/debug/risingwave
 mv target/debug/risedev-dev-"$profile" target/debug/risedev-dev
 
-echo "--- Download connector node jar"
-buildkite-agent artifact download connector-service.jar ./
-
 echo "--- Adjust permission"
 chmod +x ./target/debug/risingwave
 chmod +x ./target/debug/risedev-dev
@@ -40,24 +37,6 @@ cp ci/risedev-components.ci.env risedev-components.user.env
 echo "--- Prepare RiseDev dev cluster"
 cargo make pre-start-dev
 cargo make link-all-in-one-binaries
-
-echo "--- e2e, ci-1cn-1fe, cdc source"
-# install mysql client
-apt-get -y install mysql-client
-# import data to mysql
-mysql --host=mysql --port=3306 -u root -p123456 < ./e2e_test/source/cdc/mysql_cdc.sql
-# start risingwave cluster
-cargo make ci-start ci-1cn-1fe
-# start cdc connector node
-nohup java -cp ./connector-service.jar com.risingwave.sourcenode.service.SourceServiceMain > .risingwave/log/connector-source.log 2>&1 &
-sleep 1
-sqllogictest -p 4566 -d dev './e2e_test/source/cdc/cdc.load.slt'
-# wait for cdc loading
-sleep 4
-sqllogictest -p 4566 -d dev './e2e_test/source/cdc/cdc.check.slt'
-
-echo "--- Kill cluster"
-cargo make ci-kill
 
 echo "--- e2e, ci-3cn-1fe, streaming"
 cargo make ci-start ci-3cn-1fe
