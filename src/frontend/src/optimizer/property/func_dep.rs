6f2743267e23f0a6e75f3d75c7df5bd11834edf5
Tesla I. Zhang‮ <ice1000kotlin@foxmail.com>
2022-11-28 15:34:37 -0500

    Update src/frontend/src/optimizer/property/func_dep.rs
    Co-authored-by: xxchan <xxchan22f@gmail.com>

--- src/frontend/src/optimizer/property/func_dep.rs
+++ src/frontend/src/optimizer/property/func_dep.rs
@@ -149,7 +149,7 @@ impl fmt::Display for FunctionalDependencySet {
         f.write_str("{")?;
         self.strict
             .iter()
-            .format_with(", ", |fd, f| f(&fd))
+            .format(", ")
             .fmt(f)?;
         f.write_str("}")
     }
