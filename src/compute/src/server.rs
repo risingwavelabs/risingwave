d5c4723420dcdce5810d8e03ec6fa3da4a07250f
Bugen Zhao <i@bugenzhao.com>
2022-12-01 19:21:14 +0800

    fix(ctl): export `META_ADDR` correctly for stack trace (#6685)
    * merge config in manager
    
    Signed-off-by: Bugen Zhao <i@bugenzhao.com>
    
    * print on ctrlc
    
    Signed-off-by: Bugen Zhao <i@bugenzhao.com>
    
    * add stdout lock
    
    Signed-off-by: Bugen Zhao <i@bugenzhao.com>
    
    * decrease timeout limit
    
    Signed-off-by: Bugen Zhao <i@bugenzhao.com>
    
    * fix risectl env
    
    Signed-off-by: Bugen Zhao <i@bugenzhao.com>
    
    * try graceful shutdown
    
    Signed-off-by: Bugen Zhao <i@bugenzhao.com>
    
    * refine error message
    
    Signed-off-by: Bugen Zhao <i@bugenzhao.com>
    
    * try upload by ourselves
    
    Signed-off-by: Bugen Zhao <i@bugenzhao.com>
    
    * cleanup
    
    Signed-off-by: Bugen Zhao <i@bugenzhao.com>
    
    Signed-off-by: Bugen Zhao <i@bugenzhao.com>
    Co-authored-by: mergify[bot] <37929162+mergify[bot]@users.noreply.github.com>

--- src/compute/src/server.rs
+++ src/compute/src/server.rs
@@ -16,6 +16,7 @@ use std::net::SocketAddr;
 use std::sync::Arc;
 use std::time::Duration;
 
+use async_stack_trace::StackTraceManager;
 use risingwave_batch::executor::BatchTaskMetrics;
 use risingwave_batch::rpc::service::task_service::BatchServiceImpl;
 use risingwave_batch::task::{BatchEnvironment, BatchManager};
@@ -210,14 +211,15 @@ pub async fn compute_node_serve(
         state_store.clone(),
         streaming_metrics.clone(),
         config.streaming.clone(),
-        async_stack_trace_config.clone(),
+        async_stack_trace_config,
         config.streaming.developer.stream_enable_managed_cache,
     ));
     let source_mgr = Arc::new(TableSourceManager::new(
         source_metrics,
         stream_config.developer.stream_connector_message_buffer_size,
     ));
-    let grpc_stack_trace_mgr = GrpcStackTraceManagerRef::default();
+    let grpc_stack_trace_mgr = async_stack_trace_config
+        .map(|config| GrpcStackTraceManagerRef::new(StackTraceManager::new(config).into()));
     let dml_mgr = Arc::new(DmlManager::default());
 
     // Initialize batch environment.
@@ -273,7 +275,7 @@ pub async fn compute_node_serve(
             .initial_stream_window_size(STREAM_WINDOW_SIZE)
             .tcp_nodelay(true)
             .layer(StackTraceMiddlewareLayer::new_optional(
-                async_stack_trace_config.map(|c| (grpc_stack_trace_mgr, c)),
+                grpc_stack_trace_mgr,
             ))
             .add_service(TaskServiceServer::new(batch_srv))
             .add_service(ExchangeServiceServer::new(exchange_srv))
