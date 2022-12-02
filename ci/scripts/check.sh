6b5d1617be04919a74764ce72a31b15c07005b09
Jianwei Huang <1223644280@qq.com>
2022-12-02 17:02:40 +0800

    fix

--- ci/scripts/check.sh
+++ ci/scripts/check.sh
@@ -5,13 +5,11 @@ set -euo pipefail
 
 source ci/scripts/common.env.sh
 
-echo "--- Check release mod"
-cargo check --release --features "static-link static-log-level"
-
 echo "--- Run clippy check"
-cargo clippy --all-targets --all-features --locked -- -D warnings
+cargo clippy --release --features "static-link static-log-level" --all-targets --locked -- -D warnings
 
 echo "--- Build documentation"
 cargo doc --document-private-items --no-deps
 
 echo "--- Run doctest"
+cargo test --doc
