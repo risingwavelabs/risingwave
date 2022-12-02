a0da59134eb36ee76123284ec1b74423169964d8
Bugen Zhao <i@bugenzhao.com>
2022-12-02 18:24:42 +0800

    check both dev and release
    Signed-off-by: Bugen Zhao <i@bugenzhao.com>

--- ci/scripts/check.sh
+++ ci/scripts/check.sh
@@ -5,8 +5,11 @@ set -euo pipefail
 
 source ci/scripts/common.env.sh
 
-echo "--- Run clippy check"
-cargo clippy --release --features "static-link static-log-level" --all-targets --locked -- -D warnings
+echo "--- Run clippy check (dev, all features)"
+cargo clippy --all-targets --all-features --locked -- -D warnings
+
+echo "--- Run clippy check (release)"
+cargo clippy --release  --all-targets --features "static-link static-log-level" --locked -- -D warnings
 
 echo "--- Build documentation"
 cargo doc --document-private-items --no-deps
