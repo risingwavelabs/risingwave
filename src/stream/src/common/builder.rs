2ca4a981077a3ac8dffe6984792acbeb93c2a4b3
Bugen Zhao <i@bugenzhao.com>
2022-12-06 17:13:34 +0800

    fix(streaming): do not yield empty chunk in hash join & dynamic filter (#6748)
    * do not yield empty chunk
    
    Signed-off-by: Bugen Zhao <i@bugenzhao.com>
    
    * add util
    
    Signed-off-by: Bugen Zhao <i@bugenzhao.com>
    
    * fix hash join tests
    
    Signed-off-by: Bugen Zhao <i@bugenzhao.com>
    
    * fix dynamic filter
    
    Signed-off-by: Bugen Zhao <i@bugenzhao.com>
    
    * fix lookup
    
    Signed-off-by: Bugen Zhao <i@bugenzhao.com>
    
    * fix clippy
    
    Signed-off-by: Bugen Zhao <i@bugenzhao.com>
    
    Signed-off-by: Bugen Zhao <i@bugenzhao.com>
    Co-authored-by: mergify[bot] <37929162+mergify[bot]@users.noreply.github.com>

--- src/stream/src/common/builder.rs
+++ src/stream/src/common/builder.rs
@@ -173,7 +173,7 @@ impl StreamChunkBuilder {
     #[must_use]
     pub fn take(&mut self) -> Option<StreamChunk> {
         if self.size == 0 {
-            // FIXME: do not output empty chunk.
+            return None;
         }
 
         self.size = 0;
