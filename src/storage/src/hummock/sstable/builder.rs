65737525cfe275d23e1006f472c4c70631687a99
congyi <15605187270@163.com>
2022-12-08 13:19:53 +0800

    fix

--- src/storage/src/hummock/sstable/builder.rs
+++ src/storage/src/hummock/sstable/builder.rs
@@ -212,7 +212,7 @@ impl<W: SstableWriter> SstableBuilder<W> {
             // 2. extract_key key is not duplicate
             if !extract_key.is_empty() && extract_key != self.last_extract_key.as_slice() {
                 // avoid duplicate add to bloom filter
-                self.user_key_hashes.push(xxh32::xxh32(extract_key, 1));
+                self.user_key_hashes.push(xxh32::xxh32(extract_key, 0));
                 self.last_extract_key.clear();
                 self.last_extract_key.extend_from_slice(extract_key);
             }
