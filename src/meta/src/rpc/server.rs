16b7771afc0f3faa84d8efca27c8796307643f66
CAJan93 <jan.mensch@gmx.net>
2022-12-08 13:10:33 +0100

    define leader svc only if node is leader

--- src/meta/src/rpc/server.rs
+++ src/meta/src/rpc/server.rs
@@ -186,231 +186,6 @@ pub async fn rpc_serve_with_store<S: MetaStore>(
     )
     .await?;
 
-    let prometheus_endpoint = opts.prometheus_endpoint.clone();
-    let env = MetaSrvEnv::<S>::new(opts, meta_store.clone(), info).await;
-    let fragment_manager = Arc::new(FragmentManager::new(env.clone()).await.unwrap());
-    let meta_metrics = Arc::new(MetaMetrics::new());
-    let registry = meta_metrics.registry();
-    monitor_process(registry).unwrap();
-
-    let cluster_manager = Arc::new(
-        ClusterManager::new(env.clone(), max_heartbeat_interval)
-            .await
-            .unwrap(),
-    );
-    let heartbeat_srv = HeartbeatServiceImpl::new(cluster_manager.clone());
-
-    // above is needed for both follower and leader
-
-    // below is needed only for follower
-
-    let compactor_manager = Arc::new(
-        hummock::CompactorManager::with_meta(env.clone(), max_heartbeat_interval.as_secs())
-            .await
-            .unwrap(),
-    );
-
-    let hummock_manager = hummock::HummockManager::new(
-        env.clone(),
-        cluster_manager.clone(),
-        meta_metrics.clone(),
-        compactor_manager.clone(),
-    )
-    .await
-    .unwrap();
-
-    #[cfg(not(madsim))]
-    if let Some(ref dashboard_addr) = address_info.dashboard_addr {
-        let dashboard_service = crate::dashboard::DashboardService {
-            dashboard_addr: *dashboard_addr,
-            cluster_manager: cluster_manager.clone(),
-            fragment_manager: fragment_manager.clone(),
-            meta_store: env.meta_store_ref(),
-            prometheus_endpoint: prometheus_endpoint.clone(),
-            prometheus_client: prometheus_endpoint.as_ref().map(|x| {
-                use std::str::FromStr;
-                prometheus_http_query::Client::from_str(x).unwrap()
-            }),
-        };
-        // TODO: join dashboard service back to local thread.
-        tokio::spawn(dashboard_service.serve(address_info.ui_path));
-    }
-
-    let catalog_manager = Arc::new(CatalogManager::new(env.clone()).await.unwrap());
-
-    let (barrier_scheduler, scheduled_barriers) =
-        BarrierScheduler::new_pair(hummock_manager.clone(), env.opts.checkpoint_frequency);
-
-    let source_manager = Arc::new(
-        SourceManager::new(
-            barrier_scheduler.clone(),
-            catalog_manager.clone(),
-            fragment_manager.clone(),
-        )
-        .await
-        .unwrap(),
-    );
-
-    let barrier_manager = Arc::new(GlobalBarrierManager::new(
-        scheduled_barriers,
-        env.clone(),
-        cluster_manager.clone(),
-        catalog_manager.clone(),
-        fragment_manager.clone(),
-        hummock_manager.clone(),
-        source_manager.clone(),
-        meta_metrics.clone(),
-    ));
-
-    {
-        let source_manager = source_manager.clone();
-        tokio::spawn(async move {
-            source_manager.run().await.unwrap();
-        });
-    }
-
-    let stream_manager = Arc::new(
-        GlobalStreamManager::new(
-            env.clone(),
-            fragment_manager.clone(),
-            barrier_scheduler.clone(),
-            cluster_manager.clone(),
-            source_manager.clone(),
-            hummock_manager.clone(),
-        )
-        .unwrap(),
-    );
-
-    hummock_manager
-        .purge_stale(
-            &fragment_manager
-                .list_table_fragments()
-                .await
-                .expect("list_table_fragments"),
-        )
-        .await
-        .unwrap();
-
-    // Initialize services.
-    let backup_object_store = Arc::new(
-        parse_remote_object_store(
-            &env.opts.backup_storage_url,
-            Arc::new(ObjectStoreMetrics::unused()),
-            true,
-        )
-        .await,
-    );
-    let backup_storage = Box::new(
-        ObjectStoreMetaSnapshotStorage::new(
-            &env.opts.backup_storage_directory,
-            backup_object_store,
-        )
-        .await?,
-    );
-    let backup_manager = Arc::new(BackupManager::new(
-        env.clone(),
-        hummock_manager.clone(),
-        backup_storage,
-    ));
-    let vacuum_manager = Arc::new(hummock::VacuumManager::new(
-        env.clone(),
-        hummock_manager.clone(),
-        backup_manager,
-        compactor_manager.clone(),
-    ));
-
-    let ddl_srv = DdlServiceImpl::<S>::new(
-        env.clone(),
-        catalog_manager.clone(),
-        stream_manager.clone(),
-        source_manager.clone(),
-        cluster_manager.clone(),
-        fragment_manager.clone(),
-        barrier_manager.clone(),
-    );
-
-    let user_srv = UserServiceImpl::<S>::new(env.clone(), catalog_manager.clone());
-
-    let scale_srv = ScaleServiceImpl::<S>::new(
-        barrier_scheduler.clone(),
-        fragment_manager.clone(),
-        cluster_manager.clone(),
-        source_manager,
-        catalog_manager.clone(),
-        stream_manager.clone(),
-    );
-
-    let cluster_srv = ClusterServiceImpl::<S>::new(cluster_manager.clone());
-    let stream_srv = StreamServiceImpl::<S>::new(
-        env.clone(),
-        barrier_scheduler.clone(),
-        fragment_manager.clone(),
-    );
-    let hummock_srv = HummockServiceImpl::new(
-        hummock_manager.clone(),
-        compactor_manager.clone(),
-        vacuum_manager.clone(),
-        fragment_manager.clone(),
-    );
-    let notification_srv = NotificationServiceImpl::new(
-        env.clone(),
-        catalog_manager,
-        cluster_manager.clone(),
-        hummock_manager.clone(),
-        fragment_manager.clone(),
-    );
-    let health_srv = HealthServiceImpl::new();
-
-    if let Some(prometheus_addr) = address_info.prometheus_addr {
-        MetricsManager::boot_metrics_service(
-            prometheus_addr.to_string(),
-            meta_metrics.registry().clone(),
-        )
-    }
-
-    // Initialize sub-tasks.
-    let compaction_scheduler = Arc::new(CompactionScheduler::new(
-        env.clone(),
-        hummock_manager.clone(),
-        compactor_manager.clone(),
-    ));
-    let mut sub_tasks =
-        hummock::start_hummock_workers(vacuum_manager, compaction_scheduler, &env.opts);
-    sub_tasks.push(
-        ClusterManager::start_worker_num_monitor(
-            cluster_manager.clone(),
-            Duration::from_secs(env.opts.node_num_monitor_interval_sec),
-            meta_metrics.clone(),
-        )
-        .await,
-    );
-    sub_tasks.push(HummockManager::start_compaction_heartbeat(hummock_manager).await);
-    sub_tasks.push((lease_handle, lease_shutdown));
-    if cfg!(not(test)) {
-        sub_tasks.push(
-            ClusterManager::start_heartbeat_checker(cluster_manager, Duration::from_secs(1)).await,
-        );
-        sub_tasks.push(GlobalBarrierManager::start(barrier_manager).await);
-    }
-
-    let (idle_send, idle_recv) = tokio::sync::oneshot::channel();
-    sub_tasks.push(
-        IdleManager::start_idle_checker(env.idle_manager_ref(), Duration::from_secs(30), idle_send)
-            .await,
-    );
-
-    let shutdown_all = async move {
-        for (join_handle, shutdown_sender) in sub_tasks {
-            if let Err(_err) = shutdown_sender.send(()) {
-                // Maybe it is already shut down
-                continue;
-            }
-            if let Err(err) = join_handle.await {
-                tracing::warn!("Failed to join shutdown: {:?}", err);
-            }
-        }
-    };
-
     let mut services_leader_rx = leader_rx.clone();
     let mut note_status_leader_rx = leader_rx.clone();
     let intercept_leader_rx = leader_rx.clone();
@@ -491,6 +266,7 @@ pub async fn rpc_serve_with_store<S: MetaStore>(
             tracing::info!("Starting follower services");
             tokio::spawn(async move {
                 let health_srv = HealthServiceImpl::new();
+                // TODO: Use prometheus endpoint in follower?
                 tonic::transport::Server::builder()
                     .layer(MetricsMiddlewareLayer::new(Arc::new(MetaMetrics::new())))
                     .add_service(HealthServer::with_interceptor(health_srv, intercept_clone))
@@ -547,7 +323,240 @@ pub async fn rpc_serve_with_store<S: MetaStore>(
         // TODO: leader services should only be DEFINED if this is a leader node
         // https://risingwave-labs.slack.com/archives/C046M5Y0WH2/p1670327569092569
 
+        // leader services defined below
         tracing::info!("Starting leader services");
+
+        let prometheus_endpoint = opts.prometheus_endpoint.clone();
+        let env = MetaSrvEnv::<S>::new(opts, meta_store.clone(), info).await;
+        let fragment_manager = Arc::new(FragmentManager::new(env.clone()).await.unwrap());
+        let meta_metrics = Arc::new(MetaMetrics::new());
+        let registry = meta_metrics.registry();
+        monitor_process(registry).unwrap();
+
+        let cluster_manager = Arc::new(
+            ClusterManager::new(env.clone(), max_heartbeat_interval)
+                .await
+                .unwrap(),
+        );
+        let heartbeat_srv = HeartbeatServiceImpl::new(cluster_manager.clone());
+
+        let compactor_manager = Arc::new(
+            hummock::CompactorManager::with_meta(env.clone(), max_heartbeat_interval.as_secs())
+                .await
+                .unwrap(),
+        );
+
+        let hummock_manager = hummock::HummockManager::new(
+            env.clone(),
+            cluster_manager.clone(),
+            meta_metrics.clone(),
+            compactor_manager.clone(),
+        )
+        .await
+        .unwrap();
+
+        #[cfg(not(madsim))]
+        if let Some(ref dashboard_addr) = address_info.dashboard_addr {
+            let dashboard_service = crate::dashboard::DashboardService {
+                dashboard_addr: *dashboard_addr,
+                cluster_manager: cluster_manager.clone(),
+                fragment_manager: fragment_manager.clone(),
+                meta_store: env.meta_store_ref(),
+                prometheus_endpoint: prometheus_endpoint.clone(),
+                prometheus_client: prometheus_endpoint.as_ref().map(|x| {
+                    use std::str::FromStr;
+                    prometheus_http_query::Client::from_str(x).unwrap()
+                }),
+            };
+            // TODO: join dashboard service back to local thread.
+            tokio::spawn(dashboard_service.serve(address_info.ui_path));
+        }
+
+        let catalog_manager = Arc::new(CatalogManager::new(env.clone()).await.unwrap());
+
+        let (barrier_scheduler, scheduled_barriers) =
+            BarrierScheduler::new_pair(hummock_manager.clone(), env.opts.checkpoint_frequency);
+
+        let source_manager = Arc::new(
+            SourceManager::new(
+                barrier_scheduler.clone(),
+                catalog_manager.clone(),
+                fragment_manager.clone(),
+            )
+            .await
+            .unwrap(),
+        );
+
+        let barrier_manager = Arc::new(GlobalBarrierManager::new(
+            scheduled_barriers,
+            env.clone(),
+            cluster_manager.clone(),
+            catalog_manager.clone(),
+            fragment_manager.clone(),
+            hummock_manager.clone(),
+            source_manager.clone(),
+            meta_metrics.clone(),
+        ));
+
+        {
+            let source_manager = source_manager.clone();
+            tokio::spawn(async move {
+                source_manager.run().await.unwrap();
+            });
+        }
+
+        let stream_manager = Arc::new(
+            GlobalStreamManager::new(
+                env.clone(),
+                fragment_manager.clone(),
+                barrier_scheduler.clone(),
+                cluster_manager.clone(),
+                source_manager.clone(),
+                hummock_manager.clone(),
+            )
+            .unwrap(),
+        );
+
+        hummock_manager
+            .purge_stale(
+                &fragment_manager
+                    .list_table_fragments()
+                    .await
+                    .expect("list_table_fragments"),
+            )
+            .await
+            .unwrap();
+
+        // Initialize services.
+        let backup_object_store = Arc::new(
+            parse_remote_object_store(
+                &env.opts.backup_storage_url,
+                Arc::new(ObjectStoreMetrics::unused()),
+                true,
+            )
+            .await,
+        );
+        let backup_storage = Box::new(
+            ObjectStoreMetaSnapshotStorage::new(
+                &env.opts.backup_storage_directory,
+                backup_object_store,
+            )
+            .await
+            .unwrap(),
+            // TODO: How do I handle this panic here?
+            // How can I pass this to the caller of rpc_serve_with_store?
+        );
+        let backup_manager = Arc::new(BackupManager::new(
+            env.clone(),
+            hummock_manager.clone(),
+            backup_storage,
+        ));
+        let vacuum_manager = Arc::new(hummock::VacuumManager::new(
+            env.clone(),
+            hummock_manager.clone(),
+            backup_manager,
+            compactor_manager.clone(),
+        ));
+
+        let ddl_srv = DdlServiceImpl::<S>::new(
+            env.clone(),
+            catalog_manager.clone(),
+            stream_manager.clone(),
+            source_manager.clone(),
+            cluster_manager.clone(),
+            fragment_manager.clone(),
+            barrier_manager.clone(),
+        );
+
+        let user_srv = UserServiceImpl::<S>::new(env.clone(), catalog_manager.clone());
+
+        let scale_srv = ScaleServiceImpl::<S>::new(
+            barrier_scheduler.clone(),
+            fragment_manager.clone(),
+            cluster_manager.clone(),
+            source_manager,
+            catalog_manager.clone(),
+            stream_manager.clone(),
+        );
+
+        let cluster_srv = ClusterServiceImpl::<S>::new(cluster_manager.clone());
+        let stream_srv = StreamServiceImpl::<S>::new(
+            env.clone(),
+            barrier_scheduler.clone(),
+            fragment_manager.clone(),
+        );
+        let hummock_srv = HummockServiceImpl::new(
+            hummock_manager.clone(),
+            compactor_manager.clone(),
+            vacuum_manager.clone(),
+            fragment_manager.clone(),
+        );
+        let notification_srv = NotificationServiceImpl::new(
+            env.clone(),
+            catalog_manager,
+            cluster_manager.clone(),
+            hummock_manager.clone(),
+            fragment_manager.clone(),
+        );
+        let health_srv = HealthServiceImpl::new();
+
+        if let Some(prometheus_addr) = address_info.prometheus_addr {
+            MetricsManager::boot_metrics_service(
+                prometheus_addr.to_string(),
+                meta_metrics.registry().clone(),
+            )
+        }
+
+        // Initialize sub-tasks.
+        let compaction_scheduler = Arc::new(CompactionScheduler::new(
+            env.clone(),
+            hummock_manager.clone(),
+            compactor_manager.clone(),
+        ));
+        let mut sub_tasks =
+            hummock::start_hummock_workers(vacuum_manager, compaction_scheduler, &env.opts);
+        sub_tasks.push(
+            ClusterManager::start_worker_num_monitor(
+                cluster_manager.clone(),
+                Duration::from_secs(env.opts.node_num_monitor_interval_sec),
+                meta_metrics.clone(),
+            )
+            .await,
+        );
+        sub_tasks.push(HummockManager::start_compaction_heartbeat(hummock_manager).await);
+        sub_tasks.push((lease_handle, lease_shutdown));
+        if cfg!(not(test)) {
+            sub_tasks.push(
+                ClusterManager::start_heartbeat_checker(cluster_manager, Duration::from_secs(1))
+                    .await,
+            );
+            sub_tasks.push(GlobalBarrierManager::start(barrier_manager).await);
+        }
+
+        let (idle_send, idle_recv) = tokio::sync::oneshot::channel();
+        sub_tasks.push(
+            IdleManager::start_idle_checker(
+                env.idle_manager_ref(),
+                Duration::from_secs(30),
+                idle_send,
+            )
+            .await,
+        );
+
+        let shutdown_all = async move {
+            for (join_handle, shutdown_sender) in sub_tasks {
+                if let Err(_err) = shutdown_sender.send(()) {
+                    // Maybe it is already shut down
+                    continue;
+                }
+                if let Err(err) = join_handle.await {
+                    tracing::warn!("Failed to join shutdown: {:?}", err);
+                }
+            }
+        };
+
+        // leader services defined above
+
         tonic::transport::Server::builder()
             .layer(MetricsMiddlewareLayer::new(meta_metrics.clone()))
             .add_service(HeartbeatServiceServer::with_interceptor(
