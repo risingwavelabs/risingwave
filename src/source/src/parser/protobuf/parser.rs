ce62ac32d58aa8e9adfcae09e896800818878410
Floris Smit <floris@xomnia.com>
2022-12-11 13:41:09 +0100

    Pass properties to schema registry http client to enable basic auth

--- src/source/src/parser/protobuf/parser.rs
+++ src/source/src/parser/protobuf/parser.rs
@@ -51,7 +51,7 @@ impl ProtobufParser {
 
         let schema_bytes = if use_schema_registry {
             let kafka_topic = get_kafka_topic(&props)?;
-            let client = Client::new(url)?;
+            let client = Client::new(url, &props)?;
             compile_file_descriptor_from_schema_registry(
                 format!("{}-value", kafka_topic).as_str(),
                 &client,
