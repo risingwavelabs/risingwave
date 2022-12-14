65f242668fc85defa3f7c943d2cbc56102380d2f
Noel Kwan <noelkwan1998@gmail.com>
2022-12-14 20:35:14 +0800

    cleanup

--- src/frontend/src/optimizer/plan_node/logical_hop_window.rs
+++ src/frontend/src/optimizer/plan_node/logical_hop_window.rs
@@ -328,27 +328,19 @@ impl PredicatePushdown for LogicalHopWindow {
         //     "output_indices len for logical hop window: {:?}",
         //     self.output_indices()
         // );
-        // For reference: HOP(table_or_source, start_time, hop_size, window_size);
+
+        // For reference: HOP(table_or_source, time_column, hop_size, window_size);
         // `window_start`, `window_end` cannot be pushed-down, they are produced by HopWindow.
         let mut window_columns = FixedBitSet::with_capacity(self.output_indices().len());
         let window_start_idx = self.input().schema().len();
         let window_end_idx = self.input().schema().len() + 1;
-        let mut window_start_pos = None;
-        let mut window_end_pos = None;
         for (i, v) in self.output_indices().iter().enumerate() {
             if *v == window_start_idx {
-                window_start_pos = Some(i);
+                window_columns.insert(i);
             }
             if *v == window_end_idx {
-                window_end_pos = Some(i)
+                window_columns.insert(i);
             }
-        }
-        // just set the bits which do not mark `input.schema`.
-        if let Some(i) = window_start_pos {
-            window_columns.insert(i);
-        }
-        if let Some(i) = window_end_pos {
-            window_columns.insert(i);
         }
 
         let (time_window_pred, pushed_predicate) = predicate.split_disjoint(&window_columns);
