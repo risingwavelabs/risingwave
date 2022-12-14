e5d59d081cd8ec7dec553ceca77944a54b2d6f8d
BowenXiao1999 <931759898@qq.com>
2022-12-14 17:37:26 +0800

    try fix ci

--- ci/scripts/run-unit-test.sh
+++ ci/scripts/run-unit-test.sh
@@ -4,10 +4,6 @@ set -euo pipefail
 echo "+++ Run unit tests with coverage"
 # use tee to disable progress bar
 NEXTEST_PROFILE=ci cargo llvm-cov nextest --lcov --output-path lcov.info --features failpoints,sync_point 2> >(tee);
-
-echo "+++ Run the loom unit test"
-NEXTEST_PROFILE=ci RUSTFLAGS="--cfg loom" cargo nextest run --test loom
-
 if [[ "$RUN_SQLSMITH" -eq "1" ]]; then
     NEXTEST_PROFILE=ci cargo nextest run run_sqlsmith_on_frontend --features "failpoints sync_point enable_sqlsmith_unit_test" 2> >(tee);
 fi
@@ -15,3 +11,6 @@ fi
 echo "--- Codecov upload coverage reports"
 curl -Os https://uploader.codecov.io/latest/linux/codecov && chmod +x codecov
 ./codecov -t "$CODECOV_TOKEN" -s . -F rust
+
+echo "+++ Run the loom unit test"
+cd src/utils/task_local_alloc && NEXTEST_PROFILE=ci RUSTFLAGS="--cfg loom" cargo nextest run --test loom
