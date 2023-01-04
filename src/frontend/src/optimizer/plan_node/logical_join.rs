c479c294b5838f6ca8b601e77d79fbed188f1c19
xxchan <xxchan22f@gmail.com>
2023-01-04 18:30:29 +0100

    fix: fix InputRef's type mismatch in predicate move-around (#7191)
    fix https://github.com/risingwavelabs/risingwave/issues/7158
    
    Approved-By: chenzl25

--- src/frontend/src/optimizer/plan_node/logical_join.rs
+++ src/frontend/src/optimizer/plan_node/logical_join.rs
@@ -797,55 +797,62 @@ fn is_pure_fn_except_for_input_ref(expr: &ExprImpl) -> bool {
     }
 }
 
-// We are trying to derive a predicate to apply to the other side of a join if all
-// the `InputRef`s in the predicate are eq condition columns, and can hence be substituted
-// with the corresponding eq condition columns of the other side.
-//
-// Strategy:
-// 1. If the function is pure except for any `InputRef`, then we proceed. Else abort.
-// 2. Then, we collect `InputRef`s in the conjunction.
-// 3. If they are all columns in the given side of join eq condition, then we proceed. Else abort.
-// 4. We then rewrite the `ExprImpl`, by replacing `InputRef` column indices with
-//    the equivalent in the other side.
+/// We are trying to derive a predicate to apply to the other side of a join if all
+/// the `InputRef`s in the predicate are eq condition columns, and can hence be substituted
+/// with the corresponding eq condition columns of the other side.
+///
+/// Strategy:
+/// 1. If the function is pure except for any `InputRef` (which may refer to impure computation),
+///    then we proceed. Else abort.
+/// 2. Then, we collect `InputRef`s in the conjunction.
+/// 3. If they are all columns in the given side of join eq condition, then we proceed. Else abort.
+/// 4. We then rewrite the `ExprImpl`, by replacing `InputRef` column indices with
+///    the equivalent in the other side.
+///
+/// # Arguments
+///
+/// Suppose we derive a predicate from the left side to be pushed to the right side.
+/// * `expr`: An expr from the left side.
+/// * `col_num`: The number of columns in the left side.
 fn derive_predicate_from_eq_condition(
     expr: &ExprImpl,
     eq_condition: &EqJoinPredicate,
-    cols_num: usize,
+    col_num: usize,
     expr_is_left: bool,
 ) -> Option<ExprImpl> {
     if !is_pure_fn_except_for_input_ref(expr) {
         return None;
     }
-    let input_ref_indices = expr.collect_input_refs(cols_num);
-    let eq_predicate_indices = if expr_is_left {
+    let eq_indices = if expr_is_left {
         eq_condition.left_eq_indexes()
     } else {
         eq_condition.right_eq_indexes()
     };
-    let only_eq_indices = input_ref_indices
+    if expr
+        .collect_input_refs(col_num)
         .ones()
-        .all(|index| eq_predicate_indices.contains(&index));
-    if !only_eq_indices {
+        .any(|index| !eq_indices.contains(&index))
+    {
+        // expr contains an InputRef not in eq_condition
         return None;
     }
     // The function is pure except for `InputRef` and all `InputRef`s are `eq_condition` indices.
     // Hence, we can substitute those `InputRef`s with indices from the other side.
     let other_side_mapping = if expr_is_left {
-        eq_condition.eq_indexes().into_iter().collect()
+        eq_condition.eq_indexes_typed().into_iter().collect()
     } else {
         eq_condition
-            .eq_indexes()
+            .eq_indexes_typed()
             .into_iter()
             .map(|(x, y)| (y, x))
             .collect()
     };
     struct InputRefsRewriter {
-        mapping: HashMap<usize, usize>,
+        mapping: HashMap<InputRef, InputRef>,
     }
     impl ExprRewriter for InputRefsRewriter {
-        fn rewrite_input_ref(&mut self, mut input_ref: InputRef) -> ExprImpl {
-            input_ref.index = *self.mapping.get(&input_ref.index).unwrap();
-            input_ref.into()
+        fn rewrite_input_ref(&mut self, input_ref: InputRef) -> ExprImpl {
+            self.mapping[&input_ref].clone().into()
         }
     }
     Some(
@@ -921,12 +928,10 @@ impl PredicatePushdown for LogicalJoin {
         let right_predicate = right_from_filter.and(right_from_on);
 
         // Derive conditions to push to the other side based on eq condition columns
-        let left_cols_num = self.left().schema().len();
-        let right_cols_num = self.right().schema().len();
-        let eq_condition = EqJoinPredicate::create(left_cols_num, right_cols_num, new_on.clone());
+        let eq_condition = EqJoinPredicate::create(left_col_num, right_col_num, new_on.clone());
 
         // Only push to RHS if RHS is inner side of a join (RHS requires match on LHS)
-        let right_derived_predicate = if matches!(
+        let right_from_left = if matches!(
             join_type,
             JoinType::Inner | JoinType::LeftOuter | JoinType::RightSemi | JoinType::LeftSemi
         ) {
@@ -935,7 +940,7 @@ impl PredicatePushdown for LogicalJoin {
                     .conjunctions
                     .iter()
                     .filter_map(|expr| {
-                        derive_predicate_from_eq_condition(expr, &eq_condition, left_cols_num, true)
+                        derive_predicate_from_eq_condition(expr, &eq_condition, left_col_num, true)
                     })
                     .collect(),
             }
@@ -944,7 +949,7 @@ impl PredicatePushdown for LogicalJoin {
         };
 
         // Only push to LHS if LHS is inner side of a join (LHS requires match on RHS)
-        let left_derived_predicate = if matches!(
+        let left_from_right = if matches!(
             join_type,
             JoinType::Inner | JoinType::RightOuter | JoinType::LeftSemi | JoinType::RightSemi
         ) {
@@ -956,7 +961,7 @@ impl PredicatePushdown for LogicalJoin {
                         derive_predicate_from_eq_condition(
                             expr,
                             &eq_condition,
-                            right_cols_num,
+                            right_col_num,
                             false,
                         )
                     })
@@ -966,8 +971,8 @@ impl PredicatePushdown for LogicalJoin {
             Condition::true_cond()
         };
 
-        let left_predicate = left_predicate.and(left_derived_predicate);
-        let right_predicate = right_predicate.and(right_derived_predicate);
+        let left_predicate = left_predicate.and(left_from_right);
+        let right_predicate = right_predicate.and(right_from_left);
 
         let new_left = self.left().predicate_pushdown(left_predicate, ctx);
         let new_right = self.right().predicate_pushdown(right_predicate, ctx);
