5975001de858f4228bee1cd9dd251b7bbf00f655
xxchan <xxchan22f@gmail.com>
2023-01-09 18:02:58 +0100

    add a check for docker

--- ci/scripts/docker.sh
+++ ci/scripts/docker.sh
@@ -13,6 +13,16 @@ arch="$(uname -m)"
 echo "--- docker build and tag"
 docker build -f docker/Dockerfile -t "${ghcraddr}:${BUILDKITE_COMMIT}-${arch}" --target risingwave .
 
+echo "--- check the image can start correctly"
+container_id=$(docker run -d "${ghcraddr}:${BUILDKITE_COMMIT}-${arch}" risingwave)
+ret_code=$(docker inspect --format='{{.State.ExitCode}}' "$container_id")
+if [ "$ret_code" -ne 0 ]; then
+  echo "docker run failed with exit code $ret_code"
+  docker logs "$container_id"
+  exit 1
+fi
+docker kill "$container_id"
+
 echo "--- docker images"
 docker images
 
