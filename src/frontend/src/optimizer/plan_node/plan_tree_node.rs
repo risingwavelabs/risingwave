7a2bf8ebed72e7630ee59a041e8fa17807811fc8
jon-chuang <jon-chuang@users.noreply.github.com>
2023-01-10 14:07:34 +0800

    fix

--- src/frontend/src/optimizer/plan_node/plan_tree_node.rs
+++ src/frontend/src/optimizer/plan_node/plan_tree_node.rs
@@ -15,7 +15,6 @@
 use smallvec::SmallVec;
 
 use super::PlanRef;
-use crate::expr::{ExprImpl, ExprRewriter};
 use crate::utils::ColIndexMapping;
 
 /// The trait [`PlanNode`](super::PlanNode) really need about tree structure and used by optimizer
