af7668357a9afbb45f2ea759dcbc461da5e25ecc
congyi <15605187270@163.com>
2023-01-10 14:13:27 +0800

    minor

--- src/common/src/config.rs
+++ src/common/src/config.rs
@@ -515,7 +515,7 @@ mod default {
 
     pub mod streaming {
         pub fn barrier_interval_ms() -> u32 {
-            50
+            1000
         }
 
         pub fn in_flight_barrier_nums() -> usize {
