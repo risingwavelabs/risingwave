35107766f40ae6fcef162b7d3cedf42759abb088
William Wen <william123.wen@gmail.com>
2023-01-10 18:00:01 +0800

    fix(storage): fix reading unregistered table id

--- src/storage/hummock_test/src/state_store_tests.rs
+++ src/storage/hummock_test/src/state_store_tests.rs
@@ -13,10 +13,11 @@
 // limitations under the License.
 
 use std::ops::Bound;
+use std::ops::Bound::Unbounded;
 use std::sync::Arc;
 
 use bytes::Bytes;
-use futures::TryStreamExt;
+use futures::{pin_mut, TryStreamExt};
 use risingwave_common::catalog::TableId;
 use risingwave_hummock_sdk::key::FullKey;
 use risingwave_hummock_sdk::{HummockEpoch, HummockReadEpoch, HummockSstableId, LocalSstableInfo};
@@ -37,6 +38,44 @@ use crate::test_utils::{
     with_hummock_storage_v1, with_hummock_storage_v2, HummockStateStoreTestTrait,
     HummockV2MixedStateStore,
 };
+
+#[tokio::test]
+async fn test_empty_read_v2() {
+    let (hummock_storage, _meta_client) = with_hummock_storage_v2(Default::default()).await;
+    assert!(hummock_storage
+        .get(
+            b"test_key".as_slice(),
+            u64::MAX,
+            ReadOptions {
+                prefix_hint: None,
+                ignore_range_tombstone: false,
+                check_bloom_filter: false,
+                retention_seconds: None,
+                table_id: TableId { table_id: 2333 },
+                read_version_from_backup: false,
+            },
+        )
+        .await
+        .unwrap()
+        .is_none());
+    let stream = hummock_storage
+        .iter(
+            (Unbounded, Unbounded),
+            u64::MAX,
+            ReadOptions {
+                prefix_hint: None,
+                ignore_range_tombstone: false,
+                check_bloom_filter: false,
+                retention_seconds: None,
+                table_id: TableId { table_id: 2333 },
+                read_version_from_backup: false,
+            },
+        )
+        .await
+        .unwrap();
+    pin_mut!(stream);
+    assert!(stream.try_next().await.unwrap().is_none());
+}
 
 #[tokio::test]
 async fn test_basic_v1() {
