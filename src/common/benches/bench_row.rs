588c2a7ed9ea80c75036963e014957e6cc615844
Eurekaaw <yifei.c.wei@gmail.com>
2023-01-14 15:15:30 -0500

    add benchmark for row-level encoding and decoding
    Signed-off-by: Eurekaaw <yifei.c.wei@gmail.com>

---
+++ src/common/benches/bench_row.rs
@@ -0,0 +1,235 @@
+// Copyright 2023 Singularity Data
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+use std::collections::HashMap;
+
+use criterion::{black_box, criterion_group, criterion_main, Criterion};
+use risingwave_common::catalog::ColumnId;
+use risingwave_common::error::Result;
+use risingwave_common::row::{OwnedRow, Row, RowDeserializer};
+use risingwave_common::types::{DataType, Datum, ScalarImpl};
+use risingwave_common::util::ordered::OrderedRowSerde;
+use risingwave_common::util::sort_util::OrderType;
+use risingwave_common::util::value_encoding::row_encoding;
+
+struct Case {
+    name: String,
+    schema: Vec<DataType>,
+    column_ids: Vec<ColumnId>,
+    rows: Vec<OwnedRow>,
+    needed_schema: Vec<DataType>,
+    needed_ids: Vec<ColumnId>,
+}
+
+impl Case {
+    pub fn new(
+        name: &str,
+        schema: Vec<DataType>,
+        column_ids: Vec<ColumnId>,
+        rows: Vec<OwnedRow>,
+        needed_schema: Option<Vec<DataType>>,
+        needed_ids: Option<Vec<ColumnId>>,
+    ) -> Self {
+        Self {
+            name: name.to_string(),
+            schema: schema.clone(),
+            column_ids: column_ids.clone(),
+            rows,
+            needed_ids: needed_ids.unwrap_or(column_ids.clone()),
+            needed_schema: needed_schema.unwrap_or(schema.clone()),
+        }
+    }
+}
+
+fn memcmp_encode(c: &Case) -> Vec<Vec<u8>> {
+    let serde = OrderedRowSerde::new(
+        c.schema.clone(),
+        vec![OrderType::Descending; c.schema.len()],
+    );
+    let mut array = vec![];
+    for row in &c.rows {
+        let mut row_bytes = vec![];
+        black_box(serde.serialize(row, &mut row_bytes));
+        array.push(row_bytes);
+    }
+    array
+}
+
+fn basic_encode(c: &Case) -> Vec<Vec<u8>> {
+    let mut array = vec![];
+    for row in &c.rows {
+        let row_encoding = black_box(row.value_serialize());
+        array.push(row_encoding);
+    }
+    array
+}
+
+fn column_aware_encode(c: &Case) -> Vec<Vec<u8>> {
+    let seralizer = row_encoding::Serializer::new(&c.column_ids);
+    let mut array = vec![];
+    for row in &c.rows {
+        let row_bytes = black_box(seralizer.serialize_row_column_aware(row));
+        array.push(row_bytes);
+    }
+    array
+}
+
+fn memcmp_decode(c: &Case, bytes: &Vec<Vec<u8>>) -> Result<Vec<Vec<Datum>>> {
+    let serde = OrderedRowSerde::new(
+        c.schema.clone(),
+        vec![OrderType::Descending; c.schema.len()],
+    );
+    let column_id_to_index = c
+        .column_ids
+        .iter()
+        .enumerate()
+        .map(|(v, k)| (k, v))
+        .collect::<HashMap<_, _>>();
+    let needed_to_row = c
+        .needed_ids
+        .iter()
+        .map(|id| (id, *column_id_to_index.get(id).unwrap_or(&65536)))
+        .collect::<HashMap<_, _>>();
+    let mut res = vec![];
+    for byte in bytes.iter().enumerate() {
+        let row = black_box(serde.deserialize(&byte.1)?).into_inner();
+        let mut needed = vec![None; c.needed_ids.len()];
+        for (i, c) in c.needed_ids.iter().enumerate() {
+            let ri = *needed_to_row.get(c).unwrap();
+            if ri != 65536 {
+                if let Some(v) = &row[ri] {
+                    needed[i] = Some(v.clone());
+                }
+            }
+        }
+        res.push(needed);
+    }
+    Ok(res)
+}
+
+fn basic_decode(c: &Case, bytes: &Vec<Vec<u8>>) -> Result<Vec<Vec<Datum>>> {
+    let deserializer = RowDeserializer::new(c.schema.clone());
+    let column_id_to_index = c
+        .column_ids
+        .iter()
+        .enumerate()
+        .map(|(v, k)| (k, v))
+        .collect::<HashMap<_, _>>();
+    let needed_to_row = c
+        .needed_ids
+        .iter()
+        .map(|id| (id, *column_id_to_index.get(id).unwrap_or(&65536)))
+        .collect::<HashMap<_, _>>();
+    let mut res = vec![];
+    for byte in bytes {
+        let row = black_box(deserializer.deserialize(&byte[..])?).into_inner();
+        let mut needed = vec![None; c.needed_ids.len()];
+        for (i, c) in c.needed_ids.iter().enumerate() {
+            let ri = *needed_to_row.get(c).unwrap();
+            if ri != 65536 {
+                if let Some(v) = &row[ri] {
+                    needed[i] = Some(v.clone());
+                }
+            }
+        }
+        res.push(needed);
+    }
+    Ok(res)
+}
+
+fn column_aware_decode(c: &Case, bytes: &Vec<Vec<u8>>) -> Result<Vec<Vec<Datum>>> {
+    let deserializer = row_encoding::Deserializer::new(&c.needed_ids, &c.needed_schema);
+    let mut res = vec![];
+    for byte in bytes {
+        let row = black_box(deserializer.decode(byte)?);
+        res.push(row);
+    }
+    Ok(res)
+}
+
+fn bench_row(c: &mut Criterion) {
+    let cases = vec![
+        Case::new(
+            "Int16",
+            vec![DataType::INT16],
+            vec![ColumnId::new(0)],
+            vec![OwnedRow::new(vec![Some(ScalarImpl::Int16(5))]); 1000],
+            None,
+            None,
+        ),
+        Case::new(
+            "Int16 and String",
+            vec![DataType::Int16, DataType::Varchar],
+            vec![ColumnId::new(0), ColumnId::new(1)],
+            vec![
+                OwnedRow::new(vec![
+                    Some(ScalarImpl::Int16(5)),
+                    Some(ScalarImpl::Utf8("abc".into()))
+                ]);
+                1000
+            ],
+            None,
+            None,
+        ),
+        Case::new(
+            "Int16 and String (Only need String)",
+            vec![DataType::Int16, DataType::Varchar],
+            vec![ColumnId::new(0), ColumnId::new(1)],
+            vec![
+                OwnedRow::new(vec![
+                    Some(ScalarImpl::Int16(5)),
+                    Some(ScalarImpl::Utf8("abc".into()))
+                ]);
+                1000
+            ],
+            Some(vec![DataType::Varchar]),
+            Some(vec![ColumnId::new(1)]),
+        ),
+    ];
+    for case in &cases {
+        c.bench_function(
+            format!("memcmp encoding on {}", case.name).as_str(),
+            |bencher| bencher.iter(|| memcmp_encode(case)),
+        );
+        c.bench_function(
+            format!("basic encoding on {}", case.name).as_str(),
+            |bencher| bencher.iter(|| basic_encode(case)),
+        );
+        c.bench_function(
+            format!("column aware encoding on {}", case.name).as_str(),
+            |bencher| bencher.iter(|| column_aware_encode(case)),
+        );
+    }
+
+    for case in &cases {
+        let encode_result = memcmp_encode(&case);
+        c.bench_function(
+            format!("memcmp encoding on {}", case.name).as_str(),
+            |bencher| bencher.iter(|| memcmp_decode(case, &encode_result)),
+        );
+        let encode_result = basic_encode(&case);
+        c.bench_function(
+            format!("basic decoding on {}", case.name).as_str(),
+            |bencher| bencher.iter(|| basic_decode(case, &encode_result)),
+        );
+        let encode_result = column_aware_encode(&case);
+        c.bench_function(
+            format!("column aware encoding on {}", case.name).as_str(),
+            |bencher| bencher.iter(|| column_aware_decode(case, &encode_result)),
+        );
+    }
+}
+
+criterion_group!(benches, bench_row);
+criterion_main!(benches);
