420b8480dd9985c253a80edab8b11df01714c76f
Bugen Zhao <i@bugenzhao.com>
2023-01-19 13:36:05 +0800

    handle query statement
    Signed-off-by: Bugen Zhao <i@bugenzhao.com>

--- src/tests/simulation/src/slt.rs
+++ src/tests/simulation/src/slt.rs
@@ -64,7 +64,7 @@ pub async fn run_slt_task(cluster: Arc<Cluster>, glob: &str, opts: &KillOpts) {
             if let sqllogictest::Record::Halt { .. } = record {
                 break;
             }
-            let (is_create_table_as, is_create, is_drop, is_write) =
+            let (is_create_table_as, is_create, is_drop, is_dml, is_flush) =
                 if let sqllogictest::Record::Statement { sql, .. } = &record {
                     let is_create_table_as = is_create_table_as(sql);
                     let sql =
@@ -73,14 +73,32 @@ pub async fn run_slt_task(cluster: Arc<Cluster>, glob: &str, opts: &KillOpts) {
                         is_create_table_as,
                         !is_create_table_as && sql == "create",
                         sql == "drop",
-                        sql == "insert" || sql == "update" || sql == "delete" || sql == "flush",
+                        sql == "insert" || sql == "update" || sql == "delete",
+                        sql == "flush",
+                    )
+                } else if let sqllogictest::Record::Query { sql, .. } = &record {
+                    let sql =
+                        (sql.trim_start().split_once(' ').unwrap_or_default().0).to_lowercase();
+                    (
+                        false,
+                        false,
+                        false,
+                        sql == "insert" || sql == "update" || sql == "delete",
+                        false,
                     )
                 } else {
-                    (false, false, false, false)
+                    (false, false, false, false, false)
                 };
-            // we won't kill during create/insert/update/delete/flush since the atomicity is not
+
+            // Since we've configured the session to always enable implicit flush, we don't need to
+            // execute `FLUSH` statements.
+            if is_flush {
+                continue;
+            }
+
+            // We won't kill during create/insert/update/delete since the atomicity is not
             // guaranteed. Notice that `create table as` is also not atomic in our system.
-            if is_write || is_create_table_as {
+            if is_dml || is_create_table_as {
                 if !kill {
                     if let Err(e) = tester.run_async(record).await {
                         panic!("{}", e);
@@ -107,7 +125,9 @@ pub async fn run_slt_task(cluster: Arc<Cluster>, glob: &str, opts: &KillOpts) {
                 }
                 continue;
             }
-            if !kill || is_write || is_create_table_as {
+
+            // For normal records.
+            if !kill {
                 match tester.run_async(record).await {
                     Ok(_) => continue,
                     Err(e) => panic!("{}", e),
