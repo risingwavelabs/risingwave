c0c9edb629db4caf01853fc3b2c13e5a347eb971
Bugen Zhao <i@bugenzhao.com>
2023-02-01 23:50:28 +0800

    singleton req

--- src/meta/src/stream/stream_graph/schedule.rs
+++ src/meta/src/stream/stream_graph/schedule.rs
@@ -47,12 +47,17 @@ enum Fact {
         to: Id,
         dt: DispatcherType,
     },
-    /// A distribution requirement for a building fragment.
-    InternalReq { id: Id, dist: DistId },
     /// A distribution requirement for an existing fragment.
     ExternalReq { id: Id, dist: DistId },
-    /// The default distribution for a building fragment.
-    Default(DistId),
+    /// TODO
+    SingletonReq(Id),
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
+enum Result {
+    Required(DistId),
+    DefaultSingleton,
+    DefaultHash,
 }
 
 crepe::crepe! {
@@ -60,43 +65,46 @@ crepe::crepe! {
     struct Input(Fact);
 
     struct Edge(Id, Id, DispatcherType);
-    struct Internal(Id, DistId);
-    struct External(Id, DistId);
-    struct Default(DistId);
+    struct ExternalReq(Id, DistId);
+    struct SingletonReq(Id);
     struct Fragment(Id);
-
     struct Requirement(Id, DistId);
 
     @output
-    struct Success(Id, DistId);
+    struct Success(Id, Result);
     @output
     #[derive(Debug)]
     struct Failed(Id);
 
     // Extract facts.
     Edge(from, to, dt) <- Input(f), let Fact::Edge { from, to, dt } = f;
-    Internal(id, dist) <- Input(f), let Fact::InternalReq { id, dist } = f;
-    External(id, dist) <- Input(f), let Fact::ExternalReq { id, dist } = f;
-    Default(dist) <- Input(f), let Fact::Default(dist) = f;
+    ExternalReq(id, dist) <- Input(f), let Fact::ExternalReq { id, dist } = f;
+    SingletonReq(id) <- Input(f), let Fact::SingletonReq(id) = f;
 
     // Internal fragments.
-    Fragment(x) <- Edge(x, _, _), !External(x, _);
-    Fragment(y) <- Edge(_, y, _), !External(y, _);
+    Fragment(x) <- Edge(x, _, _), !ExternalReq(x, _);
+    Fragment(y) <- Edge(_, y, _), !ExternalReq(y, _);
 
     // Requirements in the facts.
-    Requirement(x, d) <- Internal(x, d);
-    Requirement(x, d) <- External(x, d);
+    Requirement(x, d) <- ExternalReq(x, d);
     // Requirements of `NoShuffle` edges.
     Requirement(x, d) <- Edge(x, y, NoShuffle), Requirement(y, d);
     Requirement(y, d) <- Edge(x, y, NoShuffle), Requirement(x, d);
 
+    // TODO
+    SingletonReq(y) <- Edge(_, y, Simple);
+
     // Multiple requirements lead to failure.
     Failed(x) <- Requirement(x, d1), Requirement(x, d2), (d1 != d2);
+    // TODO
+    Failed(x) <- SingletonReq(x), Requirement(x, d), let DistId::Hash(_) = d;
 
     // Take the single requirement as the result.
-    Success(x, d) <- Fragment(x), Requirement(x, d), !Failed(x);
+    Success(x, Result::Required(d)) <- Fragment(x), Requirement(x, d), !Failed(x);
     // Take the default distribution as the result, if no requirement.
-    Success(x, d) <- Fragment(x), Default(d), !Requirement(x, _);
+    Success(x, Result::DefaultSingleton) <- Fragment(x), SingletonReq(x), !Requirement(x, _);
+    // TODO
+    Success(x, Result::DefaultHash) <- Fragment(x), !SingletonReq(x), !Requirement(x, _);
 }
 
 /// The distribution of a fragment.
@@ -173,7 +181,7 @@ impl Scheduler {
 
         // Build the default hash mapping uniformly.
         let default_hash_mapping = ParallelUnitMapping::build(&round_robin);
-
+        // TODO
         let default_singleton_parallel_unit = round_robin.choose(&mut thread_rng()).unwrap().id;
 
         Ok(Self {
@@ -193,7 +201,6 @@ impl Scheduler {
         let all_hash_mappings = existing_distribution
             .values()
             .flat_map(|dist| dist.as_hash())
-            .chain(std::iter::once(&self.default_hash_mapping))
             .cloned()
             .unique()
             .collect_vec();
@@ -205,17 +212,10 @@ impl Scheduler {
 
         let mut facts = Vec::new();
 
-        // Default
-        facts.push(Fact::Default(DistId::Hash(
-            hash_mapping_id[&self.default_hash_mapping],
-        )));
-        // Internal
+        // Singletons
         for (&id, fragment) in &graph.graph.fragments {
             if fragment.is_singleton {
-                facts.push(Fact::InternalReq {
-                    id,
-                    dist: DistId::Singleton(self.default_singleton_parallel_unit),
-                });
+                facts.push(Fact::SingletonReq(id));
             }
         }
         // External
@@ -243,10 +243,18 @@ impl Scheduler {
         // Extract the results.
         let distributions = success
             .into_iter()
-            .map(|Success(id, distribution)| {
-                let distribution = match distribution {
-                    DistId::Hash(mapping) => Distribution::Hash(all_hash_mappings[mapping].clone()),
-                    DistId::Singleton(parallel_unit) => Distribution::Singleton(parallel_unit),
+            .map(|Success(id, result)| {
+                let distribution = match result {
+                    Result::Required(DistId::Singleton(parallel_unit)) => {
+                        Distribution::Singleton(parallel_unit)
+                    }
+                    Result::Required(DistId::Hash(mapping)) => {
+                        Distribution::Hash(all_hash_mappings[mapping].clone())
+                    }
+                    Result::DefaultSingleton => {
+                        Distribution::Singleton(self.default_singleton_parallel_unit)
+                    }
+                    Result::DefaultHash => Distribution::Hash(self.default_hash_mapping.clone()),
                 };
                 (id, distribution)
             })
