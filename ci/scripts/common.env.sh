69ef13daa3e7e3cefc341849bde6e27fb05ad0e7
Eric Fu <eric@singularity-data.com>
2023-02-06 19:05:35 +0800

    fix

--- ci/scripts/common.env.sh
+++ ci/scripts/common.env.sh
@@ -3,4 +3,6 @@ export PROTOC_NO_VENDOR=true
 export CARGO_HOME=/risingwave/.cargo
 export RISINGWAVE_CI=true
 export RUST_BACKTRACE=1
-export GIT_SHA=$BUILDKITE_COMMIT
+if [ -n "${BUILDKITE_COMMIT:-}" ]; then
+  export GIT_SHA=$BUILDKITE_COMMIT
+fi
