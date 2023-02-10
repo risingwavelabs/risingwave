d480cde9ec235b0e2216a276cf1e48e56e0d89e4
Xuanwo <github@xuanwo.io>
2023-02-10 16:34:50 +0800

    deps: Bump OpenDAL to 0.26
    Signed-off-by: Xuanwo <github@xuanwo.io>

--- src/object_store/src/object/opendal_engine/hdfs.rs
+++ src/object_store/src/object/opendal_engine/hdfs.rs
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-use opendal::services::hdfs;
+use opendal::services::Hdfs;
 use opendal::Operator;
 
 use super::{EngineType, OpendalObjectStore};
@@ -21,14 +21,14 @@ impl OpendalObjectStore {
     /// create opendal hdfs engine.
     pub fn new_hdfs_engine(namenode: String, root: String) -> ObjectResult<Self> {
         // Create hdfs backend builder.
-        let mut builder = hdfs::Builder::default();
+        let mut builder = Hdfs::default();
         // Set the name node for hdfs.
         builder.name_node(&namenode);
         // Set the root for hdfs, all operations will happen under this root.
         // NOTE: the root must be absolute path.
         builder.root(&root);
 
-        let op: Operator = Operator::new(builder.build()?);
+        let op: Operator = Operator::create(builder)?.finish();
         Ok(Self {
             op,
             engine_type: EngineType::Hdfs,
