50b448255628e9870afb878b91db1d00a3e80a61
Jianwei Huang <1223644280@qq.com>
2023-02-13 18:01:43 +0800

    fix

--- src/risedevtool/src/task/connector_service.rs
+++ src/risedevtool/src/task/connector_service.rs
@@ -33,7 +33,7 @@ impl ConnectorNodeService {
     fn connector_path(&self) -> Result<PathBuf> {
         let prefix_bin = env::var("PREFIX_BIN")?;
         Ok(Path::new(&prefix_bin)
-            .join("../../../../ci/connector-node")
+            .join("connector-node")
             .join("start-service.sh"))
     }
 }
