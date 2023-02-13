risingwavelabs/risingwave

commit 49dc1200c3ba60fe3abef80bb41865a959412087

fix: decouple playground with risedev (#7249)
Previously, the `risingwave playground` command trys to load `risedev.yml` to get the profile for playground. It fallbacks to default configurations if `risedev.yml` is not found (Note: users using pre-built binary always go in this path). This can cause confusion because the configuration will be different in different working directory and even bugs like #7238 (Fix #7238)

This PR makes `playground` self-contained, so that users can run the `risingwave` binary with the same configuration in any directory. The code for playground is also more straitforward and easier to change for developers.

Other minor changes about docker:
- Add a check to ensure the image starts correctly
- Remove `docker/build.sh`, which was used to build individual binaries. They are deprecated after #6653
- Fix `docker-compose.yml`: use latest image
- Add some docs about docker

Approved-By: BugenZhao
Approved-By: huangjw806
Approved-By: fuyufjh

src/cmd_all/src/playground.rs
@@ -12,34 +12,108 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-use std::env;
 use std::ffi::OsString;
-use std::net::SocketAddr;
-use std::path::PathBuf;
-use std::process::Command;
-use std::str::FromStr;
+use std::io::Write;
+use std::sync::LazyLock;
 
-use anyhow::{anyhow, Result};
+use anyhow::Result;
 use clap::StructOpt;
-use risedev::{
-    CompactorService, ComputeNodeService, ConfigExpander, FrontendService, HummockInMemoryStrategy,
-    MetaNodeService, ServiceConfig,
-};
 use risingwave_common::config::load_config;
+use tempfile::TempPath;
 use tokio::signal;
-
-fn load_risedev_config(profile: &str) -> Result<(Option<String>, Vec<ServiceConfig>)> {
-    let (config_path, risedev_config) = ConfigExpander::expand(".", profile)?;
-    let services = ConfigExpander::deserialize(&risedev_config)?;
-
-    Ok((config_path, services))
-}
 
 pub enum RisingWaveService {
     Compute(Vec<OsString>),
     Meta(Vec<OsString>),
     Frontend(Vec<OsString>),
     Compactor(Vec<OsString>),
+}
+
+impl RisingWaveService {
+    /// Extend additional arguments to the service.
+    fn extend_args(&mut self, args: &[&str]) {
+        match self {
+            RisingWaveService::Compute(args0)
+            | RisingWaveService::Meta(args0)
+            | RisingWaveService::Frontend(args0)
+            | RisingWaveService::Compactor(args0) => args0.extend(args.iter().map(|s| s.into())),
+        }
+    }
+}
+
+const IDLE_EXIT_SECONDS: u64 = 1800;
+
+/// Embed the config file and create a temporary file at runtime.
+static CONFIG_PATH_WITH_IDLE_EXIT: LazyLock<TempPath> = LazyLock::new(|| {
+    let mut file = tempfile::NamedTempFile::new().expect("failed to create temp config file");
+    write!(
+        file,
+        "[meta]
+disable_recovery = true
+dangerous_max_idle_secs = {IDLE_EXIT_SECONDS}
+max_heartbeat_interval_secs = 600",
+    )
+    .expect("failed to write config file");
+    file.into_temp_path()
+});
+
+fn get_services(profile: &str) -> (Vec<RisingWaveService>, bool) {
+    let mut services = match profile {
+        "playground" => vec![
+            RisingWaveService::Meta(osstrs([])),
+            RisingWaveService::Compute(osstrs([])),
+            RisingWaveService::Frontend(osstrs([])),
+        ],
+        "playground-3cn" => vec![
+            RisingWaveService::Meta(osstrs([])),
+            RisingWaveService::Compute(osstrs(["--host", "localhost:5687"])),
+            RisingWaveService::Compute(osstrs(["--host", "localhost:5688"])),
+            RisingWaveService::Compute(osstrs(["--host", "localhost:5689"])),
+            RisingWaveService::Frontend(osstrs([])),
+        ],
+        "online-docker-playground" | "docker-playground" => {
+            vec![
+                RisingWaveService::Meta(osstrs([
+                    "--listen-addr",
+                    "0.0.0.0:5690",
+                    "--host",
+                    "127.0.0.1",
+                    "--dashboard-host",
+                    "0.0.0.0:5691",
+                ])),
+                RisingWaveService::Compute(osstrs([
+                    "--host",
+                    "0.0.0.0:5688",
+                    "--client-address",
+                    "127.0.0.1:5688",
+                ])),
+                RisingWaveService::Frontend(osstrs([
+                    "--host",
+                    "0.0.0.0:4566",
+                    "--client-address",
+                    "127.0.0.1:4566",
+                ])),
+            ]
+        }
+        _ => {
+            tracing::warn!("Unknown playground profile. All components will be started using the default command line options.");
+            return get_services("playground");
+        }
+    };
+    let idle_exit = profile != "docker-playground";
+    if idle_exit {
+        services.iter_mut().for_each(|s| {
+            s.extend_args(&[
+                "--config-path",
+                &CONFIG_PATH_WITH_IDLE_EXIT.as_os_str().to_string_lossy(),
+            ])
+        })
+    }
+    (services, idle_exit)
+}
+
+fn osstrs<const N: usize>(s: [&str; N]) -> Vec<OsString> {
+    s.iter().map(OsString::from).collect()
 }
 
 pub async fn playground() -> Result<()> {
@@ -51,95 +125,7 @@ pub async fn playground() -> Result<()> {
         "playground".to_string()
     };
 
-    let apply_config_file = |cmd: &mut Command, config_path: Option<&str>| {
-        if let Some(c) = config_path {
-            println!("config file: {}", c);
-            cmd.arg("--config-path").arg(c);
-        }
-    };
-
-    let services = match load_risedev_config(&profile) {
-        Err(e) => {
-            tracing::warn!("Failed to load risedev config. All components will be started using the default command line options.\n{}", e);
-            vec![
-                RisingWaveService::Meta(vec!["--backend".into(), "mem".into()]),
-                RisingWaveService::Compute(vec!["--state-store".into(), "hummock+memory".into()]),
-                RisingWaveService::Frontend(vec![]),
-            ]
-        }
-        Ok((config_path, services)) => {
-            tracing::info!(
-                "Launching services from risedev config playground using profile: {}",
-                profile
-            );
-
-            let compute_node_count = services
-                .iter()
-                .filter(|s| matches!(s, ServiceConfig::ComputeNode(_)))
-                .count();
-
-            let mut rw_services = vec![];
-            for service in &services {
-                match service {
-                    ServiceConfig::ComputeNode(c) => {
-                        let mut command = Command::new("compute-node");
-                        ComputeNodeService::apply_command_args(
-                            &mut command,
-                            c,
-                            if compute_node_count > 1 {
-                                HummockInMemoryStrategy::Shared
-                            } else {
-                                HummockInMemoryStrategy::Isolated
-                            },
-                        )?;
-                        apply_config_file(&mut command, config_path.as_deref());
-                        if c.enable_tiered_cache {
-                            let prefix_data = env::var("PREFIX_DATA")?;
-                            command.arg("--file-cache-dir").arg(
-                                PathBuf::from(prefix_data)
-                                    .join("filecache")
-                                    .join(c.port.to_string()),
-                            );
-                        }
-                        rw_services.push(RisingWaveService::Compute(
-                            command.get_args().map(ToOwned::to_owned).collect(),
-                        ));
-                    }
-                    ServiceConfig::MetaNode(c) => {
-                        let mut command = Command::new("meta-node");
-                        MetaNodeService::apply_command_args(&mut command, c)?;
-                        apply_config_file(&mut command, config_path.as_deref());
-                        rw_services.push(RisingWaveService::Meta(
-                            command.get_args().map(ToOwned::to_owned).collect(),
-                        ));
-                    }
-                    ServiceConfig::Frontend(c) => {
-                        let mut command = Command::new("frontend-node");
-                        FrontendService::apply_command_args(&mut command, c)?;
-                        apply_config_file(&mut command, config_path.as_deref());
-                        rw_services.push(RisingWaveService::Frontend(
-                            command.get_args().map(ToOwned::to_owned).collect(),
-                        ));
-                    }
-                    ServiceConfig::Compactor(c) => {
-                        let mut command = Command::new("compactor");
-                        CompactorService::apply_command_args(&mut command, c)?;
-                        apply_config_file(&mut command, config_path.as_deref());
-                        rw_services.push(RisingWaveService::Compactor(
-                            command.get_args().map(ToOwned::to_owned).collect(),
-                        ));
-                    }
-                    _ => {
-                        return Err(anyhow!("unsupported service: {:?}", service));
-                    }
-                }
-            }
-            rw_services
-        }
-    };
-
-    let mut port = 4566;
-    let mut idle = None;
+    let (services, idle_exit) = get_services(&profile);
 
     for service in services {
         match service {
@@ -148,18 +134,17 @@ pub async fn playground() -> Result<()> {
                 tracing::info!("starting meta-node thread with cli args: {:?}", opts);
                 let opts = risingwave_meta::MetaNodeOpts::parse_from(opts);
 
-                let config = load_config(&opts.config_path);
-                idle = config.meta.dangerous_max_idle_secs;
+                let _config = load_config(&opts.config_path);
 
                 tracing::info!("opts: {:#?}", opts);
                 let _meta_handle = tokio::spawn(async move {
                     risingwave_meta::start(opts).await;
                     tracing::warn!("meta is stopped, shutdown all nodes");
                     // As a playground, it's fine to just kill everything.
-                    if let Some(idle) = idle {
+                    if idle_exit {
                         eprintln!("{}",
                         console::style(format_args!(
-                                "RisingWave playground exited after being idle for {idle} seconds. Bye!"
+                                "RisingWave playground exited after being idle for {IDLE_EXIT_SECONDS} seconds. Bye!"
                             )).bold());
                     }
                     std::process::exit(0);
@@ -179,7 +164,6 @@ pub async fn playground() -> Result<()> {
                 opts.insert(0, "frontend-node".into());
                 tracing::info!("starting frontend-node thread with cli args: {:?}", opts);
                 let opts = risingwave_frontend::FrontendOpts::parse_from(opts);
-                port = SocketAddr::from_str(&opts.host).unwrap().port();
                 tracing::info!("opts: {:#?}", opts);
                 let _frontend_handle =
                     tokio::spawn(async move { risingwave_frontend::start(opts).await });
@@ -203,10 +187,10 @@ pub async fn playground() -> Result<()> {
         "* {} RisingWave playground SHOULD NEVER be used in benchmarks and production environment!!!\n  It is fully in-memory",
         console::style("WARNING:").red().bold(),
     );
-    if let Some(idle) = idle {
+    if idle_exit {
         eprintln!(
             " and will be automatically stopped after being idle for {}.",
-            console::style(format_args!("{idle}s")).dim()
+            console::style(format_args!("{IDLE_EXIT_SECONDS}s")).dim()
         );
     } else {
         eprintln!();
@@ -217,7 +201,7 @@ pub async fn playground() -> Result<()> {
     );
     eprintln!(
         "* Run {} in a different terminal to start Postgres interactive shell.",
-        console::style(format_args!("psql -h localhost -p {port} -d dev -U root"))
+        console::style(format_args!("psql -h localhost -p 4566 -d dev -U root"))
             .blue()
             .bold()
     );

ci/scripts/docker.sh
@@ -3,6 +3,8 @@
 # Exits as soon as any line fails.
 set -euo pipefail
 
+# Build docker image ${BUILDKITE_COMMIT}-${arch}
+
 date="$(date +%Y%m%d)"
 ghcraddr="ghcr.io/risingwavelabs/risingwave"
 dockerhubaddr="risingwavelabs/risingwave"
@@ -10,6 +12,16 @@ arch="$(uname -m)"
 
 echo "--- docker build and tag"
 docker build -f docker/Dockerfile -t "${ghcraddr}:${BUILDKITE_COMMIT}-${arch}" --target risingwave .
+
+echo "--- check the image can start correctly"
+container_id=$(docker run -d "${ghcraddr}:${BUILDKITE_COMMIT}-${arch}" risingwave)
+ret_code=$(docker inspect --format='{{.State.ExitCode}}' "$container_id")
+if [ "$ret_code" -ne 0 ]; then
+  echo "docker run failed with exit code $ret_code"
+  docker logs "$container_id"
+  exit 1
+fi
+docker kill "$container_id"
 
 echo "--- docker images"
 docker images

ci/scripts/multi-arch-docker.sh
@@ -3,6 +3,13 @@
 # Exits as soon as any line fails.
 set -euo pipefail
 
+# Create multi-arch docker images from ${BUILDKITE_COMMIT}-x86_64 and ${BUILDKITE_COMMIT}-aarch64
+# They are created by ci/scripts/docker.sh
+#
+# Also add addtional tags to the images:
+# nightly-yyyyMMdd: nightly build in main-cron
+# latest: only push to ghcr. dockerhub latest is latest release
+
 date="$(date +%Y%m%d)"
 ghcraddr="ghcr.io/risingwavelabs/risingwave"
 dockerhubaddr="risingwavelabs/risingwave"

docker/Dockerfile
@@ -76,8 +76,6 @@ FROM image-base as risingwave
 LABEL org.opencontainers.image.source https://github.com/risingwavelabs/risingwave
 RUN mkdir -p /risingwave/bin
 COPY --from=builder /risingwave/bin/risingwave /risingwave/bin/risingwave
-# Copy risedev.yml into container
-COPY ./risedev.yml /risedev.yml
 # Set default playground mode to docker-playground profile
 ENV PLAYGROUND_PROFILE docker-playground
 ENTRYPOINT [ "/risingwave/bin/risingwave" ]

docker/build.sh
@@ -1,59 +1,0 @@
-#!/bin/bash
-
-set -e
-
-usage() {
-    {
-        echo "This script builds the docker images"
-        echo ""
-        echo "Usage:"
-        echo "$0 [-h] [-d] [-r] [-k <aws_access_key>] [-s <aws_secret_key>] [-n <namespace>]"
-        echo ""
-        echo "-c    Build one specific component. Valid values are: 'risingwave', 'compute-node', 'meta-node', 'frontend-node', 'compactor-node'"
-        echo "-h    Show this help message"
-    } 1>&2
-
-    exit 1
-}
-
-c=false
-
-while getopts ":c:h" o; do
-    case "${o}" in
-        c)
-            c=${OPTARG}
-            ;;
-        h)
-            usage
-            ;;
-        *)
-            usage
-            ;;
-    esac
-done
-shift $((OPTIND-1))
-
-
-export DOCKER_BUILDKIT=1
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-cd "$DIR"/..
-
-# By default build all or one if user specified that
-components=(
-  "frontend-node"
-  "compute-node"
-  "meta-node"
-  "compactor-node"
-  "risingwave"
-)
-if [[ $c != false ]]; then 
-    components=(
-        "$c"
-    )
-fi 
-
-for component in "${components[@]}"
-do
-  echo "--- docker build and tag : ${component}:latest"
-  docker build -f docker/Dockerfile -t "${component}:latest" --target "${component}" .
-done

docker/docker-compose.yml
@@ -2,7 +2,7 @@
 version: "3"
 services:
   compactor-0:
-    image: "ghcr.io/risingwavelabs/risingwave:nightly-20220620"
+    image: "ghcr.io/risingwavelabs/risingwave:latest"
     command:
       - compactor-node
       - "--host"
@@ -39,7 +39,7 @@ services:
       timeout: 5s
       retries: 5
   compute-node-0:
-    image: "ghcr.io/risingwavelabs/risingwave:nightly-20220620"
+    image: "ghcr.io/risingwavelabs/risingwave:latest"
     command:
       - compute-node
       - "--host"
@@ -121,7 +121,7 @@ services:
       timeout: 5s
       retries: 5
   frontend-node-0:
-    image: "ghcr.io/risingwavelabs/risingwave:nightly-20220620"
+    image: "ghcr.io/risingwavelabs/risingwave:latest"
     command:
       - frontend-node
       - "--host"
@@ -176,7 +176,7 @@ services:
       timeout: 5s
       retries: 5
   meta-node-0:
-    image: "ghcr.io/risingwavelabs/risingwave:nightly-20220620"
+    image: "ghcr.io/risingwavelabs/risingwave:latest"
     command:
       - meta-node
       - "--listen-addr"

risedev.yml
@@ -163,62 +163,6 @@ profile:
       - use: compute-node
       - use: frontend
 
-  ##########################################
-  ### Configuration used for ./risedev p ###
-  ##########################################
-
-  playground:
-    config-path: src/config/playground.toml
-    steps:
-      - use: meta-node
-      - use: compute-node
-        total-memory-bytes: 2147483648
-        parallelism: 2
-      - use: frontend
-
-  playground-3cn:
-    config-path: src/config/playground.toml
-    steps:
-      - use: meta-node
-      - use: compute-node
-        port: 5687
-        exporter-port: 1222
-      - use: compute-node
-        port: 5688
-        exporter-port: 1223
-      - use: compute-node
-        port: 5689
-        exporter-port: 1224
-      - use: frontend
-      - use: compactor
-
-  docker-playground:
-    config-path: src/config/playground.toml
-    steps:
-      - use: meta-node
-        listen-address: "0.0.0.0"
-        address: "127.0.0.1"
-      - use: compute-node
-        listen-address: "0.0.0.0"
-        address: "127.0.0.1"
-      - use: frontend
-        listen-address: "0.0.0.0"
-        address: "127.0.0.1"
-
-  # The profile for the online playground
-  online-docker-playground:
-    config-path: src/config/playground.toml
-    steps:
-      - use: meta-node
-        listen-address: "0.0.0.0"
-        address: "127.0.0.1"
-      - use: compute-node
-        listen-address: "0.0.0.0"
-        address: "127.0.0.1"
-      - use: frontend
-        listen-address: "0.0.0.0"
-        address: "127.0.0.1"
-
   ######################################
   ### Configurations used in Compose ###
   ######################################

src/cmd_all/Cargo.toml
@@ -27,6 +27,7 @@ risingwave_meta = { path = "../meta" }
 risingwave_rt = { path = "../utils/runtime" }
 sync-point = { path = "../utils/sync-point" }
 task_stats_alloc = { path = "../utils/task_stats_alloc" }
+tempfile = "3.3.0"
 tikv-jemallocator = { version = "0.5", features = ["profiling", "stats"] }
 tokio = { version = "0.2", package = "madsim-tokio", features = [
     "rt",

commit 89ff6d9a4a11f6f32094e2c775d7e426c811c097

fix: dashboard not enabled in playground mode  (#7874)
Fix #7813.

The bug was introduced in #7249. Before that, `MetaNodeService::apply_command_args` was called [here](https://github.com/risingwavelabs/risingwave/pull/7249/files#diff-e1cea435bf1e8e5ef4692d74e955179ec3f3579c6e5e596d51849fe89c54b8acL110) to generate a set of arguments including `--dashboard-host`. Some other arguments were removed (intendedly?) as well, PTAL @xxchan.

Approved-By: BugenZhao
Approved-By: TennyZhuang
Approved-By: xxchan

.config/hakari.toml
@@ -3,7 +3,7 @@
 
 hakari-package = "workspace-hack"
 
-# Format for `workspace-hack = ...` lines in other Cargo.tomls. Requires cargo-hakari 0.9.8 or above.
+# Format for `workspace-hack = ...` lines in other Cargo.tomls. Requires cargo-hakari 0.9.22 or above.
 dep-format-version = "4"
 
 # Setting workspace.resolver = "2" in the root Cargo.toml is HIGHLY recommended.

src/cmd_all/src/playground.rs
@@ -59,12 +59,12 @@ max_heartbeat_interval_secs = 600",
 fn get_services(profile: &str) -> (Vec<RisingWaveService>, bool) {
     let mut services = match profile {
         "playground" => vec![
-            RisingWaveService::Meta(osstrs([])),
+            RisingWaveService::Meta(osstrs(["--dashboard-host", "0.0.0.0:5691"])),
             RisingWaveService::Compute(osstrs([])),
             RisingWaveService::Frontend(osstrs([])),
         ],
         "playground-3cn" => vec![
-            RisingWaveService::Meta(osstrs([])),
+            RisingWaveService::Meta(osstrs(["--dashboard-host", "0.0.0.0:5691"])),
             RisingWaveService::Compute(osstrs([
                 "--listen-addr",
                 "127.0.0.1:5687",