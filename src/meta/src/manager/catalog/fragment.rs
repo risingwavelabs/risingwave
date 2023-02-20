e633c52fc903e1aa89a8bcd3ceca34bf0c3c52c1
August <pin@singularity-data.com>
2023-02-20 15:01:28 +0800

    fmt

--- src/meta/src/manager/catalog/fragment.rs
+++ src/meta/src/manager/catalog/fragment.rs
@@ -824,12 +824,12 @@ where
     /// Get and filter the upstream `Materialize` fragments of the specified relations.
     pub async fn get_upstream_mview_fragments(
         &self,
-        dependent_relations: &HashSet<TableId>,
+        dependent_relation_ids: &HashSet<TableId>,
     ) -> MetaResult<HashMap<TableId, Fragment>> {
         let map = &self.core.read().await.table_fragments;
         let mut fragments = HashMap::new();
 
-        for &table_id in dependent_relations {
+        for &table_id in dependent_relation_ids {
             if let Some(table_fragments) = map.get(&table_id) && let Some(fragment) = table_fragments.mview_fragment() {
                 fragments.insert(table_id, fragment);
             }
