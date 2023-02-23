b85274eb1dd5dc77f0a6667febd4eace6eaa3f5f
Noel Kwan <noelkwan1998@gmail.com>
2023-02-23 20:08:37 +0800

    update freeze.zip

--- src/tests/sqlsmith/scripts/gen_queries.sh
+++ src/tests/sqlsmith/scripts/gen_queries.sh
@@ -3,34 +3,48 @@
 set -euxo pipefail
 
 export TEST_NUM=32
+export RW_HOME="../../../.."
 export LOGDIR=".risingwave/log"
-export TESTS_FOLDER="./src/tests/sqlsmith/tests"
-export BASE_FOLDER="$TESTS_FOLDER/freeze"
+export TESTS_FOLDER="src/tests/sqlsmith/tests"
+export OUTDIR="$TESTS_FOLDER/freeze"
+export TESTDATA="src/tests/sqlsmith/tests/testdata"
+export MADSIM_BIN="target/sim/ci-sim/risingwave_simulation"
+
+build_madsim() {
+  cargo make sslt-build-all --profile ci-sim
+}
 
 generate_deterministic() {
-    seq "$TEST_NUM" | \
-      parallel "mkdir -p $BASE_FOLDER/{}; \
-       MADSIM_TEST_SEED={} \
-       ./target/sim/ci-sim/risingwave_simulation \
+  seq "$TEST_NUM" | \
+    parallel "mkdir -p $OUTDIR/{}; \
+      MADSIM_TEST_SEED={} $MADSIM_BIN \
         --sqlsmith 100 \
-        --generate-sqlsmith-queries $BASE_FOLDER/{} \
-        ./src/tests/sqlsmith/tests/testdata \
-         2> $LOGDIR/fuzzing-{}.log && rm $LOGDIR/fuzzing-{}.log"
+        --generate-sqlsmith-queries $OUTDIR/{} \
+        $TESTDATA \
+        2> $LOGDIR/fuzzing-{}.log && rm $LOGDIR/fuzzing-{}.log"
 }
 
 generate_sqlsmith() {
-  mkdir -p "$BASE_FOLDER/$1"
+  mkdir -p "$OUTDIR/$1"
   ./risedev d
   ./target/debug/sqlsmith test \
     --testdata ./src/tests/sqlsmith/tests/testdata \
-    --generate "$BASE_FOLDER/$1"
+    --generate "$OUTDIR/$1"
 }
 
-cargo make sslt-build-all --profile ci-sim
+create_artifacts() {
+  cd $TESTS_FOLDER
+  zip -r "freeze.zip" freeze
+  rm -r freeze
+  cd -
+}
 
-generate_deterministic
+main() {
+  cd $RW_HOME
+  build_madsim
+  generate_deterministic
+  create_artifacts
+  cd -
+}
 
-cd $TESTS_FOLDER
-zip -r "freeze.zip" freeze
-rm -r freeze
-cd -
+main
