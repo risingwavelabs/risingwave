4c9ef8416b682be0558c8bbe49e894987acba0ad
Xiangjin <xiangjin@risingwave-labs.com>
2023-02-25 12:33:26 +0800

    disable last few cases

--- src/tests/regress/data/sql/jsonb.sql
+++ src/tests/regress/data/sql/jsonb.sql
@@ -60,11 +60,11 @@ SELECT ''::jsonb;				-- ERROR, no value
 SELECT '    '::jsonb;			-- ERROR, no value
 
 -- Multi-line JSON input to check ERROR reporting
-SELECT '{
-		"one": 1,
-		"two":"two",
-		"three":
-		true}'::jsonb; -- OK
+--@ SELECT '{
+--@ 		"one": 1,
+--@ 		"two":"two",
+--@ 		"three":
+--@ 		true}'::jsonb; -- OK
 SELECT '{
 		"one": 1,
 		"two":,"two",  -- ERROR extraneous comma before field "two"
@@ -933,12 +933,12 @@ SELECT '{"aa":["a","aaa"],"qq":{"a":"12","b":"16","c":["c1","c2"],"d":{"d1":"d1"
 SELECT '{"aa":["a","aaa"],"qq":{"a":"12","b":"16","c":["c1","c2",["c3"],{"c4":4}],"d":{"d1":"d1","d2":"d2"}}}'::jsonb;
 SELECT '{"ff":["a","aaa"]}'::jsonb;
 
-SELECT
-  '{"ff":{"a":12,"b":16},"qq":123,"x":[1,2],"Y":null}'::jsonb -> 'ff',
-  '{"ff":{"a":12,"b":16},"qq":123,"x":[1,2],"Y":null}'::jsonb -> 'qq',
-  ('{"ff":{"a":12,"b":16},"qq":123,"x":[1,2],"Y":null}'::jsonb -> 'Y') IS NULL AS f,
-  ('{"ff":{"a":12,"b":16},"qq":123,"x":[1,2],"Y":null}'::jsonb ->> 'Y') IS NULL AS t,
-   '{"ff":{"a":12,"b":16},"qq":123,"x":[1,2],"Y":null}'::jsonb -> 'x';
+--@ SELECT
+--@   '{"ff":{"a":12,"b":16},"qq":123,"x":[1,2],"Y":null}'::jsonb -> 'ff',
+--@   '{"ff":{"a":12,"b":16},"qq":123,"x":[1,2],"Y":null}'::jsonb -> 'qq',
+--@   ('{"ff":{"a":12,"b":16},"qq":123,"x":[1,2],"Y":null}'::jsonb -> 'Y') IS NULL AS f,
+--@   ('{"ff":{"a":12,"b":16},"qq":123,"x":[1,2],"Y":null}'::jsonb ->> 'Y') IS NULL AS t,
+--@    '{"ff":{"a":12,"b":16},"qq":123,"x":[1,2],"Y":null}'::jsonb -> 'x';
 
 -- nested containment
 --@ SELECT '{"a":[1,2],"c":"b"}'::jsonb @> '{"a":[1,2]}';
