85646ba62b6fc9d6b37e6fc86141b0db92cb69a1
Bugen Zhao <i@bugenzhao.com>
2023-02-28 00:26:57 +0800

    feat: adding column of table schema change (#8063)
    * remove hanging channels
    
    Signed-off-by: Bugen Zhao <i@bugenzhao.com>
    
    * minor refactor
    
    Signed-off-by: Bugen Zhao <i@bugenzhao.com>
    
    * add procedure
    
    Signed-off-by: Bugen Zhao <i@bugenzhao.com>
    
    * more procedure
    
    Signed-off-by: Bugen Zhao <i@bugenzhao.com>
    
    * more and more procedure
    
    Signed-off-by: Bugen Zhao <i@bugenzhao.com>
    
    * bug fixes
    
    Signed-off-by: Bugen Zhao <i@bugenzhao.com>
    
    * receiver with updated upstream
    
    Signed-off-by: Bugen Zhao <i@bugenzhao.com>
    
    * insert with version
    
    Signed-off-by: Bugen Zhao <i@bugenzhao.com>
    
    * pass dispatcher
    
    Signed-off-by: Bugen Zhao <i@bugenzhao.com>
    
    * make it run
    
    Signed-off-by: Bugen Zhao <i@bugenzhao.com>
    
    * add col index mapping proto
    
    Signed-off-by: Bugen Zhao <i@bugenzhao.com>
    
    * use col index mapping to rewrite
    
    Signed-off-by: Bugen Zhao <i@bugenzhao.com>
    
    * refine docs
    
    Signed-off-by: Bugen Zhao <i@bugenzhao.com>
    
    * update proto
    
    Signed-off-by: Bugen Zhao <i@bugenzhao.com>
    
    * update merges
    
    Signed-off-by: Bugen Zhao <i@bugenzhao.com>
    
    * pause then replace
    
    Signed-off-by: Bugen Zhao <i@bugenzhao.com>
    
    * add tests
    
    Signed-off-by: Bugen Zhao <i@bugenzhao.com>
    
    * refactor to ddl controller
    
    Signed-off-by: Bugen Zhao <i@bugenzhao.com>
    
    * refactor catalog
    
    Signed-off-by: Bugen Zhao <i@bugenzhao.com>
    
    ---------
    
    Signed-off-by: Bugen Zhao <i@bugenzhao.com>

--- src/frontend/src/handler/alter_table.rs
+++ src/frontend/src/handler/alter_table.rs
@@ -15,6 +15,7 @@
 use anyhow::Context;
 use pgwire::pg_response::{PgResponse, StatementType};
 use risingwave_common::error::{ErrorCode, Result};
+use risingwave_common::util::column_index_mapping::ColIndexMapping;
 use risingwave_pb::catalog::Table;
 use risingwave_pb::stream_plan::stream_fragment_graph::Parallelism;
 use risingwave_pb::stream_plan::StreamFragmentGraph;
@@ -127,22 +128,29 @@ pub async fn handle_add_column(
         (graph, table)
     };
 
-    // TODO: for test purpose only, we drop the original table and create a new one. This is wrong
-    // and really dangerous in production.
+    // Calculate the mapping from the original columns to the new columns.
+    let col_index_mapping = ColIndexMapping::new(
+        original_catalog
+            .columns()
+            .iter()
+            .map(|old_c| {
+                table.columns.iter().position(|new_c| {
+                    new_c.get_column_desc().unwrap().column_id == old_c.column_id().get_id()
+                })
+            })
+            .collect(),
+    );
+
     if cfg!(debug_assertions) {
         let catalog_writer = session.env().catalog_writer();
 
-        // TODO: call replace_table RPC
-        // catalog_writer.replace_table(table, graph).await?;
-
         catalog_writer
-            .drop_table(None, original_catalog.id())
+            .replace_table(table, graph, col_index_mapping)
             .await?;
-        catalog_writer.create_table(None, table, graph).await?;
 
         Ok(PgResponse::empty_result_with_notice(
             StatementType::ALTER_TABLE,
-            "The `ALTER TABLE` feature is incomplete and NO DATA is preserved! This feature is not available in production.".to_owned(),
+            "The `ALTER TABLE` feature is incomplete and data will be corrupted! This feature is not available in production.".to_owned(),
         ))
     } else {
         Err(ErrorCode::NotImplemented(
--- src/meta/src/model/stream.rs
+++ src/meta/src/model/stream.rs
@@ -191,6 +191,12 @@ impl TableFragments {
         self.state == State::Created
     }
 
+    /// Set the table ID.
+    // TODO: remove this workaround for replacing table.
+    pub fn set_table_id(&mut self, table_id: TableId) {
+        self.table_id = table_id;
+    }
+
     /// Set the state of the table fragments.
     pub fn set_state(&mut self, state: State) {
         self.state = state;
