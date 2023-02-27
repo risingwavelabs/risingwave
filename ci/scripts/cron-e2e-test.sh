9d0418be56e3845dc05e928613814d61f965e4a8
Noel Kwan <47273164+kwannoel@users.noreply.github.com>
2023-02-27 10:10:47 +0800

    fix(ci): sqlsmith test filepath (#8183)
    * fix filepath difference
    
    * use absolute filepath + run deterministic fuzzing in cron
    
    * fix
    
    * update pr template for sqlsmith
    
    * add up to 100 queries
    
    * generate 100 queries instead
    
    * fix ci scripts
    
    ---------
    
    Co-authored-by: TennyZhuang <zty0826@gmail.com>

--- ci/scripts/cron-e2e-test.sh
+++ ci/scripts/cron-e2e-test.sh
@@ -7,5 +7,4 @@ source ci/scripts/common.env.sh
 export RUN_COMPACTION=1;
 export RUN_META_BACKUP=1;
 export RUN_DELETE_RANGE=1;
-export RUN_DETERMINISTIC_SQLSMITH=1;
 source ci/scripts/run-e2e-test.sh
--- ci/scripts/cron-fuzz-test.sh
+++ ci/scripts/cron-fuzz-test.sh
@@ -6,4 +6,5 @@ set -euo pipefail
 source ci/scripts/common.env.sh
 export RUN_SQLSMITH=1
 export SQLSMITH_COUNT=1000
+export TEST_NUM=100
 source ci/scripts/run-fuzz-test.sh
