50d1486abdc04439735988aacac3557d510ab1bf
Renjie Liu <liurenjie2008@gmail.com>
2023-03-02 15:28:13 +0800

    Revert

--- src/tests/simulation/src/lib.rs
+++ src/tests/simulation/src/lib.rs
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-// #![cfg(madsim)]
+#![cfg(madsim)]
 #![feature(trait_alias)]
 #![feature(lint_reasons)]
 #![feature(once_cell)]
