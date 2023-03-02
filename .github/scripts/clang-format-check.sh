0f254ed7c5fbcd10df4b15868376e3ef14a6faae
tabVersion <tabvision@bupt.icu>
2023-03-02 16:17:25 +0800

    fix
    Signed-off-by: tabVersion <tabvision@bupt.icu>

--- .github/scripts/clang-format-check.sh
+++
@@ -1,5 +0,0 @@
-sudo apt install clang-format
-
-cd connector_node
-clang-format --dry-run -Werror --style="{BasedOnStyle: Google, ReflowComments: false}" proto/**/*.proto\
- || echo "proto format check failed" && exit 1
