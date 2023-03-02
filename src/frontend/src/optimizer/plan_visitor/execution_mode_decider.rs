ba4c23c801251597192f79b471a04852b2357049
Dylan Chen <zilin@singularity-data.com>
2023-03-02 13:53:39 +0800

    limit scan can also run in local mode

--- src/frontend/src/optimizer/plan_visitor/execution_mode_decider.rs
+++ src/frontend/src/optimizer/plan_visitor/execution_mode_decider.rs
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-use crate::optimizer::plan_node::{BatchSeqScan, BatchValues};
+use crate::optimizer::plan_node::{BatchLimit, BatchSeqScan, BatchValues, PlanTreeNodeUnary};
 use crate::optimizer::plan_visitor::PlanVisitor;
 use crate::PlanRef;
 
@@ -20,6 +20,7 @@ use crate::PlanRef;
 pub struct ExecutionModeDecider {}
 
 impl ExecutionModeDecider {
+    /// If the plan should run in local mode, return true; otherwise, return false.
     pub fn run_in_local_mode(plan: PlanRef) -> bool {
         let mut decider = ExecutionModeDecider {};
         decider.visit(plan)
@@ -31,6 +32,10 @@ impl PlanVisitor<bool> for ExecutionModeDecider {
         a & b
     }
 
+    /// Point select, index lookup and two side bound range scan.
+    /// select * from t where id = 1
+    /// select * from t where k = 1
+    /// select * from t where id between 1 and 5
     fn visit_batch_seq_scan(&mut self, batch_seq_scan: &BatchSeqScan) -> bool {
         !batch_seq_scan.scan_ranges().is_empty()
             && batch_seq_scan
@@ -39,7 +44,22 @@ impl PlanVisitor<bool> for ExecutionModeDecider {
                 .all(|x| x.has_eq_conds() || x.two_side_bound())
     }
 
+    /// Simple value select.
+    /// select 1
     fn visit_batch_values(&mut self, _batch_values: &BatchValues) -> bool {
         true
+    }
+
+    /// Limit scan.
+    /// select * from t limit 1
+    /// select * from t order by k limit 1
+    fn visit_batch_limit(&mut self, batch_limit: &BatchLimit) -> bool {
+        if let Some(batch_seq_scan) = batch_limit.input().as_batch_seq_scan()
+            && batch_seq_scan.scan_ranges().is_empty()
+            && batch_limit.limit() + batch_limit.offset() < 100{
+            true
+        } else {
+            self.visit(batch_limit.input())
+        }
     }
 }
