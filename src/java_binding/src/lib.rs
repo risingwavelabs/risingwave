485037ae2628057b993c1492f8eca0bb5c053068
William Wen <william123.wen@gmail.com>
2023-03-03 13:35:50 +0800

    remove C header in comment

--- src/java_binding/src/lib.rs
+++ src/java_binding/src/lib.rs
@@ -209,9 +209,6 @@ where
     }
 }
 
-// JNIEXPORT jint JNICALL Java_com_risingwave_java_binding_Binding_vnodeCount
-// (JNIEnv *, jclass);
-
 #[no_mangle]
 pub extern "system" fn Java_com_risingwave_java_binding_Binding_vnodeCount(
     _env: EnvParam<'_>,
