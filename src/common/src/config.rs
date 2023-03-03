579e060ac31e75a34f723cce11ce41801e46a9bd
gun9nir <gun9nir.guo@gmail.com>
2023-03-03 08:10:01 +0000

    warn

--- src/common/src/config.rs
+++ src/common/src/config.rs
@@ -17,10 +17,12 @@
 //! [`RwConfig`] corresponds to the whole config file and each other config struct corresponds to a
 //! section in `risingwave.toml`.
 
+use std::collections::HashMap;
 use std::fs;
 
 use clap::ValueEnum;
 use serde::{Deserialize, Serialize};
+use serde_json::Value;
 
 /// Use the maximum value for HTTP/2 connection window size to avoid deadlock among multiplexed
 /// streams on the same connection.
@@ -30,6 +32,33 @@ pub const MAX_CONNECTION_WINDOW_SIZE: u32 = (1 << 31) - 1;
 pub const STREAM_WINDOW_SIZE: u32 = 32 * 1024 * 1024; // 32 MB
 /// For non-user-facing components where the CLI arguments do not override the config file.
 pub const NO_OVERRIDE: Option<NoOverride> = None;
+
+macro_rules! for_all_config_sections {
+    ($macro:ident) => {
+        $macro! {
+            { server },
+            { meta },
+            { batch },
+            { streaming },
+            { storage },
+            { storage.file_cache },
+        }
+    };
+}
+
+macro_rules! impl_warn_unrecognized_fields {
+    ($({ $($field_path:ident).+ },)*) => {
+        fn warn_unrecognized_fields(config: &RwConfig) {
+            $(
+                if !config.$($field_path).+.unrecognized.is_empty() {
+                    println!("unrecognized fields in config section [{}]: {:?}", stringify!($($field_path).+), config.$($field_path).+.unrecognized.keys());
+                }
+            )*
+        }
+    };
+}
+
+for_all_config_sections!(impl_warn_unrecognized_fields);
 
 pub fn load_config(path: &str, cli_override: Option<impl OverrideConfig>) -> RwConfig
 where
@@ -42,11 +71,26 @@ where
             .unwrap_or_else(|e| panic!("failed to open config file '{}': {}", path, e));
         toml::from_str(config_str.as_str()).unwrap_or_else(|e| panic!("parse error {}", e))
     };
-    // TODO(zhidong): warn deprecated config
     if let Some(cli_override) = cli_override {
         cli_override.r#override(&mut config);
     }
+    warn_unrecognized_fields(&config);
     config
+}
+
+#[test]
+fn test() {
+    let c: RwConfig = toml::from_str(
+        r#"
+        [batch]
+        aaa = "??"
+
+        [storage.file_cache]
+        bbb = 1
+    "#,
+    )
+    .unwrap();
+    warn_unrecognized_fields(&c);
 }
 
 pub trait OverrideConfig {
@@ -80,8 +124,10 @@ pub struct RwConfig {
     #[serde(default)]
     pub storage: StorageConfig,
 
-    #[serde(default)]
-    pub backup: BackupConfig,
+    // #[serde(default)]
+    // pub backup: BackupConfig,
+    #[serde(flatten)]
+    pub unrecognized: HashMap<String, Value>,
 }
 
 #[derive(Copy, Clone, Debug, Default, ValueEnum, Serialize, Deserialize)]
@@ -93,7 +139,6 @@ pub enum MetaBackend {
 
 /// The section `[meta]` in `risingwave.toml`.
 #[derive(Clone, Debug, Serialize, Deserialize)]
-#[serde(deny_unknown_fields)]
 pub struct MetaConfig {
     /// Threshold used by worker node to filter out new SSTs when scanning object store, during
     /// full SST GC.
@@ -150,6 +195,9 @@ pub struct MetaConfig {
     /// Schedule ttl_reclaim compaction for all compaction groups with this interval.
     #[serde(default = "default::meta::periodic_ttl_reclaim_compaction_interval_sec")]
     pub periodic_ttl_reclaim_compaction_interval_sec: u64,
+
+    #[serde(flatten)]
+    pub unrecognized: HashMap<String, Value>,
 }
 
 impl Default for MetaConfig {
@@ -160,7 +208,6 @@ impl Default for MetaConfig {
 
 /// The section `[server]` in `risingwave.toml`.
 #[derive(Clone, Debug, Serialize, Deserialize)]
-#[serde(deny_unknown_fields)]
 pub struct ServerConfig {
     /// The interval for periodic heartbeat from worker to the meta service.
     #[serde(default = "default::server::heartbeat_interval_ms")]
@@ -178,6 +225,9 @@ pub struct ServerConfig {
     /// 0 = close metrics
     /// >0 = open metrics
     pub metrics_level: u32,
+
+    #[serde(flatten)]
+    pub unrecognized: HashMap<String, Value>,
 }
 
 impl Default for ServerConfig {
@@ -188,7 +238,6 @@ impl Default for ServerConfig {
 
 /// The section `[batch]` in `risingwave.toml`.
 #[derive(Clone, Debug, Serialize, Deserialize)]
-#[serde(deny_unknown_fields)]
 pub struct BatchConfig {
     /// The thread number of the batch task runtime in the compute node. The default value is
     /// decided by `tokio`.
@@ -200,6 +249,9 @@ pub struct BatchConfig {
 
     #[serde(default)]
     pub distributed_query_limit: Option<u64>,
+
+    #[serde(flatten)]
+    pub unrecognized: HashMap<String, Value>,
 }
 
 impl Default for BatchConfig {
@@ -210,7 +262,6 @@ impl Default for BatchConfig {
 
 /// The section `[streaming]` in `risingwave.toml`.
 #[derive(Clone, Debug, Serialize, Deserialize)]
-#[serde(deny_unknown_fields)]
 pub struct StreamingConfig {
     /// The interval of periodic barrier.
     #[serde(default = "default::streaming::barrier_interval_ms")]
@@ -243,6 +294,9 @@ pub struct StreamingConfig {
     /// Max unique user stream errors per actor
     #[serde(default = "default::streaming::unique_user_stream_errors")]
     pub unique_user_stream_errors: usize,
+
+    #[serde(flatten)]
+    pub unrecognized: HashMap<String, Value>,
 }
 
 impl Default for StreamingConfig {
@@ -253,7 +307,6 @@ impl Default for StreamingConfig {
 
 /// The section `[storage]` in `risingwave.toml`.
 #[derive(Clone, Debug, Serialize, Deserialize)]
-#[serde(deny_unknown_fields)]
 pub struct StorageConfig {
     // TODO(zhidong): Remove in 0.1.18 release
     // NOTE: It is now a system parameter and should not be used directly.
@@ -340,6 +393,9 @@ pub struct StorageConfig {
 
     #[serde(default = "default::storage::max_concurrent_compaction_task_number")]
     pub max_concurrent_compaction_task_number: u64,
+
+    #[serde(flatten)]
+    pub unrecognized: HashMap<String, Value>,
 }
 
 impl Default for StorageConfig {
@@ -352,7 +408,6 @@ impl Default for StorageConfig {
 ///
 /// It's put at [`StorageConfig::file_cache`].
 #[derive(Clone, Debug, Serialize, Deserialize)]
-#[serde(deny_unknown_fields)]
 pub struct FileCacheConfig {
     #[serde(default = "default::file_cache::dir")]
     pub dir: String,
@@ -371,6 +426,9 @@ pub struct FileCacheConfig {
 
     #[serde(default = "default::file_cache::cache_file_max_write_size_mb")]
     pub cache_file_max_write_size_mb: usize,
+
+    #[serde(flatten)]
+    pub unrecognized: HashMap<String, Value>,
 }
 
 impl Default for FileCacheConfig {
@@ -391,7 +449,6 @@ pub enum AsyncStackTraceOption {
 ///
 /// It is put at [`BatchConfig::developer`] and [`StreamingConfig::developer`].
 #[derive(Clone, Debug, Serialize, Deserialize)]
-#[serde(deny_unknown_fields)]
 pub struct DeveloperConfig {
     /// The size of the channel used for output to exchange/shuffle.
     #[serde(default = "default::developer::batch_output_channel_size")]
@@ -429,6 +486,9 @@ pub struct DeveloperConfig {
     /// in remote exchange.
     #[serde(default = "default::developer::stream_exchange_batched_permits")]
     pub stream_exchange_batched_permits: usize,
+
+    #[serde(flatten)]
+    pub unrecognized: HashMap<String, Value>,
 }
 
 impl Default for DeveloperConfig {
@@ -439,7 +499,6 @@ impl Default for DeveloperConfig {
 
 /// Configs for meta node backup
 #[derive(Clone, Debug, Serialize, Deserialize)]
-#[serde(deny_unknown_fields)]
 pub struct BackupConfig {
     // TODO: Remove in 0.1.18 release
     // NOTE: It is now a system parameter and should not be used directly.
@@ -451,6 +510,9 @@ pub struct BackupConfig {
     /// Remote directory for storing snapshots.
     #[serde(default = "default::backup::storage_directory")]
     pub storage_directory: String,
+
+    #[serde(flatten)]
+    pub unrecognized: HashMap<String, Value>,
 }
 
 impl Default for BackupConfig {
