7969e369c42a1c066aa977710e199483f0cbe065
Zhidong Guo <52783948+Gun9niR@users.noreply.github.com>
2023-03-06 00:31:11 +0800

    feat(system param): sync param changes to worker nodes (#8271)

--- src/meta/src/manager/system_param/mod.rs
+++ src/meta/src/manager/system_param/mod.rs
@@ -16,12 +16,16 @@ pub mod model;
 
 use std::ops::DerefMut;
 use std::sync::Arc;
+use std::time::Duration;
 
 use risingwave_common::system_param::reader::SystemParamsReader;
 use risingwave_common::system_param::{default, set_system_param};
 use risingwave_common::{for_all_undeprecated_params, key_of};
+use risingwave_pb::meta::subscribe_response::{Info, Operation};
 use risingwave_pb::meta::SystemParams;
+use tokio::sync::oneshot::Sender;
 use tokio::sync::RwLock;
+use tokio::task::JoinHandle;
 
 use self::model::SystemParamsModel;
 use super::NotificationManagerRef;
@@ -29,15 +33,15 @@ use crate::model::{ValTransaction, VarTransaction};
 use crate::storage::{MetaStore, Transaction};
 use crate::{MetaError, MetaResult};
 
-pub type SystemParamManagerRef<S> = Arc<SystemParamManager<S>>;
+pub type SystemParamsManagerRef<S> = Arc<SystemParamsManager<S>>;
 
-pub struct SystemParamManager<S: MetaStore> {
+pub struct SystemParamsManager<S: MetaStore> {
     meta_store: Arc<S>,
     notification_manager: NotificationManagerRef<S>,
     params: RwLock<SystemParams>,
 }
 
-impl<S: MetaStore> SystemParamManager<S> {
+impl<S: MetaStore> SystemParamsManager<S> {
     /// Return error if `init_params` conflict with persisted system params.
     pub async fn new(
         meta_store: Arc<S>,
@@ -89,7 +93,50 @@ impl<S: MetaStore> SystemParamManager<S> {
             ))
             .await;
 
+        // Sync params to worker nodes.
+        self.notify_workers(params).await;
+
         Ok(())
+    }
+
+    // Periodically sync params to worker nodes.
+    pub async fn start_params_notifier(
+        system_params_manager: Arc<Self>,
+    ) -> (JoinHandle<()>, Sender<()>) {
+        const NOTIFY_INTERVAL: Duration = Duration::from_millis(5000);
+
+        let (shutdown_tx, mut shutdown_rx) = tokio::sync::oneshot::channel();
+        let join_handle = tokio::spawn(async move {
+            let mut interval = tokio::time::interval(NOTIFY_INTERVAL);
+            interval.set_missed_tick_behavior(tokio::time::MissedTickBehavior::Delay);
+            loop {
+                tokio::select! {
+                    _ = interval.tick() => {},
+                    _ = &mut shutdown_rx => {
+                        tracing::info!("System params notifier is stopped");
+                        return;
+                    }
+                }
+                system_params_manager
+                    .notify_workers(&*system_params_manager.params.read().await)
+                    .await;
+            }
+        });
+
+        (join_handle, shutdown_tx)
+    }
+
+    // Notify workers of parameter change.
+    async fn notify_workers(&self, params: &SystemParams) {
+        self.notification_manager
+            .notify_frontend(Operation::Update, Info::SystemParams(params.clone()))
+            .await;
+        self.notification_manager
+            .notify_compute(Operation::Update, Info::SystemParams(params.clone()))
+            .await;
+        self.notification_manager
+            .notify_compactor(Operation::Update, Info::SystemParams(params.clone()))
+            .await;
     }
 }
 
