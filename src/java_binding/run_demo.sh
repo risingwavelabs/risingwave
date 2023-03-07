b96f8078a5ff33f4c70673197a2a16e47f156de4
William Wen <william123.wen@gmail.com>
2023-03-07 14:25:51 +0800

    feat(java-binding): add script to generate data for java binding demo

--- src/java_binding/run_demo.sh
+++ src/java_binding/run_demo.sh
@@ -4,12 +4,18 @@
 
 set -ex
 
+
+set +x
+INSERT_DATA=$(python ${RISINGWAVE_ROOT}/src/java_binding/gen-demo-insert-data.py 30000)
+
 psql -d ${DB_NAME} -h localhost -p 4566 -U root << EOF
 DROP TABLE IF EXISTS ${TABLE_NAME};
 CREATE TABLE ${TABLE_NAME} (v1 smallint, v2 int, v3 bigint, v4 float4, v5 float8, v6 bool, v7 varchar, may_null bigint);
-INSERT INTO ${TABLE_NAME} values (1, 1, 1, 1.0, 1.0, false, 'aaa', 1), (2, 2, 2, 2.0, 2.0, true, 'bbb', NULL);
+INSERT INTO ${TABLE_NAME} values ${INSERT_DATA};
 FLUSH;
 EOF
+
+set -x
 
 cd ${RISINGWAVE_ROOT}/java
 
