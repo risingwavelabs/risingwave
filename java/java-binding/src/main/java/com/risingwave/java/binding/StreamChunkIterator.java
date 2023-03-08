da22cd544e7d76a063a39c8e6c34290ed6fd4f28
yufansong <yufansong@foxmail.com>
2023-03-08 13:37:06 -0500

    create stream chunk iterator

---
+++ java/java-binding/src/main/java/com/risingwave/java/binding/StreamChunkIterator.java
@@ -0,0 +1,29 @@
+package com.risingwave.java.binding;
+
+import com.risingwave.proto.JavaBinding.ReadPlan;
+
+public class StreamChunkIterator implements AutoCloseable {
+    private final long pointer;
+    private boolean isClosed;
+
+    public StreamChunkIterator(ReadPlan readPlan) {
+        this.pointer = Binding.iteratorNew(readPlan.toByteArray());
+        this.isClosed = false;
+    }
+
+    public KeyedRow next() {
+        long pointer = Binding.iteratorNext(this.pointer);
+        if (pointer == 0) {
+            return null;
+        }
+        return new KeyedRow(pointer);
+    }
+
+    @Override
+    public void close() {
+        if (!isClosed) {
+            isClosed = true;
+            Binding.iteratorClose(pointer);
+        }
+    }
+}
