bc633ee8cbe5a5b4fe5a91a3c5a1dba05af65272
Xiangjin <xiangjin@risingwave-labs.com>
2023-03-08 16:20:10 +0800

    avoid COPY

--- src/tests/regress/data/sql/interval.sql
+++ src/tests/regress/data/sql/interval.sql
@@ -99,16 +99,16 @@ DROP TABLE INTERVAL_TBL_OF;
 -- stored internally.
 
 CREATE TABLE INTERVAL_MULDIV_TBL (span interval);
-COPY INTERVAL_MULDIV_TBL FROM STDIN;
-41 mon 12 days 360:00
--41 mon -12 days +360:00
--12 days
-9 mon -27 days 12:34:56
--3 years 482 days 76:54:32.189
-4 mon
-14 mon
-999 mon 999 days
-\.
+-- COPY INTERVAL_MULDIV_TBL FROM STDIN;
+INSERT INTO INTERVAL_MULDIV_TBL VALUES
+('41 mon 12 days 360:00'),
+('-41 mon -12 days +360:00'),
+('-12 days'),
+('9 mon -27 days 12:34:56'),
+('-3 years 482 days 76:54:32.189'),
+('4 mon'),
+('14 mon'),
+('999 mon 999 days');
 
 SELECT span * 0.3 AS product
 FROM INTERVAL_MULDIV_TBL;
