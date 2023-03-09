a3ff099cfb2e4683df9aad6a0eb2c7d9bf121fb2
Dylan Chen <zilin@singularity-data.com>
2023-03-09 21:37:42 +0800

    support left outer join, null safe, and fix bug for temporal join executor

--- src/stream/src/executor/temporal_join.rs
+++ src/stream/src/executor/temporal_join.rs
@@ -12,35 +12,54 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+use std::alloc::Global;
+use std::sync::Arc;
+
 use either::Either;
 use futures::stream::{self, PollNext};
 use futures::{StreamExt, TryStreamExt};
 use futures_async_stream::try_stream;
+use local_stats_alloc::{SharedStatsAlloc, StatsAlloc};
+use lru::DefaultHasher;
 use risingwave_common::array::{Op, StreamChunk};
 use risingwave_common::catalog::Schema;
 use risingwave_common::row::{OwnedRow, Row, RowExt};
+use risingwave_common::util::epoch::Epoch;
+use risingwave_common::util::iter_util::ZipEqFast;
 use risingwave_hummock_sdk::{HummockEpoch, HummockReadEpoch};
 use risingwave_storage::table::batch_table::storage_table::StorageTable;
 use risingwave_storage::StateStore;
 
 use super::{Barrier, Executor, Message, MessageStream, StreamExecutorError, StreamExecutorResult};
-use crate::cache::ManagedLruCache;
+use crate::cache::{new_with_hasher_in, ManagedLruCache};
 use crate::common::StreamChunkBuilder;
+use crate::executor::monitor::StreamingMetrics;
+use crate::executor::{ActorContextRef, BoxedExecutor, JoinType, JoinTypePrimitive, PkIndices};
+use crate::task::AtomicU64Ref;
 
-pub struct TemporalJoinExecutor<S: StateStore> {
-    left: Box<dyn Executor>,
-    right: TemporalSide<S>,
-    right_stream: Box<dyn Executor>,
+pub struct TemporalJoinExecutor<S: StateStore, const T: JoinTypePrimitive> {
+    // TODO: update metrics
+    #[allow(dead_code)]
+    ctx: ActorContextRef,
+    left: BoxedExecutor,
+    right: BoxedExecutor,
+    right_table: TemporalSide<S>,
     left_join_keys: Vec<usize>,
     right_join_keys: Vec<usize>,
+    null_safe: Vec<bool>,
     output_indices: Vec<usize>,
+    pk_indices: PkIndices,
     schema: Schema,
     chunk_size: usize,
+    identity: String,
+    // TODO: update metrics
+    #[allow(dead_code)]
+    metrics: Arc<StreamingMetrics>,
 }
 
 struct TemporalSide<S: StateStore> {
     source: StorageTable<S>,
-    cache: ManagedLruCache<OwnedRow, Option<OwnedRow>>,
+    cache: ManagedLruCache<OwnedRow, Option<OwnedRow>, DefaultHasher, SharedStatsAlloc<Global>>,
 }
 
 impl<S: StateStore> TemporalSide<S> {
@@ -63,7 +82,7 @@ impl<S: StateStore> TemporalSide<S> {
         })
     }
 
-    fn update(&mut self, payload: Vec<StreamChunk>, join_keys: &[usize]) {
+    fn update(&mut self, payload: Vec<StreamChunk>, join_keys: &[usize], epoch: u64) {
         payload.iter().flat_map(|c| c.rows()).for_each(|(op, row)| {
             let key = row.project(join_keys).into_owned_row();
             if let Some(value) = self.cache.get_mut(&key) {
@@ -72,7 +91,8 @@ impl<S: StateStore> TemporalSide<S> {
                     Op::Delete | Op::UpdateDelete => *value = None,
                 };
             }
-        })
+        });
+        self.cache.update_epoch(epoch);
     }
 }
 
@@ -100,16 +120,15 @@ pub async fn chunks_until_barrier(stream: impl MessageStream, expected_barrier: 
 
 // Align the left and right inputs according to their barriers,
 // such that in the produced stream, an aligned interval starts with
-// `InternalMessage::Barrier(right_chunks, barrier)` and followed by
-// any number of `InternalMessage::Chunk(left_chunk)`.
-// Notice that we collect all chunks from the right side up front,
-// prioritizing before any chunks from the left side in the same interval.
+// any number of `InternalMessage::Chunk(left_chunk)` and followed by
+// `InternalMessage::Barrier(right_chunks, barrier)`.
 #[try_stream(ok = InternalMessage, error = StreamExecutorError)]
 async fn align_input(left: Box<dyn Executor>, right: Box<dyn Executor>) {
     let mut left = Box::pin(left.execute());
     let mut right = Box::pin(right.execute());
     // Keep producing intervals until stream exhaustion or errors.
     loop {
+        let mut right_chunks = vec![];
         // Produce an aligned interval.
         'inner: loop {
             let mut combined = stream::select_with_strategy(
@@ -117,15 +136,10 @@ async fn align_input(left: Box<dyn Executor>, right: Box<dyn Executor>) {
                 right.by_ref().map(Either::Right),
                 |_: &mut ()| PollNext::Left,
             );
-            let mut left_chunks = vec![];
-            let mut right_chunks = vec![];
             match combined.next().await {
-                Some(Either::Left(Ok(Message::Chunk(c)))) => left_chunks.push(c),
+                Some(Either::Left(Ok(Message::Chunk(c)))) => yield InternalMessage::Chunk(c),
                 Some(Either::Right(Ok(Message::Chunk(c)))) => right_chunks.push(c),
                 Some(Either::Left(Ok(Message::Barrier(b)))) => {
-                    for chunk in left_chunks {
-                        yield InternalMessage::Chunk(chunk);
-                    }
                     let mut remain = chunks_until_barrier(right.by_ref(), b.clone())
                         .try_collect()
                         .await?;
@@ -152,17 +166,65 @@ async fn align_input(left: Box<dyn Executor>, right: Box<dyn Executor>) {
     }
 }
 
-impl<S: StateStore> TemporalJoinExecutor<S> {
+impl<S: StateStore, const T: JoinTypePrimitive> TemporalJoinExecutor<S, T> {
+    #[allow(clippy::too_many_arguments)]
+    pub fn new(
+        ctx: ActorContextRef,
+        left: BoxedExecutor,
+        right: BoxedExecutor,
+        table: StorageTable<S>,
+        left_join_keys: Vec<usize>,
+        right_join_keys: Vec<usize>,
+        null_safe: Vec<bool>,
+        pk_indices: PkIndices,
+        output_indices: Vec<usize>,
+        executor_id: u64,
+        watermark_epoch: AtomicU64Ref,
+        metrics: Arc<StreamingMetrics>,
+        chunk_size: usize,
+    ) -> Self {
+        let schema_fields = [left.schema().fields.clone(), right.schema().fields.clone()].concat();
+
+        let schema: Schema = output_indices
+            .iter()
+            .map(|&idx| schema_fields[idx].clone())
+            .collect();
+
+        let alloc = StatsAlloc::new(Global).shared();
+
+        let cache = new_with_hasher_in(watermark_epoch, DefaultHasher::default(), alloc);
+
+        Self {
+            ctx,
+            left,
+            right,
+            right_table: TemporalSide {
+                source: table,
+                cache,
+            },
+            left_join_keys,
+            right_join_keys,
+            null_safe,
+            output_indices,
+            schema,
+            chunk_size,
+            pk_indices,
+            identity: format!("TemporalJoinExecutor {:X}", executor_id),
+            metrics,
+        }
+    }
+
     #[try_stream(ok = Message, error = StreamExecutorError)]
     async fn into_stream(mut self) {
         let (left_map, right_map) = StreamChunkBuilder::get_i2o_mapping(
             self.output_indices.iter().cloned(),
             self.left.schema().len(),
-            self.right_stream.schema().len(),
+            self.right.schema().len(),
         );
+
         let mut prev_epoch = None;
         #[for_await]
-        for msg in align_input(self.left, self.right_stream) {
+        for msg in align_input(self.left, self.right) {
             match msg? {
                 InternalMessage::Chunk(chunk) => {
                     let mut builder = StreamChunkBuilder::new(
@@ -174,8 +236,19 @@ impl<S: StateStore> TemporalJoinExecutor<S> {
                     let epoch = prev_epoch.expect("Chunk data should come after some barrier.");
                     for (op, row) in chunk.rows() {
                         let key = row.project(&self.left_join_keys);
-                        if let Some(right_row) = self.right.lookup(key, epoch).await? {
+                        if key
+                            .iter()
+                            .zip_eq_fast(self.null_safe.iter())
+                            .any(|(datum, can_null)| datum.is_none() && !*can_null)
+                        {
+                            continue;
+                        }
+                        if let Some(right_row) = self.right_table.lookup(key, epoch).await? {
                             if let Some(chunk) = builder.append_row(op, row, &right_row) {
+                                yield Message::Chunk(chunk);
+                            }
+                        } else if T == JoinType::LeftOuter {
+                            if let Some(chunk) = builder.append_row_update(op, row) {
                                 yield Message::Chunk(chunk);
                             }
                         }
@@ -186,7 +259,8 @@ impl<S: StateStore> TemporalJoinExecutor<S> {
                 }
                 InternalMessage::Barrier(updates, barrier) => {
                     prev_epoch = Some(barrier.epoch.curr);
-                    self.right.update(updates, &self.right_join_keys);
+                    self.right_table
+                        .update(updates, &self.right_join_keys, barrier.epoch.curr);
                     yield Message::Barrier(barrier)
                 }
             }
@@ -194,7 +268,7 @@ impl<S: StateStore> TemporalJoinExecutor<S> {
     }
 }
 
-impl<S: StateStore> Executor for TemporalJoinExecutor<S> {
+impl<S: StateStore, const T: JoinTypePrimitive> Executor for TemporalJoinExecutor<S, T> {
     fn execute(self: Box<Self>) -> super::BoxedMessageStream {
         self.into_stream().boxed()
     }
@@ -204,10 +278,10 @@ impl<S: StateStore> Executor for TemporalJoinExecutor<S> {
     }
 
     fn pk_indices(&self) -> super::PkIndicesRef<'_> {
-        self.left.pk_indices()
+        &self.pk_indices
     }
 
     fn identity(&self) -> &str {
-        "TemporalJoinExecutor"
+        self.identity.as_str()
     }
 }
