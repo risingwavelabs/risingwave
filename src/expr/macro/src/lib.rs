ad12df1cd2ff76179bf10d2c5c02add9ed36794b
Runji Wang <wangrunji0408@163.com>
2023-03-12 14:52:22 +0800

    parse writer and batch
    Signed-off-by: Runji Wang <wangrunji0408@163.com>

--- src/expr/macro/src/lib.rs
+++ src/expr/macro/src/lib.rs
@@ -1,6 +1,6 @@
 use itertools::Itertools;
 use proc_macro::TokenStream;
-use proc_macro2::{Ident, Span, TokenStream as TokenStream2};
+use proc_macro2::{Span, TokenStream as TokenStream2};
 use quote::{format_ident, quote, ToTokens};
 use syn::{parse_macro_input, Error, Result};
 
@@ -18,8 +18,7 @@ pub fn function(attr: TokenStream, item: TokenStream) -> TokenStream {
 }
 
 fn parse_function(attr: syn::AttributeArgs, item: syn::ItemFn) -> Result<TokenStream2> {
-    let fn_attr = FunctionAttr::parse(&attr)?;
-    let user_fn_attr = UserFunctionAttr::parse(&item)?;
+    let fn_attr = FunctionAttr::parse(&attr, &item)?;
 
     let mut tokens = item.into_token_stream();
     tokens.extend(fn_attr.generate_descriptors()?);
@@ -32,11 +31,12 @@ struct FunctionAttr {
     args: Vec<String>,
     ret: String,
     batch: Option<String>,
+    user_fn: UserFunctionAttr,
 }
 
 impl FunctionAttr {
     /// Parse the attribute of the function macro.
-    fn parse(attr: &syn::AttributeArgs) -> Result<Self> {
+    fn parse(attr: &syn::AttributeArgs, item: &syn::ItemFn) -> Result<Self> {
         let sig = attr.get(0).ok_or_else(|| {
             Error::new(
                 Span::call_site(),
@@ -56,6 +56,17 @@ impl FunctionAttr {
             .split_once("(")
             .ok_or_else(|| Error::new_spanned(sig, "expected '('"))?;
 
+        let batch = attr.iter().find_map(|n| {
+            let syn::NestedMeta::Meta(syn::Meta::NameValue(nv)) = n else { return None };
+            if !nv.path.is_ident("batch") {
+                return None;
+            };
+            let syn::Lit::Str(ref lit_str) = nv.lit else { return None };
+            Some(lit_str.value())
+        });
+
+        let user_fn = UserFunctionAttr::parse(item)?;
+
         Ok(FunctionAttr {
             name: name.trim().to_string(),
             args: args
@@ -64,7 +75,8 @@ impl FunctionAttr {
                 .map(|s| s.trim().to_string())
                 .collect(),
             ret: ret.trim().to_string(),
-            batch: None,
+            batch,
+            user_fn,
         })
     }
 
@@ -85,6 +97,7 @@ impl FunctionAttr {
                 args: args.iter().map(|s| s.to_string()).collect(),
                 ret: ret.to_string(),
                 batch: self.batch.clone(),
+                user_fn: self.user_fn.clone(),
             };
             tokens.extend(attr.generate_descriptor_one()?);
         }
@@ -140,41 +153,52 @@ impl FunctionAttr {
     }
 }
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 struct UserFunctionAttr {
+    /// The last argument type is `&mut dyn Writer`.
+    writer: bool,
     /// The argument type are `Option`s.
     nullable: bool,
-    /// The return type is a `Result`.
+    /// The return type is `Result`.
     fallible: bool,
 }
 
 impl UserFunctionAttr {
     fn parse(item: &syn::ItemFn) -> Result<Self> {
         Ok(UserFunctionAttr {
+            writer: last_arg_is_writer(item),
             nullable: args_are_all_option(item),
             fallible: return_value_is_result(item),
         })
     }
 }
 
-fn args_are_all_option(item: &syn::ItemFn) -> bool {
-    item.sig
-        .inputs
-        .iter()
-        .map(|arg| match arg {
-            syn::FnArg::Typed(arg) => &arg.ty,
-            _ => panic!("All arguments must be typed"),
-        })
-        .all(|ty| {
-            matches!(ty.as_ref(), syn::Type::Path(path) if path.path.segments.last().map_or(false, |s| s.ident == "Option"))
-        })
+/// Check if the last argument is `&mut dyn Writer`.
+fn last_arg_is_writer(item: &syn::ItemFn) -> bool {
+    let Some(syn::FnArg::Typed(arg)) = item.sig.inputs.last() else { return false };
+    let syn::Type::Reference(syn::TypeReference { elem, .. }) = arg.ty.as_ref() else { return false };
+    let syn::Type::TraitObject(syn::TypeTraitObject { bounds, .. }) = elem.as_ref() else { return false };
+    let Some(syn::TypeParamBound::Trait(syn::TraitBound { path, .. })) = bounds.first() else { return false };
+    path.segments.last().map_or(false, |s| s.ident == "Writer")
 }
 
-fn return_value_is_result(item: &syn::ItemFn) -> bool {
-    match &item.sig.output {
-        syn::ReturnType::Default => false,
-        syn::ReturnType::Type(_, ty) => {
-            matches!(ty.as_ref(), syn::Type::Path(path) if path.path.segments.last().map_or(false, |s| s.ident == "Result"))
+/// Check if all arguments are `Option`s.
+fn args_are_all_option(item: &syn::ItemFn) -> bool {
+    for arg in &item.sig.inputs {
+        let syn::FnArg::Typed(arg) = arg else { return false };
+        let syn::Type::Path(path) = arg.ty.as_ref() else { return false };
+        let Some(seg) = path.path.segments.last() else { return false };
+        if seg.ident != "Option" {
+            return false;
         }
     }
+    true
+}
+
+/// Check if the return value is `Result`.
+fn return_value_is_result(item: &syn::ItemFn) -> bool {
+    let syn::ReturnType::Type(_, ty) = &item.sig.output else { return false };
+    let syn::Type::Path(path) = ty.as_ref() else { return false };
+    let Some(seg) = path.path.segments.last() else { return false };
+    seg.ident == "Result"
 }
