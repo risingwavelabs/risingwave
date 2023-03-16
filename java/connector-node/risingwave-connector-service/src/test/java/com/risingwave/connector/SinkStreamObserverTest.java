2cd21632e84cc863e584798a199a7a0bde57c469
xx01cyx <caoyuanxin0531@outlook.com>
2023-03-16 10:00:06 +0000

    fmt

--- java/connector-node/risingwave-connector-service/src/test/java/com/risingwave/connector/SinkStreamObserverTest.java
+++ java/connector-node/risingwave-connector-service/src/test/java/com/risingwave/connector/SinkStreamObserverTest.java
@@ -15,8 +15,8 @@
 package com.risingwave.connector;
 
 import com.risingwave.connector.api.TableSchema;
-import com.risingwave.proto.ConnectorServiceProto;
 import com.risingwave.proto.Catalog.SinkType;
+import com.risingwave.proto.ConnectorServiceProto;
 import com.risingwave.proto.ConnectorServiceProto.SinkConfig;
 import com.risingwave.proto.Data.Op;
 import io.grpc.stub.StreamObserver;
