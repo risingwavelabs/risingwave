11cde4a448081a4b07c028786f868847e1bf1b8a
Runji Wang <wangrunji0408@163.com>
2023-03-16 17:32:58 +0800

    add requirements and long description
    Signed-off-by: Runji Wang <wangrunji0408@163.com>

--- src/udf/python/setup.py
+++ src/udf/python/setup.py
@@ -1,10 +1,15 @@
 from setuptools import find_packages, setup
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setup(
     name="risingwave",
-    version="0.0.1",
+    version="0.0.2",
     author="RisingWave Labs",
     description="RisingWave Python API",
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     url="https://github.com/risingwavelabs/risingwave",
     packages=find_packages(),
     classifiers=[
@@ -12,4 +17,5 @@ setup(
         "License :: OSI Approved :: Apache Software License"
     ],
     python_requires=">=3.10",
+    install_requires=['pyarrow'],
 )
