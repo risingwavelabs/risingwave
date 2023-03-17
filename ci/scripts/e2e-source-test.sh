afdb30a6b6e2a3824b574eb4198b29dbdbb35320
StrikeW <wangsiyuanse@gmail.com>
2023-03-17 15:59:28 +0800

    minor

--- ci/scripts/e2e-source-test.sh
+++ ci/scripts/e2e-source-test.sh
@@ -6,7 +6,7 @@ set -euo pipefail
 source ci/scripts/common.env.sh
 
 # prepare environment
-export CONNECTOR_RPC_ENDPOINT="localhost:60061"
+export CONNECTOR_RPC_ENDPOINT="localhost:50051"
 
 while getopts 'p:' opt; do
     case ${opt} in
@@ -66,7 +66,8 @@ psql -h db -U postgres -d cdc_test < ./e2e_test/source/cdc/postgres_cdc.sql
 node_port=50051
 node_timeout=10
 
-mkdir -p .risingwave/log
+# start risingwave cluster
+cargo make ci-start ci-1cn-1fe-with-recovery
 ./connector-node/start-service.sh -p $node_port > .risingwave/log/connector-node.log 2>&1 &
 
 echo "waiting for connector node to start"
@@ -86,9 +87,6 @@ do
     fi
     sleep 0.1
 done
-
-# start risingwave cluster
-cargo make ci-start ci-1cn-1fe-with-recovery
 sleep 2
 
 echo "---- mysql & postgres cdc validate test"
