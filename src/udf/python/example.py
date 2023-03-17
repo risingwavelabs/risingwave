658669d6a8341214f439abee64e5bdfff0240e68
Runji Wang <wangrunji0408@163.com>
2023-03-17 11:56:40 +0800

    add tcp function
    Signed-off-by: Runji Wang <wangrunji0408@163.com>

--- src/udf/python/example.py
+++ src/udf/python/example.py
@@ -1,6 +1,8 @@
+import socket
 from typing import Iterator
 from risingwave.udf import udf, udtf, UdfServer
 import random
+import struct
 
 
 @udf(input_types=[], result_type='INT')
@@ -32,6 +34,15 @@ def series2(n: int) -> Iterator[tuple[int, str]]:
         yield i, str(i)
 
 
+@udtf(input_types=['BINARY'], result_types=['VARCHAR', 'VARCHAR', 'INT', 'INT'])
+def extract_tcp_info(tcp_packet: bytes):
+    src_addr, dst_addr = struct.unpack('!4s4s', tcp_packet[12:20])
+    src_port, dst_port = struct.unpack('!HH', tcp_packet[20:24])
+    src_addr = socket.inet_ntoa(src_addr)
+    dst_addr = socket.inet_ntoa(dst_addr)
+    return src_addr, dst_addr, src_port, dst_port
+
+
 if __name__ == '__main__':
     server = UdfServer(location="0.0.0.0:8815")
     server.add_function(random_int)
@@ -39,4 +50,5 @@ if __name__ == '__main__':
     server.add_function(gcd3)
     server.add_function(series)
     server.add_function(series2)
+    server.add_function(extract_tcp_info)
     server.serve()
