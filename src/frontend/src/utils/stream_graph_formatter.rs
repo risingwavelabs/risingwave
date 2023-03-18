97f72caa96ba0bcf79b55cccf28f7f701d0b3fdb
ice1000 <ice1000kotlin@foxmail.com>
2023-03-18 00:52:59 +0000

    Reupdate test data

--- src/frontend/src/utils/stream_graph_formatter.rs
+++ src/frontend/src/utils/stream_graph_formatter.rs
@@ -35,7 +35,7 @@ pub fn explain_stream_graph(graph: &StreamFragmentGraph, is_verbose: bool) -> St
         ..Default::default()
     };
     for p in pretty {
-        config.unicode(&mut output, &p);
+        config.width = config.unicode(&mut output, &p);
         output.push('\n');
     }
     output
