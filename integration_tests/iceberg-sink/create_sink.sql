f1021dfa0c40391ba2f75d7bfa0d2c77c1c4ac0a
Eric Fu <eric@singularity-data.com>
2023-03-22 08:17:09 +0000

    fix integration test

--- integration_tests/iceberg-sink/create_sink.sql
+++ integration_tests/iceberg-sink/create_sink.sql
@@ -3,6 +3,7 @@ FROM
     bhv_mv WITH (
     connector = 'iceberg',
     type = 'upsert',
+    primary_key = 'user_id, target_id, event_timestamp',
     warehouse.path = 's3://hummock001/iceberg-data',
     s3.endpoint = 'http://minio-0:9301',
     s3.access.key = 'hummockadmin',
