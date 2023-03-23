4fde8c46c474c743c61be94085906f8c5fd89409
Renjie Liu <liurenjie2008@gmail.com>
2023-03-23 11:22:32 +0800

    Add timeout

--- src/tests/simulation/src/utils/assert_result.rs
+++ src/tests/simulation/src/utils/assert_result.rs
@@ -12,6 +12,13 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+use std::future::Future;
+use std::pin::{pin, Pin};
+use std::task::{Context, Poll};
+use std::time::{Duration, Instant};
+
+use pin_project::pin_project;
+
 pub trait AssertResult: AsRef<str> + Sized {
     /// Asserts that the result is equal to the expected result after trimming whitespace.
     #[track_caller]
@@ -29,3 +36,58 @@ pub trait AssertResult: AsRef<str> + Sized {
 }
 
 impl<S: AsRef<str>> AssertResult for S {}
+
+/// A wrapper around a Future which adds timing data.
+#[pin_project]
+pub struct Timed<Fut, F>
+where
+    Fut: Future,
+    F: Fn(&Fut::Output, Duration),
+{
+    #[pin]
+    inner: Fut,
+    f: F,
+    start: Option<Instant>,
+}
+
+impl<Fut, F> Future for Timed<Fut, F>
+where
+    Fut: Future,
+    F: Fn(&Fut::Output, Duration),
+{
+    type Output = Fut::Output;
+
+    fn poll(self: Pin<&mut Self>, cx: &mut Context) -> Poll<Self::Output> {
+        let this = self.project();
+        let start = this.start.get_or_insert_with(Instant::now);
+
+        match this.inner.poll(cx) {
+            // If the inner future is still pending, this wrapper is still pending.
+            Poll::Pending => Poll::Pending,
+
+            // If the inner future is done, measure the elapsed time and finish this wrapper future.
+            Poll::Ready(v) => {
+                let elapsed = start.elapsed();
+                (this.f)(&v, elapsed);
+
+                Poll::Ready(v)
+            }
+        }
+    }
+}
+
+pub trait TimedExt: Sized + Future {
+    fn timed<F>(self, f: F) -> Timed<Self, F>
+    where
+        F: Fn(&Self::Output, Duration),
+    {
+        Timed {
+            inner: self,
+            f,
+            start: None,
+        }
+    }
+}
+
+// All futures can use the `.timed` method defined above
+impl<F: Future> TimedExt for F {}
