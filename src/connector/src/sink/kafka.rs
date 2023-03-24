248768104c7397a0f652d697a6b88b68fc21255d
xx01cyx <caoyuanxin0531@outlook.com>
2023-03-24 12:10:08 +0000

    fix test

--- src/connector/src/sink/kafka.rs
+++ src/connector/src/sink/kafka.rs
@@ -532,14 +532,15 @@ mod test {
     #[test]
     fn parse_kafka_config() {
         let properties: HashMap<String, String> = hashmap! {
+            "connector".to_string() => "kafka".to_string(),
             "properties.bootstrap.server".to_string() => "localhost:9092".to_string(),
             "topic".to_string() => "test".to_string(),
             "type".to_string() => "append-only".to_string(),
             "use_transaction".to_string() => "False".to_string(),
-            "security_protocol".to_string() => "SASL".to_string(),
-            "sasl_mechanism".to_string() => "SASL".to_string(),
-            "sasl_username".to_string() => "test".to_string(),
-            "sasl_password".to_string() => "test".to_string(),
+            "properties.security.protocol".to_string() => "SASL".to_string(),
+            "properties.sasl.mechanism".to_string() => "SASL".to_string(),
+            "properties.sasl.username".to_string() => "test".to_string(),
+            "properties.sasl.password".to_string() => "test".to_string(),
             "identifier".to_string() => "test_sink_1".to_string(),
             "properties.timeout".to_string() => "5s".to_string(),
         };
@@ -552,10 +553,10 @@ mod test {
     #[tokio::test]
     async fn test_kafka_producer() -> Result<()> {
         let properties = hashmap! {
-            "kafka.brokers".to_string() => "localhost:29092".to_string(),
+            "properties.bootstrap.server".to_string() => "localhost:29092".to_string(),
             "identifier".to_string() => "test_sink_1".to_string(),
             "type".to_string() => "append-only".to_string(),
-            "kafka.topic".to_string() => "test_topic".to_string(),
+            "topic".to_string() => "test_topic".to_string(),
         };
         let schema = Schema::new(vec![
             Field {
