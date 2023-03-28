e9c19e1df8c6eab91c80e8d5406a1659c2b553e0
Kexiang Wang <kx.wang@hotmail.com>
2023-03-27 21:10:47 -0400

    small memory in cluster.rs

--- src/tests/simulation/src/cluster.rs
+++ src/tests/simulation/src/cluster.rs
@@ -254,7 +254,7 @@ impl Cluster {
                 "--advertise-addr",
                 &format!("192.168.3.{i}:5688"),
                 "--total-memory-bytes",
-                "7516192768",
+                "6979321856",
                 "--parallelism",
                 &conf.compute_node_cores.to_string(),
             ]);
