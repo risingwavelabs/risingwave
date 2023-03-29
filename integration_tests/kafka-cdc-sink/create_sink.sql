6ddd935ade1b53180a8b096ee255a1622d4b320e
Renjie Liu <liurenjie2008@gmail.com>
2023-03-29 19:20:26 +0800

    Add consume

--- integration_tests/kafka-cdc-sink/create_sink.sql
+++ integration_tests/kafka-cdc-sink/create_sink.sql
@@ -2,7 +2,7 @@ CREATE SINK IF NOT EXISTS counts_sink
 FROM counts
 WITH (
 connector = 'kafka',
-properties.bootstrap.server='message_queue:9092',
+properties.bootstrap.server='message_queue:29092',
 topic = 'counts',
 type = 'debezium',
 use_transaction = 'false',
