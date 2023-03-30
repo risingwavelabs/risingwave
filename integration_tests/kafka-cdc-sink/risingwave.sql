597e9a10e7fc987179c32d9adecdbbf1864c2fe0
Renjie Liu <liurenjie2008@gmail.com>
2023-03-30 16:48:26 +0800

    Finish pipeline

--- integration_tests/kafka-cdc-sink/risingwave.sql
+++ integration_tests/kafka-cdc-sink/risingwave.sql
@@ -13,3 +13,15 @@ CREATE TABLE metrics (
 ) ROW FORMAT JSON;
 
 CREATE MATERIALIZED VIEW counts as select id, sum(num) from metrics group by id;
+
+
+CREATE SINK IF NOT EXISTS counts_sink
+FROM counts
+WITH (
+connector = 'kafka',
+properties.bootstrap.server='message_queue:29092',
+topic = 'counts',
+type = 'debezium',
+use_transaction = 'false',
+primary_key = 'id'
+);
