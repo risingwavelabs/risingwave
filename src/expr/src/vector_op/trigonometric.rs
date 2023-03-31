b0c3f8aad545ede2770e2a35b7607b9db8522721
CAJan93 <jan.mensch@gmx.net>
2023-03-31 09:57:22 +0200

    implement unit-tests

--- src/expr/src/vector_op/trigonometric.rs
+++ src/expr/src/vector_op/trigonometric.rs
@@ -71,11 +71,13 @@ pub fn tanh_f64(input: F64) -> F64 {
     f64::tanh(input.0).into()
 }
 
-// TODO: correct definition?
 #[function("coth(float64) -> float64")]
 pub fn coth_f64(input: F64) -> F64 {
-    let res = 1.0 / f64::tanh(input.0);
-    res.into()
+    if input.0 == 0.0 {
+        return f64::NAN.into();
+    }
+    // https://en.wikipedia.org/wiki/Hyperbolic_functions#Exponential_definitions
+    (f64::cosh(input.0) / f64::sinh(input.0)).into()
 }
 
 #[function("asinh(float64) -> float64")]
@@ -93,19 +95,18 @@ pub fn atanh_f64(input: F64) -> F64 {
     f64::atanh(input.0).into()
 }
 
-
-
 #[cfg(test)]
 mod tests {
 
+    use num_traits::ToPrimitive;
     use risingwave_common::types::F64;
 
     use crate::vector_op::trigonometric::*;
 
     /// numbers are equal within a rounding error
     fn assert_similar(lhs: F64, rhs: F64) {
-        let x = F64::from(lhs.abs() - rhs.abs()).abs() <= 0.000000000000001;
-        assert!(x);
+        let x = F64::from(lhs.abs() - rhs.abs()).abs();
+        assert!(x <= 0.000000000000001);
     }
 
     #[test]
@@ -142,6 +143,40 @@ mod tests {
             atan2_f64(y, x),
             two * atan_f64(y / (F64::from(F64::from(x.powi(2) + y.powi(2)).sqrt()) + x)),
         )
+    }
+
+    #[test]
+    fn test_hyperbolic_trigonometric_funcs() {
+        let two = F64::from(2);
+        let one = F64::from(1);
+        let x = F64::from(5);
+        let y = F64::from(3);
+        // https://en.wikipedia.org/wiki/Hyperbolic_functions#Sums_of_arguments
+        assert_similar(
+            sinh_f64(x + y),
+            sinh_f64(x) * cosh_f64(y) + cosh_f64(x) * sinh_f64(y),
+        );
+        assert_similar(
+            cosh_f64(x + y),
+            cosh_f64(x) * cosh_f64(y) + sinh_f64(x) * sinh_f64(y),
+        );
+        assert_similar(
+            tanh_f64(x + y),
+            (tanh_f64(x) + tanh_f64(y)) / (one + tanh_f64(x) * tanh_f64(y)),
+        );
+        // https://en.wikipedia.org/wiki/Hyperbolic_functions#Useful_relations
+        assert_similar(coth_f64(-x), -coth_f64(x));
+        assert_similar(tanh_f64(-x), -tanh_f64(x));
+        // https://en.wikipedia.org/wiki/Inverse_hyperbolic_functions#Other_identities
+        assert_similar(two * acosh_f64(x), acosh_f64(two * x.powi(2) - one)); // for x >= 1
+        assert_similar(two * asinh_f64(x), acosh_f64(two * x.powi(2) + one)); // for x >= 0
+        assert_similar(
+            asinh_f64(F64::from(x.powi(2).to_f64().unwrap() - 1.to_f64().unwrap()) / (two * x)),
+            atanh_f64(
+                F64::from(x.powi(2).to_f64().unwrap() - 1.to_f64().unwrap())
+                    / F64::from(x.powi(2).to_f64().unwrap() + 1.to_f64().unwrap()),
+            ),
+        );
     }
 
     // TODO: add tests here
