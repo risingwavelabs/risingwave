fbda2b6c473883a953d61b76014d6865741c0c12
Runji Wang <wangrunji0408@163.com>
2023-04-03 17:51:37 +0800

    update UDF server for test
    Signed-off-by: Runji Wang <wangrunji0408@163.com>

--- e2e_test/udf/test.py
+++ e2e_test/udf/test.py
@@ -1,7 +1,8 @@
 import socket
 import struct
 import sys
-from typing import Iterator, Tuple
+from typing import Iterator, Optional, Tuple
+from decimal import Decimal
 sys.path.append('src/udf/python')  # noqa
 
 from risingwave.udf import udf, udtf, UdfServer
@@ -24,7 +25,7 @@ def gcd3(x: int, y: int, z: int) -> int:
     return gcd(gcd(x, y), z)
 
 
-@udf(input_types=['BYTEA'], result_type='STRUCT<src_ip VARCHAR, dst_ip VARCHAR, src_port SMALLINT, dst_port SMALLINT>')
+@udf(input_types=['BYTEA'], result_type='STRUCT<VARCHAR, VARCHAR, SMALLINT, SMALLINT>')
 def extract_tcp_info(tcp_packet: bytes):
     src_addr, dst_addr = struct.unpack('!4s4s', tcp_packet[12:20])
     src_port, dst_port = struct.unpack('!HH', tcp_packet[20:24])
@@ -45,6 +46,29 @@ def series2(n: int) -> Iterator[Tuple[int, str]]:
         yield i, f'#{i}'
 
 
+@udf(input_types='VARCHAR', result_type='DECIMAL')
+def hex_to_dec(hex: Optional[str]) -> Optional[Decimal]:
+    if not hex:
+        return None
+
+    hex = hex.strip()
+    dec = Decimal(0)
+
+    while hex:
+        chunk = hex[:16]
+        chunk_value = int(hex[:16], 16)
+        dec = dec * (1 << (4 * len(chunk))) + chunk_value
+        hex = hex[16:]
+    return dec
+
+
+@udf(input_types=["VARCHAR[]", "INT"], result_type="VARCHAR")
+def array_access(list: list[str], idx: int) -> Optional[str]:
+    if idx == 0 or idx > len(list):
+        return None
+    return list[idx - 1]
+
+
 if __name__ == '__main__':
     server = UdfServer()
     server.add_function(int_42)
@@ -53,4 +77,6 @@ if __name__ == '__main__':
     server.add_function(series)
     server.add_function(series2)
     server.add_function(extract_tcp_info)
+    server.add_function(hex_to_dec)
+    server.add_function(array_access)
     server.serve()
