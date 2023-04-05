89d578c55f17efb2e9897d939d9808e7711c6042
tabVersion <tabvision@bupt.icu>
2023-04-05 17:08:10 +0800

    run slt
    Signed-off-by: tabVersion <tabvision@bupt.icu>

--- ci/scripts/e2e-sink-test.sh
+++ ci/scripts/e2e-sink-test.sh
@@ -99,6 +99,7 @@ echo "--- testing sinks"
 sqllogictest -p 4566 -d dev './e2e_test/sink/append_only_sink.slt'
 sqllogictest -p 4566 -d dev './e2e_test/sink/create_sink_as.slt'
 sqllogictest -p 4566 -d dev './e2e_test/sink/blackhole_sink.slt'
+sqllogictest -p 4566 -d dev './e2e_test/sink/remote/types.slt'
 sleep 1
 
 # check sink destination postgres
