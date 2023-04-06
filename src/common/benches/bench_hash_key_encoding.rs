f80ef5d17af1824ea7a048aea3719a880cdf30e5
Noel Kwan <47273164+kwannoel@users.noreply.github.com>
2023-04-06 12:26:26 +0800

    bench(common): add more benchmark cases for unaligned number of keys (#9018)

--- src/common/benches/bench_hash_key_encoding.rs
+++ src/common/benches/bench_hash_key_encoding.rs
@@ -232,6 +232,22 @@ fn case_builders() -> Vec<HashKeyBenchCaseBuilder> {
             },
             describe: "large mixed".to_string(),
         },
+        // These benchmark cases will test unaligned key sizes.
+        // For instance five keys of Int64 cannot fit within Key256 (5 * 64 = 320 > 256),
+        // so it has to go to next largest keysize, Key512.
+        // This means 24 bytes of wasted memory.
+        HashKeyBenchCaseBuilder {
+            data_types: vec![DataType::Int64; 5],
+            describe: "unaligned small fixed".to_string(),
+        },
+        HashKeyBenchCaseBuilder {
+            data_types: vec![DataType::Int64; 9],
+            describe: "unaligned medium fixed".to_string(),
+        },
+        HashKeyBenchCaseBuilder {
+            data_types: vec![DataType::Int64; 17],
+            describe: "unaligned large fixed".to_string(),
+        },
     ]
 }
 
