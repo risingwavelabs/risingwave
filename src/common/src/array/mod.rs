dc89f19bd976778e63bce53207f9bbf984d53334
Shanicky Chen <peng@risingwave-labs.com>
2023-04-06 17:43:25 +0800

    Remove unused ArrayType import in risingwave_pb.

--- src/common/src/array/mod.rs
+++ src/common/src/array/mod.rs
@@ -59,7 +59,7 @@ pub use jsonb_array::{JsonbArray, JsonbArrayBuilder, JsonbRef, JsonbVal};
 pub use list_array::{ListArray, ListArrayBuilder, ListRef, ListValue};
 use paste::paste;
 pub use primitive_array::{PrimitiveArray, PrimitiveArrayBuilder, PrimitiveArrayItemType};
-use risingwave_pb::data::{ArrayType, PbArray, PbArrayType};
+use risingwave_pb::data::{PbArray, PbArrayType};
 pub use serial_array::{Serial, SerialArray, SerialArrayBuilder};
 pub use stream_chunk::{Op, StreamChunk, StreamChunkTestExt};
 pub use struct_array::{StructArray, StructArrayBuilder, StructRef, StructValue};
