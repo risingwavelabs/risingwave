c20d21c2b7f38e46629f69601487094d3fa2a00c
Erich Ess <erich.g.ess@gmail.com>
2023-04-07 15:21:31 -0400

    Do not bind the table when looking up its information for resolving pg_indexes_size

--- src/frontend/src/binder/relation/table_or_source.rs
+++ src/frontend/src/binder/relation/table_or_source.rs
@@ -315,7 +315,7 @@ impl Binder {
         })
     }
 
-    pub(crate) fn bind_table_by_id(&mut self, table_id: &TableId) -> Result<BoundBaseTable> {
+    pub(crate) fn get_table_by_id(&mut self, table_id: &TableId) -> Result<BoundBaseTable> {
         let db = &self.db_name;
         // Find the schema catalog that contains this TableId
         let schema = self.catalog.get_schema_by_table_id(db, table_id)?;
@@ -324,15 +324,15 @@ impl Binder {
         // Get the table catalog for this table
         let table_catalog = self.catalog.get_table_by_id(table_id)?;
         // Get the columns for this table
-        let columns = table_catalog.columns.clone();
+        // let columns = table_catalog.columns.clone();
 
-        self.bind_table_to_context(
-            columns
-                .iter()
-                .map(|c| (c.is_hidden, (&c.column_desc).into())),
-            table_catalog.name().to_string(),
-            None,
-        )?;
+        // self.bind_table_to_context(
+        // columns
+        // .iter()
+        // .map(|c| (c.is_hidden, (&c.column_desc).into())),
+        // table_catalog.name().to_string(),
+        // None,
+        // )?;
 
         // Create a BoundBaseTable
         Ok(BoundBaseTable {
--- src/frontend/src/binder/select.rs
+++ src/frontend/src/binder/select.rs
@@ -618,7 +618,7 @@ impl Binder {
             .as_ref()
             .ok_or_else(|| ErrorCode::BindError("No Value".to_string()))?
         {
-            ScalarImpl::Int32(id) => self.bind_table_by_id(&TableId::new(*id as u32))?,
+            ScalarImpl::Int32(id) => self.get_table_by_id(&TableId::new(*id as u32))?,
             ScalarImpl::Utf8(name) => self.get_object_by_name(name)?,
             _ => {
                 return Err(ErrorCode::BindError(
