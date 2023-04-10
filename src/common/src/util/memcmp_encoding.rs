2aacf37494ea62b0b2ff3b07b052b505aace8714
Shanicky Chen <peng@risingwave-labs.com>
2023-04-10 17:40:16 +0800

    Improve ScalarImpl and memcmp_encoding.rs with ? operator and import statement update.

--- src/common/src/util/memcmp_encoding.rs
+++ src/common/src/util/memcmp_encoding.rs
@@ -22,7 +22,6 @@ use crate::array::{ArrayImpl, DataChunk};
 use crate::row::Row;
 use crate::types::num256::Int256;
 use crate::types::{DataType, Date, Datum, ScalarImpl, Time, Timestamp, ToDatumRef, F32, F64};
-use crate::types::num256::Int256;
 use crate::util::sort_util::{ColumnOrder, OrderType};
 
 // NULL > any non-NULL value by default
