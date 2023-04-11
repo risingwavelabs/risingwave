07270155bf21abd3d1427b680aea5c19a7d101a6
August <pin@singularity-data.com>
2023-04-11 13:19:36 +0800

    feat: support show create source/sink command (#9083)

--- src/frontend/src/handler/create_table.rs
+++ src/frontend/src/handler/create_table.rs
@@ -532,6 +532,7 @@ fn gen_table_plan_inner(
         info: Some(source_info),
         owner: session.user_id(),
         watermark_descs: watermark_descs.clone(),
+        definition: "".to_string(),
         optional_associated_table_id: Some(OptionalAssociatedTableId::AssociatedTableId(
             TableId::placeholder().table_id,
         )),
