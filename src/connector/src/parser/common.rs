9f7f702b7bb09934bee422f843eebae1ff7f0fb0
Shanicky Chen <peng@risingwave-labs.com>
2023-04-11 18:45:25 +0800

    Remove num256 import, fix hash size calculation, update num_traits

--- src/connector/src/parser/common.rs
+++ src/connector/src/parser/common.rs
@@ -12,9 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-
 use std::borrow::Cow;
-
 use std::str::FromStr;
 
 use anyhow::{anyhow, Result};
