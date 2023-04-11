3dc54c11b6c49f76a6176ac5fc56f3bcbb74952c
lmatz <lmatz823@gmail.com>
2023-04-11 20:25:58 +0800

    update tests

--- src/connector/src/parser/maxwell/simd_json_parser.rs
+++ src/connector/src/parser/maxwell/simd_json_parser.rs
@@ -122,8 +122,8 @@ mod tests {
     #[tokio::test]
     async fn test_json_parser() {
         let descs = vec![
-            SourceColumnDesc::simple("ID", DataType::Int32, 0.into()),
-            SourceColumnDesc::simple("NAME", DataType::Varchar, 1.into()),
+            SourceColumnDesc::simple("id", DataType::Int32, 0.into()),
+            SourceColumnDesc::simple("name", DataType::Varchar, 1.into()),
             SourceColumnDesc::simple("is_adult", DataType::Int16, 2.into()),
             SourceColumnDesc::simple("birthday", DataType::Timestamp, 3.into()),
         ];
