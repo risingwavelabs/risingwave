1c89d47d95ac2654cfb146cd1373b2c42923cb24
Chengxu Bian <cbian7@wisc.edu>
2023-04-11 18:40:13 -0500

    refactor start telemetry reporting

--- src/frontend/src/session.rs
+++ src/frontend/src/session.rs
@@ -280,7 +280,7 @@ impl FrontendEnv {
         // change because if any of configs disable telemetry, we should never start it
         if config.server.telemetry_enabled && telemetry_env_enabled() {
             if telemetry_enabled {
-                telemetry_manager.start_telemetry_reporting();
+                telemetry_manager.start_telemetry_reporting().await;
             }
             let (telemetry_join_handle, telemetry_shutdown_sender) =
                 telemetry_manager.watch_params_change();
