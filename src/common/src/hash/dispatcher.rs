b92a2bc9e1b61ae7749c459c0b260008ca72401f
Shanicky Chen <peng@risingwave-labs.com>
2023-04-12 14:14:21 +0800

    Remove unused import and unsupported return type.

--- src/common/src/hash/dispatcher.rs
+++ src/common/src/hash/dispatcher.rs
@@ -12,8 +12,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-use num256::Int256;
-
 use super::HashKey;
 use crate::array::serial_array::Serial;
 use crate::hash;
