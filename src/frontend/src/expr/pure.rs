05aa06b063c5a8309c1ccbbfe37b4bdf4ebbe6c7
st1page <1245835950@qq.com>
2023-04-13 14:32:56 +0800

    match all

--- src/frontend/src/expr/pure.rs
+++ src/frontend/src/expr/pure.rs
@@ -28,10 +28,129 @@ impl ExprVisitor<bool> for ImpureAnalyzer {
     }
 
     fn visit_function_call(&mut self, func_call: &super::FunctionCall) -> bool {
-        matches!(
-            func_call.get_expr_type(),
-            expr_node::Type::Now | expr_node::Type::Udf
-        )
+        match func_call.get_expr_type() {
+            expr_node::Type::Unspecified
+            | expr_node::Type::InputRef
+            | expr_node::Type::ConstantValue
+            | expr_node::Type::Add
+            | expr_node::Type::Subtract
+            | expr_node::Type::Multiply
+            | expr_node::Type::Divide
+            | expr_node::Type::Modulus
+            | expr_node::Type::Equal
+            | expr_node::Type::NotEqual
+            | expr_node::Type::LessThan
+            | expr_node::Type::LessThanOrEqual
+            | expr_node::Type::GreaterThan
+            | expr_node::Type::GreaterThanOrEqual
+            | expr_node::Type::And
+            | expr_node::Type::Or
+            | expr_node::Type::Not
+            | expr_node::Type::In
+            | expr_node::Type::Some
+            | expr_node::Type::All
+            | expr_node::Type::BitwiseAnd
+            | expr_node::Type::BitwiseOr
+            | expr_node::Type::BitwiseXor
+            | expr_node::Type::BitwiseNot
+            | expr_node::Type::BitwiseShiftLeft
+            | expr_node::Type::BitwiseShiftRight
+            | expr_node::Type::Extract
+            | expr_node::Type::DatePart
+            | expr_node::Type::TumbleStart
+            | expr_node::Type::ToTimestamp
+            | expr_node::Type::AtTimeZone
+            | expr_node::Type::DateTrunc
+            | expr_node::Type::ToTimestamp1
+            | expr_node::Type::CastWithTimeZone
+            | expr_node::Type::Cast
+            | expr_node::Type::Substr
+            | expr_node::Type::Length
+            | expr_node::Type::Like
+            | expr_node::Type::Upper
+            | expr_node::Type::Lower
+            | expr_node::Type::Trim
+            | expr_node::Type::Replace
+            | expr_node::Type::Position
+            | expr_node::Type::Ltrim
+            | expr_node::Type::Rtrim
+            | expr_node::Type::Case
+            | expr_node::Type::RoundDigit
+            | expr_node::Type::Round
+            | expr_node::Type::Ascii
+            | expr_node::Type::Translate
+            | expr_node::Type::Coalesce
+            | expr_node::Type::ConcatWs
+            | expr_node::Type::Abs
+            | expr_node::Type::SplitPart
+            | expr_node::Type::Ceil
+            | expr_node::Type::Floor
+            | expr_node::Type::ToChar
+            | expr_node::Type::Md5
+            | expr_node::Type::CharLength
+            | expr_node::Type::Repeat
+            | expr_node::Type::ConcatOp
+            | expr_node::Type::BoolOut
+            | expr_node::Type::OctetLength
+            | expr_node::Type::BitLength
+            | expr_node::Type::Overlay
+            | expr_node::Type::RegexpMatch
+            | expr_node::Type::Pow
+            | expr_node::Type::Exp
+            | expr_node::Type::Chr
+            | expr_node::Type::StartsWith
+            | expr_node::Type::Initcap
+            | expr_node::Type::Lpad
+            | expr_node::Type::Rpad
+            | expr_node::Type::Reverse
+            | expr_node::Type::Strpos
+            | expr_node::Type::ToAscii
+            | expr_node::Type::ToHex
+            | expr_node::Type::QuoteIdent
+            | expr_node::Type::Sin
+            | expr_node::Type::Cos
+            | expr_node::Type::Tan
+            | expr_node::Type::Cot
+            | expr_node::Type::Asin
+            | expr_node::Type::Acos
+            | expr_node::Type::Atan
+            | expr_node::Type::Atan2
+            | expr_node::Type::Sqrt
+            | expr_node::Type::Degrees
+            | expr_node::Type::Radians
+            | expr_node::Type::IsTrue
+            | expr_node::Type::IsNotTrue
+            | expr_node::Type::IsFalse
+            | expr_node::Type::IsNotFalse
+            | expr_node::Type::IsNull
+            | expr_node::Type::IsNotNull
+            | expr_node::Type::IsDistinctFrom
+            | expr_node::Type::IsNotDistinctFrom
+            | expr_node::Type::Neg
+            | expr_node::Type::Field
+            | expr_node::Type::Array
+            | expr_node::Type::ArrayAccess
+            | expr_node::Type::Row
+            | expr_node::Type::ArrayToString
+            | expr_node::Type::ArrayCat
+            | expr_node::Type::ArrayAppend
+            | expr_node::Type::ArrayPrepend
+            | expr_node::Type::FormatType
+            | expr_node::Type::ArrayDistinct
+            | expr_node::Type::ArrayLength
+            | expr_node::Type::Cardinality
+            | expr_node::Type::ArrayRemove
+            | expr_node::Type::HexToInt256
+            | expr_node::Type::JsonbAccessInner
+            | expr_node::Type::JsonbAccessStr
+            | expr_node::Type::JsonbTypeof
+            | expr_node::Type::JsonbArrayLength
+            | expr_node::Type::Pi => false,
+            expr_node::Type::Vnode
+            | expr_node::Type::Now
+            | expr_node::Type::Proctime
+            | expr_node::Type::Udf => true,
+        }
     }
 }
 
