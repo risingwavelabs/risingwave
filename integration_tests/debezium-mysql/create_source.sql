e07fc6cb71b953cefad2e35892a9b6ea48ecfc46
Tao Wu <wutao@singularity-data.com>
2023-04-16 17:35:54 +0800

    add test for dbz mysql

--- integration_tests/debezium-mysql/create_source.sql
+++ integration_tests/debezium-mysql/create_source.sql
@@ -1,14 +1,6 @@
-create materialized source orders (
-    order_id int,
-    order_date bigint,
-    customer_name varchar,
-    price decimal,
-    product_id int,
-    order_status smallint,
-    PRIMARY KEY (order_id)
-) with (
+CREATE TABLE orders (order_id INT PRIMARY KEY) with (
     connector = 'kafka',
-    kafka.topic = 'orders',
+    kafka.topic = 'mysql.mydb.orders',
     kafka.brokers = 'message_queue:29092',
     kafka.scan.startup.mode = 'earliest'
-);
+) ROW FORMAT DEBEZIUM_AVRO ROW SCHEMA LOCATION CONFLUENT SCHEMA REGISTRY 'http://message_queue:8081';
