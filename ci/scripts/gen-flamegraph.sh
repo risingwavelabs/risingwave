c5ec5a9f1d32c4fd0ffdc2ea6a34802a3d11d84a
Noel Kwan <noelkwan1998@gmail.com>
2023-04-17 13:50:54 +0800

    try run rw

--- ci/scripts/gen-flamegraph.sh
+++ ci/scripts/gen-flamegraph.sh
@@ -1,6 +1,19 @@
 #!/usr/bin/env bash
 
+set -euo pipefail
+
+print_machine_debug_info() {
+  echo "Free space on Machine"
+  df -h
+  echo "Files on Machine"
+  ls
+}
+
+echo "--- Machine Debug Info"
+print_machine_debug_info
 echo "--- Setting up RW"
+cd risingwave
+ENABLE_RW_RELEASE_PROFILE=true ./risedev d bench
 echo "Success!"
 echo "--- Running Benchmarks"
 echo "Success!"
