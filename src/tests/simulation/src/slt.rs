8becf3b27345e63ebca5d1f04f7bfc64b534106d
zwang28 <84491488@qq.com>
2023-04-18 17:02:10 +0800

    tune test

--- src/tests/simulation/src/slt.rs
+++ src/tests/simulation/src/slt.rs
@@ -79,8 +79,7 @@ const KILL_IGNORE_FILES: &[&str] = &[
     // TPCH queries are too slow for recovery.
     "tpch_snapshot.slt",
     "tpch_upstream.slt",
-    // We already have visibility_all cases.
-    "visibility_checkpoint.slt",
+    "visibility_all.slt",
     // This depends on session config.
     "session_timezone.slt",
 ];
