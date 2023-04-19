fef41cab1081de0619fb89d5673b35ede15b9062
Noel Kwan <noelkwan1998@gmail.com>
2023-04-19 14:39:24 +0800

    remove sudo

--- ci/scripts/flamegraph-env-build.sh
+++ ci/scripts/flamegraph-env-build.sh
@@ -7,8 +7,8 @@ source ci/scripts/common.sh
 
 echo "--- Installing go toolchain"
 wget https://go.dev/dl/go1.20.3.linux-amd64.tar.gz
-sudo rm -rf /usr/local/go
-sudo tar -C /usr/local -xzf go1.20.3.linux-amd64.tar.gz
+rm -rf /usr/local/go
+tar -C /usr/local -xzf go1.20.3.linux-amd64.tar.gz
 export PATH=$PATH:/usr/local/go/bin
 
 echo "--- Installing promql cli client"
