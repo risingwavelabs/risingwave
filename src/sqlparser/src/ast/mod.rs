c9a575aa479c74bbdd404c5dc54f103fe2f84326
Richard Chien <stdrc@outlook.com>
2023-04-20 20:04:22 +0800

    remove incorrect `impl Default` in sqlparser
    Signed-off-by: Richard Chien <stdrc@outlook.com>

--- src/sqlparser/src/ast/mod.rs
+++ src/sqlparser/src/ast/mod.rs
@@ -660,19 +660,6 @@ pub struct WindowFrame {
     // TBD: EXCLUDE
 }
 
-impl Default for WindowFrame {
-    /// returns default value for window frame
-    ///
-    /// see <https://www.sqlite.org/windowfunctions.html#frame_specifications>
-    fn default() -> Self {
-        Self {
-            units: WindowFrameUnits::Range,
-            start_bound: WindowFrameBound::Preceding(None),
-            end_bound: None,
-        }
-    }
-}
-
 #[derive(Debug, Clone, PartialEq, Eq, Hash)]
 #[cfg_attr(feature = "serde", derive(Serialize, Deserialize))]
 pub enum WindowFrameUnits {
@@ -2416,11 +2403,11 @@ impl fmt::Display for SetVariableValue {
 mod tests {
     use super::*;
 
-    #[test]
-    fn test_window_frame_default() {
-        let window_frame = WindowFrame::default();
-        assert_eq!(WindowFrameBound::Preceding(None), window_frame.start_bound);
-    }
+    // #[test]
+    // fn test_window_frame_default() {
+    //     let window_frame = WindowFrame::default();
+    //     assert_eq!(WindowFrameBound::Preceding(None), window_frame.start_bound);
+    // }
 
     #[test]
     fn test_grouping_sets_display() {
