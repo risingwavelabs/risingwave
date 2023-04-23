05bae6258bed6b52041245901b32aa07206b8250
Bugen Zhao <i@bugenzhao.com>
2023-04-23 14:26:13 +0800

    refactor(test): split simulated integration tests into scale and recovery
    Signed-off-by: Bugen Zhao <i@bugenzhao.com>

--- ci/scripts/build-simulation.sh
+++ ci/scripts/build-simulation.sh
@@ -11,13 +11,13 @@ cp ci/risedev-components.ci.env risedev-components.user.env
 echo "--- Build deterministic simulation e2e test runner"
 cargo make sslt-build-all --profile ci-sim
 
-echo "--- Build and archive deterministic scaling simulation tests"
-NEXTEST_PROFILE=ci-scaling cargo make sarchive-scale-test --cargo-profile ci-sim
+echo "--- Build and archive deterministic simulation integration tests"
+NEXTEST_PROFILE=ci-sim cargo make sarchive-it-test --cargo-profile ci-sim
 
 echo "--- Upload artifacts"
 mv target/sim/ci-sim/risingwave_simulation ./risingwave_simulation
 
-artifacts=(risingwave_simulation scale-test.tar.zst)
+artifacts=(risingwave_simulation simulation-it-test.tar.zst)
 echo -n "${artifacts[*]}" | parallel -d ' ' "buildkite-agent artifact upload ./{}"
 
 echo "--- Show sccache stats"
