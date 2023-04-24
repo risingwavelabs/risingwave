e4fcb3a5682ccfba936431e91d6346bd5a9096f0
Noel Kwan <noelkwan1998@gmail.com>
2023-04-24 19:15:53 +0800

    just use seccomp

--- ci/scripts/gen-flamegraph.sh
+++ ci/scripts/gen-flamegraph.sh
@@ -130,7 +130,7 @@ configure_all() {
 ############## Start benchmark environment
 
 start_nperf() {
-  echo '1' | tee /proc/sys/kernel/perf_event_paranoid
+  # echo '1' | tee /proc/sys/kernel/perf_event_paranoid
   ./nperf record -p $(pidof compute-node) -o perf.data
 }
 
