c3e92d5828b40fcdb1d44ac58983ae70173c3bea
Ubuntu <honeta@qq.com>
2023-04-26 09:04:28 +0000

    fix

--- src/frontend/src/optimizer/plan_node/logical_over_agg.rs
+++ src/frontend/src/optimizer/plan_node/logical_over_agg.rs
@@ -401,12 +401,10 @@ impl ColPrunable for LogicalOverAgg {
                 .iter()
                 .map(|&idx| mapping.map(idx))
                 .collect_vec();
+            let src_size = new_over_agg.schema().len();
             LogicalProject::with_mapping(
                 new_over_agg.into(),
-                ColIndexMapping::with_remaining_columns(
-                    &output_required_cols,
-                    new_over_agg.schema().len(),
-                ),
+                ColIndexMapping::with_remaining_columns(&output_required_cols, src_size),
             )
             .into()
         }
