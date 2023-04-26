9388eab8e183b3a9c73fd7dab2a3bc55d6b72082
xiangjinwu <17769960+xiangjinwu@users.noreply.github.com>
2023-04-26 18:16:39 +0800

    fix(expr): float should round ties to even (#9476)

--- src/tests/regress/data/sql/int4.sql
+++ src/tests/regress/data/sql/int4.sql
@@ -133,14 +133,14 @@ SELECT (-2147483648)::int4 / (-1)::int2;
 --@ SELECT (-2147483648)::int4 % (-1)::int2;
 
 -- check rounding when casting from float
---@ SELECT x, x::int4 AS int4_value
---@ FROM (VALUES (-2.5::float8),
---@              (-1.5::float8),
---@              (-0.5::float8),
---@              (0.0::float8),
---@              (0.5::float8),
---@              (1.5::float8),
---@              (2.5::float8)) t(x);
+SELECT x, x::int4 AS int4_value
+FROM (VALUES (-2.5::float8),
+             (-1.5::float8),
+             (-0.5::float8),
+             (0.0::float8),
+             (0.5::float8),
+             (1.5::float8),
+             (2.5::float8)) t(x);
 
 -- check rounding when casting from numeric
 SELECT x, x::int4 AS int4_value
