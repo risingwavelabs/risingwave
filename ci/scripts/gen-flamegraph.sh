33f7afa20d54e1686ae72a1e8c96eb10cb14079a
Noel Kwan <noelkwan1998@gmail.com>
2023-04-27 21:31:37 +0800

    revert to use ps instead

--- ci/scripts/gen-flamegraph.sh
+++ ci/scripts/gen-flamegraph.sh
@@ -166,10 +166,7 @@ start_kafka() {
   ./kafka_2.13-3.4.0/bin/kafka-server-start.sh ./kafka_2.13-3.4.0/config/server.properties --override num.partitions=8 > kafka.log 2>&1 &
   sleep 10
   echo "Should have zookeeper and kafka running"
-  echo "zookeeper PID: "
-  pgrep zookeeper
-  echo "kafka PID: "
-  pgrep kafka
+  ps
   # TODO(kwannoel): `trap ERR` and upload these logs.
   # buildkite-agent artifact upload ./zookeeper.log
   # buildkite-agent artifact upload ./kafka.log
