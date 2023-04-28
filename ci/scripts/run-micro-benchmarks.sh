2bf0e8015bbf850d121d2a7ea862a18963031a70
Noel Kwan <noelkwan1998@gmail.com>
2023-04-28 17:01:27 +0800

    finish script for running microbench

--- ci/scripts/run-micro-benchmarks.sh
+++ ci/scripts/run-micro-benchmarks.sh
@@ -1,2 +1,48 @@
 #!/usr/bin/env bash
 
+set -euo pipefail
+
+# NOTE(kwannoel): The benchmarks here will run **per PR**, and daily in main cron.
+# only add top-level microbench here. For example, executor micro-benchmarks.
+
+# Space delimited micro-benchmarks to run.
+# Each micro-benchmark added here should return a singular result indicative of performance.
+BENCHMARKS="stream_hash_agg json_parser"
+
+# cargo criterion --bench stream_hash_agg --message-format=json
+bench() {
+  BENCHMARK_NAME=$1
+  for LINE in $(cargo criterion --bench "$BENCHMARK_NAME" --message-format=json)
+  do
+    REASON="$(jq ".reason" <<< "$LINE")"
+    if [[ $REASON == \"benchmark-complete\" ]]; then
+      ID="$(jq ".id" <<< "$LINE")"
+      MEAN="$(jq ".mean" <<< "$LINE")"
+      EST="$(jq ".estimate" <<< $MEAN)"
+      UNIT="$(jq ".unit" <<< $MEAN)"
+
+      echo "Benchmark ID: $ID"
+      echo "Average Time Taken: $EST"
+
+      JSON="  {\"benchmark_id\": $ID, \"time_taken\": $EST, \"unit\": $UNIT},"
+      echo -n "Json output: "
+      echo "$JSON" | tee -a results.json
+    fi
+  done
+}
+
+main() {
+  echo '[' > results.json
+  for BENCHMARK in $BENCHMARKS
+  do
+    echo "--- Running $BENCHMARK"
+    bench $BENCHMARK
+  done
+  # FIXME: the `-i` (inplace) flag doesn't work with this sed expr for some reason...
+  NO_TRAILING_COMMA=$(sed -E '$ s/(.*),$/\1/' ./results.json)
+  echo "$NO_TRAILING_COMMA" > ./results.json
+  echo ']' >> results.json
+  # buildkite-agent artifact upload ./results.json
+}
+
+main
