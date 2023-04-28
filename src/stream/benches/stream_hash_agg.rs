22b9f32ec8e9fff26631740fbd552eb27eeb6c4d
Noel Kwan <noelkwan1998@gmail.com>
2023-04-28 17:09:19 +0800

    fix conflicts

--- src/stream/benches/stream_hash_agg.rs
+++ src/stream/benches/stream_hash_agg.rs
@@ -180,7 +180,7 @@ fn setup_bench_hash_agg<S: StateStore>(store: S) -> BoxedExecutor {
         num_of_chunks,
         chunk_size,
         &input_data_types,
-        &VarcharProperty::Constant, // Corresponds to a single date value, which q17 should expect.
+        &VarcharProperty::Constant,
     );
 
     // ---- Create MockSourceExecutor ----
