189a280b8c583e98e648995ccd34c2df8d885a54
Noel Kwan <noelkwan1998@gmail.com>
2023-04-30 22:02:13 +0800

    add benchmark

---
+++ src/common/benches/bench_array.rs
@@ -0,0 +1,40 @@
+// Copyright 2023 RisingWave Labs
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+use criterion::{criterion_group, criterion_main, Criterion, BatchSize, black_box};
+use itertools::Itertools;
+use risingwave_common::array::{ArrayBuilderImpl, DataChunk};
+use risingwave_common::hash::{calc_hash_key_kind, HashKey, HashKeyDispatcher};
+use risingwave_common::test_utils::rand_chunk;
+use risingwave_common::types::{DataType, Datum};
+
+pub fn bench_bigint(c: &mut Criterion) {
+    c.bench_function("bench_bigint", |b| {
+        b.iter_batched(
+            || ArrayBuilderImpl::from_type(&black_box(DataType::Int64), 1024),
+            |mut builder| {
+                for _i in 0..black_box(100) {
+                    let datum: i64 = black_box(3);
+                    let datum: Datum = Some(datum.into());
+                    black_box(builder.append_datum_n(10, datum));
+                }
+                // black_box(builder.finish())
+            },
+            BatchSize::SmallInput
+        )
+    });
+}
+
+criterion_group!(benches, bench_bigint);
+criterion_main!(benches);
