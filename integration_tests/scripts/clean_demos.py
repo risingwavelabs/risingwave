31c7b5cabfd86d1a231f169ab23d8598e3474d3f
Jianwei Huang <1223644280@qq.com>
2023-05-12 19:16:36 +0800

    fix integration test pipeline matrix limits

---
+++ integration_tests/scripts/clean_demos.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+
+from os.path import (dirname, abspath)
+import os
+import subprocess
+import argparse
+
+
+def clean_demo(demo: str, format: str):
+    file_dir = dirname(abspath(__file__))
+    project_dir = dirname(file_dir)
+    demo_dir = os.path.join(project_dir, demo)
+    print("Clean demo: {}".format(demo))
+
+    subprocess.run(["docker", "compose", "down"], cwd=demo_dir, check=True)
+
+arg_parser = argparse.ArgumentParser(description='Clean the demo')
+arg_parser.add_argument('--format',
+                        metavar='format',
+                        type=str,
+                        help='the format of output data',
+                        default='json')
+arg_parser.add_argument('--case',
+                        metavar='case',
+                        type=str,
+                        help='the test case')
+args = arg_parser.parse_args()
+
+clean_demo(args.case, args.format)
