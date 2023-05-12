947498e9ab10fb107d16958f1302e3c32de033c2
xxchan <xxchan22f@gmail.com>
2023-05-12 19:55:18 +0200

    fix

--- ci/build-ci-image.sh
+++ ci/build-ci-image.sh
@@ -30,7 +30,7 @@ fi
 set -e
 
 echo "--- Docker login"
-aws ecr get-login-password | docker login --username AWS --password-stdin public.ecr.aws/x5u3w5h6
+aws ecr get-login-password --region us-east-2 | docker login --username AWS --password-stdin 639303875316.dkr.ecr.us-east-2.amazonaws.com
 
 echo "--- Check image existence"
 set +e
