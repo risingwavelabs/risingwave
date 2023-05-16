3ecdc8c6571c62ef67750cd711c50a60e9fddee4
jon-chuang <jon-chuang@users.noreply.github.com>
2023-05-16 21:10:07 +0800

    add CI test

--- ci/scripts/run-unit-test.sh
+++ ci/scripts/run-unit-test.sh
@@ -3,6 +3,8 @@
 # Exits as soon as any line fails.
 set -euo pipefail
 
+REPO_ROOT=${PWD}
+
 echo "+++ Run unit tests with coverage"
 # use tee to disable progress bar
 NEXTEST_PROFILE=ci cargo llvm-cov nextest --lcov --output-path lcov.info --features failpoints,sync_point 2> >(tee);
@@ -10,3 +12,8 @@ NEXTEST_PROFILE=ci cargo llvm-cov nextest --lcov --output-path lcov.info --featu
 echo "--- Codecov upload coverage reports"
 curl -Os https://uploader.codecov.io/latest/linux/codecov && chmod +x codecov
 ./codecov -t "$CODECOV_TOKEN" -s . -F rust
+
+echo "+++ Run python UDF SDK unit tests"
+cd ${REPO_ROOT}/src/udf/python
+pytest
+cd ${REPO_ROOT}
