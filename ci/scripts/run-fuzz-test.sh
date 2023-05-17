24b09289d4aa9e80b25b0e2df4b5ade7aa3bd530
Noel Kwan <noelkwan1998@gmail.com>
2023-05-17 18:56:09 +0800

    export RUST_LOG

--- ci/scripts/run-fuzz-test.sh
+++ ci/scripts/run-fuzz-test.sh
@@ -4,6 +4,7 @@
 set -euo pipefail
 
 export LOGDIR=.risingwave/log
+export RUST_LOG=info
 
 if [[ $RUN_SQLSMITH_FRONTEND -eq "1" ]]; then
     echo "--- Run sqlsmith frontend tests"
@@ -42,7 +43,7 @@ if [[ "$RUN_SQLSMITH" -eq "1" ]]; then
     cargo make ci-start ci-3cn-1fe
 
     echo "--- e2e, ci-3cn-1fe, run fuzzing"
-    timeout 20m RUST_LOG=info ./target/debug/sqlsmith test \
+    timeout 20m ./target/debug/sqlsmith test \
       --count "$SQLSMITH_COUNT" \
       --testdata ./src/tests/sqlsmith/tests/testdata \
       2>"$LOGDIR/fuzzing.log" && rm "$LOGDIR/fuzzing.log"
