5c4dadc7df78959dee340be5d211c62991472740
Noel Kwan <noelkwan1998@gmail.com>
2023-05-18 19:39:31 +0800

    fix

--- ci/scripts/deterministic-e2e-test.sh
+++ ci/scripts/deterministic-e2e-test.sh
@@ -18,8 +18,8 @@ popd
 
 echo "--- Extract data for SqlSmith"
 pushd ./src/tests/sqlsmith/tests
+git clone https://"$GITHUB_TOKEN"@github.com/risingwavelabs/sqlsmith-query-snapshots.git
 # FIXME(kwannoel): Uncomment this to stage changes. Should have a better approach.
-# git clone https://"$GITHUB_TOKEN"@github.com/risingwavelabs/sqlsmith-query-snapshots.git
 # pushd sqlsmith-query-snapshots
 # git checkout stage
 # popd
