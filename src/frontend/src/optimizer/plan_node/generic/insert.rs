bf3d24c1ecc93cdd70bb05beb32daa341e44c9d2
ice1000 <ice1000kotlin@foxmail.com>
2023-05-18 01:06:26 +0000

    pub

--- src/frontend/src/optimizer/plan_node/generic/insert.rs
+++ src/frontend/src/optimizer/plan_node/generic/insert.rs
@@ -19,20 +19,20 @@ use risingwave_common::catalog::{Schema, TableVersionId};
 
 use super::GenericPlanRef;
 use crate::catalog::TableId;
-use crate::OptimizerContextRef;
 use crate::expr::ExprImpl;
+use crate::OptimizerContextRef;
 
 #[derive(Debug, Clone, Educe)]
 #[educe(PartialEq, Eq, Hash)]
 pub struct Insert<PlanRef: Eq + Hash> {
     #[educe(PartialEq(ignore))]
     #[educe(Hash(ignore))]
-    table_name: String, // explain-only
-    table_id: TableId,
-    table_version_id: TableVersionId,
-    input: PlanRef,
-    column_indices: Vec<usize>,              // columns in which to insert
-    default_columns: Vec<(usize, ExprImpl)>, // columns to be set to default
-    row_id_index: Option<usize>,
-    returning: bool,
+    pub table_name: String, // explain-only
+    pub table_id: TableId,
+    pub table_version_id: TableVersionId,
+    pub input: PlanRef,
+    pub column_indices: Vec<usize>, // columns in which to insert
+    pub default_columns: Vec<(usize, ExprImpl)>, // columns to be set to default
+    pub row_id_index: Option<usize>,
+    pub returning: bool,
 }
