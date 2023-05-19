bc99bf2ff4b46531f888ef1a6b7a2061336b7d77
StrikeW <wangsiyuanse@gmail.com>
2023-05-19 12:11:06 +0800

    update

--- integration_tests/mysql-cdc/create_mv.sql
+++ integration_tests/mysql-cdc/create_mv.sql
@@ -1,12 +1,3 @@
--- CREATE MATERIALIZED VIEW product_count AS
--- SELECT
---     product_id,
---     COUNT(*) as product_count
--- FROM
---     orders_rw
--- GROUP BY
---     product_id;
-
 CREATE MATERIALIZED VIEW lineitem_count AS
 SELECT
     COUNT(*) as cnt
--- integration_tests/mysql-cdc/create_source.sql
+++ integration_tests/mysql-cdc/create_source.sql
@@ -1,22 +1,3 @@
--- create table orders_rw (
---     order_id int,
---     order_date bigint,
---     customer_name varchar,
---     price decimal,
---     product_id int,
---     order_status smallint,
---     PRIMARY KEY (order_id)
--- ) with (
---     connector = 'mysql-cdc',
---     hostname = 'mysql',
---     port = '3306',
---     username = 'root',
---     password = '123456',
---     database.name = 'mydb',
---     table.name = 'myorders',
---     server.id = '1'
--- );
-
 CREATE TABLE lineitem_rw (
    L_ORDERKEY BIGINT,
    L_PARTKEY BIGINT,
