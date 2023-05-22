05a9a165280ba379d121fa466fd19f6642f69cef
Ubuntu <honeta@qq.com>
2023-05-22 09:19:47 +0000

    Support multiple windows

--- src/frontend/src/optimizer/rule/over_window_to_agg_and_join_rule.rs
+++ src/frontend/src/optimizer/rule/over_window_to_agg_and_join_rule.rs
@@ -35,60 +35,66 @@ impl OverWindowToAggAndJoinRule {
 impl Rule for OverWindowToAggAndJoinRule {
     fn apply(&self, plan: PlanRef) -> Option<PlanRef> {
         let over_window = plan.as_logical_over_window()?;
-        if over_window.window_functions().len() != 1 {
+        let window_functions = over_window.window_functions();
+        if window_functions.iter().any(|window| {
+            !window.order_by.is_empty()
+                || !window.frame.bounds.start_is_unbounded()
+                || !window.frame.bounds.end_is_unbounded()
+        }) {
             return None;
         }
-        let window = &over_window.window_functions()[0];
-        if !window.order_by.is_empty()
-            || !window.frame.bounds.start_is_unbounded()
-            || !window.frame.bounds.end_is_unbounded()
-        {
-            return None;
-        }
-        let group_exprs: Vec<ExprImpl> = window
+        let group_exprs: Vec<ExprImpl> = window_functions[0]
             .partition_by
             .iter()
             .map(|x| x.clone().into())
             .collect_vec();
         let mut select_exprs = group_exprs.clone();
-        if let WindowFuncKind::Aggregate(kind) = window.kind {
-            let agg_call = AggCall::new(
-                kind,
-                window.args.iter().map(|x| x.clone().into()).collect_vec(),
-                false,
-                OrderBy::any(),
-                Condition::true_cond(),
-            );
-            if agg_call.is_err() {
+        for window in window_functions {
+            if let WindowFuncKind::Aggregate(kind) = window.kind {
+                let agg_call = AggCall::new(
+                    kind,
+                    window.args.iter().map(|x| x.clone().into()).collect_vec(),
+                    false,
+                    OrderBy::any(),
+                    Condition::true_cond(),
+                );
+                if agg_call.is_err() {
+                    return None;
+                }
+                select_exprs.push(agg_call.unwrap().into());
+            } else {
                 return None;
             }
-            select_exprs.push(agg_call.unwrap().into());
-        } else {
-            return None;
         }
 
         let input_len = over_window.input().schema().len();
         let mut out_fields = (0..input_len).collect_vec();
-        out_fields.push(input_len + group_exprs.len());
+        for i in 0..window_functions.len() {
+            out_fields.push(input_len + group_exprs.len() + i);
+        }
         let common_input = LogicalShare::create(over_window.input());
         let agg = LogicalAgg::create(select_exprs, group_exprs, None, common_input.clone());
         if agg.is_err() {
             return None;
         }
         let mut on_clause = Condition::true_cond();
-        window.partition_by.iter().enumerate().for_each(|(idx, x)| {
-            on_clause = on_clause.clone().and(Condition::with_expr(
-                FunctionCall::new(
-                    Type::Equal,
-                    vec![
-                        x.clone().into(),
-                        InputRef::new(idx + input_len, x.data_type.clone()).into(),
-                    ],
-                )
-                .unwrap()
-                .into(),
-            ))
-        });
+        window_functions[0]
+            .partition_by
+            .iter()
+            .enumerate()
+            .for_each(|(idx, x)| {
+                on_clause = on_clause.clone().and(Condition::with_expr(
+                    FunctionCall::new(
+                        Type::Equal,
+                        vec![
+                            x.clone().into(),
+                            InputRef::new(idx + input_len, x.data_type.clone()).into(),
+                        ],
+                    )
+                    .unwrap()
+                    .into(),
+                ))
+            });
         Some(
             LogicalProject::with_out_col_idx(
                 LogicalJoin::new(common_input, agg.unwrap().0, JoinType::Inner, on_clause).into(),
