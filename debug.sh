fec4fd7903ce78a35bbe524eea7e81b3b0363d6b
Noel Kwan <noelkwan1998@gmail.com>
2023-05-22 12:33:08 +0800

    checkpoint

--- debug.sh
+++ debug.sh
@@ -1,9 +1,37 @@
 #!/usr/bin/env bash
+set -euo pipefail
+
+execute_queries() {
+  QUERIES=$1
+  # Delimit by newlines ONLY using bash's IFS (internal field separator)
+  IFS_RESET=$IFS
+  IFS=$'\n'
+
+  for QUERY in $QUERIES
+  do
+    echo "[EXECUTING]: $QUERY"
+    echo -n "[RESULT]: "; psql -h localhost -p 4566 -d dev -U root -c "$QUERY"
+    echo ""
+  done
+
+  IFS=$IFS_RESET
+}
+
 # TODO(kwannoel): Make this an actual test script.
-./risedev clean-data
+echo "--- Clean data"
+./risedev clean-data >/dev/null 2>&1
+echo "--- Start cluster"
 ./risedev d ci-3cn-2fe-3meta-with-recovery
-psql -h localhost -p 4566 -d dev -U root -c "
-CREATE TABLE t(v1 int);
-CREATE MATERIALIZED VIEW m2(v1 int);
-"
-./risedev k
+QUERIES="CREATE TABLE t(v1 int primary key);
+INSERT INTO t values (1), (2), (3), (4);
+FLUSH;
+CREATE MATERIALIZED VIEW m as select * from t;"
+
+execute_queries "$QUERIES"
+
+echo "--- KILL ALL NODES EXCEPT META"
+
+echo "--- WAIT FOR RECOVERY"
+
+echo "--- KILL"
+./risedev k >/dev/null 2>&1
