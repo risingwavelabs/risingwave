75bcb50311cbc74bff86a085129094245e2a9797
Little-Wallace <bupt2013211450@gmail.com>
2023-05-23 14:43:25 +0800

    fix che3ck
    Signed-off-by: Little-Wallace <bupt2013211450@gmail.com>

--- src/storage/src/hummock/compactor/shared_buffer_compact.rs
+++ src/storage/src/hummock/compactor/shared_buffer_compact.rs
@@ -23,13 +23,12 @@ use itertools::Itertools;
 use risingwave_common::cache::CachePriority;
 use risingwave_common::catalog::TableId;
 use risingwave_common::hash::VirtualNode;
-use risingwave_hummock_sdk::compact::append_sstable_info_to_string;
 use risingwave_hummock_sdk::compaction_group::StaticCompactionGroupId;
 use risingwave_hummock_sdk::key::{FullKey, TableKey, UserKey};
 use risingwave_hummock_sdk::key_range::KeyRange;
 use risingwave_hummock_sdk::{CompactionGroupId, HummockEpoch, LocalSstableInfo};
 use risingwave_pb::hummock::compact_task;
-use tracing::{error, info};
+use tracing::error;
 
 use crate::filter_key_extractor::FilterKeyExtractorImpl;
 use crate::hummock::compactor::compaction_filter::DummyCompactionFilter;
