2725deae6d06c64faecaddcf760f24102d0774c1
weili <a320_271N@outlook.com>
2023-05-26 15:03:44 +0800

    "upgrade" sink e2e test

--- e2e_test/sink/remote/mysql_create_table.sql
+++ e2e_test/sink/remote/mysql_create_table.sql
@@ -1,11 +1,17 @@
 CREATE TABLE t_remote (
-    id integer PRIMARY KEY,
-    v_varchar varchar(100),
-    v_smallint smallint,
-    v_integer integer,
-    v_bigint bigint,
-    v_decimal decimal,
-    v_float float,
-    v_double double,
-    v_timestamp timestamp
+    id BIGINT PRIMARY KEY,
+    v_varchar VARCHAR(255),
+    v_text TEXT,
+    v_integer INT,
+    v_smallint SMALLINT,
+    v_bigint BIGINT,
+    v_decimal DECIMAL(10,2),
+    v_real FLOAT,
+    v_double DOUBLE,
+    v_boolean BOOLEAN,
+    v_date DATE,
+    v_time TIME,
+    v_timestamp TIMESTAMP,
+    v_jsonb JSON,
+    v_bytea BLOB
 );
