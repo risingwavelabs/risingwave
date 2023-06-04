8af99ccedf3b4628865ac534f34e62fb5eb57b7e
xxchan <xxchan22f@gmail.com>
2023-06-04 22:28:35 +0200

    Discard changes to src/cmd/src/bin/compactor.rs

--- src/cmd/src/bin/compactor.rs
+++ src/cmd/src/bin/compactor.rs
@@ -11,6 +11,7 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
+
 #![cfg_attr(coverage, feature(no_coverage))]
 
 risingwave_cmd::main!(compactor);
