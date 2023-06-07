a7a4b5634e75a2ca2c6541c3b2648968da25ae27
StrikeW <wangsiyuanse@gmail.com>
2023-06-07 10:39:11 +0800

    jdbc sink data type tests

--- integration_tests/mysql-sink/mysql_prepare.sql
+++ integration_tests/mysql-sink/mysql_prepare.sql
@@ -2,3 +2,23 @@ CREATE TABLE target_count (
   target_id VARCHAR(128) primary key,
   target_count BIGINT
 );
+
+
+-- sink table
+CREATE TABLE data_types (
+    id BIGINT PRIMARY KEY,
+    varchar_column VARCHAR(255),
+    text_column TEXT,
+    integer_column INT,
+    smallint_column SMALLINT,
+    bigint_column BIGINT,
+    decimal_column DECIMAL(10,2),
+    real_column FLOAT,
+    double_column DOUBLE,
+    boolean_column BOOLEAN,
+    date_column DATE,
+    time_column TIME,
+    timestamp_column TIMESTAMP,
+    jsonb_column JSON,
+    bytea_column BLOB,
+);
