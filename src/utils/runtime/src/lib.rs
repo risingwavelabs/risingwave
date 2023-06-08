217fc24be94d48c8aee28432b3aed4409f09fd4b
congyi wang <58715567+wcy-fdu@users.noreply.github.com>
2023-06-08 14:01:47 +0800

    refactor(aws): bump aws sdk version (#10091)
    Co-authored-by: Runji Wang <wangrunji0408@163.com>

--- src/utils/runtime/src/lib.rs
+++ src/utils/runtime/src/lib.rs
@@ -22,7 +22,7 @@ use std::time::Duration;
 
 use futures::Future;
 use tracing::Level;
-use tracing_subscriber::filter::{Directive, Targets};
+use tracing_subscriber::filter::{Directive, LevelFilter, Targets};
 use tracing_subscriber::layer::SubscriberExt;
 use tracing_subscriber::prelude::*;
 use tracing_subscriber::{filter, EnvFilter};
@@ -156,6 +156,8 @@ pub fn init_risingwave_logger(settings: LoggerSettings) {
             .with_target("aws_sdk_ec2", Level::INFO)
             .with_target("aws_sdk_s3", Level::INFO)
             .with_target("aws_config", Level::WARN)
+            .with_target("aws_smithy_types", Level::INFO)
+            .with_target("aws_credential_types", LevelFilter::INFO)
             // Only enable WARN and ERROR for 3rd-party crates
             .with_target("aws_endpoint", Level::WARN)
             .with_target("hyper", Level::WARN)
