2cd9f603c409f3f814ed2192fd88d80c32be277a
Runji Wang <wangrunji0408@163.com>
2023-06-13 14:18:27 +0800

    move to ./java folder

--- ci/scripts/build-other.sh
+++ ci/scripts/build-other.sh
@@ -6,18 +6,13 @@ set -euo pipefail
 source ci/scripts/common.sh
 
 
-echo "--- Build Java connector node"
+echo "--- Build Java packages"
 cd java
 mvn -B package -Dmaven.test.skip=true
 cd ..
 
-echo "--- Build Java UDF SDK"
-cd src/udf/java
-mvn -B package
-cd ../../..
-
 echo "--- Upload Java artifacts"
 cp java/connector-node/assembly/target/risingwave-connector-1.0.0.tar.gz ./risingwave-connector.tar.gz
-cp src/udf/java/target/risingwave-udf-example.jar ./risingwave-udf-example.jar
+cp java/udf/target/risingwave-udf-example.jar ./risingwave-udf-example.jar
 buildkite-agent artifact upload ./risingwave-connector.tar.gz
 buildkite-agent artifact upload ./risingwave-udf-example.jar
