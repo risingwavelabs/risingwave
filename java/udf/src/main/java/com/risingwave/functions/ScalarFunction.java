66a47e63df74d2aefe9af6358459ec23054b6907
Runji Wang <wangrunji0408@163.com>
2023-06-13 15:50:10 +0800

    add license header

--- java/udf/src/main/java/com/risingwave/functions/ScalarFunction.java
+++ java/udf/src/main/java/com/risingwave/functions/ScalarFunction.java
@@ -1,8 +1,22 @@
+// Copyright 2023 RisingWave Labs
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package com.risingwave.functions;
 
 /**
- * Base interface for a user-defined scalar function. A user-defined scalar function
- * maps zero, one, or multiple scalar values to a new scalar value.
+ * Base interface for a user-defined scalar function. A user-defined scalar
+ * function maps zero, one, or multiple scalar values to a new scalar value.
  * 
  * <p>
  * The behavior of a {@link ScalarFunction} can be defined by implementing a
