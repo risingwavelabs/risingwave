bee47706f8c71f1f9be4b12e8598f303532db9f3
StrikeW <wangsiyuanse@gmail.com>
2023-06-14 01:10:42 +0800

    copyright

--- java/connector-node/risingwave-sink-jdbc/src/main/java/com/risingwave/connector/jdbc/PostgresDialect.java
+++ java/connector-node/risingwave-sink-jdbc/src/main/java/com/risingwave/connector/jdbc/PostgresDialect.java
@@ -1,3 +1,17 @@
+// Copyright 2023 RisingWave Labs
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package com.risingwave.connector.jdbc;
 
 import java.util.List;
@@ -11,14 +25,6 @@ public class PostgresDialect implements JdbcDialect {
         return identifier;
     }
 
-    /**
-     * INSERT INTO t1 VALUES (1, 8.8, 'AA') on conflict (v1) DO UPDATE SET
-     * v1=EXCLUDED.v1,v2=EXCLUDED.v2,v3=EXCLUDED.v3;
-     *
-     * <p>prepared statement: INSERT INTO t1 (field1, field2, field3) VALUES (?, ?, ?) on conflict
-     * (pk_field1, pk_field2) DO UPDATE SET
-     * field1=EXCLUDED.field1,field2=EXCLUDED.field2,field3=EXCLUDED.field3;
-     */
     @Override
     public Optional<String> getUpsertStatement(
             String tableName, List<String> fieldNames, List<String> primaryKeyFields) {
