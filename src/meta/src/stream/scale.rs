1f5aef2245b9fec74cd917ecd9ca6e1401ecfd65
Shanicky Chen <peng@risingwave-labs.com>
2023-06-16 17:10:05 +0800

    Update scale.rs file, remove error handling and update HashMap capacity. #codechange

--- src/meta/src/stream/scale.rs
+++ src/meta/src/stream/scale.rs
@@ -1533,7 +1533,7 @@ where
             })
             .collect();
 
-        let all_table_fragments = self.fragment_manager.list_table_fragments().await?;
+        let all_table_fragments = self.fragment_manager.list_table_fragments().await;
 
         let mut target_plan = HashMap::with_capacity(fragment_ids.len());
 
