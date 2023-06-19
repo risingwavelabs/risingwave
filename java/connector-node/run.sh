c99c3f9d4618cfbdd2076bc30c78fabf9f434a23
yufansong <yufan@risingwave-labs.com>
2023-06-19 17:32:53 +0800

    remove bash script

--- java/connector-node/run.sh
+++
@@ -1,7 +0,0 @@
-RISINGWAVE_ROOT=$(git rev-parse --show-toplevel)
-JAVA_ROOT=$RISINGWAVE_ROOT/java
-CONNECTOR_ROOT=$JAVA_ROOT/connector-node
-# Build shared library file
-cd $RISINGWAVE_ROOT && cargo build -p risingwave_java_binding
-# specify the Djava.library.path, please make sure the shared library you needed exist in /target/debug
-cd ${CONNECTOR_ROOT}/assembly/target && java -classpath "./libs/*" -Djava.library.path=${RISINGWAVE_ROOT}/target/debug com.risingwave.connector.ConnectorService
