469bc5679de53b97c47563bd96dfbec8e122b634
CAJan93 <jan.mensch@gmx.net>
2023-06-19 12:14:53 +0200

    risedev c

--- src/meta/src/manager/cluster.rs
+++ src/meta/src/manager/cluster.rs
@@ -188,7 +188,7 @@ where
     ) -> MetaResult<WorkerType> {
         let mut core = self.core.write().await;
         let mut worker_opt: Option<&mut Worker> = None;
-        for (_, w) in &mut core.workers {
+        for w in core.workers.values_mut() {
             if w.worker_id() == worker_id {
                 worker_opt = Some(w);
                 break;
