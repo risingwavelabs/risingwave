492b5178dbd9f1ec48e3eddfa6d875a48a9882a2
Noel Kwan <noelkwan1998@gmail.com>
2023-06-22 12:41:22 +0800

    remove parsing

--- src/tests/sqlsmith/src/runner.rs
+++ src/tests/sqlsmith/src/runner.rs
@@ -40,16 +40,6 @@ pub async fn run_pre_generated(client: &Client, outdir: &str) {
     let timeout_duration = 3;
     let queries_path = format!("{}/queries.sql", outdir);
     let queries = read_file_contents(queries_path).unwrap();
-    let ddl = queries
-        .lines()
-        .filter(|s| s.starts_with("CREATE"))
-        .collect::<String>();
-    tracing::info!("[DDL]: {}", ddl);
-    let dml = queries
-        .lines()
-        .filter(|s| s.starts_with("INSERT"))
-        .collect::<String>();
-    tracing::info!("[DML]: {}", dml);
     for statement in parse_sql(&queries) {
         let sql = statement.to_string();
         tracing::info!("[EXECUTING STATEMENT]: {}", sql);
