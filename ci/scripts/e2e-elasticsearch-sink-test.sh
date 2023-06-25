67da3aad3df5b8bf5492d97b03b6994eec17dc4a
wu <f_dogs@protonmail.com>
2023-06-25 15:58:06 +0800

    trash

--- ci/scripts/e2e-elasticsearch-sink-test.sh
+++ ci/scripts/e2e-elasticsearch-sink-test.sh
@@ -5,19 +5,18 @@
 # set -euo pipefail
 
 echo "--- preparing elasticsearch"
+
+
+groupadd elasticsearch
+useradd elasticsearch -g elasticsearch -p elasticsearch
+
+su - elasticsearch
+
 # install elasticsearch
 wget https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-7.17.10-linux-x86_64.tar.gz
 wget https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-7.17.10-linux-x86_64.tar.gz.sha512
 shasum -a 512 -c elasticsearch-7.17.10-linux-x86_64.tar.gz.sha512
 tar -xzf elasticsearch-7.17.10-linux-x86_64.tar.gz
-
-groupadd elasticsearch
-useradd elasticsearch -g elasticsearch -p elasticsearch
-
-chown -R elasticsearch:elasticsearch ./elasticsearch-7.17.10
-chmod o+x ./elasticsearch-7.17.10
-chgrp elasticsearch ./elasticsearch-7.17.10
-su - elasticsearch
 
 ./bin/elasticsearch
 
