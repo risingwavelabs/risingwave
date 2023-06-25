f469a2026f86465678ac1fdcf6a1c7ec19c6ea33
wu <f_dogs@protonmail.com>
2023-06-25 19:57:26 +0800

    fix sort

--- e2e_test/sink/elasticsearch/elasticsearch.py
+++ e2e_test/sink/elasticsearch/elasticsearch.py
@@ -1,5 +1,6 @@
 import sys
 import json
+from operator import itemgetter
 
 expected_output_file = sys.argv[1]
 test_output_file = sys.argv[2]
@@ -9,12 +10,12 @@ test_shard = []
 with open(expected_output_file) as file:
     line = file.readline()
     file = json.loads(line)
-    expected_shard = sorted(file["hits"].items())
+    expected_shard = sorted(file["hits"]["hits"], key=itemgetter("_id"))
 
 with open(test_output_file) as file:
     line = file.readline()
     file = json.loads(line)
-    test_shard = sorted(file["hits"].items())
+    test_shard = sorted(file["hits"]["hits"], key=itemgetter("_id"))
 
 if test_shard != expected_shard:
     print(test_shard)
