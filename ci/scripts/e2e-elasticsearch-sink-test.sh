924edd153e90c381cf2051ff64806551c8dcfba0
wu <f_dogs@protonmail.com>
2023-06-25 17:34:23 +0800

    fix

--- ci/scripts/e2e-elasticsearch-sink-test.sh
+++ ci/scripts/e2e-elasticsearch-sink-test.sh
@@ -15,7 +15,7 @@ echo "testing sink result"
 # curl result is of the form
 # {"took":3,"timed_out":false,"_shards": .. }, took varies from query and therefore
 # we cut "{"took":3,"timed_out":false,"_shards":" out by awk.
-curl curl -XGET "http://elasticsearch:9200/test/_search" -H 'Content-Type: application/json' -d'{"query":{"match_all":{}}}' | awk -F"_shards\":{" '{print$2}' | awk -F"}}]}}" '{print $1}' > ./e2e_test/sink/elasticsearch/elasticsearch_sink.tmp.result
+curl -XGET "http://elasticsearch:9200/test/_search" -H 'Content-Type: application/json' -d'{"query":{"match_all":{}}}' | awk -F"_shards\":{" '{print$2}' | awk -F"}}]}}" '{print $1}' > ./e2e_test/sink/elasticsearch/elasticsearch_sink.tmp.result
 diff ./e2e_test/sink/elasticsearch/elasticsearch_sink.result ./e2e_test/sink/elasticsearch/elasticsearch_sink.tmp.result
 if [ $? -ne 0 ]; then
   echo "The output is not as expected."
