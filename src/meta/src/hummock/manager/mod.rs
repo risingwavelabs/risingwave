07ed41e8ff6320f20ffa378993412b4c7114ea04
Little-Wallace <bupt2013211450@gmail.com>
2023-06-27 17:15:30 +0800

    fix format
    Signed-off-by: Little-Wallace <bupt2013211450@gmail.com>

--- src/meta/src/hummock/manager/mod.rs
+++ src/meta/src/hummock/manager/mod.rs
@@ -911,12 +911,10 @@ where
             let (compact_task_size, compact_task_file_count, _) =
                 estimate_state_for_compaction(&compact_task);
 
-            let level_type_label =
-                format!(
-                    "L{}->L{}",
-                    compact_task.input_ssts[0].level_idx,
-                    compact_task.input_ssts[1].level_idx,
-                );
+            let level_type_label = format!(
+                "L{}->L{}",
+                compact_task.input_ssts[0].level_idx, compact_task.input_ssts[1].level_idx,
+            );
 
             let level_count = compact_task.input_ssts.len();
             if compact_task.input_ssts[0].level_idx == 0 {
