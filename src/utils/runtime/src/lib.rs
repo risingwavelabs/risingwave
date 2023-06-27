ff14af72044fdda20d9ae1db91cbbcabeeb75376
congyi <15605187270@163.com>
2023-06-27 21:06:46 +0800

    add log layer

--- src/utils/runtime/src/lib.rs
+++ src/utils/runtime/src/lib.rs
@@ -25,6 +25,7 @@ use futures::Future;
 use risingwave_common::metrics::MetricsLayer;
 use risingwave_common::util::env_var::is_ci;
 use tracing::level_filters::LevelFilter as Level;
+use tracing_log::LogTracer;
 use tracing_subscriber::filter::{FilterFn, Targets};
 use tracing_subscriber::fmt::time::OffsetTime;
 use tracing_subscriber::layer::SubscriberExt;
@@ -371,6 +372,7 @@ pub fn init_risingwave_logger(settings: LoggerSettings, registry: prometheus::Re
         layers.push(Box::new(MetricsLayer::new(registry).with_filter(filter)));
     }
 
+    LogTracer::init().expect("Failed to set logger");
     tracing_subscriber::registry().with(layers).init();
 
     // TODO: add file-appender tracing subscriber in the future
