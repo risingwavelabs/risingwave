48bf18f2978e1e694a7fca5b389814a7b28c8fcd
wu <f_dogs@protonmail.com>
2023-06-27 08:04:36 +0000

    clean code

--- src/connector/src/parser/unified/bytes.rs
+++ src/connector/src/parser/unified/bytes.rs
@@ -40,8 +40,8 @@ impl ChangeEvent for BytesChangeEvent {
         Ok(ChangeEventOperation::Upsert)
     }
 
-    fn access_field(&self, _name: &str, type_expected: &DataType) -> super::AccessResult {
-        self.value_accessor.access(&vec![], Some(type_expected))
+    fn access_field(&self, name: &str, type_expected: &DataType) -> super::AccessResult {
+        self.value_accessor.access(&[name], Some(type_expected))
     }
 }
 
@@ -49,15 +49,11 @@ impl Access for BytesAccess {
     /// path is empty currently, type_expected should be `Bytea`
     fn access(&self, _path: &[&str], type_expected: Option<&DataType>) -> AccessResult {
         if let Some(DataType::Bytea) = type_expected {
-            // TODO: figure out how to prevent this clone
-            let test = ScalarImpl::Bytea(self.bytes.clone().into());
-            println!("{:?}", test);
-            // let test = ScalarImpl::Bytea("\\x233".as_bytes().into());
-            return Ok(Some(test));
+            return Ok(Some(ScalarImpl::Bytea(Box::from(self.bytes.as_slice()))));
         }
         return Err(AccessError::TypeError {
             expected: "Bytea".to_string(),
-            got: format!("{:?}", type_expected),
+            got: format!("{:?}", type_expected),    
             value: "()".to_string(),
         });
     }
