d3ae8ecd6b31dd3cc2ced962f9cb2330d595d445
StrikeW <wangsiyuanse@gmail.com>
2023-06-29 10:05:31 +0800

    license

--- java/connector-node/risingwave-sink-jdbc/src/main/java/com/risingwave/connector/jdbc/SchemaTableName.java
+++ java/connector-node/risingwave-sink-jdbc/src/main/java/com/risingwave/connector/jdbc/SchemaTableName.java
@@ -1,9 +1,23 @@
+// Copyright 2023 RisingWave Labs
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package com.risingwave.connector.jdbc;
 
 /**
  * A simple class to hold the schema name and table name. A database dialect may extend this class
- * and override the getNormalizedTableName() method to generate a qualified table name to be used in
- * SQL statements.
+ * to customize the schema name and table name for the database. And the getNormalizedTableName()
+ * should generage a normalized table name for the database.
  */
 public class SchemaTableName {
     protected String schemaName;
