8ef743f56b1655ee7731bf885b5428421322eaba
StrikeW <wangsiyuanse@gmail.com>
2023-06-29 12:36:44 +0800

    fix(jdbc-sink): add schema support for postgres sink (#10576)

---
+++ java/connector-node/risingwave-sink-jdbc/src/main/java/com/risingwave/connector/jdbc/SchemaTableName.java
@@ -0,0 +1,42 @@
+// Copyright 2023 RisingWave Labs
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.risingwave.connector.jdbc;
+
+/** A simple class to hold the necessary information to identify a table in the database. */
+public class SchemaTableName {
+    protected String schemaName;
+    protected String tableName;
+
+    public SchemaTableName(String schemaName, String tableName) {
+        this.schemaName = schemaName;
+        this.tableName = tableName;
+    }
+
+    public String getSchemaName() {
+        return schemaName;
+    }
+
+    public String getTableName() {
+        return tableName;
+    }
+
+    public void setSchemaName(String schemaName) {
+        this.schemaName = schemaName;
+    }
+
+    public void setTableName(String tableName) {
+        this.tableName = tableName;
+    }
+}
