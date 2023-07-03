f8ae2cc9b02c34826813a3e4c574a5d773ba4dc8
Zhanxiang (Patrick) Huang <hzxa21@hotmail.com>
2023-07-03 14:27:51 +0800

    chore: update barrier number metrics regularly (#10692)

--- src/meta/src/barrier/mod.rs
+++ src/meta/src/barrier/mod.rs
@@ -614,6 +614,7 @@ where
                     self.handle_new_barrier(&barrier_complete_tx, &mut state, &mut checkpoint_control).await;
                 }
             }
+            checkpoint_control.update_barrier_nums_metrics();
         }
     }
 
@@ -797,8 +798,6 @@ where
         state: &mut BarrierManagerState,
         checkpoint_control: &mut CheckpointControl<S>,
     ) {
-        checkpoint_control.update_barrier_nums_metrics();
-
         let BarrierCompletion { prev_epoch, result } = completion;
 
         // Received barrier complete responses with an epoch that is not managed by checkpoint
