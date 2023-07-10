2140b25d115dd79f28cdcf14c6f0ba88fd02796e
StrikeW <wangsiyuanse@gmail.com>
2023-07-10 11:05:30 +0800

    feat(pg-cdc): fix publication and support user-provided publication  (#10804)

--- e2e_test/source/cdc/postgres_cdc_insert.sql
+++ e2e_test/source/cdc/postgres_cdc_insert.sql
@@ -1,2 +1,7 @@
 INSERT INTO shipments
 VALUES (default,10004,'Beijing','Shanghai',false);
+
+INSERT INTO person VALUES (1003, 'vicky jones', 'kedmrpz@xiauh.com', '5536 1959 5460 2096', 'portland');
+INSERT INTO person VALUES (1004, 'julie white', 'egpemle@lrhcg.com', '0052 8113 1582 4430', 'seattle');
+
+insert into abs.t1 values (2, 2.2, 'bbb', '1234.5431');
