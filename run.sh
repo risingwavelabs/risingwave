2b4b6b606368e9e384e4092c65264b23c4152cd6
Noel Kwan <noelkwan1998@gmail.com>
2023-07-11 15:52:23 +0800

    bench with temporal filter

--- run.sh
+++ run.sh
@@ -4,4 +4,4 @@ source ../risingwave-nix/functions.sh
 # RUST_LOG="risingwave_stream=trace" ./risedev d full-without-monitoring
 RUST_LOG="risingwave_stream=trace" ./risedev d
 # sleep 10
-f queries.sql
+f queries2.sql
