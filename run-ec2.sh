f4dbba00660d67c08ac05e35d972b3d17dc18e7d
Noel Kwan <noelkwan1998@gmail.com>
2023-07-11 16:31:10 +0800

    fix

--- run-ec2.sh
+++ run-ec2.sh
@@ -3,6 +3,11 @@
 source ../risingwave-nix/functions.sh
 # RUST_LOG="risingwave_stream=trace" ./risedev d full-without-monitoring
 RUST_LOG="risingwave_stream=trace" ./risedev d full
-# sleep 10
+sleep 10
 f queries2.sql
+for i in $(seq 1 100000)
+do
+    psql -h localhost -p 4566 -d dev -U root -c "insert into sales values ($i, $i, $i * interval '1 hour' + timestamp '2023-01-01 00:00:01');"
+done
+
 ./risedev k
--- run.sh
+++ run.sh
@@ -3,5 +3,6 @@
 source ../risingwave-nix/functions.sh
 # RUST_LOG="risingwave_stream=trace" ./risedev d full-without-monitoring
 RUST_LOG="risingwave_stream=trace" ./risedev d
+
 # sleep 10
 f queries2.sql
