f4f00ac96c5a8d52ffde8f327ba00559fa2513fa
Noel Kwan <noelkwan1998@gmail.com>
2023-07-14 14:38:25 +0800

    fix

--- run-ec2.sh
+++ run-ec2.sh
@@ -1,13 +1,18 @@
 #!/usr/bin/env bash
 
+./risedev k && ./risedev clean-data
+
 # source ../risingwave-nix/functions.sh
 # RUST_LOG="risingwave_stream=trace" ./risedev d full-without-monitoring
 RUST_LOG="risingwave_stream=trace" ./risedev d full
 sleep 10
-f queries2.sql
-for i in $(seq 1 100000)
+psql -h localhost -p 4566 -d dev -U root -f queries2.sql
+for i in $(seq 1 10000000)
 do
-    psql -h localhost -p 4566 -d dev -U root -c "insert into sales values ($i, $i, $i * interval '1 hour' + timestamp '2023-01-01 00:00:01');"
+    psql -h localhost -p 4566 -d dev -U root -c "
+insert into sales values ($i, $i, $i * interval '1 hour' + timestamp '2023-01-01 00:00:01');
+" &
+if [[ $(( $i % 16 )) -eq 0 ]]; then
+        wait
+fi
 done
-
-./risedev k
