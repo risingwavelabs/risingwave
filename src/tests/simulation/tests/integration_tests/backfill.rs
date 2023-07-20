6a13306c0fe630b950ff2d99e25c94f732227fb0
Noel Kwan <noelkwan1998@gmail.com>
2023-07-20 21:55:17 +0800

    add deterministic backfill tests

---
+++ src/tests/simulation/tests/integration_tests/backfill.rs
@@ -0,0 +1,83 @@
+// Copyright 2023 RisingWave Labs
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+use std::sync::LazyLock;
+use std::time::Duration;
+
+use anyhow::Result;
+use tokio::join;
+use itertools::Itertools;
+use madsim::time::sleep;
+use risingwave_simulation::cluster::{Cluster, Configuration, Session};
+use risingwave_simulation::ctl_ext::predicate::{identity_contains, no_identity_contains};
+use risingwave_simulation::utils::AssertResult;
+
+const ROOT_TABLE_CREATE: &str = "create table t1 (_id int, data jsonb);";
+const INSERT_SEED_SQL: &str = r#"insert into t1 values (1, '{"orders": {"id": 1, "price": "2.30", "customer_id": 2}}');"#;
+const INSERT_RECURSE_SQL: &str = "insert into t1 select _id + 1, data from t1;";
+const MV1: &str = r#"
+create materialized view mv1 as
+with p1 as (
+	select
+		_id as id,
+		(data ->> 'orders')::jsonb as orders
+	from t1
+),
+p2 as (
+	select
+	 id,
+	 orders ->> 'id' as order_id,
+	 orders ->> 'price' as order_price,
+	 orders ->> 'customer_id' as order_customer_id
+	from p1
+)
+select
+    id,
+    order_id,
+    order_price,
+    order_customer_id
+from p2;
+"#;
+
+// FIXME: This test fails to get backfill to process updates
+// concurrently with Materialized View Creation.
+// It mirrors the process in `run-backfill-tests.sh`.
+#[madsim::test]
+async fn test_backfill_with_upstream_and_snapshot_read() -> Result<()> {
+    let mut cluster = Cluster::start(Configuration::for_scale()).await?;
+    let mut session = cluster.start_session();
+    let mut session2 = cluster.start_session();
+
+    session.run(ROOT_TABLE_CREATE).await?;
+
+    session.run(INSERT_SEED_SQL).await?;
+    session.run("flush").await?;
+
+    for _ in 0..18 {
+        session.run(INSERT_RECURSE_SQL).await?;
+        session.run("flush").await?;
+    }
+
+    // Run create mv async
+    let mv1_task = tokio::spawn(async move { session.run(MV1).await });
+
+    // Create lots of base table update + barrier
+    for _ in 0..1000 {
+            match session2.run(INSERT_SEED_SQL).await.map(|_| ()) {
+                Ok(_) => session2.run("flush").await.map(|_| ())?,
+                e @ Err(_) => e?,
+            };
+    }
+    join!(mv1_task).0?.map(|_| ())
+}
