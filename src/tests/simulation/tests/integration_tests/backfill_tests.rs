374d4d1461be6bbdf0701cce62ed0bd0603b8a40
Noel Kwan <noelkwan1998@gmail.com>
2023-07-22 16:10:58 +0800

    fix

--- src/tests/simulation/tests/integration_tests/backfill_tests.rs
+++ src/tests/simulation/tests/integration_tests/backfill_tests.rs
@@ -16,18 +16,21 @@ use std::sync::LazyLock;
 use std::time::Duration;
 
 use anyhow::Result;
-use itertools::Itertools;
+use futures::future::try_join_all;
+use itertools::{any, Itertools};
 use madsim::time::sleep;
 use risingwave_simulation::cluster::{Cluster, Configuration, Session};
 use risingwave_simulation::ctl_ext::predicate::{identity_contains, no_identity_contains};
 use risingwave_simulation::utils::AssertResult;
 use tokio::join;
 
+const SET_PARALLELISM: &str = "SET STREAMING_PARALLELISM=1;";
 const ROOT_TABLE_CREATE: &str = "create table t1 (_id int, data jsonb);";
 const INSERT_SEED_SQL: &str =
     r#"insert into t1 values (1, '{"orders": {"id": 1, "price": "2.30", "customer_id": 2}}');"#;
 const INSERT_AND_FLUSH_SQL: &str = r#"insert into t1 values (1, '{"orders": {"id": 1, "price": "2.30", "customer_id": 2}}'); FLUSH;"#;
 const INSERT_RECURSE_SQL: &str = "insert into t1 select _id + 1, data from t1;";
+const INSERT_RECURSE_AND_FLUSH_SQL: &str = "insert into t1 select _id + 1, data from t1;";
 const MV1: &str = r#"
 create materialized view mv1 as
 with p1 as (
@@ -54,26 +57,43 @@ from p2;
 
 #[madsim::test]
 async fn test_backfill_with_upstream_and_snapshot_read() -> Result<()> {
-    let mut cluster = Cluster::start(Configuration::for_scale()).await?;
+    let mut cluster = Cluster::start(Configuration::for_backfill()).await?;
     let mut session = cluster.start_session();
-    let mut session2 = cluster.start_session();
 
+    session.run(SET_PARALLELISM).await?;
     session.run(ROOT_TABLE_CREATE).await?;
 
     session.run(INSERT_SEED_SQL).await?;
     session.run("flush").await?;
 
-    for _ in 0..20 {
+    // Create snapshot
+    for _ in 0..18 {
         session.run(INSERT_RECURSE_SQL).await?;
+        session.run("flush").await?;
     }
 
-    // Run create mv async
-    let mv1_task = tokio::spawn(async move { session.run(MV1).await });
+    // Create sessions for running updates concurrently.
+    let sessions = (0..3).map(|_| cluster.start_session()).collect_vec();
+    let mut tasks = vec![];
 
-    // Create lots of base table update + barrier
-    for _ in 0..1000_000 {
-        session2.run(INSERT_AND_FLUSH_SQL).await?;
+    // Create lots of base table update
+    for mut session in sessions.into_iter() {
+        let task = tokio::spawn(async move {
+            session.run(INSERT_RECURSE_SQL).await?;
+            anyhow::Ok(())
+        });
+        tasks.push(task);
     }
+
+    // ... Concurrently run create mv async
+    let mv1_task = tokio::spawn(async move {
+        session.run(SET_PARALLELISM).await?;
+        session.run(MV1).await
+    });
+
     mv1_task.await??;
+    for task in tasks {
+        task.await??;
+    }
     Ok(())
 }
