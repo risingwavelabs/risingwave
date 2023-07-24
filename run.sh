0066ef3910bfa3aa000105c2d5e2b566ede383c2
Noel Kwan <noelkwan1998@gmail.com>
2023-07-24 13:40:53 +0800

    update run.sh

--- run.sh
+++ run.sh
@@ -1,9 +1,20 @@
 #!/usr/bin/env bash
 
-source ../risingwave-nix/functions.sh
+f() {
+  psql -h localhost -p 4566 -d dev -U root -f "$@"
+}
 
-# RUST_LOG="risingwave_stream=trace" ./risedev d full-without-monitoring
-./risedev d full
+./risedev k
+./risedev clean-data
+RUST_LOG="risingwave_stream=trace" ./risedev d full
 
-# sleep 10
 f queries3.sql </dev/null
+
+sleep 100
+./risedev k
+
+echo -n "number of delete_ranges: "
+cat .risingwave/log/compute-node*.log | rg "state_table: delete range" | wc -l
+
+echo "^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^"
+echo "After implementing dynamic-filter cache, this number should be 0."
