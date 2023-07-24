6b9ab91b8c0c199c999b7d23830116c14e188fd7
Noel Kwan <noelkwan1998@gmail.com>
2023-07-24 13:03:46 +0800

    update query

--- run.sh
+++ run.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
 
-# source ../risingwave-nix/functions.sh
+source ../risingwave-nix/functions.sh
+
 # RUST_LOG="risingwave_stream=trace" ./risedev d full-without-monitoring
-RUST_LOG="risingwave_stream=trace" ./risedev d
+./risedev d full
 
 # sleep 10
-f queries2.sql
+f queries3.sql </dev/null
