ea7da33e6dcef086694941dc3160ba8cbfbc87a5
TennyZhuang <zty0826@gmail.com>
2023-07-27 10:47:53 +0800

    Apply suggestions from code review
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- src/utils/runtime/src/prof.rs
+++ src/utils/runtime/src/prof.rs
@@ -1,3 +1,17 @@
+// Copyright 2023 RisingWave Labs
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 fn spawn_prof_thread(profile_path: String) -> std::thread::JoinHandle<()> {
     tracing::info!("writing prof data to directory {}", profile_path);
     let profile_path = PathBuf::from(profile_path);
