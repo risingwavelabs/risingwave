ce4e806d7ab0314f36386fae7b8f329329cf90a9
TennyZhuang <zty0826@gmail.com>
2023-07-27 12:05:33 +0800

    address comments
    Signed-off-by: TennyZhuang <zty0826@gmail.com>

--- src/utils/runtime/src/lib.rs
+++ src/utils/runtime/src/lib.rs
@@ -32,36 +32,6 @@ pub use deadlock::*;
 mod panic_hook;
 pub use panic_hook::*;
 
-fn spawn_prof_thread(profile_path: String) -> std::thread::JoinHandle<()> {
-    tracing::info!("writing prof data to directory {}", profile_path);
-    let profile_path = PathBuf::from(profile_path);
-
-    std::fs::create_dir_all(&profile_path).unwrap();
-
-    std::thread::spawn(move || {
-        let mut cnt = 0;
-        loop {
-            let guard = pprof::ProfilerGuardBuilder::default()
-                .blocklist(&["libc", "libgcc", "pthread", "vdso"])
-                .build()
-                .unwrap();
-            std::thread::sleep(Duration::from_secs(60));
-            match guard.report().build() {
-                Ok(report) => {
-                    let profile_svg = profile_path.join(format!("{}.svg", cnt));
-                    let file = std::fs::File::create(&profile_svg).unwrap();
-                    report.flamegraph(file).unwrap();
-                    tracing::info!("produced {:?}", profile_svg);
-                }
-                Err(err) => {
-                    tracing::warn!("failed to generate flamegraph: {}", err);
-                }
-            }
-            cnt += 1;
-        }
-    })
-}
-
 /// Start RisingWave components with configs from environment variable.
 ///
 /// Currently, the following env variables will be read:
