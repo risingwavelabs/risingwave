db1c3ea8ebc4d13d727f92c9fbf7244b6a05b895
August <pin@singularity-data.com>
2023-07-28 15:48:12 +0800

    rewrite pg_locks

--- src/frontend/src/catalog/system_catalog/pg_catalog/pg_locks.rs
+++ src/frontend/src/catalog/system_catalog/pg_catalog/pg_locks.rs
@@ -12,43 +12,39 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+use std::sync::LazyLock;
+
 use risingwave_common::catalog::PG_CATALOG_SCHEMA_NAME;
-use risingwave_common::error::Result;
-use risingwave_common::row::OwnedRow;
 use risingwave_common::types::DataType;
 
-use crate::catalog::system_catalog::{BuiltinTable, SysCatalogReaderImpl};
+use crate::catalog::system_catalog::{infer_dummy_view_sql, BuiltinView, SystemCatalogColumnsDef};
+
+pub const PG_LOCKS_COLUMNS: &[SystemCatalogColumnsDef<'_>] = &[
+    (DataType::Varchar, "locktype"),
+    (DataType::Int32, "database"), // oid
+    (DataType::Int32, "relation"), // oid
+    (DataType::Int32, "page"),
+    (DataType::Int16, "tuple"),
+    (DataType::Varchar, "virtualxid"),
+    (DataType::Int32, "transactionid"), // xid
+    (DataType::Int32, "classid"),       // oid
+    (DataType::Int32, "objid"),         // oid
+    (DataType::Int16, "objsubid"),
+    (DataType::Varchar, "virtualtransaction"),
+    (DataType::Int32, "pid"),
+    (DataType::Varchar, "mode"),
+    (DataType::Boolean, "granted"),
+    (DataType::Boolean, "fastpath"),
+    (DataType::Timestamptz, "waitstart"),
+];
 
 /// The catalog `pg_locks` provides access to information about the locks held by active processes
 /// within the database server.
 /// Reference: [`https://www.postgresql.org/docs/current/view-pg-locks.html`].
 /// Currently, we don't have any type of lock.
-pub const PG_LOCKS: BuiltinTable = BuiltinTable {
+pub static PG_LOCKS: LazyLock<BuiltinView> = LazyLock::new(|| BuiltinView {
     name: "pg_locks",
     schema: PG_CATALOG_SCHEMA_NAME,
-    columns: &[
-        (DataType::Varchar, "locktype"),
-        (DataType::Int32, "database"), // oid
-        (DataType::Int32, "relation"), // oid
-        (DataType::Int32, "page"),
-        (DataType::Int16, "tuple"),
-        (DataType::Varchar, "virtualxid"),
-        (DataType::Int32, "transactionid"), // xid
-        (DataType::Int32, "classid"),       // oid
-        (DataType::Int32, "objid"),         // oid
-        (DataType::Int16, "objsubid"),
-        (DataType::Varchar, "virtualtransaction"),
-        (DataType::Int32, "pid"),
-        (DataType::Varchar, "mode"),
-        (DataType::Boolean, "granted"),
-        (DataType::Boolean, "fastpath"),
-        (DataType::Timestamptz, "waitstart"),
-    ],
-    pk: &[],
-};
-
-impl SysCatalogReaderImpl {
-    pub fn read_locks_info(&self) -> Result<Vec<OwnedRow>> {
-        Ok(vec![])
-    }
-}
+    columns: PG_LOCKS_COLUMNS,
+    sql: infer_dummy_view_sql(PG_LOCKS_COLUMNS),
+});
