90bf75cff075437a3e3d4a7851095c8e4f7cc1e4
tabVersion <tabvision@bupt.icu>
2023-07-28 16:06:52 +0800

    fix

--- src/connector/src/source/kafka/mod.rs
+++ src/connector/src/source/kafka/mod.rs
@@ -12,10 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-use std::collections::HashMap;
 use std::time::Duration;
 
-use anyhow::anyhow;
 use serde::Deserialize;
 use serde_with::{serde_as, DisplayFromStr};
 
