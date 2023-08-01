52d6aedd25f0afe490ab6ef6db627064cf40b02d
Runji Wang <wangrunji0408@163.com>
2023-08-01 22:52:47 +0800

    don't maintain count_ones in builder
    Signed-off-by: Runji Wang <wangrunji0408@163.com>

--- src/common/src/buffer/bitmap.rs
+++ src/common/src/buffer/bitmap.rs
@@ -48,7 +48,6 @@ use crate::estimate_size::EstimateSize;
 pub struct BitmapBuilder {
     len: usize,
     data: Vec<usize>,
-    count_ones: usize,
 }
 
 const BITS: usize = usize::BITS as usize;
@@ -59,7 +58,6 @@ impl BitmapBuilder {
         BitmapBuilder {
             len: 0,
             data: Vec::with_capacity(Bitmap::vec_len(capacity)),
-            count_ones: 0,
         }
     }
 
@@ -68,7 +66,6 @@ impl BitmapBuilder {
         BitmapBuilder {
             len,
             data: vec![0; Bitmap::vec_len(len)],
-            count_ones: 0,
         }
     }
 
@@ -78,16 +75,10 @@ impl BitmapBuilder {
 
         let byte = &mut self.data[n / BITS];
         let mask = 1 << (n % BITS);
-        match (*byte & mask != 0, val) {
-            (true, false) => {
-                *byte &= !mask;
-                self.count_ones -= 1;
-            }
-            (false, true) => {
-                *byte |= mask;
-                self.count_ones += 1;
-            }
-            _ => {}
+        if val {
+            *byte |= mask;
+        } else {
+            *byte &= !mask;
         }
     }
 
@@ -105,7 +96,6 @@ impl BitmapBuilder {
             self.data.push(0);
         }
         self.data[self.len / BITS] |= (bit_set as usize) << (self.len % BITS);
-        self.count_ones += bit_set as usize;
         self.len += 1;
         self
     }
@@ -124,9 +114,6 @@ impl BitmapBuilder {
         if bit_set && self.len % BITS != 0 {
             // remove tailing 1s
             *self.data.last_mut().unwrap() &= (1 << (self.len % BITS)) - 1;
-        }
-        if bit_set {
-            self.count_ones += n;
         }
         self
     }
@@ -149,7 +136,6 @@ impl BitmapBuilder {
         if self.len % BITS == 0 {
             // fast path: self is aligned
             self.len += other.len();
-            self.count_ones += other.count_ones;
             if let Some(bits) = &other.bits {
                 // append the bytes
                 self.data.extend_from_slice(bits);
@@ -175,10 +161,11 @@ impl BitmapBuilder {
 
     /// Finishes building and returns the bitmap.
     pub fn finish(self) -> Bitmap {
+        let count_ones = self.data.iter().map(|&x| x.count_ones()).sum::<u32>() as usize;
         Bitmap {
             num_bits: self.len(),
-            count_ones: self.count_ones,
-            bits: (self.count_ones != 0 && self.count_ones != self.len).then(|| self.data.into()),
+            count_ones,
+            bits: (count_ones != 0 && count_ones != self.len).then(|| self.data.into()),
         }
     }
 
@@ -987,23 +974,20 @@ mod tests {
     #[test]
     fn test_bitmap_set() {
         let mut b = BitmapBuilder::zeroed(10);
-        assert_eq!(b.count_ones, 0);
 
         b.set(0, true);
         b.set(7, true);
         b.set(8, true);
         b.set(9, true);
-        assert_eq!(b.count_ones, 4);
 
         b.set(7, false);
         b.set(8, false);
-        assert_eq!(b.count_ones, 2);
 
         b.append(true);
-        assert_eq!(b.len, 11);
-        assert_eq!(b.count_ones, 3);
+        assert_eq!(b.len(), 11);
 
         let b = b.finish();
+        assert_eq!(b.count_ones(), 3);
         assert_eq!(&b.bits.unwrap()[..], &[0b0110_0000_0001]);
     }
 
