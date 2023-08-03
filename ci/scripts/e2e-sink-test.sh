2e3391294f084f22959ce66e1d46cd460361b7b7
tabVersion <tabvision@bupt.icu>
2023-08-03 17:09:04 +0800

    setup kafka before sink test

--- ci/scripts/e2e-sink-test.sh
+++ ci/scripts/e2e-sink-test.sh
@@ -55,6 +55,7 @@ node_timeout=10
 
 echo "--- starting risingwave cluster with connector node"
 cargo make ci-start ci-kafka
+./scripts/source/prepare_ci_kafka.sh
 ./connector-node/start-service.sh -p $node_port > .risingwave/log/connector-node.log 2>&1 &
 
 echo "waiting for connector node to start"
