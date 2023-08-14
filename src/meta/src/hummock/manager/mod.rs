e558eeefb817dfe2490cfd29aec7aac3ea98cd7d
Wallace <bupt2013211450@gmail.com>
2023-08-14 14:15:23 +0800

    fix(compaction): select wrong task which cause sstable files fail to concat (#11653)
    Signed-off-by: Little-Wallace <bupt2013211450@gmail.com>

--- src/meta/src/hummock/manager/mod.rs
+++ src/meta/src/hummock/manager/mod.rs
@@ -1245,6 +1245,9 @@ where
                     add_prost_table_stats_map(&mut version_stats.table_stats, table_stats_change);
                 }
 
+                // apply version delta before we persist this change. If it causes panic we can
+                // recover to a correct state after restarting meta-node.
+                current_version.apply_version_delta(&version_delta);
                 commit_multi_var!(
                     self,
                     None,
@@ -1255,7 +1258,6 @@ where
                     version_stats
                 )?;
                 branched_ssts.commit_memory();
-                current_version.apply_version_delta(&version_delta);
 
                 trigger_version_stat(&self.metrics, current_version, &versioning.version_stats);
                 trigger_delta_log_stats(&self.metrics, versioning.hummock_version_deltas.len());
