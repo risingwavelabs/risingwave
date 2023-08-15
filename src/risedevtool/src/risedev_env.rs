bc9168f635088481d0e48a4fbd96286f4f1d7b63
Bugen Zhao <i@bugenzhao.com>
2023-08-15 15:22:39 +0800

    fix check
    Signed-off-by: Bugen Zhao <i@bugenzhao.com>

--- src/risedevtool/src/risedev_env.rs
+++ src/risedevtool/src/risedev_env.rs
@@ -12,6 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+#![allow(clippy::doc_markdown)] // RiseDev
+
 use std::fmt::Write;
 use std::process::Command;
 
