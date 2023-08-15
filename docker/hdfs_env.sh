062a6ba0d10f0d144d5709ff320a74f1ea9643e0
congyi <15605187270@163.com>
2023-08-15 17:45:29 +0800

    fmt shell file

--- docker/hdfs_env.sh
+++ docker/hdfs_env.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-export CLASSPATH=$(find $HADOOP_HOME -iname "*.jar" | xargs echo | tr ' ' ':'):${CLASSPATH}
+export CLASSPATH=$(find "$HADOOP_HOME" -iname "*.jar" | xargs echo | tr ' ' ':'):${CLASSPATH}
 export CLASSPATH=$HADOOP_CONF_DIR:$CLASSPATH
 
 /risingwave/bin/risingwave "$@"
