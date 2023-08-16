d19cf036ad78d601b46dbe6f43c348c3240b57cf
wu <f_dogs@protonmail.com>
2023-08-16 05:23:35 +0000

    fix

--- src/meta/src/stream/stream_graph/fragment.rs
+++ src/meta/src/stream/stream_graph/fragment.rs
@@ -17,7 +17,7 @@ use std::num::NonZeroUsize;
 use std::ops::Deref;
 use std::sync::LazyLock;
 
-use anyhow::{anyhow, Context};
+use anyhow::Context;
 use enum_as_inner::EnumAsInner;
 use itertools::Itertools;
 use risingwave_common::bail;
@@ -40,7 +40,7 @@ use crate::model::FragmentId;
 use crate::storage::MetaStore;
 use crate::stream::stream_graph::id::{GlobalFragmentId, GlobalFragmentIdGen, GlobalTableIdGen};
 use crate::stream::stream_graph::schedule::Distribution;
-use crate::{MetaError, MetaResult};
+use crate::MetaResult;
 
 /// The fragment in the building phase, including the [`StreamFragment`] from the frontend and
 /// several additional helper fields.
@@ -355,19 +355,11 @@ impl StreamFragmentGraph {
             }
         }
         if new_ids.len() != old_tables.len() {
-            return Err(MetaError::from(anyhow!(
-                "Different number of internal tables.\n
-                New: {}, Old: {}",
-                new_ids.len(),
-                old_tables.len(),
-            )));
-        }
-        if new_ids.len() != old_tables.len() {
-            return Err(MetaError::from(anyhow!(
-                "Numbers of internal tables mismatch. New: {}, Old: {}",
+            bail!(
+                "Different number of internal tables. New: {}, Old: {}",
                 new_ids.len(),
                 old_tables.len()
-            )));
+            );
         }
         old_tables.sort_by(|a, b| a.id.cmp(&b.id));
         new_ids.sort();
@@ -379,13 +371,11 @@ impl StreamFragmentGraph {
             for table in &mut fragment.internal_tables {
                 let old_table = id_map.get(&table.id).unwrap();
                 if old_table.get_columns() != table.get_columns() {
-                    return Err(MetaError::from(anyhow!(
-                        "Mismatch of internal tables.\n
-                        Old table: {:?}\n
-                        New table: {:?}\n",
+                    bail!(
+                        "Mismatch of internal tables.\nOld table: {:?}\nNew table: {:?}\n",
                         old_table,
                         table
-                    )));
+                    );
                 }
                 table.id = old_table.get_id();
             }
