077f55b8a5aecc4493e4bc08f3ed83aece79ae1d
Noel Kwan <noelkwan1998@gmail.com>
2023-08-16 18:03:26 +0800

    docs

--- src/sqlparser/src/bin/sqlparser.rs
+++ src/sqlparser/src/bin/sqlparser.rs
@@ -1,6 +1,11 @@
 use std::io;
 
 use risingwave_sqlparser::parser::Parser;
+
+/// Just read from stdin and print the AST
+/// ```shell
+/// echo "SELECT 1;" | cargo run --bin sqlparser
+/// ```
 fn main() {
     let mut buffer = String::new();
     io::stdin().read_line(&mut buffer).unwrap();
