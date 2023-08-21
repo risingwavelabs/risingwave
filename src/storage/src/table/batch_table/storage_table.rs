bf19bda30c3e7d3aa092120ef15e22b495690cdb
xxchan <xxchan22f@gmail.com>
2023-08-21 17:24:41 +0200

    revert changes about TAIT

--- src/storage/src/table/batch_table/storage_table.rs
+++ src/storage/src/table/batch_table/storage_table.rs
@@ -17,6 +17,7 @@ use std::ops::Bound::{self, Excluded, Included, Unbounded};
 use std::ops::{Index, RangeBounds};
 use std::sync::Arc;
 
+use auto_enums::auto_enum;
 use await_tree::InstrumentAwait;
 use bytes::Bytes;
 use futures::future::try_join_all;
@@ -488,16 +489,15 @@ impl<S: StateStore, SD: ValueRowSerde> StorageTableInner<S, SD> {
         }))
         .await?;
 
-        // #[auto_enum(futures03::Stream)]
-        // let iter = match iterators.len() {
-        //     0 => unreachable!(),
-        //     1 => iterators.into_iter().next().unwrap(),
-        //     // Concat all iterators if not to preserve order.
-        //     _ if !ordered => futures::stream::iter(iterators).flatten(),
-        //     // Merge all iterators if to preserve order.
-        //     _ => merge_sort(iterators.into_iter().map(Box::pin).collect()),
-        // };
-        let iter = merge_sort(iterators.into_iter().map(Box::pin).collect());
+        #[auto_enum(futures03::Stream)]
+        let iter = match iterators.len() {
+            0 => unreachable!(),
+            1 => iterators.into_iter().next().unwrap(),
+            // Concat all iterators if not to preserve order.
+            _ if !ordered => futures::stream::iter(iterators).flatten(),
+            // Merge all iterators if to preserve order.
+            _ => merge_sort(iterators.into_iter().map(Box::pin).collect()),
+        };
 
         Ok(iter)
     }
