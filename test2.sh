0f15403144732de5f26b9633b26766127d93281f
Noel Kwan <noelkwan1998@gmail.com>
2023-08-21 18:22:14 +0800

    add dylans repro in test2.sh

---
+++ test2.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+set +e
+./risedev k
+./risedev clean-data
+set -e
+
+./risedev d
+
+./risedev psql -c "
+SET STREAMING_PARALLELISM=1;
+create table t(v1 int);
+create materialized view mv1 as select v1 from t;
+insert into t values (1), (2), (3), (4);
+flush;
+"
+
+sleep 10
+
+./risedev psql -c "
+
+select * from mv1;
+
+explain create index i1 on mv1(v1);
+create index i1 on mv1(v1);
+select * from i1;
+"
