7494d0a162a2efb3018e4607bc5313d50372a076
William Wen <william123.wen@gmail.com>
2023-08-22 13:04:20 +0800

    fix unused import

--- src/storage/src/monitor/hummock_state_store_metrics.rs
+++ src/storage/src/monitor/hummock_state_store_metrics.rs
@@ -12,7 +12,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-use std::io::{Error, ErrorKind, Result};
 use std::ops::Deref;
 use std::sync::{Arc, LazyLock};
 
