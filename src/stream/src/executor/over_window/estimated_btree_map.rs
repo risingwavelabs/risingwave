5615cba4f3b9e09571cd986207d6a4672116633a
Richard Chien <stdrc@outlook.com>
2023-08-22 20:12:31 +0800

    add `retain_range` to `EstimatedBTreeMap`
    Signed-off-by: Richard Chien <stdrc@outlook.com>

--- src/stream/src/executor/over_window/estimated_btree_map.rs
+++ src/stream/src/executor/over_window/estimated_btree_map.rs
@@ -13,6 +13,7 @@
 // limitations under the License.
 
 use std::collections::BTreeMap;
+use std::ops::{Bound, RangeInclusive};
 
 use risingwave_common::estimate_size::{EstimateSize, KvSize};
 
@@ -82,6 +83,35 @@ where
         self.inner.clear();
         self.heap_size.set(0);
     }
+
+    /// Retain the given range of entries in the map, removing others.
+    pub fn retain_range(&mut self, range: RangeInclusive<&K>) -> (BTreeMap<K, V>, BTreeMap<K, V>)
+    where
+        K: Clone,
+    {
+        let start = *range.start();
+        let end = *range.end();
+
+        // [ left, [mid], right ]
+        let mut mid_right = self.inner.split_off(start);
+        let mid_right_split_key = mid_right.lower_bound(Bound::Excluded(end)).key().cloned();
+        let right = if let Some(ref mid_right_split_key) = mid_right_split_key {
+            mid_right.split_off(mid_right_split_key)
+        } else {
+            Default::default()
+        };
+        let mid = mid_right;
+        let left = std::mem::replace(&mut self.inner, mid);
+
+        for (k, v) in &left {
+            self.heap_size.sub(k, v);
+        }
+        for (k, v) in &right {
+            self.heap_size.sub(k, v);
+        }
+
+        (left, right)
+    }
 }
 
 impl<K, V> EstimateSize for EstimatedBTreeMap<K, V>
@@ -91,5 +121,40 @@ where
 {
     fn estimated_heap_size(&self) -> usize {
         self.heap_size.size()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::EstimatedBTreeMap;
+
+    #[test]
+    fn test_retain_range() {
+        let mut map = EstimatedBTreeMap::new();
+
+        let (left, right) = map.retain_range(&1..=&10);
+        assert!(left.is_empty());
+        assert!(right.is_empty());
+
+        map.insert(1, "hello".to_string());
+        map.insert(6, "world".to_string());
+        let (left, right) = map.retain_range(&6..=&6);
+        assert_eq!(map.len(), 1);
+        assert_eq!(map.inner[&6], "world".to_string());
+        assert_eq!(left.len(), 1);
+        assert_eq!(left[&1], "hello".to_string());
+        assert!(right.is_empty());
+
+        map.insert(8, "risingwave".to_string());
+        map.insert(3, "great".to_string());
+        map.insert(0, "wooow".to_string());
+        let (left, right) = map.retain_range(&2..=&7);
+        assert_eq!(map.len(), 2);
+        assert_eq!(map.inner[&3], "great".to_string());
+        assert_eq!(map.inner[&6], "world".to_string());
+        assert_eq!(left.len(), 1);
+        assert_eq!(left[&0], "wooow".to_string());
+        assert_eq!(right.len(), 1);
+        assert_eq!(right[&8], "risingwave".to_string());
     }
 }
