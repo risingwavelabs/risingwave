4a8269a79b4b62980fc0ced9e3419509ca4c6539
Noel Kwan <noelkwan1998@gmail.com>
2023-08-25 13:22:52 +0800

    run it at the very start

--- ci/scripts/run-e2e-test.sh
+++ ci/scripts/run-e2e-test.sh
@@ -52,6 +52,38 @@ mv target/debug/risingwave_e2e_extended_mode_test-"$profile" target/debug/rising
 
 chmod +x ./target/debug/risingwave_e2e_extended_mode_test
 
+if [[ "$mode" == "standalone" ]]; then
+  run_sql() {
+    psql -h localhost -p 4566 -d dev -U root -f "$@"
+  }
+
+  echo "--- e2e, standalone, cluster-persistence-test"
+  cluster_start
+  run_sql "CREATE TABLE t (v1 int);
+  INSERT INTO t VALUES (1);
+  INSERT INTO t VALUES (2);
+  INSERT INTO t VALUES (3);
+  INSERT INTO t VALUES (4);
+  INSERT INTO t VALUES (5);
+  flush;"
+
+  EXPECTED=$(run_sql "SELECT * FROM t;")
+  echo -e "Expected:\n$EXPECTED"
+
+  restart_standalone
+
+  ACTUAL=$(run_sql "SELECT * FROM t;")
+  echo -e "Expected:\n$ACTUAL"
+
+  if [[ "$EXPECTED" != "$ACTUAL" ]]; then
+    echo "Expected: $EXPECTED"
+    echo "Actual: $ACTUAL"
+    exit 1
+  fi
+
+  echo "--- Kill cluster"
+  cluster_stop
+fi
 
 echo "--- e2e, $mode, streaming"
 RUST_LOG="info,risingwave_stream=info,risingwave_batch=info,risingwave_storage=info" \
@@ -179,37 +211,4 @@ if [[ "$RUN_META_BACKUP" -eq "1" ]]; then
     bash "${test_root}/run_all.sh"
     echo "--- Kill cluster"
     cargo make kill
-fi
-
-if [[ "$mode" == "standalone" ]]; then
-  run_sql() {
-    psql -h localhost -p 4566 -d dev -U root -f "$@"
-  }
-
-  echo "--- e2e, standalone, cluster-persistence-test"
-  cluster_start
-  run_sql "CREATE TABLE t (v1 int);
-  INSERT INTO t VALUES (1);
-  INSERT INTO t VALUES (2);
-  INSERT INTO t VALUES (3);
-  INSERT INTO t VALUES (4);
-  INSERT INTO t VALUES (5);
-  flush;"
-
-  EXPECTED=$(run_sql "SELECT * FROM t;")
-  echo -e "Expected:\n$EXPECTED"
-
-  restart_standalone
-
-  ACTUAL=$(run_sql "SELECT * FROM t;")
-  echo -e "Expected:\n$ACTUAL"
-
-  if [[ "$EXPECTED" != "$ACTUAL" ]]; then
-    echo "Expected: $EXPECTED"
-    echo "Actual: $ACTUAL"
-    exit 1
-  fi
-
-  echo "--- Kill cluster"
-  cluster_stop
 fi
