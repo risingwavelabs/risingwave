0a52c3b9bee258210b57d9b7cbff5cb4746beb7d
xxhZs <1060434431@qq.com>
2023-08-29 18:12:12 +0800

    fix

--- integration_tests/mfa-change-demo/server/model/sql.py
+++ integration_tests/mfa-change-demo/server/model/sql.py
@@ -1,6 +1,6 @@
 GET_BATCH_FOR_TRAINING = """
 select  l.fare_amount,d.*, p.*
-from location_id_store l 
+from location_id_store l
 join converted_features_with_do_location d on l.window_start = d.window_start and l.do_location_id = d.do_location_id
 join converted_features_with_pu_location p on l.window_start = p.window_start and l.pu_location_id = p.pu_location_id;
 """
