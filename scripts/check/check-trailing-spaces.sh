d8a27d2c31a426eeb1e80873d7715cc9103367bb
Richard Chien <stdrc@outlook.com>
2023-09-01 17:11:46 +0800

    use `git grep` for fast searching
    Signed-off-by: Richard Chien <stdrc@outlook.com>

--- scripts/check/check-trailing-spaces.sh
+++ scripts/check/check-trailing-spaces.sh
@@ -14,49 +14,18 @@ BOLD='\033[1m'
 NONE='\033[0m'
 
 print_help() {
-    echo "Usage: $self [-f|--fix] [-j|--parallel <parallel>]"
+    echo "Usage: $self [-f|--fix]"
     echo
     echo "Options:"
     echo "  -f, --fix              Fix trailing spaces."
-    echo "  -j, --parallel <parallel>"
-    echo "                         Number of processes to run in parallel."
     echo "  -h, --help             Show this help message and exit."
 }
 
-do_check() {
-    # The following is modified from https://github.com/raisedevs/find-trailing-whitespace/blob/restrict-to-plaintext-only/entrypoint.sh.
-
-    files=$1
-    has_trailing_spaces=false
-
-    for file in $files; do
-        lines=$(grep -E -rnIH "[[:space:]]+$" "$file" | cut -f-2 -d ":" || echo "")
-        if [ ! -z "$lines" ]; then
-            has_trailing_spaces=true
-            if [[ $fix == true ]]; then
-                sed -i '' -e's/[[:space:]]*$//' "$file"
-            fi
-            echo -e "${BLUE}$lines${NONE}"
-        fi
-    done
-
-    if [[ $has_trailing_spaces == true ]]; then
-        exit 1
-    else
-        exit 0
-    fi
-}
-
 fix=false
-parallel=$(nproc)
 while [ $# -gt 0 ]; do
     case $1 in
     -f | --fix)
         fix=true
-        ;;
-    -j | --parallel)
-        shift
-        parallel=$1
         ;;
     -h | --help)
         print_help
@@ -71,40 +40,29 @@ while [ $# -gt 0 ]; do
     shift
 done
 
-file_chunks=()
-for i in $(seq 1 $parallel); do
-    file_chunks+=("")
-done
+temp_file=$(mktemp)
 
-i=0
-for file in $(git grep --cached -Il '' -- ':!src/tests/regress/data'); do
-    file_chunks[$((i % parallel))]="${file_chunks[$((i % parallel))]} $file"
-    ((i++))
-done
+echo -ne "${BLUE}"
+git grep -nIP --untracked '[[:space:]]+$' -- ':!src/tests/regress/data' | tee $temp_file || true
+echo -ne "${NONE}"
 
-pids=()
-for chunk in "${file_chunks[@]}"; do
-    do_check "$chunk" &
-    pids+=($!)
-done
+bad_files=$(cat $temp_file | cut -f1 -d ':' | sort -u)
+rm $temp_file
 
-n_found=0
-for pid in "${pids[@]}"; do
-    if ! wait $pid; then
-        ((n_found++))
-    fi
-done
+if [ ! -z "$bad_files" ]; then
+    if [[ $fix == true ]]; then
+        for file in $bad_files; do
+            sed -i '' -e's/[[:space:]]*$//' "$file"
+        done
 
-if [ $n_found -ne 0 ]; then
-    if [[ $fix == false ]]; then
+        echo
+        echo -e "${GREEN}${BOLD}All trailing spaces listed above have been cleaned.${NONE}"
+        exit 0
+    else
         echo
         echo -e "${RED}${BOLD}Please clean all the trailing spaces listed above.${NONE}"
         echo -e "${BOLD}You can run '$self --fix' for convenience.${NONE}"
         exit 1
-    else
-        echo
-        echo -e "${GREEN}${BOLD}All trailing spaces listed above have been cleaned.${NONE}"
-        exit 0
     fi
 else
     echo -e "${GREEN}${BOLD}No trailing spaces found.${NONE}"
