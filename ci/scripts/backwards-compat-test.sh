0cf2f8ea55ffff719ada92b819715db502e632b2
Noel Kwan <noelkwan1998@gmail.com>
2023-09-05 11:05:18 +0800

    update tag + import common utils

--- ci/scripts/backwards-compat-test.sh
+++ ci/scripts/backwards-compat-test.sh
@@ -28,6 +28,8 @@ if [[ "$profile" != "ci-dev" ]] && [[ "$profile" != "ci-release" ]]; then
     exit 1
 fi
 
+source backwards-compat-tests/scripts/utils.sh
+
 ################################### ENVIRONMENT VARIABLES
 
 LOG_DIR=.risingwave/log
@@ -38,156 +40,9 @@ QUERY_LOG_FILE="$LOG_DIR/query.log"
 # TODO(kwannoel): automatically derive this by:
 # 1. Fetching major version.
 # 2. Find the earliest minor version of that major version.
-TAG=v0.18.0
+TAG=1.0.0
 # Duration to wait for recovery (seconds)
 RECOVERY_DURATION=20
-
-################################### TEST UTILIIES
-
-assert_not_empty() {
-  set +e
-  if [[ $(wc -l < "$1" | sed 's/^ *//g') -gt 1 ]]; then
-    echo "assert_not_empty PASSED for $1"
-  else
-    echo "assert_not_empty FAILED for $1"
-    buildkite-agent artifact upload "$1"
-    exit 1
-  fi
-  set -e
-}
-
-assert_eq() {
-  set +e
-  if [[ -z $(diff "$1" "$2") ]]; then
-    echo "assert_eq PASSED for $1 and $2"
-  else
-    echo "FAILED"
-    buildkite-agent artifact upload "$1"
-    buildkite-agent artifact upload "$2"
-    exit 1
-  fi
-  set -e
-}
-
-################################### QUERIES
-
-run_sql () {
-    psql -h localhost -p 4566 -d dev -U root -c "$@"
-}
-
-seed_table() {
-  START="$1"
-  END="$2"
-  for i in $(seq "$START" "$END")
-  do
-    run_sql "INSERT into t values ($i, $i);" 1>$QUERY_LOG_FILE 2>&1
-  done
-  run_sql "flush;"
-}
-
-random_delete() {
-  START=$1
-  END=$2
-  COUNT=$3
-  for i in $(seq 1 "$COUNT")
-  do
-    run_sql "DELETE FROM t WHERE v1 = $(("$RANDOM" % END));" 1>$QUERY_LOG_FILE 2>&1
-  done
-  run_sql "flush;"
-}
-
-random_update() {
-  START=$1
-  END=$2
-  COUNT=$3
-  for _i in $(seq 1 "$COUNT")
-  do
-    run_sql "UPDATE t SET v2 = v2 + 1 WHERE v1 = $(("$RANDOM" % END));" 1>$QUERY_LOG_FILE 2>&1
-  done
-  run_sql "flush;"
-}
-
-# Setup table and materialized view.
-# Run updates and deletes on the table.
-# Get the results.
-# TODO: Run nexmark, tpch queries
-run_sql_old_cluster() {
-  run_sql "CREATE TABLE t(v1 int primary key, v2 int);"
-
-  seed_table 1 10000
-
-  run_sql "CREATE MATERIALIZED VIEW m as SELECT * from t;" &
-  CREATE_MV_PID=$!
-
-  seed_table 10001 20000
-
-  random_update 1 20000 1000
-
-  random_delete 1 20000 1000
-
-  wait $CREATE_MV_PID
-
-  run_sql "CREATE MATERIALIZED VIEW m2 as SELECT v1, sum(v2) FROM m GROUP BY v1;"
-
-  run_sql "select * from m ORDER BY v1;" > BEFORE_1
-  run_sql "select * from m2 ORDER BY v1;" > BEFORE_2
-}
-
-# Just check if the results are the same as old cluster.
-run_sql_new_cluster() {
-  run_sql "SELECT * from m ORDER BY v1;" > AFTER_1
-  run_sql "select * from m2 ORDER BY v1;" > AFTER_2
-}
-
-run_updates_and_deletes_new_cluster() {
-  random_update 1 20000 1000
-  random_delete 1 20000 1000
-}
-
-################################### CLUSTER CONFIGURATION
-
-configure_rw() {
-echo "--- Setting up cluster config"
-cat <<EOF > risedev-profiles.user.yml
-full-without-monitoring:
-  steps:
-    - use: minio
-    - use: etcd
-    - use: meta-node
-    - use: compute-node
-    - use: frontend
-    - use: compactor
-EOF
-
-cat <<EOF > risedev-components.user.env
-RISEDEV_CONFIGURED=true
-
-ENABLE_MINIO=true
-ENABLE_ETCD=true
-ENABLE_KAFKA=true
-
-# Fetch risingwave binary from release.
-ENABLE_BUILD_RUST=false
-
-# Ensure it will link the all-in-one binary from our release.
-ENABLE_ALL_IN_ONE=true
-
-# ENABLE_RELEASE_PROFILE=true
-EOF
-}
-
-configure_latest_rw() {
-cat <<EOF > risedev-profiles.user.yml
-full-without-monitoring:
-  steps:
-    - use: minio
-    - use: etcd
-    - use: meta-node
-    - use: compute-node
-    - use: frontend
-    - use: compactor
-EOF
-}
 
 echo "--- Configuring RW"
 configure_rw
