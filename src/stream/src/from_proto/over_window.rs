5087685908fdbe0a2134598ef0b8401e4c0799d5
Noel Kwan <noelkwan1998@gmail.com>
2023-09-06 12:20:30 +0800

    fix

--- src/stream/src/from_proto/over_window.rs
+++ src/stream/src/from_proto/over_window.rs
@@ -75,7 +75,7 @@ impl ExecutorBuilder for OverWindowExecutorBuilder {
             watermark_epoch: stream.get_watermark_epoch(),
             metrics: params.executor_stats,
             chunk_size: params.env.config().developer.chunk_size,
-            cache_policy: OverWindowCachePolicy::from_protobuf(node.get_cache_policy()?),
+            cache_policy: OverWindowCachePolicy::from_protobuf(node.get_cache_policy().unwrap_or_default()),
         })
         .boxed())
     }
