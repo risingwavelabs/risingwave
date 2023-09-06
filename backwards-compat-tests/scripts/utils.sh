182656d12ef5429d882ed6bbc4e3f2fb0e02846d
Noel Kwan <noelkwan1998@gmail.com>
2023-09-06 21:25:31 +0800

    use git branch -r to list remote branches

--- backwards-compat-tests/scripts/utils.sh
+++ backwards-compat-tests/scripts/utils.sh
@@ -131,8 +131,8 @@ get_rw_versions() {
   # First we obtain a list of versions from git branch names.
   # Then we normalize them to semver format (MAJOR.MINOR.PATCH).
   echo "--- git branch output"
-  git branch --all </dev/null
-  local branches=$(git branch | grep -E "^  v[0-9]*\.[0-9]*.*-rc" | tr -d ' ' | sed -E 's/v([0-9]*\.[0-9])\-rc/\1.0/' | tr -d '\-vrc' | tr -d ' ')
+  git branch -r
+  local branches=$(git branch -r | grep -E "^  origin\/v[0-9]*\.[0-9]*.*-rc" | tr -d ' ' | sed -E 's/origin\/v([0-9]*\.[0-9])\-rc/\1.0/' | tr -d '\-vrcorigin\/' | tr -d ' ')
   echo "--- BRANCHES"
   echo "$branches"
 
