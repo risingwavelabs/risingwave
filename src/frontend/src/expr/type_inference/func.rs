9c98d269e04313fe98e5a7a3843243ede5dd31a0
Xu <xzhseh@gmail.com>
2023-09-06 14:31:10 -0400

    feat(expr): add `array_min` (#12071)
    Signed-off-by: Runji Wang <wangrunji0408@163.com>
    Co-authored-by: Runji Wang <wangrunji0408@163.com>

--- src/frontend/src/expr/type_inference/func.rs
+++ src/frontend/src/expr/type_inference/func.rs
@@ -595,6 +595,12 @@ fn infer_type_for_special(
 
             Ok(Some(inputs[0].return_type()))
         }
+        ExprType::ArrayMin => {
+            ensure_arity!("array_min", | inputs | == 1);
+            inputs[0].ensure_array_type()?;
+
+            Ok(Some(inputs[0].return_type().as_list().clone()))
+        }
         ExprType::ArrayDims => {
             ensure_arity!("array_dims", | inputs | == 1);
             inputs[0].ensure_array_type()?;
