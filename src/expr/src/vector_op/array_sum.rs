d265da8c3340849189f3b797ce495a0376e0989d
Runji Wang <wangrunji0408@163.com>
2023-09-08 17:18:51 +0800

    refactor `array_sum`
    Signed-off-by: Runji Wang <wangrunji0408@163.com>

--- src/expr/src/vector_op/array_sum.rs
+++ src/expr/src/vector_op/array_sum.rs
@@ -12,68 +12,26 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-use risingwave_common::array::{ListRef};
-use risingwave_common::types::{Scalar, ScalarImpl, ScalarRefImpl};
+use risingwave_common::array::{ArrayError, ListRef};
+use risingwave_common::types::{CheckedAdd, ScalarRefImpl};
 use risingwave_expr_macro::function;
 
 use crate::{ExprError, Result};
 
-// #[function("array_sum(list) -> *float")]
-// #[function("array_sum(list) -> decimal")]
-// #[function("array_sum(list) -> serial")]
-// #[function("array_sum(list) -> int256")]
-// #[function("array_sum(list) -> date")]
-// #[function("array_sum(list) -> time")]
-// #[function("array_sum(list) -> timestamp")]
-// #[function("array_sum(list) -> timestamptz")]
-// #[function("array_sum(list) -> varchar")]
-// #[function("array_sum(list) -> bytea")]
 #[function("array_sum(list) -> *int")]
-pub fn array_sum<T: Scalar>(list: ListRef<'_>) -> Result<Option<T>> {
-    let flag = match list.iter().flatten().next() {
-        Some(v) => match v {
-            ScalarRefImpl::Int16(_) => 0,
-            ScalarRefImpl::Int32(_) => 1,
-            ScalarRefImpl::Int64(_) => 2,
-            _ => -1,
-        },
-        None => -1,
-    };
-
-    match flag {
-        0 => {
-            let mut ret = 0;
-            for e in list.iter().flatten() {
-                if let ScalarRefImpl::Int16(v) = e {
-                    ret += v;
-                } else {
-                    panic!("Expected ScalarRefImpl::Int16");
-                }
-            }
-            Ok(Some(ScalarImpl::from(ret).try_into()?))
-        }
-        1 => {
-            let mut ret = 0;
-            for e in list.iter().flatten() {
-                if let ScalarRefImpl::Int32(v) = e {
-                    ret += v;
-                } else {
-                    panic!("Expected ScalarRefImpl::Int32");
-                }
-            }
-            Ok(Some(ScalarImpl::from(ret).try_into()?))
-        }
-        2 => {
-            let mut ret = 0;
-            for e in list.iter().flatten() {
-                if let ScalarRefImpl::Int64(v) = e {
-                    ret += v;
-                } else {
-                    panic!("Expected ScalarRefImpl::Int64");
-                }
-            }
-            Ok(Some(ScalarImpl::from(ret).try_into()?))
-        }
-        _ => Err(ExprError::NumericOutOfRange),
+#[function("array_sum(list) -> *float")]
+#[function("array_sum(list) -> decimal")]
+#[function("array_sum(list) -> interval")]
+fn array_sum<T>(list: ListRef<'_>) -> Result<T>
+where
+    T: Default + for<'a> TryFrom<ScalarRefImpl<'a>, Error = ArrayError> + CheckedAdd<Output = T>,
+{
+    let mut sum = T::default();
+    for e in list.iter().flatten() {
+        let v = e.try_into()?;
+        sum = sum
+            .checked_add(v)
+            .ok_or_else(|| ExprError::NumericOutOfRange)?;
     }
+    Ok(sum)
 }
