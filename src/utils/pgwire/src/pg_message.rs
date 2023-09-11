ec89027c862a7b64d72a52ab0deaa4ec00ec08f9
Tao Wu <wutao@singularity-data.com>
2023-09-11 16:57:12 +0800

    fix: reserve variable buffer for parameter status messages

--- src/utils/pgwire/src/pg_message.rs
+++ src/utils/pgwire/src/pg_message.rs
@@ -451,8 +451,8 @@ impl<'a> BeMessage<'a> {
 
                 // Parameter names and values are passed as null-terminated strings
                 let iov = &mut [name, b"\0", value, b"\0"].map(IoSlice::new);
-                let mut buffer = [0u8; 64]; // this should be enough
-                let cnt = buffer.as_mut().write_vectored(iov).unwrap();
+                let mut buffer = vec![]; // this should be enough
+                let cnt = buffer.write_vectored(iov).unwrap();
 
                 buf.put_u8(b'S');
                 write_body(buf, |stream| {
