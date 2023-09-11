4fc8808891ea13fc32557c86ff6c47f24a6dd140
xxchan <xxchan22f@gmail.com>
2023-09-11 13:13:42 +0800

    add trivial diff to rust code

--- src/common/src/lib.rs
+++ src/common/src/lib.rs
@@ -12,6 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+
 #![expect(
     refining_impl_trait,
     reason = "Some of the Row::iter() implementations returns ExactSizeIterator. Is this reasonable?"
