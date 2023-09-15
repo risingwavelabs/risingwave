5c74e7d6abfa05f57241d2e96a353e83b8ed1b7c
Tao Wu <wutao@singularity-data.com>
2023-09-15 11:27:45 +0800

    Update src/utils/pgwire/src/pg_message.rs
    Co-authored-by: TennyZhuang <zty0826@gmail.com>

--- src/utils/pgwire/src/pg_message.rs
+++ src/utils/pgwire/src/pg_message.rs
@@ -451,7 +451,7 @@ impl<'a> BeMessage<'a> {
 
                 // Parameter names and values are passed as null-terminated strings
                 let iov = &mut [name, b"\0", value, b"\0"].map(IoSlice::new);
-                let mut buffer = vec![]; // this should be enough
+                let mut buffer = vec![];
                 let cnt = buffer.write_vectored(iov).unwrap();
 
                 buf.put_u8(b'S');
