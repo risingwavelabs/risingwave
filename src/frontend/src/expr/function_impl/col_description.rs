e36dff343d894dfd964e3650b4da80263c6c4115
TennyZhuang <zty0826@gmail.com>
2023-09-18 15:25:15 +0800

    re-export #[function] macros in expr
    Signed-off-by: TennyZhuang <zty0826@gmail.com>

--- src/frontend/src/expr/function_impl/col_description.rs
+++ src/frontend/src/expr/function_impl/col_description.rs
@@ -14,8 +14,7 @@
 
 use std::fmt::Write;
 
-use risingwave_expr::ExprError;
-use risingwave_expr_macro::function;
+use risingwave_expr::{function, ExprError};
 
 #[function("col_description(varchar, int32) -> varchar")]
 fn col_description(_name: &str, _col: i32, writer: &mut impl Write) -> Result<(), ExprError> {
