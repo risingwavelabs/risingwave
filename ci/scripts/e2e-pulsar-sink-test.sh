5ee3a5700242b6056d4a7c5c9e2b37dab20c5222
Rossil <40714231+Rossil2012@users.noreply.github.com>
2023-09-21 20:38:00 +0800

    test(sink): add e2e test for pulsar-sink

---
+++ ci/scripts/e2e-pulsar-sink-test.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+# Exits as soon as any line fails.
+set -euo pipefail
+
+echo "--- check pulsar"
+curl http://pulsar:6651/admin/v2/clusters
+
+echo "--- testing pulsar sink"
+sqllogictest -p 4566 -d dev './e2e_test/sink/pulsar_sink.slt'
+
+sleep 5
