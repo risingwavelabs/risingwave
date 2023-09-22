faa46db50dafc61623ee1a0b285f37ef548b8a95
Rossil <40714231+Rossil2012@users.noreply.github.com>
2023-09-22 13:36:45 +0800

    fix pulsar url

--- ci/scripts/e2e-pulsar-sink-test.sh
+++ ci/scripts/e2e-pulsar-sink-test.sh
@@ -4,7 +4,7 @@
 set -euo pipefail
 
 echo "--- check pulsar"
-curl http://127.0.0.1:6651/admin/v2/clusters
+curl http://pulsar:6651/admin/v2/clusters
 
 echo "--- testing pulsar sink"
 sqllogictest -p 4566 -d dev './e2e_test/sink/pulsar_sink.slt'
