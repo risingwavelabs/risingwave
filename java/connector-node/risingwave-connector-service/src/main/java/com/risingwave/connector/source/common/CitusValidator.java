746a793bbf0cddfcf4db7501b815a174d2fc9ae8
StrikeW <wangsiyuanse@gmail.com>
2023-09-22 14:58:36 +0800

    fix citus publication alter

---
+++ java/connector-node/risingwave-connector-service/src/main/java/com/risingwave/connector/source/common/CitusValidator.java
@@ -0,0 +1,18 @@
+package com.risingwave.connector.source.common;
+
+import com.risingwave.connector.api.TableSchema;
+
+import java.sql.SQLException;
+import java.util.Map;
+
+public class CitusValidator extends PostgresValidator {
+    public CitusValidator(Map<String, String> userProps, TableSchema tableSchema) throws SQLException {
+        super(userProps, tableSchema);
+    }
+
+    @Override
+    protected void alterPublicationIfNeeded() throws SQLException {
+        // do nothing for citus worker node,
+        // since we created a FOR ALL TABLES publication when creating the connector
+    }
+}
