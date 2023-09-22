7bb5b0e4d15e3c63680ed49850ec51db66736fec
Rossil <40714231+Rossil2012@users.noreply.github.com>
2023-09-22 14:06:34 +0800

    expose pulsar and rest api port

--- ci/scripts/e2e-pulsar-sink-test.sh
+++ ci/scripts/e2e-pulsar-sink-test.sh
@@ -3,8 +3,8 @@
 # Exits as soon as any line fails.
 set -euo pipefail
 
-echo "--- check pulsar"
-curl http://pulsar:8080/admin/v2/clusters
+# echo "--- check pulsar"
+# curl http://pulsar:8080/admin/v2/clusters
 
 echo "--- testing pulsar sink"
 sqllogictest -p 4566 -d dev './e2e_test/sink/pulsar_sink.slt'
