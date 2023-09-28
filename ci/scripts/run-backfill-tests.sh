1f81eeb1ab37eb1aedbebd62d63b322601b64cfd
Noel Kwan <noelkwan1998@gmail.com>
2023-09-28 21:10:25 +0800

    add back all the test

--- ci/scripts/run-backfill-tests.sh
+++ ci/scripts/run-backfill-tests.sh
@@ -257,10 +257,10 @@ test_foreground_sink_cancel() {
 main() {
   set -euo pipefail
   # test_snapshot_and_upstream_read
-  # test_background_ddl_recovery
-  # test_background_ddl_cancel
-  # test_foreground_ddl_no_recover
-  # test_foreground_ddl_cancel
+  test_background_ddl_recovery
+  test_background_ddl_cancel
+  test_foreground_ddl_no_recover
+  test_foreground_ddl_cancel
   test_foreground_index_cancel
   test_foreground_sink_cancel
 }
