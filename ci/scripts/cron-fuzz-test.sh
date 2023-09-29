3ce5f1e712d8cb8ddb0b0b8a152aea9f02e4d1b4
Noel Kwan <noelkwan1998@gmail.com>
2023-09-29 10:23:06 +0800

    disable frontend + merge sqlsmith-test workflow back into main-cron

--- ci/scripts/cron-fuzz-test.sh
+++ ci/scripts/cron-fuzz-test.sh
@@ -4,12 +4,14 @@
 set -euo pipefail
 
 source ci/scripts/common.sh
-export RUN_SQLSMITH=0
+
 # NOTE(kwannoel): Disabled because there's some breakage after #12485,
 # see https://github.com/risingwavelabs/risingwave/issues/12577.
 # Frontend is relatively stable, e2e fuzz test will cover the same cases also,
 # so we can just disable it.
 # export RUN_SQLSMITH_FRONTEND=1
+
+export RUN_SQLSMITH=1
 export SQLSMITH_COUNT=1000
 export TEST_NUM=100
 source ci/scripts/run-fuzz-test.sh
