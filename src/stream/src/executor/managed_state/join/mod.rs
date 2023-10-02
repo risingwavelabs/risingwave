48384ef43d38a1fce9122d7a048d0c90c3410d9d
Eric Fu <eric@singularity-data.com>
2023-10-02 08:08:51 +0000

    bugfix

--- src/stream/src/executor/managed_state/join/mod.rs
+++ src/stream/src/executor/managed_state/join/mod.rs
@@ -558,7 +558,9 @@ impl<K: HashKey, S: StateStore> JoinHashMap<K, S> {
     /// This is only supposed to called after [`Self::take_state()`], especially when the entry is too large to be resident in memory
     pub fn remove_empty_state(&mut self, key: &K) {
         let state = self.inner.pop(key);
-        assert!(state.is_none(), "state should be empty");
+        if let Some(entry) = state {
+            assert!(entry.0.is_none(), "state should be taken")
+        }
     }
 
     /// Manipulate the degree of the given [`JoinRow`] and [`EncodedJoinRow`] with `action`, both in
