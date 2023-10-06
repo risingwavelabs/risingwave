2108362f6bf9d72e60ea929b7bf2ca99e9fc50ec
StrikeW <wangsiyuanse@gmail.com>
2023-10-06 17:37:39 +0800

    Revert "Revert "refactor(connector): replace validate source rpc with jni (#12270)""
    This reverts commit fb7b1d10d982add28a2adcdea173ff0ada5f3b3a.

--- java/connector-node/risingwave-connector-service/src/main/java/com/risingwave/connector/source/SourceValidateHandler.java
+++ java/connector-node/risingwave-connector-service/src/main/java/com/risingwave/connector/source/SourceValidateHandler.java
@@ -53,7 +53,7 @@ public class SourceValidateHandler {
         }
     }
 
-    private ConnectorServiceProto.ValidateSourceResponse validateResponse(String message) {
+    public static ConnectorServiceProto.ValidateSourceResponse validateResponse(String message) {
         return ConnectorServiceProto.ValidateSourceResponse.newBuilder()
                 .setError(
                         ConnectorServiceProto.ValidationError.newBuilder()
@@ -62,14 +62,14 @@ public class SourceValidateHandler {
                 .build();
     }
 
-    private void ensurePropNotNull(Map<String, String> props, String name) {
+    public static void ensurePropNotNull(Map<String, String> props, String name) {
         if (!props.containsKey(name)) {
             throw ValidatorUtils.invalidArgument(
                     String.format("'%s' not found, please check the WITH properties", name));
         }
     }
 
-    private void validateSource(ConnectorServiceProto.ValidateSourceRequest request)
+    public static void validateSource(ConnectorServiceProto.ValidateSourceRequest request)
             throws Exception {
         var props = request.getPropertiesMap();
 
