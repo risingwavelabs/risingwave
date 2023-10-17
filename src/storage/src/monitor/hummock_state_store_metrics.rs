527a276aaade1e16b565d57bc7cd50bc40fc07ec
congyi wang <58715567+wcy-fdu@users.noreply.github.com>
2023-10-17 13:57:04 +0800

    feat(metrics): bring back some mem table memory metrics (#12833)

--- src/storage/src/monitor/hummock_state_store_metrics.rs
+++ src/storage/src/monitor/hummock_state_store_metrics.rs
@@ -17,8 +17,8 @@ use std::sync::{Arc, OnceLock};
 use prometheus::core::{AtomicU64, Collector, Desc, GenericCounter, GenericGauge};
 use prometheus::{
     exponential_buckets, histogram_opts, proto, register_histogram_vec_with_registry,
-    register_int_counter_vec_with_registry, register_int_gauge_with_registry, Gauge, IntGauge,
-    Opts, Registry,
+    register_int_counter_vec_with_registry, register_int_gauge_vec_with_registry,
+    register_int_gauge_with_registry, Gauge, IntGauge, IntGaugeVec, Opts, Registry,
 };
 use risingwave_common::config::MetricLevel;
 use risingwave_common::metrics::{RelabeledCounterVec, RelabeledHistogramVec};
@@ -67,6 +67,10 @@ pub struct HummockStateStoreMetrics {
 
     // uploading task
     pub uploader_uploading_task_size: GenericGauge<AtomicU64>,
+
+    // memory
+    pub mem_table_memory_size: IntGaugeVec,
+    pub mem_table_item_count: IntGaugeVec,
 }
 
 pub static GLOBAL_HUMMOCK_STATE_STORE_METRICS: OnceLock<HummockStateStoreMetrics> = OnceLock::new();
@@ -339,11 +343,28 @@ impl HummockStateStoreMetrics {
             registry
         )
         .unwrap();
+
         let read_req_check_bloom_filter_counts = RelabeledCounterVec::with_metric_level(
             MetricLevel::Info,
             read_req_check_bloom_filter_counts,
             metric_level,
         );
+
+        let mem_table_memory_size = register_int_gauge_vec_with_registry!(
+            "state_store_mem_table_memory_size",
+            "Memory usage of mem_table",
+            &["table_id", "instance_id"],
+            registry
+        )
+        .unwrap();
+
+        let mem_table_item_count = register_int_gauge_vec_with_registry!(
+            "state_store_mem_table_item_count",
+            "Item counts in mem_table",
+            &["table_id", "instance_id"],
+            registry
+        )
+        .unwrap();
 
         Self {
             bloom_filter_true_negative_counts,
@@ -369,6 +390,8 @@ impl HummockStateStoreMetrics {
             spill_task_size_from_sealed: spill_task_size.with_label_values(&["sealed"]),
             spill_task_size_from_unsealed: spill_task_size.with_label_values(&["unsealed"]),
             uploader_uploading_task_size,
+            mem_table_memory_size,
+            mem_table_item_count,
         }
     }
 
