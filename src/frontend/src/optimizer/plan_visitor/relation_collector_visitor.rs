abafae03a9474647304207f998a105f9ad982cdf
stonepage <40830455+st1page@users.noreply.github.com>
2023-10-23 14:23:34 +0800

    chore(optimizer): rename logical to core (#12975)

--- src/frontend/src/optimizer/plan_visitor/relation_collector_visitor.rs
+++ src/frontend/src/optimizer/plan_visitor/relation_collector_visitor.rs
@@ -52,8 +52,8 @@ impl PlanVisitor for RelationCollectorVisitor {
     }
 
     fn visit_batch_seq_scan(&mut self, plan: &crate::optimizer::plan_node::BatchSeqScan) {
-        if !plan.logical().is_sys_table {
-            self.relations.insert(plan.logical().table_desc.table_id);
+        if !plan.core().is_sys_table {
+            self.relations.insert(plan.core().table_desc.table_id);
         }
     }
 
@@ -64,7 +64,7 @@ impl PlanVisitor for RelationCollectorVisitor {
     }
 
     fn visit_stream_table_scan(&mut self, plan: &StreamTableScan) {
-        let logical = plan.logical();
+        let logical = plan.core();
         if !logical.is_sys_table {
             self.relations.insert(logical.table_desc.table_id);
         }
