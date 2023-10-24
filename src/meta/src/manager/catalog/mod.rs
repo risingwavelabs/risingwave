95e60426b3519a0add8bbb186eb4d32a92eaf492
Noel Kwan <noelkwan1998@gmail.com>
2023-10-24 13:32:36 +0800

    add more trace

--- src/meta/src/manager/catalog/mod.rs
+++ src/meta/src/manager/catalog/mod.rs
@@ -734,6 +734,7 @@ impl CatalogManager {
         for &dependent_relation_id in &table.dependent_relations {
             database_core.increase_ref_count(dependent_relation_id);
         }
+        tracing::debug!("increase ref for table: {}", table.id);
         user_core.increase_ref(table.owner);
         Ok(())
     }
@@ -1400,6 +1401,7 @@ impl CatalogManager {
 
         // `tables_removed` contains both index table and mv.
         for table in &tables_removed {
+            tracing::debug!("decrease ref for {}", table.id);
             user_core.decrease_ref(table.owner);
         }
 
