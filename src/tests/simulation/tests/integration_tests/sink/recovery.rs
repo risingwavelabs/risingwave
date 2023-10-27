92470cbe7417b6ef8acfa16a1b7bb89bbea05ff9
William Wen <44139337+wenym1@users.noreply.github.com>
2023-10-27 19:55:16 +0800

    test(sink): add recovery test for sink (#12701)

--- src/tests/simulation/tests/integration_tests/sink/recovery.rs
+++ src/tests/simulation/tests/integration_tests/sink/recovery.rs
@@ -17,6 +17,7 @@ use std::sync::Arc;
 use std::time::Duration;
 
 use anyhow::Result;
+use risingwave_simulation::cluster::{Cluster, KillOpts};
 use tokio::time::sleep;
 
 use crate::sink::utils::{
@@ -25,7 +26,34 @@ use crate::sink::utils::{
 };
 use crate::{assert_eq_with_err_returned as assert_eq, assert_with_err_returned as assert};
 
-async fn basic_test_inner(is_decouple: bool) -> Result<()> {
+async fn kill_and_check(
+    cluster: &mut Cluster,
+    test_sink: &SimulationTestSink,
+    target: usize,
+) -> anyhow::Result<()> {
+    let mut prev_count = 0;
+    sleep(Duration::from_secs(2)).await;
+    for i in 0..5 {
+        let curr_count = test_sink.store.id_count();
+        if curr_count == target {
+            assert!(i > 0, "test finish without kill");
+            break;
+        }
+        assert!(
+            curr_count > prev_count,
+            "not make progress between kill. Prev count {}, curr count {}, i {}",
+            prev_count,
+            curr_count,
+            i
+        );
+        prev_count = curr_count;
+        cluster.kill_node(&KillOpts::ALL).await;
+        sleep(Duration::from_secs(10)).await;
+    }
+    Ok(())
+}
+
+async fn recovery_test_inner(is_decouple: bool) -> Result<()> {
     let mut cluster = start_sink_test_cluster().await?;
 
     let test_sink = SimulationTestSink::register_new();
@@ -43,15 +71,19 @@ async fn basic_test_inner(is_decouple: bool) -> Result<()> {
     session.run(CREATE_SINK).await?;
     assert_eq!(6, test_sink.parallelism_counter.load(Relaxed));
 
-    test_sink
-        .store
-        .wait_for_count(test_source.id_list.len())
-        .await?;
+    let count = test_source.id_list.len();
 
+    kill_and_check(&mut cluster, &test_sink, count).await?;
+
+    test_sink.store.wait_for_count(count).await?;
+
+    let mut session = cluster.start_session();
     session.run(DROP_SINK).await?;
     session.run(DROP_SOURCE).await?;
 
     assert_eq!(0, test_sink.parallelism_counter.load(Relaxed));
+    assert!(test_sink.store.inner().checkpoint_count > 0);
+
     test_sink.store.check_simple_result(&test_source.id_list)?;
     assert!(test_sink.store.inner().checkpoint_count > 0);
 
@@ -59,32 +91,11 @@ async fn basic_test_inner(is_decouple: bool) -> Result<()> {
 }
 
 #[tokio::test]
-async fn test_sink_basic() -> Result<()> {
-    basic_test_inner(false).await
+async fn test_sink_recovery() -> Result<()> {
+    recovery_test_inner(false).await
 }
 
 #[tokio::test]
-async fn test_sink_decouple_basic() -> Result<()> {
-    basic_test_inner(true).await
-}
-
-#[tokio::test]
-async fn test_sink_decouple_blackhole() -> Result<()> {
-    let mut cluster = start_sink_test_cluster().await?;
-
-    let test_source = SimulationTestSource::register_new(12, 0..500000, 0.2, 50);
-
-    let mut session = cluster.start_session();
-
-    session.run("set streaming_parallelism = 6").await?;
-    session.run("set sink_decouple = true").await?;
-    session.run(CREATE_SOURCE).await?;
-    session
-        .run("create sink test_sink from test_source with (connector = 'blackhole')")
-        .await?;
-
-    session.run(DROP_SINK).await?;
-    session.run(DROP_SOURCE).await?;
-
-    Ok(())
+async fn test_sink_decouple_recovery() -> Result<()> {
+    recovery_test_inner(true).await
 }
