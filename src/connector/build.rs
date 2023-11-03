606e9ad26d3523474a00831394e84cdaed97a3ac
Tao Wu <wutao@singularity-data.com>
2023-11-03 11:25:33 +0800

    add comment

--- src/connector/build.rs
+++ src/connector/build.rs
@@ -13,6 +13,8 @@
 // limitations under the License.
 
 fn main() {
+    // This function extracts WITH options from the Rust code and maintains
+    // them in a single source of truth `with_options.yaml`.
     with_options_build::update_with_options_yaml();
 
     let proto_dir = "./src/test_data/proto_recursive";
