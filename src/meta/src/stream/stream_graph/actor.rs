055973bc98edac412e9fc2b38fdf925744ac60f3
Eric Fu <eric@singularity-data.com>
2023-11-08 14:18:38 +0800

    fix comment

--- src/meta/src/stream/stream_graph/actor.rs
+++ src/meta/src/stream/stream_graph/actor.rs
@@ -121,7 +121,7 @@ impl ActorBuilder {
     /// During this process, the following things will be done:
     /// 1. Replace the logical `Exchange` in node's input with `Merge`, which can be executed on the
     /// compute nodes.
-    /// 2. Fill the upstream mview info of the `Merge` node under the `Chain` node.
+    /// 2. Fill the upstream mview info of the `Merge` node under the `StreamScan` node.
     fn rewrite(&self) -> MetaResult<StreamNode> {
         self.rewrite_inner(&self.nodes, 0)
     }
