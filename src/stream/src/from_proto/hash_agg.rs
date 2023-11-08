4726cdd3dd9eabb8f923ce537b42a3d4ec6c16b8
Richard Chien <stdrc@outlook.com>
2023-11-08 10:08:52 +0800

    fix(stream agg): use executor schema derived by optimizer instead of generating again in agg executors (#13289)
    Signed-off-by: Richard Chien <stdrc@outlook.com>

--- src/stream/src/from_proto/hash_agg.rs
+++ src/stream/src/from_proto/hash_agg.rs
@@ -100,8 +100,11 @@ impl ExecutorBuilder for HashAggExecutorBuilder {
 
                 input,
                 actor_ctx: params.actor_context,
-                pk_indices: params.pk_indices,
-                executor_id: params.executor_id,
+                info: ExecutorInfo {
+                    schema: params.schema,
+                    pk_indices: params.pk_indices,
+                    identity: params.identity,
+                },
 
                 extreme_cache_size: stream.config.developer.unsafe_extreme_cache_size,
 
