8479f432f3f75b44cb6ece0536867ecbae8d7926
Shanicky Chen <peng@risingwave-labs.com>
2023-11-13 23:56:06 +0800

    rollback
    Signed-off-by: Shanicky Chen <peng@risingwave-labs.com>

--- src/common/src/hash/consistent_hash/vnode.rs
+++ src/common/src/hash/consistent_hash/vnode.rs
@@ -124,7 +124,7 @@ impl VirtualNode {
                     extract_vnode_id_from_row_id(serial.as_row_id())
                 } else {
                     let (row, _) = data_chunk.row_at(idx);
-                    Self::compute_row(row, keys)
+                    row.hash(Crc32FastBuilder).into()
                 }
             } ).collect();
         }
