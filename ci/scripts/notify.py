e48374868e1850e28df793c2d46a3b985083aab1
Noel Kwan <noelkwan1998@gmail.com>
2023-11-15 11:06:58 +0800

    interim commit

---
+++ ci/scripts/notify.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+
+test_owners_map = {
+    "test-notify": [],
+    "e2e-iceberg-sink-tests": [],
+    "e2e-java-binding-tests": [],
+    "e2e-clickhouse-sink-tests": [],
+    "e2e-pulsar-sink-tests": [],
+    "s3-source-test-for-opendal-fs-engine": [],
+    "pulsar-source-tests": [],
+    "connector-node-integration-test-11": [],
+    "connector-node-integration-test-17": []
+}
+
+OUTCOME=$(buildkite-agent step get "outcome" --step "test-notify")
+echo "outcome: $$OUTCOME"
+
+if [[ $$OUTCOME == "hard_failed" || $$OUTCOME == "soft_failed" ]]; then
+cat <<- YAML | buildkite-agent pipeline upload
+steps:
+- label: "notify test inner"
+command: echo "notify test"
+notify:
+- slack:
+channels:
+- "#notification-buildkite"
+message: "<@noelkwan> test"
+YAML
+fi
