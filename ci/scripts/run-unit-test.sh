369ebc2fb97855d2a4d40279d58e6507df11c24d
xxchan <xxchan22f@gmail.com>
2023-11-15 17:53:35 +0800

    revert ci

--- ci/scripts/run-unit-test.sh
+++ ci/scripts/run-unit-test.sh
@@ -11,12 +11,8 @@ python3 -m pytest
 cd ${REPO_ROOT}
 
 echo "+++ Run unit tests with coverage"
-cargo test --no-run --features failpoints,sync_point --workspace --exclude risingwave_simulation --timings
-buildkite-agent artifact upload target/cargo-timings/cargo-timing.html
+# use tee to disable progress bar
 NEXTEST_PROFILE=ci cargo llvm-cov nextest --lcov --output-path lcov.info --features failpoints,sync_point --workspace --exclude risingwave_simulation
-
-echo "--- Show sccache stats"
-sccache --show-stats
 
 echo "--- Codecov upload coverage reports"
 curl -Os https://uploader.codecov.io/latest/linux/codecov && chmod +x codecov
