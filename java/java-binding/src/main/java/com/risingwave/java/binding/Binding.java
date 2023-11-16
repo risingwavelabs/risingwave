462add06977632720303d295f5e249b1ef82afce
William Wen <william123.wen@gmail.com>
2023-11-16 11:08:12 +0800

    use full class name

--- java/java-binding/src/main/java/com/risingwave/java/binding/Binding.java
+++ java/java-binding/src/main/java/com/risingwave/java/binding/Binding.java
@@ -83,8 +83,8 @@ public class Binding {
 
     public static native boolean sendCdcSourceMsgToChannel(long channelPtr, byte[] msg);
 
-    public static native JniSinkWriterStreamRequest recvSinkWriterRequestFromChannel(
-            long channelPtr);
+    public static native com.risingwave.java.binding.JniSinkWriterStreamRequest
+            recvSinkWriterRequestFromChannel(long channelPtr);
 
     public static native boolean sendSinkWriterResponseToChannel(long channelPtr, byte[] msg);
 
