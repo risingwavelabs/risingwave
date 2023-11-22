27012ee23b19cdfb79539152c8146d071052be50
Bugen Zhao <i@bugenzhao.com>
2023-11-22 16:29:11 +0800

    update ci script for extended error ui test
    Signed-off-by: Bugen Zhao <i@bugenzhao.com>

--- ci/scripts/run-e2e-test.sh
+++ ci/scripts/run-e2e-test.sh
@@ -109,7 +109,8 @@ cluster_stop
 echo "--- e2e, $mode, error ui"
 RUST_LOG="info,risingwave_stream=info,risingwave_batch=info,risingwave_storage=info" \
 cluster_start
-sqllogictest -p 4566 -d dev './e2e_test/error_ui/**/*.slt'
+sqllogictest -p 4566 -d dev './e2e_test/error_ui/simple/**/*.slt'
+sqllogictest -p 4566 -d dev -e postgres-extended './e2e_test/error_ui/extended/**/*.slt'
 
 echo "--- Kill cluster"
 cluster_stop
