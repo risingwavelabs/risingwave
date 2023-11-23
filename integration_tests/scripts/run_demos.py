b61a48632a2a26929096af0c87a025c47bc6c50b
xfz <xfz2000x@gmail.com>
2023-11-23 13:20:50 +0800

    update

--- integration_tests/scripts/run_demos.py
+++ integration_tests/scripts/run_demos.py
@@ -240,7 +240,7 @@ def run_elasticsearch_sink_demo():
     print("Running demo: {}".format(demo))
 
     subprocess.run(["docker", "compose", "up", "-d", "--build"], cwd=demo_dir, check=True)
-    sleep(60)
+    sleep(120)
 
     sql_files = ['create_source.sql', 'create_mv.sql', 'create_sink.sql']
     for fname in sql_files:
@@ -253,7 +253,7 @@ def run_elasticsearch_sink_demo():
     # wait for half min ingestion
     sleep(30)
 
-    versions = ['8']
+    versions = ['7', '8']
     sink_check_file = os.path.join(demo_dir, 'sink_check')
     with open(sink_check_file) as f:
         relations = f.read().strip().split(",")
