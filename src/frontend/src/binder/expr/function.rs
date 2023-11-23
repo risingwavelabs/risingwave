edd0805dc45f1dc7ff92597c0e84ee0885c2cebf
jinser <aimer@purejs.icu>
2023-11-23 17:40:36 +0800

    feat: support `convert_from`/`convert_to` function that only support UTF8 encoding (#13597)

--- src/frontend/src/binder/expr/function.rs
+++ src/frontend/src/binder/expr/function.rs
@@ -803,6 +803,8 @@ impl Binder {
                 ("string_to_array", raw_call(ExprType::StringToArray)),
                 ("encode", raw_call(ExprType::Encode)),
                 ("decode", raw_call(ExprType::Decode)),
+                ("convert_from", raw_call(ExprType::ConvertFrom)),
+                ("convert_to", raw_call(ExprType::ConvertTo)),
                 ("sha1", raw_call(ExprType::Sha1)),
                 ("sha224", raw_call(ExprType::Sha224)),
                 ("sha256", raw_call(ExprType::Sha256)),
