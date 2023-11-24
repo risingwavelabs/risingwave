13b334c20c2027f946ab11f19f557b5d125c5750
William Wen <william123.wen@gmail.com>
2023-11-24 10:49:00 +0800

    change error to warn

--- src/storage/src/hummock/event_handler/hummock_event_handler.rs
+++ src/storage/src/hummock/event_handler/hummock_event_handler.rs
@@ -628,7 +628,7 @@ impl HummockEventHandler {
                 )) {
                     Ok(_) => {}
                     Err(_) => {
-                        error!(
+                        warn!(
                             "RegisterReadVersion send fail table_id {:?} instance_is {:?}",
                             table_id, instance_id
                         )
