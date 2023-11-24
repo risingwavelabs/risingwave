28d4fce3a7974943da14deca102cf950668c6556
August <pin@singularity-data.com>
2023-11-24 15:24:00 +0800

    feat: support sink/view column schema in commands show/create and system catalog (#13626)

--- src/frontend/src/handler/show.rs
+++ src/frontend/src/handler/show.rs
@@ -20,7 +20,7 @@ use pgwire::pg_protocol::truncated_fmt;
 use pgwire::pg_response::{PgResponse, StatementType};
 use pgwire::pg_server::Session;
 use pgwire::types::Row;
-use risingwave_common::catalog::{ColumnCatalog, DEFAULT_SCHEMA_NAME};
+use risingwave_common::catalog::{ColumnCatalog, ColumnDesc, DEFAULT_SCHEMA_NAME};
 use risingwave_common::error::{ErrorCode, Result};
 use risingwave_common::types::DataType;
 use risingwave_common::util::addr::HostAddr;
@@ -56,6 +56,34 @@ pub fn get_columns_from_table(
     };
 
     Ok(column_catalogs)
+}
+
+pub fn get_columns_from_sink(
+    session: &SessionImpl,
+    sink_name: ObjectName,
+) -> Result<Vec<ColumnCatalog>> {
+    let binder = Binder::new_for_system(session);
+    let sink = binder.bind_sink_by_name(sink_name.clone())?;
+    Ok(sink.sink_catalog.full_columns().to_vec())
+}
+
+pub fn get_columns_from_view(
+    session: &SessionImpl,
+    view_name: ObjectName,
+) -> Result<Vec<ColumnCatalog>> {
+    let binder = Binder::new_for_system(session);
+    let view = binder.bind_view_by_name(view_name.clone())?;
+
+    Ok(view
+        .view_catalog
+        .columns
+        .iter()
+        .enumerate()
+        .map(|(idx, field)| ColumnCatalog {
+            column_desc: ColumnDesc::from_field_with_column_id(field, idx as _),
+            is_hidden: false,
+        })
+        .collect())
 }
 
 pub fn get_indexes_from_table(
@@ -142,7 +170,17 @@ pub async fn handle_show_object(
             .map(|t| t.name.clone())
             .collect(),
         ShowObject::Columns { table } => {
-            let columns = get_columns_from_table(&session, table)?;
+            let Ok(columns) = get_columns_from_table(&session, table.clone())
+                .or(get_columns_from_sink(&session, table.clone()))
+                .or(get_columns_from_view(&session, table.clone()))
+            else {
+                return Err(CatalogError::NotFound(
+                    "table, source, sink or view",
+                    table.to_string(),
+                )
+                .into());
+            };
+
             let rows = col_descs_to_rows(columns);
 
             return Ok(PgResponse::builder(StatementType::SHOW_COMMAND)
