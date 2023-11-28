21047de479b9dc078025ee4c978ce6aa3d8aa321
Shanicky Chen <peng@risingwave-labs.com>
2023-11-28 16:31:06 +0800

    tmp

--- src/meta/src/model/stream.rs
+++ src/meta/src/model/stream.rs
@@ -37,6 +37,11 @@ use crate::stream::{build_actor_connector_splits, build_actor_split_impls, Split
 /// Column family name for table fragments.
 const TABLE_FRAGMENTS_CF_NAME: &str = "cf/table_fragments";
 
+pub enum TableFragmentsParallelism {
+    Adaptive,
+    Fixed(u32),
+}
+
 /// Fragments of a streaming job.
 ///
 /// We store whole fragments in a single column family as follow:
@@ -60,6 +65,8 @@ pub struct TableFragments {
 
     /// The streaming context associated with this stream plan and its fragments
     pub ctx: StreamContext,
+
+    pub assigned_parallelism: TableFragmentsParallelism,
 }
 
 #[derive(Debug, Clone, Default)]
