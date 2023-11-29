bf5e1300a2dcae2ae925ad28db93089e4a372c29
xxchan <xxchan22f@gmail.com>
2023-11-29 14:21:32 +0800

    merge #13716

--- ci/scripts/misc-check-cron.sh
+++ ci/scripts/misc-check-cron.sh
@@ -5,9 +5,5 @@ set -euo pipefail
 
 source ci/scripts/common.sh
 
-# RUSTSEC-2023-0052: https://github.com/risingwavelabs/risingwave/issues/11842
-# RUSTSEC-2023-0071 https://github.com/risingwavelabs/risingwave/issues/13703
 echo "--- Run audit check"
-cargo audit \
-  --ignore RUSTSEC-2023-0052 \
-  --ignore RUSTSEC-2023-0071
+cargo audit
