df71b1b5be4f8e44374a19c37048f733412a01e7
Noel Kwan <noelkwan1998@gmail.com>
2023-11-29 10:10:22 +0800

    patch

--- ci/scripts/check.sh
+++ ci/scripts/check.sh
@@ -50,4 +50,5 @@ sccache --zero-stats
 
 echo "--- Run audit check"
 cargo audit \
-  --ignore RUSTSEC-2023-0052 # https://github.com/risingwavelabs/risingwave/issues/11842
+  --ignore RUSTSEC-2023-0052 # https://github.com/risingwavelabs/risingwave/issues/11842 \
+  --ignore RUSTSEC-2023-0071 # https://github.com/risingwavelabs/risingwave/issues/13703
