0aab29ee9c749502cbc26cd4c2a727b82cea4c1f
Tao Wu <wutao@singularity-data.com>
2023-11-30 21:18:51 +0800

    revert redis-sink

--- integration_tests/redis-sink/create_source.sql
+++ integration_tests/redis-sink/create_source.sql
@@ -8,8 +8,9 @@ CREATE table user_behaviors (
     parent_target_id VARCHAR,
     PRIMARY KEY(user_id)
 ) WITH (
-    connector = 'kafka',
-    topic = 'user_behaviors',
-    properties.bootstrap.server = 'message_queue:29092',
-    scan.startup.mode = 'earliest'
+    connector = 'datagen',
+    fields.user_id.kind = 'sequence',
+    fields.user_id.start = 1,
+    fields.user_id.end = 100,
+    datagen.rows.per.second = '100'
 ) FORMAT PLAIN ENCODE JSON;
