efebb49126b3b4d040d74eabeed453d06f0c2bcc
xxchan <xxchan22f@gmail.com>
2023-12-01 01:02:11 +0800

    try debug build

--- ci/scripts/release.sh
+++ ci/scripts/release.sh
@@ -54,8 +54,8 @@ if [[ -n "${BUILDKITE_TAG}" ]]; then
 fi
 
 echo "--- Build risingwave release binary"
-cargo build -p risingwave_cmd_all --features "rw-static-link" --profile release
-cargo build --bin risectl --features "rw-static-link" --profile release
+cargo build -p risingwave_cmd_all --features "rw-static-link" --profile dev
+cargo build --bin risectl --features "rw-static-link" --profile dev
 cd target/release && chmod +x risingwave risectl
 
 echo "--- Upload nightly binary to s3"
