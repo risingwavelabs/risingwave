5c2be072e9f6796ec209cf1ca3b816fa9034eb5c
Jinser Kafka <aimer@purejs.icu>
2023-12-06 23:28:58 +0800

    test: update mindsdb docker compose to follow upstream

--- integration_tests/mindsdb/create_database.sql
+++ integration_tests/mindsdb/create_database.sql
@@ -2,7 +2,7 @@ CREATE DATABASE example_data
 WITH ENGINE = "postgres",
 PARAMETERS = {
   "user": "root",
-  "host": "frontend-node-0",
+  "host": "risingwave-standalone",
   "port": "4566",
   "database": "dev"
 };
