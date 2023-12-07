fe936fff7cc396ea1f5cb0c6dc83ec904a03224b
Tao Wu <wutao@singularity-data.com>
2023-12-08 03:14:42 +0800

    add comment to query sink

--- integration_tests/mindsdb/query_sink.sh
+++ integration_tests/mindsdb/query_sink.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -x  # Enable printing of each command
 
-# The model creation may take a long time. Our estimate is 30 seconds.
+# The model creation may take a long time. Our estimate is 30 seconds. But it can be longer in lower-perf machines.
 sleep 30
 
 QUERY='SELECT rental_price FROM home_rentals_model WHERE number_of_bathrooms = 2 AND sqft = 1000;'
