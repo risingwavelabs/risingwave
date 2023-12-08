2e0fdadef003c3225a3457dd4d99f66719cf85e1
Rossil <40714231+Rossil2012@users.noreply.github.com>
2023-12-08 09:52:25 +0800

    fix model def in meta sql store

--- src/meta/model_v2/migration/src/m20230908_072257_init.rs
+++ src/meta/model_v2/migration/src/m20230908_072257_init.rs
@@ -498,8 +498,8 @@ impl MigrationTrait for Migration {
                     .col(ColumnDef::new(Source::RowIdIndex).integer())
                     .col(ColumnDef::new(Source::Columns).json().not_null())
                     .col(ColumnDef::new(Source::PkColumnIds).json().not_null())
-                    .col(ColumnDef::new(Source::Properties).json().not_null())
-                    .col(ColumnDef::new(Source::Options).json().not_null())
+                    .col(ColumnDef::new(Source::WithProperties).json().not_null())
+                    .col(ColumnDef::new(Source::FormatEncodeOptions).json().not_null())
                     .col(ColumnDef::new(Source::Definition).string().not_null())
                     .col(ColumnDef::new(Source::SourceInfo).json())
                     .col(ColumnDef::new(Source::WatermarkDescs).json().not_null())
@@ -1036,8 +1036,8 @@ enum Source {
     RowIdIndex,
     Columns,
     PkColumnIds,
-    Properties,
-    Options,
+    WithProperties,
+    FormatEncodeOptions,
     Definition,
     SourceInfo,
     WatermarkDescs,
