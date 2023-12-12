12f643ec345603f64b0dd5652660019a2dcf7823
xfz <xfz2000x@gmail.com>
2023-12-12 11:03:22 +0800

    fix

--- integration_tests/cockroach-sink/prepare.sh
+++ integration_tests/cockroach-sink/prepare.sh
@@ -3,4 +3,4 @@
 set -euo pipefail
 
 # setup cockroach
-docker compose exec postgres bash -c "psql postgresql://root@cockroachdb:26257/defaultdb < postgres_prepare.sql"
+docker compose exec postgres bash -c "psql postgresql://root@cockroachdb:26257/defaultdb < cockroach_prepare.sql"
--- integration_tests/tidb-cdc-sink/prepare.sh
+++ integration_tests/tidb-cdc-sink/prepare.sh
@@ -2,8 +2,8 @@
 
 set -euo pipefail
 
-sleep 10
-
 docker compose exec mysql bash -c "mysql --password= -h tidb --port 4000 -u root test < tidb_create_tables.sql"
 
 docker compose exec mysql bash -c "mysql --password= -h tidb --port 4000 -u root test < tidb_prepare.sql"
+
+sleep 15
