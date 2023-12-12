96bca4ce66071640dfa5c4807b8e582cb35dacca
Richard Chien <stdrc@outlook.com>
2023-12-12 17:50:32 +0800

    refactor(streaming): clean up unnecessary `metrics` field in agg executor (#13946)
    Signed-off-by: Richard Chien <stdrc@outlook.com>

--- src/stream/src/from_proto/simple_agg.rs
+++ src/stream/src/from_proto/simple_agg.rs
@@ -71,7 +71,6 @@ impl ExecutorBuilder for SimpleAggExecutorBuilder {
             intermediate_state_table,
             distinct_dedup_tables,
             watermark_epoch: stream.get_watermark_epoch(),
-            metrics: params.executor_stats,
             extra: SimpleAggExecutorExtraArgs {},
         })?
         .boxed())
