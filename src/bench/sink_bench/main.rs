98f3cc55e1dddbf446c59c724ada8c17f9123304
xxhZs <1060434431@qq.com>
2023-12-12 13:34:59 +0800

    save

--- src/bench/sink_bench/main.rs
+++ src/bench/sink_bench/main.rs
@@ -21,19 +21,22 @@ use core::sync::atomic::{AtomicU64, Ordering};
 use core::time::Duration;
 use std::collections::{HashMap, HashSet};
 use std::future::{poll_fn, Future};
+use risingwave_common::array::error::anyhow;
 use futures::prelude::stream::PollNext;
 use futures::stream::select_with_strategy;
 use futures_async_stream::{try_stream, for_await};
 use futures::{Stream, StreamExt, TryStreamExt, select};
 
-
+use risingwave_connector::dispatch_sink;
 use futures::prelude::future::{try_join_all, Either};
+use risingwave_common::catalog::TableId;
 use risingwave_common::error::{RwError, anyhow_error};
+use risingwave_connector::sink::catalog::SinkType;
 use risingwave_connector::sink::log_store::{TruncateOffset, LogStoreResult, LogStoreReadItem};
 use risingwave_connector::source::StreamChunkWithState;
 use risingwave_connector::{sink::{build_sink, Sink, SinkWriterParam, log_store::LogReader, SinkParam, LogSinker}, source::{datagen::{DatagenSplitEnumerator, DatagenProperties, DatagenSplitReader}, SplitEnumerator, create_split_reader, SplitReader, Column, DataType}, parser::{ParserConfig, SpecificParserConfig, EncodingProperties, ProtocolProperties}};
-use risingwave_stream::executor::{Message, StreamExecutorError, Barrier};
-use tokio::sync::RwLock;
+use risingwave_stream::executor::{Message, StreamExecutorError, Barrier, SinkExecutor, StreamExecutorResult};
+use tokio::sync::{RwLock, oneshot};
 use tokio::sync::mpsc::UnboundedReceiver;
 use tokio::sync::oneshot::Receiver;
 use tokio::time::sleep;
@@ -80,7 +83,7 @@ pub struct MockDatagenSource{
     rx: RwLock<Receiver<()>>,
 }
 impl MockDatagenSource{
-    pub async fn new() -> MockDatagenSource{
+    pub async fn new(rx: Receiver<()>) -> MockDatagenSource{
         let properties= DatagenProperties{
             split_num: Some("1".to_string()),
             rows_per_second: 10000,
@@ -97,7 +100,7 @@ impl MockDatagenSource{
         };
         let mock_datum = vec![
             Column {
-                name: "_".to_string(),
+                name: "id".to_string(),
                 data_type: DataType::Int64,
                 is_visible: true,
             },
@@ -114,9 +117,9 @@ impl MockDatagenSource{
         ];
         
         let splits = datagen_enumerator.list_splits().await.unwrap();
-        let reader = DatagenSplitReader::new(properties.clone(), splits, parser_config.clone(), Default::default(), Some(mock_datum.clone())).await.unwrap();
+        let datagen_split_reader = DatagenSplitReader::new(properties.clone(), splits, parser_config.clone(), Default::default(), Some(mock_datum.clone())).await.unwrap();
 
-        MockDatagenSource{ reader }
+        MockDatagenSource{ epoch: AtomicU64::new(0), rx: RwLock::new(rx), datagen_split_reader }
     }
 
     #[try_stream(ok = Message, error = StreamExecutorError)]
@@ -171,5 +174,75 @@ async fn consume_log_stream<S: Sink> (sink: S, log_reader: MockRangeLogReader, s
 async fn main() {
     // let sinkParam = SinkParam{ sink_id: todo!(), properties: todo!(), columns: todo!(), downstream_pk: todo!(), sink_type: todo!(), format_desc: todo!(), db_name: todo!(), sink_from_name: todo!(), target_table: todo!() };
     // let sink = build_sink(sinkParam).unwrap(); 
-    MockDatagenSource::new().await;
+    let (tx, rx) = oneshot::channel();
+    let mock_datagen_source = MockDatagenSource::new(rx).await;
+    let columns = vec![
+        Column {
+            name: "id".to_string(),
+            data_type: DataType::Int64,
+            is_visible: true,
+        },
+        Column {
+            name: "random_int".to_string(),
+            data_type: DataType::Int32,
+            is_visible: true,
+        },
+        Column {
+            name: "random_int2".to_string(),
+            data_type: DataType::Int32,
+            is_visible: true,
+        },
+    ];
+    let sink_param = SinkParam {
+        sink_id: "test".to_string(),
+        properties: HashMap::default(),
+        columns: vec![
+            Column {
+                name: "id".to_string(),
+                data_type: DataType::Int64,
+                is_visible: true,
+            },
+            Column {
+                name: "random_int".to_string(),
+                data_type: DataType::Int32,
+                is_visible: true,
+            },
+            Column {
+                name: "random_int2".to_string(),
+                data_type: DataType::Int32,
+                is_visible: true,
+            },
+        ],
+        downstream_pk: vec!["id".to_string()],
+        sink_type: SinkType::AppendOnly,
+        format_desc: None,
+        db_name: "test".to_string(),
+        sink_from_name: "test".to_string(),
+        target_table: Some(TableId::new(1)),
+    };
+    let sink = build_sink(sink_param).unwrap();
+    let sink_writer_param = SinkWriterParam::for_test();
+    dispatch_sink!(sink, sink, {
+        execute_consume_log(
+            sink,
+            mock_datagen_source,
+            sink_writer_param
+        )
+    })
+}
+
+async fn execute_consume_log<S: Sink, R: LogReader>(
+    sink: S,
+    log_reader: R,
+    sink_writer_param: SinkWriterParam,
+) -> StreamExecutorResult<Message> {
+    let metrics = sink_writer_param.sink_metrics.clone();
+    let log_sinker = sink.new_log_sinker(sink_writer_param).await?;
+
+    let log_reader = log_reader
+        .transform_chunk(move |chunk| {
+                chunk
+        });
+    log_sinker.consume_log_and_sink(log_reader).await?;
+    Err(anyhow!("end of stream").into())
 }
