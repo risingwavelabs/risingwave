e5612b3a07309b53bd77a72f8c73cec533b0a3ac
yufansong <yufan@risingwave-labs.com>
2023-12-12 18:54:31 -0800

    run correctly

--- integration_tests/nats/create_source.sql
+++ integration_tests/nats/create_source.sql
@@ -1,3 +1,5 @@
+CREATE TABLE personnel (id integer, name varchar,);
+
 CREATE TABLE nats_source_table
 (
   id integer,
@@ -6,6 +8,44 @@ CREATE TABLE nats_source_table
 WITH (
   connector='nats',
     server_url='nats-server:4222',
-    subject='subject2',
+    subject='subject1',
+    stream = 'my_stream',
     connect_mode='plain'
 ) FORMAT PLAIN ENCODE JSON;
+
+CREATE SINK nats_sink
+FROM
+  personnel WITH (
+    connector = 'nats',
+    server_url = 'nats-server:4222',
+    subject = 'subject1',
+    type = 'append-only',
+    force_append_only = 'true',
+    connect_mode = 'plain'
+  );
+
+INSERT INTO
+  personnel
+VALUES
+  (1, 'Alice'),
+  (2, 'Bob');
+
+INSERT INTO
+  personnel
+VALUES
+  (3, 'Tom'),
+  (4, 'Jerry');
+
+INSERT INTO
+  personnel
+VALUES
+  (5, 'Araminta'),
+  (6, 'Clover');
+
+INSERT INTO
+  personnel
+VALUES
+  (7, 'Posey'),
+  (8, 'Waverly');
+
+FLUSH;
