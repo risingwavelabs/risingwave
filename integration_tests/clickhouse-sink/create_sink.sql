471ff63359a3499b24f053ca5fd97e4972c38624
xfz <xfz2000x@gmail.com>
2023-12-18 11:22:35 +0800

    test: add clickhouse-sink compatible test

--- integration_tests/clickhouse-sink/create_sink.sql
+++ integration_tests/clickhouse-sink/create_sink.sql
@@ -10,3 +10,16 @@ FROM
     clickhouse.database = 'default',
     clickhouse.table='demo_test',
 );
+
+CREATE SINK ck_types_sink
+FROM
+    ck_types WITH (
+    connector = 'clickhouse',
+    type = 'append-only',
+    force_append_only='true',
+    clickhouse.url = 'http://clickhouse-server-1:8123',
+    clickhouse.user = 'default',
+    clickhouse.password = '',
+    clickhouse.database = 'default',
+    clickhouse.table='ck_types',
+);
