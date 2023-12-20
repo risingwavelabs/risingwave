861c551eba7e644ffe5d6b0585f26fe5fa23522d
Bugen Zhao <i@bugenzhao.com>
2023-12-20 17:58:43 +0800

    use `execute_and_catch`
    Signed-off-by: Bugen Zhao <i@bugenzhao.com>

--- src/jni_core/src/tracing_slf4j.rs
+++ src/jni_core/src/tracing_slf4j.rs
@@ -18,43 +18,43 @@ use jni::objects::JString;
 use jni::sys::jint;
 use tracing::Level;
 
-use crate::EnvParam;
+use crate::{execute_and_catch, EnvParam};
 
 #[no_mangle]
 pub(crate) extern "system" fn Java_com_risingwave_java_binding_Binding_tracingSlf4jEvent(
-    mut env: EnvParam<'_>,
+    env: EnvParam<'_>,
     class_name: JString<'_>,
     level: jint,
     message: JString<'_>,
 ) {
-    let Ok(class_name) = env.get_string(&class_name) else {
-        return;
-    };
-    let class_name: Cow<'_, str> = (&class_name).into();
+    execute_and_catch(env, move |env| {
+        let class_name = env.get_string(&class_name)?;
+        let class_name: Cow<'_, str> = (&class_name).into();
 
-    let Ok(message) = env.get_string(&message) else {
-        return;
-    };
-    let message: Cow<'_, str> = (&message).into();
+        let message = env.get_string(&message)?;
+        let message: Cow<'_, str> = (&message).into();
 
-    macro_rules! event {
-        ($lvl:expr) => {
-            tracing::event!(
-                target: "risingwave_connector_node",
-                $lvl,
-                class = class_name.as_ref(),
-                "{message}",
-            )
-        };
-    }
+        macro_rules! event {
+            ($lvl:expr) => {
+                tracing::event!(
+                    target: "risingwave_connector_node",
+                    $lvl,
+                    class = class_name.as_ref(),
+                    "{message}",
+                )
+            };
+        }
 
-    // See `com.risingwave.tracing.TracingSlf4jImpl`.
-    match level {
-        0 => event!(Level::ERROR),
-        1 => event!(Level::WARN),
-        2 => event!(Level::INFO),
-        3 => event!(Level::DEBUG),
-        4 => event!(Level::TRACE),
-        _ => unreachable!(),
-    }
+        // See `com.risingwave.tracing.TracingSlf4jImpl`.
+        match level {
+            0 => event!(Level::ERROR),
+            1 => event!(Level::WARN),
+            2 => event!(Level::INFO),
+            3 => event!(Level::DEBUG),
+            4 => event!(Level::TRACE),
+            _ => unreachable!(),
+        }
+
+        Ok(())
+    })
 }
