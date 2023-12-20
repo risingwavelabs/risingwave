feadac76121fb8b385433dabd2b8b79f2720e318
Bohan Zhang <tabvision@bupt.icu>
2023-12-20 14:06:36 +0800

    feat: introduce `include` clause to add additional connector columns (#13707)
    Signed-off-by: tabVersion <tabvision@bupt.icu>
    Co-authored-by: xxchan <xxchan22f@gmail.com>

--- src/frontend/src/binder/expr/mod.rs
+++ src/frontend/src/binder/expr/mod.rs
@@ -18,6 +18,7 @@ use risingwave_common::error::{ErrorCode, Result, RwError};
 use risingwave_common::types::DataType;
 use risingwave_common::util::iter_util::zip_eq_fast;
 use risingwave_common::{bail_not_implemented, not_implemented};
+use risingwave_pb::plan_common::{AdditionalColumnType, ColumnDescVersion};
 use risingwave_sqlparser::ast::{
     Array, BinaryOperator, DataType as AstDataType, Expr, Function, JsonPredicateType, ObjectName,
     Query, StructField, TrimWhereField, UnaryOperator,
@@ -575,6 +576,8 @@ pub fn bind_struct_field(column_def: &StructField) -> Result<ColumnDesc> {
         type_name: "".to_string(),
         generated_or_default_column: None,
         description: None,
+        additional_column_type: AdditionalColumnType::Normal,
+        version: ColumnDescVersion::Pr13707,
     })
 }
 
--- src/sqlparser/src/parser.rs
+++ src/sqlparser/src/parser.rs
@@ -2442,6 +2442,7 @@ impl Parser {
         } else {
             false
         };
+        let include_options = self.parse_include_options()?;
 
         // PostgreSQL supports `WITH ( options )`, before `AS`
         let with_options = self.parse_with_properties()?;
@@ -2494,7 +2495,22 @@ impl Parser {
             append_only,
             query,
             cdc_table_info,
+            include_column_options: include_options,
         })
+    }
+
+    pub fn parse_include_options(&mut self) -> Result<Vec<(Ident, Option<Ident>)>, ParserError> {
+        let mut options = vec![];
+        while self.parse_keyword(Keyword::INCLUDE) {
+            let add_column = self.parse_identifier()?;
+            if self.parse_keyword(Keyword::AS) {
+                let column_alias = self.parse_identifier()?;
+                options.push((add_column, Some(column_alias)));
+            } else {
+                options.push((add_column, None));
+            }
+        }
+        Ok(options)
     }
 
     pub fn parse_columns_with_watermark(&mut self) -> Result<ColumnsDefTuple, ParserError> {
