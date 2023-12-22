596af890be3b8eb49eef093125a9a43a1d5bf20e
xfz <xfz2000x@gmail.com>
2023-12-22 16:21:44 +0800

    update

--- integration_tests/starrocks-sink/sink_check.py
+++ integration_tests/starrocks-sink/sink_check.py
@@ -6,7 +6,7 @@ relations = ['demo.demo_bhv_table']
 failed_cases = []
 for rel in relations:
     sql = f'SELECT COUNT(*) FROM {rel};'
-    print(f"Running SQL: {sql} ON Doris")
+    print(f"Running SQL: {sql} ON Starrocks")
     command = f'mysql -uroot -P9030 -h127.0.0.1 -e "{sql}"'
     output = subprocess.check_output(
         ["docker", "compose", "exec", "starrocks-fe", "bash", "-c", command])
