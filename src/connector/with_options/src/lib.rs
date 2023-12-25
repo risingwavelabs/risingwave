d1c422482b5b43894dfd08e6e8ce7f942847bdb5
xxchan <xxchan22f@gmail.com>
2023-12-25 12:23:26 +0800

    refactor(connector): Add a dummy trait `WithOptions`

---
+++ src/connector/with_options/src/lib.rs
@@ -0,0 +1,53 @@
+// Copyright 2023 RisingWave Labs
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+use proc_macro::TokenStream;
+use quote::quote;
+use syn::{parse_macro_input, DeriveInput};
+
+/// Annotates that the struct represents the WITH properties for a connector.
+#[proc_macro_derive(WithOptions, attributes(with_option))]
+pub fn derive_helper_attr(input: TokenStream) -> TokenStream {
+    let input = parse_macro_input!(input as DeriveInput);
+
+    let fields = match input.data {
+        syn::Data::Struct(ref data) => match data.fields {
+            syn::Fields::Named(ref fields) => &fields.named,
+            _ => return quote! { compile_error!("WithOptions can only be derived for structs with named fields"); }.into(),
+        },
+        _ => return quote! { compile_error!("WithOptions can only be derived for structs"); }.into(),
+    };
+    let struct_name = input.ident;
+
+    let mut assert_impls = vec![];
+
+    for field in fields {
+        let field_name = field.ident.as_ref().unwrap();
+
+        assert_impls.push(quote!(
+            self.#field_name.assert_receiver_is_with_options();
+        ))
+    }
+
+    // This macro is only be expected to used in risingwave_connector. This trait is also defined there.
+    let code = quote! {
+        impl crate::with_options::WithOptions for #struct_name {
+            fn assert_receiver_is_with_options(&self) {
+                #(#assert_impls)*
+            }
+        }
+    };
+
+    code.into()
+}
