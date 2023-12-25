04407c90d1f5a35002a46f5c28c42aee6857348c
William Wen <william123.wen@gmail.com>
2023-12-25 19:21:47 +0800

    fmt

--- src/storage/hummock_test/src/failpoint_tests.rs
+++ src/storage/hummock_test/src/failpoint_tests.rs
@@ -95,10 +95,13 @@ async fn test_failpoints_state_store_read_upload() {
         .await
         .unwrap();
 
-    local.seal_current_epoch(
-        3,
-        risingwave_storage::store::SealCurrentEpochOptions::for_test(),
-    ).await.unwrap();
+    local
+        .seal_current_epoch(
+            3,
+            risingwave_storage::store::SealCurrentEpochOptions::for_test(),
+        )
+        .await
+        .unwrap();
 
     // Get the value after flushing to remote.
     let anchor_prefix_hint = {
@@ -134,10 +137,13 @@ async fn test_failpoints_state_store_read_upload() {
         .await
         .unwrap();
 
-    local.seal_current_epoch(
-        u64::MAX,
-        risingwave_storage::store::SealCurrentEpochOptions::for_test(),
-    ).await.unwrap();
+    local
+        .seal_current_epoch(
+            u64::MAX,
+            risingwave_storage::store::SealCurrentEpochOptions::for_test(),
+        )
+        .await
+        .unwrap();
 
     // sync epoch1 test the read_error
     let ssts = hummock_storage
