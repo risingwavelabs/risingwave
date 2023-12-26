d25c5da46c51581c1dd2dc4c627ebbda96573a3d
Bugen Zhao <i@bugenzhao.com>
2023-12-26 15:25:07 +0800

    feat: show thread name in logs for playground and standalone (#14114)
    Signed-off-by: Bugen Zhao <i@bugenzhao.com>

--- src/cmd_all/src/bin/risingwave.rs
+++ src/cmd_all/src/bin/risingwave.rs
@@ -193,14 +193,16 @@ fn main() -> Result<()> {
 
 fn playground(opts: PlaygroundOpts) {
     let settings = risingwave_rt::LoggerSettings::new("playground")
-        .with_target("risingwave_storage", Level::WARN);
+        .with_target("risingwave_storage", Level::WARN)
+        .with_thread_name(true);
     risingwave_rt::init_risingwave_logger(settings);
     risingwave_rt::main_okk(risingwave_cmd_all::playground(opts)).unwrap();
 }
 
 fn standalone(opts: StandaloneOpts) {
     let settings = risingwave_rt::LoggerSettings::new("standalone")
-        .with_target("risingwave_storage", Level::WARN);
+        .with_target("risingwave_storage", Level::WARN)
+        .with_thread_name(true);
     risingwave_rt::init_risingwave_logger(settings);
     risingwave_rt::main_okk(risingwave_cmd_all::standalone(opts)).unwrap();
 }
