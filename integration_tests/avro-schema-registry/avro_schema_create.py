48b5f2d22fc22afd9561c02063a850459a2c2004
Tao Wu <wutao@singularity-data.com>
2023-12-26 16:10:26 +0800

    update avro_schema_create

--- integration_tests/avro-schema-registry/avro_schema_create.py
+++ integration_tests/avro-schema-registry/avro_schema_create.py
@@ -1,10 +1,23 @@
-import psycopg2
+"""
+A helper script for creating a Kafka Avro sink in the upsert mode.
+The subject name for Kafka value is '{schema_name}.{table_name}'.
+For Kafka key, it's '{schema_name}.{table_name}-key'.
+
+This script will retrieve a RisingWave table's schema,
+convert it to Avro schema, and register it to Confluent Schema Registry.
+
+Please note that not all RisingWave types are suppported due to the limitation
+of Avro.
+"""
+
+from dataclasses import dataclass
 import sys
 import json
 from confluent_kafka.schema_registry.schema_registry_client import SchemaRegistryClient
 from confluent_kafka.schema_registry import Schema
+import psycopg2
 
-# PostgreSQL Connection Details
+# PostgreSQL Details
 DB_NAME = "dev"
 DB_USER = "root"
 DB_PASSWORD = ""
@@ -12,94 +25,152 @@ DB_HOST = sys.argv[1]
 DB_PORT = "4566"
 PG_TABLE = "t"
 PG_SCHEMA = "public"
+PG_PRIMARY_KEYS = ""  # default to table's primary keys
 
 # Schema Registry Details
 SCHEMA_REGISTRY_URL = f"http://{sys.argv[2]}:8081"
 
-
-def fetch_postgres_schema():
-    conn = psycopg2.connect(
-        dbname=DB_NAME, user=DB_USER, password=DB_PASSWORD, host=DB_HOST, port=DB_PORT
-    )
-    cursor = conn.cursor()
-
-    # Fetch PostgreSQL schema information for a specific table
-    cursor.execute(
-        f"""
-        SELECT table_name, column_name, data_type
-        FROM information_schema.columns
-        WHERE table_schema = '{PG_SCHEMA}' AND table_name = '{PG_TABLE}';
-    """
-    )
-
-    postgres_schema = []
-    for _, column_name, data_type in cursor.fetchall():
-        postgres_schema.append({"name": column_name, "type": data_type})
-
-    conn.close()
-    return postgres_schema
-
-
-def generate_avro_schema(postgres_schema):
-    # Convert PostgreSQL data types to Avro data types
-    avro_data_types = {
-        "boolean": "boolean",
-        "smallint": "int",
-        "integer": "int",
-        "bigint": "long",
-        "real": "float",
-        "double precision": "double",
-        "character varying": "string",
-        "bytea": "bytes",
-        "timestamp with time zone": {"type": "long", "logicalType": "timestamp-micros"},
-        "timestamp without time zone": {
+AVRO_DATA_TYPES = {
+    "boolean": "boolean",
+    "smallint": "int",
+    "integer": "int",
+    "bigint": "long",
+    "real": "float",
+    "double precision": "double",
+    "character varying": "string",
+    "bytea": "bytes",
+    "timestamp with time zone": {"type": "long", "logicalType": "timestamp-micros"},
+    "timestamp without time zone": {
             "type": "long",
             "logicalType": "local-timestamp-micros",
-        },
-        "date": {"type": "int", "logicalType": "date"},
-        "time without time zone": {"type": "long", "logicalType": "time-micros"},
-    }
-
-    avro_schema = {
-        "type": "record",
-        "name": "{}.{}".format(PG_SCHEMA, PG_TABLE),
-        "fields": [],
-    }
-
-    for column in postgres_schema:
-        avro_data_type = avro_data_types.get(column["type"])
-        if avro_data_type is None:
-            raise Exception("Unsupported PostgreSQL data type '{}' for column '{}'.".format(
-                column['type'], column['name']))
-        avro_schema["fields"].append(
-            {"name": column["name"], "type": ["null", avro_data_type]}
-        )
-
-    return avro_schema
+    },
+    "date": {"type": "int", "logicalType": "date"},
+    "time without time zone": {"type": "long", "logicalType": "time-micros"},
+}
 
 
-def register_avro_schema(avro_schema, subject_name):
-    avro_schema_str = json.dumps(avro_schema)
-    print(avro_schema_str)
+@dataclass
+class ColumnDef:
+    """A RisingWave column"""
+    name: str
+    type: str
+    is_primary_key: bool
 
-    # Create a Kafka Producer client
-    client = SchemaRegistryClient({
-        "url": SCHEMA_REGISTRY_URL
-    })
 
-    # Register the schema
-    schema_id = client.register_schema(
-        subject_name, Schema(avro_schema_str, "AVRO"))
+class AvroSchema:
+    schema_json: str
+    subject_name: str
 
-    print("Schema registered successfully with ID:", schema_id)
+    def __init__(self, subject_name: str, fields: [ColumnDef]):
+        self.subject_name = subject_name
+
+        schema = {
+            "type": "record",
+            "name": subject_name,
+            "fields": []
+        }
+        for field in fields:
+            avro_data_type = AVRO_DATA_TYPES.get(field.type)
+            if avro_data_type is None:
+                err = f"Unsupported PostgreSQL data type '{field.type}' for"
+                f"column '{field.name}'."
+                raise Exception(err)
+            schema["fields"].append(
+                {"name": field.name, "type": ["null", avro_data_type]}
+            )
+        self.schema_json = json.dumps(schema)
+
+    def register(self, client: SchemaRegistryClient):
+        """Register the schema to Schema Registry"""
+        print(f"Registering schema {self.subject_name}")
+        print(self.schema_json)
+        schema_id = client.register_schema(
+            self.subject_name, Schema(self.schema_json, "AVRO"))
+        print("Schema registered successfully with ID:", schema_id)
+
+
+@dataclass
+class KeyValueSchema:
+    key: AvroSchema
+    value: AvroSchema
+
+    def __init__(self, columns: [ColumnDef]):
+        """Convert the RisignWave table schema to Avro schema"""
+
+        pk_columns = [col for col in columns if col.is_primary_key]
+        self.key = AvroSchema(f"{PG_SCHEMA}.{PG_TABLE}-key", pk_columns)
+        self.value = AvroSchema(f"{PG_SCHEMA}.{PG_TABLE}", columns)
+
+    def register(self):
+        """Register the schema to Schema Registry"""
+
+        # Create a Kafka Producer client
+        client = SchemaRegistryClient({
+            "url": SCHEMA_REGISTRY_URL
+        })
+        self.key.register(client)
+        self.value.register(client)
+
+
+def get_primary_keys(table_name, conn):
+    """Retrieves the primary keys of a table.
+    Returns:
+        tuple: A tuple containing the primary key column names, or None
+               if no primary key is found.
+    """
+
+    with conn.cursor() as cursor:
+        cursor.execute(f"DESCRIBE {table_name}")
+        rows = cursor.fetchall()
+
+        primary_key_cols = None
+        for row in rows:
+            if row[0] == 'primary key':
+                # Split composite keys
+                primary_key_cols = tuple(row[1].split(", "))
+                break
+
+    return primary_key_cols
+
+
+def fetch_postgres_schema() -> [ColumnDef]:
+    """Fetch a table's columns, with primary keys unset."""
+    pg_schema = []
+    with psycopg2.connect(
+            dbname=DB_NAME,
+            user=DB_USER,
+            password=DB_PASSWORD,
+            host=DB_HOST,
+            port=DB_PORT
+    ) as conn:
+        with conn.cursor() as cursor:
+
+            # Fetch PostgreSQL schema information for a specific table
+            cursor.execute(
+                f"""
+                SELECT table_name, column_name, data_type
+                FROM information_schema.columns
+                WHERE table_schema = '{PG_SCHEMA}' AND table_name = '{PG_TABLE}';
+            """
+            )
+
+            pg_schema = [ColumnDef(column_name, data_type, False)
+                         for _, column_name, data_type in cursor.fetchall()]
+
+        primary_keys = [pk.strip() for pk in PG_PRIMARY_KEYS.split(',')]
+        if len(primary_keys) == 0:
+            primary_keys = get_primary_keys(table_name=PG_TABLE, conn=conn)
+        for column in pg_schema:
+            if column.name in primary_keys:
+                column.is_primary_key = True
+
+    return pg_schema
 
 
 if __name__ == "__main__":
     postgres_schema = fetch_postgres_schema()
 
     if postgres_schema:
-        avro_schema = generate_avro_schema(postgres_schema)
-        subject_name = f"{PG_SCHEMA}-{PG_TABLE}"
-        register_avro_schema(avro_schema, subject_name)
+        KeyValueSchema(postgres_schema).register()
     else:
         print(f"No schema found for table '{PG_SCHEMA}.{PG_TABLE}'.")
