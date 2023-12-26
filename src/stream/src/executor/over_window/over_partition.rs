869629a43ddc09f8438959ab64e3544795427557
Richard Chien <stdrc@outlook.com>
2023-12-26 15:12:03 +0800

    refactor: remove unnecessary type aliases
    Signed-off-by: Richard Chien <stdrc@outlook.com>

--- src/stream/src/executor/over_window/over_partition.rs
+++ src/stream/src/executor/over_window/over_partition.rs
@@ -21,7 +21,7 @@ use std::ops::{Bound, RangeInclusive};
 
 use futures_async_stream::for_await;
 use risingwave_common::array::stream_record::Record;
-use risingwave_common::estimate_size::collections::BTreeMap as EstimatedBTreeMap;
+use risingwave_common::estimate_size::collections::EstimatedBTreeMap;
 use risingwave_common::row::{OwnedRow, Row};
 use risingwave_common::session_config::OverWindowCachePolicy as CachePolicy;
 use risingwave_common::types::Sentinelled;
