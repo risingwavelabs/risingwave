36809447d20716e260d9174de7ead108fb789aed
August <pin@singularity-data.com>
2023-12-28 18:14:31 +0800

    feat: introduce rw_streaming_parallelism system view

--- src/frontend/src/catalog/system_catalog/mod.rs
+++ src/frontend/src/catalog/system_catalog/mod.rs
@@ -439,6 +439,7 @@ prepare_sys_catalog! {
     { BuiltinCatalog::Table(&RW_RELATION_INFO), read_relation_info await },
     { BuiltinCatalog::Table(&RW_SYSTEM_TABLES), read_system_table_info },
     { BuiltinCatalog::View(&RW_RELATIONS) },
+    { BuiltinCatalog::View(&RW_STREAMING_PARALLELISM) },
     { BuiltinCatalog::Table(&RW_COLUMNS), read_rw_columns_info },
     { BuiltinCatalog::Table(&RW_TYPES), read_rw_types },
     { BuiltinCatalog::Table(&RW_HUMMOCK_PINNED_VERSIONS), read_hummock_pinned_versions await },
