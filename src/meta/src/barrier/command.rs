a8fee92c4b60af547f465e259b685efa6340c0bb
August <pin@singularity-data.com>
2024-01-03 18:48:29 +0800

    fix: missing persist for post collect of replace command

--- src/meta/src/barrier/command.rs
+++ src/meta/src/barrier/command.rs
@@ -914,7 +914,7 @@ impl CommandContext {
                             new_table_fragments,
                             merge_updates,
                             dispatchers,
-                            init_split_assignment: _,
+                            init_split_assignment,
                         }) = replace_table
                         {
                             let table_ids =
@@ -931,6 +931,7 @@ impl CommandContext {
                                     new_table_fragments,
                                     merge_updates,
                                     dispatchers,
+                                    init_split_assignment.clone(),
                                 )
                                 .await?;
                         }
@@ -970,7 +971,7 @@ impl CommandContext {
                 new_table_fragments,
                 merge_updates,
                 dispatchers,
-                ..
+                init_split_assignment,
             }) => {
                 let MetadataManager::V1(mgr) = &self.metadata_manager else {
                     unimplemented!("implement replace funcs in v2");
@@ -988,6 +989,7 @@ impl CommandContext {
                         new_table_fragments,
                         merge_updates,
                         dispatchers,
+                        init_split_assignment.clone(),
                     )
                     .await?;
             }
