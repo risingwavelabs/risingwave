974fa5342f655232c555b1e00e674765b4ef9b95
Bugen Zhao <i@bugenzhao.com>
2024-01-04 14:39:43 +0800

    fix(dashboard): minor style tweaks (#14356)
    Signed-off-by: Bugen Zhao <i@bugenzhao.com>
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

---
+++ dashboard/hook/useErrorToast.ts
@@ -0,0 +1,47 @@
+/*
+ * Copyright 2024 RisingWave Labs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import { AlertStatus, useToast } from "@chakra-ui/react"
+import { useCallback } from "react"
+
+export default function useErrorToast() {
+  const toast = useToast()
+
+  return useCallback(
+    (e: any, status: AlertStatus = "error") => {
+      let title: string
+      let description: string | undefined
+
+      if (e instanceof Error) {
+        title = e.message
+        description = e.cause?.toString()
+      } else {
+        title = e.toString()
+      }
+
+      toast({
+        title,
+        description,
+        status,
+        duration: 5000,
+        isClosable: true,
+      })
+
+      console.error(e)
+    },
+    [toast]
+  )
+}
