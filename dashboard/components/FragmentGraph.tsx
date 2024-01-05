490d592514ce25552d57d79f0238b676b9bc98e6
Bugen Zhao <i@bugenzhao.com>
2024-01-05 14:40:12 +0800

    fix multiple rendering
    Signed-off-by: Bugen Zhao <i@bugenzhao.com>

--- dashboard/components/FragmentGraph.tsx
+++ dashboard/components/FragmentGraph.tsx
@@ -333,7 +333,7 @@ export default function FragmentGraph({
       }
 
       const createFragment = (sel: Enter<FragmentSelection>) => {
-        const gSel = sel.append("g").attr("class", "actor").call(applyFragment)
+        const gSel = sel.append("g").attr("class", "fragment").call(applyFragment)
         return gSel
       }
 
