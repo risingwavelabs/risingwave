0f868bce5a8df12a63975dfca8962d69be868c23
zwang28 <84491488@qq.com>
2024-01-11 22:27:30 +0800

    fix ut

--- src/common/src/system_param/mod.rs
+++ src/common/src/system_param/mod.rs
@@ -374,6 +374,8 @@ macro_rules! impl_system_params_for_test {
             };
             ret.data_directory = Some("hummock_001".to_string());
             ret.state_store = Some("hummock+memory".to_string());
+            ret.backup_storage_url = Some("memory".into());
+            ret.backup_storage_directory = Some("backup".into());
             ret
         }
     };
