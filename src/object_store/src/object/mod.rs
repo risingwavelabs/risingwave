257202b9a6699aa85548f3a5e98b3d1ca5dbda7e
Kevin Axel <kevinaxel@163.com>
2024-01-12 22:53:53 +0800

    fix check
    Signed-off-by: Kevin Axel <kevinaxel@163.com>

--- src/object_store/src/object/mod.rs
+++ src/object_store/src/object/mod.rs
@@ -12,6 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+#[cfg(madsim)]
 pub mod sim;
 use std::ops::{Range, RangeBounds};
 use std::sync::Arc;
