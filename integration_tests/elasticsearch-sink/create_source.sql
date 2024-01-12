0247cad54fad24b920deacdd7bc534b7370e1677
xfz <xfz2000x@gmail.com>
2024-01-12 11:39:53 +0800

    update

--- integration_tests/elasticsearch-sink/create_source.sql
+++ integration_tests/elasticsearch-sink/create_source.sql
@@ -2,7 +2,7 @@ CREATE table user_behaviors (
     user_id int,
     target_id VARCHAR,
     target_type VARCHAR,
-    event_timestamp TIMESTAMPTZ,
+    event_timestamp TIMESTAMP,
     behavior_type VARCHAR,
     parent_target_type VARCHAR,
     parent_target_id VARCHAR,
