b54062b7f2f4fbe04379d3e2643d8191e6141086
Li0k <yuli@singularity-data.com>
2024-01-12 14:03:59 +0800

    chore(storage): disable merge imm (#14511)

--- src/common/src/config.rs
+++ src/common/src/config.rs
@@ -1086,7 +1086,7 @@ pub mod default {
         }
 
         pub fn imm_merge_threshold() -> usize {
-            4
+            0 // disable
         }
 
         pub fn write_conflict_detection_enabled() -> bool {
@@ -1170,7 +1170,7 @@ pub mod default {
             3
         }
         pub fn mem_table_spill_threshold() -> usize {
-            4 << 20
+            0 // disable
         }
 
         pub fn compactor_fast_max_compact_delete_ratio() -> u32 {
