555c1bbe2b1f59d8b521c25a405342f340489dfc
xfz <xfz2000x@gmail.com>
2024-01-15 20:49:39 +0800

    test: enable doris/starrocks upsert integration test

--- integration_tests/doris-sink/doris_prepare.sql
+++ integration_tests/doris-sink/doris_prepare.sql
@@ -11,5 +11,15 @@ PROPERTIES (
     "replication_allocation" = "tag.location.default: 1"
 );
 
+CREATE table upsert_table(
+    user_id int,
+    target_id text,
+    event_timestamp_local datetime
+) UNIQUE KEY(`user_id`)
+DISTRIBUTED BY HASH(`user_id`) BUCKETS 1
+PROPERTIES (
+    "replication_allocation" = "tag.location.default: 1"
+);
+
 CREATE USER 'users'@'%' IDENTIFIED BY '123456';
 GRANT ALL ON *.* TO 'users'@'%';
--- integration_tests/starrocks-sink/sink_check.py
+++ integration_tests/starrocks-sink/sink_check.py
@@ -1,7 +1,7 @@
 import subprocess
 import sys
 
-relations = ['demo.demo_bhv_table']
+relations = ['demo.demo_bhv_table', 'demo.upsert_table']
 
 failed_cases = []
 for rel in relations:
@@ -17,6 +17,30 @@ for rel in relations:
     print(f"{rows} rows in {rel}")
     if rows < 1:
         failed_cases.append(rel)
+
+# update data
+subprocess.run(["docker", "compose", "exec", "postgres", "bash", "-c", "psql -h risingwave-standalone -p 4566 -d dev -U root -f update_delete.sql"], check=True)
+
+# delete
+sql = f"SELECT COUNT(*) FROM demo.upsert_table;"
+command = f'mysql -uroot -P9030 -h127.0.0.1 -e "{sql}"'
+output = subprocess.check_output(
+    ["docker", "compose", "exec", "starrocks-fe", "bash", "-c", command])
+rows = int(output.decode('utf-8').split('\n')[1])
+print(f"{rows} rows in demo.upsert_table")
+if rows != 3:
+    print(f"rows expected 3, get {rows}")
+    failed_cases.append("delete demo.upsert_table")
+
+# update
+sql = f"SELECT target_id FROM demo.upsert_table WHERE user_id = 3;"
+command = f'mysql -uroot -P9030 -h127.0.0.1 -e "{sql}"'
+output = subprocess.check_output(
+    ["docker", "compose", "exec", "starrocks-fe", "bash", "-c", command])
+id = int(output.decode('utf-8').split('\n')[1])
+if id != 30:
+    print(f"target_id expected 30, get {rows}")
+    failed_cases.append("update demo.upsert_table")
 
 if len(failed_cases) != 0:
     print(f"Data check failed for case {failed_cases}")
