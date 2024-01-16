2bfcfd9a12a2d951888cd72db530b9663ceb5066
xxchan <xxchan22f@gmail.com>
2024-01-16 22:47:16 +0800

    remove comment

--- ci/scripts/run-unit-test.sh
+++ ci/scripts/run-unit-test.sh
@@ -10,10 +10,6 @@ cd ${REPO_ROOT}/src/expr/udf/python
 python3 -m pytest
 cd ${REPO_ROOT}
 
-echo "+++ Run unit tests with coverage"
+echo "+++ Run unit tests"
 # use tee to disable progress bar
 NEXTEST_PROFILE=ci cargo nextest run --features failpoints,sync_point --workspace --exclude risingwave_simulation
-
-# echo "--- Codecov upload coverage reports"
-# curl -Os https://uploader.codecov.io/latest/linux/codecov && chmod +x codecov
-# ./codecov -t "$CODECOV_TOKEN" -s . -F rust
