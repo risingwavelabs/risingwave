9ebf73443d198268fb845cf2e028ee208e6f68ec
xfz <73645462+xuefengze@users.noreply.github.com>
2024-01-16 16:39:27 +0800

    test: add `upsert` test for `doris/starrocks-sink` integration test (#14587)

--- integration_tests/starrocks-sink/starrocks_prepare.sql
+++ integration_tests/starrocks-sink/starrocks_prepare.sql
@@ -9,5 +9,13 @@ CREATE table demo_bhv_table(
 PRIMARY KEY(`user_id`)
 DISTRIBUTED BY HASH(`user_id`) properties("replication_num" = "1");
 
+CREATE table upsert_table(
+    user_id int,
+    target_id text,
+    event_timestamp_local datetime
+) ENGINE=OLAP
+PRIMARY KEY(`user_id`)
+DISTRIBUTED BY HASH(`user_id`) properties("replication_num" = "1");
+
 CREATE USER 'users'@'%' IDENTIFIED BY '123456';
 GRANT ALL ON *.* TO 'users'@'%';
