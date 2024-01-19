40b50248b418c9d008fdc344caa959d593edf34c
Rossil <40714231+Rossil2012@users.noreply.github.com>
2024-01-19 14:37:11 +0800

    fix confict

--- src/bench/sink_bench/main.rs
+++ src/bench/sink_bench/main.rs
@@ -240,7 +240,7 @@ impl MockDatagenSource {
         loop {
             for i in &mut readers {
                 let item = i.next().await.unwrap().unwrap();
-                yield Message::Chunk(item.chunk);
+                yield Message::Chunk(item);
             }
         }
     }
