d93f03e6228f3fb64f300eb1c19c435dd815fb32
Eric Fu <eric@singularity-data.com>
2024-01-20 19:05:29 +0800

    add new test case and fix existing

--- src/frontend/src/optimizer/plan_node/generic/join.rs
+++ src/frontend/src/optimizer/plan_node/generic/join.rs
@@ -12,7 +12,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-use std::collections::hash_map::Entry;
 use std::collections::HashMap;
 
 use itertools::{EitherOrBoth, Itertools};
