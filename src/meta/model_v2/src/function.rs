04505d8a977bceef24aa86ddc1ef40222d94abbb
Runji Wang <wangrunji0408@163.com>
2024-01-22 15:35:35 +0800

    encode arg_names as comma separated string in sql backend
    Signed-off-by: Runji Wang <wangrunji0408@163.com>

--- src/meta/model_v2/src/function.rs
+++ src/meta/model_v2/src/function.rs
@@ -36,7 +36,8 @@ pub struct Model {
     #[sea_orm(primary_key, auto_increment = false)]
     pub function_id: FunctionId,
     pub name: String,
-    pub arg_names: Vec<String>,
+    // encode Vec<String> as comma separated string
+    pub arg_names: String,
     pub arg_types: DataTypeArray,
     pub return_type: DataType,
     pub language: String,
@@ -91,7 +92,7 @@ impl From<PbFunction> for ActiveModel {
         Self {
             function_id: Set(function.id as _),
             name: Set(function.name),
-            arg_names: Set(function.arg_names),
+            arg_names: Set(function.arg_names.join(",")),
             arg_types: Set(DataTypeArray(function.arg_types)),
             return_type: Set(DataType(function.return_type.unwrap())),
             language: Set(function.language),
