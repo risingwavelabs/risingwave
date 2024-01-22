4c891497bbf2a3bb7d2f013bd7308c3be176d50f
Bugen Zhao <i@bugenzhao.com>
2024-01-22 17:34:06 +0800

    comment why disable sccahe
    Signed-off-by: Bugen Zhao <i@bugenzhao.com>

--- ci/scripts/check-dylint.sh
+++ ci/scripts/check-dylint.sh
@@ -4,7 +4,7 @@
 set -euo pipefail
 
 source ci/scripts/common.sh
-unset RUSTC_WRAPPER
+unset RUSTC_WRAPPER # disable sccache, see https://github.com/mozilla/sccache/issues/861
 
 echo "--- Run dylint check (dev, all features)"
 # Instead of `-D warnings`, we only deny warnings from our own lints. This is because...
