e901dd2903f6df5780bc1438f1a8423c567140ba
tabVersion <tabvision@bupt.icu>
2024-01-22 16:02:17 +0800

    stash

--- src/expr/impl/src/scalar/encrypt.rs
+++ src/expr/impl/src/scalar/encrypt.rs
@@ -11,3 +11,35 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
+
+use aes_gcm::{Aes128Gcm, Aes256Gcm, Key, KeyInit, OsRng};
+use risingwave_expr::{function, ExprError, Result};
+
+enum CypherEnum {
+    Aes128(Aes128Gcm),
+    Aes256(Aes256Gcm),
+}
+
+fn build_encrypt(key: &[u8], mode: &str) -> Result<CypherEnum> {
+    let cypher = match mode.to_lowercase().as_str() {
+        "aes128" => CypherEnum::Aes128(Aes128Gcm::new(Key::<Aes128Gcm>::from_slice(key))),
+        "aes256" => CypherEnum::Aes256(Aes256Gcm::new(Key::<Aes256Gcm>::from_slice(key))),
+        _ => {
+            return Err(ExprError::InvalidParam {
+                name: "mode",
+                reason: format!("invalid mode: {}, only accept aes128 or aes256 here", mode).into(),
+            });
+        }
+    };
+
+    Ok(cypher)
+}
+
+/// from pg doc https://www.postgresql.org/docs/current/pgcrypto.html#PGCRYPTO-RAW-ENC-FUNCS
+#[function(
+    "decrypt(bytea, bytea, text) -> bytea",
+    prebuilt = "build_encrypt($1, $2)"
+)]
+pub fn decrypt(data: &[u8], cypher: CypherEnum) -> Result<Vec<u8>> {
+    Ok(data)
+}
