ec282b7ffe58848aa3abd23ab81cbf7936b6731f
Bugen Zhao <i@bugenzhao.com>
2024-01-23 16:49:22 +0800

    fix flags
    Signed-off-by: Bugen Zhao <i@bugenzhao.com>

--- ci/scripts/check-dylint.sh
+++ ci/scripts/check-dylint.sh
@@ -11,4 +11,4 @@ echo "--- Run dylint check (dev, all features)"
 # - Warnings from `check` or `clippy` are already checked in `check.sh`.
 # - The toolchain used for linting could be slightly different from the one used to
 #   compile RisingWave. Warnings from `rustc` itself may produce false positives.
-DYLINT_RUSTFLAGS="-A warnings D rw_warnings" cargo dylint --all -- --all-targets --all-features --locked
+DYLINT_RUSTFLAGS="-A warnings -D rw_warnings" cargo dylint --all -- --all-targets --all-features --locked
