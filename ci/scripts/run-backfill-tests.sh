88f1abb1a639ce70757f47ba1853643b7577cf18
Noel Kwan <noelkwan1998@gmail.com>
2024-01-23 12:59:43 +0800

    add separate tests for storage throughput

--- ci/scripts/run-backfill-tests.sh
+++ ci/scripts/run-backfill-tests.sh
@@ -24,9 +24,11 @@ COMMON_DIR=$BACKGROUND_DDL_DIR/common
 
 CLUSTER_PROFILE='ci-1cn-1fe-kafka-with-recovery'
 if [[ -n "${BUILDKITE:-}" ]]; then
-  RUNTIME_CLUSTER_PROFILE='ci-3cn-1fe-with-minio-rate-limit'
+  RUNTIME_CLUSTER_PROFILE='ci-3cn-1fe'
+  MINIO_RATE_LIMIT_CLUSTER_PROFILE='ci-3cn-1fe-with-minio-rate-limit'
 else
-  RUNTIME_CLUSTER_PROFILE='ci-3cn-1fe-with-monitoring-and-minio-rate-limit'
+  RUNTIME_CLUSTER_PROFILE='ci-3cn-1fe-with-monitoring'
+  MINIO_RATE_LIMIT_CLUSTER_PROFILE='ci-3cn-1fe-with-monitoring-and-minio-rate-limit'
 fi
 export RUST_LOG="info,risingwave_stream=info,risingwave_batch=info,risingwave_storage=info" \
 
@@ -245,23 +247,39 @@ test_backfill_snapshot_runtime() {
   kill_cluster
 }
 
+# Throttle the storage throughput.
+# Arrangement Backfill should not fail because of this.
+test_backfill_snapshot_with_limited_storage_throughput() {
+  echo "--- e2e, test_backfill_snapshot_runtime"
+  cargo make ci-start $MINIO_RATE_LIMIT_CLUSTER_PROFILE
+  sqllogictest -p 4566 -d dev 'e2e_test/backfill/runtime/create_table.slt'
+  sqllogictest -p 4566 -d dev 'e2e_test/backfill/runtime/insert_snapshot.slt'
+  sqllogictest -p 4566 -d dev 'e2e_test/backfill/runtime/create_arrangement_backfill_mv.slt'
+  sqllogictest -p 4566 -d dev 'e2e_test/backfill/runtime/create_no_shuffle_mv.slt'
+  sqllogictest -p 4566 -d dev 'e2e_test/backfill/runtime/validate_rows_no_shuffle.slt'
+  sqllogictest -p 4566 -d dev 'e2e_test/backfill/runtime/validate_rows_arrangement.slt'
+
+  kill_cluster
+}
+
 main() {
   set -euo pipefail
-#  test_snapshot_and_upstream_read
-#  test_backfill_tombstone
-#  test_replication_with_column_pruning
-#  test_sink_backfill_recovery
+  test_snapshot_and_upstream_read
+  test_backfill_tombstone
+  test_replication_with_column_pruning
+  test_sink_backfill_recovery
 
   # Only if profile is "ci-release", run it.
   if [[ ${profile:-} == "ci-release" ]]; then
     echo "--- Using release profile, running backfill performance tests."
     # Need separate tests, we don't want to backfill concurrently.
     # It's difficult to measure the time taken for each backfill if we do so.
-#    test_no_shuffle_backfill_snapshot_and_upstream_runtime
-#    test_arrangement_backfill_snapshot_and_upstream_runtime
+    test_no_shuffle_backfill_snapshot_and_upstream_runtime
+    test_arrangement_backfill_snapshot_and_upstream_runtime
 
     # Backfill will happen in sequence here.
     test_backfill_snapshot_runtime
+    test_backfill_snapshot_with_limited_storage_throughput
 
     # No upstream only tests, because if there's no snapshot,
     # Backfill will complete almost immediately.
