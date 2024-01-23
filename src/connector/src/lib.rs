3ca818d5dc1dafe7145fade659987e57d2790e13
Bugen Zhao <i@bugenzhao.com>
2024-01-23 16:08:48 +0800

    minor fixes
    Signed-off-by: Bugen Zhao <i@bugenzhao.com>

--- src/connector/src/lib.rs
+++ src/connector/src/lib.rs
@@ -33,7 +33,7 @@
 #![feature(error_generic_member_access)]
 #![feature(register_tool)]
 #![register_tool(rw)]
-// #![allow(rw::format_error)] // TODO(error-handling): need further refactoring
+#![allow(rw::format_error)] // TODO(error-handling): need further refactoring
 
 use std::time::Duration;
 
