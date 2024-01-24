88a4bb42eb7f6e99cc8f183b316db5b332c285b7
Noel Kwan <noelkwan1998@gmail.com>
2024-01-24 09:36:46 +0800

    add arrangement backfill background ddl recovery test

--- ci/scripts/deterministic-recovery-test.sh
+++ ci/scripts/deterministic-recovery-test.sh
@@ -32,16 +32,41 @@ filter_stack_trace_for_all_logs() {
 trap filter_stack_trace_for_all_logs ERR
 
 echo "--- deterministic simulation e2e, ci-3cn-2fe-3meta, recovery, background_ddl"
-seq $TEST_NUM | parallel MADSIM_TEST_SEED={} './risingwave_simulation --kill --kill-rate=${KILL_RATE} ./e2e_test/background_ddl/sim/basic.slt 2> $LOGDIR/recovery-background-ddl-{}.log && rm $LOGDIR/recovery-background-ddl-{}.log'
+seq $TEST_NUM | parallel MADSIM_TEST_SEED={} './risingwave_simulation \
+--kill \
+--kill-rate=${KILL_RATE} \
+--use-arrangement-backfill=${USE_ARRANGEMENT_BACKFILL:-} \
+./e2e_test/background_ddl/sim/basic.slt \
+2> $LOGDIR/recovery-background-ddl-{}.log && rm $LOGDIR/recovery-background-ddl-{}.log'
 
 echo "--- deterministic simulation e2e, ci-3cn-2fe-3meta, recovery, ddl"
-seq $TEST_NUM | parallel MADSIM_TEST_SEED={} './risingwave_simulation --kill --kill-rate=${KILL_RATE} --background-ddl-rate=${BACKGROUND_DDL_RATE} ./e2e_test/ddl/\*\*/\*.slt 2> $LOGDIR/recovery-ddl-{}.log && rm $LOGDIR/recovery-ddl-{}.log'
+seq $TEST_NUM | parallel MADSIM_TEST_SEED={} './risingwave_simulation \
+--kill \
+--kill-rate=${KILL_RATE} \
+--background-ddl-rate=${BACKGROUND_DDL_RATE} \
+--use-arrangement-backfill=${USE_ARRANGEMENT_BACKFILL:-} \
+./e2e_test/ddl/\*\*/\*.slt 2> $LOGDIR/recovery-ddl-{}.log && rm $LOGDIR/recovery-ddl-{}.log'
 
 echo "--- deterministic simulation e2e, ci-3cn-2fe-3meta, recovery, streaming"
-seq $TEST_NUM | parallel MADSIM_TEST_SEED={} './risingwave_simulation --kill --kill-rate=${KILL_RATE} --background-ddl-rate=${BACKGROUND_DDL_RATE} ./e2e_test/streaming/\*\*/\*.slt 2> $LOGDIR/recovery-streaming-{}.log && rm $LOGDIR/recovery-streaming-{}.log'
+seq $TEST_NUM | parallel MADSIM_TEST_SEED={} './risingwave_simulation \
+--kill \
+--kill-rate=${KILL_RATE} \
+--background-ddl-rate=${BACKGROUND_DDL_RATE} \
+--use-arrangement-backfill=${USE_ARRANGEMENT_BACKFILL:-} \
+./e2e_test/streaming/\*\*/\*.slt 2> $LOGDIR/recovery-streaming-{}.log && rm $LOGDIR/recovery-streaming-{}.log'
 
 echo "--- deterministic simulation e2e, ci-3cn-2fe-3meta, recovery, batch"
-seq $TEST_NUM | parallel MADSIM_TEST_SEED={} './risingwave_simulation --kill --kill-rate=${KILL_RATE} --background-ddl-rate=${BACKGROUND_DDL_RATE} ./e2e_test/batch/\*\*/\*.slt 2> $LOGDIR/recovery-batch-{}.log && rm $LOGDIR/recovery-batch-{}.log'
+seq $TEST_NUM | parallel MADSIM_TEST_SEED={} './risingwave_simulation \
+--kill \
+--kill-rate=${KILL_RATE} \
+--background-ddl-rate=${BACKGROUND_DDL_RATE} \
+--use-arrangement-backfill=${USE_ARRANGEMENT_BACKFILL:-} \
+./e2e_test/batch/\*\*/\*.slt 2> $LOGDIR/recovery-batch-{}.log && rm $LOGDIR/recovery-batch-{}.log'
 
 echo "--- deterministic simulation e2e, ci-3cn-2fe-3meta, recovery, kafka source,sink"
-seq $TEST_NUM | parallel MADSIM_TEST_SEED={} './risingwave_simulation --kill --kill-rate=${KILL_RATE} --kafka-datadir=./scripts/source/test_data ./e2e_test/source/basic/kafka\*.slt 2> $LOGDIR/recovery-source-{}.log && rm $LOGDIR/recovery-source-{}.log'
+seq $TEST_NUM | parallel MADSIM_TEST_SEED={} './risingwave_simulation \
+--kill \
+--kill-rate=${KILL_RATE} \
+--kafka-datadir=./scripts/source/test_data \
+--use-arrangement-backfill=${USE_ARRANGEMENT_BACKFILL:-} \
+./e2e_test/source/basic/kafka\*.slt 2> $LOGDIR/recovery-source-{}.log && rm $LOGDIR/recovery-source-{}.log'
