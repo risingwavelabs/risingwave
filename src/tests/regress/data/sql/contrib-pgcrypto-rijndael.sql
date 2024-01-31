f41ee0fd3db8b693c575d00b666caeacf8968c9e
Xiangjin <xiangjin@risingwave-labs.com>
2024-01-31 15:35:36 +0800

    enable pgcrypto aes regress tests except for key padding / iv support

--- src/tests/regress/data/sql/contrib-pgcrypto-rijndael.sql
+++ src/tests/regress/data/sql/contrib-pgcrypto-rijndael.sql
@@ -32,30 +32,30 @@ SELECT encrypt(
 
 -- key padding
 
-SELECT encrypt(
-'\x0011223344',
-'\x000102030405',
-'aes-cbc');
-
-SELECT encrypt(
-'\x0011223344',
-'\x000102030405060708090a0b0c0d0e0f10111213',
-'aes-cbc');
-
-SELECT encrypt(
-'\x0011223344',
-'\x000102030405060708090a0b0c0d0e0f101112131415161718191a1b',
-'aes-cbc');
-
--- empty data
-select encrypt('', 'foo', 'aes');
--- 10 bytes key
-select encrypt('foo', '0123456789', 'aes');
--- 22 bytes key
-select encrypt('foo', '0123456789012345678901', 'aes');
-
--- decrypt
-select encode(decrypt(encrypt('foo', '0123456', 'aes'), '0123456', 'aes'), 'escape');
+--@ SELECT encrypt(
+--@ '\x0011223344',
+--@ '\x000102030405',
+--@ 'aes-cbc');
+--@ 
+--@ SELECT encrypt(
+--@ '\x0011223344',
+--@ '\x000102030405060708090a0b0c0d0e0f10111213',
+--@ 'aes-cbc');
+--@ 
+--@ SELECT encrypt(
+--@ '\x0011223344',
+--@ '\x000102030405060708090a0b0c0d0e0f101112131415161718191a1b',
+--@ 'aes-cbc');
+--@ 
+--@ -- empty data
+--@ select encrypt('', 'foo', 'aes');
+--@ -- 10 bytes key
+--@ select encrypt('foo', '0123456789', 'aes');
+--@ -- 22 bytes key
+--@ select encrypt('foo', '0123456789012345678901', 'aes');
+--@ 
+--@ -- decrypt
+--@ select encode(decrypt(encrypt('foo', '0123456', 'aes'), '0123456', 'aes'), 'escape');
 -- data not multiple of block size
 select encode(decrypt(encrypt('foo', '0123456', 'aes') || '\x00'::bytea, '0123456', 'aes'), 'escape');
 -- bad padding
@@ -64,9 +64,9 @@ select encode(decrypt(encrypt('foo', '0123456', 'aes') || '\x00'::bytea, '012345
 select encode(decrypt_iv('\xa21a9c15231465964e3396d32095e67eb52bab05f556a581621dee1b85385789', '0123456', 'abcd', 'aes'), 'escape');
 
 -- iv
-select encrypt_iv('foo', '0123456', 'abcd', 'aes');
-select encode(decrypt_iv('\x2c24cb7da91d6d5699801268b0f5adad', '0123456', 'abcd', 'aes'), 'escape');
+--@ select encrypt_iv('foo', '0123456', 'abcd', 'aes');
+--@ select encode(decrypt_iv('\x2c24cb7da91d6d5699801268b0f5adad', '0123456', 'abcd', 'aes'), 'escape');
 
 -- long message
-select encrypt('Lets try a longer message.', '0123456789', 'aes');
-select encode(decrypt(encrypt('Lets try a longer message.', '0123456789', 'aes'), '0123456789', 'aes'), 'escape');
+--@ select encrypt('Lets try a longer message.', '0123456789', 'aes');
+--@ select encode(decrypt(encrypt('Lets try a longer message.', '0123456789', 'aes'), '0123456789', 'aes'), 'escape');
