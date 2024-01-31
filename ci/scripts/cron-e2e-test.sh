3f1cfc37ef8f686073eb36c3a8f19c3ad25d257e
Noel Kwan <noelkwan1998@gmail.com>
2024-01-31 12:06:18 +0800

    disable range test

--- ci/scripts/cron-e2e-test.sh
+++ ci/scripts/cron-e2e-test.sh
@@ -6,5 +6,7 @@ set -euo pipefail
 source ci/scripts/common.sh
 export RUN_COMPACTION=0;
 export RUN_META_BACKUP=1;
-export RUN_DELETE_RANGE=1;
+# NOTE(wenym): Delete range is disabled so its tests will fail.
+# So we just disable the delete range test for now.
+export RUN_DELETE_RANGE=0;
 source ci/scripts/run-e2e-test.sh
