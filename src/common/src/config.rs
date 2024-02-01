9749e4c46f57e5c9738abfafe06fd6f8c0a1b8e0
August <pin@singularity-data.com>
2024-02-01 17:43:35 +0800

    fix: initialize backup system parameter for in memory mode (#14921)

--- src/common/src/config.rs
+++ src/common/src/config.rs
@@ -966,10 +966,13 @@ impl SystemConfig {
         // Initialize backup_storage_url and backup_storage_directory if not set.
         if let Some(state_store) = &system_params.state_store
             && let Some(data_directory) = &system_params.data_directory
-            && let Some(hummock_state_store) = state_store.strip_prefix("hummock+")
         {
             if system_params.backup_storage_url.is_none() {
-                system_params.backup_storage_url = Some(hummock_state_store.to_owned());
+                if let Some(hummock_state_store) = state_store.strip_prefix("hummock+") {
+                    system_params.backup_storage_url = Some(hummock_state_store.to_owned());
+                } else {
+                    system_params.backup_storage_url = Some("memory".to_string());
+                }
                 tracing::info!("initialize backup_storage_url based on state_store");
             }
             if system_params.backup_storage_directory.is_none() {
