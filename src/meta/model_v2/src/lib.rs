32762399011b9ce332285e355899d0ede3b2c55f
Shanicky Chen <peng@risingwave-labs.com>
2024-02-01 14:08:27 +0800

    feat: support scaling in sql backend (#14757)
    Signed-off-by: Shanicky Chen <peng@risingwave-labs.com>

--- src/meta/model_v2/src/lib.rs
+++ src/meta/model_v2/src/lib.rs
@@ -227,3 +227,11 @@ derive_from_json_struct!(
     FragmentVnodeMapping,
     risingwave_pb::common::ParallelUnitMapping
 );
+
+#[derive(Clone, Debug, PartialEq, FromJsonQueryResult, Serialize, Deserialize)]
+pub enum StreamingParallelism {
+    Auto,
+    Fixed(usize),
+}
+
+impl Eq for StreamingParallelism {}
