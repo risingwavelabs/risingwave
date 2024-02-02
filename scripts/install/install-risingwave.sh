dc20db054158a59315cc0458ed458a56fc8aa810
Noel Kwan <noelkwan1998@gmail.com>
2024-02-02 11:53:22 +0800

    address cases

--- scripts/install/install-risingwave.sh
+++ scripts/install/install-risingwave.sh
@@ -2,12 +2,17 @@
 
 OS=$(uname -s)
 ARCH=$(uname -m)
-VERSION=$(curl -s https://api.github.com/repos/risingwavelabs/risingwave/releases/latest | jq '.tag_name' -r)
+VERSION=$(curl -s https://api.github.com/repos/risingwavelabs/risingwave/releases/latest | grep '.tag_name' | sed -E -n 's/.*(v[0-9]+.[0-9]+.[0-9])\",/\1/p')
 BASE_URL="https://github.com/risingwavelabs/risingwave/releases/download"
 
 # TODO(kwannoel): Add support for other OS and architectures.
 if [[ "${OS}" == "Linux" && "${ARCH}" == "x86_64" || "${ARCH}" == "amd64" ]]; then
   BASE_ARCHIVE_NAME="risingwave-${VERSION}-x86_64-unknown-linux-all-in-one"
+  ARCHIVE_NAME="${BASE_ARCHIVE_NAME}.tar.gz"
+  URL="${BASE_URL}/${VERSION}/${ARCHIVE_NAME}"
+  USE_BREW=0
+elif [[ "${OS}" == "Darwin" ]] && [[ "${ARCH}" == "x86_64" || "${ARCH}" == "amd64" || "${ARCH}" == "aarch64" || "${ARCH}" == "arm64" ]]; then
+  USE_BREW=1
 else
   echo
   echo "Unsupported OS or Architecture: ${OS}-${ARCH}"
@@ -21,9 +26,24 @@ else
   exit 1
 fi
 
-ARCHIVE_NAME="${BASE_ARCHIVE_NAME}.tar.gz"
-URL="${BASE_URL}/${VERSION}/${ARCHIVE_NAME}"
+############# Setup data directories
+echo "Setting up data directories."
+mkdir -p "${HOME}/.risingwave/data/state_store"
+mkdir -p "${HOME}/.risingwave/data/meta_store"
 
+############# BREW INSTALL
+if [[ "${USE_BREW}" -eq 1 ]]; then
+  echo "Installing RisingWave using Homebrew."
+  brew tap risingwavelabs/risingwave
+  brew install risingwave
+  echo "Successfully installed RisingWave using Homebrew."
+  echo
+  echo "You can run it as:"
+  echo "  risingwave standalone"
+  exit 0
+fi
+
+############# BINARY INSTALL
 echo
 echo "Downloading ${URL} into ${PWD}."
 echo
