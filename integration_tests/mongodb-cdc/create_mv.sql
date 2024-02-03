e4782a51edf4000ca8860da18f2b264fc37b4309
StrikeW <wangsiyuanse@gmail.com>
2024-02-03 16:44:22 +0800

    clean node 2

--- integration_tests/mongodb-cdc/create_mv.sql
+++
@@ -1,7 +0,0 @@
-CREATE MATERIALIZED VIEW normalized_users AS
-SELECT
-    payload ->> 'name' as name,
-    payload ->> 'email' as email,
-    payload ->> 'address' as address
-FROM
-    users;
--- integration_tests/mongodb-cdc/create_source.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE users (_id JSONB PRIMARY KEY, payload JSONB) WITH (
-    connector = 'kafka',
-    kafka.topic = 'dbserver1.random_data.users',
-    kafka.brokers = 'message_queue:29092',
-    kafka.scan.startup.mode = 'earliest'
-) FORMAT DEBEZIUM_MONGO ENCODE JSON;
