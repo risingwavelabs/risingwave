07eef88e8d7749f7c8b9374db1d22f633ab44b16
Runji Wang <wangrunji0408@163.com>
2024-02-04 21:13:54 +0800

    fix recovery test
    Signed-off-by: Runji Wang <wangrunji0408@163.com>

--- ci/scripts/deterministic-recovery-test.sh
+++ ci/scripts/deterministic-recovery-test.sh
@@ -4,6 +4,8 @@
 set -euo pipefail
 
 source ci/scripts/common.sh
+rustup default stable
+rustup +stable target add wasm32-wasi
 
 echo "--- Download artifacts"
 download-and-decompress-artifact risingwave_simulation .
