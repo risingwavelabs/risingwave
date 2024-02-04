e3d408eeafc8a446cd3228a2e6db5178a21f1a6c
August <pin@singularity-data.com>
2024-02-04 19:09:24 +0800

    fix: fix some corner cases in alter rename schema in sql backend

--- src/frontend/src/observer/observer_manager.rs
+++ src/frontend/src/observer/observer_manager.rs
@@ -229,6 +229,7 @@ impl FrontendObserverNode {
                     let Some(relation) = relation.relation_info.as_ref() else {
                         continue;
                     };
+                    println!("heiheihei: {:?}", relation);
                     match relation {
                         RelationInfo::Table(table) => match resp.operation() {
                             Operation::Add => catalog_guard.create_table(table),
