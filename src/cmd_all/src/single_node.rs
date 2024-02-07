0142bdc05a22fc525040b8deb55a5ae4a1553de9
Noel Kwan <noelkwan1998@gmail.com>
2024-02-07 19:10:41 +0800

    add test

--- src/cmd_all/src/single_node.rs
+++ src/cmd_all/src/single_node.rs
@@ -390,6 +390,7 @@ mod test {
     #[test]
     fn test_parse_extra_opts() {
         let raw_opts = "
+--meta-addr=127.0.0.1:8888
 --meta-extra-opts=--advertise-addr 127.0.0.1:9999 --data-directory \"some path with spaces\" \
 --state-store hummock+fs:///root/.risingwave/state_store \
 --sql-endpoint sqlite:///root/.risingwave/meta_store/single_node.db?mode=rwc
@@ -403,7 +404,9 @@ mod test {
                     prometheus_listener_addr: None,
                     config_path: None,
                     store_directory: None,
-                    meta_addr: None,
+                    meta_addr: Some(
+                        "127.0.0.1:8888",
+                    ),
                     compute_addr: None,
                     frontend_addr: None,
                     compactor_addr: None,
@@ -415,6 +418,7 @@ mod test {
                     ),
                     frontend_extra_opts: None,
                     compactor_extra_opts: None,
+                    dump_opts: false,
                 }"#]],
         );
         let actual_parsed = normalize_single_node_opts(&actual).unwrap();
@@ -502,7 +506,7 @@ mod test {
                             port: None,
                             meta_addr: List(
                                 [
-                                    http://0.0.0.0:5690/,
+                                    127.0.0.1:8888,
                                 ],
                             ),
                             prometheus_listener_addr: "0.0.0.0:1250",
@@ -522,7 +526,7 @@ mod test {
                             prometheus_listener_addr: "0.0.0.0:1250",
                             meta_address: List(
                                 [
-                                    http://0.0.0.0:5690/,
+                                    127.0.0.1:8888,
                                 ],
                             ),
                             compaction_worker_threads_number: None,
