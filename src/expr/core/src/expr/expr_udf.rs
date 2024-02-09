347fa58464d845b0d3839cb9901248fc3efca0e0
Kexiang Wang <kx.wang@hotmail.com>
2024-02-09 00:59:43 -0500

    set default number to 10s

--- src/expr/core/src/expr/expr_udf.rs
+++ src/expr/core/src/expr/expr_udf.rs
@@ -201,7 +201,7 @@ impl Build for UdfExpression {
             span: format!("expr_udf_call ({})", udf.identifier).into(),
             disable_retry_count: AtomicU8::new(0),
             timeout: match udf.timeout_ms {
-                0 => None,
+                0 => Some(Duration::from_millis(10000 as u64)),
                 ms => Some(Duration::from_millis(ms as u64)),
             },
         })
