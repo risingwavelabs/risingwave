61081e471e6ab4c92a6d6c73fcb64801d33ebe0b
August <pin@singularity-data.com>
2024-02-22 16:47:12 +0800

    fix

--- src/meta/src/barrier/recovery.rs
+++ src/meta/src/barrier/recovery.rs
@@ -192,7 +192,7 @@ impl GlobalBarrierManagerContext {
             tokio::spawn(async move {
                 let res: MetaResult<()> = try {
                     tracing::debug!("recovering stream job {}", table.id);
-                    finished.await.context("failed to finish command")??;
+                    finished.await.ok().context("failed to finish command")??;
 
                     tracing::debug!("finished stream job {}", table.id);
                     // Once notified that job is finished we need to notify frontend.
