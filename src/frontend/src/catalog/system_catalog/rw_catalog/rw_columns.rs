8bb1650af608bf7dd3fdeb4054b9450751eb27cf
Eric Fu <eric@singularity-data.com>
2024-02-23 17:04:14 +0800

    show PKs in pg_constraint & refactor rw_columns

--- src/frontend/src/catalog/system_catalog/rw_catalog/rw_columns.rs
+++ src/frontend/src/catalog/system_catalog/rw_catalog/rw_columns.rs
@@ -15,6 +15,7 @@
 use risingwave_common::types::Fields;
 use risingwave_frontend_macro::system_catalog;
 
+use crate::catalog::schema_catalog::SchemaCatalog;
 use crate::catalog::system_catalog::SysCatalogReaderImpl;
 use crate::error::Result;
 
@@ -39,112 +40,108 @@ fn read_rw_columns(reader: &SysCatalogReaderImpl) -> Result<Vec<RwColumn>> {
     let schemas = catalog_reader.iter_schemas(&reader.auth_context.database)?;
 
     Ok(schemas
-        .flat_map(|schema| {
-            let view_rows = schema.iter_view().flat_map(|view| {
-                view.columns
-                    .iter()
-                    .enumerate()
-                    .map(|(index, column)| RwColumn {
-                        relation_id: view.id as i32,
-                        name: column.name.clone(),
-                        position: index as i32 + 1,
-                        is_hidden: false,
-                        is_primary_key: false,
-                        is_distribution_key: false,
-                        data_type: column.data_type().to_string(),
-                        type_oid: column.data_type().to_oid(),
-                        type_len: column.data_type().type_len(),
-                        udt_type: column.data_type().pg_name().into(),
-                    })
-            });
-
-            let sink_rows = schema
-                .iter_sink()
-                .flat_map(|sink| {
-                    sink.full_columns()
-                        .iter()
-                        .enumerate()
-                        .map(|(index, column)| RwColumn {
-                            relation_id: sink.id.sink_id as i32,
-                            name: column.name().into(),
-                            position: index as i32 + 1,
-                            is_hidden: column.is_hidden,
-                            is_primary_key: sink.downstream_pk.contains(&index),
-                            is_distribution_key: sink.distribution_key.contains(&index),
-                            data_type: column.data_type().to_string(),
-                            type_oid: column.data_type().to_oid(),
-                            type_len: column.data_type().type_len(),
-                            udt_type: column.data_type().pg_name().into(),
-                        })
-                })
-                .chain(view_rows);
-
-            let catalog_rows = schema
-                .iter_system_tables()
-                .flat_map(|table| {
-                    table
-                        .columns
-                        .iter()
-                        .enumerate()
-                        .map(move |(index, column)| RwColumn {
-                            relation_id: table.id.table_id as i32,
-                            name: column.name().into(),
-                            position: index as i32 + 1,
-                            is_hidden: column.is_hidden,
-                            is_primary_key: table.pk.contains(&index),
-                            is_distribution_key: false,
-                            data_type: column.data_type().to_string(),
-                            type_oid: column.data_type().to_oid(),
-                            type_len: column.data_type().type_len(),
-                            udt_type: column.data_type().pg_name().into(),
-                        })
-                })
-                .chain(sink_rows);
-
-            let table_rows = schema
-                .iter_valid_table()
-                .flat_map(|table| {
-                    table
-                        .columns
-                        .iter()
-                        .enumerate()
-                        .map(move |(index, column)| RwColumn {
-                            relation_id: table.id.table_id as i32,
-                            name: column.name().into(),
-                            position: index as i32 + 1,
-                            is_hidden: column.is_hidden,
-                            is_primary_key: table.pk().iter().any(|idx| idx.column_index == index),
-                            is_distribution_key: table.distribution_key.contains(&index),
-                            data_type: column.data_type().to_string(),
-                            type_oid: column.data_type().to_oid(),
-                            type_len: column.data_type().type_len(),
-                            udt_type: column.data_type().pg_name().into(),
-                        })
-                })
-                .chain(catalog_rows);
-
-            // source columns
-            schema
-                .iter_source()
-                .flat_map(|source| {
-                    source
-                        .columns
-                        .iter()
-                        .enumerate()
-                        .map(move |(index, column)| RwColumn {
-                            relation_id: source.id as i32,
-                            name: column.name().into(),
-                            position: index as i32 + 1,
-                            is_hidden: column.is_hidden,
-                            is_primary_key: source.pk_col_ids.contains(&column.column_id()),
-                            is_distribution_key: false,
-                            data_type: column.data_type().to_string(),
-                            type_oid: column.data_type().to_oid(),
-                            type_len: column.data_type().type_len(),
-                            udt_type: column.data_type().pg_name().into(),
-                        })
-                })
-                .chain(table_rows)
-        })
+        .flat_map(|schema| read_rw_columns_in_schema(schema))
         .collect())
+}
+
+fn read_rw_columns_in_schema(schema: &SchemaCatalog) -> Vec<RwColumn> {
+    let view_rows = schema.iter_view().flat_map(|view| {
+        view.columns
+            .iter()
+            .enumerate()
+            .map(|(index, column)| RwColumn {
+                relation_id: view.id as i32,
+                name: column.name.clone(),
+                position: index as i32 + 1,
+                is_hidden: false,
+                is_primary_key: false,
+                is_distribution_key: false,
+                data_type: column.data_type().to_string(),
+                type_oid: column.data_type().to_oid(),
+                type_len: column.data_type().type_len(),
+                udt_type: column.data_type().pg_name().into(),
+            })
+    });
+
+    let sink_rows = schema.iter_sink().flat_map(|sink| {
+        sink.full_columns()
+            .iter()
+            .enumerate()
+            .map(|(index, column)| RwColumn {
+                relation_id: sink.id.sink_id as i32,
+                name: column.name().into(),
+                position: index as i32 + 1,
+                is_hidden: column.is_hidden,
+                is_primary_key: sink.downstream_pk.contains(&index),
+                is_distribution_key: sink.distribution_key.contains(&index),
+                data_type: column.data_type().to_string(),
+                type_oid: column.data_type().to_oid(),
+                type_len: column.data_type().type_len(),
+                udt_type: column.data_type().pg_name().into(),
+            })
+    });
+
+    let catalog_rows = schema.iter_system_tables().flat_map(|table| {
+        table
+            .columns
+            .iter()
+            .enumerate()
+            .map(move |(index, column)| RwColumn {
+                relation_id: table.id.table_id as i32,
+                name: column.name().into(),
+                position: index as i32 + 1,
+                is_hidden: column.is_hidden,
+                is_primary_key: table.pk.contains(&index),
+                is_distribution_key: false,
+                data_type: column.data_type().to_string(),
+                type_oid: column.data_type().to_oid(),
+                type_len: column.data_type().type_len(),
+                udt_type: column.data_type().pg_name().into(),
+            })
+    });
+
+    let table_rows = schema.iter_valid_table().flat_map(|table| {
+        table
+            .columns
+            .iter()
+            .enumerate()
+            .map(move |(index, column)| RwColumn {
+                relation_id: table.id.table_id as i32,
+                name: column.name().into(),
+                position: index as i32 + 1,
+                is_hidden: column.is_hidden,
+                is_primary_key: table.pk().iter().any(|idx| idx.column_index == index),
+                is_distribution_key: table.distribution_key.contains(&index),
+                data_type: column.data_type().to_string(),
+                type_oid: column.data_type().to_oid(),
+                type_len: column.data_type().type_len(),
+                udt_type: column.data_type().pg_name().into(),
+            })
+    });
+
+    let schema_rows = schema.iter_source().flat_map(|source| {
+        source
+            .columns
+            .iter()
+            .enumerate()
+            .map(move |(index, column)| RwColumn {
+                relation_id: source.id as i32,
+                name: column.name().into(),
+                position: index as i32 + 1,
+                is_hidden: column.is_hidden,
+                is_primary_key: source.pk_col_ids.contains(&column.column_id()),
+                is_distribution_key: false,
+                data_type: column.data_type().to_string(),
+                type_oid: column.data_type().to_oid(),
+                type_len: column.data_type().type_len(),
+                udt_type: column.data_type().pg_name().into(),
+            })
+    });
+
+    return view_rows
+        .chain(sink_rows)
+        .chain(catalog_rows)
+        .chain(table_rows)
+        .chain(schema_rows)
+        .collect();
 }
