b4dbe30d35f6f613fd182666634c9c276738d93a
Zihao Xu <xzhseh@gmail.com>
2024-02-27 01:32:32 +0800

    add basic simplify rule

--- src/frontend/src/optimizer/rule/stream/simplify_filter_expression_rule.rs
+++ src/frontend/src/optimizer/rule/stream/simplify_filter_expression_rule.rs
@@ -16,25 +16,91 @@ use risingwave_common::types::DataType;
 use risingwave_pb::plan_common::JoinType;
 
 use crate::expr::{
-    try_derive_watermark, ExprRewriter, FunctionCall, InputRef, WatermarkDerivation,
+    Expr, ExprImpl, ExprRewriter, FunctionCall, InputRef, WatermarkDerivation
 };
-use crate::optimizer::plan_node::generic::GenericPlanRef;
-use crate::optimizer::plan_node::{LogicalFilter, LogicalJoin, LogicalNow};
+use crate::expr::{ExprDisplay, ExprType, ExprVisitor, ImpureAnalyzer};
+use crate::optimizer::plan_node::{ExprRewritable, LogicalFilter, LogicalJoin, LogicalNow, LogicalShare, PlanTreeNodeUnary};
 use crate::optimizer::rule::{BoxedRule, Rule};
 use crate::optimizer::PlanRef;
-use crate::utils::Condition;
 
 pub struct SimplifyFilterExpressionRule {}
 impl Rule for SimplifyFilterExpressionRule {
     fn apply(&self, plan: PlanRef) -> Option<PlanRef> {
         let filter: &LogicalFilter = plan.as_logical_filter()?;
         println!("filter: {:#?}", filter);
-        Some(plan)
+        let mut rewriter = SimplifyFilterExpressionRewriter {};
+        let logical_share_plan = filter.input();
+        let share: &LogicalShare = logical_share_plan.as_logical_share()?;
+        let input = share.input().rewrite_exprs(&mut rewriter);
+        let share = LogicalShare::new(input);
+        Some(LogicalFilter::create(share.into(), filter.predicate().clone()))
     }
 }
 
 impl SimplifyFilterExpressionRule {
     pub fn create() -> BoxedRule {
         Box::new(SimplifyFilterExpressionRule {})
+    }
+}
+
+/// If ever `Not (e)` and `(e)` appear together
+fn check_pattern(e1: ExprImpl, e2: ExprImpl) -> bool {
+    println!("e1: {:#?}", e1);
+    println!("e2: {:#?}", e2);
+    let ExprImpl::FunctionCall(e1_func) = e1.clone() else {
+        return false;
+    };
+    let ExprImpl::FunctionCall(e2_func) = e2.clone() else {
+        return false;
+    };
+    if e1_func.func_type() != ExprType::Not && e2_func.func_type() != ExprType::Not {
+        return false;
+    }
+    if e1_func.func_type() != ExprType::Not {
+        if e2_func.inputs().len() != 1 {
+            println!("1");
+            return false;
+        }
+            println!("2");
+        e1 == e2_func.inputs()[0].clone()
+    } else {
+        if e1_func.inputs().len() != 1 {
+            println!("3");
+            return false;
+        }
+            println!("4");
+        e2 == e1_func.inputs()[0].clone()
+    }
+}
+
+struct SimplifyFilterExpressionRewriter {}
+
+impl ExprRewriter for SimplifyFilterExpressionRewriter {
+    /// The pattern we aim to optimize, e.g.,
+    /// 1. (NOT (e)) OR (e) => True | (NOT (e)) AND (e) => False
+    /// 2. (NOT (e1) AND NOT (e2)) OR (e1 OR e2) => True # TODO
+    fn rewrite_expr(&mut self, expr: ExprImpl) -> ExprImpl {
+        println!("expr: {:#?}", expr);
+        let ExprImpl::FunctionCall(func_call) = expr.clone() else {
+            return expr;
+        };
+        if func_call.func_type() != ExprType::Or && func_call.func_type() != ExprType::And {
+            return expr;
+        }
+        assert_eq!(func_call.return_type(), DataType::Boolean);
+        // Currently just optimize the first rule
+        if func_call.inputs().len() != 2 {
+            return expr;
+        }
+        let inputs = func_call.inputs();
+        if check_pattern(inputs[0].clone(), inputs[1].clone()) {
+            match func_call.func_type() {
+                ExprType::Or => ExprImpl::literal_bool(true),
+                ExprType::And => ExprImpl::literal_bool(false),
+                _ => expr,
+            }
+        } else {
+            expr
+        }
     }
 }
