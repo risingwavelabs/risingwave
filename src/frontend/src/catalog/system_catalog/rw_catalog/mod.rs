808ce788bdcdd32e8ce3f40eb72a591de9dda6fe
William Wen <william123.wen@gmail.com>
2024-02-28 18:50:27 +0800

    feat(frontend): support system table to query storage table watermark

--- src/frontend/src/catalog/system_catalog/rw_catalog/mod.rs
+++ src/frontend/src/catalog/system_catalog/rw_catalog/mod.rs
@@ -12,6 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+mod rw_actor_infos;
 mod rw_actors;
 mod rw_columns;
 mod rw_connections;
---
+++ src/frontend/src/catalog/system_catalog/rw_catalog/rw_actor_infos.rs
@@ -0,0 +1,55 @@
+// Copyright 2024 RisingWave Labs
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+use itertools::Itertools;
+use risingwave_common::types::Fields;
+use risingwave_frontend_macro::system_catalog;
+
+use crate::catalog::system_catalog::SysCatalogReaderImpl;
+use crate::error::Result;
+
+#[derive(Fields)]
+struct RwActorInfo {
+    #[primary_key]
+    actor_id: i32,
+    fragment_id: i32,
+    node: String,
+    dispatcher: String,
+}
+
+#[system_catalog(table, "rw_catalog.rw_actor_infos")]
+async fn read_rw_actors(reader: &SysCatalogReaderImpl) -> Result<Vec<RwActorInfo>> {
+    let table_ids = reader
+        .meta_client
+        .list_table_fragment_states()
+        .await?
+        .into_iter()
+        .map(|fragment| fragment.table_id)
+        .collect_vec();
+    let table_fragments = reader.meta_client.list_table_fragments(&table_ids).await?;
+    Ok(table_fragments
+        .into_iter()
+        .flat_map(|(_, fragment_info)| {
+            fragment_info.fragments.into_iter().flat_map(|fragment| {
+                let fragment_id = fragment.id;
+                fragment.actors.into_iter().map(move |actor| RwActorInfo {
+                    actor_id: actor.id as _,
+                    fragment_id: fragment_id as _,
+                    node: serde_json::to_string(&actor.node).unwrap(),
+                    dispatcher: serde_json::to_string(&actor.dispatcher).unwrap(),
+                })
+            })
+        })
+        .collect())
+}
