5e64e620cf2a71151059fb71176ac013de43a193
Renjie Liu <liurenjie2008@gmail.com>
2024-02-28 16:03:06 +0800

    ci: Fix tests

--- integration_tests/iceberg-sink2/run.sh
+++ integration_tests/iceberg-sink2/run.sh
@@ -1,3 +1,10 @@
+#!/usr/bin/env bash
+
+# Exits as soon as any line fails.
+set -euo pipefail
+
+curl -sSL https://install.python-poetry.org | python3 -
+
 # Don't remove the `--quiet` option since poetry has a bug when printing output, see
 # https://github.com/python-poetry/poetry/issues/3412
 "$HOME"/.local/bin/poetry update --quiet
