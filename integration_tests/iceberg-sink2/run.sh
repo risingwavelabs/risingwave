28fa30c9ee0d68ffe8b4e634ff731cb6328c2dbe
Renjie Liu <liurenjie2008@gmail.com>
2024-03-01 09:12:35 +0800

    Fix

--- integration_tests/iceberg-sink2/run.sh
+++ integration_tests/iceberg-sink2/run.sh
@@ -3,7 +3,14 @@
 # Exits as soon as any line fails.
 set -euo pipefail
 
+# Install poetry
+
+export POETRY_HOME=/opt/poetry
+python3 -m venv $POETRY_HOME
+$POETRY_HOME/bin/pip install poetry==1.8.0
+$POETRY_HOME/bin/poetry --version
+
 # Don't remove the `--quiet` option since poetry has a bug when printing output, see
 # https://github.com/python-poetry/poetry/issues/3412
-/opt/poetry/bin/poetry update --quiet
-/opt/poetry/bin/poetry run python main.py
+$POETRY_HOME/bin/poetry update --quiet
+$POETRY_HOME/bin/poetry run python main.py
