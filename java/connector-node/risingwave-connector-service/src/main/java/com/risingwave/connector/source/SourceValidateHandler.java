37c87f35780174df2b21020adf39e119bc6506c8
Eric Fu <eric@singularity-data.com>
2024-03-04 16:27:21 +0800

    fix(cdc-connector): allow empty passwords

--- java/connector-node/risingwave-connector-service/src/main/java/com/risingwave/connector/source/SourceValidateHandler.java
+++ java/connector-node/risingwave-connector-service/src/main/java/com/risingwave/connector/source/SourceValidateHandler.java
@@ -65,7 +65,7 @@ public class SourceValidateHandler {
     private static void ensurePropNotBlank(Map<String, String> props, String name) {
         if (StringUtils.isBlank(props.get(name))) {
             throw ValidatorUtils.invalidArgument(
-                    String.format("'%s' not found, please check the WITH properties", name));
+                    String.format("'%s' cannot be empty, please check the WITH properties", name));
         }
     }
 
@@ -74,7 +74,6 @@ public class SourceValidateHandler {
         ensurePropNotBlank(props, DbzConnectorConfig.PORT);
         ensurePropNotBlank(props, DbzConnectorConfig.DB_NAME);
         ensurePropNotBlank(props, DbzConnectorConfig.USER);
-        ensurePropNotBlank(props, DbzConnectorConfig.PASSWORD);
         // ensure table name is passed by user in non-sharing mode
         if (!isMultiTableShared) {
             ensurePropNotBlank(props, DbzConnectorConfig.TABLE_NAME);
