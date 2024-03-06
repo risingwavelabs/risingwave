de217fdbc48a3e10a43a03ff5c2e5e79b2b791b7
xxchan <xxchan22f@gmail.com>
2024-03-06 10:59:53 +0800

    reuse trad_souce

--- src/stream/src/from_proto/source_backfill.rs
+++ src/stream/src/from_proto/source_backfill.rs
@@ -38,67 +38,16 @@ impl ExecutorBuilder for KafkaBackfillExecutorBuilder {
         node: &Self::Node,
         store: impl StateStore,
     ) -> StreamResult<Executor> {
-        let [input]: [_; 1] = params.input.try_into().unwrap();
-
-        // let (sender, barrier_receiver) = unbounded_channel();
-        // stream
-        //     .context
-        //     .barrier_manager()
-        //     .register_sender(params.actor_context.id, sender);
-        let system_params = params.env.system_params_manager_ref().get_params();
-
         let source_id = TableId::new(node.source_id);
         let source_name = node.source_name.clone();
         let source_info = node.get_info()?;
 
-        let mut source_columns = node.columns.clone();
-
-        {
-            // compatible code: introduced in https://github.com/risingwavelabs/risingwave/pull/13707
-            // for upsert and (avro | protobuf) overwrite the `_rw_key` column's ColumnDesc.additional_column_type to Key
-            if source_info.format() == FormatType::Upsert
-                && (source_info.row_encode() == PbEncodeType::Avro
-                    || source_info.row_encode() == PbEncodeType::Protobuf)
-            {
-                let _ = source_columns.iter_mut().map(|c| {
-                    let _ = c.column_desc.as_mut().map(|desc| {
-                        let is_bytea = desc
-                            .get_column_type()
-                            .map(|col_type| col_type.type_name == PbTypeName::Bytea as i32)
-                            .unwrap();
-                        if desc.name == default_key_column_name_version_mapping(
-                            &desc.version()
-                        )
-                            && is_bytea
-                            // the column is from a legacy version
-                            && desc.version == ColumnDescVersion::Unspecified as i32
-                        {
-                            desc.additional_column = Some(AdditionalColumn {
-                                column_type: Some(ColumnType::Key(AdditionalColumnKey {})),
-                            });
-                        }
-                    });
-                });
-            }
-        }
-        let source_desc_builder = SourceDescBuilder::new(
-            source_columns.clone(),
-            params.env.source_metrics(),
-            node.row_id_index.map(|x| x as _),
-            node.with_properties.clone(),
+        let source_desc_builder = super::source::create_source_desc_builder(
+            node.columns.clone(),
+            &params,
             source_info.clone(),
-            params.env.connector_params(),
-            params.env.config().developer.connector_message_buffer_size,
-            // `pk_indices` is used to ensure that a message will be skipped instead of parsed
-            // with null pk when the pk column is missing.
-            //
-            // Currently pk_indices for source is always empty since pk information is not
-            // passed via `StreamSource` so null pk may be emitted to downstream.
-            //
-            // TODO: use the correct information to fill in pk_dicies.
-            // We should consdier add back the "pk_column_ids" field removed by #8841 in
-            // StreamSource
-            params.info.pk_indices.clone(),
+            node.row_id_index,
+            node.with_properties.clone(),
         );
 
         let source_ctrl_opts = SourceCtrlOpts {
@@ -106,9 +55,10 @@ impl ExecutorBuilder for KafkaBackfillExecutorBuilder {
             rate_limit: None,
         };
 
-        let source_column_ids: Vec<_> = source_columns
+        let source_column_ids: Vec<_> = source_desc_builder
+            .column_catalogs_to_source_column_descs()
             .iter()
-            .map(|column| ColumnId::from(column.get_column_desc().unwrap().column_id))
+            .map(|column| column.column_id)
             .collect();
 
         // FIXME: remove this. It's wrong
@@ -135,12 +85,13 @@ impl ExecutorBuilder for KafkaBackfillExecutorBuilder {
             params.info.clone(),
             stream_source_core,
             params.executor_stats.clone(),
-            // barrier_receiver,
-            system_params,
+            params.env.system_params_manager_ref().get_params(),
             source_ctrl_opts.clone(),
             params.env.connector_params(),
             backfill_state_table,
         );
+        let [input]: [_; 1] = params.input.try_into().unwrap();
+
         Ok((
             params.info,
             KafkaBackfillExecutor { inner: exec, input }.boxed(),
