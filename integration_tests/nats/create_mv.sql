b58ae7cd6cfd2582aa2f21d57e25406bbe1260a8
Tao Wu <wutao@singularity-data.com>
2024-03-06 17:24:28 +0800

    fix nats integration test

---
+++ integration_tests/nats/create_mv.sql
@@ -0,0 +1,32 @@
+CREATE TABLE
+  personnel (id integer, name varchar);
+
+
+CREATE SINK nats_sink
+FROM
+  personnel
+WITH
+  (
+    connector = 'nats',
+    server_url = 'nats-server:4222',
+    subject = 'subject1',
+    type = 'append-only',
+    force_append_only = 'true',
+    connect_mode = 'plain'
+  );
+
+
+INSERT INTO
+  personnel
+VALUES
+  (1, 'Alice'),
+  (2, 'Bob'),
+  (3, 'Tom'),
+  (4, 'Jerry'),
+  (5, 'Araminta'),
+  (6, 'Clover'),
+  (7, 'Posey'),
+  (8, 'Waverly');
+
+
+FLUSH;
