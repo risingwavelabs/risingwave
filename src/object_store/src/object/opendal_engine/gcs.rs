d6c9947f0c96cbe994e3d60033bde6c7c4679f2d
zwang28 <84491488@qq.com>
2024-03-11 17:05:39 +0800

    set opendal retry layer and timeout layer

--- src/object_store/src/object/opendal_engine/gcs.rs
+++ src/object_store/src/object/opendal_engine/gcs.rs
@@ -12,7 +12,10 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-use opendal::layers::{LoggingLayer, RetryLayer};
+use std::env;
+use std::time::Duration;
+
+use opendal::layers::{LoggingLayer, RetryLayer, TimeoutLayer};
 use opendal::services::Gcs;
 use opendal::Operator;
 
@@ -34,9 +37,39 @@ impl OpendalObjectStore {
         if let Ok(cred) = cred {
             builder.credential(&cred);
         }
+        let op_timeout = env::var("RW_OPENDAL_TIMEOUT")
+            .unwrap_or("60".into())
+            .parse()
+            .unwrap();
+        let op_speed = env::var("RW_OPENDAL_SPEED")
+            .unwrap_or("1048576".into())
+            .parse()
+            .unwrap();
+        let op_retry_min_delay = env::var("RW_OPENDAL_RETRY_MIN_DELAY")
+            .unwrap_or("1".into())
+            .parse()
+            .unwrap();
+        let op_retry_max_delay = env::var("RW_OPENDAL_RETRY_MAX_DELAY")
+            .unwrap_or("5".into())
+            .parse()
+            .unwrap();
+        let op_retry_max_times = env::var("RW_OPENDAL_RETRY_MAX_TIMES")
+            .unwrap_or("10".into())
+            .parse()
+            .unwrap();
         let op: Operator = Operator::new(builder)?
             .layer(LoggingLayer::default())
-            .layer(RetryLayer::default())
+            .layer(
+                RetryLayer::default()
+                    .with_min_delay(Duration::from_secs(op_retry_min_delay))
+                    .with_max_delay(Duration::from_secs(op_retry_max_delay))
+                    .with_max_times(op_retry_max_times),
+            )
+            .layer(
+                TimeoutLayer::default()
+                    .with_timeout(Duration::from_secs(op_timeout))
+                    .with_speed(op_speed),
+            )
             .finish();
         Ok(Self {
             op,
