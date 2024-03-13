92f77581902b155a2cc8f84d99e6d2dedc73120a
Bugen Zhao <i@bugenzhao.com>
2024-03-13 15:54:31 +0800

    add info log
    Signed-off-by: Bugen Zhao <i@bugenzhao.com>

--- src/meta/dashboard/examples/serve.rs
+++ src/meta/dashboard/examples/serve.rs
@@ -2,6 +2,8 @@ use risingwave_meta_dashboard::router;
 
 #[tokio::main]
 async fn main() {
+    tracing_subscriber::fmt::init();
+
     axum::Server::bind(&"0.0.0.0:10188".parse().unwrap())
         .serve(router().into_make_service())
         .await
--- src/meta/dashboard/src/lib.rs
+++ src/meta/dashboard/src/lib.rs
@@ -5,8 +5,10 @@ mod proxy;
 
 pub fn router() -> Router {
     if cfg!(dashboard_built) {
+        tracing::info!("using embedded dashboard assets");
         embed::router()
     } else {
+        tracing::info!("using proxied dashboard assets");
         proxy::router()
     }
 }
