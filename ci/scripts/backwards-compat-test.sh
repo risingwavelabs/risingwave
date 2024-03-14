d113880b2e1aa010074f65266e7304ad334ab398
Noel Kwan <noelkwan1998@gmail.com>
2024-03-14 12:19:06 +0800

    mention pr

--- ci/scripts/backwards-compat-test.sh
+++ ci/scripts/backwards-compat-test.sh
@@ -67,6 +67,7 @@ ENABLE_BUILD_RUST=false
 ENABLE_RELEASE_PROFILE=false
 EOF
 
+# See https://github.com/risingwavelabs/risingwave/pull/15448
 if version_le "${VERSION:-}" "1.8.0" ; then
   echo "ENABLE_ALL_IN_ONE=true" >> risedev-components.user.env
 fi
