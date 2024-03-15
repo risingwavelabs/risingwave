666f2ca1b5c848775aa0faf9ee3ab2c008dc69cf
rentianyue-jk <rentianyue-jk@360shuke.com>
2024-03-15 14:33:51 +0800

    fix lint error

--- src/object_store/src/object/s3.rs
+++ src/object_store/src/object/s3.rs
@@ -519,7 +519,11 @@ impl ObjectStore for S3ObjectStore {
             }
         }
         if !all_errors.is_empty() {
-            return Err(ObjectError::internal(format!("DeleteObjects request returned exception for some objects: {:?}", all_errors)));
+            return Err(ObjectError::internal(
+                format!(
+                    "DeleteObjects request returned exception for some objects: {:?}",
+                    all_errors
+                )));
         }
 
         Ok(())
