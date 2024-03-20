1f8978886a9ee4b9d16058f04861c03420eed281
zwang28 <70626450+zwang28@users.noreply.github.com>
2024-03-20 11:03:07 +0800

    refactor(backup): restore object store after meta store (#15795)

--- src/meta/src/backup_restore/restore.rs
+++ src/meta/src/backup_restore/restore.rs
@@ -176,13 +176,14 @@ async fn dispatch<L: Loader<S>, W: Writer<S>, S: Metadata>(
     if opts.dry_run {
         return Ok(());
     }
+    let hummock_version = target_snapshot.metadata.hummock_version_ref().clone();
+    writer.write(target_snapshot).await?;
     restore_hummock_version(
         &opts.hummock_storage_url,
         &opts.hummock_storage_directory,
-        target_snapshot.metadata.hummock_version_ref(),
+        &hummock_version,
     )
     .await?;
-    writer.write(target_snapshot).await?;
     Ok(())
 }
 
