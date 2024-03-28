27b01d624b86e946309a3195f293a9075e599e60
xfz <xfz2000x@gmail.com>
2024-03-28 22:23:28 +0800

    fix: add volumes for doris

--- integration_tests/doris-sink/prepare.sh
+++ integration_tests/doris-sink/prepare.sh
@@ -3,4 +3,4 @@
 set -euo pipefail
 
 # setup doris
-docker compose exec doris bash -c "mysql -uroot -P9030 -h127.0.0.1 < doris_prepare.sql"
+docker compose exec doris bash -c "mysql -uroot -P9030 -h127.0.0.1 < /doris_prepare.sql"
