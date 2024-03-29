0346e217c11e49a27a6ffe8d38618a5871fc1dd2
Patrick Huang <hzxa21@hotmail.com>
2024-03-29 08:51:17 +0000

    fix: donot use opendal for s3 by default

--- src/common/src/config.rs
+++ src/common/src/config.rs
@@ -1721,7 +1721,7 @@ pub mod default {
             }
 
             pub mod developer {
-                use crate::util::env_var::env_var_is_false_or;
+                use crate::util::env_var::env_var_is_true_or;
                 const RW_USE_OPENDAL_FOR_S3: &str = "RW_USE_OPENDAL_FOR_S3";
 
                 pub fn object_store_retry_unknown_service_error() -> bool {
@@ -1737,7 +1737,7 @@ pub mod default {
                     // The reason why we use !env_var_is_false_or(RW_USE_OPENDAL_FOR_S3, false) here is
                     // 1. Maintain compatibility so that there is no behavior change in cluster with RW_USE_OPENDAL_FOR_S3 set.
                     // 2. Change the default behavior to use opendal for s3 if RW_USE_OPENDAL_FOR_S3 is not set.
-                    !env_var_is_false_or(RW_USE_OPENDAL_FOR_S3, false)
+                    env_var_is_true_or(RW_USE_OPENDAL_FOR_S3, false)
                 }
             }
         }
