031d6674a7f52759c5cc4f10f32db6843e843409
xfz <xfz2000x@gmail.com>
2024-03-29 18:42:29 +0800

    test: add jsonb column

--- ci/scripts/e2e-doris-sink-test.sh
+++ ci/scripts/e2e-doris-sink-test.sh
@@ -31,7 +31,7 @@ echo "--- create doris table"
 apt-get update -y && apt-get install -y mysql-client
 sleep 2
 mysql -uroot -P 9030 -h doris-server -e "CREATE database demo;use demo;
-CREATE table demo_bhv_table(v1 int,v2 smallint,v3 bigint,v4 float,v5 double,v6 string,v7 datev2,v8 datetime,v9 boolean) UNIQUE KEY(\`v1\`)
+CREATE table demo_bhv_table(v1 int,v2 smallint,v3 bigint,v4 float,v5 double,v6 string,v7 datev2,v8 datetime,v9 boolean,v10 json) UNIQUE KEY(\`v1\`)
 DISTRIBUTED BY HASH(\`v1\`) BUCKETS 1
 PROPERTIES (
     \"replication_allocation\" = \"tag.location.default: 1\"
@@ -47,7 +47,7 @@ mysql -uroot -P 9030 -h doris-server -e "select * from demo.demo_bhv_table" > ./
 
 
 if cat ./query_result.csv | sed '1d; s/\t/,/g' | awk -F "," '{
-    exit !($1 == 1 && $2 == 1 && $3 == 1 && $4 == 1.1 && $5 == 1.2 && $6 == "test" && $7 == "2013-01-01" && $8 == "2013-01-01 01:01:01" && $9 == 0); }'; then
+    exit !($1 == 1 && $2 == 1 && $3 == 1 && $4 == 1.1 && $5 == 1.2 && $6 == "test" && $7 == "2013-01-01" && $8 == "2013-01-01 01:01:01" && $9 == 0 && $10 == "{\"a\":1}"); }'; then
   echo "Doris sink check passed"
 else
   cat ./query_result.csv
