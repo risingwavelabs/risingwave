4f8882c3f57885b11d67da95d25acf7204fbcfc0
William Wen <william123.wen@gmail.com>
2024-03-30 16:33:52 +0800

    fix and add ut

--- src/storage/src/hummock/iterator/change_log.rs
+++ src/storage/src/hummock/iterator/change_log.rs
@@ -248,7 +248,7 @@ impl<NI: HummockIterator<Direction = Forward>, OI: HummockIterator<Direction = F
                     // old value iter has advanced over the current range
                     break;
                 }
-                Ordering::Equal => match oldest_epoch.cmp(&old_value_iter_key.epoch_with_gap) {
+                Ordering::Equal => match old_value_iter_key.epoch_with_gap.cmp(&oldest_epoch) {
                     Ordering::Less => {
                         assert!(
                                 old_value_iter_key.epoch_with_gap.pure_epoch() < self.min_epoch,
@@ -353,11 +353,14 @@ impl StateStoreIter<StateStoreReadLogItem> for ChangeLogIterator {
 mod tests {
     use std::collections::BTreeMap;
     use std::ops::Bound;
+    use std::ops::Bound::Unbounded;
 
     use bytes::Bytes;
     use itertools::Itertools;
+    use rand::{thread_rng, Rng, RngCore};
     use risingwave_common::catalog::TableId;
-    use risingwave_common::util::epoch::test_epoch;
+    use risingwave_common::hash::table_distribution::TableDistribution;
+    use risingwave_common::util::epoch::{test_epoch, EpochPair};
     use risingwave_hummock_sdk::key::{TableKey, UserKey};
     use risingwave_hummock_sdk::EpochWithGap;
 
@@ -366,11 +369,34 @@ mod tests {
         iterator_test_table_key_of, iterator_test_value_of,
     };
     use crate::hummock::iterator::MergeIterator;
-    use crate::mem_table::{MemTable, MemTableHummockIterator};
-    use crate::store::{ChangeLogValue, OpConsistencyLevel};
+    use crate::mem_table::{KeyOp, MemTable, MemTableHummockIterator, MemTableStore};
+    use crate::memory::MemoryStateStore;
+    use crate::store::{
+        ChangeLogValue, InitOptions, LocalStateStore, NewLocalOptions, OpConsistencyLevel,
+        ReadLogOptions, ReadOptions, SealCurrentEpochOptions, StateStoreRead, CHECK_BYTES_EQUAL,
+    };
+    use crate::store_impl::boxed_state_store::DynamicDispatchedStateStoreIter;
+    use crate::StateStore;
 
     #[tokio::test]
-    async fn test_basic() {
+    async fn test_empty() {
+        let table_id = TableId::new(233);
+        let epoch = EpochWithGap::new_from_epoch(test_epoch(1));
+        let empty = BTreeMap::new();
+        let new_value_iter = MemTableHummockIterator::new(&empty, epoch, table_id);
+        let old_value_iter = MemTableHummockIterator::new(&empty, epoch, table_id);
+        let mut iter = ChangeLogIteratorInner::new(
+            (epoch.pure_epoch(), epoch.pure_epoch()),
+            Bound::Unbounded,
+            new_value_iter,
+            old_value_iter,
+        );
+        iter.rewind().await.unwrap();
+        assert!(!iter.is_valid());
+    }
+
+    #[tokio::test]
+    async fn test_append_only() {
         let table_id = TableId::new(233);
 
         let count = 100;
@@ -418,5 +444,215 @@ mod tests {
             iter.next().await.unwrap();
         }
         assert!(!iter.is_valid());
+    }
+
+    #[tokio::test]
+    async fn test_delete_only() {
+        let table_id = TableId::new(233);
+
+        let count = 100;
+        let kvs = (0..count)
+            .map(|i| {
+                (
+                    TableKey(Bytes::from(iterator_test_table_key_of(i))),
+                    Bytes::from(iterator_test_value_of(i)),
+                )
+            })
+            .collect_vec();
+        let mut new_value_memtable = MemTable::new(OpConsistencyLevel::Inconsistent);
+        let mut old_value_memtable = MemTable::new(OpConsistencyLevel::Inconsistent);
+        for (key, value) in &kvs {
+            new_value_memtable
+                .delete(key.clone(), Bytes::new())
+                .unwrap();
+            old_value_memtable
+                .insert(key.clone(), value.clone())
+                .unwrap();
+        }
+        let epoch = EpochWithGap::new_from_epoch(test_epoch(1));
+        let new_value_iter =
+            MemTableHummockIterator::new(&new_value_memtable.buffer, epoch, table_id);
+        let old_value_iter =
+            MemTableHummockIterator::new(&old_value_memtable.buffer, epoch, table_id);
+        let mut iter = ChangeLogIteratorInner::new(
+            (epoch.pure_epoch(), epoch.pure_epoch()),
+            Bound::Unbounded,
+            new_value_iter,
+            old_value_iter,
+        );
+        iter.rewind().await.unwrap();
+        for (key, value) in kvs {
+            assert!(iter.is_valid());
+            assert_eq!(
+                UserKey {
+                    table_id,
+                    table_key: key.to_ref(),
+                },
+                iter.key()
+            );
+            assert_eq!(ChangeLogValue::Delete(value.as_ref()), iter.log_value());
+            iter.next().await.unwrap();
+        }
+        assert!(!iter.is_valid());
+    }
+
+    async fn gen_test_data(
+        table_id: TableId,
+        epoch_count: usize,
+        key_count: usize,
+        delete_ratio: f64,
+    ) -> (Vec<(u64, MemTableStore, MemTableStore)>, MemoryStateStore) {
+        let state_store = MemoryStateStore::new();
+        let mut rng = thread_rng();
+        let mut local = state_store
+            .new_local(NewLocalOptions {
+                table_id,
+                op_consistency_level: OpConsistencyLevel::ConsistentOldValue(
+                    CHECK_BYTES_EQUAL.clone(),
+                ),
+                table_option: Default::default(),
+                is_replicated: false,
+                vnodes: TableDistribution::all_vnodes(),
+            })
+            .await;
+        let mut logs = Vec::new();
+        for epoch_idx in 1..=epoch_count {
+            let epoch = test_epoch(epoch_idx as _);
+            let mut new_values = MemTableStore::new();
+            let mut old_values = MemTableStore::new();
+            if epoch_idx == 1 {
+                local
+                    .init(InitOptions {
+                        epoch: EpochPair::new_test_epoch(epoch),
+                    })
+                    .await
+                    .unwrap();
+            } else {
+                local.flush().await.unwrap();
+                local.seal_current_epoch(
+                    epoch,
+                    SealCurrentEpochOptions {
+                        table_watermarks: None,
+                        switch_op_consistency_level: None,
+                    },
+                );
+            }
+            for key_idx in 0..key_count {
+                let key = TableKey(Bytes::from(iterator_test_table_key_of(key_idx)));
+                if rng.gen_bool(delete_ratio) {
+                    if let Some(prev_value) = local
+                        .get(
+                            key.clone(),
+                            ReadOptions {
+                                prefix_hint: None,
+                                ignore_range_tombstone: false,
+                                prefetch_options: Default::default(),
+                                cache_policy: Default::default(),
+                                retention_seconds: None,
+                                table_id,
+                                read_version_from_backup: false,
+                            },
+                        )
+                        .await
+                        .unwrap()
+                    {
+                        new_values.insert(key.clone(), KeyOp::Delete(Bytes::new()));
+                        old_values.insert(key.clone(), KeyOp::Insert(prev_value.clone()));
+                        local.delete(key, prev_value).unwrap();
+                    }
+                } else {
+                    let value = Bytes::copy_from_slice(rng.next_u64().to_string().as_bytes());
+                    new_values.insert(key.clone(), KeyOp::Insert(value.clone()));
+                    let prev_value = local
+                        .get(
+                            key.clone(),
+                            ReadOptions {
+                                prefix_hint: None,
+                                ignore_range_tombstone: false,
+                                prefetch_options: Default::default(),
+                                cache_policy: Default::default(),
+                                retention_seconds: None,
+                                table_id,
+                                read_version_from_backup: false,
+                            },
+                        )
+                        .await
+                        .unwrap();
+                    if let Some(prev_value) = prev_value.clone() {
+                        old_values.insert(key.clone(), KeyOp::Insert(prev_value));
+                    }
+                    local.insert(key, value, prev_value).unwrap();
+                }
+            }
+            logs.push((epoch, new_values, old_values));
+        }
+        local.flush().await.unwrap();
+        local.seal_current_epoch(
+            test_epoch((epoch_count + 1) as _),
+            SealCurrentEpochOptions {
+                table_watermarks: None,
+                switch_op_consistency_level: None,
+            },
+        );
+        (logs, state_store)
+    }
+
+    #[tokio::test]
+    async fn test_random_data() {
+        let table_id = TableId::new(233);
+        let epoch_count = 10;
+        let (logs, state_store) = gen_test_data(table_id, epoch_count, 10000, 0.2).await;
+        assert_eq!(logs.len(), epoch_count);
+        for start_epoch_idx in 0..epoch_count {
+            for end_epoch_idx in start_epoch_idx + 1..epoch_count {
+                let new_value_iter =
+                    MergeIterator::new(logs.iter().map(|(epoch, new_value_memtable, _)| {
+                        MemTableHummockIterator::new(
+                            new_value_memtable,
+                            EpochWithGap::new_from_epoch(*epoch),
+                            table_id,
+                        )
+                    }));
+                let old_value_iter =
+                    MergeIterator::new(logs.iter().map(|(epoch, _, old_value_memtable)| {
+                        MemTableHummockIterator::new(
+                            old_value_memtable,
+                            EpochWithGap::new_from_epoch(*epoch),
+                            table_id,
+                        )
+                    }));
+                let epoch_range = (logs[start_epoch_idx].0, logs[end_epoch_idx].0);
+                let mut change_log_iter = ChangeLogIteratorInner::new(
+                    epoch_range,
+                    Unbounded,
+                    new_value_iter,
+                    old_value_iter,
+                );
+                change_log_iter.rewind().await.unwrap();
+                let mut expected_change_log_iter = state_store
+                    .iter_log(
+                        epoch_range,
+                        (Unbounded, Unbounded),
+                        ReadLogOptions { table_id },
+                    )
+                    .await
+                    .unwrap();
+                while let Some((key, change_log_value)) =
+                    expected_change_log_iter.try_next().await.unwrap()
+                {
+                    assert!(change_log_iter.is_valid());
+                    assert_eq!(
+                        change_log_iter.key(),
+                        UserKey {
+                            table_id,
+                            table_key: key,
+                        },
+                    );
+                    assert_eq!(change_log_iter.log_value(), change_log_value);
+                    change_log_iter.next().await.unwrap();
+                }
+                assert!(!change_log_iter.is_valid());
+            }
+        }
     }
 }
