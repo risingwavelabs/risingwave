e909810738364f151056722e14aeb59c2bd83164
xfz <xfz2000x@gmail.com>
2024-04-08 14:31:53 +0800

    update

--- integration_tests/citus-cdc/create_mv.sql
+++ integration_tests/citus-cdc/create_mv.sql
@@ -3,9 +3,3 @@ SELECT
     COUNT(*) as cnt
 FROM
     orders_rw;
-
-CREATE MATERIALIZED VIEW citus_all_types_count AS
-SELECT
-    COUNT(*) as cnt
-FROM
-    citus_all_types;
--- integration_tests/citus-cdc/query.sql
+++ integration_tests/citus-cdc/query.sql
@@ -1,3 +1,1 @@
 SELECT * FROM orders_rw_count LIMIT 10;
-
-SELECT * FROM citus_all_types_count;
