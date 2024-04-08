85f74b198208426750dcc1eb4f07ffd1f2d0b235
Jinser Kafka <aimer@purejs.icu>
2024-04-08 11:17:03 +0800

    test: add sink check for HiveMQ integration test

---
+++ integration_tests/hivemq/sink_check.py
@@ -0,0 +1,35 @@
+import sys
+import subprocess
+
+
+output = subprocess.Popen(
+    [
+        "docker",
+        "compose",
+        "exec",
+        "mqtt",
+        "mosquitto_sub",
+        "-h",
+        "localhost",
+        "-t",
+        "test",
+        "-p",
+        "1883",
+        "-C",
+        "1",
+        "-W",
+        "120",
+    ],
+    stdout=subprocess.PIPE,
+)
+
+rows = subprocess.check_output(["wc", "-l"], stdin=output.stdout)
+output.stdout.close()  # type: ignore
+output.wait()
+
+rows = int(rows.decode("utf8").strip())
+
+print(f"{rows} rows in 'test'")
+if rows < 1:
+    print("Data check failed for case 'test'")
+    sys.exit(1)
