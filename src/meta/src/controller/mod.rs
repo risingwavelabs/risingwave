a1b4b5165a3020e08690992abc0d62bb45051344
Giovanny Gutiérrez <giovanny.gutierrez@commure.com>
2024-04-10 11:24:29 -0500

    feat(udf): Add deno as UDF language (#15719)
    Co-authored-by: Runji Wang <wangrunji0408@163.com>

--- src/meta/src/controller/mod.rs
+++ src/meta/src/controller/mod.rs
@@ -317,6 +317,8 @@ impl From<ObjectModel<function::Model>> for PbFunction {
             compressed_binary: value.0.compressed_binary,
             kind: Some(value.0.kind.into()),
             always_retry_on_network_error: value.0.always_retry_on_network_error,
+            runtime: value.0.runtime,
+            function_type: value.0.function_type,
         }
     }
 }
