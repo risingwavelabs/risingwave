feb8beb0a446fb8ee7101dbe9e9b10b1e2fbaa27
Noel Kwan <noelkwan1998@gmail.com>
2024-04-15 10:03:18 +0800

    add deterministic fuzz test workflow

---
+++ run-deterministic-fuzz-test.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+
+# Exits as soon as any line fails.
+set -euo pipefail
+
+export LOGDIR=.risingwave/log
+export RUST_LOG=info
+
+while getopts 'p:' opt; do
+    case ${opt} in
+        p )
+            profile=$OPTARG
+            ;;
+        \? )
+            echo "Invalid Option: -$OPTARG" 1>&2
+            exit 1
+            ;;
+        : )
+            echo "Invalid option: $OPTARG requires an argument" 1>&2
+            ;;
+    esac
+done
+shift $((OPTIND -1))
+
+echo "--- Download artifacts"
+download-and-decompress-artifact risingwave_simulation .
+chmod +x ./risingwave_simulation
+
+echo "--- deterministic simulation e2e, ci-3cn-2fe, fuzzing (seed)"
+seq 32 | parallel MADSIM_TEST_SEED={} './risingwave_simulation --sqlsmith 100 ./src/tests/sqlsmith/tests/testdata 2> $LOGDIR/fuzzing-{}.log && rm $LOGDIR/fuzzing-{}.log'
