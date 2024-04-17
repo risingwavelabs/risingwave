15b2e96f960be5d1092210dfe415ad24c144988b
Gio Gutierrez <giovanny.gutierrez@commure.com>
2024-04-17 14:44:04 -0500

    feat: Use venv to install requirements

--- ci/scripts/run-e2e-test.sh
+++ ci/scripts/run-e2e-test.sh
@@ -75,6 +75,10 @@ mv target/debug/risingwave_e2e_extended_mode_test-"$profile" target/debug/rising
 
 chmod +x ./target/debug/risingwave_e2e_extended_mode_test
 
+python3 -m venv .venv
+source .venv/bin/activate
+pip3 install -r e2e_test/udf/requirements.txt -q
+
 echo "--- e2e, $mode, streaming"
 RUST_LOG="info,risingwave_stream=info,risingwave_batch=info,risingwave_storage=info" \
 cluster_start
