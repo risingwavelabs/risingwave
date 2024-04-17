5bf45c8a47cd000e6f42c8c68c147841a13a426b
xxhZs <1060434431@qq.com>
2024-04-17 17:04:02 +0800

    add v2
    fix ci
    
    fix ci

--- ci/scripts/run-e2e-test.sh
+++ ci/scripts/run-e2e-test.sh
@@ -97,8 +97,6 @@ sqllogictest -p 4566 -d dev './e2e_test/database/prepare.slt'
 sqllogictest -p 4566 -d test './e2e_test/database/test.slt'
 
 echo "--- e2e, $mode, subscription"
-curl -sSL https://install.python-poetry.org | python3 -
-python3 -m pip install psycopg2-binary
 sqllogictest -p 4566 -d dev './e2e_test/subscription/check_sql_statement.slt'
 python3 ./e2e_test/subscription/main.py
 
