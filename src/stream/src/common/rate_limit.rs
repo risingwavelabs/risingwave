45807cc50f82b2a57e7f1b6b7a4b9dc5fab433cd
Richard Chien <stdrc@outlook.com>
2024-04-19 21:43:28 +0800

    add comment
    Signed-off-by: Richard Chien <stdrc@outlook.com>

--- src/stream/src/common/rate_limit.rs
+++ src/stream/src/common/rate_limit.rs
@@ -12,6 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+/// Get the rate-limited max chunk size.
 pub(crate) fn limited_chunk_size(rate_limit: Option<u32>) -> usize {
     let config_chunk_size = crate::config::chunk_size();
     rate_limit
