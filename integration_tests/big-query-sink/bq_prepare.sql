f4ca7c8ad5e520a7c79252aab5c6be3838af4e76
xfz <xfz2000x@gmail.com>
2024-04-25 17:27:32 +0800

    delete bq tables

--- integration_tests/big-query-sink/bq_prepare.sql
+++ integration_tests/big-query-sink/bq_prepare.sql
@@ -1,38 +1,38 @@
 DROP TABLE IF EXISTS `rwctest.bqtest.bq_sink`;
-CREATE TABLE `rwctest.bqtest.bq_sink` (
-    user_id INT64,
-    target_id STRING,
-    event_timestamp TIMESTAMP
-);
+-- CREATE TABLE `rwctest.bqtest.bq_sink` (
+    -- user_id INT64,
+    -- target_id STRING,
+    -- event_timestamp TIMESTAMP
+-- );
 
 DROP TABLE IF EXISTS `rwctest.bqtest.bq_sink_data_types`;
-CREATE TABLE `rwctest.bqtest.bq_sink_data_types` (
-    types_id INT64,
-    c_boolean BOOL,
-    c_smallint INT64,
-    c_integer INT64,
-    c_bigint INT64,
-    c_decimal NUMERIC,
-    c_double_precision FLOAT64,
-    c_varchar STRING,
-    c_bytea BYTES,
-    c_date DATE,
-    c_timestamp DATETIME,
-    c_timestamptz TIMESTAMP,
-    c_interval INTERVAL,
-    c_jsonb STRING,
-    c_boolean_array ARRAY<BOOL>,
-    c_smallint_array ARRAY<INT64>,
-    c_integer_array ARRAY<INT64>,
-    c_bigint_array ARRAY<INT64>,
-    c_decimal_array ARRAY<NUMERIC>,
-    c_double_precision_array ARRAY<FLOAT64>,
-    c_varchar_array ARRAY<STRING>,
-    c_bytea_array ARRAY<BYTES>,
-    c_date_array ARRAY<DATE>,
-    c_timestamp_array ARRAY<DATETIME>,
-    c_timestamptz_array ARRAY<TIMESTAMP>,
-    c_interval_array ARRAY<INTERVAL>,
-    c_jsonb_array ARRAY<STRING>,
-    c_struct STRUCT<s_int INT64, s_bool BOOL>
-);
+-- CREATE TABLE `rwctest.bqtest.bq_sink_data_types` (
+    -- types_id INT64,
+    -- c_boolean BOOL,
+    -- c_smallint INT64,
+    -- c_integer INT64,
+    -- c_bigint INT64,
+    -- c_decimal NUMERIC,
+    -- c_double_precision FLOAT64,
+    -- c_varchar STRING,
+    -- c_bytea BYTES,
+    -- c_date DATE,
+    -- c_timestamp DATETIME,
+    -- c_timestamptz TIMESTAMP,
+    -- c_interval INTERVAL,
+    -- c_jsonb STRING,
+    -- c_boolean_array ARRAY<BOOL>,
+    -- c_smallint_array ARRAY<INT64>,
+    -- c_integer_array ARRAY<INT64>,
+    -- c_bigint_array ARRAY<INT64>,
+    -- c_decimal_array ARRAY<NUMERIC>,
+    -- c_double_precision_array ARRAY<FLOAT64>,
+    -- c_varchar_array ARRAY<STRING>,
+    -- c_bytea_array ARRAY<BYTES>,
+    -- c_date_array ARRAY<DATE>,
+    -- c_timestamp_array ARRAY<DATETIME>,
+    -- c_timestamptz_array ARRAY<TIMESTAMP>,
+    -- c_interval_array ARRAY<INTERVAL>,
+    -- c_jsonb_array ARRAY<STRING>,
+    -- c_struct STRUCT<s_int INT64, s_bool BOOL>
+-- );
--- integration_tests/big-query-sink/prepare.sh
+++ integration_tests/big-query-sink/prepare.sh
@@ -12,4 +12,4 @@ bq_prepare_content=$(cat $bq_prepare_file)
 
 docker compose exec gcloud-cli bq query --use_legacy_sql=false "$bq_prepare_content"
 
-sleep 20
+sleep 10
