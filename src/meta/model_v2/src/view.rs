9e6528a45c7417a41f8dd245057d51996d10d3a0
zwang28 <70626450+zwang28@users.noreply.github.com>
2024-04-26 15:37:02 +0800

    feat(meta): support sql meta store backup and restoration (#16384)

--- src/meta/model_v2/src/view.rs
+++ src/meta/model_v2/src/view.rs
@@ -15,10 +15,11 @@
 use risingwave_pb::catalog::PbView;
 use sea_orm::entity::prelude::*;
 use sea_orm::ActiveValue::Set;
+use serde::{Deserialize, Serialize};
 
 use crate::{FieldArray, Property, ViewId};
 
-#[derive(Clone, Debug, PartialEq, DeriveEntityModel, Eq)]
+#[derive(Clone, Debug, PartialEq, DeriveEntityModel, Eq, Serialize, Deserialize)]
 #[sea_orm(table_name = "view")]
 pub struct Model {
     #[sea_orm(primary_key, auto_increment = false)]
