15bfd02594400d003b1d2d8250cd4b4b934fddbb
tabVersion <tabvision@bupt.icu>
2024-04-28 17:37:03 +0800

    fix license

--- src/frontend/src/handler/create_secret.rs
+++ src/frontend/src/handler/create_secret.rs
@@ -1,3 +1,17 @@
+// Copyright 2024 RisingWave Labs
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 use pgwire::pg_response::{PgResponse, StatementType};
 use risingwave_sqlparser::ast::{CreateSecretStatement, Value};
 
@@ -30,7 +44,7 @@ pub async fn handle_create_secret(
             return Err(ErrorCode::InvalidParameterValue(
                 "secret payload must be a string".to_string(),
             )
-            .into());
+                .into());
         }
     };
 
