e5c9c4829a7ae48c47e8d43385f5df68225c01f5
Croxx <mrcroxx@outlook.com>
2024-05-10 12:59:16 +0800

    refactor: simplify block cache and file cache with foyer v0.8 (#16500)
    Signed-off-by: MrCroxx <mrcroxx@outlook.com>

--- src/common/src/catalog/mod.rs
+++ src/common/src/catalog/mod.rs
@@ -33,6 +33,7 @@ use risingwave_pb::catalog::{
 };
 use risingwave_pb::plan_common::ColumnDescVersion;
 pub use schema::{test_utils as schema_test_utils, Field, FieldDisplay, Schema};
+use serde::{Deserialize, Serialize};
 
 use crate::array::DataChunk;
 pub use crate::constants::hummock;
@@ -228,7 +229,20 @@ impl From<SchemaId> for u32 {
     }
 }
 
-#[derive(Clone, Copy, Debug, Display, Default, Hash, PartialOrd, PartialEq, Eq, Ord)]
+#[derive(
+    Clone,
+    Copy,
+    Debug,
+    Display,
+    Default,
+    Hash,
+    PartialOrd,
+    PartialEq,
+    Eq,
+    Ord,
+    Serialize,
+    Deserialize,
+)]
 #[display("{table_id}")]
 pub struct TableId {
     pub table_id: u32,
