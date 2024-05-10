7cdfa15c25533811d9cdb5fbaa9a146da36422b8
CAJan93 <jan.mensch@gmx.net>
2024-05-10 16:01:49 +0200

    try windowing

--- integration_tests/tidb-cdc-sink/create_mv.sql
+++ integration_tests/tidb-cdc-sink/create_mv.sql
@@ -1,7 +1,7 @@
 --
 -- Find the top10 hotest hashtags.
 --
-CREATE MATERIALIZED VIEW hot_hashtags4 AS WITH tags AS (
+CREATE MATERIALIZED VIEW hot_hashtags13 AS WITH tags AS (
     SELECT
         unnest(regexp_matches(tweet.text, '#\w+', 'g')) AS hashtag,
         tweet.created_at AT TIME ZONE 'UTC' AS created_at
@@ -12,13 +12,21 @@ CREATE MATERIALIZED VIEW hot_hashtags4 AS WITH tags AS (
 SELECT
     hashtag,
     COUNT(*) AS hashtag_occurrences,
-    window_start
+    window_start,
+    window_end
 FROM
-    TUMBLE(tags, created_at, INTERVAL '30 seconds')
+    HOP(
+        tags,
+        created_at,
+        INTERVAL '5 seconds',
+        INTERVAL '30 seconds'
+    )
 GROUP BY
     hashtag,
-    window_start
+    window_start,
+    window_end
 ORDER BY
+    window_start DESC,
     hashtag_occurrences DESC
 LIMIT
     10;
