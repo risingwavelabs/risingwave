2d80db7c962d3692c34a20b3e610a5042c761c19
Noel Kwan <noelkwan1998@gmail.com>
2024-05-16 11:45:47 +0800

    trace 10K

--- src/stream/src/executor/hash_join.rs
+++ src/stream/src/executor/hash_join.rs
@@ -842,8 +842,8 @@ impl<K: HashKey, S: StateStore, const T: JoinTypePrimitive> HashJoinExecutor<K, 
 
             if let Some(rows) = &matched_rows {
                 join_matched_join_keys.observe(rows.len() as _);
-                if rows.len() > 100000 {
-                    tracing::warn!( "Join matched join keys count is too large: {}, update_table_id: {}, match_table_id: {}, join_key: {:?}, actor_id: {}, fragment_id: {}", 
+                if rows.len() > 10000 {
+                    tracing::error!( "large join key: {}, update_table_id: {}, match_table_id: {}, join_key: {:?}, actor_id: {}, fragment_id: {}",
                         rows.len(),
                         side_update.ht.table_id(),
                         side_match.ht.table_id(),
