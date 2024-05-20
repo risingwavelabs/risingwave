7f7f81f21014bd2b8a9e3d6872243be80153ab0f
Noel Kwan <noelkwan1998@gmail.com>
2024-05-20 14:44:50 +0800

    refactor fe

--- src/frontend/src/optimizer/plan_node/stream_sink.rs
+++ src/frontend/src/optimizer/plan_node/stream_sink.rs
@@ -45,6 +45,7 @@ use super::{generic, ExprRewritable, PlanBase, PlanRef, StreamNode, StreamProjec
 use crate::error::{ErrorCode, Result};
 use crate::expr::{ExprImpl, FunctionCall, InputRef};
 use crate::optimizer::plan_node::expr_visitable::ExprVisitable;
+use crate::optimizer::plan_node::utils::plan_has_backfill_leaf_nodes;
 use crate::optimizer::plan_node::PlanTreeNodeUnary;
 use crate::optimizer::property::{Distribution, Order, RequiredDist};
 use crate::stream_fragmenter::BuildFragmentGraphState;
@@ -373,7 +374,9 @@ impl StreamSink {
         };
         let input = required_dist.enforce_if_not_satisfies(input, &Order::any())?;
         let distribution_key = input.distribution().dist_column_indices().to_vec();
-        let create_type = if input.ctx().session_ctx().config().background_ddl() {
+        let create_type = if input.ctx().session_ctx().config().background_ddl()
+            && plan_has_backfill_leaf_nodes(&input)
+        {
             CreateType::Background
         } else {
             CreateType::Foreground
