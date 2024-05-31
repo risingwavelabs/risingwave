e36bdd4ce63c3c1a93c366c85e48c75cd9555a64
TennyZhuang <zty0826@gmail.com>
2024-05-31 14:00:57 +0800

    fix dylint and use eprintln
    Signed-off-by: TennyZhuang <zty0826@gmail.com>

--- src/sqlparser/src/bin/sqlparser.rs
+++ src/sqlparser/src/bin/sqlparser.rs
@@ -1,4 +1,18 @@
-#![allow(rw::format_error)]
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#![feature(register_tool)]
+#![register_tool(rw)]
+#![allow(rw::format_error)] // test code
 
 use std::io;
 
@@ -21,7 +35,7 @@ fn main() {
             }
         }
         Err(e) => {
-            println!("{}", e);
+            eprintln!("{}", e);
         }
     }
 }
