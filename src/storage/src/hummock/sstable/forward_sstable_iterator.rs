51573416889f05b8657e1ca5c4249376ecb73beb
zwang28 <84491488@qq.com>
2024-06-03 14:56:33 +0800

    temp_6

--- src/storage/src/hummock/sstable/forward_sstable_iterator.rs
+++ src/storage/src/hummock/sstable/forward_sstable_iterator.rs
@@ -121,7 +121,6 @@ impl SstableIterator {
         // When all data are in block cache, it is highly possible that this iterator will stay on a
         // worker thread for a full time. Therefore, we use tokio's unstable API consume_budget to
         // do cooperative scheduling.
-        tokio::task::consume_budget().await;
 
         let mut hit_cache = false;
         if idx >= self.sst.block_count() {
