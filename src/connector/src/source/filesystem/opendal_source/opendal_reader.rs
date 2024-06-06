5e5589f75649c1ec8d566feab0a905ae1cd74aac
congyi <15605187270@163.com>
2024-06-06 17:31:42 +0800

    handle error

--- src/connector/src/source/filesystem/opendal_source/opendal_reader.rs
+++ src/connector/src/source/filesystem/opendal_source/opendal_reader.rs
@@ -19,7 +19,7 @@ use arrow_array::RecordBatch;
 use async_trait::async_trait;
 use futures::TryStreamExt;
 use futures_async_stream::try_stream;
-use opendal::Operator;
+use opendal::{Operator, Reader};
 use parquet::arrow::ParquetRecordBatchStreamBuilder;
 use risingwave_common::array::{DataChunk, StreamChunk};
 use tokio::io::BufReader;
@@ -92,30 +92,35 @@ impl<Src: OpendalSource> OpendalReader<Src> {
                 .range(split.offset as u64..)
                 .into_future() // Unlike `rustc`, `try_stream` seems require manual `into_future`.
                 .await?;
-
             if let EncodingProperties::Parquet = &self.parser_config.specific.encoding_config {
                 let record_batch_stream = Box::pin(
                     ParquetRecordBatchStreamBuilder::new(file_reader)
-                        .await
-                        .unwrap()
+                        .await?
                         .with_batch_size(self.source_ctx.source_ctrl_opts.chunk_size)
-                        .build()
-                        .unwrap(),
+                        .build()?,
                 );
 
                 #[for_await]
                 for record_batch in record_batch_stream {
-                    let record_batch: RecordBatch = record_batch.unwrap();
+                    let record_batch: RecordBatch = record_batch?;
                     let chunk: StreamChunk =
-                        record_batch_to_chunk(record_batch, self.columns.clone()).unwrap();
+                        convert_record_batch_to_stream_chunk(record_batch, self.columns.clone())?;
+                    self.source_ctx
+                        .metrics
+                        .partition_input_count
+                        .with_label_values(&[
+                            &actor_id,
+                            &source_id,
+                            &split_id,
+                            &source_name,
+                            &fragment_id,
+                        ])
+                        .inc_by(chunk.cardinality() as u64);
                     yield chunk;
                 }
             } else {
-                let data_stream = Self::stream_read_object(
-                    self.connector.op.clone(),
-                    split,
-                    self.source_ctx.clone(),
-                );
+                let data_stream =
+                    Self::stream_read_object(file_reader, split, self.source_ctx.clone());
 
                 let parser =
                     ByteStreamSourceParserImpl::create(self.parser_config.clone(), source_ctx)
@@ -148,7 +153,7 @@ impl<Src: OpendalSource> OpendalReader<Src> {
 
     #[try_stream(boxed, ok = Vec<SourceMessage>, error = crate::error::ConnectorError)]
     pub async fn stream_read_object(
-        op: Operator,
+        file_reader: Reader,
         split: OpendalFsSplit<Src>,
         source_ctx: SourceContextRef,
     ) {
@@ -161,14 +166,8 @@ impl<Src: OpendalSource> OpendalReader<Src> {
 
         let object_name = split.name.clone();
 
-        let reader = op
-            .reader_with(&object_name)
-            .range(split.offset as u64..)
-            .into_future() // Unlike `rustc`, `try_stream` seems require manual `into_future`.
-            .await?;
-
         let stream_reader = StreamReader::new(
-            reader.map_err(|e| std::io::Error::new(std::io::ErrorKind::Other, e)),
+            file_reader.map_err(|e| std::io::Error::new(std::io::ErrorKind::Other, e)),
         );
         let buf_reader = BufReader::new(stream_reader);
         let stream = ReaderStream::with_capacity(buf_reader, STREAM_READER_CAPACITY);
@@ -220,7 +219,7 @@ impl<Src: OpendalSource> OpendalReader<Src> {
 }
 
 // pub type RecordBatchStream = BoxStream<'static, Result<RecordBatch>>;
-fn record_batch_to_chunk(
+fn convert_record_batch_to_stream_chunk(
     record_batch: RecordBatch,
     source_columns: Option<Vec<Column>>,
 ) -> Result<StreamChunk, crate::error::ConnectorError> {
@@ -230,10 +229,10 @@ fn record_batch_to_chunk(
             for source_column in source_columns {
                 if let Some(parquet_column) = record_batch.column_by_name(&source_column.name) {
                     let converted_arrow_data_type =
-                        arrow_schema::DataType::try_from(&source_column.data_type).unwrap();
+                        arrow_schema::DataType::try_from(&source_column.data_type)?;
 
                     if &converted_arrow_data_type == parquet_column.data_type() {
-                        let column = Arc::new(parquet_column.try_into().unwrap());
+                        let column = Arc::new(parquet_column.try_into()?);
                         chunk_columns.push(column);
                     }
                 } else {
@@ -246,7 +245,7 @@ fn record_batch_to_chunk(
         None => {
             let mut chunk_columns = Vec::with_capacity(record_batch.num_columns());
             for array in record_batch.columns() {
-                let column = Arc::new(array.try_into().unwrap());
+                let column = Arc::new(array.try_into()?);
                 chunk_columns.push(column);
             }
             return Ok(DataChunk::new(chunk_columns, record_batch.num_rows()).into());
