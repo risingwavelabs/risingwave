21babd5e513e3d12bac67a494005d8e8d9b46c68
August <pin@singularity-data.com>
2024-06-11 14:47:48 +0800

    chore: expose associated source in rw_sources (#17150)

--- src/frontend/src/handler/show.rs
+++ src/frontend/src/handler/show.rs
@@ -318,7 +318,6 @@ pub async fn handle_show_object(
             .read_guard()
             .get_schema_by_name(session.database(), &schema_or_default(&schema))?
             .iter_source()
-            .filter(|t| t.associated_table_id.is_none())
             .map(|t| t.name.clone())
             .collect(),
         ShowObject::Sink { schema } => catalog_reader
