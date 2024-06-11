6a630f859fa16c9fb18a255db0aaa0de1a171496
August <pin@singularity-data.com>
2024-06-11 18:07:10 +0800

    add back streaming tests

--- ci/scripts/run-e2e-test.sh
+++ ci/scripts/run-e2e-test.sh
@@ -75,15 +75,15 @@ mv target/debug/risingwave_e2e_extended_mode_test-"$profile" target/debug/rising
 
 chmod +x ./target/debug/risingwave_e2e_extended_mode_test
 
-#echo "--- e2e, $mode, streaming"
-#RUST_LOG="info,risingwave_stream=info,risingwave_batch=info,risingwave_storage=info" \
-#cluster_start
-## Please make sure the regression is expected before increasing the timeout.
-#sqllogictest -p 4566 -d dev './e2e_test/streaming/**/*.slt' --junit "streaming-${profile}"
-#sqllogictest -p 4566 -d dev './e2e_test/backfill/sink/different_pk_and_dist_key.slt'
-#
-#echo "--- Kill cluster"
-#cluster_stop
+echo "--- e2e, $mode, streaming"
+RUST_LOG="info,risingwave_stream=info,risingwave_batch=info,risingwave_storage=info" \
+cluster_start
+# Please make sure the regression is expected before increasing the timeout.
+sqllogictest -p 4566 -d dev './e2e_test/streaming/**/*.slt' --junit "streaming-${profile}"
+sqllogictest -p 4566 -d dev './e2e_test/backfill/sink/different_pk_and_dist_key.slt'
+
+echo "--- Kill cluster"
+cluster_stop
 
 echo "--- e2e, $mode, batch"
 RUST_LOG="debug,risingwave_stream=debug,risingwave_batch=debug,risingwave_storage=debug" \
