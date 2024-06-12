8615a45c3a75b023d6890081f6e76b6f92a89493
Bugen Zhao <i@bugenzhao.com>
2024-06-12 13:24:08 +0800

    fix unused imports
    Signed-off-by: Bugen Zhao <i@bugenzhao.com>

--- src/connector/src/parser/json_parser.rs
+++ src/connector/src/parser/json_parser.rs
@@ -21,8 +21,6 @@ use jst::{convert_avro, Context};
 use risingwave_common::{bail, try_match_expand};
 use risingwave_pb::plan_common::ColumnDesc;
 
-use super::avro::schema_resolver::ConfluentSchemaCache;
-use super::unified::Access;
 use super::util::{bytes_from_url, get_kafka_topic};
 use super::{EncodingProperties, JsonProperties, SchemaRegistryAuth, SpecificParserConfig};
 use crate::error::ConnectorResult;
