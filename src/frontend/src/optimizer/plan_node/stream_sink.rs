bb48904320b656c6670d29ef404892396d7b7ac8
Richard Chien <stdrc@outlook.com>
2024-06-12 14:16:18 +0800

    chore(sink): improve force append only warning (#17213)
    Signed-off-by: Richard Chien <stdrc@outlook.com>

--- src/frontend/src/optimizer/plan_node/stream_sink.rs
+++ src/frontend/src/optimizer/plan_node/stream_sink.rs
@@ -470,7 +470,11 @@ impl StreamSink {
             (false, true, false) => {
                 Err(ErrorCode::SinkError(Box::new(Error::new(
                     ErrorKind::InvalidInput,
-                    format!("The sink cannot be append-only. Please add \"force_append_only='true'\" in {} options to force the sink to be append-only. Notice that this will cause the sink executor to drop any UPDATE or DELETE message.", if syntax_legacy { "WITH" } else { "FORMAT ENCODE" }),
+                    format!(
+                        "The sink cannot be append-only. Please add \"force_append_only='true'\" in {} options to force the sink to be append-only. \
+                        Notice that this will cause the sink executor to drop DELETE messages and convert UPDATE messages to INSERT.",
+                        if syntax_legacy { "WITH" } else { "FORMAT ENCODE" }
+                    ),
                 )))
                     .into())
             }
