351dd49d8fff7bd6f65973fbe1c4f39eb98e7bf6
Jinser Kafka <aimer@purejs.icu>
2024-06-13 06:03:27 +0800

    devshell: provide env that RisingWave playground works

--- nix/devshell.nix
+++ nix/devshell.nix
@@ -1,13 +1,16 @@
+{ inputs
+, ...
+}:
+
 {
   perSystem = { pkgs, lib, ... }: {
     devshells.default =
       let
         rust-toolchain = with pkgs;
           [
-            ((rust-bin.fromRustupToolchainFile ../rust-toolchain).override
-              {
-                extensions = [ "rust-src" "rust-analyzer" ];
-              })
+            ((rust-bin.fromRustupToolchainFile ../rust-toolchain).override {
+              extensions = [ "rust-src" "rust-analyzer" ];
+            })
           ] ++ [
             cargo-make
             cargo-nextest
@@ -17,13 +20,40 @@
           ];
       in
       {
-        packages =
-          rust-toolchain
-          ++ (with pkgs; [
-            gnumake
-            cmake
-            protobuf
-          ]);
+        imports = [
+          "${inputs.devshell}/extra/language/rust.nix"
+        ];
+        language.rust.enableDefaultToolchain = false;
+        packages = rust-toolchain ++ (with pkgs; [
+          gcc
+          lld
+          protobuf
+          pkg-config
+
+          gnumake
+          cmake
+
+          tmux
+          postgresql
+        ]);
+        env = [
+          {
+            name = "PKG_CONFIG_PATH";
+            value = lib.concatStringsSep ":" (
+              map (pkg: "${pkg}/lib/pkgconfig") (with pkgs; [
+                openssl.dev
+                cyrus_sasl.dev
+              ])
+            );
+          }
+          {
+            name = "LD_LIBRARY_PATH";
+            value = lib.makeLibraryPath (with pkgs; [
+              openssl
+              libgcc.lib
+            ]);
+          }
+        ];
       };
   };
 }
