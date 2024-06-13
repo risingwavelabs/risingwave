9bb7fb9ef6e608df98e9ce510dd2f728b2517cea
Noel Kwan <noelkwan1998@gmail.com>
2024-06-13 10:39:27 +0800

    add fragment_id to ddl and actor_id to ddl

--- src/frontend/src/catalog/system_catalog/rw_catalog/rw_fragment_id_to_ddl.rs
+++ src/frontend/src/catalog/system_catalog/rw_catalog/rw_fragment_id_to_ddl.rs
@@ -18,22 +18,21 @@ use risingwave_frontend_macro::system_catalog;
 /// Provides a mapping from `actor_id` to its ddl info.
 #[system_catalog(
 view,
-"rw_catalog.rw_actor_id_to_ddl",
+"rw_catalog.rw_fragment_id_to_ddl",
 "with
-   actor_to_job_id as (select actor_id, table_id from rw_fragments f join rw_actors a on f.fragment_id = a.fragment_id),
-   job_id_to_mv as (select actor_id, d.id as job_id, schema_id, 'mv' as ddl_type, name from rw_materialized_views d join actor_to_job_id a on d.id = a.table_id),
-   job_id_to_sink as (select actor_id, d.id as job_id, schema_id, 'sink' as ddl_type, name from rw_sinks d join actor_to_job_id a on d.id = a.table_id),
-   job_id_to_source as (select actor_id, d.id as job_id, schema_id, 'source' as ddl_type, name from rw_sources d join actor_to_job_id a on d.id = a.table_id),
-   job_id_to_table as (select actor_id, d.id as job_id, schema_id, 'table' as ddl_type, name from rw_tables d join actor_to_job_id a on d.id = a.table_id)
+   job_id_to_mv as (select fragment_id, d.id as job_id, schema_id, 'mv' as ddl_type, name from rw_materialized_views d join rw_fragments f on d.id = f.table_id),
+   job_id_to_sink as (select fragment_id, d.id as job_id, schema_id, 'sink' as ddl_type, name from rw_sinks d join rw_fragments f on d.id = f.table_id),
+   job_id_to_source as (select fragment_id, d.id as job_id, schema_id, 'source' as ddl_type, name from rw_sources d join rw_fragments f on d.id = f.table_id),
+   job_id_to_table as (select fragment_id, d.id as job_id, schema_id, 'table' as ddl_type, name from rw_tables d join rw_fragments f on d.id = f.table_id)
    select * from job_id_to_mv
      union all select * from job_id_to_sink
        union all select * from job_id_to_source
          union all select * from job_id_to_table"
 )]
 #[derive(Fields)]
-struct RwActorIdToDdl {
+struct RwFragmentIdToDdl {
     #[primary_key]
-    actor_id: i32,
+    fragment_id: i32,
     job_id: i32,
     schema_id: i32,
     ddl_type: String,
