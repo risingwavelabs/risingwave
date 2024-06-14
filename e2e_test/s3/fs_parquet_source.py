21ad23abe0417a6b1d10f356aa9fb4934c101332
congyi <15605187270@163.com>
2024-06-14 15:14:44 +0800

    add ci

--- e2e_test/s3/fs_parquet_source.py
+++ e2e_test/s3/fs_parquet_source.py
@@ -1,42 +1,29 @@
 import os
 import sys
-import pandas as pd
-import pyarrow as pa
-import pyarrow.parquet as pq
 import random
 import psycopg2
-
+import json
+import pyarrow as pa
+import pyarrow.parquet as pq
+import pandas as pd
 from time import sleep
-from io import StringIO
 from minio import Minio
-from functools import partial
 
-def gen_data(file_num, row_nums):
-    schema = pa.schema([
-    ('id',  pa.int32()),
-    ('name', pa.string()),    
-    ('sex', pa.int32()),    
-        ])
-    id_data = [i for i in range(row_nums)]
-    id_column = pa.array(ids_data, type=pa.int32())
-    
-    name_data = [f'name{i}_for_test'for i in range(row_nums)]
-    name_column = pa.array(name_data, type=pa.string())
-    
-    sex_data = [i % 2 for i in range(row_nums)]
-    sex_column = pa.array(sex_data, type=pa.int32())
-    
-    batch = pa.RecordBatch.from_arrays(
-        [id_column, name_column, sex_column],
-        schema = schema
-    )
-    table = pa.Table.from_batches([batch])
- 
-# 写 Parquet 文件 plain.parquet
-pq.write_table(table, 'test_5.parquet')
-    
 
-def do_test(config, file_num, item_num_per_file, prefix, fmt):
+def gen_data(file_num, item_num_per_file):
+    assert item_num_per_file % 2 == 0, \
+        f'item_num_per_file should be even to ensure sum(mark) == 0: {item_num_per_file}'
+    return [
+        [{
+            'id': file_id * item_num_per_file + item_id,
+            'name': f'{file_id}_{item_id}_{file_id * item_num_per_file + item_id}',
+            'sex': item_id % 2,
+            'mark': (-1) ** (item_id % 2),
+        } for item_id in range(item_num_per_file)]
+        for file_id in range(file_num)
+    ]
+
+def do_test(config, file_num, item_num_per_file, prefix):
     conn = psycopg2.connect(
         host="localhost",
         port="4566",
@@ -48,17 +35,17 @@ def do_test(config, file_num, item_num_per_file, prefix, fmt):
     cur = conn.cursor()
 
     def _table():
-        return f's3_test_{fmt}'
+        return 's3_test_parquet'
 
     # Execute a SELECT statement
     cur.execute(f'''CREATE TABLE {_table()}(
-        id int,
+        id bigint,
         name TEXT,
-        sex int,
-        mark int,
+        sex bigint,
+        mark bigint,
     ) WITH (
         connector = 's3_v2',
-        match_pattern = '{prefix}*.{fmt}',
+        match_pattern = '*.parquet',
         s3.region_name = '{config['S3_REGION']}',
         s3.bucket_name = '{config['S3_BUCKET']}',
         s3.credentials.access = '{config['S3_ACCESS_KEY']}',
@@ -73,10 +60,10 @@ def do_test(config, file_num, item_num_per_file, prefix, fmt):
         result = cur.fetchone()
         if result[0] == total_rows:
             break
-        print(f"[retry {retry_no}] Now got {result[0]} rows in table, {total_rows} expected, wait 30s")
-        sleep(30)
+        print(f"[retry {retry_no}] Now got {result[0]} rows in table, {total_rows} expected, wait 10s")
+        sleep(10)
 
-    stmt = f'select count(*), sum(id), sum(sex), sum(mark) from {_table()}'
+    stmt = f'select count(*), sum(id) from {_table()}'
     print(f'Execute {stmt}')
     cur.execute(stmt)
     result = cur.fetchone()
@@ -88,8 +75,6 @@ def do_test(config, file_num, item_num_per_file, prefix, fmt):
 
     _assert_eq('count(*)', result[0], total_rows)
     _assert_eq('sum(id)', result[1], (total_rows - 1) * total_rows / 2)
-    _assert_eq('sum(sex)', result[2], total_rows / 2)
-    _assert_eq('sum(mark)', result[3], 0)
 
     print('Test pass')
 
@@ -99,18 +84,9 @@ def do_test(config, file_num, item_num_per_file, prefix, fmt):
 
 
 if __name__ == "__main__":
-    FILE_NUM = 3
-    ITEM_NUM_PER_FILE = 10000
+    FILE_NUM = 10
+    ITEM_NUM_PER_FILE = 2000
     data = gen_data(FILE_NUM, ITEM_NUM_PER_FILE)
-
-    fmt = sys.argv[1]
-    FORMATTER = {
-        'parquet': format_json,
-        'csv_with_header': partial(format_csv, with_header=True),
-        'csv_without_header': partial(format_csv, with_header=False),
-    }
-    assert fmt in FORMATTER, f"Unsupported format: {fmt}"
-    formatted_files = FORMATTER[fmt](data)
 
     config = json.loads(os.environ["S3_SOURCE_TEST_CONF"])
     client = Minio(
@@ -120,14 +96,13 @@ if __name__ == "__main__":
         secure=True,
     )
     run_id = str(random.randint(1000, 9999))
-    _local = lambda idx: f'data_{idx}.{fmt}'
-    _s3 = lambda idx: f"{run_id}_data_{idx}.{fmt}"
+    _local = lambda idx: f'data_{idx}.parquet'
+    _s3 = lambda idx: f"{run_id}_data_{idx}.parquet"
 
     # put s3 files
-    for idx, file_str in enumerate(formatted_files):
-        with open(_local(idx), "w") as f:
-            f.write(file_str)
-            os.fsync(f.fileno())
+    for idx, file_data in enumerate(data):
+        table = pa.Table.from_pandas(pd.DataFrame(file_data))
+        pq.write_table(table, _local(idx))
 
         client.fput_object(
             config["S3_BUCKET"],
@@ -136,8 +111,8 @@ if __name__ == "__main__":
         )
 
     # do test
-    do_test(config, FILE_NUM, ITEM_NUM_PER_FILE, run_id, fmt)
+    do_test(config, FILE_NUM, ITEM_NUM_PER_FILE, run_id)
 
     # clean up s3 files
-    for idx, _ in enumerate(formatted_files):
-        client.remove_object(config["S3_BUCKET"], _s3(idx))
+    for idx, _ in enumerate(data):
+       client.remove_object(config["S3_BUCKET"], _s3(idx))
