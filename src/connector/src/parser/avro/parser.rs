52874e8554459f8281028a60c434b5524e7d6dab
xxchan <xxchan22f@gmail.com>
2024-06-20 17:00:05 +0800

    cleanup avro

--- src/connector/src/parser/avro/parser.rs
+++ src/connector/src/parser/avro/parser.rs
@@ -19,12 +19,13 @@ use anyhow::Context;
 use apache_avro::types::Value;
 use apache_avro::{from_avro_datum, Reader, Schema};
 use risingwave_common::{bail, try_match_expand};
+use risingwave_connector_codec::decoder::avro::{
+    avro_schema_to_column_descs, AvroAccess, AvroParseOptions, ResolvedAvroSchema,
+};
 use risingwave_pb::plan_common::ColumnDesc;
 
-use super::schema_resolver::ConfluentSchemaCache;
-use super::util::{avro_schema_to_column_descs, ResolvedAvroSchema};
+use super::ConfluentSchemaCache;
 use crate::error::ConnectorResult;
-use crate::parser::unified::avro::{AvroAccess, AvroParseOptions};
 use crate::parser::unified::AccessImpl;
 use crate::parser::util::bytes_from_url;
 use crate::parser::{AccessBuilder, AvroProperties, EncodingProperties, EncodingType, MapHandling};
