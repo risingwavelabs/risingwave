34902d54bad5e379115ebb17bb343212deea5e54
xxchan <xxchan22f@gmail.com>
2024-06-21 00:02:53 +0800

    fix
    Signed-off-by: xxchan <xxchan22f@gmail.com>

--- src/object_store/src/object/mod.rs
+++ src/object_store/src/object/mod.rs
@@ -12,6 +12,11 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+#![expect(
+    unexpected_cfgs,
+    reason = "feature(hdfs-backend) is banned https://github.com/risingwavelabs/risingwave/pull/7875"
+)]
+
 pub mod sim;
 use std::ops::{Range, RangeBounds};
 use std::sync::Arc;
