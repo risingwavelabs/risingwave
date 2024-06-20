7df77a465af1dcd6ff1f7a477677ea9a9923e982
Shanicky Chen <peng@risingwave-labs.com>
2024-06-20 15:53:52 +0800

    fix: Fix the panic during parallel unit mapping conversion. (#17318)
    Co-authored-by: Shanicky Chen <>

--- src/meta/src/controller/streaming_job.rs
+++ src/meta/src/controller/streaming_job.rs
@@ -1443,7 +1443,7 @@ impl CatalogController {
             fragment.update(&txn).await?;
 
             let worker_slot_mapping = ParallelUnitMapping::from_protobuf(&vnode_mapping)
-                .to_worker_slot(&parallel_unit_to_worker)
+                .to_worker_slot(&parallel_unit_to_worker)?
                 .to_protobuf();
 
             fragment_mapping_to_notify.push(FragmentWorkerSlotMapping {
