9cb3eae05fdbee3c695e32c1b2bb6de286337289
stonepage <40830455+st1page@users.noreply.github.com>
2024-06-24 16:29:49 +0800

    fix: append only table with connector having retention seconds (#17387)

--- src/frontend/src/handler/create_table_as.rs
+++ src/frontend/src/handler/create_table_as.rs
@@ -90,19 +90,12 @@ pub async fn handle_create_as(
 
     let (graph, source, table) = {
         let context = OptimizerContext::from_handler_args(handler_args.clone());
-        let properties = handler_args
-            .with_options
-            .inner()
-            .clone()
-            .into_iter()
-            .collect();
         let (plan, table) = gen_create_table_plan_without_source(
             context,
             table_name.clone(),
             columns,
             vec![],
             vec![],
-            properties,
             "".to_owned(), // TODO: support `SHOW CREATE TABLE` for `CREATE TABLE AS`
             vec![],        // No watermark should be defined in for `CREATE TABLE AS`
             append_only,
