716c9b02f339eea5647d48a302da60562c423c2c
Noel Kwan <noelkwan1998@gmail.com>
2024-06-25 13:23:03 +0800

    use wait_streaming_job_finished to poll for notification version

--- src/meta/src/rpc/ddl_controller.rs
+++ src/meta/src/rpc/ddl_controller.rs
@@ -1330,8 +1330,9 @@ impl DdlController {
         };
 
         tracing::debug!(id = job_id, "finishing stream job");
-        // TODO(kwannoel): Poll the rx here.
-        let version = 0;
+        let version = self
+            .wait_streaming_job_finished_v1(mgr, stream_job.id())
+            .await?;
         tracing::debug!(id = job_id, "finished stream job");
 
         Ok(version)
