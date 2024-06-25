d8dea8de17c8c31193c4e703136babcaf85b5b7b
Bugen Zhao <i@bugenzhao.com>
2024-06-25 14:51:08 +0800

    add more unit tests
    Signed-off-by: Bugen Zhao <i@bugenzhao.com>

--- src/common/src/license/manager.rs
+++ src/common/src/license/manager.rs
@@ -35,6 +35,22 @@ pub enum Tier {
     Paid,
 }
 
+/// Issuer of the license.
+///
+/// The issuer must be `prod.risingwave.com` in production, and can be `test.risingwave.com` in
+/// development. This will be validated when refreshing the license key.
+#[derive(Debug, Clone, Deserialize)]
+pub enum Issuer {
+    #[serde(rename = "prod.risingwave.com")]
+    Prod,
+
+    #[serde(rename = "test.risingwave.com")]
+    Test,
+
+    #[serde(untagged)]
+    Unknown(String),
+}
+
 /// The content of a license.
 ///
 /// We use JSON Web Token (JWT) to represent the license. This struct is the payload.
@@ -43,14 +59,22 @@ pub enum Tier {
 #[serde(rename_all = "snake_case")]
 pub(super) struct License {
     /// Subject of the license.
+    ///
     /// See <https://tools.ietf.org/html/rfc7519#section-4.1.2>.
     #[allow(dead_code)]
     pub sub: String,
+
+    /// Issuer of the license.
+    ///
+    /// See <https://tools.ietf.org/html/rfc7519#section-4.1.1>.
+    #[allow(dead_code)]
+    pub iss: Issuer,
 
     /// Tier of the license.
     pub tier: Tier,
 
     /// Expiration time in seconds since UNIX epoch.
+    ///
     /// See <https://tools.ietf.org/html/rfc7519#section-4.1.4>.
     pub exp: u64,
 }
@@ -63,6 +87,7 @@ impl Default for License {
         Self {
             sub: "default".to_owned(),
             tier: Tier::Free,
+            iss: Issuer::Prod,
             exp: u64::MAX,
         }
     }
@@ -88,14 +113,18 @@ static PUBLIC_KEY: LazyLock<DecodingKey> = LazyLock::new(|| {
 });
 
 impl LicenseManager {
-    /// Get the singleton instance of the license manager.
-    pub fn get() -> &'static Self {
-        static INSTANCE: LazyLock<LicenseManager> = LazyLock::new(|| LicenseManager {
+    /// Create a new license manager with the default license.
+    fn new() -> Self {
+        Self {
             inner: RwLock::new(Inner {
                 license: Ok(License::default()),
             }),
-        });
+        }
+    }
 
+    /// Get the singleton instance of the license manager.
+    pub fn get() -> &'static Self {
+        static INSTANCE: LazyLock<LicenseManager> = LazyLock::new(LicenseManager::new);
         &INSTANCE
     }
 
@@ -147,3 +176,113 @@ pub const TEST_PAID_LICENSE_KEY: &str =
  "eyJhbGciOiJSUzUxMiIsInR5cCI6IkpXVCJ9.\
   eyJzdWIiOiJydy10ZXN0IiwidGllciI6InBhaWQiLCJpc3MiOiJ0ZXN0LnJpc2luZ3dhdmUuY29tIiwiZXhwIjo5OTk5OTk5OTk5fQ.\
   c6Gmb6xh3dBDYX_4cOnHUbwRXJbUCM7W3mrJA77nLC5FkoOLpGstzvQ7qfnPVBu412MFtKRDvh-Lk8JwG7pVa0WLw16DeHTtVHxZukMTZ1Q_ciZ1xKeUx_pwUldkVzv6c9j99gNqPSyTjzOXTdKlidBRLer2zP0v3Lf-ZxnMG0tEcIbTinTb3BNCtAQ8bwBSRP-X48cVTWafjaZxv_zGiJT28uV3bR6jwrorjVB4VGvqhsJi6Fd074XOmUlnOleoAtyzKvjmGC5_FvnL0ztIe_I0z_pyCMfWpyJ_J4C7rCP1aVWUImyoowLmVDA-IKjclzOW5Fvi0wjXsc6OckOc_A";
+
+// Tests below only work in debug mode.
+#[cfg(debug_assertions)]
+#[cfg(test)]
+mod tests {
+    use expect_test::expect;
+    use thiserror_ext::AsReport;
+
+    use super::*;
+
+    fn do_test(key: &str, expect: expect_test::Expect) {
+        let manager = LicenseManager::new();
+        manager.refresh(key);
+
+        match manager.license() {
+            Ok(license) => expect.assert_debug_eq(&license),
+            Err(error) => expect.assert_eq(&error.to_report_string()),
+        }
+    }
+
+    #[test]
+    fn test_paid_license_key() {
+        do_test(
+            TEST_PAID_LICENSE_KEY,
+            expect![[r#"
+                License {
+                    sub: "rw-test",
+                    iss: Test,
+                    tier: Paid,
+                    exp: 9999999999,
+                }
+            "#]],
+        );
+    }
+
+    #[test]
+    fn test_free_license_key() {
+        const KEY: &str =
+         "eyJhbGciOiJSUzUxMiIsInR5cCI6IkpXVCJ9.\
+          eyJzdWIiOiJydy10ZXN0IiwidGllciI6ImZyZWUiLCJpc3MiOiJ0ZXN0LnJpc2luZ3dhdmUuY29tIiwiZXhwIjo5OTk5OTk5OTk5fQ.\
+          ALC3Kc9LI6u0S-jeMB1YTxg1k8Azxwvc750ihuSZgjA_e1OJC9moxMvpLrHdLZDzCXHjBYi0XJ_1lowmuO_0iPEuPqN5AFpDV1ywmzJvGmMCMtw3A2wuN7hhem9OsWbwe6lzdwrefZLipyo4GZtIkg5ZdwGuHzm33zsM-X5gl_Ns4P6axHKiorNSR6nTAyA6B32YVET_FAM2YJQrXqpwA61wn1XLfarZqpdIQyJ5cgyiC33BFBlUL3lcRXLMLeYe6TjYGeV4K63qARCjM9yeOlsRbbW5ViWeGtR2Yf18pN8ysPXdbaXm_P_IVhl3jCTDJt9ctPh6pUCbkt36FZqO9A";
+
+        do_test(
+            KEY,
+            expect![[r#"
+                License {
+                    sub: "rw-test",
+                    iss: Test,
+                    tier: Free,
+                    exp: 9999999999,
+                }
+            "#]],
+        );
+    }
+
+    #[test]
+    fn test_empty_license_key() {
+        // Default license will be used.
+        do_test(
+            "",
+            expect![[r#"
+                License {
+                    sub: "default",
+                    iss: Prod,
+                    tier: Free,
+                    exp: 18446744073709551615,
+                }
+            "#]],
+        );
+    }
+
+    #[test]
+    fn test_invalid_license_key() {
+        const KEY: &str = "invalid";
+
+        do_test(KEY, expect!["invalid license key: InvalidToken"]);
+    }
+
+    #[test]
+    fn test_expired_license_key() {
+        // "exp": 0
+        const KEY: &str =
+         "eyJhbGciOiJSUzUxMiIsInR5cCI6IkpXVCJ9.\
+          eyJzdWIiOiJydy10ZXN0IiwidGllciI6InBhaWQiLCJpc3MiOiJ0ZXN0LnJpc2luZ3dhdmUuY29tIiwiZXhwIjowfQ.\
+          TyYmoT5Gw9-FN7DWDbeg3myW8g_3Xlc90i4M9bGuPf2WLv9zRMJy2r9J7sl1BO7t6F1uGgyrvNxsVRVZ2XF_WAs6uNlluYBnd4Cqvsj6Xny1XJCCo8II3RIea-ZlRjp6tc1saaoe-_eTtqDH8NIIWe73vVtBeBTBU4zAiN2vCtU_Si2XuoTLBKJMIjtn0HjLNhb6-DX2P3SCzp75tMyWzr49qcsBgratyKdu_v2kqBM1qw_dTaRg2ZeNNO6scSOBwu4YHHJTL4nUaZO2yEodI_OKUztIPLYuO2A33Fb5OE57S7LTgSzmxZLf7e23Vrck7Os14AfBQr7p9ncUeyIXhA";
+
+        do_test(KEY, expect!["invalid license key: ExpiredSignature"]);
+    }
+
+    #[test]
+    fn test_invalid_issuer() {
+        // "iss": "bad.risingwave.com"
+        const KEY: &str =
+         "eyJhbGciOiJSUzUxMiIsInR5cCI6IkpXVCJ9.\
+          eyJzdWIiOiJydy10ZXN0IiwidGllciI6ImZyZWUiLCJpc3MiOiJiYWQucmlzaW5nd2F2ZS5jb20iLCJleHAiOjk5OTk5OTk5OTl9.\
+          SUbDJTri902FbGgIoe5L3LG4edTXoR42BQCIu_KLyW41OK47bMnD2aK7JggyJmWyGtN7b_596hxM9HjU58oQtHePUo_zHi5li5IcRaMi8gqHae7CJGqOGAUo9vYOWCP5OjEuDfozJhpgcHBLzDRnSwYnWhLKtsrzb3UcpOXEqRVK7EDShBNx6kNqfYs2LlFI7ASsgFRLhoRuOTR5LeVDjj6NZfkZGsdMe1VyrODWoGT9kcAF--hBpUd1ZJ5mZ67A0_948VPFBYDbDPcTRnw1-5MvdibO-jKX49rJ0rlPXcAbqKPE_yYUaqUaORUzb3PaPgCT_quO9PWPuAFIgAb_fg";
+
+        do_test(KEY, expect!["invalid license key: InvalidIssuer"]);
+    }
+
+    #[test]
+    fn test_invalid_signature() {
+        const KEY: &str =
+        "eyJhbGciOiJSUzUxMiIsInR5cCI6IkpXVCJ9.\
+         eyJzdWIiOiJydy10ZXN0IiwidGllciI6ImZyZWUiLCJpc3MiOiJ0ZXN0LnJpc2luZ3dhdmUuY29tIiwiZXhwIjo5OTk5OTk5OTk5fQ.\
+         InvalidSignatureoe5L3LG4edTXoR42BQCIu_KLyW41OK47bMnD2aK7JggyJmWyGtN7b_596hxM9HjU58oQtHePUo_zHi5li5IcRaMi8gqHae7CJGqOGAUo9vYOWCP5OjEuDfozJhpgcHBLzDRnSwYnWhLKtsrzb3UcpOXEqRVK7EDShBNx6kNqfYs2LlFI7ASsgFRLhoRuOTR5LeVDjj6NZfkZGsdMe1VyrODWoGT9kcAF--hBpUd1ZJ5mZ67A0_948VPFBYDbDPcTRnw1-5MvdibO-jKX49rJ0rlPXcAbqKPE_yYUaqUaORUzb3PaPgCT_quO9PWPuAFIgAb_fg";
+
+        do_test(KEY, expect!["invalid license key: InvalidSignature"]);
+    }
+}
