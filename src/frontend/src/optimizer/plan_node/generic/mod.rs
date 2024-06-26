8b3128387a30173b74b965355d02451ad4af4b78
Xinhao Xu <84456268+xxhZs@users.noreply.github.com>
2024-06-26 19:24:02 +0800

    feat(steam): support stream changed log  (#17132)

--- src/frontend/src/optimizer/plan_node/generic/mod.rs
+++ src/frontend/src/optimizer/plan_node/generic/mod.rs
@@ -78,6 +78,8 @@ mod cte_ref;
 pub use cte_ref::*;
 mod recursive_union;
 pub use recursive_union::*;
+mod changelog;
+pub use changelog::*;
 
 pub trait DistillUnit {
     fn distill_with_name<'a>(&self, name: impl Into<Cow<'a, str>>) -> XmlNode<'a>;
