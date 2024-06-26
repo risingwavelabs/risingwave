0a4ab055bd67d4b272fec84ea9cf34a58f7bfebd
Noel Kwan <noelkwan1998@gmail.com>
2024-06-26 14:32:26 +0800

    add table_id_to_version map

--- src/meta/src/controller/catalog.rs
+++ src/meta/src/controller/catalog.rs
@@ -112,6 +112,7 @@ impl CatalogController {
             inner: RwLock::new(CatalogControllerInner {
                 db: meta_store.conn,
                 table_id_to_tx: HashMap::new(),
+                table_id_to_version: HashMap::new(),
             }),
         }
     }
@@ -129,7 +130,13 @@ impl CatalogController {
 
 pub struct CatalogControllerInner {
     pub(crate) db: DatabaseConnection,
+    /// `DdlController` will update this map, and pass the `tx` side to `CatalogController`.
+    /// On notifying, we can remove the entry from this map.
     pub table_id_to_tx: HashMap<TableId, Sender<MetaResult<NotificationVersion>>>,
+    /// Catalogs which were marked as finished and committed.
+    /// But the `DdlController` has not instantiated notification channel.
+    /// Once notified, we can remove the entry from this map.
+    pub table_id_to_version: HashMap<TableId, NotificationVersion>,
 }
 
 impl CatalogController {
