a2345f3528a3a454b097cbb4aa1e6c0adc224f15
August <pin@singularity-data.com>
2024-07-02 18:05:15 +0800

    fix: fix system_parameter value column type and remove unnecessary FKs in mysql

---
+++ src/meta/model_v2/migration/src/m20240702_080451_system_param_value.rs
@@ -0,0 +1,51 @@
+use sea_orm_migration::prelude::*;
+
+use crate::sea_orm::DbBackend;
+
+#[derive(DeriveMigrationName)]
+pub struct Migration;
+
+#[async_trait::async_trait]
+impl MigrationTrait for Migration {
+    async fn up(&self, manager: &SchemaManager) -> Result<(), DbErr> {
+        match manager.get_database_backend() {
+            DbBackend::MySql => {
+                manager
+                    .alter_table(
+                        Table::alter()
+                            .table(SystemParameter::Table)
+                            .modify_column(ColumnDef::new(SystemParameter::Value).text().not_null())
+                            .to_owned(),
+                    )
+                    .await?;
+            }
+            _ => {}
+        }
+        Ok(())
+    }
+
+    async fn down(&self, manager: &SchemaManager) -> Result<(), DbErr> {
+        match manager.get_database_backend() {
+            DbBackend::MySql => {
+                manager
+                    .alter_table(
+                        Table::alter()
+                            .table(SystemParameter::Table)
+                            .modify_column(
+                                ColumnDef::new(SystemParameter::Value).string().not_null(),
+                            )
+                            .to_owned(),
+                    )
+                    .await?;
+            }
+            _ => {}
+        }
+        Ok(())
+    }
+}
+
+#[derive(DeriveIden)]
+enum SystemParameter {
+    Table,
+    Value,
+}
