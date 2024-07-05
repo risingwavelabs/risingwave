407b1dcbf01d9e3cc5e586407747c57fa4650da2
Noel Kwan <noelkwan1998@gmail.com>
2024-07-05 15:07:53 +0800

    add script to find regression

---
+++ ci/scripts/find-regression.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python3
+
+import subprocess
+import os
+import sys
+from common import *
+
+def run_test_1():
+    test_map = get_test_map()
+    failed_test_map = get_failed_tests(get_mock_test_status, test_map)
+    message = generate_test_status_message(failed_test_map)
+    if message == "":
+        print("All tests passed, no need to notify")
+        return
+    else:
+        print("Some tests failed, notify users")
+        print(message)
+        cmd = format_cmd(message)
+        print(cmd)
+
+# Gets the last passing build from the day before, on the main branch.
+def get_last_passing_build_commit():
+    return
+
+def get_commits_between(last_passing_build_commit, current_build_commit):
+    return
+
+def run_pipeline_on_commit(test_commit, failed_test_map):
+    return
+
+def run(failing_test_key):
+    test_map = get_test_map()
+    failed_test_map = get_failed_tests(get_buildkite_test_status, test_map)
+    last_passing_build_commit = get_last_passing_build_commit()
+    current_build_commit = os.environ['BUILDKITE_COMMIT']
+    test_commits = get_commits_between(last_passing_build_commit, current_build_commit)
+    start = 0
+    end = len(test_commits) - 1 # Exclude the current commit
+    test_commit = None
+    result = None
+    # binary search the commits
+    while start < end:
+        mid = (start + end) // 2
+        test_commit = test_commits[mid]
+        result = run_pipeline_on_commit(test_commit, failed_test_map)
+        if result:
+            start = mid + 1
+        else:
+            end = mid
+
+    if test_commit is None:
+        print("No regression found")
+        return
+
+    print(f"Regression found at commit {test_commit}")
+
+def main():
+    if len(sys.argv) < 2:
+        print("Usage: find-regression.py <failing_test_key>")
+        sys.exit(1)
+    failing_test_key = os.environ['FAILED_TEST_KEY']
+    run(failing_test_key)
