5c7b556ea60d136c5bccf1b1f7e313d2f9c79ef0
Noel Kwan <noelkwan1998@gmail.com>
2024-07-08 16:18:44 +0800

    try

--- ci/scripts/find-regression.py
+++ ci/scripts/find-regression.py
@@ -5,55 +5,41 @@ import os
 import sys
 from common import *
 
-def run_test_1():
-    test_map = get_test_map()
-    failed_test_map = get_failed_tests(get_mock_test_status, test_map)
-    message = generate_test_status_message(failed_test_map)
-    if message == "":
-        print("All tests passed, no need to notify")
-        return
-    else:
-        print("Some tests failed, notify users")
-        print(message)
-        cmd = format_cmd(message)
-        print(cmd)
-
-# Gets the last passing build from the day before, on the main branch.
-def get_last_passing_build_commit():
-    return
-
-def get_commits_between(last_passing_build_commit, current_build_commit):
-    return
-
-
-def format_step(branch, commit, steps):
+def format_step(env, branch, commit, steps):
     print(f"Running pipeline on commit: {commit} with steps: {steps}")
-    step=f"""
+    step=f'''
 cat <<- YAML | buildkite-agent pipeline upload
 steps:
-  - trigger: "main-cron"
+  - label: "run-{commit}"
+    key: "run-{commit}"
+    trigger: "main-cron"
     build:
       branch: {branch}
       commit: {commit}
       env:
         CI_STEPS: {steps}
+  - wait
+  - label: 'check'
+    command: |
+      if [ $(buildkite-agent step get "outcome" --step "run-{commit}") == "hard_failed" ]; then
+        START_COMMIT={env["START_COMMIT"]} END_COMMIT={env["END_COMMIT"]} BUILDKITE_BRANCH={env["BRANCH"]} BISECT_STEPS={env["BISECT_STEPS"]} ci/scripts/find-regression.py failed 
+      else
+        START_COMMIT={env["START_COMMIT"]} END_COMMIT={env["END_COMMIT"]} BUILDKITE_BRANCH={env["BRANCH"]} BISECT_STEPS={env["BISECT_STEPS"]} ci/scripts/find-regression.py passed 
+      fi
 YAML
-        """
+        '''
     return step
 
 
 # Triggers a buildkite job to run the pipeline on the given commit, with the specified tests.
-def run_pipeline_on_commit(branch, commit, steps):
-    step = format_step(branch, commit, steps)
+def run_pipeline_on_commit(env, branch, commit, steps):
+    step = format_step(env, branch, commit, steps)
     print(f"Running upload pipeline: step={step}")
     subprocess.run(step, shell=True)
 
 def run(failing_test_key):
     test_map = get_test_map()
-    failed_test_map = get_failed_tests(get_buildkite_test_status, test_map)
-    last_passing_build_commit = get_last_passing_build_commit()
     current_build_commit = os.environ['BUILDKITE_COMMIT']
-    test_commits = get_commits_between(last_passing_build_commit, current_build_commit)
     start = 0
     end = len(test_commits) - 1 # Exclude the current commit
     test_commit = None
@@ -74,24 +60,33 @@ def run(failing_test_key):
 
     print(f"Regression found at commit {test_commit}")
 
-# def main():
-#     if len(sys.argv) < 2:
-#         print("Usage: find-regression.py <failing_test_key>")
-#         sys.exit(1)
-#     failing_test_key = os.environ['FAILED_TEST_KEY']
-#     run(failing_test_key)
-
 def main():
-    start_commit = os.environ['START_COMMIT']
-    end_commit = os.environ['END_COMMIT']
-    branch = os.environ['BUILDKITE_BRANCH']
-    steps = os.environ['BISECT_STEPS']
+    env = {
+        "START_COMMIT": os.environ['START_COMMIT'],
+        "END_COMMIT": os.environ['END_COMMIT'],
+        "BRANCH": os.environ['BUILDKITE_BRANCH'],
+        "BISECT_STEPS": os.environ['BISECT_STEPS'],
+        "STATE": sys.argv[1],
+    }
+
     print(f'''
-    START_COMMIT: {start_commit}
-    END_COMMIT: {end_commit}
-    BRANCH: {branch}
-    BISECT_STEPS: {steps}
+    START_COMMIT: {env["START_COMMIT"]}
+    END_COMMIT: {env["END_COMMIT"]}
+    BRANCH: {env["BRANCH"]}
+    BISECT_STEPS: {env["BISECT_STEPS"]}
+    STATE: {env["STATE"]}
     ''')
-    run_pipeline_on_commit("kwannoel/find-regress", "f0fa34cdeed95a08b2c7d8428a17d6de27b6588d", "e2e-test")
+
+    if env["STATE"] == "start":
+        print("start")
+        run_pipeline_on_commit(env, "kwannoel/find-regress", "f0fa34cdeed95a08b2c7d8428a17d6de27b6588d", "e2e-test")
+    elif env["STATE"] == "failed":
+        print("failed")
+    elif env["STATE"] == "passed":
+        print("passed")
+    else:
+        print("Invalid type")
+        sys.exit(1)
+
 
 main()
