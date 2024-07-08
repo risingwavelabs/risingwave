15ecdef599bf04579ac8ff12614d4809854a52a7
Bugen Zhao <i@bugenzhao.com>
2024-07-08 14:00:27 +0800

    dummy election client
    Signed-off-by: Bugen Zhao <i@bugenzhao.com>

--- src/meta/src/rpc/election/mod.rs
+++ src/meta/src/rpc/election/mod.rs
@@ -15,7 +15,7 @@ pub mod etcd;
 pub mod sql;
 
 use serde::Serialize;
-use tokio::sync::watch::Receiver;
+use tokio::sync::watch::{self, Receiver};
 
 use crate::MetaResult;
 
@@ -39,4 +39,50 @@ pub trait ElectionClient: Send + Sync + 'static {
     async fn leader(&self) -> MetaResult<Option<ElectionMember>>;
     async fn get_members(&self) -> MetaResult<Vec<ElectionMember>>;
     async fn is_leader(&self) -> bool;
+}
+
+pub struct DummyElectionClient {
+    id: String,
+    dummy_channel: watch::Sender<bool>,
+}
+
+impl DummyElectionClient {
+    pub fn new(id: String) -> Self {
+        let dummy_channel = watch::channel(true).0;
+        Self { id, dummy_channel }
+    }
+
+    fn self_member(&self) -> ElectionMember {
+        ElectionMember {
+            id: self.id.clone(),
+            is_leader: true,
+        }
+    }
+}
+
+#[async_trait::async_trait]
+impl ElectionClient for DummyElectionClient {
+    fn id(&self) -> MetaResult<String> {
+        Ok(self.id.clone())
+    }
+
+    async fn run_once(&self, _ttl: i64, _stop: Receiver<()>) -> MetaResult<()> {
+        futures::future::pending().await
+    }
+
+    fn subscribe(&self) -> Receiver<bool> {
+        self.dummy_channel.subscribe()
+    }
+
+    async fn leader(&self) -> MetaResult<Option<ElectionMember>> {
+        Ok(Some(self.self_member()))
+    }
+
+    async fn get_members(&self) -> MetaResult<Vec<ElectionMember>> {
+        Ok(vec![self.self_member()])
+    }
+
+    async fn is_leader(&self) -> bool {
+        true
+    }
 }
