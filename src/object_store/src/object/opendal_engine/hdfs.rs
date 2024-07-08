146859c20aae860d8926d9c3954e88c368532de0
congyi <15605187270@163.com>
2024-07-08 15:42:49 +0800

    update more fix

--- src/object_store/src/object/opendal_engine/hdfs.rs
+++ src/object_store/src/object/opendal_engine/hdfs.rs
@@ -20,7 +20,7 @@ use opendal::Operator;
 use risingwave_common::config::ObjectStoreConfig;
 
 use super::{EngineType, OpendalObjectStore};
-use crate::object::opendal_engine::ATOMIC_WRITE_DIR;
+// use crate::object::opendal_engine::ATOMIC_WRITE_DIR;
 use crate::object::ObjectResult;
 
 impl OpendalObjectStore {
@@ -43,10 +43,11 @@ impl OpendalObjectStore {
         // Set the name node for hdfs.
         builder.name_node(&namenode);
         builder.root(&root);
-        if config.set_atomic_write_dir {
-            let atomic_write_dir = format!("{}/{}", root, ATOMIC_WRITE_DIR);
-            builder.atomic_write_dir(&atomic_write_dir);
-        }
+        // todo: reopen the following lines after https://github.com/apache/opendal/issues/4867 is resolved.
+        // if config.set_atomic_write_dir {
+        //     let atomic_write_dir = format!("{}/{}", root, ATOMIC_WRITE_DIR);
+        //     builder.atomic_write_dir(&atomic_write_dir);
+        // }
         let op: Operator = Operator::new(builder)?
             .layer(LoggingLayer::default())
             .finish();
