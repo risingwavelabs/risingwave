167e938f9a3817870a1688ac972fb17c2a2d4a57
Kexiang Wang <kx.wang@hotmail.com>
2024-07-18 22:26:31 -0400

    fix(cdc): fix uppercase-identifier of pgcdc

--- java/connector-node/risingwave-connector-service/src/main/java/com/risingwave/connector/source/common/PostgresValidator.java
+++ java/connector-node/risingwave-connector-service/src/main/java/com/risingwave/connector/source/common/PostgresValidator.java
@@ -180,7 +180,8 @@ public class PostgresValidator extends DatabaseValidator implements AutoCloseabl
         // check primary key
         // reference: https://wiki.postgresql.org/wiki/Retrieve_primary_key_columns
         try (var stmt = jdbcConnection.prepareStatement(ValidatorUtils.getSql("postgres.pk"))) {
-            stmt.setString(1, this.schemaName + "." + this.tableName);
+            stmt.setString(1, schemaName);
+            stmt.setString(2, tableName);
             var res = stmt.executeQuery();
             var pkFields = new HashSet<String>();
             while (res.next()) {
