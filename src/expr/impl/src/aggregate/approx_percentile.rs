686983bd194ee353cdd617b8c157c147e1457e01
Noel Kwan <noelkwan1998@gmail.com>
2024-07-25 13:57:22 +0800

    add state for single phase approx percentile + pass relative_error

--- src/expr/impl/src/aggregate/approx_percentile.rs
+++ src/expr/impl/src/aggregate/approx_percentile.rs
@@ -12,6 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+use std::collections::BTreeMap;
 use std::ops::Range;
 
 use risingwave_common::array::*;
@@ -22,19 +23,52 @@ use risingwave_expr::{build_aggregate, Result};
 
 #[build_aggregate("approx_percentile(float8) -> float8")]
 fn build(agg: &AggCall) -> Result<Box<dyn AggregateFunction>> {
-    let fraction = agg.direct_args[0]
+    let quantile = agg.direct_args[0]
         .literal()
-        .map(|x| (*x.as_float64()).into());
-    Ok(Box::new(ApproxPercentile { fraction }))
+        .map(|x| (*x.as_float64()).into())
+        .unwrap();
+    let relative_error = agg.direct_args[1]
+        .literal()
+        .map(|x| (*x.as_float64()).into())
+        .unwrap();
+    Ok(Box::new(ApproxPercentile {
+        quantile,
+        relative_error,
+    }))
 }
 
 #[allow(dead_code)]
 pub struct ApproxPercentile {
-    fraction: Option<f64>,
+    quantile: f64,
+    relative_error: f64,
 }
 
-#[derive(Debug, Default, EstimateSize)]
-struct State(Vec<f64>);
+type BucketCount = u64;
+type BucketId = u64;
+type Count = u64;
+
+#[derive(Debug, Default)]
+struct State {
+    count: BucketCount,
+    buckets: BTreeMap<BucketId, Count>,
+}
+
+impl State {
+    fn new() -> Self {
+        Self {
+            count: 0,
+            buckets: BTreeMap::new(),
+        }
+    }
+}
+
+impl EstimateSize for State {
+    fn estimated_heap_size(&self) -> usize {
+        let count_size = 1;
+        let bucket_size = self.buckets.len() * 2;
+        count_size + bucket_size
+    }
+}
 
 impl AggStateDyn for State {}
 
