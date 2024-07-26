d0338e94ea3ac2dc97cf5c67f91ed0770d40527d
WanYixian <150207222+WanYixian@users.noreply.github.com>
2024-07-26 17:53:10 +0800

    Update java/connector-node/risingwave-sink-mock-flink/risingwave-sink-mock-flink-common/src/main/java/com/risingwave/mock/flink/common/FlinkDynamicUtil.java
    Co-authored-by: Richard Chien <stdrc@outlook.com>

--- java/connector-node/risingwave-sink-mock-flink/risingwave-sink-mock-flink-common/src/main/java/com/risingwave/mock/flink/common/FlinkDynamicUtil.java
+++ java/connector-node/risingwave-sink-mock-flink/risingwave-sink-mock-flink-common/src/main/java/com/risingwave/mock/flink/common/FlinkDynamicUtil.java
@@ -120,7 +120,7 @@ public class FlinkDynamicUtil {
                 throw Status.FAILED_PRECONDITION
                         .withDescription(
                                 String.format(
-                                        "Name mismatch. RisingWave is %s", columnDesc.getName()))
+                                        "Name mismatch. Column `%s` on RisingWave side is not found on Flink side.", columnDesc.getName()))
                         .asRuntimeException();
             }
             if (!checkType(columnDesc.getDataType(), flinkColumnMap.get(columnDesc.getName()))) {
