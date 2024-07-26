cb7ce18d5fca4c04e40c3cb8aaff62825d25f1d6
Noel Kwan <noelkwan1998@gmail.com>
2024-07-26 11:16:52 +0800

    add zero, neg buckets

--- src/expr/impl/src/aggregate/approx_percentile.rs
+++ src/expr/impl/src/aggregate/approx_percentile.rs
@@ -13,6 +13,7 @@
 // limitations under the License.
 
 use std::collections::BTreeMap;
+use std::mem::size_of;
 use std::ops::Range;
 
 use risingwave_common::array::*;
@@ -26,7 +27,6 @@ use risingwave_expr::{build_aggregate, Result};
 /// For two phase agg, we still use `DDSketch`.
 /// Then we also need to store the `relative_error` of the sketch, so we can report it
 /// in an internal table, if it changes.
-/// TODO(kwannoel): We also need to test 0 < x < 1 range.
 #[build_aggregate("approx_percentile(float8) -> float8", state = "bytea")]
 fn build(agg: &AggCall) -> Result<Box<dyn AggregateFunction>> {
     let quantile = agg.direct_args[0]
@@ -51,18 +51,21 @@ type BucketCount = u64;
 type BucketId = i32;
 type Count = u64;
 
-// TODO(kwannoel): handle `0` and negative values.
 #[derive(Debug, Default)]
 struct State {
     count: BucketCount,
-    buckets: BTreeMap<BucketId, Count>,
+    pos_buckets: BTreeMap<BucketId, Count>,
+    zeros: Count,
+    neg_buckets: BTreeMap<BucketId, Count>,
 }
 
 impl EstimateSize for State {
     fn estimated_heap_size(&self) -> usize {
         let count_size = 1;
-        let bucket_size = self.buckets.len() * 2;
-        count_size + bucket_size
+        let pos_buckets_size = self.pos_buckets.len() * 2;
+        let zero_bucket_size = size_of::<Count>();
+        let neg_buckets_size = self.pos_buckets.len() * 2;
+        count_size + pos_buckets_size + zero_bucket_size + neg_buckets_size
     }
 }
 
@@ -72,16 +75,35 @@ impl ApproxPercentile {
     fn add_datum(&self, state: &mut State, op: Op, datum: DatumRef<'_>) {
         if let Some(value) = datum {
             let prim_value = value.into_float64().into_inner();
-            let bucket_id = prim_value.log(self.base).ceil() as BucketId;
+            let (non_neg, abs_value) = if prim_value < 0.0 {
+                (false, -prim_value)
+            } else {
+                (true, prim_value)
+            };
+            let bucket_id = abs_value.log(self.base).ceil() as BucketId;
             match op {
                 Op::Delete | Op::UpdateDelete => {
-                    let count = state.buckets.entry(bucket_id).or_insert(0);
-                    *count -= 1;
+                    if abs_value == 0.0 {
+                        state.zeros -= 1;
+                    } else if non_neg {
+                        let count = state.pos_buckets.entry(bucket_id).or_insert(0);
+                        *count -= 1;
+                    } else {
+                        let count = state.neg_buckets.entry(bucket_id).or_insert(0);
+                        *count -= 1;
+                    }
                     state.count -= 1;
                 }
                 Op::Insert | Op::UpdateInsert => {
-                    let count = state.buckets.entry(bucket_id).or_insert(0);
-                    *count += 1;
+                    if abs_value == 0.0 {
+                        state.zeros += 1;
+                    } else if non_neg {
+                        let count = state.pos_buckets.entry(bucket_id).or_insert(0);
+                        *count += 1;
+                    } else {
+                        let count = state.neg_buckets.entry(bucket_id).or_insert(0);
+                        *count += 1;
+                    }
                     state.count += 1;
                 }
             }
@@ -127,7 +149,20 @@ impl AggregateFunction for ApproxPercentile {
         let state = state.downcast_ref::<State>();
         let quantile_count = (state.count as f64 * self.quantile) as u64;
         let mut acc_count = 0;
-        for (bucket_id, count) in &state.buckets {
+        for (bucket_id, count) in state.neg_buckets.iter().rev() {
+            acc_count += count;
+            if acc_count >= quantile_count {
+                // approx value = -2 * y^i / (y + 1)
+                let approx_percentile = -2.0 * self.base.powi(*bucket_id) / (self.base + 1.0);
+                let approx_percentile = ScalarImpl::Float64(approx_percentile.into());
+                return Ok(Datum::from(approx_percentile));
+            }
+        }
+        acc_count += state.zeros;
+        if acc_count >= quantile_count {
+            return Ok(Datum::from(ScalarImpl::Float64(0.0.into())));
+        }
+        for (bucket_id, count) in &state.pos_buckets {
             acc_count += count;
             if acc_count >= quantile_count {
                 // approx value = 2 * y^i / (y + 1)
@@ -143,7 +178,15 @@ impl AggregateFunction for ApproxPercentile {
         let state = state.downcast_ref::<State>();
         let mut encoded_state = Vec::with_capacity(state.estimated_heap_size());
         encoded_state.extend_from_slice(&state.count.to_be_bytes());
-        for (bucket_id, count) in &state.buckets {
+        encoded_state.extend_from_slice(&state.zeros.to_be_bytes());
+        let neg_buckets_size =
+            state.neg_buckets.len() * (size_of::<BucketId>() + size_of::<Count>());
+        encoded_state.extend_from_slice(&neg_buckets_size.to_be_bytes());
+        for (bucket_id, count) in &state.neg_buckets {
+            encoded_state.extend_from_slice(&bucket_id.to_be_bytes());
+            encoded_state.extend_from_slice(&count.to_be_bytes());
+        }
+        for (bucket_id, count) in &state.pos_buckets {
             encoded_state.extend_from_slice(&bucket_id.to_be_bytes());
             encoded_state.extend_from_slice(&count.to_be_bytes());
         }
@@ -160,13 +203,26 @@ impl AggregateFunction for ApproxPercentile {
         let mut cursor = 0;
         state.count = u64::from_be_bytes(encoded_state[cursor..cursor + 8].try_into().unwrap());
         cursor += 8;
+        state.zeros = u64::from_be_bytes(encoded_state[cursor..cursor + 8].try_into().unwrap());
+        cursor += 8;
+        let neg_buckets_size =
+            usize::from_be_bytes(encoded_state[cursor..cursor + 8].try_into().unwrap());
+        cursor += 8;
+        while cursor < neg_buckets_size {
+            let bucket_id =
+                i32::from_be_bytes(encoded_state[cursor..cursor + 4].try_into().unwrap());
+            cursor += 4;
+            let count = u64::from_be_bytes(encoded_state[cursor..cursor + 8].try_into().unwrap());
+            cursor += 8;
+            state.neg_buckets.insert(bucket_id, count);
+        }
         while cursor < encoded_state.len() {
             let bucket_id =
                 i32::from_be_bytes(encoded_state[cursor..cursor + 4].try_into().unwrap());
             cursor += 4;
             let count = u64::from_be_bytes(encoded_state[cursor..cursor + 8].try_into().unwrap());
             cursor += 8;
-            state.buckets.insert(bucket_id, count);
+            state.pos_buckets.insert(bucket_id, count);
         }
         Ok(AggregateState::Any(Box::new(state)))
     }
