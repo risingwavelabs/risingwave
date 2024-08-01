1c5d4561f966e12eb7412d7b042628c3ae43b2b1
Bugen Zhao <i@bugenzhao.com>
2024-08-01 16:49:38 +0800

    add oltp embedded back
    Signed-off-by: Bugen Zhao <i@bugenzhao.com>

--- src/meta/node/src/server.rs
+++ src/meta/node/src/server.rs
@@ -17,7 +17,7 @@ use std::time::Duration;
 
 use anyhow::Context;
 use etcd_client::ConnectOptions;
-// use otlp_embedded::TraceServiceServer;
+use otlp_embedded::TraceServiceServer;
 use regex::Regex;
 use risingwave_common::monitor::{RouterExt, TcpConfig};
 use risingwave_common::secret::LocalSecretManager;
@@ -482,11 +482,11 @@ pub async fn start_service_as_election_leader(
         prometheus_selector.clone(),
     ));
 
-    // let trace_state = otlp_embedded::State::new(otlp_embedded::Config {
-    //     max_length: opts.cached_traces_num,
-    //     max_memory_usage: opts.cached_traces_memory_limit_bytes,
-    // });
-    // let trace_srv = otlp_embedded::TraceServiceImpl::new(trace_state.clone());
+    let trace_state = otlp_embedded::State::new(otlp_embedded::Config {
+        max_length: opts.cached_traces_num,
+        max_memory_usage: opts.cached_traces_memory_limit_bytes,
+    });
+    let trace_srv = otlp_embedded::TraceServiceImpl::new(trace_state.clone());
 
     #[cfg(not(madsim))]
     let _dashboard_task = if let Some(ref dashboard_addr) = address_info.dashboard_addr {
@@ -497,7 +497,7 @@ pub async fn start_service_as_election_leader(
             metadata_manager: metadata_manager.clone(),
             compute_clients: ComputeClientPool::new(1), // typically no need for plural clients
             diagnose_command,
-            // trace_state,
+            trace_state,
         };
         let task = tokio::spawn(dashboard_service.serve());
         Some(task)
@@ -772,7 +772,7 @@ pub async fn start_service_as_election_leader(
         risingwave_pb::meta::event_log::Event::MetaNodeStart(event),
     ]);
 
-    let server = tonic::transport::Server::builder()
+    let server_builder = tonic::transport::Server::builder()
         .layer(MetricsMiddlewareLayer::new(meta_metrics))
         .layer(TracingExtractLayer::new())
         .add_service(HeartbeatServiceServer::new(heartbeat_srv))
@@ -794,17 +794,19 @@ pub async fn start_service_as_election_leader(
         .add_service(ServingServiceServer::new(serving_srv))
         .add_service(CloudServiceServer::new(cloud_srv))
         .add_service(SinkCoordinationServiceServer::new(sink_coordination_srv))
-        .add_service(EventLogServiceServer::new(event_log_srv))
-        // .add_service(TraceServiceServer::new(trace_srv))
-        .monitored_serve_with_shutdown(
-            address_info.listen_addr,
-            "grpc-meta-leader-service",
-            TcpConfig {
-                tcp_nodelay: true,
-                keepalive_duration: None,
-            },
-            shutdown.clone().cancelled_owned(),
-        );
+        .add_service(EventLogServiceServer::new(event_log_srv));
+    #[cfg(not(madsim))] // `otlp-embedded` does not use madsim-patched tonic
+    let server_builder = server_builder.add_service(TraceServiceServer::new(trace_srv));
+
+    let server = server_builder.monitored_serve_with_shutdown(
+        address_info.listen_addr,
+        "grpc-meta-leader-service",
+        TcpConfig {
+            tcp_nodelay: true,
+            keepalive_duration: None,
+        },
+        shutdown.clone().cancelled_owned(),
+    );
     started::set();
     let _server_handle = tokio::spawn(server);
 
