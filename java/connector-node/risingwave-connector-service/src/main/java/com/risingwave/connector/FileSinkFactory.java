530d8b591406d60113c36e65cc7be30722ee89b7
congyi <15605187270@163.com>
2024-08-02 14:42:59 +0800

    bring back java file sink

---
+++ java/connector-node/risingwave-connector-service/src/main/java/com/risingwave/connector/FileSinkFactory.java
@@ -0,0 +1,41 @@
+// Copyright 2024 RisingWave Labs
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.risingwave.connector;
+
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.risingwave.connector.api.TableSchema;
+import com.risingwave.connector.api.sink.SinkFactory;
+import com.risingwave.connector.api.sink.SinkWriter;
+import com.risingwave.connector.api.sink.SinkWriterV1;
+import com.risingwave.proto.Catalog.SinkType;
+import java.util.Map;
+
+public class FileSinkFactory implements SinkFactory {
+    @Override
+    public SinkWriter createWriter(TableSchema tableSchema, Map<String, String> tableProperties) {
+        ObjectMapper mapper = new ObjectMapper();
+        FileSinkConfig config = mapper.convertValue(tableProperties, FileSinkConfig.class);
+        return new SinkWriterV1.Adapter(new FileSink(config, tableSchema));
+    }
+
+    @Override
+    public void validate(
+            TableSchema tableSchema, Map<String, String> tableProperties, SinkType sinkType) {
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.configure(DeserializationFeature.FAIL_ON_MISSING_CREATOR_PROPERTIES, true);
+        mapper.convertValue(tableProperties, FileSinkConfig.class);
+    }
+}
