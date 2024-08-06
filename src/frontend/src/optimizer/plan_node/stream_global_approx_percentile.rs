84c656f2fa5eb265dfbb8c6f29e6fc324142acdb
Noel Kwan <noelkwan1998@gmail.com>
2024-08-06 14:18:02 +0800

    fix comments

--- src/frontend/src/optimizer/plan_node/stream_global_approx_percentile.rs
+++ src/frontend/src/optimizer/plan_node/stream_global_approx_percentile.rs
@@ -109,8 +109,8 @@ impl StreamNode for StreamGlobalApproxPercentile {
         bucket_table_builder.add_column(&Field::with_name(DataType::Int16, "sign"));
         bucket_table_builder.add_column(&Field::with_name(DataType::Int32, "bucket_id"));
         bucket_table_builder.add_column(&Field::with_name(DataType::Int64, "count"));
-        bucket_table_builder.add_order_column(0, OrderType::ascending()); // bucket_id
-        bucket_table_builder.add_order_column(1, OrderType::ascending()); // sign
+        bucket_table_builder.add_order_column(0, OrderType::ascending()); // sign
+        bucket_table_builder.add_order_column(1, OrderType::ascending()); // bucket_id
 
         // setup table: total_count
         let mut count_table_builder = TableCatalogBuilder::default();
