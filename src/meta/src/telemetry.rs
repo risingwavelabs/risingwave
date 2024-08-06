47edc46ee9cf7a056facf6b25bb1758b2ff77f77
tabVersion <tabvision@bupt.icu>
2024-08-06 16:15:41 +0800

    fix
    Signed-off-by: tabVersion <tabvision@bupt.icu>

--- src/meta/src/telemetry.rs
+++ src/meta/src/telemetry.rs
@@ -24,7 +24,9 @@ use risingwave_common::telemetry::{
 };
 use risingwave_common::{GIT_SHA, RW_VERSION};
 use risingwave_pb::common::WorkerType;
-use risingwave_pb::telemetry::{PbTelemetryDatabaseObject, PbTelemetryClusterType, PbTelemetryEventStage};
+use risingwave_pb::telemetry::{
+    PbTelemetryClusterType, PbTelemetryDatabaseObject, PbTelemetryEventStage,
+};
 use serde::{Deserialize, Serialize};
 use thiserror_ext::AsReport;
 
