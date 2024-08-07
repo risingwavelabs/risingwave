0dee4393083f70370f6301cdb45859cf55ba07b7
StrikeW <wangsiyuanse@gmail.com>
2024-08-07 15:13:31 +0800

    minor

--- src/cmd_all/src/standalone.rs
+++ src/cmd_all/src/standalone.rs
@@ -504,7 +504,7 @@ mod test {
                                 ],
                             ),
                             prometheus_listener_addr: "127.0.0.1:1234",
-                            health_check_listener_addr: "127.0.0.1:6786",
+                            frontend_rpc_listener_addr: "127.0.0.1:6786",
                             config_path: "src/config/test.toml",
                             metrics_level: None,
                             enable_barrier_read: None,
