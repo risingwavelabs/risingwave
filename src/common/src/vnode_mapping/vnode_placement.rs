deefc9a558990d3b40279931392873c31065a67d
StrikeW <wangsiyuanse@gmail.com>
2024-08-14 20:57:32 +0800

    fix

--- src/common/src/vnode_mapping/vnode_placement.rs
+++ src/common/src/vnode_mapping/vnode_placement.rs
@@ -232,7 +232,7 @@ mod tests {
         let worker_1 = WorkerNode {
             id: 1,
             parallelism: 1,
-            property: Some(serving_property),
+            property: Some(serving_property.clone()),
             ..Default::default()
         };
 
@@ -247,7 +247,7 @@ mod tests {
         let worker_2 = WorkerNode {
             id: 2,
             parallelism: 50,
-            property: Some(serving_property),
+            property: Some(serving_property.clone()),
             ..Default::default()
         };
 
@@ -266,7 +266,7 @@ mod tests {
         let worker_3 = WorkerNode {
             id: 3,
             parallelism: 60,
-            property: Some(serving_property),
+            property: Some(serving_property.clone()),
             ..Default::default()
         };
         let re_pu_mapping_2 = place_vnode(
