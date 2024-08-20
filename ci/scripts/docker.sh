1205935f533b9550fbe7affa8aa2c1e3ae28687a
Noel Kwan <noelkwan1998@gmail.com>
2024-08-20 14:02:25 +0800

    add docker no lto

--- ci/scripts/docker.sh
+++ ci/scripts/docker.sh
@@ -6,6 +6,7 @@ set -euo pipefail
 ghcraddr="ghcr.io/risingwavelabs/risingwave"
 dockerhubaddr="risingwavelabs/risingwave"
 arch="$(uname -m)"
+CARGO_BUILD_PROFILE=${CARGO_BUILD_PROFILE:-production}
 
 echo "--- ghcr login"
 echo "$GHCR_TOKEN" | docker login ghcr.io -u "$GHCR_USERNAME" --password-stdin
@@ -23,12 +24,15 @@ fi
 
 # Build RisingWave docker image ${BUILDKITE_COMMIT}-${arch}
 echo "--- docker build and tag"
+echo "CARGO_BUILD_PROFILE is set to ${CARGO_BUILD_PROFILE}"
 docker buildx create \
   --name container \
   --driver=docker-container
 
 docker buildx build -f docker/Dockerfile \
-  --build-arg "GIT_SHA=${BUILDKITE_COMMIT}" -t "${ghcraddr}:${BUILDKITE_COMMIT}-${arch}" \
+  --build-arg "GIT_SHA=${BUILDKITE_COMMIT}" \
+  --build-arg "CARGO_BUILD_PROFILE=${CARGO_BUILD_PROFILE:-production}" \
+  -t "${ghcraddr}:${BUILDKITE_COMMIT}-${arch}" \
   --progress plain \
   --builder=container \
   --load \
