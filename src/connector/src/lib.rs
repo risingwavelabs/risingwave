785e1660eb33fede057e37b24891eef1b3c69fb5
xxhZs <1060434431@qq.com>
2024-08-22 12:25:12 +0800

    su
    su
    
    fmt

--- src/connector/src/lib.rs
+++ src/connector/src/lib.rs
@@ -75,27 +75,6 @@ where
     })
 }
 
-pub(crate) fn deserialize_optional_u64_from_string<'de, D>(
-    deserializer: D,
-) -> Result<Option<u64>, D::Error>
-where
-    D: de::Deserializer<'de>,
-{
-    let s: String = de::Deserialize::deserialize(deserializer)?;
-    if s.is_empty() {
-        Ok(None)
-    } else {
-        s.parse()
-            .map_err(|_| {
-                de::Error::invalid_value(
-                    de::Unexpected::Str(&s),
-                    &"integer greater than or equal to 0",
-                )
-            })
-            .map(Some)
-    }
-}
-
 pub(crate) fn deserialize_optional_string_seq_from_string<'de, D>(
     deserializer: D,
 ) -> std::result::Result<Option<Vec<String>>, D::Error>
