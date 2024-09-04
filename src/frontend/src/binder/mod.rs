f53edc2fd8c4391482b8705c597abf9cab0a5a99
Dylan <zilin@risingwave-labs.com>
2024-09-04 17:45:22 +0800

    feat(iceberg): support dql for nimtable (#18408)

--- src/frontend/src/binder/mod.rs
+++ src/frontend/src/binder/mod.rs
@@ -364,7 +364,6 @@ impl Binder {
         matches!(self.bind_for, BindFor::Stream)
     }
 
-    #[expect(dead_code)]
     fn is_for_batch(&self) -> bool {
         matches!(self.bind_for, BindFor::Batch)
     }
