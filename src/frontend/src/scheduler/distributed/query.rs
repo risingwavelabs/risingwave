ac9931d4c591e0f5354bc03f0df0fe239b8c117e
Dylan Chen <zilin@singularity-data.com>
2024-09-04 10:22:07 +0800

    fmt

--- src/frontend/src/scheduler/distributed/query.rs
+++ src/frontend/src/scheduler/distributed/query.rs
@@ -472,7 +472,10 @@ pub(crate) mod tests {
     use risingwave_batch::worker_manager::worker_node_manager::{
         WorkerNodeManager, WorkerNodeSelector,
     };
-    use risingwave_common::catalog::{ColumnCatalog, ColumnDesc, ConflictBehavior, CreateType, StreamJobStatus, DEFAULT_SUPER_USER_ID, Engine};
+    use risingwave_common::catalog::{
+        ColumnCatalog, ColumnDesc, ConflictBehavior, CreateType, Engine, StreamJobStatus,
+        DEFAULT_SUPER_USER_ID,
+    };
     use risingwave_common::hash::{WorkerSlotId, WorkerSlotMapping};
     use risingwave_common::types::DataType;
     use risingwave_pb::common::worker_node::Property;
