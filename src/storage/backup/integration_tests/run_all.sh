e8eb0ed45331bd0c73c4d850ff86ee75ca7a9f95
zwang28 <84491488@qq.com>
2024-09-12 22:28:21 +0800

    fix meta backup tests

--- src/storage/backup/integration_tests/run_all.sh
+++ src/storage/backup/integration_tests/run_all.sh
@@ -5,7 +5,7 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 tests=( \
 "test_basic.sh" \
 "test_pin_sst.sh" \
-#"test_query_backup.sh" \
+"test_query_backup.sh" \
 "test_set_config.sh" \
 )
 for t in "${tests[@]}"
