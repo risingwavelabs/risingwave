8d3800417d388d1396bde72fc881576154d1acb5
xxhZs <1060434431@qq.com>
2024-09-14 13:46:18 +0800

    add ppa

--- ci/scripts/e2e-cassandra-sink-test.sh
+++ ci/scripts/e2e-cassandra-sink-test.sh
@@ -43,6 +43,8 @@ export LATEST_CASSANDRA_VERSION=$(get_latest_cassandra_version)
 export CASSANDRA_DIR="./apache-cassandra-${LATEST_CASSANDRA_VERSION}"
 
 # Cassandra only support python 3.11
+apt-get install -y software-properties-common
+add-apt-repository ppa:deadsnakes/ppa
 apt-get update
 apt-get install -y python3.11
 apt-get install -y python3.11-venv
