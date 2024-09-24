f8ad15fc6fc65c4f4859ce1721bb1539c361810f
lmatz <lmatz823@gmail.com>
2024-09-24 12:09:37 +0800

    cherry-pick fix: a few fixes to Cassandra e2e test (#18657)
    Co-authored-by: Xinhao Xu <84456268+xxhZs@users.noreply.github.com>
    Co-authored-by: William Wen <44139337+wenym1@users.noreply.github.com>

--- ci/scripts/e2e-cassandra-sink-test.sh
+++ ci/scripts/e2e-cassandra-sink-test.sh
@@ -33,39 +33,31 @@ tar xf ./risingwave-connector.tar.gz -C ./connector-node
 
 echo "--- starting risingwave cluster"
 risedev ci-start ci-sink-test
-sleep 1
+# Wait cassandra server to start
+sleep 40
 
-echo "--- create cassandra table"
-curl https://downloads.apache.org/cassandra/4.1.3/apache-cassandra-4.1.3-bin.tar.gz  --output apache-cassandra-4.1.3-bin.tar.gz
-tar xfvz apache-cassandra-4.1.3-bin.tar.gz
-# remove bundled packages, and use installed packages, because Python 3.12 has removed asyncore, but I failed to install libev support for bundled Python driver.
-rm apache-cassandra-4.1.3/lib/six-1.12.0-py2.py3-none-any.zip
-rm apache-cassandra-4.1.3/lib/cassandra-driver-internal-only-3.25.0.zip
-apt-get install -y libev4 libev-dev
-pip3 install --break-system-packages cassandra-driver
+echo "--- install cassandra"
+wget $(get_latest_cassandra_download_url) -O cassandra_latest.tar.gz
+tar xfvz cassandra_latest.tar.gz
+export LATEST_CASSANDRA_VERSION=$(get_latest_cassandra_version)
+export CASSANDRA_DIR="./apache-cassandra-${LATEST_CASSANDRA_VERSION}"
 
-cd apache-cassandra-4.1.3/bin
+# Cassandra only support python 3.11
+apt-get install -y software-properties-common
+add-apt-repository ppa:deadsnakes/ppa
+apt-get update
+apt-get install -y python3.11
+apt-get install -y python3.11-venv
+python3.11 -m venv cqlsh_env
+source cqlsh_env/bin/activate
+
 export CQLSH_HOST=cassandra-server
 export CQLSH_PORT=9042
-./cqlsh -e "CREATE KEYSPACE demo WITH replication = {'class': 'SimpleStrategy', 'replication_factor': 1};use demo;
-CREATE table demo_bhv_table(v1 int primary key,v2 smallint,v3 bigint,v4 float,v5 double,v6 text,v7 date,v8 timestamp,v9 boolean);"
 
 echo "--- testing sinks"
-cd ../../
 sqllogictest -p 4566 -d dev './e2e_test/sink/cassandra_sink.slt'
-sleep 1
-cd apache-cassandra-4.1.3/bin
-./cqlsh -e "COPY demo.demo_bhv_table TO './query_result.csv' WITH HEADER = false AND ENCODING = 'UTF-8';"
 
-if cat ./query_result.csv | awk -F "," '{
-    exit !($1 == 1 && $2 == 1 && $3 == 1 && $4 == 1.1 && $5 == 1.2 && $6 == "test" && $7 == "2013-01-01" && $8 == "2013-01-01 01:01:01.000+0000" && $9 == "False\r"); }'; then
-  echo "Cassandra sink check passed"
-else
-  echo "The output is not as expected."
-  echo "output:"
-  cat ./query_result.csv
-  exit 1
-fi
+deactivate
 
 echo "--- Kill cluster"
 cd ../../
