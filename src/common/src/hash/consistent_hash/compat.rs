f5d165644768449bd364a717421b88211358f99d
Bugen Zhao <i@bugenzhao.com>
2024-09-29 14:18:31 +0800

    refactor: use 1 for vnode count of singletons for backward compatibility

--- src/common/src/hash/consistent_hash/compat.rs
+++ src/common/src/hash/consistent_hash/compat.rs
@@ -80,14 +80,23 @@ impl VnodeCount {
 }
 
 /// A trait for accessing the vnode count field with backward compatibility.
+///
+/// # `maybe_`?
+///
+/// The reason why there's a `maybe_` prefix on the protobuf field is that, a getter
+/// method with the same name as the field will be generated for `prost` structs.
+/// Directly naming it `vnode_count` will lead to the method `vnode_count()` returning
+/// `0` when the field is unset, which can be misleading sometimes.
+///
+/// Instead, we name the field as `maybe_vnode_count` and provide the method `vnode_count`
+/// through this trait, ensuring that backward compatibility is handled properly.
 pub trait VnodeCountCompat {
     /// Get the `maybe_vnode_count` field.
     fn vnode_count_inner(&self) -> VnodeCount;
 
-    /// Returns the vnode count, or [`VirtualNode::COUNT_FOR_COMPAT`] if the vnode count is not set,
-    /// typically for backward compatibility. Panics if the field is a placeholder.
-    ///
-    /// Equivalent to `self.vnode_count_inner().value()`.
+    /// Returns the vnode count if it's set. Otherwise, returns [`VirtualNode::COUNT_FOR_COMPAT`]
+    /// for distributed tables/fragments, and `1` for singleton tables/fragments, for backward
+    /// compatibility. Panics if the field is a placeholder.
     ///
     /// See the documentation on the field of the implementing type for more details.
     fn vnode_count(&self) -> usize {
@@ -116,8 +125,39 @@ macro_rules! impl_maybe_vnode_count_compat {
     };
 }
 
-impl_maybe_vnode_count_compat!(
-    risingwave_pb::plan_common::StorageTableDesc,
-    risingwave_pb::catalog::Table,
-    risingwave_pb::meta::table_fragments::Fragment,
-);
+// TODO!!!!!!!!!!!!!!!!!
+
+// impl VnodeCountCompat for risingwave_pb::catalog::Table {
+//     fn vnode_count(&self) -> usize {
+//         if let Some(vnode_count) = self.maybe_vnode_count {
+//             return vnode_count as _;
+//         }
+
+//         // Compatibility: derive vnode count from distribution.
+//         if self.distribution_key.is_empty()
+//             && self.dist_key_in_pk.is_empty()
+//             && self.vnode_col_index.is_none()
+//         {
+//             // Singleton table.
+//             1
+//         } else {
+//             VirtualNode::COUNT_FOR_COMPAT
+//         }
+//     }
+// }
+
+// impl VnodeCountCompat for risingwave_pb::plan_common::StorageTableDesc {
+//     fn vnode_count(&self) -> usize {
+//         if let Some(vnode_count) = self.maybe_vnode_count {
+//             return vnode_count as _;
+//         }
+
+//         // Compatibility: derive vnode count from distribution.
+//         if self.dist_key_in_pk_indices.is_empty() && self.vnode_col_idx_in_pk.is_none() {
+//             // Singleton table.
+//             1
+//         } else {
+//             VirtualNode::COUNT_FOR_COMPAT
+//         }
+//     }
+// }
