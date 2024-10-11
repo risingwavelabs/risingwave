69750ed8279e4964c255db63c20c44838b33e56f
xxchan <xxchan22f@gmail.com>
2024-10-11 21:00:31 +0800

    update
    Signed-off-by: xxchan <xxchan22f@gmail.com>

--- ci/scripts/e2e-sink-test.sh
+++ ci/scripts/e2e-sink-test.sh
@@ -124,8 +124,8 @@ risedev ci-kill
 echo "--- e2e, ci-1cn-1fe, nexmark endless"
 RUST_LOG="info,risingwave_stream=info,risingwave_batch=info,risingwave_storage=info" \
 risedev ci-start ci-1cn-1fe
-sqllogictest -p 4566 -d dev './e2e_test/source/nexmark_endless_mvs/*.slt'
-sqllogictest -p 4566 -d dev './e2e_test/source/nexmark_endless_sinks/*.slt'
+sqllogictest -p 4566 -d dev './e2e_test/source_inline/nexmark_endless_mvs/*.slt'
+sqllogictest -p 4566 -d dev './e2e_test/source_inline/nexmark_endless_sinks/*.slt'
 
 echo "--- Kill cluster"
 risedev ci-kill
