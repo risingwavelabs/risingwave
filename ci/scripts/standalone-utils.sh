c1a5b0e55a00f47f2dde75ce32a46f9f2764d7fc
August <pin@singularity-data.com>
2024-10-14 18:21:47 +0800

    fix standalone config

--- ci/scripts/standalone-utils.sh
+++ ci/scripts/standalone-utils.sh
@@ -19,7 +19,7 @@ start_standalone_without_compactor() {
         --dashboard-host 127.0.0.1:5691 \
         --prometheus-host 127.0.0.1:1250 \
         --backend sqlite \
-        --sql-endpoint sqlite://${RW_SQLITE_DB} \
+        --sql-endpoint ${RW_SQLITE_DB} \
         --state-store hummock+minio://hummockadmin:hummockadmin@127.0.0.1:9301/hummock001 \
         --data-directory hummock_001" \
      --compute-opts=" \
@@ -50,7 +50,7 @@ start_standalone() {
         --dashboard-host 127.0.0.1:5691 \
         --prometheus-host 127.0.0.1:1250 \
         --backend sqlite \
-        --sql-endpoint sqlite://${RW_SQLITE_DB} \
+        --sql-endpoint ${RW_SQLITE_DB} \
         --state-store hummock+minio://hummockadmin:hummockadmin@127.0.0.1:9301/hummock001 \
         --data-directory hummock_001" \
      --compute-opts=" \
