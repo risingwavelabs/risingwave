244229d6dec91c9c8edd59b03534b6cf278cae5c
Noel Kwan <noelkwan1998@gmail.com>
2024-10-17 13:54:43 +0800

    handle timeout case - buildkite outcome step can included errored for timeout

--- ci/scripts/notify.py
+++ ci/scripts/notify.py
@@ -111,11 +111,14 @@ def get_failed_tests(get_test_status, test_map):
     failed_test_map = {}
     for test in test_map.keys():
         test_status = get_test_status(test)
-        if test_status == "hard_failed" or test_status == "soft_failed":
-            print(f"{test} failed with outcome {test_status}")
+        if test_status == "hard_failed" or test_status == "soft_failed" or test_status == "errored":
+            print(f"{test} failed with outcome: {test_status}")
             failed_test_map[test] = test_map[test]
+        elif test_status == "passed":
+            print(f"{test} passed with outcome: {test_status}")
         else:
-            print(f"{test} passed with outcome {test_status}")
+            print(f"{test} failed with unknown outcome: {test_status}")
+            failed_test_map[test] = test_map[test]
     return failed_test_map
 
 def generate_test_status_message(failed_test_map):
@@ -192,14 +195,14 @@ def run_test_1():
 
 def main():
     test_map = get_test_map()
+    print("--- Getting failed tests")
     failed_test_map = get_failed_tests(get_buildkite_test_status, test_map)
     message = generate_test_status_message(failed_test_map)
     if message == "":
-        print("All tests passed, no need to notify")
+        print("--- Tests passed, no need to notify")
         return
     else:
-        print("Some tests failed, notify users")
-        print(message)
+        print("--- Some tests failed, notify users")
         cmd = format_cmd(message)
         print(cmd)
         subprocess.run(cmd, shell=True)
