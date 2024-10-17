c8f6f7c7d8496b0e4e06e78bae66cb85fe19853a
William Wen <william123.wen@gmail.com>
2024-10-17 16:38:37 +0800

    use shade iceberg

--- src/connector/src/sink/iceberg/java/src/main/java/com/risingwave/nimtable/SparkIcebergCompaction.java
+++ src/connector/src/sink/iceberg/java/src/main/java/com/risingwave/nimtable/SparkIcebergCompaction.java
@@ -10,9 +10,10 @@ public class SparkIcebergCompaction {
         String database = args[0];
         String table = args[1];
         SparkSession session = SparkSession.builder()
-                .config("spark.sql.extensions", "org.apache.iceberg.spark.extensions.IcebergSparkSessionExtensions")
-                .config("spark.sql.catalog.nimtable", "org.apache.iceberg.spark.SparkCatalog")
+                .config("spark.sql.extensions", "nimtable.shaded.org.apache.iceberg.spark.extensions.IcebergSparkSessionExtensions")
+                .config("spark.sql.catalog.nimtable", "nimtable.shaded.org.apache.iceberg.spark.SparkCatalog")
                 .getOrCreate();
+        session.sparkContext().
         List<Row> rows = session.sql(String.format("CALL nimtable.system.rewrite_data_files(table => '%s.%s', options => map('rewrite-all', 'true'))", database, table)).collectAsList();
         System.out.printf("compaction success: %s/%s, output: %s%n", database, table, rows);
         try {
