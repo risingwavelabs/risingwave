7248864339da2f9696838e5860865008d2b972c6
Chengyou Liu <chengyou.liu0@gmail.com>
2024-10-18 14:06:53 +0800

    run docker scout in container

--- ci/scripts/docker-scout.sh
+++ ci/scripts/docker-scout.sh
@@ -9,13 +9,17 @@ arch="$(uname -m)"
 echo "--- ghcr login"
 echo "$GHCR_TOKEN" | docker login ghcr.io -u "$GHCR_USERNAME" --password-stdin
 
+echo "--- dockerhub login"
+echo "$DOCKER_TOKEN" | docker login -u "risingwavelabs" --password-stdin
+
 echo "--- pull docker image ---"
 echo "pulling ${ghcraddr}:${BUILDKITE_COMMIT}-${arch}"
 docker pull "${ghcraddr}:${BUILDKITE_COMMIT}-${arch}"
 
 echo "--- check vulnerabilities ---"
-docker scout quickview "${ghcraddr}:${BUILDKITE_COMMIT}-${arch}" | tee scout-quickview.txt
-docker scout cves "${ghcraddr}:${BUILDKITE_COMMIT}-${arch}" | tee scout-cves.txt
-docker scout recommendations "${ghcraddr}:${BUILDKITE_COMMIT}-${arch}" | tee scout-recommendations.txt
+alias docker-scout="docker run -it -e DOCKER_SCOUT_HUB_USER=risingwavelabs -e DOCKER_SCOUT_HUB_PASSWORD=$DOCKER_TOKEN docker/scout-cli"
+docker-scout quickview "${ghcraddr}:${BUILDKITE_COMMIT}-${arch}" | tee scout-quickview.txt
+docker-scout cves "${ghcraddr}:${BUILDKITE_COMMIT}-${arch}" | tee scout-cves.txt
+docker-scout recommendations "${ghcraddr}:${BUILDKITE_COMMIT}-${arch}" | tee scout-recommendations.txt
 
 export SCOUT_REPORT=$(echo -e "Scout Quickview\n\n" && cat scout-quickview.txt && echo -e "\n\nScout CVEs\n\n" && cat scout-cves.txt && echo -e "\n\nScout Recommendations\n\n" && cat scout-recommendations.txt)
