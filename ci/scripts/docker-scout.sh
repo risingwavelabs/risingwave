ae4634d6424198da1d581ced275d7c7746de9296
Chengyou Liu <chengyou.liu0@gmail.com>
2024-10-18 17:36:42 +0800

    fix docker scout function

--- ci/scripts/docker-scout.sh
+++ ci/scripts/docker-scout.sh
@@ -5,6 +5,7 @@ set -euo pipefail
 
 ghcraddr="ghcr.io/risingwavelabs/risingwave"
 arch="$(uname -m)"
+image="${ghcraddr}:${BUILDKITE_COMMIT}-${arch}"
 
 echo "--- ghcr login"
 echo "$GHCR_TOKEN" | docker login ghcr.io -u "$GHCR_USERNAME" --password-stdin
@@ -12,18 +13,18 @@ echo "$GHCR_TOKEN" | docker login ghcr.io -u "$GHCR_USERNAME" --password-stdin
 echo "--- dockerhub login"
 echo "$DOCKER_TOKEN" | docker login -u "risingwavelabs" --password-stdin
 
-echo "--- pull docker image ---"
-echo "pulling ${ghcraddr}:${BUILDKITE_COMMIT}-${arch}"
-docker pull "${ghcraddr}:${BUILDKITE_COMMIT}-${arch}"
+echo "--- pull docker image"
+echo "pulling ${image}"
+docker pull "${image}"
 
-echo "--- check vulnerabilities ---"
+echo "--- check vulnerabilities"
 function docker-scout {
-    docker run -it -e DOCKER_SCOUT_HUB_USER=risingwavelabs -e DOCKER_SCOUT_HUB_PASSWORD=$DOCKER_TOKEN -u root -v /var/run/docker.sock:/var/run/docker.sock docker/scout-cli
+    docker run -it -e DOCKER_SCOUT_HUB_USER=risingwavelabs -e DOCKER_SCOUT_HUB_PASSWORD=$DOCKER_TOKEN -u root -v /var/run/docker.sock:/var/run/docker.sock docker/scout-cli "$@"
 }
-docker-scout quickview "${ghcraddr}:${BUILDKITE_COMMIT}-${arch}"
-docker-scout recommendations "${ghcraddr}:${BUILDKITE_COMMIT}-${arch}"
-docker-scout cves --format sarif -o scout.sarif "${ghcraddr}:${BUILDKITE_COMMIT}-${arch}"
+docker-scout quickview ${image}
+docker-scout recommendations "${image}"
+docker-scout cves --format sarif -o scout.sarif "${image}"
 
 export SCOUT_REPORT=$(cat scout.sarif)
-echo "--- scout report ---"
+echo "--- scout report"
 cat scout.sarif
