63e63f68588a46c62ddc8c1a4070e0cbd6325728
Chengyou Liu <chengyou.liu0@gmail.com>
2024-10-23 17:01:01 +0800

    fix notify

--- ci/scripts/docker-scout-notify.sh
+++ ci/scripts/docker-scout-notify.sh
@@ -1,14 +1,17 @@
 #!/usr/bin/env bash
 
-report=$(buildkite-agent meta-data get SCOUT_REPORT)
+buildkite-agent meta-data get SCOUT_REPORT > scout.report
+report=$(sed 's/\\n/          \\n/g')
 
 cat >> step.yaml << EOF
 - label: "docker scout slack notification"
   notify:
     - slack:
         channels:
-        - "#notification-buildkite"
-        message: "Docker Scout Report\n${report}"
+          - "#notification-buildkite"
+        message: |
+          Docker Scout Report
+          ${report}
 EOF
 
 buildkite-agent pipeline upload step.yaml
