3fb138351e751e9d8951d9a951db3b56ff3ae245
Xinhao Xu <84456268+xxhZs@users.noreply.github.com>
2024-10-24 13:42:51 +0800

    cherry-pick fix: changelog #18785 + #19029 + #19030 to branch release-2.0 (#19096)
    Signed-off-by: yihong0618 <zouzou0208@gmail.com>
    Co-authored-by: yihong <zouzou0208@gmail.com>
    Co-authored-by: xiangjinwu <17769960+xiangjinwu@users.noreply.github.com>

--- src/sqlparser/src/ast/query.rs
+++ src/sqlparser/src/ast/query.rs
@@ -335,8 +335,8 @@ impl fmt::Display for Cte {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         match &self.cte_inner {
             CteInner::Query(query) => write!(f, "{} AS ({})", self.alias, query)?,
-            CteInner::ChangeLog(ident) => {
-                write!(f, "{} AS changelog from {}", self.alias, ident.value)?
+            CteInner::ChangeLog(obj_name) => {
+                write!(f, "{} AS changelog from {}", self.alias, obj_name)?
             }
         }
         Ok(())
@@ -347,7 +347,7 @@ impl fmt::Display for Cte {
 #[cfg_attr(feature = "serde", derive(Serialize, Deserialize))]
 pub enum CteInner {
     Query(Query),
-    ChangeLog(Ident),
+    ChangeLog(ObjectName),
 }
 
 /// One item of the comma-separated list following `SELECT`
