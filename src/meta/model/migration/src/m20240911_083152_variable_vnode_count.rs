657cd117fbf147797a2e8137cd7cdfce656291c9
Bugen Zhao <i@bugenzhao.com>
2024-10-25 14:30:57 +0800

    move migration into a separate step
    Signed-off-by: Bugen Zhao <i@bugenzhao.com>

--- src/meta/model/migration/src/m20240911_083152_variable_vnode_count.rs
+++ src/meta/model/migration/src/m20240911_083152_variable_vnode_count.rs
@@ -21,35 +21,11 @@ impl MigrationTrait for Migration {
             )
             .await?;
 
-        // Fill vnode count with 1 for singleton tables.
-        manager
-            .exec_stmt(
-                UpdateStatement::new()
-                    .table(Table::Table)
-                    .values([(Table::VnodeCount, Expr::value(1))])
-                    .and_where(Expr::col(Table::DistributionKey).eq(Expr::value("[]")))
-                    .and_where(Expr::col(Table::DistKeyInPk).eq(Expr::value("[]")))
-                    .and_where(Expr::col(Table::VnodeColIndex).is_null())
-                    .to_owned(),
-            )
-            .await?;
-
         manager
             .alter_table(
                 MigrationTable::alter()
                     .table(Fragment::Table)
                     .add_column(col!(Fragment::VnodeCount))
-                    .to_owned(),
-            )
-            .await?;
-
-        // Fill vnode count with 1 for singleton fragments.
-        manager
-            .exec_stmt(
-                UpdateStatement::new()
-                    .table(Fragment::Table)
-                    .values([(Fragment::VnodeCount, Expr::value(1))])
-                    .and_where(Expr::col(Fragment::DistributionType).eq(Expr::value("SINGLE")))
                     .to_owned(),
             )
             .await?;
@@ -98,16 +74,12 @@ impl MigrationTrait for Migration {
 enum Fragment {
     Table,
     VnodeCount,
-    DistributionType,
 }
 
 #[derive(DeriveIden)]
 enum Table {
     Table,
     VnodeCount,
-    DistributionKey,
-    DistKeyInPk,
-    VnodeColIndex,
 }
 
 #[derive(DeriveIden)]
