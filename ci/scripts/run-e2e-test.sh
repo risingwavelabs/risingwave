8b4d84f0754b1da8034bf40cd08b74aeaa000e8b
Bugen Zhao <i@bugenzhao.com>
2024-10-28 15:39:33 +0800

    recreate meta store when cluster-stop
    Signed-off-by: Bugen Zhao <i@bugenzhao.com>

--- ci/scripts/run-e2e-test.sh
+++ ci/scripts/run-e2e-test.sh
@@ -64,6 +64,13 @@ cluster_stop() {
   else
     risedev ci-kill
   fi
+
+  # Clean up meta store database.
+  if [[ $mode == *"mysql-backend" ]]; then
+    mysql -h mysql -P 3306 -u root -p123456 -e "DROP DATABASE IF EXISTS metadata; CREATE DATABASE metadata;"
+  elif [[ $mode == *"pg-backend" ]]; then
+    PGPASSWORD=postgres psql -h db -p 5432 -U postgres -c "DROP DATABASE IF EXISTS metadata; CREATE DATABASE metadata;"
+  fi
 }
 
 download_and_prepare_rw "$profile" common
