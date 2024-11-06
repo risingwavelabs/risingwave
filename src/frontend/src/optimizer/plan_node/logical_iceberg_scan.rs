d9e4a9eec8a1737700aab8ba4feebb86bead1eba
Noel Kwan <noelkwan1998@gmail.com>
2024-11-06 08:58:01 +0800

    Revert "interim commit: add iceberg_predicate to batch"
    This reverts commit 4859705e496e77eb9005316e87a6fec1844caf76.

--- src/frontend/src/optimizer/plan_node/logical_iceberg_scan.rs
+++ src/frontend/src/optimizer/plan_node/logical_iceberg_scan.rs
@@ -342,8 +342,7 @@ impl PredicatePushdown for LogicalIcebergScan {
 
 impl ToBatch for LogicalIcebergScan {
     fn to_batch(&self) -> Result<PlanRef> {
-        let plan: PlanRef =
-            BatchIcebergScan::new(self.core.clone(), self.iceberg_predicate.clone()).into();
+        let plan: PlanRef = BatchIcebergScan::new(self.core.clone()).into();
         Ok(plan)
     }
 }
