816082d5a0a54b5cef4bb19d2d2da99335cbff8e
Noel Kwan <noelkwan1998@gmail.com>
2024-11-14 19:34:41 +0800

    remove more unnecessary stuff + make compile

--- src/connector/src/sink/postgres.rs
+++ src/connector/src/sink/postgres.rs
@@ -25,8 +25,6 @@ use risingwave_common::types::{DataType, Decimal};
 use serde_derive::Deserialize;
 use serde_with::{serde_as, DisplayFromStr};
 use simd_json::prelude::ArrayTrait;
-use tiberius::numeric::Numeric;
-use tiberius::{AuthMethod, Client, ColumnData, Config, Query};
 use tokio::net::TcpStream;
 use tokio_util::compat::TokioAsyncWriteCompatExt;
 use with_options::WithOptions;
@@ -59,10 +57,7 @@ pub struct PostgresConfig {
     pub database: String,
     #[serde(rename = "postgres.table")]
     pub table: String,
-    #[serde(
-        rename = "postgres.max_batch_rows",
-        default = "default_max_batch_rows"
-    )]
+    #[serde(rename = "postgres.max_batch_rows", default = "default_max_batch_rows")]
     #[serde_as(as = "DisplayFromStr")]
     pub max_batch_rows: usize,
     pub r#type: String, // accept "append-only" or "upsert"
@@ -135,89 +130,6 @@ impl Sink for PostgresSink {
             return Err(SinkError::Config(anyhow!(
                 "Primary key not defined for upsert Postgres sink (please define in `primary_key` field)")));
         }
-
-        // NOTE(kwannoel): postgresql types should be superset of rw.
-        // for f in self.schema.fields() {
-        //     check_data_type_compatibility(&f.data_type)?;
-        // }
-
-        // // Query table metadata from SQL Server.
-        // let mut sql_server_table_metadata = HashMap::new();
-        // let mut sql_client = PostgresClient::new(&self.config).await?;
-        // let query_table_metadata_error = || {
-        //     SinkError::Postgres(anyhow!(format!(
-        //         "Postgres table {} metadata error",
-        //         self.config.table
-        //     )))
-        // };
-        //
-        // // Query the column information from the `information_schema.columns` table
-        // let table_name = "your_table_name";
-        // let rows = sqlx::query(
-        //     r#"
-        // SELECT column_name, data_type, is_nullable
-        // FROM information_schema.columns
-        // WHERE table_name = $1
-        // ORDER BY ordinal_position;
-        // "#
-        // )
-        //     .bind(table_name)
-        //     .fetch_all(&pool)
-        //     .await?;
-        //
-        // // Print the schema details
-        // for row in rows {
-        //     let column_name: &str = row.get("column_name");
-        //     let data_type: &str = row.get("data_type");
-        //     let is_nullable: &str = row.get("is_nullable");
-        //
-        //     println!("Column: {}, Type: {}, Nullable: {}", column_name, data_type, is_nullable);
-        // }
-        //
-        // // Validate Column name and Primary Key
-        // for (idx, col) in self.schema.fields().iter().enumerate() {
-        //     let rw_is_pk = self.pk_indices.contains(&idx);
-        //     match sql_server_table_metadata.get(&col.name) {
-        //         None => {
-        //             return Err(SinkError::Postgres(anyhow!(format!(
-        //                 "column {} not found in the downstream SQL Server table {}",
-        //                 col.name, self.config.table
-        //             ))));
-        //         }
-        //         Some(sql_server_is_pk) => {
-        //             if self.is_append_only {
-        //                 continue;
-        //             }
-        //             if rw_is_pk && !*sql_server_is_pk {
-        //                 return Err(SinkError::Postgres(anyhow!(format!(
-        //                     "column {} specified in primary_key mismatches with the downstream SQL Server table {} PK",
-        //                     col.name, self.config.table,
-        //                 ))));
-        //             }
-        //             if !rw_is_pk && *sql_server_is_pk {
-        //                 return Err(SinkError::Postgres(anyhow!(format!(
-        //                     "column {} unspecified in primary_key mismatches with the downstream SQL Server table {} PK",
-        //                     col.name, self.config.table,
-        //                 ))));
-        //             }
-        //         }
-        //     }
-        // }
-        //
-        // if !self.is_append_only {
-        //     let sql_server_pk_count = sql_server_table_metadata
-        //         .values()
-        //         .filter(|is_pk| **is_pk)
-        //         .count();
-        //     if sql_server_pk_count != self.pk_indices.len() {
-        //         return Err(SinkError::Postgres(anyhow!(format!(
-        //             "primary key does not match between RisingWave sink ({}) and SQL Server table {} ({})",
-        //             self.pk_indices.len(),
-        //             self.config.table,
-        //             sql_server_pk_count,
-        //         ))));
-        //     }
-        // }
 
         Ok(())
     }
@@ -293,131 +205,6 @@ impl PostgresSinkWriter {
     }
 
     async fn flush(&mut self) -> Result<()> {
-        use std::fmt::Write;
-        if self.ops.is_empty() {
-            return Ok(());
-        }
-        let mut query_str = String::new();
-        let col_num = self.schema.fields.len();
-        let mut next_param_id = 1;
-        let non_pk_col_indices = (0..col_num)
-            .filter(|idx| !self.pk_indices.contains(idx))
-            .collect::<Vec<usize>>();
-        let all_col_names = self
-            .schema
-            .fields
-            .iter()
-            .map(|f| format!("[{}]", f.name))
-            .collect::<Vec<_>>()
-            .join(",");
-        let all_source_col_names = self
-            .schema
-            .fields
-            .iter()
-            .map(|f| format!("[SOURCE].[{}]", f.name))
-            .collect::<Vec<_>>()
-            .join(",");
-        let pk_match = self
-            .pk_indices
-            .iter()
-            .map(|idx| {
-                format!(
-                    "[SOURCE].[{}]=[TARGET].[{}]",
-                    &self.schema[*idx].name, &self.schema[*idx].name
-                )
-            })
-            .collect::<Vec<_>>()
-            .join(" AND ");
-        let param_placeholders = |param_id: &mut usize| {
-            let params = (*param_id..(*param_id + col_num))
-                .map(|i| format!("@P{}", i))
-                .collect::<Vec<_>>()
-                .join(",");
-            *param_id += col_num;
-            params
-        };
-        let set_all_source_col = non_pk_col_indices
-            .iter()
-            .map(|idx| {
-                format!(
-                    "[{}]=[SOURCE].[{}]",
-                    &self.schema[*idx].name, &self.schema[*idx].name
-                )
-            })
-            .collect::<Vec<_>>()
-            .join(",");
-        // TODO: avoid repeating the SQL
-        for op in &self.ops {
-            match op {
-                SqlOp::Insert(_) => {
-                    write!(
-                        &mut query_str,
-                        "INSERT INTO [{}] ({}) VALUES ({});",
-                        self.config.table,
-                        all_col_names,
-                        param_placeholders(&mut next_param_id),
-                    )
-                    .unwrap();
-                }
-                SqlOp::Merge(_) => {
-                    write!(
-                        &mut query_str,
-                        r#"MERGE [{}] AS [TARGET]
-                        USING (VALUES ({})) AS [SOURCE] ({})
-                        ON {}
-                        WHEN MATCHED THEN UPDATE SET {}
-                        WHEN NOT MATCHED THEN INSERT ({}) VALUES ({});"#,
-                        self.config.table,
-                        param_placeholders(&mut next_param_id),
-                        all_col_names,
-                        pk_match,
-                        set_all_source_col,
-                        all_col_names,
-                        all_source_col_names,
-                    )
-                    .unwrap();
-                }
-                SqlOp::Delete(_) => {
-                    write!(
-                        &mut query_str,
-                        r#"DELETE FROM [{}] WHERE {};"#,
-                        self.config.table,
-                        self.pk_indices
-                            .iter()
-                            .map(|idx| {
-                                let condition =
-                                    format!("[{}]=@P{}", self.schema[*idx].name, next_param_id);
-                                next_param_id += 1;
-                                condition
-                            })
-                            .collect::<Vec<_>>()
-                            .join(" AND "),
-                    )
-                    .unwrap();
-                }
-            }
-        }
-
-        let mut query = Query::new(query_str);
-        for op in self.ops.drain(..) {
-            match op {
-                SqlOp::Insert(row) => {
-                    bind_params(&mut query, row, &self.schema, 0..col_num)?;
-                }
-                SqlOp::Merge(row) => {
-                    bind_params(&mut query, row, &self.schema, 0..col_num)?;
-                }
-                SqlOp::Delete(row) => {
-                    bind_params(
-                        &mut query,
-                        row,
-                        &self.schema,
-                        self.pk_indices.iter().copied(),
-                    )?;
-                }
-            }
-        }
-        query.execute(&mut self.sql_client.inner_client).await?;
         Ok(())
     }
 }
@@ -469,150 +256,12 @@ impl SinkWriter for PostgresSinkWriter {
 }
 
 #[derive(Debug)]
-pub struct PostgresClient {
-    pub inner_client: Client<tokio_util::compat::Compat<TcpStream>>,
-}
+pub struct PostgresClient {}
 
 impl PostgresClient {
-    async fn new(msconfig: &PostgresConfig) -> Result<Self> {
-        let mut config = Config::new();
-        config.host(&msconfig.host);
-        config.port(msconfig.port);
-        config.authentication(AuthMethod::sql_server(&msconfig.user, &msconfig.password));
-        config.database(&msconfig.database);
-        config.trust_cert();
-        Self::new_with_config(config).await
+    async fn new(pg_config: &PostgresConfig) -> Result<Self> {
+        todo!()
     }
-
-    pub async fn new_with_config(mut config: Config) -> Result<Self> {
-        let tcp = TcpStream::connect(config.get_addr())
-            .await
-            .context("failed to connect to sql server")
-            .map_err(SinkError::Postgres)?;
-        tcp.set_nodelay(true)
-            .context("failed to setting nodelay when connecting to sql server")
-            .map_err(SinkError::Postgres)?;
-
-        let client = match Client::connect(config.clone(), tcp.compat_write()).await {
-            // Connection successful.
-            Ok(client) => client,
-            // The server wants us to redirect to a different address
-            Err(tiberius::error::Error::Routing { host, port }) => {
-                config.host(&host);
-                config.port(port);
-                let tcp = TcpStream::connect(config.get_addr())
-                    .await
-                    .context("failed to connect to sql server after routing")
-                    .map_err(SinkError::Postgres)?;
-                tcp.set_nodelay(true)
-                    .context(
-                        "failed to setting nodelay when connecting to sql server after routing",
-                    )
-                    .map_err(SinkError::Postgres)?;
-                // we should not have more than one redirect, so we'll short-circuit here.
-                Client::connect(config, tcp.compat_write()).await?
-            }
-            Err(e) => return Err(e.into()),
-        };
-
-        Ok(Self {
-            inner_client: client,
-        })
-    }
-}
-
-fn bind_params(
-    query: &mut Query<'_>,
-    row: impl Row,
-    schema: &Schema,
-    col_indices: impl Iterator<Item = usize>,
-) -> Result<()> {
-    use risingwave_common::types::ScalarRefImpl;
-    for col_idx in col_indices {
-        match row.datum_at(col_idx) {
-            Some(data_ref) => match data_ref {
-                ScalarRefImpl::Int16(v) => query.bind(v),
-                ScalarRefImpl::Int32(v) => query.bind(v),
-                ScalarRefImpl::Int64(v) => query.bind(v),
-                ScalarRefImpl::Float32(v) => query.bind(v.into_inner()),
-                ScalarRefImpl::Float64(v) => query.bind(v.into_inner()),
-                ScalarRefImpl::Utf8(v) => query.bind(v.to_owned()),
-                ScalarRefImpl::Bool(v) => query.bind(v),
-                ScalarRefImpl::Decimal(v) => match v {
-                    Decimal::Normalized(d) => {
-                        query.bind(decimal_to_sql(&d));
-                    }
-                    Decimal::NaN | Decimal::PositiveInf | Decimal::NegativeInf => {
-                        tracing::warn!(
-                            "Inf, -Inf, Nan in RisingWave decimal is converted into SQL Server null!"
-                        );
-                        query.bind(None as Option<Numeric>);
-                    }
-                },
-                ScalarRefImpl::Date(v) => query.bind(v.0),
-                ScalarRefImpl::Timestamp(v) => query.bind(v.0),
-                ScalarRefImpl::Timestamptz(v) => query.bind(v.timestamp_micros()),
-                ScalarRefImpl::Time(v) => query.bind(v.0),
-                ScalarRefImpl::Bytea(v) => query.bind(v.to_vec()),
-                ScalarRefImpl::Interval(_) => return Err(data_type_not_supported("Interval")),
-                ScalarRefImpl::Jsonb(_) => return Err(data_type_not_supported("Jsonb")),
-                ScalarRefImpl::Struct(_) => return Err(data_type_not_supported("Struct")),
-                ScalarRefImpl::List(_) => return Err(data_type_not_supported("List")),
-                ScalarRefImpl::Int256(_) => return Err(data_type_not_supported("Int256")),
-                ScalarRefImpl::Serial(_) => return Err(data_type_not_supported("Serial")),
-                ScalarRefImpl::Map(_) => return Err(data_type_not_supported("Map")),
-            },
-            None => match schema[col_idx].data_type {
-                DataType::Boolean => {
-                    query.bind(None as Option<bool>);
-                }
-                DataType::Int16 => {
-                    query.bind(None as Option<i16>);
-                }
-                DataType::Int32 => {
-                    query.bind(None as Option<i32>);
-                }
-                DataType::Int64 => {
-                    query.bind(None as Option<i64>);
-                }
-                DataType::Float32 => {
-                    query.bind(None as Option<f32>);
-                }
-                DataType::Float64 => {
-                    query.bind(None as Option<f64>);
-                }
-                DataType::Decimal => {
-                    query.bind(None as Option<Numeric>);
-                }
-                DataType::Date => {
-                    query.bind(None as Option<chrono::NaiveDate>);
-                }
-                DataType::Time => {
-                    query.bind(None as Option<chrono::NaiveTime>);
-                }
-                DataType::Timestamp => {
-                    query.bind(None as Option<chrono::NaiveDateTime>);
-                }
-                DataType::Timestamptz => {
-                    query.bind(None as Option<i64>);
-                }
-                DataType::Varchar => {
-                    query.bind(None as Option<String>);
-                }
-                DataType::Bytea => {
-                    query.bind(None as Option<Vec<u8>>);
-                }
-                DataType::Interval => return Err(data_type_not_supported("Interval")),
-                DataType::Struct(_) => return Err(data_type_not_supported("Struct")),
-                DataType::List(_) => return Err(data_type_not_supported("List")),
-                DataType::Jsonb => return Err(data_type_not_supported("Jsonb")),
-                DataType::Serial => return Err(data_type_not_supported("Serial")),
-                DataType::Int256 => return Err(data_type_not_supported("Int256")),
-                DataType::Map(_) => return Err(data_type_not_supported("Map")),
-            },
-        };
-    }
-    Ok(())
 }
 
 fn data_type_not_supported(data_type_name: &str) -> SinkError {
@@ -644,19 +293,4 @@ fn check_data_type_compatibility(data_type: &DataType) -> Result<()> {
         DataType::Int256 => Err(data_type_not_supported("Int256")),
         DataType::Map(_) => Err(data_type_not_supported("Map")),
     }
-}
-
-/// The implementation is copied from tiberius crate.
-fn decimal_to_sql(decimal: &rust_decimal::Decimal) -> Numeric {
-    let unpacked = decimal.unpack();
-
-    let mut value = (((unpacked.hi as u128) << 64)
-        + ((unpacked.mid as u128) << 32)
-        + unpacked.lo as u128) as i128;
-
-    if decimal.is_sign_negative() {
-        value = -value;
-    }
-
-    Numeric::new_with_scale(value, decimal.scale() as u8)
 }
