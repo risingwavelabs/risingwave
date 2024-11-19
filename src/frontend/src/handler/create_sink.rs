43eab8ba2dbcce0c793e3a0135caceaadf8423fe
Shanicky Chen <peng@risingwave-labs.com>
2024-11-19 14:38:59 +0800

    Remove circular dependency check for sinks
    Signed-off-by: Shanicky Chen <peng@risingwave-labs.com>

--- src/frontend/src/handler/create_sink.rs
+++ src/frontend/src/handler/create_sink.rs
@@ -453,8 +453,6 @@ pub async fn handle_create_sink(
     if let Some(table_catalog) = target_table_catalog {
         use crate::handler::alter_table_column::hijack_merger_for_target_table;
 
-        check_cycle_for_sink(session.as_ref(), sink.clone(), table_catalog.id())?;
-
         let (mut graph, mut table, source) =
             reparse_table_for_sink(&session, &table_catalog).await?;
 
@@ -528,112 +526,6 @@ pub fn fetch_incoming_sinks(
     }
 
     Ok(sinks)
-}
-
-fn check_cycle_for_sink(
-    session: &SessionImpl,
-    sink_catalog: SinkCatalog,
-    table_id: catalog::TableId,
-) -> Result<()> {
-    let reader = session.env().catalog_reader().read_guard();
-
-    let mut sinks = HashMap::new();
-    let mut sources = HashMap::new();
-    let mut views = HashMap::new();
-    let db_name = session.database();
-    for schema in reader.iter_schemas(db_name)? {
-        for sink in schema.iter_sink() {
-            sinks.insert(sink.id.sink_id, sink.as_ref());
-        }
-
-        for source in schema.iter_source() {
-            sources.insert(source.id, source.as_ref());
-        }
-
-        for view in schema.iter_view() {
-            views.insert(view.id, view.as_ref());
-        }
-    }
-
-    struct Context<'a> {
-        reader: &'a CatalogReadGuard,
-        sink_index: &'a HashMap<u32, &'a SinkCatalog>,
-        source_index: &'a HashMap<u32, &'a SourceCatalog>,
-        view_index: &'a HashMap<u32, &'a ViewCatalog>,
-    }
-
-    impl Context<'_> {
-        fn visit_table(
-            &self,
-            table: &TableCatalog,
-            target_table_id: catalog::TableId,
-            path: &mut Vec<String>,
-        ) -> Result<()> {
-            if table.id == target_table_id {
-                path.reverse();
-                path.push(table.name.clone());
-                return Err(RwError::from(ErrorCode::BindError(
-                    format!(
-                        "Creating such a sink will result in circular dependency, path = [{}]",
-                        path.join(", ")
-                    )
-                    .to_string(),
-                )));
-            }
-
-            for sink_id in &table.incoming_sinks {
-                if let Some(sink) = self.sink_index.get(sink_id) {
-                    path.push(sink.name.clone());
-                    self.visit_dependent_jobs(&sink.dependent_relations, target_table_id, path)?;
-                    path.pop();
-                } else {
-                    bail!("sink not found: {:?}", sink_id);
-                }
-            }
-
-            self.visit_dependent_jobs(&table.dependent_relations, target_table_id, path)?;
-
-            Ok(())
-        }
-
-        fn visit_dependent_jobs(
-            &self,
-            dependent_jobs: &[TableId],
-            target_table_id: TableId,
-            path: &mut Vec<String>,
-        ) -> Result<()> {
-            for table_id in dependent_jobs {
-                if let Ok(table) = self.reader.get_any_table_by_id(table_id) {
-                    path.push(table.name.clone());
-                    self.visit_table(table.as_ref(), target_table_id, path)?;
-                    path.pop();
-                } else if self.source_index.contains_key(&table_id.table_id)
-                    || self.view_index.contains_key(&table_id.table_id)
-                {
-                    continue;
-                } else {
-                    bail!("streaming job not found: {:?}", table_id);
-                }
-            }
-
-            Ok(())
-        }
-    }
-
-    let mut path = vec![];
-
-    path.push(sink_catalog.name.clone());
-
-    let ctx = Context {
-        reader: &reader,
-        sink_index: &sinks,
-        source_index: &sources,
-        view_index: &views,
-    };
-
-    ctx.visit_dependent_jobs(&sink_catalog.dependent_relations, table_id, &mut path)?;
-
-    Ok(())
 }
 
 pub(crate) async fn reparse_table_for_sink(
