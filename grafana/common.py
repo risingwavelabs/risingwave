a426ba7a1098c4f980a94907eb82ce983e57caba
Eric Fu <eric@singularity-data.com>
2024-12-02 15:51:20 +0800

    add user panel

--- grafana/common.py
+++ grafana/common.py
@@ -527,3 +527,8 @@ def quantile(f, percentiles):
     return list(
         map(lambda p: f(quantile_map[str(p)][0], quantile_map[str(p)][1]), percentiles)
     )
+
+
+def epoch_to_unix_millis(epoch_expr):
+    # UNIX_RISINGWAVE_DATE_SEC
+    return f"(1617235200000+({epoch_expr} != 0)/65536)"
