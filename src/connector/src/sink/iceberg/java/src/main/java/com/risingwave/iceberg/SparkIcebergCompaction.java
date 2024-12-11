21823a60c3daac428e779ac02bb5d09a5fe4a539
Dylan Chen <zilin@singularity-data.com>
2024-12-11 18:10:16 +0800

    fix

--- src/connector/src/sink/iceberg/java/src/main/java/com/risingwave/iceberg/SparkIcebergCompaction.java
+++ src/connector/src/sink/iceberg/java/src/main/java/com/risingwave/iceberg/SparkIcebergCompaction.java
@@ -12,12 +12,12 @@ public class SparkIcebergCompaction {
         String table = args[2];
         SparkSession session = SparkSession.builder()
                 .config("spark.sql.extensions", "org.apache.iceberg.spark.extensions.IcebergSparkSessionExtensions")
-                .config(String.format("spark.sql.catalog.{}", "org.apache.iceberg.spark.SparkCatalog", catalog))
+                .config(String.format("spark.sql.catalog.%s", catalog), "org.apache.iceberg.spark.SparkCatalog")
                 .getOrCreate();
-        List<Row> rows = session.sql(String.format("CALL {}.system.rewrite_data_files(table => '%s.%s', options => map('rewrite-all', 'true'))", catalog, database, table)).collectAsList();
+        List<Row> rows = session.sql(String.format("CALL %s.system.rewrite_data_files(table => '%s.%s', options => map('rewrite-all', 'true'))", catalog, database, table)).collectAsList();
         System.out.printf("compaction success: %s/%s, output: %s%n", database, table, rows);
         try {
-            List<Row> expireSnapshotOutputRows = session.sql(String.format("CALL {}.system.expire_snapshots(table => '%s.%s')", catalog, database, table)).collectAsList();
+            List<Row> expireSnapshotOutputRows = session.sql(String.format("CALL %s.system.expire_snapshots(table => '%s.%s')", catalog, database, table)).collectAsList();
             System.out.printf("expire_snapshots success: %s/%s, output: %s%n", database, table, expireSnapshotOutputRows);
         } catch (Throwable e) {
             System.err.printf("failed to run expire snapshot: %s%n", e);
