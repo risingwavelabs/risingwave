a6cb6de0afd20338681d5baa48f1570a97200bc1
Li0k <yuli@singularity-data.com>
2024-12-26 15:59:19 +0800

    fix(catalog): update migration

---
+++ src/meta/model/migration/src/m20241226_074013_clean_watermark_index_in_pk.rs
@@ -0,0 +1,39 @@
+use sea_orm_migration::prelude::{Table as MigrationTable, *};
+
+use crate::utils::ColumnDefExt;
+
+#[derive(DeriveMigrationName)]
+pub struct Migration;
+
+#[async_trait::async_trait]
+impl MigrationTrait for Migration {
+    async fn up(&self, manager: &SchemaManager) -> Result<(), DbErr> {
+        // Replace the sample below with your own migration scripts
+        manager
+            .alter_table(
+                MigrationTable::alter()
+                    .table(Table::Table)
+                    .add_column(ColumnDef::new(Table::CleanWatermarkIndexInPk).rw_binary(manager))
+                    .to_owned(),
+            )
+            .await
+    }
+
+    async fn down(&self, manager: &SchemaManager) -> Result<(), DbErr> {
+        // Replace the sample below with your own migration scripts
+        manager
+            .alter_table(
+                MigrationTable::alter()
+                    .table(Table::Table)
+                    .drop_column(Table::CleanWatermarkIndexInPk)
+                    .to_owned(),
+            )
+            .await
+    }
+}
+
+#[derive(DeriveIden)]
+enum Table {
+    Table,
+    CleanWatermarkIndexInPk,
+}
