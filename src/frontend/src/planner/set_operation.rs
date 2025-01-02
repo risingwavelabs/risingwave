cbf002fcf1973aff2867e3c0549aac38fc97135e
Croxx <mrcroxx@outlook.com>
2025-01-02 13:42:53 +0800

    chore: update license header and happy 2025 (#19979)
    Signed-off-by: MrCroxx <mrcroxx@outlook.com>

--- src/frontend/src/planner/set_operation.rs
+++ src/frontend/src/planner/set_operation.rs
@@ -1,6 +1,6 @@
 use risingwave_common::util::column_index_mapping::ColIndexMapping;
 
-// Copyright 2024 RisingWave Labs
+// Copyright 2025 RisingWave Labs
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
