c9e8984d0ebd359d3152a6a79d2b03a0be8f17d7
Shanicky Chen <peng@risingwave-labs.com>
2025-01-03 02:45:25 +0800

    Refactor recovery, enhance error logging, adjust parallelism control in streaming processes.

--- src/meta/src/rpc/ddl_controller.rs
+++ src/meta/src/rpc/ddl_controller.rs
@@ -401,16 +401,6 @@ impl DdlController {
             deferred = true;
         }
 
-        if !deferred
-            && !self
-                .metadata_manager
-                .list_background_creating_jobs()
-                .await?
-                .is_empty()
-        {
-            bail!("The system is creating jobs in the background, please try again later")
-        }
-
         self.stream_manager
             .alter_table_parallelism(table_id, parallelism.into(), deferred)
             .await
