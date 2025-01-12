e5a35468ad0335685f8fc3b97890cfc9a91732d3
William Wen <william123.wen@gmail.com>
2025-01-12 16:31:09 +0800

    refactor(storage): create new read snapshot to read for StateStore trait

--- src/storage/hummock_test/src/hummock_read_version_tests.rs
+++ src/storage/hummock_test/src/hummock_read_version_tests.rs
@@ -36,7 +36,7 @@ use risingwave_storage::hummock::shared_buffer::shared_buffer_batch::SharedBuffe
 use risingwave_storage::hummock::store::version::{
     read_filter_for_version, HummockReadVersion, StagingData, StagingSstableInfo, VersionUpdate,
 };
-use risingwave_storage::hummock::test_utils::gen_dummy_batch;
+use risingwave_storage::hummock::test_utils::*;
 
 use crate::test_utils::prepare_first_valid_version;
 
