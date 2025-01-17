bb90e0fe057c04b52b38804f545264c2813d4bf8
xxchan <xxchan22f@gmail.com>
2025-01-17 09:13:26 +0800

    feat: delete Kafka consumer group on drop (#20065)
    Signed-off-by: xxchan <xxchan22f@gmail.com>

--- src/connector/src/source/kafka/source/reader.rs
+++ src/connector/src/source/kafka/source/reader.rs
@@ -77,14 +77,7 @@ impl SplitReader for KafkaSplitReader {
         properties.connection.set_security_properties(&mut config);
         properties.set_client(&mut config);
 
-        let group_id_prefix = properties
-            .group_id_prefix
-            .as_deref()
-            .unwrap_or("rw-consumer");
-        config.set(
-            "group.id",
-            format!("{}-{}", group_id_prefix, source_ctx.fragment_id),
-        );
+        config.set("group.id", properties.group_id(source_ctx.fragment_id));
 
         let ctx_common = KafkaContextCommon::new(
             broker_rewrite_map,
