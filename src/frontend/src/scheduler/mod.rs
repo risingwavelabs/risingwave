b808d69a2bbebeed2ffc8218e37dc94721c42e44
Kexiang Wang <kx.wang@hotmail.com>
2025-01-21 18:15:38 -0500

    fix

--- src/frontend/src/scheduler/mod.rs
+++ src/frontend/src/scheduler/mod.rs
@@ -35,8 +35,7 @@ pub use local::*;
 mod fast_insert;
 pub use fast_insert::*;
 
-// pub use crate::scheduler::fast_insert::{run_inner_call, run_inner_call_2};
-pub use crate::scheduler::task_context::FrontendBatchTaskContext;
+use crate::scheduler::task_context::FrontendBatchTaskContext;
 
 mod error;
 pub mod streaming_manager;
--- src/frontend/src/scheduler/plan_fragmenter.rs
+++ src/frontend/src/scheduler/plan_fragmenter.rs
@@ -570,7 +570,6 @@ impl Debug for QueryStage {
             .field("parallelism", &self.parallelism)
             .field("exchange_info", &self.exchange_info)
             .field("has_table_scan", &self.has_table_scan())
-            .field("stages", &self.root)
             .finish()
     }
 }
