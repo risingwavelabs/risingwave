d615af7ef8acd972ec47bb88696f25a5a398a3d8
Richard Chien <stdrc@outlook.com>
2025-01-22 11:08:02 +0800

    migrate `Function::identifier`
    Signed-off-by: Richard Chien <stdrc@outlook.com>

--- src/meta/model/migration/src/lib.rs
+++ src/meta/model/migration/src/lib.rs
@@ -31,6 +31,7 @@ mod m20241121_101830_table_engine;
 mod m20241125_043732_connection_params;
 mod m20241202_071413_resource_group;
 mod m20241226_074013_clean_watermark_index_in_pk;
+mod m20250121_085800_change_wasm_udf_identifier;
 mod utils;
 
 pub struct Migrator;
@@ -100,6 +101,7 @@ impl MigratorTrait for Migrator {
             Box::new(m20241121_101830_table_engine::Migration),
             Box::new(m20241202_071413_resource_group::Migration),
             Box::new(m20241226_074013_clean_watermark_index_in_pk::Migration),
+            Box::new(m20250121_085800_change_wasm_udf_identifier::Migration),
         ]
     }
 }
