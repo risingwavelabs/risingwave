80f04e7e3d93918b997e4f4b6519614799b3aa72
Noel Kwan <noelkwan1998@gmail.com>
2025-02-05 20:52:28 +0800

    fmt

--- src/stream/src/from_proto/sync_log_store.rs
+++ src/stream/src/from_proto/sync_log_store.rs
@@ -16,9 +16,7 @@ use risingwave_pb::stream_plan::SyncLogStoreNode;
 use risingwave_storage::StateStore;
 
 use crate::common::log_store_impl::kv_log_store::serde::LogStoreRowSerde;
-use crate::common::log_store_impl::kv_log_store::{
-    KvLogStoreMetrics, FIRST_SEQ_ID, KV_LOG_STORE_V2_INFO,
-};
+use crate::common::log_store_impl::kv_log_store::{KvLogStoreMetrics, KV_LOG_STORE_V2_INFO};
 use crate::error::StreamResult;
 use crate::executor::{Executor, SyncedKvLogStoreExecutor};
 use crate::from_proto::ExecutorBuilder;
