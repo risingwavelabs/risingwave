428aa9adb247fed87d1cd6a09c6e637a2ed0d107
Kexiang Wang <kx.wang@hotmail.com>
2025-02-14 17:31:52 -0500

    fmt

--- e2e_test/webhook/sender.py
+++ e2e_test/webhook/sender.py
@@ -77,7 +77,7 @@ def send_test_primary_key(secret):
     }
     send_webhook(url, headers, payload_json)
     
-    
+
 def send_validate_raw_string(secret):
     payload = message
     payload['source'] = "github"
@@ -92,7 +92,6 @@ def send_validate_raw_string(secret):
         "X-Hub-Signature": signature  # Custom signature header
     }
     send_webhook(url, headers, payload_json)
-    
 
 
 def send_github_hmac_sha256(secret):
