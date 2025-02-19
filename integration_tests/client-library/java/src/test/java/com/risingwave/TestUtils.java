01a7c40e8c5ef69257e014b0af450b8c13591d46
xxhZs <1060434431@qq.com>
2025-02-19 12:44:17 +0800

    add ci

--- integration_tests/client-library/java/src/test/java/com/risingwave/TestUtils.java
+++ integration_tests/client-library/java/src/test/java/com/risingwave/TestUtils.java
@@ -18,4 +18,17 @@ public class TestUtils {
 
         return DriverManager.getConnection(url, props);
     }
+
+    public static Connection establishExtendedConnection() throws SQLException {
+        // TODO: remove preferQueryMode=simple.
+        final String url = "jdbc:postgresql://risingwave-standalone:4566/dev";
+        final String user = "root";
+        final String password = "";
+
+        Properties props = new Properties();
+        props.setProperty("user", user);
+        props.setProperty("password", password);
+
+        return DriverManager.getConnection(url, props);
+    }
 }
