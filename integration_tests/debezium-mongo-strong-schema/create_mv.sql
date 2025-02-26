686ce5cf6703e434b482d6cd026d91960bf25262
cl <cailue@apache.org>
2025-02-26 15:35:11 +0800

    patch: update ci
    Signed-off-by: cl <cailue@apache.org>

--- integration_tests/debezium-mongo-strong-schema/create_mv.sql
+++ integration_tests/debezium-mongo-strong-schema/create_mv.sql
@@ -2,6 +2,7 @@ CREATE MATERIALIZED VIEW normalized_users AS
 SELECT
     name as name,
     email as email,
-    address as address
+    address as address,
+    last_login as last_login
 FROM
     users;
