afc52783dbb556b5359ec58aa3b7b1480132850d
Noel Kwan <noelkwan1998@gmail.com>
2025-02-27 11:25:04 +0800

    fetch fragments from meta

--- src/frontend/src/handler/explain_analyze_stream_job.rs
+++ src/frontend/src/handler/explain_analyze_stream_job.rs
@@ -1,3 +1,5 @@
+use std::ops::Deref;
+
 use crate::error::Result;
 use crate::handler::{HandlerArgs, RwPgResponse};
 
@@ -5,5 +7,28 @@ pub async fn handle_explain_analyze_stream_job(
     handler_args: HandlerArgs,
     job_id: u32,
 ) -> Result<RwPgResponse> {
+    // query meta for fragment graph (names only)
+    let meta_client = handler_args.session.env().meta_client();
+    // TODO(kwannoel): Only fetch the names, actor_ids and graph of the fragments
+    let fragments = {
+        let mut fragment_map = meta_client.list_table_fragments(&[job_id]).await?;
+        let (fragment_job_id, table_fragment_info) = fragment_map.drain().next().unwrap();
+        assert_eq!(fragment_job_id, job_id);
+        table_fragment_info.fragments
+    };
     todo!()
 }
+// fn fragments_to_graph(fragments: Vec<TableFragmentInfo>) -> FragmentGraph {
+//     let mut graph = FragmentGraph::new();
+//     for fragment in fragments {
+//         let fragment_id = fragment.fragment_id;
+//         let fragment_name = fragment.fragment_name;
+//         let actor_id = fragment.actor_id;
+//         let mut fragment_node = FragmentNode::new(fragment_id, fragment_name, actor_id);
+//         for dep in fragment.dependencies {
+//             fragment_node.add_dependency(dep);
+//         }
+//         graph.add_node(fragment_node);
+//     }
+//     graph
+// }
