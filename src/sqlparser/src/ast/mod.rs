c8ba5a5dab304060896920cbf7c86234b0d779a2
Xiangjin <xiangjin@risingwave-labs.com>
2025-02-27 23:31:48 +0800

    ensure parse-ability before Statement::to_string returns

--- src/sqlparser/src/ast/mod.rs
+++ src/sqlparser/src/ast/mod.rs
@@ -1633,10 +1633,19 @@ pub enum Statement {
 }
 
 impl fmt::Display for Statement {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        let mut buf = String::new();
+        self.fmt_inner(&mut buf)?;
+        let _ = Parser::parse_sql(&buf).unwrap();
+        f.write_str(&buf)
+    }
+}
+
+impl Statement {
     // Clippy thinks this function is too complicated, but it is painful to
     // split up without extracting structs for each `Statement` variant.
     #[allow(clippy::cognitive_complexity)]
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+    fn fmt_inner(&self, mut f: impl std::fmt::Write) -> fmt::Result {
         match self {
             Statement::Explain {
                 analyze,
@@ -1648,9 +1657,9 @@ impl fmt::Display for Statement {
                 if *analyze {
                     write!(f, "ANALYZE ")?;
                 }
-                options.fmt(f)?;
+                write!(f, "{}", options)?;
 
-                write!(f, "{}", statement)
+                statement.fmt_inner(f)
             }
             Statement::Query(s) => write!(f, "{}", s),
             Statement::Truncate { table_name } => {
@@ -2220,7 +2229,8 @@ impl fmt::Display for Statement {
                 if !data_types.is_empty() {
                     write!(f, "({}) ", display_comma_separated(data_types))?;
                 }
-                write!(f, "AS {}", statement)
+                write!(f, "AS ")?;
+                statement.fmt_inner(f)
             }
             Statement::Comment {
                 object_type,
