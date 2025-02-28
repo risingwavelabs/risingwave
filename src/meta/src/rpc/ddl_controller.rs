866921ef875e7b0b2b14c3ad4938a30a60b2a949
Shanicky Chen <peng@risingwave-labs.com>
2025-02-28 17:35:42 +0800

    feat: introduce `adaptive_parallelism_strategy` (#20589)
    Signed-off-by: Shanicky Chen <peng@risingwave-labs.com>

--- src/meta/src/rpc/ddl_controller.rs
+++ src/meta/src/rpc/ddl_controller.rs
@@ -1699,6 +1699,14 @@ impl DdlController {
             resource_group.clone(),
         )?;
 
+        let parallelism = self
+            .env
+            .system_params_reader()
+            .await
+            .adaptive_parallelism_strategy()
+            .compute_target_parallelism(parallelism.get());
+
+        let parallelism = NonZeroUsize::new(parallelism).expect("parallelism must be positive");
         let actor_graph_builder = ActorGraphBuilder::new(
             id,
             resource_group,
