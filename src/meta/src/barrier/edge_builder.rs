d31fa749b006be3d74e8310667a3af77ea4084c5
William Wen <william123.wen@gmail.com>
2025-03-04 22:03:40 +0800

    fix merge update

--- src/meta/src/barrier/edge_builder.rs
+++ src/meta/src/barrier/edge_builder.rs
@@ -17,6 +17,7 @@ use std::collections::HashMap;
 use risingwave_common::bitmap::Bitmap;
 use risingwave_meta_model::fragment::DistributionType;
 use risingwave_pb::stream_plan::update_mutation::MergeUpdate;
+use tracing::warn;
 
 use crate::controller::fragment::InflightFragmentInfo;
 use crate::controller::utils::compose_dispatchers;
@@ -120,19 +121,35 @@ impl FragmentEdgeBuilder {
         original_upstream_fragment_id: FragmentId,
         new_upstream_fragment_id: FragmentId,
     ) {
-        let fragment = &self.fragments[&fragment_id];
-        let new_upstream_fragment = &self.fragments[&new_upstream_fragment_id];
-        self.result
-            .merge_updates
-            .entry(fragment_id)
-            .or_default()
-            .extend(fragment.actors.keys().map(|actor_id| MergeUpdate {
-                actor_id: *actor_id,
-                upstream_fragment_id: original_upstream_fragment_id,
-                new_upstream_fragment_id: Some(new_upstream_fragment_id),
-                added_upstream_actor_id: new_upstream_fragment.actors.keys().cloned().collect(),
-                removed_upstream_actor_id: vec![],
-            }));
+        let fragment_merge_updates = self.result.merge_updates.entry(fragment_id).or_default();
+        if let Some(fragment_upstreams) = self.result.upstreams.get_mut(&fragment_id) {
+            fragment_upstreams.retain(|&actor_id, actor_upstreams| {
+                if let Some(new_upstreams) = actor_upstreams.remove(&new_upstream_fragment_id) {
+                    fragment_merge_updates.push(MergeUpdate {
+                        actor_id,
+                        upstream_fragment_id: original_upstream_fragment_id,
+                        new_upstream_fragment_id: Some(new_upstream_fragment_id),
+                        added_upstream_actor_id: new_upstreams.into_iter().collect(),
+                        removed_upstream_actor_id: vec![],
+                    })
+                } else if cfg!(debug_assertions) {
+                    panic!("cannot find new upstreams for actor {} in fragment {} to new_upstream {}. Current upstreams {:?}", actor_id, fragment_id, new_upstream_fragment_id, actor_upstreams);
+                } else {
+                    warn!(actor_id, fragment_id, new_upstream_fragment_id, ?actor_upstreams, "cannot find new upstreams for actor");
+                }
+                !actor_upstreams.is_empty()
+            })
+        } else if cfg!(debug_assertions) {
+            panic!(
+                "cannot find new upstreams for fragment {} to new_upstream {} to replace {}. Current upstreams: {:?}",
+                fragment_id,
+                new_upstream_fragment_id,
+                original_upstream_fragment_id,
+                self.result.upstreams
+            );
+        } else {
+            warn!(fragment_id, new_upstream_fragment_id, original_upstream_fragment_id, upstreams = ?self.result.upstreams, "cannot find new upstreams to replace");
+        }
     }
 
     pub(super) fn build(self) -> FragmentEdgeBuildResult {
