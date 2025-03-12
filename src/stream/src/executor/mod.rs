e0cfd7b660d93744601eb7e01cc8b529da493f38
Noel Kwan <noelkwan1998@gmail.com>
2025-03-12 17:02:56 +0800

    decouple metrics

--- src/stream/src/executor/mod.rs
+++ src/stream/src/executor/mod.rs
@@ -155,6 +155,7 @@ pub use sink::SinkExecutor;
 pub use sort::*;
 pub use stateless_simple_agg::StatelessSimpleAggExecutor;
 pub use sync_kv_log_store::SyncedKvLogStoreExecutor;
+pub use sync_kv_log_store::metrics::SyncedKvLogStoreMetrics;
 pub use temporal_join::TemporalJoinExecutor;
 pub use top_n::{
     AppendOnlyGroupTopNExecutor, AppendOnlyTopNExecutor, GroupTopNExecutor, TopNExecutor,
