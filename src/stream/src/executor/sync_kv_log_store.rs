aad97cb0bea54cf56b2806e3e012d25df8050469
Noel Kwan <noelkwan1998@gmail.com>
2025-03-13 21:05:39 +0800

    limit flush read size

--- src/stream/src/executor/sync_kv_log_store.rs
+++ src/stream/src/executor/sync_kv_log_store.rs
@@ -420,7 +420,7 @@ impl<S: LocalStateStore> WriteFuture<S> {
         let instant = Instant::now() + duration;
         tracing::trace!(?instant, ?duration, "write_future_pause");
         Self::Paused {
-            sleep_future: Some(Box::pin(sleep_until(instant))),
+            sleep_future: None, // Some(Box::pin(sleep_until(instant))),
             message,
             stream,
             write_state,
@@ -1040,10 +1040,22 @@ impl SyncedLogStoreBuffer {
     }
 
     fn pop_front(&mut self) -> Option<(u64, LogStoreBufferItem)> {
-        let item = self.buffer.pop_front();
-        match &item {
-            Some((_, LogStoreBufferItem::Flushed { .. })) => {
-                self.flushed_count -= 1;
+        let mut item = self.buffer.pop_front();
+        match &mut item {
+            Some((epoch, LogStoreBufferItem::Flushed { start_seq_id, end_seq_id, vnode_bitmap, chunk_id })) => {
+                let end_seq_id_bound = *start_seq_id + 256;
+                if *end_seq_id > end_seq_id_bound {
+                    let new_item = LogStoreBufferItem::Flushed {
+                        start_seq_id: end_seq_id_bound + 1,
+                        end_seq_id: *end_seq_id,
+                        vnode_bitmap: vnode_bitmap.clone(),
+                        chunk_id: *chunk_id,
+                    };
+                    *end_seq_id = end_seq_id_bound;
+                    self.buffer.push_front((*epoch, new_item));
+                } else {
+                    self.flushed_count -= 1;
+                }
             }
             Some((_, LogStoreBufferItem::StreamChunk { chunk, .. })) => {
                 self.current_size -= chunk.cardinality();
