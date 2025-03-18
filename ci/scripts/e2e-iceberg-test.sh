ffb64e2aa2a40f6f8cf77baff2384f777017a76e
xxchan <xxchan22f@gmail.com>
2025-03-18 18:26:06 +0800

    ci: unify iceberg test script
    Signed-off-by: xxchan <xxchan22f@gmail.com>

--- ci/scripts/e2e-iceberg-test.sh
+++ ci/scripts/e2e-iceberg-test.sh
@@ -5,9 +5,6 @@ set -euo pipefail
 
 source ci/scripts/common.sh
 
-# prepare environment
-export CONNECTOR_LIBS_PATH="./connector-node/libs"
-
 while getopts 'p:' opt; do
     case ${opt} in
         p )
@@ -26,21 +23,43 @@ shift $((OPTIND -1))
 
 download_and_prepare_rw "$profile" source
 
-echo "--- Download connector node package"
-buildkite-agent artifact download risingwave-connector.tar.gz ./
-mkdir ./connector-node
-tar xf ./risingwave-connector.tar.gz -C ./connector-node
-
 echo "--- starting risingwave cluster"
-mkdir -p .risingwave/log
+PGPASSWORD=postgres psql -h db -p 5432 -U postgres -c "DROP DATABASE IF EXISTS metadata;" -c "CREATE DATABASE metadata;"
 risedev ci-start ci-iceberg-test
-sleep 1
 
 # prepare minio iceberg sink
 echo "--- preparing iceberg"
-.risingwave/bin/mcli -C .risingwave/config/mcli mb hummock-minio/iceberg
-wget --no-verbose https://rw-ci-deps-dist.s3.amazonaws.com/spark-3.3.1-bin-hadoop3.tgz
-tar -xf spark-3.3.1-bin-hadoop3.tgz --no-same-owner
+.risingwave/bin/mcli -C .risingwave/config/mcli mb hummock-minio/icebergdata
+
+cd e2e_test/iceberg
+bash ./start_spark_connect_server.sh
+
+echo "--- Running tests"
+# Don't remove the `--quiet` option since poetry has a bug when printing output, see
+# https://github.com/python-poetry/poetry/issues/3412
+poetry update --quiet
+poetry run python main.py -t ./test_case/no_partition_append_only.toml
+poetry run python main.py -t ./test_case/no_partition_upsert.toml
+poetry run python main.py -t ./test_case/partition_append_only.toml
+poetry run python main.py -t ./test_case/partition_upsert.toml
+poetry run python main.py -t ./test_case/range_partition_append_only.toml
+poetry run python main.py -t ./test_case/range_partition_upsert.toml
+poetry run python main.py -t ./test_case/append_only_with_checkpoint_interval.toml
+poetry run python main.py -t ./test_case/iceberg_select_empty_table.toml
+poetry run python main.py -t ./test_case/iceberg_source_equality_delete.toml
+poetry run python main.py -t ./test_case/iceberg_source_position_delete.toml
+poetry run python main.py -t ./test_case/iceberg_source_all_delete.toml
+poetry run python main.py -t ./test_case/iceberg_source_explain_for_delete.toml
+poetry run python main.py -t ./test_case/iceberg_predicate_pushdown.toml
+poetry run python main.py -t ./test_case/iceberg_connection.toml
+
+echo "--- Running benchmarks"
+poetry run python main.py -t ./benches/predicate_pushdown.toml
+
+echo "--- Running iceberg engine tests"
+sqllogictest -p 4566 -d dev './e2e_test/iceberg/test_case/iceberg_engine.slt'
+
+echo "--- Legacy test"
 DEPENDENCIES=org.apache.iceberg:iceberg-spark-runtime-3.3_2.12:1.3.1,org.apache.hadoop:hadoop-aws:3.3.2
 spark-3.3.1-bin-hadoop3/bin/spark-sql --packages $DEPENDENCIES \
     --conf spark.sql.catalog.demo=org.apache.iceberg.spark.SparkCatalog \
@@ -91,5 +110,24 @@ spark-3.3.1-bin-hadoop3/bin/spark-sql --packages $DEPENDENCIES \
     --S --e "drop table demo.demo_db.e2e_demo_table;"
 
 
+
+echo "--- Legacy test (CDC)"
+
+# 1. import data to mysql
+mysql --host=mysql --port=3306 -u root -p123456 < ./test_case/cdc/mysql_cdc.sql
+
+# 2. create table and sink
+poetry run python main.py -t ./test_case/cdc/no_partition_cdc_init.toml
+
+# 3. insert new data to mysql
+mysql --host=mysql --port=3306 -u root -p123456 < ./test_case/cdc/mysql_cdc_insert.sql
+
+sleep 20
+
+# 4. check change
+poetry run python main.py -t ./test_case/cdc/no_partition_cdc.toml
+
+
 echo "--- Kill cluster"
+cd ../../
 risedev ci-kill
