cc52d925bd85885fd3ce3d70472ece71832e9eeb
Xinhao Xu <84456268+xxhZs@users.noreply.github.com>
2025-03-19 15:01:53 +0800

    fix(subscription):fix loss of subscription data due to reboot (#20959)  to 2.1 (#20965)

--- src/storage/hummock_sdk/src/frontend_version.rs
+++ src/storage/hummock_sdk/src/frontend_version.rs
@@ -49,8 +49,8 @@ impl FrontendHummockVersion {
                                 .0
                                 .iter()
                                 .map(|change_log| EpochNewChangeLogCommon {
-                                    new_value: vec![],
-                                    old_value: vec![],
+                                    new_value: vec![(); change_log.new_value.len()],
+                                    old_value: vec![(); change_log.new_value.len()],
                                     epochs: change_log.epochs.clone(),
                                 })
                                 .collect(),
@@ -79,8 +79,14 @@ impl FrontendHummockVersion {
                                 .0
                                 .iter()
                                 .map(|change_log| PbEpochNewChangeLog {
-                                    old_value: vec![],
-                                    new_value: vec![],
+                                    old_value: vec![
+                                        PbSstableInfo::default();
+                                        change_log.old_value.len()
+                                    ],
+                                    new_value: vec![
+                                        PbSstableInfo::default();
+                                        change_log.new_value.len()
+                                    ],
                                     epochs: change_log.epochs.clone(),
                                 })
                                 .collect(),
@@ -107,8 +113,9 @@ impl FrontendHummockVersion {
                                 .change_logs
                                 .into_iter()
                                 .map(|change_log| EpochNewChangeLogCommon {
-                                    new_value: vec![],
-                                    old_value: vec![],
+                                    // Here we need to determine if value is null but don't care what the value is, so we fill him in using `()`
+                                    new_value: vec![(); change_log.new_value.len()],
+                                    old_value: vec![(); change_log.old_value.len()],
                                     epochs: change_log.epochs,
                                 })
                                 .collect(),
