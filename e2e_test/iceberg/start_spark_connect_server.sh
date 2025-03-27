249904626adae661997fd886ee5543bdf06402d8
xxchan <xxchan22f@gmail.com>
2025-03-27 17:21:27 +0800

    test(iceberg): refine test script to support test discovery, prepare env & CI parallelism (#21082)
    Signed-off-by: xxchan <xxchan22f@gmail.com>

--- e2e_test/iceberg/start_spark_connect_server.sh
+++ e2e_test/iceberg/start_spark_connect_server.sh
@@ -1,11 +1,34 @@
 #!/usr/bin/env bash
 
-set -ex
+set -e
 
-ICEBERG_VERSION=1.4.3
-SPARK_VERSION=3.4.4
+# stop if already running
+if nc -z localhost 15002; then
+    echo "Spark connect server already running at port 15002"
+    exit 0
+fi
 
-PACKAGES="org.apache.iceberg:iceberg-spark-runtime-3.4_2.12:$ICEBERG_VERSION,org.apache.hadoop:hadoop-aws:3.3.2"
+SCRIPT_DIR="$(dirname "$0")"
+cd "$SCRIPT_DIR"
+
+# check java version, only 8/11/17 are supported
+if type -p java; then
+    JAVA_VER=$(java -version 2>&1 | awk -F '"' '/version/ {print $2}' | cut -d'.' -f1)
+    if [[ "$JAVA_VER" != "8" && "$JAVA_VER" != "11" && "$JAVA_VER" != "17" ]]; then
+        echo -e "\e[31mOnly Java 8/11/17 are supported. Current version: $JAVA_VER\e[0m"
+        exit 1
+    fi
+else
+    echo -e "\e[31mJava not found. Please install Java 8/11/17.\e[0m"
+    exit 1
+fi
+
+# https://iceberg.apache.org/releases/
+# https://spark.apache.org/downloads.html
+ICEBERG_VERSION=1.8.1
+SPARK_VERSION=3.5.5
+
+PACKAGES="org.apache.iceberg:iceberg-spark-runtime-3.5_2.12:$ICEBERG_VERSION,org.apache.hadoop:hadoop-aws:3.3.2"
 PACKAGES="$PACKAGES,org.apache.spark:spark-connect_2.12:$SPARK_VERSION"
 
 SPARK_FILE="spark-${SPARK_VERSION}-bin-hadoop3.tgz"
@@ -15,6 +38,7 @@ if [ ! -d "spark-${SPARK_VERSION}-bin-hadoop3" ];then
     tar -xzf $SPARK_FILE --no-same-owner
 fi
 
+# start new server
 ./spark-${SPARK_VERSION}-bin-hadoop3/sbin/start-connect-server.sh --packages $PACKAGES \
   --master local[3] \
   --conf spark.driver.bindAddress=0.0.0.0 \
