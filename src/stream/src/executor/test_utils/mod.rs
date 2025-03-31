c1e80a754c74312edb05d15a998948f0d35894c2
Yuhao Su <yuhaosu@outlook.com>
2025-03-31 16:22:19 -0500

    minor

--- src/stream/src/executor/test_utils/mod.rs
+++ src/stream/src/executor/test_utils/mod.rs
@@ -1,13 +1,23 @@
+// Copyright 2025 RisingWave Labs
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 // Re-export everything from submodules
 pub mod agg_executor;
 pub mod hash_join_executor;
 mod mock_source;
 pub mod top_n_executor;
 
-// pub use agg_executor::*;
-// pub use hash_join_executor::*;
-// pub use top_n_executor::*;
-// pub use expr::build_from_pretty;
 use async_trait::async_trait;
 use futures::{FutureExt, StreamExt, TryStreamExt};
 use futures_async_stream::try_stream;
