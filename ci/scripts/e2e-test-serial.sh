a08d8bb9b083f29313095d33a175a2c6bc5f17e4
Noel Kwan <noelkwan1998@gmail.com>
2025-04-04 12:04:14 +0800

    separate backfill test

--- ci/scripts/e2e-test-serial.sh
+++ ci/scripts/e2e-test-serial.sh
@@ -85,7 +85,9 @@ cluster_start
 # Please make sure the regression is expected before increasing the timeout.
 risedev slt -p 4566 -d dev './e2e_test/streaming/**/*.slt' --junit "streaming-${profile}"
 risedev slt -p 4566 -d dev './e2e_test/backfill/sink/different_pk_and_dist_key.slt'
+
 if [[ "$profile" == "ci-release" ]]; then
+  echo "--- e2e, $mode, backfill"
   # only run in release-mode. It's too slow for dev-mode.
   risedev slt -p 4566 -d dev './e2e_test/backfill/backfill_order_control.slt'
 fi
