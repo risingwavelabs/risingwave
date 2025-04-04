a01422290617b0347b6ec00619982cb04fa8cff2
Noel Kwan <noelkwan1998@gmail.com>
2025-04-04 16:51:45 +0800

    add vnode stream

--- src/stream/src/common/log_store_impl/kv_log_store/mod.rs
+++ src/stream/src/common/log_store_impl/kv_log_store/mod.rs
@@ -12,6 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+use std::collections::HashMap;
 use std::sync::Arc;
 
 use risingwave_common::bitmap::Bitmap;
@@ -47,8 +48,9 @@ use risingwave_common::row::ArrayVec;
 use risingwave_common::types::{DataType, Datum};
 use risingwave_common::util::sort_util::OrderType;
 
-// TODO: unify with `risingwave_common::Epoch`
+pub(crate) type LogStoreVnodeProgress = HashMap<Epoch, (VirtualNode, SeqId)>;
 
+// TODO: unify with `risingwave_common::Epoch`
 pub(crate) type Epoch = u64;
 
 pub(crate) type SeqId = i32;
