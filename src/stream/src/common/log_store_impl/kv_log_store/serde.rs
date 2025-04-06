5f3cc2f79b4a9c3f0d618d0565b5d54a90255d34
Noel Kwan <noelkwan1998@gmail.com>
2025-04-06 23:06:03 +0800

    ignore unused vnode field

--- src/stream/src/common/log_store_impl/kv_log_store/serde.rs
+++ src/stream/src/common/log_store_impl/kv_log_store/serde.rs
@@ -670,6 +670,7 @@ pub(crate) fn merge_log_store_item_stream<S: StateStoreReadIter>(
 mod stream_de {
     use super::*;
 
+    #[expect(dead_code)]
     #[derive(Debug)]
     pub(super) struct LogStoreRow {
         pub vnode: VirtualNode,
