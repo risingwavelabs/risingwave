7f12dedec2f94521d28790da2841c4a0df74cbc4
xxchan <xxchan22f@gmail.com>
2025-04-08 16:24:04 +0800

    fix
    Signed-off-by: xxchan <xxchan22f@gmail.com>

--- ci/scripts/integration-tests.sh
+++ ci/scripts/integration-tests.sh
@@ -44,14 +44,14 @@ fi
 
 if [ "${BUILDKITE_SOURCE}" == "schedule" ]; then
   # Use ghcr nightly image for scheduled build. If not specified, we use dockerhub's 'risingwavelabs/risingwave'.
-  # use yesterday's date
-  export RW_IMAGE="ghcr.io/risingwavelabs/risingwave:nightly-$(date -v -1d '+%Y%m%d')"
+  export RW_IMAGE="ghcr.io/risingwavelabs/risingwave:nightly-$(date '+%Y%m%d')"
   echo Docker image: "$RW_IMAGE"
 fi
 
 if [ "${BUILDKITE_SOURCE}" == "webhook" ]; then
   # Use ghcr nightly image for webhook build (PR). If not specified, we use dockerhub's 'risingwavelabs/risingwave'.
-  export RW_IMAGE="ghcr.io/risingwavelabs/risingwave:nightly-$(date '+%Y%m%d')"
+  # use yesterday's date
+  export RW_IMAGE="ghcr.io/risingwavelabs/risingwave:nightly-$(date -v -1d '+%Y%m%d')"
   echo Docker image: "$RW_IMAGE"
 fi
 
