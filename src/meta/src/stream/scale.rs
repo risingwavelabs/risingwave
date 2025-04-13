981cdf688bebd23c4db66005622fb3db21b0bbf1
Peng Chen <peng@risingwave-labs.com>
2025-04-13 16:55:55 +0800

    Refactor `scale.rs`: Move 'ticker.tick()' and update 'should_trigger' to enhance initialization logic.
    Signed-off-by: Peng Chen <peng@risingwave-labs.com>

--- src/meta/src/stream/scale.rs
+++ src/meta/src/stream/scale.rs
@@ -2545,9 +2545,6 @@ impl GlobalStreamManager {
         );
         ticker.set_missed_tick_behavior(MissedTickBehavior::Skip);
 
-        // waiting for the first tick
-        ticker.tick().await;
-
         let (local_notification_tx, mut local_notification_rx) =
             tokio::sync::mpsc::unbounded_channel();
 
@@ -2555,6 +2552,9 @@ impl GlobalStreamManager {
             .notification_manager()
             .insert_local_sender(local_notification_tx)
             .await;
+
+        // waiting for the first tick
+        ticker.tick().await;
 
         let worker_nodes = self
             .metadata_manager
@@ -2569,7 +2569,7 @@ impl GlobalStreamManager {
 
         let mut previous_adaptive_parallelism_strategy = AdaptiveParallelismStrategy::default();
 
-        let mut should_trigger = false;
+        let mut should_trigger = true;
 
         loop {
             tokio::select! {
