6e52cb22914e805ed526e558e203835c26c18c3a
xxchan <xxchan22f@gmail.com>
2025-04-16 14:41:54 +0800

    fix
    Signed-off-by: xxchan <xxchan22f@gmail.com>

--- src/frontend/src/error.rs
+++ src/frontend/src/error.rs
@@ -24,6 +24,7 @@ use risingwave_expr::ExprError;
 use risingwave_pb::PbFieldNotFound;
 use risingwave_rpc_client::error::{RpcError, TonicStatusWrapper};
 use thiserror::Error;
+use thiserror_ext::AsReport;
 use tokio::task::JoinError;
 
 use crate::expr::CastError;
@@ -265,6 +266,6 @@ impl From<BoxedError> for RwError {
 
 impl From<risingwave_sqlparser::parser::ParserError> for ErrorCode {
     fn from(e: risingwave_sqlparser::parser::ParserError) -> Self {
-        ErrorCode::InvalidInputSyntax(e.to_string())
+        ErrorCode::InvalidInputSyntax(e.to_report_string())
     }
 }
