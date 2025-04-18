bbb1dac1863b88eb292d3b2a32fab0f5d8388e18
congyi wang <58715567+wcy-fdu@users.noreply.github.com>
2025-04-18 15:39:26 +0800

    chore(ci): install python dependency for opendal test  (#21467)

--- ci/scripts/e2e-test-parallel-for-opendal.sh
+++ ci/scripts/e2e-test-parallel-for-opendal.sh
@@ -25,6 +25,8 @@ download_and_prepare_rw "$profile" common
 
 echo "--- Download artifacts"
 # nothing
+echo "--- Install Python Dependencies"
+python3 -m pip install --break-system-packages psycopg2-binary
 
 host_args=(-h localhost -p 4565 -h localhost -p 4566 -h localhost -p 4567)
 
