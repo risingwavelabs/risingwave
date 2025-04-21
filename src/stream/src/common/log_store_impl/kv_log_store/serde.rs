f68569abea472d0740c81fc46fbd5e5fb9f4b13a
William Wen <william123.wen@gmail.com>
2025-04-21 11:40:51 +0800

    fix license header

--- src/stream/src/common/log_store_impl/kv_log_store/serde.rs
+++ src/stream/src/common/log_store_impl/kv_log_store/serde.rs
@@ -1,4 +1,3 @@
-use std::collections::HashMap;
 // Copyright 2025 RisingWave Labs
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
@@ -12,6 +11,8 @@ use std::collections::HashMap;
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
+
+use std::collections::HashMap;
 use std::mem::{replace, take};
 use std::pin::Pin;
 use std::sync::Arc;
