ae0eee216ae70efd72c3b50f529dcf2db38f4b9f
William Wen <william123.wen@gmail.com>
2025-04-23 16:43:02 +0800

    reproduce

--- ci/scripts/deterministic-it-test.sh
+++ ci/scripts/deterministic-it-test.sh
@@ -19,6 +19,7 @@ mv target/ci-sim target/sim
 TEST_PATTERN="$@"
 
 echo "--- Run integration tests in deterministic simulation mode"
+
 seq "$TEST_NUM" | parallel --line-buffer "MADSIM_TEST_SEED={} NEXTEST_PROFILE=ci-sim \
  cargo nextest run \
  $NEXTEST_PARTITION_ARG \
