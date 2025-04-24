e28d9e67a9ec40a6717330b34e91269d11f87af5
Bugen Zhao <i@bugenzhao.com>
2025-04-24 16:30:04 +0800

    remove task-local
    Signed-off-by: Bugen Zhao <i@bugenzhao.com>

--- src/sqlparser/src/ast/mod.rs
+++ src/sqlparser/src/ast/mod.rs
@@ -1666,65 +1666,55 @@ pub enum DescribeKind {
     Fragments,
 }
 
-task_local::task_local! {
-    /// A flag indicating whether to validate the unparsed SQL to be parsable
-    /// in [`Statement::fmt`] implementation. True by default.
-    static VALIDATE_PARSABLE: bool
-}
-fn should_validate_parsable() -> bool {
-    VALIDATE_PARSABLE.try_with(|x| *x).unwrap_or(true)
-}
-
-fn do_validate_parsable(stmt: &Statement, sql: &str) -> Result<(), ParserError> {
-    // TODO(#20713): expand this check to all statements
-    if matches!(
-        stmt,
-        Statement::CreateTable { .. } | Statement::CreateSource { .. }
-    ) {
-        let _ = Parser::parse_sql(sql)?;
-    }
-    Ok(())
-}
-
 impl fmt::Display for Statement {
+    /// Converts(unparses) the statement to a SQL string.
+    ///
+    /// If the resulting SQL is not valid, this function will panic. Use
+    /// [`Statement::try_to_string`] to get a `Result` instead.
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        if should_validate_parsable() {
-            let mut buf = String::new();
-            self.fmt_inner(&mut buf)?;
-            do_validate_parsable(self, &buf).expect("normalized SQL should be parsable");
-            f.write_str(&buf)
-        } else {
-            self.fmt_inner(f)
-        }
+        // Note: we ignore formatting options here.
+        let sql = self
+            .try_to_string()
+            .expect("normalized SQL should be parsable");
+        f.write_str(&sql)
     }
 }
 
 impl Statement {
     /// Converts(unparses) the statement to a SQL string.
     ///
-    /// Compared to `to_string` which panics if the unparsed SQL is invalid (not parsable),
-    /// this function returns an error.
+    /// If the resulting SQL is not valid, returns an error.
     pub fn try_to_string(&self) -> Result<String, ParserError> {
         let sql = self.to_string_unchecked();
-        do_validate_parsable(self, &sql)?;
+
+        // TODO(#20713): expand this check to all statements
+        if matches!(
+            self,
+            Statement::CreateTable { .. } | Statement::CreateSource { .. }
+        ) {
+            let _ = Parser::parse_sql(&sql)?;
+        }
         Ok(sql)
     }
 
     /// Converts(unparses) the statement to a SQL string.
     ///
-    /// Compared to `to_string` which panics if the unparsed SQL is invalid (not parsable),
-    /// this function skips the validation.
-    ///
-    /// Always prefer [`Statement::try_to_string`] or `to_string` to expose potential
-    /// incorrectness in [`Display`] implementation.
+    /// The result may not be valid SQL if there's an implementation bug in the `Display`
+    /// trait of any AST node. To avoid this, always prefer [`Statement::try_to_string`]
+    /// to get a `Result`, or `to_string` which panics if the SQL is invalid.
     pub fn to_string_unchecked(&self) -> String {
-        VALIDATE_PARSABLE.sync_scope(false, || self.to_string())
+        let mut buf = String::new();
+        self.fmt_unchecked(&mut buf).unwrap();
+        buf
     }
 
+    // NOTE: This function should not check the validity of the unparsed SQL (and panic).
+    //       Thus, do not recursively call `Statement::fmt`. Call `fmt_unchecked` instead.
+    //
     // Clippy thinks this function is too complicated, but it is painful to
     // split up without extracting structs for each `Statement` variant.
     #[allow(clippy::cognitive_complexity)]
-    fn fmt_inner(&self, mut f: impl std::fmt::Write) -> fmt::Result {
+    fn fmt_unchecked(&self, mut f: impl std::fmt::Write) -> fmt::Result {
         match self {
             Statement::Explain {
                 analyze,
@@ -1738,7 +1728,7 @@ impl Statement {
                 }
                 write!(f, "{}", options)?;
 
-                statement.fmt_inner(f)
+                statement.fmt_unchecked(f)
             }
             Statement::ExplainAnalyzeStreamJob {
                 target,
@@ -2326,7 +2316,7 @@ impl Statement {
                     write!(f, "({}) ", display_comma_separated(data_types))?;
                 }
                 write!(f, "AS ")?;
-                statement.fmt_inner(f)
+                statement.fmt_unchecked(f)
             }
             Statement::Comment {
                 object_type,
