ce7c10de5fa438decbc1ae907de7430b8bb995d7
zwang28 <70626450+zwang28@users.noreply.github.com>
2025-04-28 12:52:45 +0800

    fix: reset client pool for compute node (#21603)

--- src/compute/src/observer/observer_manager.rs
+++ src/compute/src/observer/observer_manager.rs
@@ -12,14 +12,19 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+use std::sync::Arc;
+
 use risingwave_common::secret::LocalSecretManager;
 use risingwave_common::system_param::local_manager::LocalSystemParamsManagerRef;
 use risingwave_common_service::ObserverState;
 use risingwave_pb::meta::subscribe_response::{Info, Operation};
 use risingwave_pb::meta::SubscribeResponse;
+use risingwave_rpc_client::{ComputeClient, RpcClientPool};
 
 pub struct ComputeObserverNode {
     system_params_manager: LocalSystemParamsManagerRef,
+    stream_client_pool: Arc<RpcClientPool<ComputeClient>>,
+    batch_client_pool: Arc<RpcClientPool<ComputeClient>>,
 }
 
 impl ObserverState for ComputeObserverNode {
@@ -45,6 +50,14 @@ impl ObserverState for ComputeObserverNode {
                         panic!("error type notification");
                     }
                 },
+                Info::Recovery(_) => {
+                    self.stream_client_pool.invalidate_all();
+                    // Reset batch client pool on recovery is always unnecessary
+                    // when serving and streaming have been separated.
+                    // It can still be used as a method to manually trigger a reset of the batch client pool.
+                    // TODO: invalidate a single batch client on any connection issue.
+                    self.batch_client_pool.invalidate_all();
+                }
                 _ => {
                     panic!("error type notification");
                 }
@@ -61,9 +74,15 @@ impl ObserverState for ComputeObserverNode {
 }
 
 impl ComputeObserverNode {
-    pub fn new(system_params_manager: LocalSystemParamsManagerRef) -> Self {
+    pub fn new(
+        system_params_manager: LocalSystemParamsManagerRef,
+        stream_client_pool: Arc<RpcClientPool<ComputeClient>>,
+        batch_client_pool: Arc<RpcClientPool<ComputeClient>>,
+    ) -> Self {
         Self {
             system_params_manager,
+            stream_client_pool,
+            batch_client_pool,
         }
     }
 }
