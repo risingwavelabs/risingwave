362ac2b18a1cfcd00d15033481d75d3a2b54c83f
William Wen <william123.wen@gmail.com>
2025-04-28 14:40:53 +0800

    fix(streaming): reset stream client pool in local barrier worker

--- src/compute/src/observer/observer_manager.rs
+++ src/compute/src/observer/observer_manager.rs
@@ -17,9 +17,11 @@ use risingwave_common::system_param::local_manager::LocalSystemParamsManagerRef;
 use risingwave_common_service::ObserverState;
 use risingwave_pb::meta::subscribe_response::{Info, Operation};
 use risingwave_pb::meta::SubscribeResponse;
+use risingwave_rpc_client::ComputeClientPoolRef;
 
 pub struct ComputeObserverNode {
     system_params_manager: LocalSystemParamsManagerRef,
+    batch_client_pool: ComputeClientPoolRef,
 }
 
 impl ObserverState for ComputeObserverNode {
@@ -45,6 +47,13 @@ impl ObserverState for ComputeObserverNode {
                         panic!("error type notification");
                     }
                 },
+                Info::Recovery(_) => {
+                    // Reset batch client pool on recovery is always unnecessary
+                    // when serving and streaming have been separated.
+                    // It can still be used as a method to manually trigger a reset of the batch client pool.
+                    // TODO: invalidate a single batch client on any connection issue.
+                    self.batch_client_pool.invalidate_all();
+                }
                 _ => {
                     panic!("error type notification");
                 }
@@ -61,9 +70,13 @@ impl ObserverState for ComputeObserverNode {
 }
 
 impl ComputeObserverNode {
-    pub fn new(system_params_manager: LocalSystemParamsManagerRef) -> Self {
+    pub fn new(
+        system_params_manager: LocalSystemParamsManagerRef,
+        batch_client_pool: ComputeClientPoolRef,
+    ) -> Self {
         Self {
             system_params_manager,
+            batch_client_pool,
         }
     }
 }
