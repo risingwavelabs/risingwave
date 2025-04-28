05ad81e285fef782d4344a48008db6006a0372ed
Noel Kwan <noelkwan1998@gmail.com>
2025-04-28 16:12:08 +0800

    add warn

--- src/stream/src/executor/mod.rs
+++ src/stream/src/executor/mod.rs
@@ -550,7 +550,10 @@ impl Barrier {
                 },
                 _,
             )) => backfill_nodes_to_pause.contains(&backfill_fragment_id),
-            _ => true,
+            _ => {
+                tracing::warn!("expected an AddMutation on Startup, instead got {:?}", self);
+                true
+            }
         }
     }
 
