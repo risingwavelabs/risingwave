baa0f9c8c24b6cdcd754a1577973a4a05e4e5326
cardigan1008 <yunboni@smail.nju.edu.cn>
2025-05-03 18:31:13 +0800

    feat(sqlsmith): support timestamp in table functions

--- src/tests/sqlsmith/src/sql_gen/table_functions.rs
+++ src/tests/sqlsmith/src/sql_gen/table_functions.rs
@@ -12,11 +12,10 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+use chrono::{Duration, NaiveDateTime};
 use rand::Rng;
-use risingwave_sqlparser::ast::{Expr, FunctionArg, ObjectName, TableAlias, TableFactor, Value};
-use crate::sql_gen::{utils::create_args, SqlGenerator, Table};
-
-use super::utils::create_table_alias;
+use risingwave_sqlparser::ast::{Expr, FunctionArg, ObjectName, TableAlias, TableFactor, Value, DataType as AstDataType};
+use crate::sql_gen::{utils::{create_args, create_table_alias}, SqlGenerator, Table};
 
 impl<R: Rng> SqlGenerator<'_, R> {
     /// Generates table functions
@@ -29,6 +28,9 @@ impl<R: Rng> SqlGenerator<'_, R> {
     }
 
     /// Generates `GENERATE_SERIES`
+    /// GENERATE_SERIES(start: INT | TIMESTAMP, end: INT | TIMESTAMP, step?: INT | INTERVAL)
+    /// - When type is INT: step is optional.
+    /// - When type is TIMESTAMP: step (INTERVAL) is required.
     fn gen_generate_series(&mut self) -> (TableFactor, Table) {
         let table_name= self.gen_table_name_with_prefix("generate_series");
         let alias = create_table_alias(&table_name);
@@ -47,6 +49,9 @@ impl<R: Rng> SqlGenerator<'_, R> {
     }
 
     /// Generates `RANGE`
+    /// RANGE(start: INT | TIMESTAMP, end: INT | TIMESTAMP, step?: INT | INTERVAL)
+    /// - When type is INT: step is optional.
+    /// - When type is TIMESTAMP: step (INTERVAL) is required.
     fn gen_range(&mut self) -> (TableFactor, Table) {
         let table_name= self.gen_table_name_with_prefix("range");
         let alias = create_table_alias(&table_name);
@@ -64,25 +69,64 @@ impl<R: Rng> SqlGenerator<'_, R> {
         (relation, table)
     }
 
-    fn integer_to_expr(num: i32) -> Expr {
+    fn integer_to_value_expr(num: i32) -> Expr {
         Expr::Value(Value::Number(num.to_string()))
     }
 
-    fn gen_start_end_step(&mut self) -> (Expr, Expr, Option<Expr>) {
+    fn gen_simple_integer_range(&mut self) -> (Expr, Expr, Option<Expr>) {
         let mut values: Vec<i32> = (0..3)
             .map(|_| self.rng.random_range(0..100))
             .collect();
         values.sort_unstable();
 
-        let start = Self::integer_to_expr(values[0]);
-        let end = Self::integer_to_expr(values[2]);
-        let step = Some(Self::integer_to_expr(values[1]));
+        let start = Self::integer_to_value_expr(values[0]);
+        let end = Self::integer_to_value_expr(values[2]);
+        let step = Some(Self::integer_to_value_expr(values[1]));
 
         if self.flip_coin() {
             (start, end, step)
         } else {
             (start, end, None)
         }
+    }
+
+    fn integer_to_timestamp_expr(offset_secs: i32) -> Expr {
+        let base = NaiveDateTime::parse_from_str("2020-01-01 00:00:00", "%Y-%m-%d %H:%M:%S").unwrap();
+        let ts = base + Duration::seconds(offset_secs as i64);
+        Expr::TypedString {
+            data_type: AstDataType::Timestamp(false),
+            value: ts.format("%Y-%m-%d %H:%M:%S").to_string(),
+        }
+    }
+
+    fn integer_to_interval_expr(num: i32) -> Expr {
+        Expr::TypedString {
+            data_type: AstDataType::Interval,
+            value: format!("{} seconds", num),
+        }
+    }
+
+    fn gen_simple_timestamp_range(&mut self) -> (Expr, Expr, Option<Expr>) {
+        let mut secs: Vec<i32> = (0..3)
+            .map(|_| self.rng.random_range(0..3600))
+            .collect();
+        secs.sort_unstable();
+
+        let start = Self::integer_to_timestamp_expr(secs[0]);
+        let end = Self::integer_to_timestamp_expr(secs[2]);
+        let step = Some(Self::integer_to_interval_expr(secs[1]));
+
+        (start, end, step)
+    }
+
+    fn gen_start_end_step(&mut self) -> (Expr, Expr, Option<Expr>) {
+        let range = match self.rng.random_range(0..=1) {
+            0..=0 => self.gen_simple_integer_range(),
+            1..=1 => self.gen_simple_timestamp_range(),
+            _ => unreachable!()
+        };
+
+        range
     }
 }
 
