20234aaf5e7c77664e41c59c1c581ab557ae4c08
xxchan <xxchan22f@gmail.com>
2025-05-07 19:49:26 +0800

    .
    Signed-off-by: xxchan <xxchan22f@gmail.com>

--- src/utils/runtime/src/logger.rs
+++ src/utils/runtime/src/logger.rs
@@ -190,7 +190,7 @@ fn disabled_filter() -> filter::Targets {
 /// RUST_LOG="risingwave_storage::hummock::event_handler=off,batch_execute=off,risingwave_batch::task=off" ENABLE_PRETTY_LOG=true risedev d
 /// ```
 pub fn init_risingwave_logger(settings: LoggerSettings) {
-    let deployment = CURRENT_DEPLOYMENT.get_mut();
+    let deployment = Deployment::current();
 
     // Default timer for logging with local time offset.
     let default_timer = OffsetTime::local_rfc_3339().unwrap_or_else(|e| {
