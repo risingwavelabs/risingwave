706c7f9e70ddcab2d54bc073512db790699eb1f0
Li0k <yuli@singularity-data.com>
2025-05-09 15:38:33 +0800

    feat(compactor): introduce iceberg compactor manager (#21785)

--- src/meta/node/src/server.rs
+++ src/meta/node/src/server.rs
@@ -29,6 +29,7 @@ use risingwave_meta::MetaStoreBackend;
 use risingwave_meta::barrier::GlobalBarrierManager;
 use risingwave_meta::controller::catalog::CatalogController;
 use risingwave_meta::controller::cluster::ClusterController;
+use risingwave_meta::hummock::IcebergCompactorManager;
 use risingwave_meta::manager::iceberg_compaction::IcebergCompactionManager;
 use risingwave_meta::manager::{META_NODE_ID, MetadataManager};
 use risingwave_meta::rpc::ElectionClientRef;
@@ -474,9 +475,18 @@ pub async fn start_service_as_election_leader(
     // TODO(shutdown): remove this as there's no need to gracefully shutdown some of these sub-tasks.
     let mut sub_tasks = vec![shutdown_handle];
 
-    let iceberg_compaction_mgr =
-        IcebergCompactionManager::new(metadata_manager.clone(), iceberg_compaction_stat_rx);
-    sub_tasks.push(iceberg_compaction_mgr.start());
+    let iceberg_compactor_manager = Arc::new(IcebergCompactorManager::new());
+
+    // TODO: introduce compactor event stream handler to handle iceberg compaction events.
+    let (iceberg_compaction_mgr, _iceberg_compactor_event_rx) = IcebergCompactionManager::build(
+        metadata_manager.clone(),
+        iceberg_compactor_manager.clone(),
+    );
+
+    sub_tasks.push(IcebergCompactionManager::compaction_stat_loop(
+        iceberg_compaction_mgr.clone(),
+        iceberg_compaction_stat_rx,
+    ));
 
     let scale_controller = Arc::new(ScaleController::new(
         &metadata_manager,
@@ -554,6 +564,7 @@ pub async fn start_service_as_election_leader(
         hummock_manager.clone(),
         metadata_manager.clone(),
         backup_manager.clone(),
+        iceberg_compaction_mgr.clone(),
     );
 
     let health_srv = HealthServiceImpl::new();
