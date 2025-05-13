148b4948b38cb89e680033c4106569bb6819398c
Bugen Zhao <i@bugenzhao.com>
2025-05-13 15:50:27 +0800

    refactor(meta): avoid using frontend generated `ColumnIndexMapping` when constructing new dispatcher for replacement (#21499)
    Signed-off-by: Bugen Zhao <i@bugenzhao.com>

--- src/meta/src/rpc/ddl_controller.rs
+++ src/meta/src/rpc/ddl_controller.rs
@@ -726,7 +726,7 @@ impl DdlController {
         fragment_graph: StreamFragmentGraph,
     ) -> MetaResult<(ReplaceStreamJobContext, StreamJobFragmentsToCreate)> {
         let (mut replace_table_ctx, mut stream_job_fragments) = self
-            .build_replace_job(stream_ctx, streaming_job, fragment_graph, None, tmp_id as _)
+            .build_replace_job(stream_ctx, streaming_job, fragment_graph, tmp_id as _)
             .await?;
 
         let target_table = streaming_job.table().unwrap();
@@ -1407,13 +1407,7 @@ impl DdlController {
         let mut drop_table_connector_ctx = None;
         let result: MetaResult<_> = try {
             let (mut ctx, mut stream_job_fragments) = self
-                .build_replace_job(
-                    ctx,
-                    &streaming_job,
-                    fragment_graph,
-                    col_index_mapping.as_ref(),
-                    tmp_id as _,
-                )
+                .build_replace_job(ctx, &streaming_job, fragment_graph, tmp_id as _)
                 .await?;
             drop_table_connector_ctx = ctx.drop_table_connector_ctx.clone();
 
@@ -1845,7 +1839,6 @@ impl DdlController {
         stream_ctx: StreamContext,
         stream_job: &StreamingJob,
         mut fragment_graph: StreamFragmentGraph,
-        col_index_mapping: Option<&ColIndexMapping>,
         tmp_job_id: TableId,
     ) -> MetaResult<(ReplaceStreamJobContext, StreamJobFragmentsToCreate)> {
         match &stream_job {
@@ -1904,20 +1897,9 @@ impl DdlController {
 
         let job_type = StreamingJobType::from(stream_job);
 
-        // Map the column indices in the dispatchers with the given mapping.
-        let (mut downstream_fragments, downstream_actor_location) =
+        // Extract the downstream fragments from the fragment graph.
+        let (downstream_fragments, downstream_actor_location) =
             self.metadata_manager.get_downstream_fragments(id).await?;
-        if let Some(mapping) = &col_index_mapping {
-            for (d, _f) in &mut downstream_fragments {
-                *d = mapping.rewrite_dispatch_strategy(d).ok_or_else(|| {
-                    // The `rewrite` only fails if some column is dropped (missing) or altered (type changed).
-                    // TODO: support altering referenced columns
-                    MetaError::invalid_parameter(
-                        "unable to drop or alter the column due to being referenced by downstream materialized views or sinks",
-                    )
-                })?;
-            }
-        }
 
         // build complete graph based on the table job type
         let complete_graph = match &job_type {
