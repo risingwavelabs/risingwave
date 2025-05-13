a730e2e7d4294fc726be5ae148e17c87ae022fbc
Croxx <mrcroxx@outlook.com>
2025-05-13 15:18:03 +0800

    feat(foyer): add config field to set file cache fifo probation ratio (#21841)
    Signed-off-by: MrCroxx <mrcroxx@outlook.com>

--- src/common/src/config.rs
+++ src/common/src/config.rs
@@ -1012,6 +1012,9 @@ pub struct FileCacheConfig {
     #[serde(default = "default::file_cache::throttle")]
     pub throttle: Throttle,
 
+    #[serde(default = "default::file_cache::fifo_probation_ratio")]
+    pub fifo_probation_ratio: f64,
+
     /// Recover mode.
     ///
     /// Options:
@@ -1960,6 +1963,10 @@ pub mod default {
 
         pub fn flush_buffer_threshold_mb() -> Option<usize> {
             None
+        }
+
+        pub fn fifo_probation_ratio() -> f64 {
+            0.1
         }
 
         pub fn recover_mode() -> RecoverMode {
