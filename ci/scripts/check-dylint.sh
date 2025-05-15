7265d0e3900a83118ddfdb5f30ca64bec3392b86
Bugen Zhao <i@bugenzhao.com>
2025-05-15 18:02:22 +0800

    chore(ci): enforce unit (UI) test of dylint
    Signed-off-by: Bugen Zhao <i@bugenzhao.com>

--- ci/scripts/check-dylint.sh
+++ ci/scripts/check-dylint.sh
@@ -21,3 +21,8 @@ echo "--- Run dylint check (dev, all features)"
 # - The toolchain used for linting could be slightly different from the one used to
 #   compile RisingWave. Warnings from `rustc` itself may produce false positives.
 DYLINT_RUSTFLAGS="-A warnings -D rw_warnings" cargo dylint --all -- --all-targets --all-features --locked
+
+echo "--- Run UI test of lints"
+cd lints
+cargo test
+cd ..
