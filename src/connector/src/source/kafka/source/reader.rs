ec2abb19d10778cde5962d6418b5dd42c9f8aaa3
tab <tabversion@bupt.icu>
2025-05-18 04:39:32 +0800

    fix

--- src/connector/src/source/kafka/source/reader.rs
+++ src/connector/src/source/kafka/source/reader.rs
@@ -100,6 +100,10 @@ impl SplitReader for KafkaSplitReader {
             .await
             .context("failed to create kafka consumer")?;
 
+        // poll consumer to trigger callback functions
+        // We don't need to wait for the result, because recv may block and wait for next new message
+        let _ = tokio::time::timeout(Duration::from_millis(100), consumer.recv()).await;
+
         let mut tpl = TopicPartitionList::with_capacity(splits.len());
 
         let mut offsets = HashMap::new();
@@ -149,10 +153,6 @@ impl SplitReader for KafkaSplitReader {
         );
 
         consumer.assign(&tpl)?;
-
-        // poll consumer to trigger callback functions
-        // We don't need to wait for the result, because recv may block and wait for next new message
-        let _ = tokio::time::timeout(Duration::from_millis(1000), consumer.recv()).await;
 
         // The two parameters below are only used by developers for performance testing purposes,
         // so we panic here on purpose if the input is not correctly recognized.
