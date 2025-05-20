29295fdde9ec0c3c7a9c984d278f030f62f9c39d
Noel Kwan <noelkwan1998@gmail.com>
2025-05-20 18:56:00 +0800

    use dedicated rt

--- src/connector/src/source/kafka/client_context.rs
+++ src/connector/src/source/kafka/client_context.rs
@@ -13,8 +13,7 @@
 // limitations under the License.
 
 use std::collections::BTreeMap;
-use std::sync::{Arc, Weak};
-use std::thread;
+use std::sync::{Arc, LazyLock, Weak};
 
 use anyhow::anyhow;
 use aws_config::Region;
@@ -24,6 +23,7 @@ use rdkafka::consumer::{Consumer, ConsumerContext, StreamConsumer};
 use rdkafka::message::DeliveryResult;
 use rdkafka::producer::ProducerContext;
 use rdkafka::{ClientContext, Statistics};
+use tokio::runtime::Runtime;
 use tokio::time::interval;
 
 use super::private_link::{BrokerAddrRewriter, PrivateLinkContextRole};
@@ -116,6 +116,14 @@ impl KafkaContextCommon {
     }
 }
 
+pub static KAFKA_SOURCE_RUNTIME: LazyLock<Runtime> = LazyLock::new(|| {
+    tokio::runtime::Builder::new_multi_thread()
+        .thread_name("rw-frontend")
+        .enable_all()
+        .build()
+        .expect("failed to build frontend runtime")
+});
+
 impl KafkaContextCommon {
     fn stats(&self, statistics: Statistics) {
         if let Some(metrics) = &self.metrics
@@ -147,14 +155,10 @@ impl KafkaContextCommon {
         {
             let region = region.clone();
             let credentials_provider = credentials_provider.clone();
-            let iam_rt = tokio::runtime::Builder::new_current_thread()
-                .enable_all()
-                .build()
-                .unwrap();
             let signer_timeout_sec = *signer_timeout_sec;
             let (token, expiration_time_ms) = {
-                let handle = thread::spawn(move || {
-                    iam_rt.block_on(async {
+                tokio::task::block_in_place(move || {
+                    KAFKA_SOURCE_RUNTIME.block_on(async {
                         timeout(
                             Duration::from_secs(signer_timeout_sec),
                             generate_auth_token_from_credentials_provider(
@@ -164,8 +168,9 @@ impl KafkaContextCommon {
                         )
                         .await
                     })
-                });
-                handle.join().unwrap()??
+                })
+                .unwrap()
+                .unwrap()
             };
             Ok(OAuthToken {
                 token,
