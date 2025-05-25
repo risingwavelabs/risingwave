31de81c4569d0817363b4e6b2471897382b808cf
Dylan <zilin@risingwave-labs.com>
2025-05-25 12:33:26 +0800

    feat(iceberg): cherry-pick init and backup hosted iceberg system table (#21980)

--- src/meta/model/migration/src/lib.rs
+++ src/meta/model/migration/src/lib.rs
@@ -37,6 +37,8 @@ mod m20250121_085800_change_wasm_udf_identifier;
 mod m20250210_170743_function_options;
 mod m20250319_062702_mysql_utf8mb4;
 mod m20250325_061743_exactly_once_iceberg_sink_metadata;
+mod m20250522_074525_iceberg_tables;
+mod m20250522_074947_iceberg_namespace_properties;
 mod utils;
 
 pub struct Migrator;
@@ -112,6 +114,8 @@ impl MigratorTrait for Migrator {
             Box::new(m20250210_170743_function_options::Migration),
             Box::new(m20250319_062702_mysql_utf8mb4::Migration),
             Box::new(m20250325_061743_exactly_once_iceberg_sink_metadata::Migration),
+            Box::new(m20250522_074525_iceberg_tables::Migration),
+            Box::new(m20250522_074947_iceberg_namespace_properties::Migration),
         ]
     }
 }
