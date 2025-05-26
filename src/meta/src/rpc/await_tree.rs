f447a22000f49230f3730e6ac51a5d6dfe0af142
Bugen Zhao <i@bugenzhao.com>
2025-05-26 18:15:27 +0800

    refactor to remove duplicated code
    Signed-off-by: Bugen Zhao <i@bugenzhao.com>

---
+++ src/meta/src/rpc/await_tree.rs
@@ -0,0 +1,85 @@
+// Copyright 2025 RisingWave Labs
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+use risingwave_common::util::StackTraceResponseExt as _;
+use risingwave_pb::common::{WorkerNode, WorkerType};
+use risingwave_pb::monitor_service::stack_trace_request::ActorTracesFormat;
+use risingwave_pb::monitor_service::{StackTraceRequest, StackTraceResponse};
+use risingwave_rpc_client::ComputeClientPool;
+
+use crate::MetaResult;
+use crate::manager::MetadataManager;
+
+pub async fn dump_cluster_await_tree(
+    metadata_manager: &MetadataManager,
+    meta_node_registry: &await_tree::Registry,
+    actor_traces_format: ActorTracesFormat,
+) -> MetaResult<StackTraceResponse> {
+    let mut all = StackTraceResponse::default();
+
+    let compute_nodes = metadata_manager
+        .list_worker_node(Some(WorkerType::ComputeNode), None)
+        .await?;
+    let compute_traces = dump_worker_node_await_tree(&compute_nodes, actor_traces_format).await?;
+    all.merge_other(compute_traces);
+
+    let compactor_nodes = metadata_manager
+        .list_worker_node(Some(WorkerType::Compactor), None)
+        .await?;
+    let compactor_traces =
+        dump_worker_node_await_tree(&compactor_nodes, actor_traces_format).await?;
+    all.merge_other(compactor_traces);
+
+    let meta_traces = dump_meta_node_await_tree(meta_node_registry).await?;
+    all.merge_other(meta_traces);
+
+    Ok(all)
+}
+
+pub async fn dump_meta_node_await_tree(
+    meta_node_registry: &await_tree::Registry,
+) -> MetaResult<StackTraceResponse> {
+    let meta_traces = meta_node_registry
+        .collect_all()
+        .into_iter()
+        .map(|(k, v)| (k.to_string(), v.to_string()))
+        .collect();
+
+    Ok(StackTraceResponse {
+        meta_traces,
+        ..Default::default()
+    })
+}
+
+pub async fn dump_worker_node_await_tree(
+    worker_nodes: impl IntoIterator<Item = &WorkerNode>,
+    actor_traces_format: ActorTracesFormat,
+) -> MetaResult<StackTraceResponse> {
+    let mut all = StackTraceResponse::default();
+
+    let req = StackTraceRequest {
+        actor_traces_format: actor_traces_format as i32,
+    };
+    // This is also applicable to compactor.
+    let compute_clients = ComputeClientPool::adhoc();
+
+    for worker_node in worker_nodes {
+        let client = compute_clients.get(worker_node).await?;
+        let result = client.stack_trace(req).await?;
+
+        all.merge_other(result);
+    }
+
+    Ok(all)
+}
--- src/rpc_client/src/meta_client.rs
+++ src/rpc_client/src/meta_client.rs
@@ -97,6 +97,9 @@ use risingwave_pb::meta::system_params_service_client::SystemParamsServiceClient
 use risingwave_pb::meta::telemetry_info_service_client::TelemetryInfoServiceClient;
 use risingwave_pb::meta::update_worker_node_schedulability_request::Schedulability;
 use risingwave_pb::meta::{FragmentDistribution, *};
+use risingwave_pb::monitor_service::monitor_service_client::MonitorServiceClient;
+use risingwave_pb::monitor_service::stack_trace_request::ActorTracesFormat;
+use risingwave_pb::monitor_service::{StackTraceRequest, StackTraceResponse};
 use risingwave_pb::secret::PbSecretRef;
 use risingwave_pb::stream_plan::StreamFragmentGraph;
 use risingwave_pb::user::update_user_request::UpdateField;
@@ -1611,6 +1614,14 @@ impl MetaClient {
         let resp = self.inner.list_iceberg_tables(request).await?;
         Ok(resp.iceberg_tables)
     }
+
+    pub async fn get_cluster_stack_trace(&self, actor_traces_format: ActorTracesFormat) -> Result<StackTraceResponse> {
+        let request = StackTraceRequest {
+            actor_traces_format: actor_traces_format as i32,
+        };
+        let resp = self.inner.stack_trace(request).await?;
+        Ok(resp)
+    }
 }
 
 #[async_trait]
@@ -1797,6 +1808,7 @@ struct GrpcMetaClientCore {
     event_log_client: EventLogServiceClient<Channel>,
     cluster_limit_client: ClusterLimitServiceClient<Channel>,
     hosted_iceberg_catalog_service_client: HostedIcebergCatalogServiceClient<Channel>,
+    monitor_client: MonitorServiceClient<Channel>,
 }
 
 impl GrpcMetaClientCore {
@@ -1825,7 +1837,9 @@ impl GrpcMetaClientCore {
         let sink_coordinate_client = SinkCoordinationServiceClient::new(channel.clone());
         let event_log_client = EventLogServiceClient::new(channel.clone());
         let cluster_limit_client = ClusterLimitServiceClient::new(channel.clone());
-        let hosted_iceberg_catalog_service_client = HostedIcebergCatalogServiceClient::new(channel);
+        let hosted_iceberg_catalog_service_client =
+            HostedIcebergCatalogServiceClient::new(channel.clone());
+        let monitor_client = MonitorServiceClient::new(channel);
 
         GrpcMetaClientCore {
             cluster_client,
@@ -1847,6 +1861,7 @@ impl GrpcMetaClientCore {
             event_log_client,
             cluster_limit_client,
             hosted_iceberg_catalog_service_client,
+            monitor_client,
         }
     }
 }
@@ -2294,6 +2309,7 @@ macro_rules! for_all_meta_rpc {
             ,{ event_log_client, add_event_log, AddEventLogRequest, AddEventLogResponse }
             ,{ cluster_limit_client, get_cluster_limits, GetClusterLimitsRequest, GetClusterLimitsResponse }
             ,{ hosted_iceberg_catalog_service_client, list_iceberg_tables, ListIcebergTablesRequest, ListIcebergTablesResponse }
+            ,{ monitor_client, stack_trace, StackTraceRequest, StackTraceResponse }
         }
     };
 }
