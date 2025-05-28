5c850fc83e43040745e491b64991d46e43d79434
Xiangjin <xiangjin@risingwave-labs.com>
2025-05-28 13:18:50 +0800

    fix(ci): spark 3.5.5 URL is no longer valid; use 3.5.6

--- e2e_test/commands/common.sh
+++ e2e_test/commands/common.sh
@@ -3,7 +3,7 @@
 # https://iceberg.apache.org/releases/
 # https://spark.apache.org/downloads.html
 ICEBERG_VERSION=1.8.1
-SPARK_VERSION=3.5.5
+SPARK_VERSION=3.5.6
 
 SPARK_PACKAGES="org.apache.iceberg:iceberg-spark-runtime-3.5_2.12:$ICEBERG_VERSION,org.apache.hadoop:hadoop-aws:3.3.2"
 SPARK_PACKAGES="$SPARK_PACKAGES,org.apache.spark:spark-connect_2.12:$SPARK_VERSION"
