7e0640f677dc0e1e72f9d76d22d66ff06fbf253d
Noel Kwan <47273164+kwannoel@users.noreply.github.com>
2025-05-30 11:43:00 +0800

    fix(ci): fix integration test filter (#22039)

--- ci/scripts/deterministic-it-test.sh
+++ ci/scripts/deterministic-it-test.sh
@@ -17,7 +17,17 @@ tar -xvf simulation-it-test.tar.zst
 mkdir target/sim
 mv target/ci-sim target/sim
 
-TEST_PATTERN="$@"
+if [[ -z "${1:-}" ]]; then
+  TEST_PATTERN=""
+else
+  TEST_PATTERN="-E 'test(/^$1/)'"
+fi
+
+# NOTE(kwannoel): for TEST_PATTERN, please consult: https://nexte.st/docs/filtersets/reference/.
+# Using substring matching may run unexpected tests.
+# For example, `scale::` will run all tests with a `scale` module prefix.
+# This includes sink::scale::*, scale::*.
+# We just want to run `scale::*`.
 
 echo "--- Run integration tests in deterministic simulation mode"
 seq "$TEST_NUM" | parallel -j 8 --line-buffer "MADSIM_TEST_SEED={} NEXTEST_PROFILE=ci-sim \
