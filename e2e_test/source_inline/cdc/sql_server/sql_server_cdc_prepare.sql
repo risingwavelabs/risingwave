d0f31cdac919f521df2be686ed011f10928b6d75
congyi <15605187270@163.com>
2025-06-10 20:20:13 +0800

    add test for binary type

--- e2e_test/source_inline/cdc/sql_server/sql_server_cdc_prepare.sql
+++ e2e_test/source_inline/cdc/sql_server/sql_server_cdc_prepare.sql
@@ -98,12 +98,35 @@ EXEC sys.sp_cdc_enable_table
   @source_name = 'test_pk_uuid',
   @role_name = NULL;
 
+CREATE TABLE test_pk_uuid (
+  id UNIQUEIDENTIFIER PRIMARY KEY,
+  NAME NVARCHAR(50),
+);
+
+EXEC sys.sp_cdc_enable_table
+  @source_schema = 'dbo',
+  @source_name = 'test_pk_uuid',
+  @role_name = NULL;
+
+CREATE TABLE test_pk_binary (
+  id UNIQUEIDENTIFIER PRIMARY KEY,
+  NAME NVARCHAR(50),
+);
+
+EXEC sys.sp_cdc_enable_table
+  @source_schema = 'dbo',
+  @source_name = 'test_pk_binary',
+  @role_name = NULL;
+
   DECLARE @i INT = 1;
 
 WHILE @i <= 2000
 BEGIN
     INSERT INTO test_pk_uuid (id, NAME)
     VALUES (NEWID(), CONCAT('TEST_NAME', @i));
+
+    INSERT INTO test_pk_binary (id, NAME)
+    VALUES (CAST(CONCAT('TEST_NAME', @i) AS BINARY(50)), CONCAT('TEST_NAME', @i));
 
     SET @i = @i + 1;
 END
