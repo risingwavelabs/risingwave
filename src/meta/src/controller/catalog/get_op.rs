d9cff8b0e915cbbe61f7183d7e2a90758419c4db
Patrick Huang <hzxa21@hotmail.com>
2025-06-11 15:23:36 +0800

    fmt

--- src/meta/src/controller/catalog/get_op.rs
+++ src/meta/src/controller/catalog/get_op.rs
@@ -12,7 +12,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-
 use super::*;
 use crate::controller::utils::{get_database_resource_group, get_existing_job_resource_group};
 
