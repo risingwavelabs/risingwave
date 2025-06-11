f1f4695a91ab9d15be5adcf9935327f7fb4c1199
Li0k <yuli@singularity-data.com>
2025-06-11 18:15:43 +0800

    fix(grafana): fix grafana

--- grafana/dashboard/dev/__init__.py
+++ grafana/dashboard/dev/__init__.py
@@ -34,6 +34,7 @@ from . import network_connection as _
 from . import iceberg_metrics as _
 from . import udf as _
 from . import sync_logstore_metrics as _
+from . import iceberg_compaction_metrics as _
 
 
 def generate_panels(panels: Panels):
