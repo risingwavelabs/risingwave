77b09edaa02ceefc6ebbb95cbe745b1f1182eb19
Noel Kwan <noelkwan1998@gmail.com>
2025-06-12 22:49:57 +0800

    fix rw_fragment_backfill_progress sql

--- src/frontend/src/catalog/system_catalog/rw_catalog/rw_fragment_backfill_progress.rs
+++ src/frontend/src/catalog/system_catalog/rw_catalog/rw_fragment_backfill_progress.rs
@@ -19,12 +19,12 @@ use risingwave_frontend_macro::system_catalog;
 #[system_catalog(
 view,
 "rw_catalog.rw_fragment_backfill_progress",
-"select
+"with 
+table_backfill_progress as (select
   progress.job_id,
   progress.fragment_id,
-  concat(job_schemas.name, '.', job_tables.name) as job_name,
-  concat(upstream_schemas.name, '.', upstream_tables.name) as upstream_table_name,
-  case when scan_info.is_snapshot_backfill AND progress.min_epoch > scan_info.backfill_epoch
+  scan_info.backfill_target_relation_id,
+  case when scan_info.backfill_type = 'SNAPSHOT_BACKFILL' AND progress.min_epoch > scan_info.backfill_epoch
   then concat('100% (', stats.total_key_count, '/', stats.total_key_count, ')')
   else
     concat(
@@ -39,24 +39,34 @@ view,
     )
   end as progress
 FROM internal_backfill_progress() progress
-JOIN rw_table_scan scan_info ON progress.job_id = scan_info.job_id AND progress.fragment_id = scan_info.fragment_id
-JOIN rw_table_stats stats ON scan_info.backfill_target_table_id = stats.id
-JOIN rw_relations job_tables ON scan_info.job_id = job_tables.id
-JOIN rw_schemas job_schemas ON job_tables.schema_id = job_schemas.id
-JOIN rw_relations upstream_tables ON scan_info.backfill_target_table_id = upstream_tables.id
-JOIN rw_schemas upstream_schemas ON upstream_tables.schema_id = upstream_schemas.id
-UNION ALL
-select
+JOIN rw_backfill_info scan_info ON progress.job_id = scan_info.job_id AND progress.fragment_id = scan_info.fragment_id
+JOIN rw_table_stats stats ON scan_info.backfill_target_relation_id = stats.id
+),
+source_backfill_progress as (select
   source_backfill_progress.job_id,
   source_backfill_progress.fragment_id,
+  scan_info.backfill_target_relation_id,
+  source_backfill_progress.backfill_progress::varchar as progress
+FROM internal_source_backfill_progress() source_backfill_progress
+JOIN rw_backfill_info scan_info ON source_backfill_progress.job_id = scan_info.job_id AND source_backfill_progress.fragment_id = scan_info.fragment_id
+),
+backfill_progress as (
+  select * from table_backfill_progress
+  UNION ALL
+  select * from source_backfill_progress
+)
+select
+  backfill_progress.job_id,
+  backfill_progress.fragment_id,
   concat(job_schemas.name, '.', job_tables.name) as job_name,
   concat(upstream_schemas.name, '.', upstream_tables.name) as upstream_table_name,
-  concat(source_backfill_progress.current_row_count, '/', source_backfill_progress.total_row_count) as progress
-FROM internal_source_backfill_progress() source_backfill_progress
-JOIN rw_relations job_tables ON source_backfill_progress.job_id = job_tables.id
-JOIN rw_schemas job_schemas ON job_tables.schema_id = job_schemas.id
-JOIN rw_relations upstream_tables ON source_backfill_progress.job_id = upstream_tables.id
-JOIN rw_schemas upstream_schemas ON upstream_tables.schema_id = upstream_schemas.id
+  backfill_progress.progress
+FROM
+  backfill_progress
+  join rw_relations job_tables on backfill_progress.job_id = job_tables.id
+  join rw_schemas job_schemas on job_tables.schema_id = job_schemas.id
+  join rw_relations upstream_tables on backfill_progress.backfill_target_relation_id = upstream_tables.id
+  join rw_schemas upstream_schemas on upstream_tables.schema_id = upstream_schemas.id
 "
 )]
 #[derive(Fields)]
