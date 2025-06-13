f494e12d3ae58944ba15404e849f5b90445c7fcb
Noel Kwan <noelkwan1998@gmail.com>
2025-06-13 16:43:57 +0800

    pause initial nodes

--- src/meta/src/barrier/checkpoint/creating_job/mod.rs
+++ src/meta/src/barrier/checkpoint/creating_job/mod.rs
@@ -34,6 +34,7 @@ use risingwave_pb::stream_service::BarrierCompleteResponse;
 use status::CreatingStreamingJobStatus;
 use tracing::{debug, info};
 
+use crate::barrier::backfill_order_control::get_nodes_with_backfill_dependencies;
 use crate::MetaResult;
 use crate::barrier::checkpoint::creating_job::status::CreateMviewLogStoreProgressTracker;
 use crate::barrier::edge_builder::FragmentEdgeBuildResult;
@@ -81,6 +82,7 @@ impl CreatingStreamingJobControl {
             "new creating job"
         );
         let snapshot_backfill_actors = info.stream_job_fragments.snapshot_backfill_actor_ids();
+        let backfill_nodes_to_pause = get_nodes_with_backfill_dependencies(&info.fragment_backfill_ordering).into_iter().collect();
         let backfill_order_state = BackfillOrderState::new(
             info.fragment_backfill_ordering.clone(),
             &info.stream_job_fragments,
@@ -133,7 +135,7 @@ impl CreatingStreamingJobControl {
             // we assume that when handling snapshot backfill, the cluster must not be paused
             pause: false,
             subscriptions_to_add: Default::default(),
-            backfill_nodes_to_pause: Default::default(),
+            backfill_nodes_to_pause,
         });
 
         control_stream_manager.add_partial_graph(database_id, Some(job_id));
