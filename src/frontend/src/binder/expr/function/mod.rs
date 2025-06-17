0b3a188bad5705bcaf66bf29509f75726e819fcb
Noel Kwan <47273164+kwannoel@users.noreply.github.com>
2025-06-17 16:03:31 +0800

    feat(frontend): add source backfill progress tracking (#22217)

--- src/frontend/src/binder/expr/function/mod.rs
+++ src/frontend/src/binder/expr/function/mod.rs
@@ -362,6 +362,15 @@ impl Binder {
                 self.ensure_table_function_allowed()?;
                 return Ok(TableFunction::new_internal_backfill_progress().into());
             }
+            // `internal_source_backfill_progress` table function
+            if func_name.eq("internal_source_backfill_progress") {
+                reject_syntax!(
+                    arg_list.variadic,
+                    "`VARIADIC` is not allowed in table function call"
+                );
+                self.ensure_table_function_allowed()?;
+                return Ok(TableFunction::new_internal_source_backfill_progress().into());
+            }
             // UDTF
             if let Some(ref udf) = udf
                 && udf.kind.is_table()
