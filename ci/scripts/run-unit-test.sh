594f4d61fdb20b1fc8b99a8aeff0fb92094e18e2
Bugen Zhao <i@bugenzhao.com>
2025-06-23 16:18:09 +0800

    change to %4m, update comments
    Signed-off-by: Bugen Zhao <i@bugenzhao.com>

--- ci/scripts/run-unit-test.sh
+++ ci/scripts/run-unit-test.sh
@@ -8,11 +8,13 @@ REPO_ROOT=${PWD}
 source ci/scripts/common.sh
 
 # Enable coverage instrumentation.
-# `cargo-nextest` will spawn a process for each test. Avoid including `%p` in the file name pattern
-# to avoid creating too many profraw files.
-# https://github.com/taiki-e/cargo-llvm-cov/issues/335#issuecomment-1890349373
 export RW_BUILD_INSTRUMENT_COVERAGE=1
-export LLVM_PROFILE_FILE='/risingwave/target/risingwave-unit-test-%2m.profraw'
+# `cargo-nextest` will spawn a process for each test. Avoid including `%p` in the file name pattern
+# to avoid creating too many profraw files. Instead, use `%m` to reuse the profraw files for the
+# same test binary. Here `%4m` means that there's a pool of 4 profraw files for each test binary.
+# This achieves a good balance between the performance of running tests and generating reports.
+# https://github.com/taiki-e/cargo-llvm-cov/issues/335#issuecomment-1890349373
+export LLVM_PROFILE_FILE='/risingwave/target/risingwave-unit-test-%4m.profraw'
 
 echo "+++ Run unit tests"
 # use tee to disable progress bar
