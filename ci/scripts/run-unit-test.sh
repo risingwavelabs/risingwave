eef045bd03a7f63032d80a50e889763de3e6d77c
Bugen Zhao <i@bugenzhao.com>
2025-06-23 14:24:19 +0800

    feat(ci): code coverage for unit test
    Signed-off-by: Bugen Zhao <i@bugenzhao.com>

--- ci/scripts/run-unit-test.sh
+++ ci/scripts/run-unit-test.sh
@@ -6,7 +6,13 @@ set -euo pipefail
 REPO_ROOT=${PWD}
 
 source ci/scripts/common.sh
-# TODO: use llvm-cov's nextest wrapper to provide coverage information
+
+# Enable coverage instrumentation.
+# `cargo-nextest` will spawn a process for each test. Avoid including `%p` in the file name pattern
+# to avoid creating too many profraw files.
+# https://github.com/taiki-e/cargo-llvm-cov/issues/335#issuecomment-1890349373
+export RW_BUILD_INSTRUMENT_COVERAGE=1
+export LLVM_PROFILE_FILE='/risingwave/target/risingwave-unit-test-%m.profraw'
 
 echo "+++ Run unit tests"
 # use tee to disable progress bar
