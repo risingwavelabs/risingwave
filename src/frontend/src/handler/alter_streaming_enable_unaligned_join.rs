7f0a44b1562bf2799a70b197b82a49fa1cb3618b
Noel Kwan <noelkwan1998@gmail.com>
2025-06-25 14:01:14 +0800

    only enable unaligned join in debug mode

--- src/frontend/src/handler/alter_streaming_enable_unaligned_join.rs
+++ src/frontend/src/handler/alter_streaming_enable_unaligned_join.rs
@@ -26,53 +26,57 @@ pub async fn handle_alter_streaming_enable_unaligned_join(
     name: ObjectName,
     enable: bool,
 ) -> Result<RwPgResponse> {
-    let session = handler_args.session.clone();
-    let job_id = {
-        let mut binder = Binder::new_for_system(&session);
-        Binder::validate_cross_db_reference(&session.database(), &name)?;
-        let not_found_err = CatalogError::NotFound("stream job", name.to_string());
+    if cfg!(debug_assertions) {
+        let session = handler_args.session.clone();
+        let job_id = {
+            let mut binder = Binder::new_for_system(&session);
+            Binder::validate_cross_db_reference(&session.database(), &name)?;
+            let not_found_err = CatalogError::NotFound("stream job", name.to_string());
 
-        if let Ok(relation) = binder.bind_catalog_relation_by_object_name(name.clone(), true) {
-            match relation {
-                Relation::Source(s) => {
-                    if s.is_shared() {
-                        s.catalog.id
-                    } else {
+            if let Ok(relation) = binder.bind_catalog_relation_by_object_name(name.clone(), true) {
+                match relation {
+                    Relation::Source(s) => {
+                        if s.is_shared() {
+                            s.catalog.id
+                        } else {
+                            bail!(ErrorCode::NotSupported(
+                                "source has no unaligned_join".to_owned(),
+                                "Please only target materialized views or sinks".to_owned(),
+                            ));
+                        }
+                    }
+                    Relation::BaseTable(t) => t.table_catalog.id.table_id,
+                    Relation::SystemTable(_t) => {
                         bail!(ErrorCode::NotSupported(
-                            "source has no unaligned_join".to_owned(),
+                            "system table has no unaligned_join".to_owned(),
                             "Please only target materialized views or sinks".to_owned(),
                         ));
                     }
+                    Relation::Share(_s) => {
+                        bail!(ErrorCode::NotSupported(
+                            "view has no unaligned_join".to_owned(),
+                            "Please only target materialized views or sinks".to_owned(),
+                        ));
+                    }
+                    _ => {
+                        // Other relation types (Subquery, Join, etc.) are not directly describable.
+                        return Err(not_found_err.into());
+                    }
                 }
-                Relation::BaseTable(t) => t.table_catalog.id.table_id,
-                Relation::SystemTable(_t) => {
-                    bail!(ErrorCode::NotSupported(
-                        "system table has no unaligned_join".to_owned(),
-                        "Please only target materialized views or sinks".to_owned(),
-                    ));
-                }
-                Relation::Share(_s) => {
-                    bail!(ErrorCode::NotSupported(
-                        "view has no unaligned_join".to_owned(),
-                        "Please only target materialized views or sinks".to_owned(),
-                    ));
-                }
-                _ => {
-                    // Other relation types (Subquery, Join, etc.) are not directly describable.
-                    return Err(not_found_err.into());
-                }
+            } else if let Ok(sink) = binder.bind_sink_by_name(name.clone()) {
+                sink.sink_catalog.id.sink_id
+            } else {
+                return Err(not_found_err.into());
             }
-        } else if let Ok(sink) = binder.bind_sink_by_name(name.clone()) {
-            sink.sink_catalog.id.sink_id
-        } else {
-            return Err(not_found_err.into());
-        }
-    };
-    let meta_client = session.env().meta_client();
-    meta_client
-        .set_sync_log_store_aligned(job_id, !enable)
-        .await?;
-    Ok(PgResponse::empty_result(
-        StatementType::ALTER_MATERIALIZED_VIEW,
-    ))
+        };
+        let meta_client = session.env().meta_client();
+        meta_client
+            .set_sync_log_store_aligned(job_id, !enable)
+            .await?;
+        Ok(PgResponse::empty_result(
+            StatementType::ALTER_MATERIALIZED_VIEW,
+        ))
+    } else {
+        bail!("ALTER STREAMING ENABLE UNALIGNED JOIN is only supported in debug mode");
+    }
 }
