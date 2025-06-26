c03b61340a9cddd00abd7469e72a95d69c3c9b53
risingwave-ci <113648949+risingwave-ci@users.noreply.github.com>
2025-06-26 19:17:27 +0800

    feat(storage): batch add flushed imm to uploader (#22342) (#22356)
    Co-authored-by: William Wen <44139337+wenym1@users.noreply.github.com>

--- src/stream/src/executor/sync_kv_log_store.rs
+++ src/stream/src/executor/sync_kv_log_store.rs
@@ -536,6 +536,7 @@ impl<S: StateStore> SyncedKvLogStoreExecutor<S> {
                 },
                 is_replicated: false,
                 vnodes: self.serde.vnodes().clone(),
+                upload_on_flush: false,
             })
             .await;
 
