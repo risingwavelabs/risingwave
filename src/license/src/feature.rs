0d1d8caf171c9c069f407baac88162e5aee5c34e
Bugen Zhao <i@bugenzhao.com>
2025-07-04 17:31:04 +0800

    bypass clippy doc
    Signed-off-by: Bugen Zhao <i@bugenzhao.com>

--- src/license/src/feature.rs
+++ src/license/src/feature.rs
@@ -12,6 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+#![allow(clippy::doc_markdown)]
+
 use serde::{Deserialize, Serialize};
 use thiserror::Error;
 
