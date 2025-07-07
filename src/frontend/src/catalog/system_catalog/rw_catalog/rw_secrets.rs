020a34d08aff37df4083718ca26f89271dd81ac1
August <pin@singularity-data.com>
2025-07-07 17:44:40 +0800

    fix: construct user info after secret and conn privilege granted or revoked

--- src/frontend/src/catalog/system_catalog/rw_catalog/rw_secrets.rs
+++ src/frontend/src/catalog/system_catalog/rw_catalog/rw_secrets.rs
@@ -14,8 +14,9 @@
 
 use risingwave_common::types::Fields;
 use risingwave_frontend_macro::system_catalog;
+use risingwave_pb::user::grant_privilege::Object as GrantObject;
 
-use crate::catalog::system_catalog::SysCatalogReaderImpl;
+use crate::catalog::system_catalog::{SysCatalogReaderImpl, get_acl_items};
 use crate::error::Result;
 
 #[derive(Fields)]
@@ -32,6 +33,9 @@ struct RwSecret {
 fn read_rw_view_info(reader: &SysCatalogReaderImpl) -> Result<Vec<RwSecret>> {
     let catalog_reader = reader.catalog_reader.read_guard();
     let schemas = catalog_reader.iter_schemas(&reader.auth_context.database)?;
+    let user_reader = reader.user_info_reader.read_guard();
+    let users = user_reader.get_all_users();
+    let username_map = user_reader.get_user_name_map();
 
     Ok(schemas
         .flat_map(|schema| {
@@ -40,7 +44,12 @@ fn read_rw_view_info(reader: &SysCatalogReaderImpl) -> Result<Vec<RwSecret>> {
                 schema_id: secret.schema_id as i32,
                 name: secret.name.clone(),
                 owner: secret.owner as i32,
-                acl: vec![],
+                acl: get_acl_items(
+                    &GrantObject::SecretId(secret.id.secret_id()),
+                    false,
+                    &users,
+                    username_map,
+                ),
             })
         })
         .collect())
