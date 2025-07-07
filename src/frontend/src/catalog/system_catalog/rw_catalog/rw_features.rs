f909f583f2fc91b0e0ce72ab08243e5eea1fb8ff
Bugen Zhao <i@bugenzhao.com>
2025-07-07 16:29:10 +0800

    add e2e tests
    Signed-off-by: Bugen Zhao <i@bugenzhao.com>

--- src/frontend/src/catalog/system_catalog/rw_catalog/rw_features.rs
+++ src/frontend/src/catalog/system_catalog/rw_catalog/rw_features.rs
@@ -6,14 +6,14 @@ use risingwave_frontend_macro::system_catalog;
 
 use crate::catalog::system_catalog::SysCatalogReaderImpl;
 use crate::error::Result;
-use crate::session::current::notice_to_user;
 
+/// Check all defined premium features and their availability.
 #[derive(Fields)]
 struct RwFeature {
     #[primary_key]
     name: &'static str,
     description: &'static str,
-    enabled: bool,
+    available: bool,
 }
 
 #[system_catalog(table, "rw_catalog.rw_features")]
@@ -22,19 +22,13 @@ fn read_rw_features(_reader: &SysCatalogReaderImpl) -> Result<Vec<RwFeature>> {
         .map(|l| l.tier.available_features().collect())
         .unwrap_or_default();
 
-    if available_features.is_empty() {
-        notice_to_user(
-            "All features seem to be disabled. Please check your license. For more information, please refer to https://go.risingwave.com/premium/.",
-        );
-    }
-
     Ok(Feature::all()
         .iter()
         .filter(|f| **f != Feature::TestDummy) // hide test feature
         .map(|f| RwFeature {
             name: f.name(),
             description: f.description(),
-            enabled: available_features.contains(f),
+            available: available_features.contains(f),
         })
         .collect())
 }
