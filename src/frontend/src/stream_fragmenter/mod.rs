fe8d79664f170c8c4eeb143209c2d68011853f43
zwang28 <84491488@qq.com>
2025-07-08 15:44:26 +0800

    add new fragment flag for CDC scan

--- src/frontend/src/stream_fragmenter/mod.rs
+++ src/frontend/src/stream_fragmenter/mod.rs
@@ -414,6 +414,9 @@ fn build_fragment(
                 current_fragment
                     .fragment_type_mask
                     .add(FragmentTypeFlag::StreamScan);
+                current_fragment
+                    .fragment_type_mask
+                    .add(FragmentTypeFlag::StreamCdcScan);
                 if let Some(o) = node.options
                     && CdcScanOptions::from_proto(&o).is_parallelized_backfill()
                 {
