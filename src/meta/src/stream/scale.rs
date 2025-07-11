139b22d794fc8045cfed867b5f2a5764ea391350
zwang28 <84491488@qq.com>
2025-07-11 18:20:41 +0800

    WIP

--- src/meta/src/stream/scale.rs
+++ src/meta/src/stream/scale.rs
@@ -1802,6 +1802,7 @@ impl ScaleController {
             }
 
             for (fragment_id, fragment) in fragments {
+                // TODO(zw): CRITICAL bug. Should keep parallelism unchanged but reschedule actors.
                 // StreamCdcScan's parallelism is determined by CdcScanOptions::backfill_parallelism.
                 if FragmentTypeMask::from(fragment.fragment_type_mask)
                     .contains(FragmentTypeFlag::StreamCdcScan)
