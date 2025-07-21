358d7a3196b6f0c96c9b1c91bffe8ee8c8dc8ce5
Shanicky Chen <peng@risingwave-labs.com>
2025-07-21 20:04:24 +0800

    Use in-memory actor cache for fragment/actor queries; validate with DB; add InflightFragmentInfo fields.
    Signed-off-by: Shanicky Chen <peng@risingwave-labs.com>

--- src/meta/src/barrier/context/recovery.rs
+++ src/meta/src/barrier/context/recovery.rs
@@ -1046,6 +1046,7 @@ impl GlobalBarrierWorkerContextImpl {
             info!(jobs=?reschedule_ids,"post applied reschedule for jobs in offline scaling");
         }
 
+
         info!("scaling actors succeed.");
         Ok(())
     }
