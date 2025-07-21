4c68aa4340c068f7cff15f335973a1a403207e19
Shanicky Chen <peng@risingwave-labs.com>
2025-07-21 20:04:24 +0800

    Use in-memory actor cache for fragment/actor queries; validate with DB; add InflightFragmentInfo fields.
    Signed-off-by: Shanicky Chen <peng@risingwave-labs.com>

--- src/meta/src/rpc/ddl_controller.rs
+++ src/meta/src/rpc/ddl_controller.rs
@@ -2061,7 +2061,7 @@ impl DdlController {
             .map(|fragment| {
                 (
                     fragment.fragment_id,
-                    fragment.actors.iter().map(|actor| actor.actor_id).collect(),
+                    fragment.actors.keys().copied().collect(),
                 )
             })
             .collect();
