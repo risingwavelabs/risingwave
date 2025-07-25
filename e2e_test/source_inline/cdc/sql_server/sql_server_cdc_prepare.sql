11a093121f690bd9febf934d2e5630ff526bbe0f
Chris Wessels <undefined.za@gmail.com>
2025-07-25 13:03:12 +0100

    fix(test): add missing c_money value in SQL Server CDC INSERT statement
    The third INSERT statement for sqlserver_all_data_types table was missing
    the c_money column value, causing "Column name or number of supplied values
    does not match table definition" error at line 79 of sql_server_cdc_prepare.sql.
    
    The table has 22 columns but the INSERT statement only had 21 values.
    Added the missing c_money value (100.0) to match the table definition.
    
    This was accidentally removed in commit ed8a7c9fb4 when adding uint256 tests.

--- e2e_test/source_inline/cdc/sql_server/sql_server_cdc_prepare.sql
+++ e2e_test/source_inline/cdc/sql_server/sql_server_cdc_prepare.sql
@@ -76,7 +76,7 @@ INSERT INTO sqlserver_all_data_types VALUES (1, 'False', 0, 0, 0, 0, 0, 0, 0, ''
 
 INSERT INTO sqlserver_all_data_types VALUES (2, 'True', 255, -32768, -2147483648, -9223372036854775808, -10.0, -9999.999999, -10000.0, 'aa', 'aa', N'🌹', N'🌹', NULL, 0xff, '6f9619ff-8b86-d011-b42d-00c04fc964ff', '1990-01-01', '13:59:59.123', '2000-01-01 11:00:00.123', '1990-01-01 00:00:01.123', '<Person> <Name>Jane Doe</Name> <Age>28</Age> </Person>', 100.5);
 
-INSERT INTO sqlserver_all_data_types VALUES (3, 'True', 127, 32767, 2147483647, 9223372036854775807, -10.0, 9999.999999, 10000.0, 'zzzz', 'zzzz', N'🌹👍', N'🌹👍', 0xffffffff, 0xffffffff, '6F9619FF-8B86-D011-B42D-00C04FC964FF', '2999-12-31', '23:59:59.999', '2099-12-31 23:59:59.999', '2999-12-31 23:59:59.999', '<Name>Jane Doe</Name>');
+INSERT INTO sqlserver_all_data_types VALUES (3, 'True', 127, 32767, 2147483647, 9223372036854775807, -10.0, 9999.999999, 10000.0, 'zzzz', 'zzzz', N'🌹👍', N'🌹👍', 0xffffffff, 0xffffffff, '6F9619FF-8B86-D011-B42D-00C04FC964FF', '2999-12-31', '23:59:59.999', '2099-12-31 23:59:59.999', '2999-12-31 23:59:59.999', '<Name>Jane Doe</Name>', 100.0);
 
 -- Create uint256 test table
 CREATE TABLE uint256_test (
