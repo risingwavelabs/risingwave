1b8f22001370f3057ee7294c58ce19e65f00b76b
William Wen <william123.wen@gmail.com>
2025-07-31 16:37:14 +0800

    use output_input_idx

--- src/frontend/src/optimizer/plan_node/generic/vector_search.rs
+++ src/frontend/src/optimizer/plan_node/generic/vector_search.rs
@@ -18,7 +18,7 @@ use risingwave_common::util::column_index_mapping::ColIndexMapping;
 use risingwave_pb::common::PbDistanceType;
 
 use crate::OptimizerContextRef;
-use crate::expr::{Expr, ExprDisplay, ExprImpl, ExprRewriter, ExprVisitor};
+use crate::expr::{ExprImpl, ExprRewriter, ExprVisitor};
 use crate::optimizer::plan_node::generic::{GenericPlanNode, GenericPlanRef};
 use crate::optimizer::property::FunctionalDependencySet;
 use crate::utils::ColIndexMappingRewriteExt;
@@ -30,8 +30,7 @@ pub struct VectorSearch<PlanRef> {
     pub left: ExprImpl,
     pub right: ExprImpl,
 
-    pub non_distance_columns: Vec<ExprImpl>,
-    pub include_distance: bool,
+    pub output_input_idx: Vec<usize>,
 
     pub input: PlanRef,
 }
@@ -43,8 +42,7 @@ impl<PlanRef: GenericPlanRef> VectorSearch<PlanRef> {
             distance_type: self.distance_type,
             left: self.left.clone(),
             right: self.right.clone(),
-            non_distance_columns: self.non_distance_columns.clone(),
-            include_distance: self.include_distance,
+            output_input_idx: self.output_input_idx.clone(),
             input,
         }
     }
@@ -52,96 +50,43 @@ impl<PlanRef: GenericPlanRef> VectorSearch<PlanRef> {
     pub(crate) fn rewrite_exprs(&mut self, r: &mut dyn ExprRewriter) {
         self.left = r.rewrite_expr(self.left.clone());
         self.right = r.rewrite_expr(self.right.clone());
-        self.non_distance_columns = self
-            .non_distance_columns
-            .iter()
-            .map(|expr| r.rewrite_expr(expr.clone()))
-            .collect();
     }
 
     pub(crate) fn visit_exprs(&self, v: &mut dyn ExprVisitor) {
-        [&self.left, &self.right]
-            .into_iter()
-            .chain(&self.non_distance_columns)
-            .for_each(|expr| {
-                v.visit_expr(expr);
-            });
+        v.visit_expr(&self.left);
+        v.visit_expr(&self.right);
     }
 
-    fn output_len(&self) -> usize {
-        self.non_distance_columns.len() + if self.include_distance { 1 } else { 0 }
-    }
-
-    pub fn o2i_col_mapping(&self) -> ColIndexMapping {
-        let input_len = self.input.schema().len();
-        let mut map = vec![None; self.output_len()];
-        for (i, expr) in self.non_distance_columns.iter().enumerate() {
-            if let ExprImpl::InputRef(input) = expr {
-                map[i] = Some(input.index())
-            }
+    pub(crate) fn i2o_mapping(&self) -> ColIndexMapping {
+        let mut mapping = vec![None; self.input.schema().len()];
+        for (output_idx, input_idx) in self.output_input_idx.iter().enumerate() {
+            mapping[*input_idx] = Some(output_idx);
         }
-        ColIndexMapping::new(map, input_len)
-    }
-
-    /// get the Mapping of columnIndex from input column index to output column index,if a input
-    /// column corresponds more than one out columns, mapping to any one
-    pub fn i2o_col_mapping(&self) -> ColIndexMapping {
-        let input_len = self.input.schema().len();
-        let mut map = vec![None; input_len];
-        for (i, expr) in self.non_distance_columns.iter().enumerate() {
-            if let ExprImpl::InputRef(input) = expr {
-                map[input.index()] = Some(i)
-            }
-        }
-        ColIndexMapping::new(map, self.output_len())
+        ColIndexMapping::new(mapping, self.output_input_idx.len() + 1)
     }
 }
 
 impl<PlanRef: GenericPlanRef> GenericPlanNode for VectorSearch<PlanRef> {
     fn functional_dependency(&self) -> FunctionalDependencySet {
-        let i2o = self.i2o_col_mapping();
-        i2o.rewrite_functional_dependency_set(self.input.functional_dependency().clone())
+        self.i2o_mapping()
+            .rewrite_functional_dependency_set(self.input.functional_dependency().clone())
     }
 
     fn schema(&self) -> Schema {
-        let o2i = self.o2i_col_mapping();
-        let input_schema = self.input.schema();
-        let ctx = self.ctx();
-        let to_field = |(i, expr): (usize, &ExprImpl)| {
-            // Get field info from o2i.
-            let name = match o2i.try_map(i) {
-                Some(input_idx) => input_schema.fields()[input_idx].name.clone(),
-                None => match expr {
-                    ExprImpl::InputRef(_) | ExprImpl::Literal(_) => {
-                        format!("{:?}", ExprDisplay { expr, input_schema })
-                    }
-                    _ => {
-                        format!("$expr{}", ctx.next_expr_display_id())
-                    }
-                },
-            };
-            Field::with_name(expr.return_type(), name)
-        };
         let fields = self
-            .non_distance_columns
+            .output_input_idx
             .iter()
-            .enumerate()
-            .map(to_field)
-            .chain(
-                self.include_distance
-                    .then(|| Field::new("vector_distance", DataType::Float64)),
-            )
+            .map(|idx| self.input.schema()[*idx].clone())
+            .chain([Field::new("vector_distance", DataType::Float64)])
             .collect();
         Schema { fields }
     }
 
     fn stream_key(&self) -> Option<Vec<usize>> {
-        let i2o = self.i2o_col_mapping();
-        self.input
-            .stream_key()?
-            .iter()
-            .map(|pk_col| i2o.try_map(*pk_col))
-            .collect::<Option<Vec<_>>>()
+        self.input.stream_key().and_then(|v| {
+            let i2o_mapping = self.i2o_mapping();
+            v.iter().map(|idx| i2o_mapping.try_map(*idx)).collect()
+        })
     }
 
     fn ctx(&self) -> OptimizerContextRef {
