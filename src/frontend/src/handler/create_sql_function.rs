b43eea104f45a50e58a9b0383c55647f96e2342e
Bugen Zhao <i@bugenzhao.com>
2025-08-04 16:50:02 +0800

    remove leading `\n` from eerror message
    Signed-off-by: Bugen Zhao <i@bugenzhao.com>

--- src/frontend/src/handler/create_sql_function.rs
+++ src/frontend/src/handler/create_sql_function.rs
@@ -147,7 +147,7 @@ pub async fn handle_create_sql_function(
         // Check if the return type mismatches
         if expr.return_type() != return_type {
             return Err(ErrorCode::InvalidInputSyntax(format!(
-                "\nreturn type mismatch detected\nexpected: [{}]\nactual: [{}]\nplease adjust your function definition accordingly",
+                "return type mismatch detected\nexpected: [{}]\nactual: [{}]\nplease adjust your function definition accordingly",
                 return_type,
                 expr.return_type()
             ))
