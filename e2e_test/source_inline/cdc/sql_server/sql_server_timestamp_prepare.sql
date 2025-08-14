561dab2b086920ba5ee2896e4daaf2cb76ea68e8
congyi <15605187270@163.com>
2025-08-14 16:13:33 +0800

    .

--- e2e_test/source_inline/cdc/sql_server/sql_server_timestamp_prepare.sql
+++ e2e_test/source_inline/cdc/sql_server/sql_server_timestamp_prepare.sql
@@ -24,4 +24,4 @@ GO
 INSERT INTO timestamp_test (id, timestamp, time_col) VALUES (1, '9999-01-01 00:00:00.000', '06:00:00.0000000');
 INSERT INTO timestamp_test (id, timestamp, time_col) VALUES (2, '0001-01-01 00:00:00.000', '00:00:00.0000000');
 INSERT INTO timestamp_test (id, timestamp, time_col) VALUES (3, '1972-01-01 00:00:00.000', '12:34:56.1234567');
-GOW
+GO
