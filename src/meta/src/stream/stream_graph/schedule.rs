62866f64eb1ddf9d2748aed57fe749074c20c9af
guluguluhhhh <1322301717@qq.com>
2025-08-19 22:52:09 +0800

    feat: frontend of gap fill

--- src/meta/src/stream/stream_graph/schedule.rs
+++ src/meta/src/stream/stream_graph/schedule.rs
@@ -348,6 +348,17 @@ impl Scheduler {
                             return;
                         }
                     }
+                    NodeBody::GapFill(node) => {
+                        // GapFill node uses buffer_table for vnode count requirement
+                        let buffer_table = node.get_state_table().unwrap();
+                        // Check if vnode_count is a placeholder, skip if so as it will be filled later
+                        if let Some(vnode_count) = buffer_table.vnode_count_inner().value_opt() {
+                            vnode_count
+                        } else {
+                            // Skip this node as vnode_count is still a placeholder
+                            return;
+                        }
+                    }
                     _ => return,
                 };
                 facts.push(Fact::Req {
---
+++ src/stream/src/from_proto/gap_fill.rs
@@ -0,0 +1,84 @@
+// Copyright 2025 RisingWave Labs
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+use risingwave_common::gap_fill_types::FillStrategy;
+use risingwave_expr::expr::build_non_strict_from_prost;
+use risingwave_pb::stream_plan::GapFillNode;
+use risingwave_storage::StateStore;
+
+use super::ExecutorBuilder;
+use crate::common::table::state_table::StateTableBuilder;
+use crate::error::StreamResult;
+use crate::executor::{Executor, GapFillExecutor, GapFillExecutorArgs};
+use crate::task::ExecutorParams;
+
+pub struct GapFillExecutorBuilder;
+
+impl ExecutorBuilder for GapFillExecutorBuilder {
+    type Node = GapFillNode;
+
+    async fn new_boxed_executor(
+        params: ExecutorParams,
+        node: &GapFillNode,
+        store: impl StateStore,
+    ) -> StreamResult<Executor> {
+        let [input]: [_; 1] = params.input.try_into().unwrap();
+
+        let time_column_index = node.get_time_column_index() as usize;
+
+        // Parse interval from ExprNode
+        let interval_expr_node = node.get_interval()?;
+        let interval_expr =
+            build_non_strict_from_prost(interval_expr_node, params.eval_error_report)?;
+
+        let fill_columns: Vec<usize> = node
+            .get_fill_columns()
+            .iter()
+            .map(|&x| x as usize)
+            .collect();
+
+        let fill_strategies: Vec<FillStrategy> = node
+            .get_fill_strategies()
+            .iter()
+            .map(|s| match s.as_str() {
+                "locf" => Ok(FillStrategy::Locf),
+                "interpolate" => Ok(FillStrategy::Interpolate),
+                "null" => Ok(FillStrategy::Null),
+                _ => anyhow::bail!("unknown fill strategy: {}", s),
+            })
+            .collect::<anyhow::Result<_>>()?;
+
+        let fill_columns_with_strategies: Vec<(usize, FillStrategy)> =
+            fill_columns.into_iter().zip(fill_strategies).collect();
+
+        let state_table =
+            StateTableBuilder::new(node.get_state_table().as_ref().unwrap(), store, None)
+                .forbid_preload_all_rows()
+                .build()
+                .await;
+
+        let exec = GapFillExecutor::new(GapFillExecutorArgs {
+            ctx: params.actor_context,
+            input,
+            schema: params.info.schema.clone(),
+            chunk_size: 1024,
+            time_column_index,
+            fill_columns: fill_columns_with_strategies,
+            gap_interval: interval_expr,
+            state_table,
+        });
+
+        Ok((params.info, exec).into())
+    }
+}
