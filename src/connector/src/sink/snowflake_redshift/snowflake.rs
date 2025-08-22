28ce9e80ec79d8251cc33b762973eb6b175fae0f
xxhZs <1060434431@qq.com>
2025-08-22 16:31:07 +0800

    add snowflake support

--- src/connector/src/sink/snowflake_redshift/snowflake.rs
+++ src/connector/src/sink/snowflake_redshift/snowflake.rs
@@ -307,6 +307,7 @@ impl Sink for SnowflakeSink {
             let client = SnowflakeJniClient::new(client, snowflake_task_ctx);
             client.execute_create_table().await?;
             client.execute_create_pipe().await?;
+            println!("Snowflake sink das");
         }
 
         Ok(())
@@ -669,7 +670,6 @@ impl SinkCommitCoordinator for SnowflakeSinkCommitter {
         let client = self.client.as_mut().ok_or_else(|| {
             SinkError::Config(anyhow!("Snowflake sink committer is not initialized."))
         })?;
-        client.execute_flush_pipe().await?;
 
         if let Some(add_columns) = add_columns {
             client
@@ -830,27 +830,6 @@ impl SnowflakeJniClient {
         Ok(())
     }
 
-    pub async fn execute_drop_pipe(&self) -> Result<()> {
-        if let Some(pipe_name) = &self.snowflake_task_context.pipe_name {
-            let drop_pipe_sql = build_drop_pipe_sql(
-                &self.snowflake_task_context.database,
-                &self.snowflake_task_context.schema_name,
-                pipe_name,
-            );
-            if self
-                .jdbc_client
-                .execute_sql_sync(vec![drop_pipe_sql])
-                .await
-                .is_err()
-            {
-                tracing::warn!("Failed to drop Snowflake sink pipe {:?}", pipe_name);
-            } else {
-                tracing::info!("Snowflake sink pipe {:?} dropped", pipe_name);
-            }
-        }
-        Ok(())
-    }
-
     pub async fn execute_flush_pipe(&self) -> Result<()> {
         if let Some(pipe_name) = &self.snowflake_task_context.pipe_name {
             let flush_pipe_sql = build_flush_pipe_sql(
@@ -942,11 +921,6 @@ fn build_create_pipe_sql(
 fn build_flush_pipe_sql(database: &str, schema: &str, pipe_name: &str) -> String {
     let pipe_name = format!(r#""{}"."{}"."{}""#, database, schema, pipe_name);
     format!("ALTER PIPE {} REFRESH;", pipe_name,)
-}
-
-fn build_drop_pipe_sql(database: &str, schema: &str, pipe_name: &str) -> String {
-    let pipe_name = format!(r#""{}"."{}"."{}""#, database, schema, pipe_name);
-    format!("DROP PIPE IF EXISTS {};", pipe_name)
 }
 
 fn build_alter_add_column_sql(
