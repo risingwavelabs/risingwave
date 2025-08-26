9908e9ee0cce2843509e22f55d9cb63f1e07d1f6
Floris Smit <florissmit10@gmail.com>
2025-08-26 16:20:54 +0200

    Add single shared client per mqtt broker

--- src/connector/src/source/mqtt/enumerator/mod.rs
+++ src/connector/src/source/mqtt/enumerator/mod.rs
@@ -12,49 +12,61 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-use std::sync::Arc;
 use std::sync::atomic::AtomicBool;
+use std::sync::{Arc, LazyLock, Weak};
 
 use async_trait::async_trait;
+use moka::future::Cache as MokaCache;
+use moka::ops::compute::Op;
 use risingwave_common::bail;
-use rumqttc::v5::{ConnectionError, Event, Incoming};
+use rumqttc::v5::{AsyncClient, ConnectionError, Event, EventLoop, Incoming};
 use thiserror_ext::AsReport;
 
 use super::MqttProperties;
 use super::source::MqttSplit;
-use crate::error::ConnectorResult;
+use crate::error::{ConnectorError, ConnectorResult};
 use crate::source::{SourceEnumeratorContextRef, SplitEnumerator};
+
+/// Consumer client is shared, and the cache doesn't manage the lifecycle, so we store `Weak` and no eviction.
+static SHARED_MQTT_CLIENT: LazyLock<MokaCache<String, Weak<MqttConnectionCheck>>> =
+    LazyLock::new(|| moka::future::Cache::builder().build());
 
 pub struct MqttSplitEnumerator {
     topic: String,
-    #[expect(dead_code)]
-    client: rumqttc::v5::AsyncClient,
+    broker: String,
+    connection_check: Arc<MqttConnectionCheck>,
+}
+
+struct MqttConnectionCheck {
+    _client: AsyncClient,
     connected: Arc<AtomicBool>,
     stopped: Arc<AtomicBool>,
 }
 
-#[async_trait]
-impl SplitEnumerator for MqttSplitEnumerator {
-    type Properties = MqttProperties;
-    type Split = MqttSplit;
+impl MqttConnectionCheck {
+    fn new(client: AsyncClient, event_loop: EventLoop, topic: String) -> Self {
+        let this = Self {
+            _client: client,
+            connected: Arc::new(AtomicBool::new(false)),
+            stopped: Arc::new(AtomicBool::new(false)),
+        };
+        this.spawn_client_loop(event_loop, topic);
+        this
+    }
 
-    async fn new(
-        properties: Self::Properties,
-        context: SourceEnumeratorContextRef,
-    ) -> ConnectorResult<MqttSplitEnumerator> {
-        let (client, mut eventloop) = properties.common.build_client(context.info.source_id, 0)?;
-        let topic = properties.topic.clone();
+    fn is_connected(&self) -> bool {
+        self.connected.load(std::sync::atomic::Ordering::Relaxed)
+    }
 
-        let connected = Arc::new(AtomicBool::new(false));
-        let connected_clone = connected.clone();
-
-        let stopped = Arc::new(AtomicBool::new(false));
-        let stopped_clone = stopped.clone();
-
+    fn spawn_client_loop(&self, mut event_loop: EventLoop, topic: String) {
+        let connected_clone = self.connected.clone();
+        let stopped_clone = self.stopped.clone();
         tokio::spawn(async move {
             while !stopped_clone.load(std::sync::atomic::Ordering::Relaxed) {
-                match eventloop.poll().await {
-                    Ok(Event::Incoming(Incoming::ConnAck(_))) => {
+                match event_loop.poll().await {
+                    Ok(Event::Incoming(Incoming::ConnAck(_)))
+                        if !connected_clone.load(std::sync::atomic::Ordering::Relaxed) =>
+                    {
                         connected_clone.store(true, std::sync::atomic::Ordering::Relaxed);
                     }
                     Ok(_)
@@ -71,22 +83,65 @@ impl SplitEnumerator for MqttSplitEnumerator {
                 }
             }
         });
+    }
+}
+
+impl Drop for MqttConnectionCheck {
+    fn drop(&mut self) {
+        self.stopped
+            .store(true, std::sync::atomic::Ordering::Relaxed);
+    }
+}
+
+#[async_trait]
+impl SplitEnumerator for MqttSplitEnumerator {
+    type Properties = MqttProperties;
+    type Split = MqttSplit;
+
+    async fn new(
+        properties: Self::Properties,
+        context: SourceEnumeratorContextRef,
+    ) -> ConnectorResult<MqttSplitEnumerator> {
+        let broker_url = properties.common.url.clone();
+        let mut connection_check: Option<Arc<MqttConnectionCheck>> = None;
+
+        SHARED_MQTT_CLIENT
+            .entry_by_ref(&properties.common.url)
+            .and_try_compute_with::<_, _, ConnectorError>(|entry| async {
+                if let Some(_connection_check) = entry.and_then(|e| e.into_value().upgrade()) {
+                    // return if the client is already built
+                    tracing::debug!("reuse existing mqtt client for {}", broker_url);
+                    connection_check = Some(_connection_check);
+                    return Ok(Op::Nop);
+                } else {
+                    tracing::debug!("build new mqtt client for {}", broker_url);
+                    let (new_client, event_loop) =
+                        properties.common.build_client(context.info.source_id, 0)?;
+                    let _connection_check = Arc::new(MqttConnectionCheck::new(new_client, event_loop, properties.topic.clone()));
+                    connection_check = Some(_connection_check.clone());
+                    Ok(Op::Put(Arc::downgrade(&_connection_check)))
+                }
+            })
+            .await?;
+
+        let Some(client_wrapper) = connection_check else {
+            bail!("failed to create or get mqtt client for {}", broker_url);
+        };
+
         Ok(Self {
             topic: properties.topic,
-            client,
-            connected,
-            stopped,
+            broker: broker_url,
+            connection_check: client_wrapper,
         })
     }
 
     async fn list_splits(&mut self) -> ConnectorResult<Vec<MqttSplit>> {
-        if !self.connected.load(std::sync::atomic::Ordering::Relaxed) {
+        if !self.connection_check.is_connected() {
             let start = std::time::Instant::now();
             loop {
-                if self.connected.load(std::sync::atomic::Ordering::Relaxed) {
+                if self.connection_check.is_connected() {
                     break;
-                }
-
+                };
                 if start.elapsed().as_secs() > 10 {
                     bail!("Failed to connect to mqtt broker");
                 }
@@ -94,13 +149,7 @@ impl SplitEnumerator for MqttSplitEnumerator {
                 tokio::time::sleep(std::time::Duration::from_millis(500)).await;
             }
         }
+        tracing::debug!("found new splits {} for broker {}", self.topic, self.broker);
         Ok(vec![MqttSplit::new(self.topic.clone())])
-    }
-}
-
-impl Drop for MqttSplitEnumerator {
-    fn drop(&mut self) {
-        self.stopped
-            .store(true, std::sync::atomic::Ordering::Relaxed);
     }
 }
