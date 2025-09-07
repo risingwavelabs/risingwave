0f891d4b5e420767a2c24c749c7ffdb66113c286
Noel Kwan <noelkwan1998@gmail.com>
2025-09-07 11:26:30 +0800

    add metrics reader

---
+++ src/common/src/metrics_reader/mod.rs
@@ -0,0 +1,32 @@
+// Copyright 2025 RisingWave Labs
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+use anyhow::Result;
+use risingwave_pb::monitor_service::{GetChannelDeltaStatsRequest, GetChannelDeltaStatsResponse};
+
+/// Trait for reading metrics from the meta node via RPC calls.
+#[async_trait::async_trait]
+pub trait MetricsReader: Send + Sync {
+    /// Fetches channel delta statistics from the meta node.
+    ///
+    /// # Arguments
+    /// * `request` - The request containing time parameters for the metrics query
+    ///
+    /// # Returns
+    /// * `Result<GetChannelDeltaStatsResponse>` - The channel delta stats response or an error
+    async fn get_channel_delta_stats(
+        &self,
+        request: GetChannelDeltaStatsRequest,
+    ) -> Result<GetChannelDeltaStatsResponse>;
+}
