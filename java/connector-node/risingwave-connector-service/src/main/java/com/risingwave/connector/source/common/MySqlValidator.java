84848016f478f5e30f9290b77d249143bce15f6e
wcy-fdu <congyi@risingwave-labs.com>
2025-09-16 15:24:38 +0800

    minor

--- java/connector-node/risingwave-connector-service/src/main/java/com/risingwave/connector/source/common/MySqlValidator.java
+++ java/connector-node/risingwave-connector-service/src/main/java/com/risingwave/connector/source/common/MySqlValidator.java
@@ -310,9 +310,7 @@ public class MySqlValidator extends DatabaseValidator implements AutoCloseable {
                 return Data.DataType.TypeName.INT32_VALUE <= val
                         && val <= Data.DataType.TypeName.INT64_VALUE;
             case "bigint":
-                return val == Data.DataType.TypeName.INT64_VALUE
-                        || val == Data.DataType.TypeName.VARCHAR_VALUE
-                        || val == Data.DataType.TypeName.DECIMAL_VALUE;
+                return val == Data.DataType.TypeName.INT64_VALUE;
             case "boolean":
             case "bool":
                 return val == Data.DataType.TypeName.BOOLEAN_VALUE;
