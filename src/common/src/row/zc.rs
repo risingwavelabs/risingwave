0ddd4518d6eabbd47d3e0164bf9f7be56eb1414d
Bugen Zhao <i@bugenzhao.com>
2025-09-23 16:40:33 +0800

    implement body
    Signed-off-by: Bugen Zhao <i@bugenzhao.com>

--- src/common/src/row/zc.rs
+++ src/common/src/row/zc.rs
@@ -17,7 +17,7 @@ use musli_zerocopy::buf::Load;
 use musli_zerocopy::{Buf, OwnedBuf, Ref, ZeroCopy};
 
 use crate::row::{OwnedRow, Row};
-use crate::types::{DatumRef, ScalarRefImpl};
+use crate::types::{Datum, DatumRef, ScalarRefImpl, ToOwnedDatum as _};
 
 #[derive(Debug, Copy, Clone, ZeroCopy)]
 #[repr(u8)]
@@ -47,18 +47,42 @@ enum ZcDatum {
     Todo(usize),
 }
 
-#[derive(Debug, Clone)]
-struct Todo;
-
 impl ScalarRefImpl<'_> {
-    fn encode_to(self, buf: &mut OwnedBuf) -> Result<ZcDatum, Todo> {
-        todo!()
+    fn encode_to(self, buf: &mut OwnedBuf, todo: &mut Vec<Datum>) -> ZcDatum {
+        match self {
+            ScalarRefImpl::Int16(v) => ZcDatum::Int16(v),
+            ScalarRefImpl::Int32(v) => ZcDatum::Int32(v),
+            ScalarRefImpl::Int64(v) => ZcDatum::Int64(v),
+            ScalarRefImpl::Float32(v) => ZcDatum::Float32(v.into_inner()),
+            ScalarRefImpl::Float64(v) => ZcDatum::Float64(v.into_inner()),
+            ScalarRefImpl::Utf8(v) => ZcDatum::Utf8(buf.store_unsized(v)),
+            ScalarRefImpl::Bool(v) => ZcDatum::Bool(v),
+            ScalarRefImpl::Bytea(v) => ZcDatum::Bytea(buf.store_unsized(v)),
+
+            _ => {
+                todo.push(self.to_owned_datum());
+                ZcDatum::Todo(todo.len() - 1)
+            }
+        }
     }
 }
 
 impl ZcDatum {
-    fn load(self, buf: &Buf) -> DatumRef<'_> {
-        todo!()
+    fn load<'a>(self, buf: &'a Buf, todo: &'a impl Row) -> DatumRef<'a> {
+        let scalar = match self {
+            ZcDatum::Null => return None,
+            ZcDatum::Todo(index) => return todo.datum_at(index),
+
+            ZcDatum::Int16(v) => ScalarRefImpl::Int16(v),
+            ZcDatum::Int32(v) => ScalarRefImpl::Int32(v),
+            ZcDatum::Int64(v) => ScalarRefImpl::Int64(v),
+            ZcDatum::Float32(v) => ScalarRefImpl::Float32(v.into()),
+            ZcDatum::Float64(v) => ScalarRefImpl::Float64(v.into()),
+            ZcDatum::Utf8(v) => ScalarRefImpl::Utf8(v.load(buf).unwrap()),
+            ZcDatum::Bool(v) => ScalarRefImpl::Bool(v),
+            ZcDatum::Bytea(v) => ScalarRefImpl::Bytea(v.load(buf).unwrap()),
+        };
+        Some(scalar)
     }
 }
 
@@ -84,23 +108,24 @@ pub struct ZcRow {
 }
 
 impl ZcRow {
+    fn zc_datums(&self) -> &[ZcDatum] {
+        self.zc.load(&self.data.buf).unwrap()
+    }
+
     /// Load `ZcDatum` into `DatumRef`.
     fn load_datum(&self, datum: ZcDatum) -> DatumRef<'_> {
-        match datum {
-            ZcDatum::Todo(j) => self.data.remaining.datum_at(j),
-            _ => datum.load(&self.data.buf),
-        }
+        datum.load(&self.data.buf, &self.data.remaining)
     }
 }
 
 impl Row for ZcRow {
     fn datum_at(&self, i: usize) -> DatumRef<'_> {
-        let zc = self.zc.load(&self.data.buf).unwrap()[i];
+        let zc = self.zc_datums()[i];
         self.load_datum(zc)
     }
 
     unsafe fn datum_at_unchecked(&self, i: usize) -> crate::types::DatumRef<'_> {
-        let zc = *unsafe { self.zc.load(&self.data.buf).unwrap().get_unchecked(i) };
+        let zc = *unsafe { self.zc_datums().get_unchecked(i) };
         self.load_datum(zc)
     }
 
@@ -109,14 +134,38 @@ impl Row for ZcRow {
     }
 
     fn iter(&self) -> impl Iterator<Item = crate::types::DatumRef<'_>> {
-        let zcs = self.zc.load(&self.data.buf).unwrap();
-        zcs.iter().map(|zc| self.load_datum(*zc))
+        self.zc_datums().iter().map(|zc| self.load_datum(*zc))
     }
+}
+
+fn row_encode_to<R: Row>(row: R, buf: &mut OwnedBuf, todo: &mut Vec<Datum>) -> Ref<[ZcDatum]> {
+    let len = row.len();
+    let mut zcs = Vec::with_capacity(len);
+
+    for datum in row.iter() {
+        let zc = match datum {
+            Some(scalar) => scalar.encode_to(buf, todo),
+            None => ZcDatum::Null,
+        };
+        zcs.push(zc);
+    }
+
+    buf.store_slice(&zcs)
 }
 
 #[easy_ext::ext(RowZcEncodeExt)]
 impl<R: Row> R {
     pub fn encode(&self) -> ZcRow {
-        todo!()
+        let mut buf = OwnedBuf::new();
+        let mut todo = Vec::new(); // TODO: reserve first
+        let zc = row_encode_to(self, &mut buf, &mut todo);
+
+        ZcRow {
+            data: Box::new(ZcRowData {
+                buf,
+                remaining: OwnedRow::new(todo),
+            }),
+            zc,
+        }
     }
 }
