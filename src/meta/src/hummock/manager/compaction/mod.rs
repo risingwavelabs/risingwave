ede77923b0970ccead3098da271d42261354a4ad
risingwave-ci <113648949+risingwave-ci@users.noreply.github.com>
2025-09-26 14:07:54 +0800

    fix: Add pruning logic to prevent the compactor from incorrectly accessing watermarks. (#22973) (#23314)
    Co-authored-by: Li0k <yuli@singularity-data.com>

--- src/meta/src/hummock/manager/compaction/mod.rs
+++ src/meta/src/hummock/manager/compaction/mod.rs
@@ -858,9 +858,12 @@ impl HummockManager {
                         &mut compact_task,
                         group_config.compaction_config.as_ref(),
                     );
+
+                    let table_ids_to_be_compacted = compact_task.build_compact_table_ids();
+
                     let (pk_prefix_table_watermarks, non_pk_prefix_table_watermarks) = version
                         .latest_version()
-                        .safe_epoch_table_watermarks(&compact_task.existing_table_ids)
+                        .safe_epoch_table_watermarks(&table_ids_to_be_compacted)
                         .into_iter()
                         .partition(|(_table_id, table_watermarke)| {
                             matches!(
