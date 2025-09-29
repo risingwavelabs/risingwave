003975220daf79108a04fc910938900c26b0ff58
William Wen <william123.wen@gmail.com>
2025-09-29 17:36:18 +0800

    feat(vector): extract common vector index reader

--- src/frontend/src/optimizer/plan_node/generic/mod.rs
+++ src/frontend/src/optimizer/plan_node/generic/mod.rs
@@ -93,10 +93,14 @@ mod postgres_query;
 pub use postgres_query::*;
 
 mod mysql_query;
+
 pub use mysql_query::*;
 
 mod locality_provider;
 pub use locality_provider::*;
+
+mod vector_index_lookup_join;
+pub use vector_index_lookup_join::*;
 
 pub trait DistillUnit {
     fn distill_with_name<'a>(&self, name: impl Into<Cow<'a, str>>) -> XmlNode<'a>;
