7fc7dfdb9059b301e9f581144b9a4ce15729fb0b
Bugen Zhao <i@bugenzhao.com>
2025-10-17 17:54:10 +0800

    fmt
    Signed-off-by: Bugen Zhao <i@bugenzhao.com>

--- src/stream/src/common/mod.rs
+++ src/stream/src/common/mod.rs
@@ -14,11 +14,11 @@
 
 pub use column_mapping::*;
 
+pub mod change_buffer;
 mod column_mapping;
 pub mod compact_chunk;
 pub mod log_store_impl;
 pub mod metrics;
 pub mod rate_limit;
-pub mod change_buffer;
 pub mod state_cache;
 pub mod table;
