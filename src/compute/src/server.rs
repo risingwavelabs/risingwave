57f3bf2dacb9677ec39e590f9fb2945ab132ee25
congyi wang <58715567+wcy-fdu@users.noreply.github.com>
2025-11-06 12:53:02 +0800

    fix(mysql-cdc): introduce OpendalSchemaHistory to resolve the schema change loss issue when debezium offline (#23672)

--- src/compute/src/server.rs
+++ src/compute/src/server.rs
@@ -37,7 +37,7 @@ use risingwave_common::telemetry::telemetry_env_enabled;
 use risingwave_common::util::addr::HostAddr;
 use risingwave_common::util::pretty_bytes::convert;
 use risingwave_common::util::tokio_util::sync::CancellationToken;
-use risingwave_common::{GIT_SHA, RW_VERSION};
+use risingwave_common::{DATA_DIRECTORY, GIT_SHA, RW_VERSION, STATE_STORE_URL};
 use risingwave_common_heap_profiling::HeapProfiler;
 use risingwave_common_service::{MetricsManager, ObserverManager, TracingExtractLayer};
 use risingwave_connector::source::iceberg::GLOBAL_ICEBERG_SCAN_METRICS;
@@ -142,6 +142,7 @@ pub async fn compute_node_serve(
     ));
 
     let state_store_url = system_params.state_store();
+    let data_directory = system_params.data_directory();
 
     let embedded_compactor_enabled =
         embedded_compactor_enabled(state_store_url, config.storage.disable_remote_compactor);
@@ -207,7 +208,23 @@ pub async fn compute_node_serve(
             .build()
             .ok(),
     };
+    // Store the state_store_url in a static OnceLock for later use in JNI crate
+    // Check the return value and if the variable is set, assert that the value is the same.
+    if let Err(existing_url) = STATE_STORE_URL.set(state_store_url.to_owned()) {
+        assert_eq!(
+            existing_url, state_store_url,
+            "STATE_STORE_URL already set with different value"
+        );
+    }
 
+    // Store the data_directory in a static OnceLock for later use in JNI crate
+    // To be extra safe, check the return value and if the variable is set, assert that the value is the same
+    if let Err(existing_dir) = DATA_DIRECTORY.set(data_directory.to_owned()) {
+        assert_eq!(
+            existing_dir, data_directory,
+            "DATA_DIRECTORY already set with different value"
+        );
+    }
     LicenseManager::get().refresh(system_params.license_key());
     let state_store = StateStoreImpl::new(
         state_store_url,
