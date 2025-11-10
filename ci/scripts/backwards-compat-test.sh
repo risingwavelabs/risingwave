39497eae3b89db75c1c313b90a8d3f96fbdf3ea6
zwang28 <84491488@qq.com>
2025-11-10 13:44:25 +0800

    install dependency

--- ci/scripts/backwards-compat-test.sh
+++ ci/scripts/backwards-compat-test.sh
@@ -33,6 +33,13 @@ else
     exit 1
 fi
 
+echo "install sqlserver client"
+curl https://packages.microsoft.com/keys/microsoft.asc | sudo apt-key add -
+curl https://packages.microsoft.com/config/ubuntu/20.04/prod.list | sudo tee /etc/apt/sources.list.d/msprod.list
+apt-get update -y
+ACCEPT_EULA=Y DEBIAN_FRONTEND=noninteractive apt-get install -y mssql-tools unixodbc-dev
+export PATH="/opt/mssql-tools/bin/:$PATH"
+
 source e2e_test/backwards-compat-tests/scripts/utils.sh
 
 ################################### Main
