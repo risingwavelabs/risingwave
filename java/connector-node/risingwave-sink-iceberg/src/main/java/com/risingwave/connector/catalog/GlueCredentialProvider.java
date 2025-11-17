9e658a6f53a7d7e8bc1b40c842e27919014cf416
Dylan Chen <zilin@singularity-data.com>
2025-11-17 13:52:01 +0800

    support s3 and glue iam role arn

--- java/connector-node/risingwave-sink-iceberg/src/main/java/com/risingwave/connector/catalog/GlueCredentialProvider.java
+++ java/connector-node/risingwave-sink-iceberg/src/main/java/com/risingwave/connector/catalog/GlueCredentialProvider.java
@@ -20,10 +20,19 @@ import java.util.Map;
 import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
 import software.amazon.awssdk.auth.credentials.AwsCredentials;
 import software.amazon.awssdk.auth.credentials.AwsCredentialsProvider;
+import software.amazon.awssdk.auth.credentials.AwsSessionCredentials;
+import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.sts.StsClient;
+import software.amazon.awssdk.services.sts.StsClientBuilder;
+import software.amazon.awssdk.services.sts.model.AssumeRoleRequest;
+import software.amazon.awssdk.services.sts.model.Credentials;
+import software.amazon.awssdk.utils.StringUtils;
 import software.amazon.awssdk.utils.Validate;
 
 /** This class is used to provide a credential to glue catalog */
 public class GlueCredentialProvider implements AwsCredentialsProvider {
+    private static final String DEFAULT_SESSION_NAME = "risingwave-glue";
     private final AwsCredentials credentials;
 
     private GlueCredentialProvider(AwsCredentials credentials) {
@@ -34,9 +43,55 @@ public class GlueCredentialProvider implements AwsCredentialsProvider {
     }
 
     public static GlueCredentialProvider create(Map<String, String> config) {
-        return new GlueCredentialProvider(
-                AwsBasicCredentials.create(
-                        config.get("glue.access-key-id"), config.get("glue.secret-access-key")));
+        Validate.notNull(config, "Config must not be null");
+        String accessKey =
+                Validate.notNull(
+                        config.get("glue.access-key-id"),
+                        "Glue access key must not be null.",
+                        new Object[0]);
+        String secretKey =
+                Validate.notNull(
+                        config.get("glue.secret-access-key"),
+                        "Glue secret key must not be null.",
+                        new Object[0]);
+
+        AwsCredentials baseCredentials = AwsBasicCredentials.create(accessKey, secretKey);
+        String assumeRoleArn = config.get("glue.iam-role-arn");
+        if (StringUtils.isBlank(assumeRoleArn)) {
+            return new GlueCredentialProvider(baseCredentials);
+        }
+
+        AwsCredentials assumed =
+                assumeRole(
+                        baseCredentials,
+                        assumeRoleArn,
+                        config.get("glue.iam-role-session-name"),
+                        config.get("client.region"));
+        return new GlueCredentialProvider(assumed);
+    }
+
+    private static AwsCredentials assumeRole(
+            AwsCredentials baseCredentials, String roleArn, String sessionName, String region) {
+        StaticCredentialsProvider provider = StaticCredentialsProvider.create(baseCredentials);
+        StsClientBuilder builder = StsClient.builder().credentialsProvider(provider);
+        if (!StringUtils.isBlank(region)) {
+            builder = builder.region(Region.of(region));
+        }
+        try (StsClient stsClient = builder.build()) {
+            AssumeRoleRequest request =
+                    AssumeRoleRequest.builder()
+                            .roleArn(roleArn)
+                            .roleSessionName(
+                                    StringUtils.isBlank(sessionName)
+                                            ? DEFAULT_SESSION_NAME
+                                            : sessionName)
+                            .build();
+            Credentials credentials = stsClient.assumeRole(request).credentials();
+            return AwsSessionCredentials.create(
+                    credentials.accessKeyId(),
+                    credentials.secretAccessKey(),
+                    credentials.sessionToken());
+        }
     }
 
     public AwsCredentials resolveCredentials() {
