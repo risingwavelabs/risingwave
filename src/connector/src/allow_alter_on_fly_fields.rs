5e8dc403d738bb9d3dd5fd7ca249dd660a5e9ee1
wcy-fdu <congyi@risingwave-labs.com>
2025-11-28 15:06:05 +0800

    revert

--- src/connector/src/allow_alter_on_fly_fields.rs
+++ src/connector/src/allow_alter_on_fly_fields.rs
@@ -92,11 +92,6 @@ mod sink_properties {
     impl_sink_name_to_config_type_name!();
 }
 
-// Constants for CDC-related field names that are allowed to be altered on the fly
-const CDC_SOURCE_WAIT_STREAMING_START_TIMEOUT: &str = "cdc.source.wait.streaming.start.timeout";
-const DEBEZIUM_MAX_QUEUE_SIZE: &str = "debezium.max.queue.size";
-const DEBEZIUM_QUEUE_MEMORY_RATIO: &str = "debezium.queue.memory.ratio";
-
 /// Map of source connector names to their `allow_alter_on_fly` field names
 pub static SOURCE_ALLOW_ALTER_ON_FLY_FIELDS: LazyLock<HashMap<String, HashSet<String>>> = LazyLock::new(|| {
     use source_properties::*;
@@ -105,34 +100,34 @@ pub static SOURCE_ALLOW_ALTER_ON_FLY_FIELDS: LazyLock<HashMap<String, HashSet<St
     map.try_insert(
         std::any::type_name::<MysqlCdcProperties>().to_owned(),
         [
-            CDC_SOURCE_WAIT_STREAMING_START_TIMEOUT.to_owned(),
-            DEBEZIUM_MAX_QUEUE_SIZE.to_owned(),
-            DEBEZIUM_QUEUE_MEMORY_RATIO.to_owned(),
+            "cdc.source.wait.streaming.start.timeout".to_owned(),
+            "debezium.max.queue.size".to_owned(),
+            "debezium.queue.memory.ratio".to_owned(),
         ].into_iter().collect(),
     ).unwrap();
     map.try_insert(
         std::any::type_name::<PostgresCdcProperties>().to_owned(),
         [
-            CDC_SOURCE_WAIT_STREAMING_START_TIMEOUT.to_owned(),
-            DEBEZIUM_MAX_QUEUE_SIZE.to_owned(),
-            DEBEZIUM_QUEUE_MEMORY_RATIO.to_owned(),
+            "cdc.source.wait.streaming.start.timeout".to_owned(),
+            "debezium.max.queue.size".to_owned(),
+            "debezium.queue.memory.ratio".to_owned(),
         ].into_iter().collect(),
     ).unwrap();
     map.try_insert(
         std::any::type_name::<SqlServerCdcProperties>().to_owned(),
         [
-            CDC_SOURCE_WAIT_STREAMING_START_TIMEOUT.to_owned(),
-            DEBEZIUM_MAX_QUEUE_SIZE.to_owned(),
-            DEBEZIUM_QUEUE_MEMORY_RATIO.to_owned(),
+            "cdc.source.wait.streaming.start.timeout".to_owned(),
+            "debezium.max.queue.size".to_owned(),
+            "debezium.queue.memory.ratio".to_owned(),
         ].into_iter().collect(),
     ).unwrap();
 
     map.try_insert(
         std::any::type_name::<MongodbCdcProperties>().to_owned(),
         [
-            CDC_SOURCE_WAIT_STREAMING_START_TIMEOUT.to_owned(),
-            DEBEZIUM_MAX_QUEUE_SIZE.to_owned(),
-            DEBEZIUM_QUEUE_MEMORY_RATIO.to_owned(),
+            "cdc.source.wait.streaming.start.timeout".to_owned(),
+            "debezium.max.queue.size".to_owned(),
+            "debezium.queue.memory.ratio".to_owned(),
         ].into_iter().collect(),
     ).unwrap();
     // KafkaProperties
