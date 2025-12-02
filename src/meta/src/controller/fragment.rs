4f5f1f056b0f242542b23a5ae114b12cd968600e
Shanicky Chen <peng@risingwave-labs.com>
2025-12-02 17:14:29 +0800

    tmp

--- src/meta/src/controller/fragment.rs
+++ src/meta/src/controller/fragment.rs
@@ -876,7 +876,7 @@ impl CatalogController {
         let mut actor_map = HashMap::with_capacity(fragment_ids.len());
         for fragment_id in fragment_ids {
             let fragment_info = guard.get_fragment(*fragment_id as _).ok_or_else(|| {
-                anyhow!("fragment {} not found in shared actor info", fragment_id)
+                panic!("fragment {} not found in shared actor info", fragment_id)
             })?;
 
             let actors = fragment_info
