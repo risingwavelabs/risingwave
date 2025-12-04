5147aee4ff2834fddea395604d00f735002b4897
Mingzhuo Yin <yinmingzhuo@gmail.com>
2025-12-04 19:22:00 +0800

    add license
    Signed-off-by: Mingzhuo Yin <yinmingzhuo@gmail.com>

--- src/frontend/src/datafusion/execute.rs
+++ src/frontend/src/datafusion/execute.rs
@@ -1,3 +1,17 @@
+// Copyright 2025 RisingWave Labs
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 use std::sync::Arc;
 
 use pgwire::pg_field_descriptor::PgFieldDescriptor;
@@ -86,7 +100,7 @@ pub async fn execute_datafusion_plan(
     };
     if let Some(timeout) = timeout {
         let exec = async move {
-            if let Err(_) = tokio::time::timeout(timeout, exec).await {
+            if tokio::time::timeout(timeout, exec).await.is_err() {
                 tracing::error!(
                     "Datafusion query execution timeout after {} seconds",
                     timeout.as_secs()
