1037be695eb7de7814d9a0d6261d076abd57c013
Dylan Chen <zilin@singularity-data.com>
2025-12-23 17:22:34 +0800

    add ProjectTopNTransposeRule after topn index selection

--- src/frontend/src/optimizer/rule/mod.rs
+++ src/frontend/src/optimizer/rule/mod.rs
@@ -124,6 +124,8 @@ mod project_eliminate_rule;
 pub use project_eliminate_rule::*;
 mod project_merge_rule;
 pub use project_merge_rule::*;
+mod project_top_n_transpose_rule;
+pub use project_top_n_transpose_rule::*;
 mod top_n_project_transpose_rule;
 pub use top_n_project_transpose_rule::*;
 mod pull_up_correlated_predicate_rule;
@@ -316,6 +318,7 @@ macro_rules! for_all_rules {
             , { SplitNowOrRule }
             , { FilterWithNowToJoinRule }
             , { GenerateSeriesWithNowRule }
+            , { ProjectTopNTransposeRule }
             , { TopNProjectTransposeRule }
             , { TopNOnIndexRule }
             , { TrivialProjectToValuesRule }
