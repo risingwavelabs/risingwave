5574dc93868c0db0aa17e8b2f5e0e08e6222eaa7
Bugen Zhao <i@bugenzhao.com>
2026-01-02 15:19:59 +0800

    update license years
    Signed-off-by: Bugen Zhao <i@bugenzhao.com>

--- src/bench/sink_bench/main.rs
+++ src/bench/sink_bench/main.rs
@@ -1,4 +1,4 @@
-// Copyright 2025 RisingWave Labs
+// Copyright 2024 RisingWave Labs
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -11,6 +11,7 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
+
 #![feature(coroutines)]
 #![feature(proc_macro_hygiene)]
 #![feature(stmt_expr_attributes)]
