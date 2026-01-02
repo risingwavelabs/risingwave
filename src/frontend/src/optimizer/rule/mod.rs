1c9ae7fdb12315cedea1c1129514cc26c579ebc1
Dylan <zilin@risingwave-labs.com>
2026-01-02 15:24:36 +0800

    fix(optimizer): fix topn index selection for scan with output col idx (2.6) (#24240)

--- src/frontend/src/optimizer/rule/mod.rs
+++ src/frontend/src/optimizer/rule/mod.rs
@@ -124,6 +124,10 @@ mod project_eliminate_rule;
 pub use project_eliminate_rule::*;
 mod project_merge_rule;
 pub use project_merge_rule::*;
+mod project_top_n_transpose_rule;
+pub use project_top_n_transpose_rule::*;
+mod top_n_project_transpose_rule;
+pub use top_n_project_transpose_rule::*;
 mod pull_up_correlated_predicate_rule;
 pub use pull_up_correlated_predicate_rule::*;
 mod pull_up_correlated_project_value_rule;
@@ -314,6 +318,8 @@ macro_rules! for_all_rules {
             , { SplitNowOrRule }
             , { FilterWithNowToJoinRule }
             , { GenerateSeriesWithNowRule }
+            , { ProjectTopNTransposeRule }
+            , { TopNProjectTransposeRule }
             , { TopNOnIndexRule }
             , { TrivialProjectToValuesRule }
             , { UnionInputValuesMergeRule }
