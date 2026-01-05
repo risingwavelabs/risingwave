d32e0a0afdf2d9a32e142aff489d92bfd638dee3
wcy-fdu <congyi@risingwave-labs.com>
2026-01-05 14:03:47 +0800

    fix unit test

--- src/connector/src/allow_alter_on_fly_fields.rs
+++ src/connector/src/allow_alter_on_fly_fields.rs
@@ -119,6 +119,7 @@ pub static SOURCE_ALLOW_ALTER_ON_FLY_FIELDS: LazyLock<HashMap<String, HashSet<St
             "cdc.source.wait.streaming.start.timeout".to_owned(),
             "debezium.max.queue.size".to_owned(),
             "debezium.queue.memory.ratio".to_owned(),
+
         ].into_iter().collect(),
     ).unwrap();
 
