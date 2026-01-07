27dba1d0dc7163303e52a1ee47be2d2bff33cf3e
Xiangjin <xiangjin@risingwave-labs.com>
2026-01-07 14:44:06 +0800

    fix key permission

--- ci/postgres-conf/00-setup-ssl-user.sh
+++ ci/postgres-conf/00-setup-ssl-user.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 set -e
+
+# Setup SSL key with correct permissions (must be owned by postgres:postgres and 0600)
+# We copy it to PGDATA which is writable by the postgres user.
+cp /etc/postgresql/server.key.mount /var/lib/postgresql/data/server.key
+chmod 600 /var/lib/postgresql/data/server.key
+
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
     CREATE USER ssl_user WITH PASSWORD 'ssl_password';
 EOSQL
