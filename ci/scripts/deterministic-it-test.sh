2d18e321e0c3310aa60115642c8e19bc56e046d0
Bugen Zhao <i@bugenzhao.com>
2026-01-09 20:30:25 +0800

    use correct profile
    Signed-off-by: Bugen Zhao <i@bugenzhao.com>

--- ci/scripts/deterministic-it-test.sh
+++ ci/scripts/deterministic-it-test.sh
@@ -12,6 +12,11 @@ mkdir -p $LOGDIR
 echo "--- Download artifacts"
 buildkite-agent artifact download simulation-it-test.tar.zst .
 
+# Manually extract instead of using `cargo nextest run --archive-file` to ensure `cargo llvm-cov`
+# can find the object files later.
+echo "--- Extract artifacts"
+tar -xvf simulation-it-test.tar.zst
+
 if [[ -z "${1:-}" ]]; then
   TEST_PATTERN=""
 elif [[ "$1" == "pull-request" ]]; then
@@ -33,12 +38,14 @@ fi
 # This achieves a good balance between the performance of running tests and generating reports.
 # https://github.com/taiki-e/cargo-llvm-cov/issues/335#issuecomment-1890349373
 export LLVM_PROFILE_FILE='/risingwave/target/risingwave-madsim-it-test-%4m.profraw'
+export RW_COVERAGE_CARGO_PROFILE="ci-sim"
 
 echo "--- Run integration tests in deterministic simulation mode"
 seq "$TEST_NUM" | parallel -j 8 --line-buffer "MADSIM_TEST_SEED={} NEXTEST_PROFILE=ci-sim \
  cargo nextest run \
  $NEXTEST_PARTITION_ARG \
  --no-fail-fast \
- --archive-file simulation-it-test.tar.zst \
+ --cargo-metadata target/nextest/cargo-metadata.json \
+ --binaries-metadata target/nextest/binaries-metadata.json \
  $TEST_PATTERN \
  2> $LOGDIR/it-test-{}.log && rm $LOGDIR/it-test-{}.log"
