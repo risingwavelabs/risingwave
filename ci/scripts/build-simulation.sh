66f3a917d1b533ef8d91bc308fda8bde0ff1d5ef
Bugen Zhao <i@bugenzhao.com>
2026-01-09 16:54:14 +0800

    try enable for madsim e2e as well
    Signed-off-by: Bugen Zhao <i@bugenzhao.com>

--- ci/scripts/build-simulation.sh
+++ ci/scripts/build-simulation.sh
@@ -7,9 +7,9 @@ source ci/scripts/common.sh
 
 echo "--- Generate RiseDev CI config"
 cp ci/risedev-components.ci.env risedev-components.user.env
+export RW_BUILD_INSTRUMENT_COVERAGE=1
 
 echo "--- Build deterministic simulation e2e test runner"
-# TODO: set `RW_BUILD_INSTRUMENT_COVERAGE=1` after addressing performance issue under madsim
 risedev sslt-build-all --profile ci-sim --timings
 
 echo "--- Show sccache stats"
@@ -17,7 +17,7 @@ sccache --show-stats
 sccache --zero-stats
 
 echo "--- Build and archive deterministic simulation integration tests"
-RW_BUILD_INSTRUMENT_COVERAGE=1 NEXTEST_PROFILE=ci-sim risedev sarchive-it-test --cargo-profile ci-sim
+NEXTEST_PROFILE=ci-sim risedev sarchive-it-test --cargo-profile ci-sim
 
 echo "--- Show sccache stats"
 sccache --show-stats
