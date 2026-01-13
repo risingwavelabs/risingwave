bf7f8f502bd040bf0cedf58af04b2844bd52bec8
noel <noelkwan1998@gmail.com>
2026-01-13 22:56:18 +0800

    fix node relative usage

--- grafana/dashboard/dev/cluster_essential.py
+++ grafana/dashboard/dev/cluster_essential.py
@@ -175,7 +175,7 @@ def _(outer_panels: Panels):
                 "Memory usage relative to k8s resource limit of container. Only works in K8s environment",
                 [
                     panels.target(
-                        '(avg by(namespace, pod) (container_memory_working_set_bytes{namespace=~"$namespace",pod=~"$pod",container=~"$component"})) / (  sum by(namespace, pod) (kube_pod_container_resource_limits{namespace=~"$namespace", pod=~"$pod", container="$component", resource="memory", unit="byte"}))',
+                        '(avg by(namespace, pod) (container_memory_working_set_bytes{namespace=~"$namespace",pod=~"$pod",container=~"$component"})) / (  sum by(namespace, pod) (kube_pod_container_resource_limits{namespace=~"$namespace", pod=~"$pod", container=~"$component", resource="memory", unit="byte"}))',
                         "avg memory usage @ {{%s}} @ {{%s}}"
                         % (COMPONENT_LABEL, NODE_LABEL),
                     )
