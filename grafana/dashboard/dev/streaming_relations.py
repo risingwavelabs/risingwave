12362c7e753f6131c8c673258006b90e4d438f06
noel <noelkwan1998@gmail.com>
2026-01-16 17:01:51 +0800

    refactor streaming relations to sink and source

--- grafana/dashboard/dev/streaming_relations.py
+++ grafana/dashboard/dev/streaming_relations.py
@@ -84,124 +84,24 @@ def _(outer_panels: Panels):
                         _relation_busy_rate_target(panels, "3m"),
                     ],
                 ),
-
-                panels.subheader("Source"),
-                panels.timeseries_rowsps(
-                    "Source Throughput(rows/s)",
-                    "The figure shows the number of rows read by each source per second.",
+                # FIXME(kwannoel): We should use the max timestamp of the database, rather than cluster level.
+                panels.timeseries_latency(
+                    "Latency of Materialize Views & Sinks",
+                    "The current epoch that the Materialize Executors or Sink Executor are processing. If an MV/Sink's epoch is far behind the others, "
+                    "it's very likely to be the performance bottleneck",
                     [
                         panels.target(
-                            f"sum(rate({metric('stream_source_output_rows_counts')}[$__rate_interval])) by (source_id, source_name, fragment_id)",
-                            "{{source_id}} {{source_name}} (fragment {{fragment_id}})",
+                            # Here we use `min` but actually no much difference. Any of the sampled `current_epoch` makes sense.
+                            f"max(timestamp({metric('stream_mview_current_epoch')}) - {epoch_to_unix_millis(metric('stream_mview_current_epoch'))}/1000) by (table_id) * on(table_id) group_left(table_name) group({metric('table_info')}) by (table_id, table_name)",
+                            "{{table_id}} {{table_name}}",
                         ),
                         panels.target(
-                            f"sum(rate({metric('file_source_input_row_count')}[$__rate_interval])) by (source_id, source_name, fragment_id)",
-                            "{{source_name}} source_id {{source_id}} (fragment {{fragment_id}})",
-                        )
-                    ],
-                ),
-                panels.timeseries_rowsps(
-                    "Source Throughput(rows/s) Per Partition",
-                    "Each query is executed in parallel with a user-defined parallelism. This figure shows the throughput of "
-                    "each parallelism. The throughput of all the parallelism added up is equal to Source Throughput(rows).",
-                    [
-                        panels.target(
-                            f"rate({metric('source_partition_input_count')}[$__rate_interval])",
-                            "actor={{actor_id}} source={{source_id}} partition={{partition}} fragment_id={{fragment_id}}",
-                        )
-                    ],
-                ),
-                panels.timeseries_bytesps(
-                    "Source Throughput(MB/s)",
-                    "The figure shows the number of bytes read by each source per second.",
-                    [
-                        panels.target(
-                            f"(sum by (source_id, source_name, fragment_id)(rate({metric('source_partition_input_bytes')}[$__rate_interval])))/(1000*1000)",
-                            "{{source_id}} {{source_name}} (fragment {{fragment_id}})",
-                        )
-                    ],
-                ),
-                panels.timeseries_bytesps(
-                    "Source Throughput(MB/s) Per Partition",
-                    "Each query is executed in parallel with a user-defined parallelism. This figure shows the throughput of "
-                    "each parallelism. The throughput of all the parallelism added up is equal to Source Throughput(MB/s).",
-                    [
-                        panels.target(
-                            f"(rate({metric('source_partition_input_bytes')}[$__rate_interval]))/(1000*1000)",
-                            "actor={{actor_id}} source={{source_id}} partition={{partition}} fragment_id={{fragment_id}}",
-                        )
-                    ],
-                ),
-                panels.timeseries_rowsps(
-                    "Source Backfill Throughput(rows/s)",
-                    "The figure shows the number of rows read by each source per second.",
-                    [
-                        panels.target(
-                            f"sum(rate({metric('stream_source_backfill_rows_counts')}[$__rate_interval])) by (source_id, source_name, fragment_id)",
-                            "{{source_id}} {{source_name}} (fragment {{fragment_id}})",
+                            f"max(timestamp({metric('log_store_latest_read_epoch')}) - {epoch_to_unix_millis(metric('log_store_latest_read_epoch'))}/1000) by (sink_id, sink_name)",
+                            "{{sink_id}} {{sink_name}} (output)",
                         ),
-                    ],
-                ),
-                panels.timeseries_count(
-                    "Source Upstream Status",
-                    "Monitor each source upstream, 0 means the upstream is not normal, 1 means the source is ready.",
-                    [
                         panels.target(
-                            f"{metric('source_status_is_up')}",
-                            "source_id={{source_id}}, source_name={{source_name}} @ {{%s}}"
-                            % NODE_LABEL,
-                        )
-                    ],
-                ),
-                panels.timeseries_ops(
-                    "Source Split Change Events frequency(events/s)",
-                    "Source Split Change Events frequency by source_id and actor_id",
-                    [
-                        panels.target(
-                            f"rate({metric('stream_source_split_change_event_count')}[$__rate_interval])",
-                            "source={{source_name}} actor={{actor_id}} @ {{%s}}"
-                            % NODE_LABEL,
-                        )
-                    ],
-                ),
-                panels.timeseries_count(
-                    "Kafka Consumer Lag Size",
-                    "Kafka Consumer Lag Size by source_id, partition and actor_id",
-                    [
-                        panels.target(
-                            f"clamp_min({metric('source_kafka_high_watermark')} - on(source_id, partition) group_right() {metric('source_latest_message_id')}, 0)",
-                            "source={{source_id}} partition={{partition}} actor_id={{actor_id}}",
-                        ),
-                    ],
-                ),
-                panels.subheader("Sink"),
-                # TODO: These 2 metrics should be deprecated because they are unaware of Log Store
-                # Let's remove them when all sinks are migrated to Log Store
-                panels.timeseries_rowsps(
-                    "Sink Executor Throughput (rows/s)",
-                    "The number of rows streamed into the SinkExecutor per second. For sinks with 'sink_decouple = true', please refer to the 'Sink Metrics' section",
-                    [
-                        panels.target(
-                            f"sum(rate({metric('stream_sink_input_row_count')}[$__rate_interval])) by (sink_id) * on(sink_id) group_left(sink_name) group({metric('sink_info')}) by (sink_id, sink_name)",
-                            "sink {{sink_id}} {{sink_name}}",
-                        ),
-                        panels.target_hidden(
-                            f"sum(rate({metric('stream_sink_input_row_count')}[$__rate_interval])) by (sink_id, actor_id) * on(actor_id) group_left(sink_name) {metric('sink_info')}",
-                            "sink {{sink_id}} {{sink_name}} - actor {{actor_id}}",
-                        ),
-                    ],
-                ),
-                panels.timeseries_bytesps(
-                    "Sink Executor Throughput (MB/s)",
-                    "The figure shows the number of bytes written SinkExecutor per second. For sinks with 'sink_decouple = true', please refer to the 'Sink Metrics' section",
-                    [
-                        panels.target(
-                            f"(sum(rate({metric('stream_sink_input_bytes')}[$__rate_interval])) by (sink_id) * on(sink_id) group_left(sink_name) group({metric('sink_info')}) by (sink_id, sink_name)) / (1000*1000)",
-                            "sink {{sink_id}} {{sink_name}}",
-                        ),
-                        panels.target_hidden(
-                            f"(sum(rate({metric('stream_sink_input_bytes')}[$__rate_interval])) by (sink_id, actor_id) * on(actor_id) group_left(sink_name) {metric('sink_info')}) / (1000*1000)",
-                            "sink {{sink_id}} {{sink_name}} - actor {{actor_id}}",
+                            f"max(timestamp({metric('log_store_latest_write_epoch')}) - {epoch_to_unix_millis(metric('log_store_latest_write_epoch'))}/1000) by (sink_id, sink_name)",
+                            "{{sink_id}} {{sink_name}} (enqueue)",
                         ),
                     ],
                 ),
@@ -281,26 +181,7 @@ def _(outer_panels: Panels):
                         ),
                     ],
                 ),
-                panels.timeseries_latency(
-                    "Latency of Materialize Views & Sinks",
-                    "The current epoch that the Materialize Executors or Sink Executor are processing. If an MV/Sink's epoch is far behind the others, "
-                    "it's very likely to be the performance bottleneck",
-                    [
-                        panels.target(
-                            # Here we use `min` but actually no much difference. Any of the sampled `current_epoch` makes sense.
-                            f"max(timestamp({metric('stream_mview_current_epoch')}) - {epoch_to_unix_millis(metric('stream_mview_current_epoch'))}/1000) by (table_id) * on(table_id) group_left(table_name) group({metric('table_info')}) by (table_id, table_name)",
-                            "{{table_id}} {{table_name}}",
-                        ),
-                        panels.target(
-                            f"max(timestamp({metric('log_store_latest_read_epoch')}) - {epoch_to_unix_millis(metric('log_store_latest_read_epoch'))}/1000) by (sink_id, sink_name)",
-                            "{{sink_id}} {{sink_name}} (output)",
-                        ),
-                        panels.target(
-                            f"max(timestamp({metric('log_store_latest_write_epoch')}) - {epoch_to_unix_millis(metric('log_store_latest_write_epoch'))}/1000) by (sink_id, sink_name)",
-                            "{{sink_id}} {{sink_name}} (enqueue)",
-                        ),
-                    ],
-                ),
+
                 panels.timeseries_latency(
                     "Snapshot Backfill Lag",
                     "",
