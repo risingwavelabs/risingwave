703bc3f59c36c570ac5d2b9b8e974327abc80d4c
noel <noelkwan1998@gmail.com>
2026-01-16 13:47:28 +0800

    cluster metrics by component

--- grafana/dashboard/dev/__init__.py
+++ grafana/dashboard/dev/__init__.py
@@ -10,35 +10,58 @@ def section(func: Callable[[Panels], list]):
     return func
 
 
-# The import order determines the order of the sections in the dashboard.
+# NOTE: The import order determines the order of the sections in the dashboard.
+
+# Essential Metrics
 from . import cluster_essential as _
+
+# Resource
+from . import resource_utilization as _
+from . import network_connection as _
+from . import memory_manager as _
+
+# Metadata
 from . import streaming_metadata as _
+from . import system_params as _
+
+# Streaming
 from . import streaming_relations as _
-from . import streaming_cdc as _
 from . import streaming_actors as _
 from . import streaming_actors_tokio as _
 from . import streaming_exchange as _
+from . import refresh_manager as _
+
+# Iceberg
+from . import iceberg_metrics as _
+from . import iceberg_compaction_metrics as _
+
+# Source
+from . import streaming_cdc as _
+from . import kinesis_metrics as _
+from . import kafka_metrics as _
+
+# Sink
+from . import sink_metrics as _
+
+# Batch
 from . import batch as _
-from . import hummock_read as _
-from . import hummock_write as _
+
+# Storage
 from . import compaction as _
 from . import object_storage as _
+from . import hummock_read as _
+from . import hummock_write as _
 from . import hummock_tiered_cache as _
 from . import hummock_manager as _
+from . import sync_logstore_metrics as _
+
+# Meta
 from . import backup_manager as _
 from . import grpc_meta as _
-from . import memory_manager as _
-from . import sink_metrics as _
-from . import kafka_metrics as _
-from . import network_connection as _
-from . import iceberg_metrics as _
+
+# Misc
 from . import udf as _
-from . import sync_logstore_metrics as _
-from . import iceberg_compaction_metrics as _
-from . import kinesis_metrics as _
-from . import system_params as _
 from . import vector_search as _
-from . import refresh_manager as _
 
 def generate_panels(panels: Panels):
     return [x for s in sections for x in s(panels)]
