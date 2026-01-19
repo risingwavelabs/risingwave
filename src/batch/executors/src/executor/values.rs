bb8fb793eee5359d6d90c9b23cd2faa9a2ae803a
Mingzhuo Yin <yinmingzhuo@gmail.com>
2026-01-19 07:52:42 +0000

    fix test-only code
    Signed-off-by: Mingzhuo Yin <yinmingzhuo@gmail.com>

--- src/batch/executors/src/executor/values.rs
+++ src/batch/executors/src/executor/values.rs
@@ -35,6 +35,23 @@ pub struct ValuesExecutor {
     chunk_size: usize,
 }
 
+impl ValuesExecutor {
+    #[cfg(test)]
+    pub(crate) fn new(
+        rows: Vec<Vec<BoxedExpression>>,
+        schema: Schema,
+        identity: String,
+        chunk_size: usize,
+    ) -> Self {
+        Self {
+            rows: rows.into_iter(),
+            schema,
+            identity,
+            chunk_size,
+        }
+    }
+}
+
 impl Executor for ValuesExecutor {
     fn schema(&self) -> &Schema {
         &self.schema
