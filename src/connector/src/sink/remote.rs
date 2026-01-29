40a3aa195fb1d09a28fd0cecc278d9f8cce99e20
congyi wang <58715567+wcy-fdu@users.noreply.github.com>
2026-01-29 16:34:39 +0800

    fix(metrics): fix sink commit metrics time unit (#24632)

--- src/connector/src/sink/remote.rs
+++ src/connector/src/sink/remote.rs
@@ -381,7 +381,7 @@ impl LogSinker for RemoteLogSinker {
                 {
                     sink_writer_metrics
                         .sink_commit_duration
-                        .observe(start_time.elapsed().as_millis() as f64);
+                        .observe(start_time.elapsed().as_secs_f64());
                 }
 
                 log_reader.truncate(persisted_offset)?;
