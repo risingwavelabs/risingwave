a429d75073f7550bd9a370d0cccf83c5bd5ebe06
EdwinaZhu <179950011+EdwinaZhu@users.noreply.github.com>
2026-02-02 19:38:43 +0800

    feat: add log store buffer memory metrics
    Signed-off-by: EdwinaZhu <179950011+EdwinaZhu@users.noreply.github.com>

--- src/stream/src/common/log_store_impl/kv_log_store/buffer.rs
+++ src/stream/src/common/log_store_impl/kv_log_store/buffer.rs
@@ -13,6 +13,7 @@
 // limitations under the License.
 
 use std::collections::VecDeque;
+use std::mem::size_of;
 use std::ops::DerefMut;
 use std::sync::Arc;
 
@@ -54,6 +55,92 @@ pub(crate) enum LogStoreBufferItem {
     },
 }
 
+impl EstimateSize for LogStoreBufferItem {
+    fn estimated_heap_size(&self) -> usize {
+        match self {
+            LogStoreBufferItem::StreamChunk { chunk, .. } => chunk.estimated_heap_size(),
+            LogStoreBufferItem::Flushed { vnode_bitmap, .. } => vnode_bitmap.estimated_heap_size(),
+            LogStoreBufferItem::Barrier { schema_change, .. } => {
+                estimate_schema_change(schema_change.as_ref())
+            }
+        }
+    }
+
+    fn estimated_size(&self) -> usize {
+        match self {
+            LogStoreBufferItem::StreamChunk {
+                chunk,
+                start_seq_id: _,
+                end_seq_id: _,
+                flushed: _,
+                chunk_id: _,
+            } => {
+                chunk.estimated_size()
+                    + size_of::<SeqId>() * 2
+                    + size_of::<bool>()
+                    + size_of::<ChunkId>()
+            }
+            LogStoreBufferItem::Flushed {
+                vnode_bitmap,
+                start_seq_id: _,
+                end_seq_id: _,
+                chunk_id: _,
+            } => vnode_bitmap.estimated_size() + size_of::<SeqId>() * 2 + size_of::<ChunkId>(),
+            LogStoreBufferItem::Barrier { schema_change, .. } => {
+                estimate_schema_change(schema_change.as_ref())
+                    + size_of::<Option<PbSinkSchemaChange>>()
+            }
+        }
+    }
+}
+
+fn estimate_schema_change(schema_change: Option<&PbSinkSchemaChange>) -> usize {
+    schema_change
+        .map(|sc| {
+            let mut size = 0;
+            size += estimate_vec(&sc.original_schema, estimate_field);
+            if let Some(op) = &sc.op {
+                use risingwave_pb::stream_plan::sink_schema_change::Op;
+                match op {
+                    Op::AddColumns(add) => {
+                        size += estimate_vec(&add.fields, estimate_field);
+                    }
+                    Op::DropColumns(drop) => {
+                        size += estimate_vec(&drop.column_names, estimate_string);
+                    }
+                }
+            }
+            size
+        })
+        .unwrap_or(0)
+}
+
+fn estimate_field(field: &risingwave_pb::plan_common::Field) -> usize {
+    let mut size = estimate_option(field.data_type.as_ref(), estimate_data_type);
+    size += estimate_string(&field.name);
+    size
+}
+
+fn estimate_data_type(dt: &risingwave_pb::data::DataType) -> usize {
+    let mut size = 0;
+    size += estimate_vec(&dt.field_type, estimate_data_type);
+    size += estimate_vec(&dt.field_names, estimate_string);
+    size += dt.field_ids.capacity() * size_of::<i32>();
+    size
+}
+
+fn estimate_string(s: &String) -> usize {
+    s.capacity()
+}
+
+fn estimate_vec<T>(v: &Vec<T>, elem_est: impl Fn(&T) -> usize) -> usize {
+    v.capacity() * size_of::<T>() + v.iter().map(elem_est).sum::<usize>()
+}
+
+fn estimate_option<T>(opt: Option<&T>, elem_est: impl Fn(&T) -> usize) -> usize {
+    opt.map(elem_est).unwrap_or(0)
+}
+
 struct LogStoreBufferInner {
     /// Items not read by log reader. Newer item at the front
     unconsumed_queue: VecDeque<(u64, LogStoreBufferItem)>,
@@ -71,14 +158,6 @@ struct LogStoreBufferInner {
 }
 
 impl LogStoreBufferInner {
-    fn item_heap_size(item: &LogStoreBufferItem) -> usize {
-        match item {
-            LogStoreBufferItem::StreamChunk { chunk, .. } => chunk.estimated_heap_size(),
-            LogStoreBufferItem::Flushed { vnode_bitmap, .. } => vnode_bitmap.estimated_heap_size(),
-            LogStoreBufferItem::Barrier { .. } => 0,
-        }
-    }
-
     fn update_buffer_metrics(&self) {
         let mut epoch_count = 0;
         let mut row_count = 0;
@@ -87,7 +166,6 @@ impl LogStoreBufferInner {
             match item {
                 LogStoreBufferItem::StreamChunk { chunk, .. } => {
                     row_count += chunk.cardinality();
-                    memory_bytes += Self::item_heap_size(item);
                 }
                 LogStoreBufferItem::Flushed {
                     start_seq_id,
@@ -95,16 +173,15 @@ impl LogStoreBufferInner {
                     ..
                 } => {
                     row_count += (end_seq_id - start_seq_id) as usize;
-                    memory_bytes += Self::item_heap_size(item);
                 }
                 LogStoreBufferItem::Barrier { .. } => {
                     epoch_count += 1;
-                    memory_bytes += Self::item_heap_size(item);
                 }
             }
+            memory_bytes += item.estimated_heap_size();
         }
         for (_, item) in &self.consumed_queue {
-            memory_bytes += Self::item_heap_size(item);
+            memory_bytes += item.estimated_heap_size();
         }
         self.metrics.buffer_unconsumed_epoch_count.set(epoch_count);
         self.metrics.buffer_unconsumed_row_count.set(row_count as _);
