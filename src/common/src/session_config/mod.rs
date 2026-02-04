827769a758ecf9f42c6f276f8d8ee18913c13e72
noel <noelkwan1998@gmail.com>
2026-02-04 10:32:08 -0800

    style: format parallelism constants

--- src/common/src/session_config/mod.rs
+++ src/common/src/session_config/mod.rs
@@ -68,12 +68,10 @@ const DISABLE_DML_RATE_LIMIT: i32 = -1;
 const DISABLE_SINK_RATE_LIMIT: i32 = -1;
 
 /// Default parallelism bound for tables
-const DEFAULT_TABLE_PARALLELISM_BOUND: std::num::NonZeroU64 =
-    std::num::NonZeroU64::new(4).unwrap();
+const DEFAULT_TABLE_PARALLELISM_BOUND: std::num::NonZeroU64 = std::num::NonZeroU64::new(4).unwrap();
 
 /// Default parallelism bound for sources
-const DEFAULT_SOURCE_PARALLELISM_BOUND: std::num::NonZeroU64 =
-    std::num::NonZeroU64::new(4).unwrap();
+const DEFAULT_SOURCE_PARALLELISM_BOUND: std::num::NonZeroU64 = std::num::NonZeroU64::new(4).unwrap();
 
 /// Default to bypass cluster limits iff in debug mode.
 const BYPASS_CLUSTER_LIMITS: bool = cfg!(debug_assertions);
