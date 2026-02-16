323976a22cc78708aa868f285394ef21f4d0515d
Bugen Zhao <i@bugenzhao.com>
2026-02-16 13:18:14 +0800

    allow deprecated protobuf enums in meta model

--- src/meta/model/src/lib.rs
+++ src/meta/model/src/lib.rs
@@ -12,6 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+#![allow(deprecated)]
+
 use std::collections::BTreeMap;
 
 pub use risingwave_common::id::*;
