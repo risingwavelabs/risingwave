0e45e8616c0fa8545be1433781ee972d2628b2e9
risingwave-ci <113648949+risingwave-ci@users.noreply.github.com>
2026-02-17 12:23:09 +0800

    feat(refill): support meta cache refill concurrency limit (#24787) (#24788)
    Signed-off-by: MrCroxx <mrcroxx.cs@gmail.com>
    Co-authored-by: Croxx <mrcroxx@outlook.com>
    (cherry picked from commit 863e271b3af8915e885ff7c691a33c358dae67bb)

--- src/common/src/config/storage.rs
+++ src/common/src/config/storage.rs
@@ -325,6 +325,12 @@ impl Default for CacheEvictionConfig {
 
 #[derive(Clone, Debug, Serialize, Deserialize, DefaultFromSerde, ConfigDoc)]
 pub struct CacheRefillConfig {
+    /// Inflight meta cache refill tasks limit.
+    ///
+    /// 0 for unlimited.
+    #[serde(default = "default::cache_refill::meta_refill_concurrency")]
+    pub meta_refill_concurrency: usize,
+
     /// `SSTable` levels to refill.
     #[serde(default = "default::cache_refill::data_refill_levels")]
     pub data_refill_levels: Vec<u32>,
@@ -1174,6 +1180,10 @@ pub mod default {
     }
 
     pub mod cache_refill {
+        pub fn meta_refill_concurrency() -> usize {
+            0
+        }
+
         pub fn data_refill_levels() -> Vec<u32> {
             vec![]
         }
