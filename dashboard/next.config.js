74994c81eff9163cea8d36b0db66f90275cc9d8c
Yuhao Su <yuhaosu@outlook.com>
2026-02-17 15:54:35 -0600

    init

--- dashboard/next.config.js
+++ dashboard/next.config.js
@@ -20,11 +20,6 @@
 const nextConfig = {
   output: "export",
   trailingSlash: true,
-  eslint: {
-    // We have a separate step for running ESLint in CI.
-    // Ignore to skip the development dependency on `eslint` for production builds.
-    ignoreDuringBuilds: true,
-  },
 }
 
 module.exports = nextConfig
