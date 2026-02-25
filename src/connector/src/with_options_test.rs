4283f5c92e961dc1ef7cd2a7a25f81aeb6a1da31
xxhx <xxhx@xxhxdeMacBook-Pro.local>
2026-02-25 19:52:19 +0800

    fix

--- src/connector/src/with_options_test.rs
+++ src/connector/src/with_options_test.rs
@@ -665,6 +665,15 @@ pub static SOURCE_ALLOW_ALTER_ON_FLY_FIELDS: LazyLock<HashMap<String, HashSet<St
 pub static SINK_ALLOW_ALTER_ON_FLY_FIELDS: LazyLock<HashMap<String, HashSet<String>>> = LazyLock::new(|| {{
     use sink_properties::*;
     let mut map = HashMap::new();{sink_entries}
+    // Jdbc
+    map.try_insert(
+        JdbcSink::SINK_NAME.to_owned(),
+        [
+            "jdbc.url".to_owned(),
+            "user".to_owned(),
+            "password".to_owned(),
+        ].into_iter().collect(),
+    ).unwrap();
     map
 }});
 
@@ -711,15 +720,11 @@ pub fn check_source_allow_alter_on_fly_fields(
             "Unknown source connector: {{connector_name}}"
         )));
     }};
-    let allowed_fields = if connector_name == JdbcSink::SINK_NAME {{
-        SINK_ALLOW_ALTER_ON_FLY_FIELDS.get(JdbcSink::SINK_NAME)
-    }} else {{
-        SOURCE_ALLOW_ALTER_ON_FLY_FIELDS.get(type_name)
-    }}.ok_or_else(||{{
-        ConnectorError::from(anyhow::anyhow!(
-            "No allow_alter_on_fly fields registered for connector: {{connector_name}}"
-        ))
-    }})?;
+    let Some(allowed_fields) = SOURCE_ALLOW_ALTER_ON_FLY_FIELDS.get(type_name) else {{
+    return Err(ConnectorError::from(anyhow::anyhow!(
+        "No allow_alter_on_fly fields registered for connector: {{connector_name}}"
+    )));
+    }};
     for field in fields {{
         if !allowed_fields.contains(field) {{
             return Err(ConnectorError::from(anyhow::anyhow!(
@@ -769,11 +774,15 @@ pub fn check_sink_allow_alter_on_fly_fields(
             "Unknown sink connector: {{sink_name}}"
         )));
     }};
-    let Some(allowed_fields) = SINK_ALLOW_ALTER_ON_FLY_FIELDS.get(type_name) else {{
-        return Err(ConnectorError::from(anyhow::anyhow!(
-            "No allow_alter_on_fly fields registered for sink: {{sink_name}}"
-        )));
-    }};
+    let allowed_fields = if sink_name == JdbcSink::SINK_NAME {{
+        SINK_ALLOW_ALTER_ON_FLY_FIELDS.get(JdbcSink::SINK_NAME)
+    }} else {{
+        SINK_ALLOW_ALTER_ON_FLY_FIELDS.get(type_name)
+    }}.ok_or_else(||{{
+        ConnectorError::from(anyhow::anyhow!(
+            "No allow_alter_on_fly fields registered for connector: {{sink_name}}"
+        ))
+    }})?;
     for field in fields {{
         if !allowed_fields.contains(field) {{
             return Err(ConnectorError::from(anyhow::anyhow!(
