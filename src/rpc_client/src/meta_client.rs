453b160ae9b0bd99d9c96efb5de8adcbe32bb24e
Dylan <zilin@risingwave-labs.com>
2026-03-09 16:48:14 +0800

    feat(iceberg): support rw_iceberg_compaction_schedules (#25013)

--- src/rpc_client/src/meta_client.rs
+++ src/rpc_client/src/meta_client.rs
@@ -1873,6 +1873,14 @@ impl MetaClient {
         Ok(resp.states)
     }
 
+    pub async fn list_iceberg_compaction_status(
+        &self,
+    ) -> Result<Vec<list_iceberg_compaction_status_response::IcebergCompactionStatus>> {
+        let request = ListIcebergCompactionStatusRequest {};
+        let resp = self.inner.list_iceberg_compaction_status(request).await?;
+        Ok(resp.statuses)
+    }
+
     pub async fn list_sink_log_store_tables(
         &self,
     ) -> Result<Vec<list_sink_log_store_tables_response::SinkLogStoreTable>> {
@@ -2534,6 +2542,7 @@ macro_rules! for_all_meta_rpc {
             ,{ stream_client, list_rate_limits, ListRateLimitsRequest, ListRateLimitsResponse }
             ,{ stream_client, list_cdc_progress, ListCdcProgressRequest, ListCdcProgressResponse }
             ,{ stream_client, list_refresh_table_states, ListRefreshTableStatesRequest, ListRefreshTableStatesResponse }
+            ,{ stream_client, list_iceberg_compaction_status, ListIcebergCompactionStatusRequest, ListIcebergCompactionStatusResponse }
             ,{ stream_client, alter_connector_props, AlterConnectorPropsRequest, AlterConnectorPropsResponse }
             ,{ stream_client, get_fragment_by_id, GetFragmentByIdRequest, GetFragmentByIdResponse }
             ,{ stream_client, get_fragment_vnodes, GetFragmentVnodesRequest, GetFragmentVnodesResponse }
