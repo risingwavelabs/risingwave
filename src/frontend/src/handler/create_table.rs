58934b2be37f1eb0ce32ce7397385ff7e3ec52fb
Dylan <zilin@risingwave-labs.com>
2026-03-16 10:48:25 +0800

    feat(iceberg): support iceberg sink commit checkpoint size threshold mb (#25058)

--- src/frontend/src/handler/create_table.rs
+++ src/frontend/src/handler/create_table.rs
@@ -95,11 +95,12 @@ mod col_id_gen;
 pub use col_id_gen::*;
 use risingwave_connector::sink::SinkParam;
 use risingwave_connector::sink::iceberg::{
-    COMPACTION_DELETE_FILES_COUNT_THRESHOLD, COMPACTION_INTERVAL_SEC, COMPACTION_MAX_SNAPSHOTS_NUM,
-    COMPACTION_SMALL_FILES_THRESHOLD_MB, COMPACTION_TARGET_FILE_SIZE_MB,
-    COMPACTION_TRIGGER_SNAPSHOT_COUNT, COMPACTION_TYPE, COMPACTION_WRITE_PARQUET_COMPRESSION,
-    COMPACTION_WRITE_PARQUET_MAX_ROW_GROUP_ROWS, CompactionType, ENABLE_COMPACTION,
-    ENABLE_SNAPSHOT_EXPIRATION, FORMAT_VERSION, ICEBERG_WRITE_MODE_COPY_ON_WRITE,
+    COMMIT_CHECKPOINT_SIZE_THRESHOLD_MB, COMPACTION_DELETE_FILES_COUNT_THRESHOLD,
+    COMPACTION_INTERVAL_SEC, COMPACTION_MAX_SNAPSHOTS_NUM, COMPACTION_SMALL_FILES_THRESHOLD_MB,
+    COMPACTION_TARGET_FILE_SIZE_MB, COMPACTION_TRIGGER_SNAPSHOT_COUNT, COMPACTION_TYPE,
+    COMPACTION_WRITE_PARQUET_COMPRESSION, COMPACTION_WRITE_PARQUET_MAX_ROW_GROUP_ROWS,
+    CompactionType, ENABLE_COMPACTION, ENABLE_SNAPSHOT_EXPIRATION, FORMAT_VERSION,
+    ICEBERG_DEFAULT_COMMIT_CHECKPOINT_SIZE_THRESHOLD_MB, ICEBERG_WRITE_MODE_COPY_ON_WRITE,
     ICEBERG_WRITE_MODE_MERGE_ON_READ, IcebergSink, IcebergWriteMode,
     SNAPSHOT_EXPIRATION_CLEAR_EXPIRED_FILES, SNAPSHOT_EXPIRATION_CLEAR_EXPIRED_META_DATA,
     SNAPSHOT_EXPIRATION_MAX_AGE_MILLIS, SNAPSHOT_EXPIRATION_RETAIN_LAST, WRITE_MODE,
@@ -1827,6 +1828,33 @@ pub async fn create_iceberg_engine_table(
     sink_with.insert(
         COMMIT_CHECKPOINT_INTERVAL.to_owned(),
         commit_checkpoint_interval.to_string(),
+    );
+
+    let commit_checkpoint_size_threshold_mb = handler_args
+        .with_options
+        .get(COMMIT_CHECKPOINT_SIZE_THRESHOLD_MB)
+        .map(|v| v.to_owned())
+        .unwrap_or_else(|| ICEBERG_DEFAULT_COMMIT_CHECKPOINT_SIZE_THRESHOLD_MB.to_string());
+    let commit_checkpoint_size_threshold_mb = commit_checkpoint_size_threshold_mb
+        .parse::<u64>()
+        .map_err(|_| {
+            ErrorCode::InvalidInputSyntax(format!(
+                "{} must be greater than 0: {}",
+                COMMIT_CHECKPOINT_SIZE_THRESHOLD_MB, commit_checkpoint_size_threshold_mb
+            ))
+        })?;
+    if commit_checkpoint_size_threshold_mb == 0 {
+        bail!("{COMMIT_CHECKPOINT_SIZE_THRESHOLD_MB} must be greater than 0");
+    }
+
+    // remove commit_checkpoint_size_threshold_mb from source options, otherwise it will be considered as an unknown field.
+    source.as_mut().map(|x| {
+        x.with_properties
+            .remove(COMMIT_CHECKPOINT_SIZE_THRESHOLD_MB)
+    });
+    sink_with.insert(
+        COMMIT_CHECKPOINT_SIZE_THRESHOLD_MB.to_owned(),
+        commit_checkpoint_size_threshold_mb.to_string(),
     );
     sink_with.insert("create_table_if_not_exists".to_owned(), "true".to_owned());
 
