1ab3df49bb291a8b8432c74c2247008527751ee6
MrCroxx <mrcroxx.cs@gmail.com>
2026-03-17 15:36:17 +0800

    fix: try fix CI
    Signed-off-by: MrCroxx <mrcroxx.cs@gmail.com>

--- ci/scripts/e2e-refill-test.sh
+++ ci/scripts/e2e-refill-test.sh
@@ -24,13 +24,38 @@ shift $((OPTIND -1))
 
 source ci/scripts/common.sh
 
+profile=${profile:-}
+mode=${mode:-}
+
+if [[ -z "$profile" ]]; then
+    echo "Missing required option: -p <profile>" 1>&2
+    exit 1
+fi
+
+if [[ -z "$mode" ]]; then
+    echo "Missing required option: -m <mode>" 1>&2
+    exit 1
+fi
+
 download_and_prepare_rw "$profile" common
+
+cleanup() {
+    risedev ci-kill || true
+}
+trap cleanup EXIT
+
+echo "--- Install Python Dependencies"
+python3 -m pip install --break-system-packages -r ./e2e_test/requirements.txt psycopg2-binary
 
 echo "--- e2e, $mode, refill"
 RUST_LOG="info,risingwave_storage=info" \
 risedev ci-start "$mode"
 
+echo "--- risedev-env"
+risedev show-risedev-env
+
 risedev slt -p 4566 -d dev './e2e_test/refill/table_refill.slt'
 
 echo "--- Kill cluster"
 risedev ci-kill
+trap - EXIT
