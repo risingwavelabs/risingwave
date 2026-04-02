b9e0ebe4ab2bf4e701bc13e1d77fa2c0d9913639
zwang28 <70626450+zwang28@users.noreply.github.com>
2026-04-02 21:42:49 +0800

    fix(metric): fix several grafana panels (#25232)

--- grafana/dashboard/user/streaming.py
+++ grafana/dashboard/user/streaming.py
@@ -5,9 +5,7 @@ from . import section
 @section
 def _(outer_panels: Panels):
     panels = outer_panels.sub_panel()
-    mv_filter = 'executor_identity=~".*MaterializeExecutor.*"'
-    table_type_filter = 'table_type=~"MATERIALIZED_VIEW"'
-    mv_throughput_query = f'sum(rate({metric("stream_executor_row_count", filter=mv_filter)}[$__rate_interval]) * on(actor_id) group_left(materialized_view_id, table_name) (group({metric("table_info", filter=table_type_filter)}) by (actor_id, materialized_view_id, table_name))) by (materialized_view_id, table_name)'
+    mv_throughput_query = f"sum(rate({metric('stream_mview_input_row_count')}[$__rate_interval])) by (table_id) * on(table_id) group_left(table_name) group({metric('table_info')}) by (table_id, table_name)"
 
     return [
         outer_panels.row_collapsed(
@@ -32,8 +30,8 @@ def _(outer_panels: Panels):
                     "The figure shows the number of bytes read by each source per second.",
                     [
                         panels.target(
-                            f"(sum by (source_id)(rate({metric('partition_input_bytes')}[$__rate_interval])))/(1000*1000)",
-                            "source={{source_id}}",
+                            f"(sum by (source_id, source_name, fragment_id)(rate({metric('source_partition_input_bytes')}[$__rate_interval])))/(1000*1000)",
+                            "{{source_id}} {{source_name}} (fragment {{fragment_id}})",
                         )
                     ],
                 ),
@@ -49,11 +47,11 @@ def _(outer_panels: Panels):
                 ),
                 panels.timeseries_rowsps(
                     "Materialized View Throughput (rows/s)",
-                    "The figure shows the number of rows written into each materialized executor actor per second.",
+                    "The figure shows the number of rows written into each materialized view per second.",
                     [
                         panels.target(
                             mv_throughput_query,
-                            "materialized view {{table_name}} table_id {{materialized_view_id}}",
+                            "materialized view {{table_name}} table_id {{table_id}}",
                         )
                     ],
                 ),
@@ -64,7 +62,7 @@ def _(outer_panels: Panels):
                         panels.target(
                             f"""
                                 sum by (table_id) (
-                                  rate({metric('stream_backfill_snapshot_read_row_count', node_filter_enabled=False, table_id_filter_enabled=True)}[$__rate_interval])
+                                  rate({metric('stream_backfill_snapshot_read_row_count', node_filter_enabled=False)}[$__rate_interval])
                                 )
                                 * on(table_id) group_left(table_name) (
                                   group({metric('table_info', node_filter_enabled=False)}) by (table_name, table_id)
@@ -75,13 +73,24 @@ def _(outer_panels: Panels):
                         panels.target(
                             f"""
                                 sum by (table_id) (
-                                  rate({metric('stream_backfill_upstream_output_row_count', node_filter_enabled=False, table_id_filter_enabled=True)}[$__rate_interval])
+                                  rate({metric('stream_backfill_upstream_output_row_count', node_filter_enabled=False)}[$__rate_interval])
                                 )
                                 * on(table_id) group_left(table_name) (
                                   group({metric('table_info', node_filter_enabled=False)}) by (table_name, table_id)
                                 )
                             """,
                             "{{table_id}}: {{table_name}} upstream",
+                        ),
+                        panels.target(
+                            f"""
+                                sum by (table_id) (
+                                    rate({metric('stream_snapshot_backfill_consume_snapshot_row_count', node_filter_enabled=False)}[$__rate_interval])
+                                )
+                                * on(table_id) group_left(table_name) (
+                                  group({metric('table_info', node_filter_enabled=False)}) by (table_name, table_id)
+                                )
+                            """,
+                            "{{table_id}}: {{table_name}} consume snapshot"
                         ),
                     ],
                 ),
