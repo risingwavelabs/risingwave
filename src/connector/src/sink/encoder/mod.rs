7870d8cb53e9d69ed62e74e6bdbd887b5d47fbf4
William Wen <44139337+wenym1@users.noreply.github.com>
2026-06-17 22:49:46 +0800

    feat(sink): support turbopuffer sink (#25984)
    (cherry picked from commit 322043dcf625a80b2ac602fae29d371d3872c807)

--- src/connector/src/sink/encoder/mod.rs
+++ src/connector/src/sink/encoder/mod.rs
@@ -148,6 +148,8 @@ pub enum CustomJsonType {
     Es,
     // starrocks' need jsonb is struct
     StarRocks,
+    // turbopuffer expects serial and decimal attributes to match `int` and `float` schema types.
+    Turbopuffer,
     None,
 }
 
