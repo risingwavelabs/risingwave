92f67e743e57d2f024f5f0fd28ae4a64a39e7e49
Dylan <zilin@risingwave-labs.com>
2026-08-03 18:49:22 +0800

    feat(batch): support await-tree dump for batch tasks (#26515)

--- src/batch/src/rpc/service/task_service.rs
+++ src/batch/src/rpc/service/task_service.rs
@@ -253,7 +253,13 @@ impl BatchServiceImpl {
             "local execute request: plan:{:?} with task id:{:?}",
             plan, task_id
         );
-        let task = BatchTaskExecution::new(&task_id, plan, context, mgr.runtime())?;
+        let task = BatchTaskExecution::new(
+            &task_id,
+            plan,
+            context,
+            mgr.runtime(),
+            mgr.await_tree_reg().cloned(),
+        )?;
         let task = Arc::new(task);
         let (tx, rx) = tokio::sync::mpsc::channel(mgr.config().developer.local_execute_buffer_size);
         if let Err(e) = task
