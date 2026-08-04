be8a123fe27db949a006aea061c8c984e67847d4
zwang28 <70626450+zwang28@users.noreply.github.com>
2026-08-04 15:58:35 +0800

    fix(batch): cap masked fragment mapping parallelism (#26560)

--- src/frontend/src/scheduler/plan_fragmenter.rs
+++ src/frontend/src/scheduler/plan_fragmenter.rs
@@ -240,6 +240,7 @@ impl BatchPlanFragmenter {
                 1,
                 &self.catalog_reader,
                 &self.worker_node_manager,
+                self.batch_parallelism,
             )?),
         )?;
         self.stage_graph = Some(
@@ -311,6 +312,10 @@ impl Query {
 
     pub fn stage(&self, stage_id: StageId) -> &QueryStage {
         &self.stage_graph.stages[&stage_id]
+    }
+
+    pub fn batch_parallelism(&self) -> usize {
+        self.stage_graph.batch_parallelism
     }
 }
 
@@ -925,6 +930,7 @@ impl StageGraph {
                     parallelism,
                     catalog_reader,
                     worker_node_manager,
+                    self.batch_parallelism,
                 )?)
             } else {
                 None
@@ -1180,6 +1186,7 @@ impl BatchPlanFragmenter {
                 parallelism,
                 &self.catalog_reader,
                 &self.worker_node_manager,
+                self.batch_parallelism,
             )?)
         } else {
             None
@@ -1281,7 +1288,7 @@ impl BatchPlanFragmenter {
         let build_table_scan_info = |name, table_catalog: &TableCatalog, scan_range| {
             let vnode_mapping = self
                 .worker_node_manager
-                .fragment_mapping(table_catalog.fragment_id)?;
+                .fragment_mapping(table_catalog.fragment_id, self.batch_parallelism)?;
             let partitions = derive_partitions(scan_range, table_catalog, &vnode_mapping)?;
             let info = TableScanInfo::new(name, partitions);
             Ok(Some(info))
@@ -1343,7 +1350,7 @@ impl BatchPlanFragmenter {
             let table_catalog = lookup_join.right_table();
             let vnode_mapping = self
                 .worker_node_manager
-                .fragment_mapping(table_catalog.fragment_id)?;
+                .fragment_mapping(table_catalog.fragment_id, self.batch_parallelism)?;
             let parallelism = vnode_mapping.iter().sorted().dedup().count();
             Ok(Some(parallelism))
         } else {
