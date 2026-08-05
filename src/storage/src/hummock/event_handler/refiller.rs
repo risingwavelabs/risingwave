383ea057ef8fbc19b788b0961c248e02e6147b7c
zwang28 <84491488@qq.com>
2026-08-05 15:09:26 +0800

    fix(storage): record meta refill success only on success

--- src/storage/src/hummock/event_handler/refiller.rs
+++ src/storage/src/hummock/event_handler/refiller.rs
@@ -964,9 +964,11 @@ impl CacheRefillTask {
                 let now = Instant::now();
                 let res = context.sstable_store.sstable(info, &mut stats).await;
                 stats.discard();
-                GLOBAL_CACHE_REFILL_METRICS
-                    .meta_refill_success_duration
-                    .observe(now.elapsed().as_secs_f64());
+                if res.is_ok() {
+                    GLOBAL_CACHE_REFILL_METRICS
+                        .meta_refill_success_duration
+                        .observe(now.elapsed().as_secs_f64());
+                }
                 drop(permit);
 
                 res
