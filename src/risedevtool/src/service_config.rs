c5df23c8372f70fd05dfc06f2f0d43a8361d9523
StandingMan <jmtangcs@gmail.com>
2026-08-15 19:50:30 +0800

    feat(risedev): support local Doris sink tests
    Signed-off-by: StandingMan <jmtangcs@gmail.com>

--- src/risedevtool/src/service_config.rs
+++ src/risedevtool/src/service_config.rs
@@ -397,6 +397,27 @@ pub struct ClickHouseConfig {
 #[derive(Clone, Debug, PartialEq, Serialize, Deserialize)]
 #[serde(rename_all = "kebab-case")]
 #[serde(deny_unknown_fields)]
+pub struct DorisConfig {
+    #[serde(rename = "use")]
+    phantom_use: Option<String>,
+    pub id: String,
+
+    pub address: String,
+    pub http_port: u16,
+    pub query_port: u16,
+    pub be_http_port: u16,
+
+    pub user: String,
+    pub password: String,
+    pub database: String,
+
+    pub image: String,
+    pub user_managed: bool,
+}
+
+#[derive(Clone, Debug, PartialEq, Serialize, Deserialize)]
+#[serde(rename_all = "kebab-case")]
+#[serde(deny_unknown_fields)]
 pub enum Application {
     Metastore,
     Connector,
@@ -609,6 +630,7 @@ pub enum ServiceConfig {
     Pulsar(PulsarConfig),
     Redis(RedisConfig),
     ClickHouse(ClickHouseConfig),
+    Doris(DorisConfig),
     MySql(MySqlConfig),
     Postgres(PostgresConfig),
     SqlServer(SqlServerConfig),
@@ -638,6 +660,7 @@ pub enum TaskGroup {
     Mqtt,
     Redis,
     ClickHouse,
+    Doris,
     Lakekeeper,
     Moat,
 }
@@ -661,6 +684,7 @@ impl ServiceConfig {
             Self::Pulsar(c) => &c.id,
             Self::Redis(c) => &c.id,
             Self::ClickHouse(c) => &c.id,
+            Self::Doris(c) => &c.id,
             Self::Opendal(c) => &c.id,
             Self::MySql(c) => &c.id,
             Self::Postgres(c) => &c.id,
@@ -695,6 +719,7 @@ impl ServiceConfig {
             Self::Pulsar(c) => Some(c.http_port),
             Self::Redis(c) => Some(c.port),
             Self::ClickHouse(c) => Some(c.http_port),
+            Self::Doris(c) => Some(c.query_port),
             Self::Opendal(_) => None,
             Self::MySql(c) => Some(c.port),
             Self::Postgres(c) => Some(c.port),
@@ -728,6 +753,7 @@ impl ServiceConfig {
             Self::Pulsar(c) => c.user_managed,
             Self::Redis(c) => c.user_managed,
             Self::ClickHouse(c) => c.user_managed,
+            Self::Doris(c) => c.user_managed,
             Self::Opendal(_c) => false,
             Self::MySql(c) => c.user_managed,
             Self::Postgres(c) => c.user_managed,
@@ -762,6 +788,7 @@ impl ServiceConfig {
             ServiceConfig::Pulsar(_) => Pulsar,
             ServiceConfig::Redis(_) => Redis,
             ServiceConfig::ClickHouse(_) => ClickHouse,
+            ServiceConfig::Doris(_) => Doris,
             ServiceConfig::MySql(my_sql_config) => {
                 if matches!(my_sql_config.application, Application::Metastore) {
                     RisingWave
