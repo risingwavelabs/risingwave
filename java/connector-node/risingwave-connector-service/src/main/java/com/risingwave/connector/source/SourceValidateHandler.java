c233c4d9a739d98283be5ace01ec011bd798d9db
xiaochong <xiaochong@risingwave-labs.com>
2026-09-01 19:41:47 -0400

    feat(connector): validate Oracle CDC heartbeat table

--- java/connector-node/risingwave-connector-service/src/main/java/com/risingwave/connector/source/SourceValidateHandler.java
+++ java/connector-node/risingwave-connector-service/src/main/java/com/risingwave/connector/source/SourceValidateHandler.java
@@ -249,6 +249,16 @@ public class SourceValidateHandler {
                 ensureRequiredProps(props, isCdcSourceJob);
                 ensurePropNotBlank(props, DbzConnectorConfig.ORACLE_PDB_NAME);
                 ensurePropNotBlank(props, DbzConnectorConfig.ORACLE_SCHEMA_NAME);
+                if (DbzConnectorConfig.isHeartbeatEnabled(props)) {
+                    ensurePropNotBlank(props, DbzConnectorConfig.ORACLE_HEARTBEAT_TABLE_NAME);
+                }
+                if (props.containsKey(DbzConnectorConfig.HEARTBEAT_ACTION_QUERY_KEY)) {
+                    throw ValidatorUtils.invalidArgument(
+                            String.format(
+                                    "'%s' is generated internally; configure '%s' instead",
+                                    DbzConnectorConfig.HEARTBEAT_ACTION_QUERY_KEY,
+                                    DbzConnectorConfig.ORACLE_HEARTBEAT_TABLE_NAME));
+                }
                 validateQueueMemoryRatio(props);
                 try (var oracleValidator = new OracleValidator(props, isCdcSourceJob)) {
                     oracleValidator.validateAll();
