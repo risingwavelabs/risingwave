24f3006bfa04136cc09d0b3eea8f581d379a9ff4
xiaochong <xiaochong@risingwave-labs.com>
2026-09-08 18:36:35 -0400

    build(risedev): add option to disable the LanceDB sink

--- src/risedevtool/config/src/main.rs
+++ src/risedevtool/config/src/main.rs
@@ -74,6 +74,7 @@ pub enum Components {
     NoBacktrace,
     Udf,
     NoDefaultFeatures,
+    NoLanceDbSink,
     Moat,
     DataFusion,
     Adbc,
@@ -100,6 +101,7 @@ impl Components {
             Self::NoBacktrace => "[Runtime] Disable backtrace",
             Self::Udf => "[Build] Enable UDF",
             Self::NoDefaultFeatures => "[Build] Disable default features",
+            Self::NoLanceDbSink => "[Build] Disable LanceDB sink",
             Self::Moat => "[Component] Enable Moat",
             Self::DataFusion => "[Build] Enable DataFusion",
             Self::Adbc => "[Component] ADBC Snowflake Driver",
@@ -212,6 +214,11 @@ Add --no-default-features to build command.
 Currently, default features are: rw-static-link, all-connectors
 "
             }
+            Self::NoLanceDbSink => {
+                "
+Exclude the LanceDB sink from default connectors to reduce build size.
+Other default connectors remain enabled unless default features are disabled."
+            }
             Self::Moat => {
                 "
 Enable Moat as distributed hybrid cache service."
@@ -250,6 +257,7 @@ This will download the ADBC Snowflake driver shared library (.so/.dylib)."
             "DISABLE_BACKTRACE" => Some(Self::NoBacktrace),
             "ENABLE_UDF" => Some(Self::Udf),
             "DISABLE_DEFAULT_FEATURES" => Some(Self::NoDefaultFeatures),
+            "DISABLE_LANCEDB_SINK" => Some(Self::NoLanceDbSink),
             "ENABLE_MOAT" => Some(Self::Moat),
             "ENABLE_DATAFUSION" => Some(Self::DataFusion),
             "ENABLE_ADBC" => Some(Self::Adbc),
@@ -277,6 +285,7 @@ This will download the ADBC Snowflake driver shared library (.so/.dylib)."
             Self::NoBacktrace => "DISABLE_BACKTRACE",
             Self::Udf => "ENABLE_UDF",
             Self::NoDefaultFeatures => "DISABLE_DEFAULT_FEATURES",
+            Self::NoLanceDbSink => "DISABLE_LANCEDB_SINK",
             Self::Moat => "ENABLE_MOAT",
             Self::DataFusion => "ENABLE_DATAFUSION",
             Self::Adbc => "ENABLE_ADBC",
