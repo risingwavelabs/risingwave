a04fb0488535eaf3cf041d1840a547f3f7b44606
zwang28 <70626450+zwang28@users.noreply.github.com>
2026-09-18 05:52:44 +0000

    fix(stream): use database barrier interval for now (#27118)

--- src/meta/src/barrier/checkpoint/independent_job/creating_job/mod.rs
+++ src/meta/src/barrier/checkpoint/independent_job/creating_job/mod.rs
@@ -107,6 +107,7 @@ impl CreatingStreamingJobControl {
         notifier: Option<&mut NotifierStarter>,
         snapshot_backfill_upstream_tables: HashSet<TableId>,
         snapshot_epoch: u64,
+        barrier_interval_ms: u32,
         since_timestamp_upstream_log_epochs: Option<(&TableLogEpochs, PartialGraphId, u64)>,
         version_stat: &HummockVersionStats,
         term_id: &str,
@@ -173,6 +174,7 @@ impl CreatingStreamingJobControl {
                     partial_graph_manager.pending_barrier_infos(upstream_partial_graph_id),
                     snapshot_epoch,
                     new_upstream_barrier_prev_epoch,
+                    barrier_interval_ms,
                 )?;
             (initial_barrier, Some(barriers_to_inject))
         } else {
@@ -181,6 +183,7 @@ impl CreatingStreamingJobControl {
                     &mut prev_epoch_fake_physical_time,
                     &mut pending_non_checkpoint_barriers,
                     PbBarrierKind::Checkpoint,
+                    barrier_interval_ms,
                 ),
                 None,
             )
@@ -309,6 +312,7 @@ impl CreatingStreamingJobControl {
                 create_mview_tracker,
                 snapshot_backfill_actors,
                 snapshot_epoch,
+                barrier_interval_ms,
                 info: job_info,
                 pending_non_checkpoint_barriers,
             };
@@ -403,6 +407,7 @@ impl CreatingStreamingJobControl {
                     } else {
                         BarrierKind::Barrier
                     },
+                    barrier_interval_ms: upstream_barrier_info.barrier_interval_ms,
                 });
                 prev_epoch = *epoch;
             }
@@ -411,6 +416,7 @@ impl CreatingStreamingJobControl {
             prev_epoch: TracedEpoch::new(Epoch(prev_epoch)),
             curr_epoch: TracedEpoch::new(Epoch(upstream_barrier_info.curr_epoch())),
             kind: BarrierKind::Checkpoint(pending_non_checkpoint_barriers),
+            barrier_interval_ms: upstream_barrier_info.barrier_interval_ms,
         });
         Ok(ret)
     }
@@ -448,6 +454,7 @@ impl CreatingStreamingJobControl {
         pending_upstream_barriers: impl Iterator<Item = &BarrierInfo>,
         snapshot_epoch: u64,
         new_upstream_barrier_prev_epoch: u64,
+        barrier_interval_ms: u32,
     ) -> MetaResult<(BarrierInfo, Vec<BarrierInfo>)> {
         let mut initial_barrier = None;
         let mut barriers = vec![];
@@ -487,6 +494,7 @@ impl CreatingStreamingJobControl {
                         } else {
                             BarrierKind::Barrier
                         },
+                        barrier_interval_ms,
                     },
                 );
                 prev_epoch = *epoch;
@@ -507,6 +515,7 @@ impl CreatingStreamingJobControl {
                     prev_epoch: TracedEpoch::new(Epoch(prev_epoch)),
                     curr_epoch: TracedEpoch::new(Epoch(new_upstream_barrier_prev_epoch)),
                     kind: BarrierKind::Checkpoint(pending_non_checkpoint_barriers),
+                    barrier_interval_ms,
                 },
             );
         } else {
@@ -524,6 +533,7 @@ impl CreatingStreamingJobControl {
                     prev_epoch: TracedEpoch::new(Epoch(prev_epoch)),
                     curr_epoch: TracedEpoch::new(Epoch(first_pending_barrier.prev_epoch())),
                     kind: BarrierKind::Checkpoint(take(&mut pending_non_checkpoint_barriers)),
+                    barrier_interval_ms,
                 },
             );
             prev_epoch = first_pending_barrier.prev_epoch();
@@ -545,6 +555,7 @@ impl CreatingStreamingJobControl {
                         } else {
                             BarrierKind::Barrier
                         },
+                        barrier_interval_ms: pending_barrier.barrier_interval_ms,
                     },
                 );
                 prev_epoch = pending_barrier.curr_epoch();
@@ -587,6 +598,7 @@ impl CreatingStreamingJobControl {
             &mut prev_epoch_fake_physical_time,
             &mut pending_non_checkpoint_barriers,
             PbBarrierKind::Initial,
+            upstream_barrier_info.barrier_interval_ms,
         );
         Ok((
             CreatingStreamingJobStatus::ConsumingSnapshot {
@@ -605,6 +617,7 @@ impl CreatingStreamingJobControl {
                 .collect(),
                 info,
                 snapshot_epoch,
+                barrier_interval_ms: upstream_barrier_info.barrier_interval_ms,
                 pending_non_checkpoint_barriers,
             },
             barrier_info,
@@ -1135,6 +1148,7 @@ mod tests {
                 [].iter(),
                 40,
                 60,
+                1000,
             )
             .unwrap();
 
@@ -1170,11 +1184,13 @@ mod tests {
                 prev_epoch: TracedEpoch::new(Epoch(60)),
                 curr_epoch: TracedEpoch::new(Epoch(65)),
                 kind: BarrierKind::Barrier,
+                barrier_interval_ms: 1000,
             },
             BarrierInfo {
                 prev_epoch: TracedEpoch::new(Epoch(65)),
                 curr_epoch: TracedEpoch::new(Epoch(70)),
                 kind: BarrierKind::Checkpoint(vec![60, 65]),
+                barrier_interval_ms: 1000,
             },
         ];
 
@@ -1184,6 +1200,7 @@ mod tests {
                 pending_upstream_barriers.iter(),
                 40,
                 70,
+                1000,
             )
             .unwrap();
 
@@ -1219,21 +1236,25 @@ mod tests {
                 prev_epoch: TracedEpoch::new(Epoch(66)),
                 curr_epoch: TracedEpoch::new(Epoch(67)),
                 kind: BarrierKind::Barrier,
+                barrier_interval_ms: 1000,
             },
             BarrierInfo {
                 prev_epoch: TracedEpoch::new(Epoch(67)),
                 curr_epoch: TracedEpoch::new(Epoch(68)),
                 kind: BarrierKind::Barrier,
+                barrier_interval_ms: 1000,
             },
             BarrierInfo {
                 prev_epoch: TracedEpoch::new(Epoch(68)),
                 curr_epoch: TracedEpoch::new(Epoch(69)),
                 kind: BarrierKind::Barrier,
+                barrier_interval_ms: 1000,
             },
             BarrierInfo {
                 prev_epoch: TracedEpoch::new(Epoch(69)),
                 curr_epoch: TracedEpoch::new(Epoch(70)),
                 kind: BarrierKind::Barrier,
+                barrier_interval_ms: 1000,
             },
         ];
 
@@ -1243,6 +1264,7 @@ mod tests {
                 pending_upstream_barriers.iter(),
                 60,
                 70,
+                1000,
             )
             .unwrap();
 
@@ -1300,6 +1322,7 @@ mod tests {
                 [].iter(),
                 60,
                 66,
+                1000,
             )
             .unwrap();
 
--- src/meta/src/barrier/rpc.rs
+++ src/meta/src/barrier/rpc.rs
@@ -60,7 +60,7 @@ use tokio::time::{Instant, sleep};
 use tracing::{debug, error, info, warn};
 use uuid::Uuid;
 
-use super::{BarrierKind, TracedEpoch};
+use super::TracedEpoch;
 use crate::barrier::BackfillOrderState;
 use crate::barrier::backfill_order_control::get_nodes_with_backfill_dependencies;
 use crate::barrier::cdc_progress::CdcTableBackfillTracker;
@@ -619,6 +619,7 @@ impl PartialGraphRecoverer<'_> {
     pub(super) fn inject_database_initial_barrier(
         &mut self,
         database_id: DatabaseId,
+        barrier_interval_ms: u32,
         jobs: HashMap<JobId, HashMap<FragmentId, InflightFragmentInfo>>,
         job_extra_info: &HashMap<JobId, StreamingJobExtraInfo>,
         state_table_committed_epochs: &mut HashMap<TableId, u64>,
@@ -792,12 +793,7 @@ impl PartialGraphRecoverer<'_> {
         );
         let prev_epoch = TracedEpoch::new(Epoch(prev_epoch));
         // Use a different `curr_epoch` for each recovery attempt.
-        let curr_epoch = prev_epoch.next();
-        let barrier_info = BarrierInfo {
-            prev_epoch,
-            curr_epoch,
-            kind: BarrierKind::Initial,
-        };
+        let barrier_info = BarrierInfo::new_initial(prev_epoch, barrier_interval_ms);
 
         let mut ongoing_snapshot_backfill_jobs: HashMap<JobId, _> = HashMap::new();
         for (job_id, fragment_infos) in snapshot_backfill_jobs {
@@ -1181,6 +1177,7 @@ impl PartialGraphRecoverer<'_> {
                 upstream_table_ids,
                 snapshot_epoch,
                 committed_epoch,
+                barrier_info.barrier_interval_ms,
                 job_backfill_orders,
                 hummock_version_stats,
                 mutation,
@@ -1300,16 +1297,7 @@ impl ControlStreamManager {
 
                 {
                     let mutation = mutation.clone();
-                    let barrier = Barrier {
-                        epoch: Some(risingwave_pb::data::Epoch {
-                            curr: barrier_info.curr_epoch(),
-                            prev: barrier_info.prev_epoch(),
-                        }),
-                        mutation: mutation.clone().map(|_| BarrierMutation { mutation }),
-                        tracing_context: TracingContext::from_span(barrier_info.curr_epoch.span())
-                            .to_protobuf(),
-                        kind: barrier_info.kind.to_protobuf() as i32,
-                    };
+                    let barrier = barrier_to_protobuf(barrier_info, mutation);
 
                     node.handle
                         .request_sender
@@ -1461,6 +1449,24 @@ impl ControlStreamManager {
                 }
             })
             .collect()
+    }
+}
+
+pub(super) fn barrier_to_protobuf(
+    barrier_info: &BarrierInfo,
+    mutation: Option<Mutation>,
+) -> Barrier {
+    Barrier {
+        epoch: Some(risingwave_pb::data::Epoch {
+            curr: barrier_info.curr_epoch(),
+            prev: barrier_info.prev_epoch(),
+        }),
+        mutation: mutation.map(|mutation| BarrierMutation {
+            mutation: Some(mutation),
+        }),
+        tracing_context: TracingContext::from_span(barrier_info.curr_epoch.span()).to_protobuf(),
+        kind: barrier_info.kind.to_protobuf() as i32,
+        barrier_interval_ms: barrier_info.barrier_interval_ms,
     }
 }
 
